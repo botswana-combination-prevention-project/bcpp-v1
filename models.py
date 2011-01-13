71cc057ccbba33b38c8522766fc381e68a812913
erik van widenfelt <ew2789@gmail.com>
2011-01-13 14:16:14 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_code_lists@2 2d834fd6-8cdc-49e2-960a-59c1fb8158c1

---
+++ models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from bhp_basic_models.models import MyBasicModel
+
+class MyCodeList (MyBasicModel):
+    code = models.CharField("Code",
+        max_length=15,
+        unique=True)
+    short_name = models.CharField("Name",
+        max_length=35)    
+    long_name = models.CharField("Long Name",
+        max_length=255,
+        blank=True)    
+    
+    class Meta:
+        abstract=True
+
+#dx and ssx        
+class DiagnosisCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class SignsSymptomsCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class MedicationCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class BodySiteCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class OrganismCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+#ARV medications
+class ArvCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class ArvDoseStatus (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+
+class ArvModificationCode (MyCodeList):
+    list_ref = models.CharField("List Reference",
+        max_length=35)    
+    
