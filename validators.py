207907ca3ea74d2f4b5b832370790ba563aa9783
erik van widenfelt <ew2789@gmail.com>
2011-01-18 13:38:32 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@20 bf858008-1fca-4ad0-9432-481439aa1b6d

--- validators.py
+++ validators.py
@@ -23,6 +23,13 @@ def date_not_future (value):
     if value > now:
         raise ValidationError(u'Date cannot be a future date. You entered %s' % (value,))
 
+def dob_gt_eq_18 (value):
+    now  = date.today()
+    d18 = 365*18
+    age_in_days = timedelta(days=d18)
+    if value > now - age_in_days:
+        raise ValidationError(u'Participant must be 18yrs or older. Date of birth suggests otherwise. You entered %s ' % (value))
+
 def datetime_is_future (value):
     now  = datetime.today()
     if value < now:
@@ -47,3 +54,11 @@ def datetime_not_before_study_start (value):
     protocol_number = dte.protocol_number
     if value < started:
         raise ValidationError(u'Date and time cannot be before the study started. %s started on %s. You entered %s.' % (protocol_number, started, value,))
+
+def eligible_if_yes (value):        
+    if value != 'Yes':
+        raise ValidationError('If No, Participant is NOT ELIGIBLE and registration cannot continue.')
+        
+def eligible_if_no (value):        
+    if value != 'No':
+        raise ValidationError('If Yes, Participant is NOT ELIGIBLE and registration cannot continue.')        
