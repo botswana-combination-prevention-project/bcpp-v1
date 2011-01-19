4fead55bb495bf41ba075901a84b15834933bb6d
erik van widenfelt <ew2789@gmail.com>
2011-01-19 05:18:23 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@2 72d6f641-25d3-4f20-903c-4c2123898bce

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
-from bhp_study_variables.models import Configuration
 from django.contrib import admin
+from bhp_variables.models import StudySpecific
 
-admin.site.register(Configuration)
+admin.site.register(StudySpecific)
 
--- models.py
+++ models.py
@@ -2,18 +2,29 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from bhp_basic_models.models import MyBasicModel
 
-class Configuration (MyBasicModel):
+class StudySpecific (MyBasicModel):
     protocol_number = models.CharField("Protocol number",
         max_length=10,
-        unique=True)
+        unique=True,
+        help_text="e.g. BHP056, ...")
     protocol_title = models.CharField("Protocol Title",
-        max_length=100)    
-    study_start_datetime = models.DateTimeField("Study Start Date and Time")
-    subject_identifier_seed = models.IntegerField("Subject Identifier Seed (Integer)")
+        max_length=100,
+        help_text="Local name for the protocol, e.g. Mashi, Mmabana, ...")    
+    research_title = models.CharField("Protocol Title",
+        max_length=250,
+        help_text="Protocol title used on the grant")    
+    study_start_datetime = models.DateTimeField("Study Start Date and Time",
+        help_text="This is usually the date at which IRB approval was given OR, if later than IRB approval, the date of site activation"
+        )
+    subject_identifier_seed = models.IntegerField("Subject Identifier Seed (Integer)",
+        help_text="an integer, usually 1000")
     subject_identifier_prefix = models.CharField("Subject Identifier prefix",
-        max_length=3
+        max_length=3,
+        blank=True,
+        help_text="Usually the numeric part of protocol_number. E.g. for BHP056 use '056'"
         )
     device_id = models.IntegerField("device id",
+        help_text="a numeric ID between 10-99 to be part of an identifier that represents the server that allocates an identifier"
         validators = [
             MinValueValidator(10),
             MaxValueValidator(99),
