1bb34c3c5c1eef2555dba8d33b8c64e83104d5d5
erik van widenfelt <ew2789@gmail.com>
2011-02-01 18:42:03 +0000

    dob_gt, dob_lt
    git-svn-id: http://192.168.1.50/svn/bhp_common@25 bf858008-1fca-4ad0-9432-481439aa1b6d

--- validators.py
+++ validators.py
@@ -30,6 +30,20 @@ def dob_gt_eq_18 (value):
     if value > now - age_in_days:
         raise ValidationError(u'Participant must be 18yrs or older. Date of birth suggests otherwise. You entered %s ' % (value))
 
+def dob_gt_eq (value, age_in_years):
+    now  = date.today()
+    day_years = 365*age_in_years
+    age_in_days = timedelta(days=day_years)
+    if value => now - age_in_days:
+        raise ValidationError(u'Participant must be %syrs or older. Date of birth suggests otherwise. You entered %s ' % (age_in_years, value))
+
+def dob_lt_eq (value, age_in_years):
+    now  = date.today()
+    day_years = 365*age_in_years
+    age_in_days = timedelta(days=day_years)
+    if value <= now - age_in_days:
+        raise ValidationError(u'Participant must be %syrs or younger. Date of birth suggests otherwise. You entered %s ' % (age_in_years, value))
+
 def datetime_is_future (value):
     now  = datetime.today()
     if value < now:
