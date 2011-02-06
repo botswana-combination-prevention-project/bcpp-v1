6075aeb98aa2a87538c4b651ea66ea3ba3e142d8
erik van widenfelt <ew2789@gmail.com>
2011-02-06 16:36:27 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@33 bf858008-1fca-4ad0-9432-481439aa1b6d

--- validators.py
+++ validators.py
@@ -1,9 +1,16 @@
 from datetime import datetime, date, timedelta
+import re
+from django.core.exceptions import ValidationError
 from django.core.exceptions import ValidationError
 from bhp_variables.models import StudySpecific
 from common import TelephoneNumber
 
 
+def TelephoneNumber(value, pattern, word):
+    str_value = "%s" % (value)
+    p = re.compile(pattern)
+    if p.match(str_value) == None:
+        raise ValidationError(u'Invalid %s number. You entered %s.' % (word, str_value))
 
 def BWCellNumber(value):
     TelephoneNumber(value, '^[7]{1}[123456]{1}[0-9]{6}$', 'cell')
