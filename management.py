61128343fda96d086f20aed69236305f3f446fa0
erik van widenfelt <ew2789@gmail.com>
2011-02-06 17:23:08 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@7 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- management.py
+++
@@ -1,8 +0,0 @@
-from django.dispatch import dispatcher
-from django.db.models import signals
- 
-def my_syncdb_func():
-    # put your code here...
-    
-dispatcher.connect(my_syncdb_func, signal=signals.post_syncdb)
-
