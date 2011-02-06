ee9aa6bc2d8c7dcc9f6b9b7d79c035eaa33fe0c0
erik van widenfelt <ew2789@gmail.com>
2011-02-06 16:36:40 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@34 bf858008-1fca-4ad0-9432-481439aa1b6d

--- common.py
+++
@@ -1,8 +0,0 @@
-import re
-from django.core.exceptions import ValidationError
-
-def TelephoneNumber(value, pattern, word):
-    str_value = "%s" % (value)
-    p = re.compile(pattern)
-    if p.match(str_value) == None:
-        raise ValidationError(u'Invalid %s number. You entered %s.' % (word, str_value))
