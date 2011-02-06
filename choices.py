e4f2cf00414cb084b49554d2b85c24a68ede3c8a
erik van widenfelt <ew2789@gmail.com>
2011-02-06 17:22:13 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@3 a1c8eae8-7d53-4db9-beb7-be5090263015

---
+++ choices.py
@@ -0,0 +1,14 @@
+REGISTRATION_STATUS = (
+    ('SCREENING','SCREENING'),
+    ('REGISTERED','REGISTERED'),
+    ('RANDOMIZED','RANDOMIZED'),            
+)
+
+SUBJECT_TYPE = (
+    ('ADULT','ADULT'),
+    ('MOTHER','MOTHER'),
+    ('INFANT','INFANT'),
+    ('SUBJECT','SUBJECT'),
+    ('INDEX','INDEX'),
+    ('PARTNER','PARTNER'),                                                                                                                            
+)
