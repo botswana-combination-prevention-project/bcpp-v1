5d3a56a5f3524f552a4301ff15b7acccc37cb529
erik van widenfelt <ew2789@gmail.com>
2011-02-07 14:51:03 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@6 72d6f641-25d3-4f20-903c-4c2123898bce

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from bhp_variables.models import StudySpecific
+from models import StudySpecific, StudySite
 
 admin.site.register(StudySpecific)
+admin.site.register(StudySite)
 
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from bhp_common.models import MyBasicModel
+from bhp_common.models import MyBasicUuidModel
+from choices import GENDER_OF_CONSENT
 
-class StudySpecific (MyBasicModel):
+class StudySpecific (MyBasicUuidModel):
     protocol_number = models.CharField("Protocol number",
         max_length=10,
         unique=True,
@@ -16,11 +17,23 @@ class StudySpecific (MyBasicModel):
     study_start_datetime = models.DateTimeField("Study Start Date and Time",
         help_text="This is usually the date at which IRB approval was given OR, if later than IRB approval, the date of site activation"
         )
+    minimum_age_of_consent = models.IntegerField(
+        verbose_name = "Minumum age of consent (>=)",
+        )    
+    maximum_age_of_consent = models.IntegerField(
+        verbose_name = "Maximum age of consent (<=)",
+        )    
+
+    gender_of_consent = models.CharField(
+        verbose_name = "Gender",
+        max_length=3,
+        choices=GENDER_OF_CONSENT,
+        )
+        
     subject_identifier_seed = models.IntegerField("Subject Identifier Seed (Integer)",
         help_text="an integer, usually 1000")
     subject_identifier_prefix = models.CharField("Subject Identifier prefix",
         max_length=3,
-        blank=True,
         help_text="Usually the numeric part of protocol_number. E.g. for BHP056 use '056'"
         )
     subject_identifier_modulus = models.IntegerField("Subject Identifier modulus",
@@ -37,3 +50,15 @@ class StudySpecific (MyBasicModel):
 
     def __unicode__(self):
         return "%s: %s started on %s" % (self.protocol_number, self.protocol_title, self.study_start_datetime)
+        
+        
+class StudySite(MyBasicUuidModel):
+    site_code = models.CharField(max_length=4)        
+    site_name = models.CharField(max_length=35)    
+    
+    def __unicode__(self):
+        return "%s %s" % (self.site_code, self.site_name)
+        
+    class Meta:
+        unique_together = [('site_code', 'site_name')]    
+        
