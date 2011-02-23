915c1c7809975c2c484f35955b2f1eea2d3b6cca
erik van widenfelt <ew2789@gmail.com>
2011-02-23 16:05:12 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@13 a1c8eae8-7d53-4db9-beb7-be5090263015

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from bhp_consent.models import SubjectIdentifierAuditTrail
 from bhp_variables.models import StudySpecific
 from models import RegisteredSubject, RandomizedSubject
 
-def AllocateIdentifier(ObjConsent, user):
+def AllocateIdentifier(ObjConsent, subject_type='subject', user=''):
     
     """
     Allocate an identifier at the time of consent
@@ -46,7 +46,7 @@ def AllocateIdentifier(ObjConsent, user):
     subject_identifier['device_id']= settings.device_id
     
     # using the seed, device and audit trail id determine the integer segment of the id 
-    subject_identifier['int'] = ((subject_identifier['seed'] + subject_identifier['audit_id']) * 100) + subject_identifier['device_id']
+    subject_identifier['int'] = (((subject_identifier['seed']* subject_identifier['device_id']) + subject_identifier['audit_id'])) 
     
     # using the integer segment, calculate the check digit
     check_digit = subject_identifier['int'] % subject_identifier['modulus']
@@ -76,25 +76,92 @@ def AllocateIdentifier(ObjConsent, user):
     audit.subject_identifier = subject_identifier['identifier']
     audit.save()
     
+    RegisterSubject (subject_identifier['identifier'], ObjConsent.pk, ObjConsent.first_name, ObjConsent.initials, subject_type, user)
+    
     # return the new subject identifier to the form currently being save()'d
     return subject_identifier['identifier']
     
+def AllocateInfantIdentifier(ObjParentForm, registered_mother, live_infants, user):
+    
+    """
+    Allocate an identifier at the time of consent
+    
+    generate a mother identifier and save it back to the consent form 
+    and update the identifier audit trail.
+    
+    ObjParentForm is for example, mp005 (delivery form), or the 
+    form that has the live_infants field
+    
+        
+    """
+    
+    subject_identifier = {}
+    
+    #get the consent just created
+    #consent = SubjectConsent.objects.get(pk=new_pk)
 
 
-def RegisterSubject (ObjConsent, subject_type, user):
+    rm = RegisteredSubject.objects.get(subject_consent_id=registered_mother)
+
+    subject_identifier['mother'] = rm.subject_identifier
+    
+    #add a new record in the audit trail for this consent and identifier-'to be'
+    #leave subject_identifier 'null' for now
+    #audit = SubjectIdentifierAuditTrail(
+    #    subject_consent_id=consent, 
+    #    first_name = consent.first_name,
+    #    initials = consent.initials,
+    #    date_allocated=datetime.now(),
+    #    user_created = user,
+    #    )
+    #audit.save()
+
+    # get the number of live births from mp005
+    #if live_infants == 1:
+
+    # we use the mother's consent as the consent pk to store in 
+    # registered subject for this/these infant(s)
+    first_name=''
+    initials=''
+    if live_infants == 1:
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '10')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+    if live_infants == 2:
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '25')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '35')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+    if live_infants == 3:
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '47')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '57')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+        subject_identifier['id'] = "%s-%s" % (subject_identifier['mother'], '67')
+        ret = RegisterSubject(subject_identifier['id'], registered_mother, first_name, initials, 'infant', user)
+           
+
+
+    # update subject_identifier to the audit trail table
+    # audit.subject_identifier = subject_identifier['identifier']
+    # audit.save()
+    
+    # return the new subject identifier to the form currently being save()'d
+    return True
+
+def RegisterSubject (identifier, consent_pk, first_name, initials, subject_type, user):
     
     #try:
     #consent = SubjectConsent.objects.get(pk=subject_consent) 
     
     ObjRS = RegisteredSubject(    
-        subject_identifier = ObjConsent.subject_identifier,
+        subject_identifier = identifier,
         registration_datetime=datetime.now(),
         subject_type = subject_type,
         user_created=user,
         created=datetime.now(),
-        subject_consent_id=ObjConsent.pk,
-        first_name=ObjConsent.first_name,
-        initials=ObjConsent.initials,
+        subject_consent_id=consent_pk,
+        first_name=first_name,
+        initials=initials,
         registration_status='registered',
         )
     
