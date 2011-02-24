e92345f651084a8b8d2cd2d7cb2c7ffbaa09769b
erik van widenfelt <ew2789@gmail.com>
2011-02-24 05:03:35 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@14 a1c8eae8-7d53-4db9-beb7-be5090263015

--- admin.py
+++ admin.py
@@ -3,7 +3,19 @@ from bhp_common.models import MyModelAdmin, MyStackedInline
 from models import RegisteredSubject, OffStudyReason, OffStudy, RandomizedSubject
 from models import DeathCauseInfo, DeathCauseCategory, DeathMedicalResponsibility, DeathReasonHospitalized, DeathForm
 
-admin.site.register(RegisteredSubject)
+
+class RegisteredSubjectAdmin (MyModelAdmin):
+
+  list_display = (
+        'subject_identifier',
+        'subject_consent_id',        
+        'first_name',
+        'initials',
+        'user_created',
+        'created',        
+    )   
+admin.site.register(RegisteredSubject, RegisteredSubjectAdmin)
+
 admin.site.register(RandomizedSubject)
 admin.site.register(OffStudyReason)
 admin.site.register(OffStudy)
