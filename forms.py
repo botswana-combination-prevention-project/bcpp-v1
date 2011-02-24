36f3296feb1ecb652ede1cf6a3401ec5b0e583e1
erik van widenfelt <ew2789@gmail.com>
2011-02-24 14:31:42 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@42 833481ee-4f4c-48f6-8756-2185fbac41e5

--- forms.py
+++ forms.py
@@ -1,5 +1,32 @@
 #from django import forms
 
 
+class BaseSubjectConsentForm(forms.ModelForm):
 
+    def clean(self):
+
+        cleaned_data = self.cleaned_data 
+
+        my_gender = cleaned_data.get("gender")
+           
+        """
+        check omang if identity_type is omang
+        """                        
+        if cleaned_data.get("identity_type") == 'OMANG':
+            check_omang_field(cleaned_data.get("identity"), my_gender)            
+            
+        """
+        check 1st and last letters of initials match subjects name
+        """
+        my_first_name = cleaned_data.get("first_name") 
+        my_last_name = cleaned_data.get("last_name") 
+        my_initials = cleaned_data.get("initials") 
+        check_initials_field(my_first_name, my_last_name, my_initials)
+            
+
+        # Always return the full collection of cleaned data.
+        return cleaned_data    
+    
+    class Meta:
+        abstract = True
 
