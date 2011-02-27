665fc566017109e45bce6881942f636303190139
erik van widenfelt <ew2789@gmail.com>
2011-02-27 20:42:17 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_search@1 76f19ef0-b40f-46b0-95e1-91d90dd81aff

---
+++ forms.py
@@ -0,0 +1,45 @@
+from datetime import date, datetime, timedelta
+from django import forms
+
+class DateRangeSearchForm(forms.Form):
+    date_start = forms.CharField(
+        max_length=10,
+        label="Start Date",
+        help_text="Format is YYYY-MM-DD",
+        error_messages={'required': 'Please enter a valid date.'},
+        initial=date.today()
+        )
+    date_end = forms.CharField(
+        max_length=10,
+        label="End Date",
+        help_text="Format is YYYY-MM-DD",
+        error_messages={'required': 'Please enter a valid date.'},
+        initial=date.today()
+        )
+    def help_text(self):
+        return "Enter the start date and the end date for the period to be listed. Format is YYYY-MM-DD."
+
+class WeekNumberSearchForm(forms.Form):
+    date_start = forms.CharField(
+        max_length=2,
+        label="Start Week Number",
+        help_text="Format is WW",
+        error_messages={'required': 'Please enter a valid week number to start (0-52).'},
+        initial=date.today().isocalendar()[1]
+        )
+    date_end = forms.CharField(
+        max_length=2,
+        label="End Week Number",
+        help_text="Format is WW",
+        error_messages={'required': 'Please enter a valid week number to end (0-52).'},
+        initial=date.today().isocalendar()[1]
+        )
+    year = forms.CharField(
+        max_length=4,
+        label="Year",
+        help_text="Format is YYYY",
+        error_messages={'required': 'Please enter a valid year.'},
+        initial=date.today().isocalendar()[0]
+        )
+    def help_text(self):
+        return "Enter a week number range (0-52) and the year"
