5489480812b8800c78034f03d9f9ee1276663bed
erik van widenfelt <ew2789@gmail.com>
2011-02-28 06:15:38 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@22 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- models/subject_appointment.py
+++ models/subject_appointment.py
@@ -5,61 +5,8 @@ from bhp_common.models import MyBasicListModel, MyBasicUuidModel
 from bhp_common.fields import NameField, DobField, InitialsField
 from bhp_common.validators import datetime_is_future, date_not_future
 from bhp_registration.models import RegisteredSubject
+from bhp_appointment import BaseAppointmentModel
 from bhp_visit.models import VisitDefinition
-from bhp_visit.choices import APPT_STATUS
-
-"""
-    An appointment covers ONE VisitTracking record. 
-    So the user must make an appointment before tracking the visit.
-    
-    Subject must be consented before making an appointment
-    
-"""    
-
-class BaseAppointmentModel (MyBasicUuidModel):
-
-    appt_datetime = models.DateTimeField(
-        verbose_name=_("Appointment date and time"),
-        help_text="",
-        validators=[datetime_is_future,],
-        )
-    appt_status = models.CharField(
-        verbose_name = _("Status"),
-        choices=APPT_STATUS,
-        max_length=25,
-        )
-    contact_tel = models.CharField(
-        verbose_name = _("Contact Tel"), 
-        max_length=250, 
-        blank=True,
-        )
-    comment = models.CharField("Comment", 
-        max_length=250, 
-        blank=True,
-        )        
-        
-    class Meta:
-        abstract=True        
-
-
-class GeneralAppointment(BaseAppointmentModel): 
-
-    first_name = NameField(
-        verbose_name = _("First name"),
-        max_length=25,
-        )
-
-    initials = InitialsField()
-    
-    dob = DobField(
-        validators=[date_not_future,],
-        )
-
-    visit_reason = models.CharField(
-        verbose_name=_("Reason for visit"), 
-        max_length = 25,
-        help_text=_("Reason for visit"),
-        )
 
 
 class RegisteredSubjectAppointment(BaseAppointmentModel):
