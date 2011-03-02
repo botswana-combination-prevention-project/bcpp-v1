7c081ccb27e9719bb0fd872e85fdd855cc73a986
erik van widenfelt <ew2789@gmail.com>
2011-03-02 05:12:08 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_adverse@1 89b61199-f52f-466d-8b3a-10004189116d

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from bhp_common.models import MyModelAdmin, MyStackedInline
+from models import DeathCauseInfo, DeathCauseCategory, DeathMedicalResponsibility, DeathReasonHospitalized, DeathForm
+
+admin.site.register(DeathCauseInfo)
+admin.site.register(DeathCauseCategory)
+admin.site.register(DeathMedicalResponsibility)
+admin.site.register(DeathReasonHospitalized)
+admin.site.register(DeathForm)
