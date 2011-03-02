ef617439f83bf38c89d79cd913d661dbb89d9241
erik van widenfelt <ew2789@gmail.com>
2011-03-02 05:14:07 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@17 a1c8eae8-7d53-4db9-beb7-be5090263015

--- admin.py
+++ admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from bhp_common.models import MyModelAdmin, MyStackedInline
 from models import RegisteredSubject, OffStudyReason, OffStudy, RandomizedSubject
-from models import DeathCauseInfo, DeathCauseCategory, DeathMedicalResponsibility, DeathReasonHospitalized, DeathForm
-
 
 class RegisteredSubjectAdmin (MyModelAdmin):
 
@@ -18,10 +16,8 @@ class RegisteredSubjectAdmin (MyModelAdmin):
 admin.site.register(RegisteredSubject, RegisteredSubjectAdmin)
 
 admin.site.register(RandomizedSubject)
+
 admin.site.register(OffStudyReason)
+
 admin.site.register(OffStudy)
-admin.site.register(DeathCauseInfo)
-admin.site.register(DeathCauseCategory)
-admin.site.register(DeathMedicalResponsibility)
-admin.site.register(DeathReasonHospitalized)
-admin.site.register(DeathForm)
+
