1f8c40f07082ee8f12187270adcd8c856b1e13da
erik van widenfelt <ew2789@gmail.com>
2011-03-02 05:14:21 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_search@2 76f19ef0-b40f-46b0-95e1-91d90dd81aff

--- forms.py
+++ forms.py
@@ -1,6 +1,14 @@
 from datetime import date, datetime, timedelta
 from django import forms
 
+class SearchForm(forms.Form):
+    search_term = forms.CharField(
+        max_length=20, 
+        label="",
+        help_text="enter all or part of a word, name, identifier, etc",
+        error_messages={'required': 'Please enter a search term.'},
+        )
+
 class DateRangeSearchForm(forms.Form):
     date_start = forms.CharField(
         max_length=10,
