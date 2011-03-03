7ab12d40696f03b20814191043f40a816ea60b7a
erik van widenfelt <ew2789@gmail.com>
2011-03-03 08:29:15 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@58 bf858008-1fca-4ad0-9432-481439aa1b6d

--- utils.py
+++ utils.py
@@ -10,11 +10,11 @@ def check_initials_field(first_name, last_name, initials):
         
     """
     if initials != None and first_name != None:
-        if first_name[:1] != initials[:1]:
+        if first_name[:1].upper() != initials[:1].upper():
             raise forms.ValidationError("First initial does not match first name, expected '%s' but you wrote %s." % (first_name[:1], initials[:1]))
             
     if initials != None and last_name != None:
-        if last_name[:1] != initials[-1:]:
+        if last_name[:1].upper() != initials[-1:].upper():
             raise forms.ValidationError("Last initial does not match last name, expected '%s' but you wrote %s." % (last_name[:1], initials[-1:]))
             
 def check_omang_field(omang, gender):            
