f7dcbe53deceb69de3c84675297761b4a824d670
erik van widenfelt <ew2789@gmail.com>
2011-03-09 08:54:23 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@22 a1c8eae8-7d53-4db9-beb7-be5090263015

---
+++ models/subject_identifier_audit_trail.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from bhp_common.fields import MyUUIDField
+from bhp_common.models import MyBasicUuidModel
+
+
+class SubjectIdentifierAuditTrail(MyBasicUuidModel):
+    """
+    A table to track every attempt to allocate a subject identifier
+    to a consented subject 'by this device'. If a subject consent is deleted
+    the record in this table remains. So this is not a master list of
+    valid identifiers. 
+    See also AllocateSubjectIdentifier()
+    """
+    subject_consent_id = MyUUIDField()
+    subject_identifier = models.CharField(max_length=25)
+    first_name = models.CharField(max_length=250) 
+    initials = models.CharField(max_length=3)
+    date_allocated = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.subject_identifier
+        
+    class Meta:
+        ordering = ['-date_allocated']
