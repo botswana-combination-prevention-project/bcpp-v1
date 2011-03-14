4f6377cb1dbbea95fcfe790ed9f76f3140d39f08
erik van widenfelt <ew2789@gmail.com>
2011-03-14 06:46:44 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@66 bf858008-1fca-4ad0-9432-481439aa1b6d

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import re, socket, sys
+import syslog
+from ifconfig import ifconfig
 from django import forms
 from django.core.exceptions import ValidationError
-
+from bhp_variables.models import StudySpecific
 
 def check_initials_field(first_name, last_name, initials):
     """
@@ -31,15 +33,38 @@ def check_omang_field(omang, gender):
             raise ValidationError(u'Invalid Omang number for gender (%s). You entered %s.' % (gender, str_value))
             
 def os_variables():
-            
-    a = {}
-    a['hostname'] = socket.gethostname()
+       
+    syslog.syslog('os_variables:')
+        
+    variables = {}
+
+    variables['hostname'] = socket.gethostname()
+
+    sp = StudySpecific.objects.all()[0]
     
-    return a
+    if not sp.hostname_prefix:
+        msg = 'StudySpecific.hostname_prefix not set'
+        syslog.syslog(syslog.LOG_WARNING, msg)    
+    
+    config = ifconfig('wlan0')
+    
+    if config:
+        variables['wlan_ipaddr'] = config['addr'] 
+        msg = 'interface wlan0 ipaddr is %s' % (variables['wlan_ipaddr'])
+        variables['wlan_network'] = '%s.%s.%s.0/24' % (variables['wlan_ipaddr'].split('.')[0], variables['wlan_ipaddr'].split('.')[1], variables['wlan_ipaddr'].split('.')[2])
+        syslog.syslog(syslog.LOG_INFO, msg)
+    else:
+        msg = 'interface wlan0 is not configured! '
+        syslog.syslog(syslog.LOG_WARNING, msg)
+
+    if variables['wlan_ipaddr']:
+        derived_hostname = '%s%s' % (sp.hostname_prefix, variables['wlan_ipaddr'].split('.')[3].zfill(2))
+        if not derived_hostname == variables['hostname'] :
+            msg = 'hostname is %s but should be %s based on ip=%s and hostname_prefix=%s. Check wlan0 config and/or StudySpecific.hostname_prefix' % (variables['hostname'], derived_hostname, variables['wlan_ipaddr'], sp.hostname_prefix)
+            syslog.syslog(syslog.LOG_ERR, msg)
+
+    return variables
+    
     
 
-                
-
-
-    
 
