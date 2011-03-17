f8a342682aba484f2b110159e3f5340ad21ff5c1
erik van widenfelt <ew2789@gmail.com>
2011-03-17 10:47:22 +0000

    update admin.py, remove StudySpecificSetting
    git-svn-id: http://192.168.1.50/svn/bhp_variables@18 72d6f641-25d3-4f20-903c-4c2123898bce

--- admin.py
+++ admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from models import StudySpecific, StudySite, StudySpecificSetting
+from models import StudySpecific, StudySite
 
 admin.site.register(StudySpecific)
 
 admin.site.register(StudySite)
 
-class StudySpecificSettingAdmin (admin.ModelAdmin):
-    list_display = (
-        'setting_keyword',
-        'setting_value',
-    )
-
-admin.site.register(StudySpecificSetting, StudySpecificSettingAdmin)
 
