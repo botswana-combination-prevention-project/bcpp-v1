e0250a37abef00c18222668a56633834df89aa9c
erik van widenfelt <ew2789@gmail.com>
2011-03-20 16:31:12 +0000

    move models to bhp_visit, remove migrations
    git-svn-id: http://192.168.1.50/svn/bhp_appointment@9 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- choices.py
+++
@@ -1,5 +0,0 @@
-APPT_STATUS = (
-    ('Scheduled', 'Scheduled'),
-    ('Subject Seen', 'Subject Seen'),
-    ('Cancelled', 'Cancelled'),        
-    )    
--- models/__init__.py
+++
@@ -1,2 +0,0 @@
-from base_appointment import *
-from registered_subject_appointment import *
