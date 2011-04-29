069c5d782602304cd936240941cd0e05e606174d
panto motshegwa <pmotshegwa@bhp.org.bw>
2011-04-29 12:57:19 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_report@38 20e02364-0de8-4df6-ba75-255d08af7f65

--- views/view.py
+++ views/view.py
@@ -1,21 +1,38 @@
-# Create your views here.
-"""
-import pyodbc
-from bhp_lab_temptables.models import LabSimpleResult
-from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
-from bhp_lab_result_report.lab_specimens import fetch_receiving
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from bhp_common.utils import os_variables
+from settings import DATABASES
+from bhp_lab_core.models import Result, ResultItem
 
-def detail(request, report_id):
-	p = get_object_or_404(LabSimpleResult, pk=report_id)
-	return render_to_response('detail.html', {'obj': p})
 
-def view(request, report_id):
-	p = get_object_or_404(LabSimpleResult, pk=report_id)
-	result = fetch_receiving(p.sample_id)
+@login_required
+def view(request, **kwargs):
+
+    section_name = kwargs.get('section_name')
+    search_name = "result"
+    result_identifier = kwargs.get('result_identifier')
+    limit = 20
+    template = 'result_report.html'
+                  
+    if result_identifier is not None:
+        result = get_object_or_404(Result, result_identifier=result_identifier)
+        items = ResultItem.objects.filter(result=result)
+        
+        payload = {
+        'result': result,
+        'receive': result.order.aliquot.receive,
+        'order': result.order,
+        'aliquot': result.order.aliquot,
+        'result_items': items,
+        'section_name': section_name,
+        'result_include_file': "detail.html",
+        'receiving_include_file':"receiving.html",
+        'orders_include_file': "orders.html",
+        'result_items_include_file': "result_items.html",
+        'top_result_include_file': "result_include.html",
+        }
+        
+        return render_to_response(template, payload, context_instance=RequestContext(request))   
     
-	return render_to_response('view.html', {'obj': result})
-	
-def printPDF(request, report_id):
-	return HttpResponse("You're printing result %s." % report_id)
-"""
