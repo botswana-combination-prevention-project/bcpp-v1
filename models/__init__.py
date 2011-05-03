ad7cdf4c2f98b4f5761e9ed4de37f6974dfa06d8
erik van widenfelt <ew2789@gmail.com>
2011-05-03 08:11:13 +0000

    changes related to result report
    git-svn-id: http://192.168.1.50/svn/bhp_research_protocol@10 2269900f-212b-4854-b747-6958fb70d0de

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from funding_source import *
 from principal_investigator import *
 from site_leader import *
 from protocol import *
+from location import *
 from site import *
 from research_site import *
 
