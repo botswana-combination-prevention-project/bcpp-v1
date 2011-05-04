264d4b18de94db3e2e373785b6201479c4582a96
panto motshegwa <pmotshegwa@bhp.org.bw>
2011-05-04 08:02:01 +0000

    added page numbering
    git-svn-id: http://192.168.1.50/svn/lab_result_report@51 20e02364-0de8-4df6-ba75-255d08af7f65

--- views/view_result_as_pdf.py
+++ views/view_result_as_pdf.py
@@ -1,6 +1,7 @@
 import cStringIO as StringIO
 import ho.pisa as pisa
 import cgi
+from math import ceil,trunc
 from reportlab.pdfgen import canvas
 from django.template.loader import render_to_string, get_template
 from django.http import HttpResponse
@@ -23,8 +24,14 @@ def view_result_as_pdf(request, **kwargs):
                   
     result = get_object_or_404(Result, result_identifier=result_identifier)
     items = ResultItem.objects.filter(result=result)
-
+    
+    total_page_number = trunc(ceil(items.count()/float(18)))
+    
+    #raise TypeError(items.count()/float(18))
+    
     payload = {
+        'pagesize': 'A4',
+        'total_page_number':total_page_number,
         'result': result,
         'receive': result.order.aliquot.receive,
         'order': result.order,
