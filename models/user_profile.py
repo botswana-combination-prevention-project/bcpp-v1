201d79495fcb2eed8b09758829bc5a7f58a53c29
erik van widenfelt <ew2789@gmail.com>
2011-05-09 20:41:09 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_userprofile@3 286037fc-dbac-43a1-8671-deda17dc685f

--- models/user_profile.py
+++ models/user_profile.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from bhp_common.fields import NameField, InitialsField
+from bhp_userprofile.choices import SALUTATION
 
 class UserProfile(models.Model):
     # add AUTH_PROFILE_MODULE = "bhp_userprofile.bhpprofile" o settings.py
     # This is the only required field
     user = models.OneToOneField(User)
 
+    salutation = models.CharField(
+        max_length=10,
+        choices=SALUTATION,
+        default='NONE',
+        )
+
     initials = InitialsField(
         unique=True,
         )
 
     def __unicode__(self):
-        return '%s, %s' % (self.user.last_name, self.user.first_name)
+        return '%s %s' % (self.user.first_name, self.user.last_name)
 
     class Meta:
         app_label = 'bhp_userprofile'
