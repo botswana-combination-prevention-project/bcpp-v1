37d7190e58d734db4502f1b1e5b32c99faed4522
panto motshegwa <pmotshegwa@bhp.org.bw>
2011-05-10 06:02:14 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_report@57 20e02364-0de8-4df6-ba75-255d08af7f65

--- views/view_result_as_pdf.py
+++ views/view_result_as_pdf.py
@@ -1,6 +1,7 @@
 import cStringIO as StringIO
 import ho.pisa as pisa
 import cgi
+import sys, os, subprocess
 from math import ceil,trunc
 from reportlab.pdfgen import canvas
 from django.template.loader import render_to_string, get_template
@@ -53,6 +54,77 @@ def view_result_as_pdf(request, **kwargs):
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(myfile.getvalue(),mimetype='application/pdf')
     response['Content-Disposition'] = "attachment; filename=%s.pdf" % (result.result_identifier)
+   
     return response
     
+def print_result_as_pdf(**kwargs):
+    import cStringIO as StringIO
+    import ho.pisa as pisa
+    import sys, os, subprocess
+    from math import ceil,trunc
+    from django.template.loader import render_to_string
+    from django.shortcuts import get_object_or_404
+    from bhp_lab_core.models import Result, ResultItem
 
+    section_name = 'result'
+    search_name = "result"
+    #result_identifier = kwargs.get('result_identifier')
+    limit = 20
+    template = 'result_report_pdf.html'
+    #result_ids = ['1240636-01','1242053-01','1241852-01','1242079-01','1241851-01']
+    result_ids = ['1240636-01','1242053-01']
+   
+    for result_identifier in result_ids:
+        file_name = "/home/pmotshegwa/sources/printed_results/%s.pdf" % (result_identifier)
+        file = open(file_name, "wb")
+         
+        result = get_object_or_404(Result, result_identifier=result_identifier)
+        items = ResultItem.objects.filter(result=result)
+        
+        total_page_number = trunc(ceil(items.count()/float(18)))
+        
+        payload = {
+            'pagesize': 'A4',
+            'total_page_number':total_page_number,
+            'result': result,
+            'receive': result.order.aliquot.receive,
+            'order': result.order,
+            'aliquot': result.order.aliquot,
+            'result_items': items,
+            'section_name': section_name,
+            'result_include_file': "detail.html",
+            'receiving_include_file':"receiving.html",
+            'orders_include_file': "orders.html",
+            'result_items_include_file': "result_items.html",
+            'top_result_include_file': "result_include.html",
+        }
+        
+        file_data = render_to_string(template, payload,)
+        myfile = StringIO.StringIO()
+        pdf = pisa.CreatePDF(file_data, myfile)
+        #myfile.seek(0)
+              
+        if pdf.err:
+             print "*** %d ERRORS OCCURED" % pdf.err
+        else:
+        
+            # Write pdf file to disk
+            file.write(myfile.getvalue())
+            file.close()
+        
+            #
+            if sys.platform == "darwin":
+                command = "open {0}".format( file_name )
+            else:
+                command = "lp {0}".format( file_name )
+
+            try:
+                p = subprocess.Popen(command, shell = True)
+                pid , sts = os.waitpid(p.pid, 0)
+            except subprocess.CalledProcessError, e:
+                print "Error", e.returncode
+                print e.output
+                
+    return 
+    
+    
