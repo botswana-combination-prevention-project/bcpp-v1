9480c4e9230594213f88d595ea2901188a5975ea
erik van widenfelt <ew2789@gmail.com>
2011-05-11 18:23:24 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@21 72d6f641-25d3-4f20-903c-4c2123898bce

---
+++ models/study_site.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from bhp_common.models import MyBasicUuidModel
+
+
+class StudySite(MyBasicUuidModel):
+    
+    site_code = models.CharField(max_length=4)        
+    
+    site_name = models.CharField(max_length=35)    
+    
+    def __unicode__(self):
+        return "%s %s" % (self.site_code, self.site_name)
+        
+    class Meta:
+        unique_together = [('site_code', 'site_name')]   
+        ordering = ['site_code',] 
