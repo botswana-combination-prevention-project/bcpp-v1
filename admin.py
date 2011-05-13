4ef7fa16e8c145f351d547c4059b45251ec42d0d
erik van widenfelt <ew2789@gmail.com>
2011-05-13 14:19:08 +0000

    add hiv status
    git-svn-id: http://192.168.1.50/svn/lab_grading@16 d0c47599-63b2-4c0f-8d21-292bf8f76f30

--- admin.py
+++ admin.py
@@ -8,7 +8,7 @@ admin.site.register(GradingList, GradingListAdmin)
 
 
 class GradingListItemAdmin(MyModelAdmin):
-    list_display = ('test_code', 'gender', 'grade', 'lln', 'uln', 'age_low', 'age_low_unit','age_low_quantifier','age_high','age_high_unit','age_high_quantifier', 'panic_value_low', 'panic_value_high', 'grading_list')
-    search_fields = ['grade', 'test_code__code','test_code__name','lln', 'uln',]
+    list_display = ('test_code', 'gender', 'hiv_status','grade', 'lln', 'uln', 'age_low', 'age_low_unit','age_low_quantifier','age_high','age_high_unit','age_high_quantifier', 'panic_value_low', 'panic_value_high', 'grading_list')
+    search_fields = ['grade', 'test_code__code','test_code__name','lln', 'uln', 'hiv_status',]
 admin.site.register(GradingListItem, GradingListItemAdmin)
 
