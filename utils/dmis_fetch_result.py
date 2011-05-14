e777b13092fe0dc80a2adf21db1cd36fd6978742
erik van widenfelt <ew2789@gmail.com>
2011-05-14 08:54:05 +0000

    fetch result
    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@107 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_result.py
+++ utils/dmis_fetch_result.py
@@ -1,4 +1,6 @@
-
+"""
+import results for orders already in the system
+"""
 
 def fetch_results_from_dmis(**kwargs):
 
@@ -46,8 +48,6 @@ def fetch_or_create_result(**kwargs):
     for row in cursor_result:
 
         result_identifier=AllocateResultIdentifier(oOrder)    
-
-                 
         
         oResult = Result.objects.create(
             result_identifier=result_identifier,
@@ -60,11 +60,13 @@ def fetch_or_create_result(**kwargs):
             )
             
 
-        sql ='select sample_assay_date, utestid, \
+        sql ='select l21d.sample_assay_date, \
+              utestid, \
               result as result_value, \
               result_quantifier, \
               status, \
               mid,\
+              l21d.validation_ref as validation_reference, \
               l21d.datelastmodified as result_item_datetime, \
               l21d.keyopcreated as user_created, \
               l21.datecreated as created \
@@ -74,24 +76,61 @@ def fetch_or_create_result(**kwargs):
         
         cursor_resultitem.execute(sql)
         
-        oResultSource=ResultSource.objects.filter(name__iexact='dmis')
-        if oResultSource:
-            oResultSource=ResultSource.objects.get(name__iexact='dmis')
-        else:
-            oResultSource=ResultSource.objects.create(
-                name='dmis',
-                short_name='dmis',                
-                display_index=10,
-                )
-            oResultSource.save()  
+        fetch_or_create_resultsource()
         
         for ritem in cursor_resultitem:
             oTestCode=TestCode.objects.get(code__exact=ritem.utestid)
+            # change NT system username to auto
             if ritem.user_created=='NT AUTHORITY\SYSTEM':
                 user_created='auto'
             else:
                 user_created=ritem.user_created
-                
+            # evaluate validation_reference
+            if validation_reference == '-9':
+                # this is an item from GetResults TCP connected to PSM
+                result_item_source = 'psm_interface'                
+                result_item_source_reference = ''
+                validation_reference == 'dmis-auto'
+            elif ritem.validation_reference == 'LAB21:MANUAL':
+                # manual entry and no validation -- straight to LAB21 tableset
+                result_item_source = 'manual_entry'                
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'auto'                
+            elif re.search('^rad[0-9A-F]{5}\.tmp$', validation_reference):    
+                # this is an item from GetResults Flatfile and validated via the LAB05 path
+                result_item_source = 'cd4_interface'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'lab05'                               
+            elif re.search('^LAB23:', validation_reference):          
+                # manual entry and validated via the LAB23 validation path                           
+                result_item_source = 'manual_entry'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'lab23'                               
+            elif re.search('^IMPORT', validation_reference):          
+                # manual entry and validated via the LAB23 validation path                           
+                result_item_source = 'direct_import'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'auto'     
+            elif re.search('^LB003:', validation_reference):          
+                # manual entry and validated via the LAB23 validation path                           
+                result_item_source = 'direct_import'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'auto'     
+            elif re.search('^LB004:', validation_reference):          
+                # manual entry and validated via the LAB23 validation path                           
+                result_item_source = 'direct_import'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'auto'  
+            elif re.search('^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$', validation_reference):                   
+                result_item_source = 'manual_entry'
+                result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+                validation_reference == 'auto'  
+            else:
+                # missed a case? let's hear about it
+                raise TypeError('Validation reference \'%s\' was not expected. See dmis_fetch_result.' % ritem.validation_reference)
+
+            # create a new result item. set validation to 'P', we'll import 
+            # full validation information later 
             ResultItem.objects.create(
                 result=oResult,
                 test_code=oTestCode,
@@ -99,6 +138,7 @@ def fetch_or_create_result(**kwargs):
                 result_item_value=ritem.result_value,
                 result_item_quantifier=ritem.result_quantifier,
                 validation_status='P',
+                validation_reference=validation_reference,
                 result_item_source=oResultSource,
                 result_item_source_reference=ritem.mid,
                 result_item_operator=user_created,                
@@ -109,6 +149,36 @@ def fetch_or_create_result(**kwargs):
             
     return oResult
     
+def fetch_or_create_resultsource( **kwargs ):
+    from bhp_lab_core.models import ResultSource
+
+    interfaces = ['psm_interface', 'cd4_interface','auto', 'manual_entry', 'direct_import',]
+    display_index = 0
+    # populate if not already ...
+    for interface in interfaces:
+        if not oResultSource=ResultSource.objects.filter(name__iexact=interface)
+            oResultSource=ResultSource.objects.create(
+                name=interface,
+                short_name=interface,                
+                display_index=display_index+10,
+                )
+            oResultSource.save()  
+    # create a new one if given argument and does not exist already
+    if kwargs.get('interface'):        
+        if not oResultSource=ResultSource.objects.filter(name__iexact=kwargs.get('interface'))
+            oResultSource=ResultSource.objects.create(
+                name=kwargs.get('interface'),
+                short_name=kwargs.get('interface'),                
+                display_index=display_index+11,
+                )
+            oResultSource.save()  
+        else:
+            oResultSource=ResultSource.objects.get(name__iexact=kwargs.get('interface'))                        
+    else:
+        oResultSource = None
+            
+    return oResultSource         
+
 if __name__ == "__main__":
     
     import sys,os
