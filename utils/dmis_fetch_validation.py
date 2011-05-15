5219dfd034afe02274dec4d9f440fc079be8123d
erik van widenfelt <ew2789@gmail.com>
2011-05-15 12:17:44 +0000

    validation
    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@138 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_validation.py
+++ utils/dmis_fetch_validation.py
@@ -16,75 +16,75 @@ def fetch_validation_from_dmis(**kwargs):
     oDirect_interface = ResultSource.objects.get(name__iexact='direct_import') 
     oManual_interface = ResultSource.objects.get(name__iexact='manual_entry')                           
 
-    oResults  = Result.objects.all()
+    oResultItems  = ResultItem.objects.all()
     
-    for oResult in oResults:
-        for oResultItem in oResult.result_item:
-            if oResultItem.result_item_source==oPSM_interface:
-                #use lab21 information for PSM, Manual, Import
+    for oResultItem in oResultItems:
+        oResult=oResultItem.result
+        if oResultItem.result_item_source==oPSM_interface:
+            #use lab21 information for PSM, Manual, Import
+            oResultItem.update(
+                result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
+                validation_status='F',                        
+                validation_datetime=oResult.result_datetime,
+                validation_username='auto',
+                )
+        elif oResultItem.result_item_source==oDirect_interface:
+            #use lab21 information for Import
+            oResultItem.update(
+                result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
+                validation_status='F',                        
+                validation_datetime=oResult.result_datetime,
+                validation_username='auto',
+                )
+        elif oResultItem.result_item_source==oCD4_interface:
+            #this returns only one record per result, only, so update all items as one
+            # hmmm ... all imported results are from LAB21 which implies result_accepted=1, add "where result_accepted=1"                
+            sql = "select top 1 result_accepted_username as operator, \
+                    result_accepted_username as validation_username, \
+                    l5.result_accessed_date as validation_datetime, \
+                    archive_filename+' ('+exp_filename+')' as result_item_source_reference \
+                    from bhplab.dbo.lab05response as l5 \
+                    left join bhplab.dbo.results_101 as r101 on l5.result_guid=r101.result_guid \
+                    where result_accepted=1 and l5.result_guid='%'" % oResult.dmis_result_guid 
+                    
+            cursor_result = cnxn2.cursor()        
+            for row in cursor_result:        
                 oResultItem.update(
-                    result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
-                    validation_status='F',                        
-                    validation_datetime=oResult.result_datetime,
-                    validation_username='auto',
+                    result_item_operator=row.operator.strip('BHP\\bhp\\'),
+                    validation_status='F',  
+                    validation_datetime=row.validation_datetime,
+                    validation_username=row.validation_username.strip('BHP\\bhp\\'),
                     )
-            elif oResultItem.result_item_source==oDirect_interface:
-                #use lab21 information for Import
-                oResultItem.update(
-                    result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
-                    validation_status='F',                        
-                    validation_datetime=oResult.result_datetime,
-                    validation_username='auto',
-                    )
-            elif oResultItem.result_item_source==oCD4_interface:
-                #this returns only one record per result, only, so update all items as one
-                # hmmm ... all imported results are from LAB21 which implies result_accepted=1, add "where result_accepted=1"                
-                sql = "select top 1 result_accepted_username as operator, \
-                        result_accepted_username as validation_username, \
-                        l5.result_accessed_date as validation_datetime, \
-                        archive_filename+' ('+exp_filename+')' as result_item_source_reference \
-                        from bhplab.dbo.lab05response as l5 \
-                        left join bhplab.dbo.results_101 as r101 on l5.result_guid=r101.result_guid \
-                        where result_accepted=1 and l5.result_guid='%'" % oResult.dmis_result_guid 
-                        
-                cursor_result = cnxn2.cursor()        
-                for row in cursor_result:        
-                    oResultItem.update(
-                        result_item_operator=row.operator.strip('BHP\\bhp\\'),
-                        validation_status='F',  
-                        validation_datetime=row.validation_datetime,
-                        validation_username=row.validation_username.strip('BHP\\bhp\\'),
-                        )
 
-            elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()<>'lab23':                
-                #use lab21 information for PSM, Manual, Import
-                oResultItem.update(
-                    result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
-                    validation_status='F',                        
-                    validation_datetime=oResult.result_datetime,
-                    validation_username='auto',
-                    )
-            elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()=='lab23':
-                #this returns one record per result, only, so update all items as one
-                sql = "select lower(L23.operator) as operator, \
-                        lower(l23d.checkbatch_user) as validation_username, \
-                        l23d.datelastmodified as validation_datetime, \
-                        convert(varchar, l23.id) as validation_reference \
-                        from bhplab.dbo.lab23response as l23 \
-                        left join bhplab.dbo.lab23responseq001x0 as l23d on l23.q001x0=l23d.qid1x0 \
-                        where result_accepted=1 and upper(ltrim(rtrim(utestid)))='%s' and result_guid='%'" % ( oResult.test_code__code, oResult.dmis_result_guid)
+        elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()<>'lab23':                
+            #use lab21 information for PSM, Manual, Import
+            oResultItem.update(
+                result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
+                validation_status='F',                        
+                validation_datetime=oResult.result_datetime,
+                validation_username='auto',
+                )
+        elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()=='lab23':
+            #this returns one record per result, only, so update all items as one
+            sql = "select lower(L23.operator) as operator, \
+                    lower(l23d.checkbatch_user) as validation_username, \
+                    l23d.datelastmodified as validation_datetime, \
+                    convert(varchar, l23.id) as validation_reference \
+                    from bhplab.dbo.lab23response as l23 \
+                    left join bhplab.dbo.lab23responseq001x0 as l23d on l23.q001x0=l23d.qid1x0 \
+                    where result_accepted=1 and upper(ltrim(rtrim(utestid)))='%s' and result_guid='%'" % ( oResult.test_code__code, oResult.dmis_result_guid)
 
-                cursor_result = cnxn2.cursor()            
-                for row in cursor_result:  
-                    oResultItem.update(
-                        result_item_operator=row.operator.strip('BHP\\bhp\\'),
-                        validation_reference=row.validation_reference,
-                        validation_status='F',
-                        validation_datetime=row.validation_datetime,
-                        validation_username=row.validation_username.strip('BHP\\bhp\\')                        
-                        )
-            else:
-                raise TypeError('Unknown case result_item_source in dmis_fetch_validation. Got \'%s\' from result %s.' % (oResult.resultitem.result_item_source, oResult) )
+            cursor_result = cnxn2.cursor()            
+            for row in cursor_result:  
+                oResultItem.update(
+                    result_item_operator=row.operator.strip('BHP\\bhp\\'),
+                    validation_reference=row.validation_reference,
+                    validation_status='F',
+                    validation_datetime=row.validation_datetime,
+                    validation_username=row.validation_username.strip('BHP\\bhp\\')                        
+                    )
+        else:
+            raise TypeError('Unknown case result_item_source in dmis_fetch_validation. Got \'%s\' from result %s.' % (oResult.resultitem.result_item_source, oResult) )
     
 if __name__ == "__main__":
     
