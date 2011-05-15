3c94902b34476a8e1653e1cddcc979765c60be4f
erik van widenfelt <ew2789@gmail.com>
2011-05-15 12:36:10 +0000

    validation
    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@139 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_validation.py
+++ utils/dmis_fetch_validation.py
@@ -22,20 +22,22 @@ def fetch_validation_from_dmis(**kwargs):
         oResult=oResultItem.result
         if oResultItem.result_item_source==oPSM_interface:
             #use lab21 information for PSM, Manual, Import
-            oResultItem.update(
+            oResultItem(
                 result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
                 validation_status='F',                        
                 validation_datetime=oResult.result_datetime,
                 validation_username='auto',
                 )
+            oResultItem.save()        
         elif oResultItem.result_item_source==oDirect_interface:
             #use lab21 information for Import
-            oResultItem.update(
+            oResultItem(
                 result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
                 validation_status='F',                        
                 validation_datetime=oResult.result_datetime,
                 validation_username='auto',
                 )
+            oResultItem.save()                        
         elif oResultItem.result_item_source==oCD4_interface:
             #this returns only one record per result, only, so update all items as one
             # hmmm ... all imported results are from LAB21 which implies result_accepted=1, add "where result_accepted=1"                
@@ -49,21 +51,23 @@ def fetch_validation_from_dmis(**kwargs):
                     
             cursor_result = cnxn2.cursor()        
             for row in cursor_result:        
-                oResultItem.update(
+                oResultItem(
                     result_item_operator=row.operator.strip('BHP\\bhp\\'),
                     validation_status='F',  
                     validation_datetime=row.validation_datetime,
                     validation_username=row.validation_username.strip('BHP\\bhp\\'),
                     )
+                oResultItem.save()                            
 
         elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()<>'lab23':                
             #use lab21 information for PSM, Manual, Import
-            oResultItem.update(
+            oResultItem(
                 result_item_operator=oResult.user_created.strip('BHP\\bhp\\'),            
                 validation_status='F',                        
                 validation_datetime=oResult.result_datetime,
                 validation_username='auto',
                 )
+            oResultItem.save()                        
         elif oResultItem.result_item_source==oManual_interface and oResultItem.validation_reference.lower()=='lab23':
             #this returns one record per result, only, so update all items as one
             sql = "select lower(L23.operator) as operator, \
@@ -76,13 +80,14 @@ def fetch_validation_from_dmis(**kwargs):
 
             cursor_result = cnxn2.cursor()            
             for row in cursor_result:  
-                oResultItem.update(
+                oResultItem(
                     result_item_operator=row.operator.strip('BHP\\bhp\\'),
                     validation_reference=row.validation_reference,
                     validation_status='F',
                     validation_datetime=row.validation_datetime,
                     validation_username=row.validation_username.strip('BHP\\bhp\\')                        
                     )
+                oResultItem.save()                            
         else:
             raise TypeError('Unknown case result_item_source in dmis_fetch_validation. Got \'%s\' from result %s.' % (oResult.resultitem.result_item_source, oResult) )
     
