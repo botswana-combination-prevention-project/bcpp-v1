33757a2c8146ea7d236aae1699dbb566d3208ea8
erik van widenfelt <ew2789@gmail.com>
2011-05-23 12:10:09 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@169 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_receive_order.py
+++ utils/dmis_fetch_receive_order.py
@@ -72,7 +72,7 @@ def fetch_receive_from_dmis(process_status, **kwargs):
             and sample_date_drawn <= \'%s\' \
             group by l.pid, l.tid, l.sample_condition, l.sample_site_id, l.sample_visitid ,l.sample_protocolnumber, l.pat_id, l.gender, l.pinitials, l.keyopcreated, l.keyoplastmodified, l21.id, l21.panel_id  \
             having count(*)=1 \
-            order by min(l.id) desc' % (has_order_sql, last_import_datetime, import_datetime.strftime('%Y-%m-%d %H:%M'), now.strftime('%Y-%m-%d %H:%M'))
+            order by min(l.id) desc' % (has_order_sql, last_import_datetime.strftime('%Y-%m-%d %H:%M'), import_datetime.strftime('%Y-%m-%d %H:%M'), now.strftime('%Y-%m-%d %H:%M'))
 
     #raise TypeError(sql)
 
