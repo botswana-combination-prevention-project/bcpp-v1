5a1299946a92df15b494c8cf52cee2c1f379c688
erik van widenfelt <ew2789@gmail.com>
2011-05-31 09:01:47 +0000

    remove baseemployee dependecy
    git-svn-id: http://192.168.1.50/svn/bhp_research_protocol@22 2269900f-212b-4854-b747-6958fb70d0de

--- models/principal_investigator.py
+++ models/principal_investigator.py
@@ -3,14 +3,29 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
 from bhp_common.validators import datetime_not_future, datetime_is_future
 from bhp_common.choices import YES_NO
-from bhp_employee.models import BaseEmployee
+from bhp_common.models import MyBasicUuidModel
 
-class PrincipalInvestigator (BaseEmployee):
+class PrincipalInvestigator (MyBasicUuidModel):
     
-    pass
+    first_name = NameField(
+        verbose_name = _("First name")
+        )
+        
+    last_name = NameField(
+        verbose_name = _("Last name")
+    )
+    
+    initials = InitialsField()
+
+    def __unicode__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
+           
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        unique_together = ['last_name', 'first_name']
+        abstract = True
     
     def get_absolute_url(self):
         return "/bhp_lab_registration/principalinvestigator/%s/" % self.id   
     
-    class Meta(Employee.Meta):
-        app_label = 'bhp_research_protocol'
+
--- models/site_leader.py
+++ models/site_leader.py
@@ -3,12 +3,31 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
 from bhp_common.validators import datetime_not_future, datetime_is_future
 from bhp_common.choices import YES_NO
-from bhp_employee.models import BaseEmployee
+from bhp_common.models import MyBasicUuidModel
 
-class SiteLeader (BaseEmployee):
-    
-    def get_absolute_url(self):
-        return "/bhp_lab_registration/supervisor/%s/" % self.id   
+class SiteLeader (MyBasicUuidModel):
+
+    first_name = NameField(
+        verbose_name = _("First name")
+        )
         
-    class Meta(Employee.Meta):
-        app_label = 'bhp_research_protocol'
+    last_name = NameField(
+        verbose_name = _("Last name")
+    )
+    
+    initials = InitialsField()
+
+    def __unicode__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
+           
+    def get_absolute_url(self):
+        return "/bhp_lab_registration/siteleader/%s/" % self.id   
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        unique_together = ['last_name', 'first_name']
+        abstract = True
+
+
+        
+
