93fb9d92dbde75343082fc969e44dc2cf847e253
erik van widenfelt <ew2789@gmail.com>
2011-06-01 08:00:06 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@190 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_result.py
+++ utils/dmis_fetch_result.py
@@ -85,8 +85,8 @@ def fetch_or_create_result(**kwargs):
                 try:
                     oTestCode=TestCode.objects.get(code__exact=test_code)
                 except:
-                    raise TypeError(test_code)
-                    oTestCodeGroup = TestCodeGroup.objects.filter(code__exact='000')
+                    #raise TypeError(test_code)
+                    oTestCodeGroup = TestCodeGroup.objects.get(code__exact='000')
                     TestCode.objects.create( 
                         code=test_code,
                         name=test_code,
