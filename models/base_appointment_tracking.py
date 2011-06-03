1317ae05b7f7e404083bcb99eca28c2cefcbc953
erik van widenfelt <ew2789@gmail.com>
2011-06-03 22:47:00 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@59 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

---
+++ models/base_appointment_tracking.py
@@ -0,0 +1,111 @@
+from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+from bhp_common.validators import datetime_not_before_study_start, datetime_not_future, datetime_is_future, datetime_is_after_consent
+from bhp_common.models import MyBasicListModel, MyBasicUuidModel
+from bhp_common.fields import OtherCharField
+from bhp_visit.choices import SUBJECT_TYPE, VISIT_REASON, VISIT_INFO_SOURCE
+from bhp_visit.models import Appointment
+
+class ApptInfoSource (MyBasicListModel):
+    
+    subject_type = models.CharField(
+        max_length = 25,
+        choices=SUBJECT_TYPE,
+        )  
+    class Meta:
+        ordering = ['display_index']  
+        app_label = 'bhp_visit'            
+
+class ApptReason (MyBasicListModel):
+    class Meta:
+        ordering = ['display_index']
+        app_label = 'bhp_visit'                       
+
+class ApptCurrentStatus (MyBasicListModel):
+    class Meta:
+        ordering = ['display_index']   
+        app_label = 'bhp_visit'                    
+
+
+class BaseAppointmentTracking (MyBasicUuidModel):
+    
+    """Base model for Appt Tracking (AF002).
+
+    For entry, requires an appointment be created first, so there
+    is no direct reference to 'registered subject' in this model.
+    
+    List of appointments in admin.py should be limited to scheduled
+    appointments for the current registered subject. 
+    
+    Other ideas: ADD should only allow 'scheduled', and CHANGE only allow 'seen'
+    Admin should change the status after ADD.
+    
+    """        
+
+    appointment  = models.OneToOneField(Appointment)
+    
+    appt_datetime = models.DateTimeField(
+        verbose_name = "Visit Date and Time",
+        validators = [
+            datetime_not_before_study_start,
+            datetime_is_after_consent,
+            datetime_not_future,
+            ],
+        )
+        
+    
+    #current_status = models.ForeignKey(ApptCurrentStatus,
+    #    verbose_name = "What is the subject\'s current study status?",
+    #    help_text = "",
+    #    )
+      
+    reason = models.CharField( 
+        verbose_name = "What is the reason for this visit?",
+        max_length=25,
+        #choices=VISIT_REASON,
+        help_text="",
+        )  
+        
+    reason_missed = models.CharField(
+        verbose_name = "If 'missed' above, Reason scheduled visit was missed",
+        max_length = 35,
+        blank = True,
+        null = True,   
+        )  
+
+    info_source = models.CharField(
+        verbose_name = "What is the main source of this information?",
+        max_length=25,
+        #choices=VISIT_INFO_SOURCE,
+        help_text="",
+        )
+        
+    info_source_other = OtherCharField()   
+        
+    """
+        this value should be suggested by the sytem but may be edited by the user.
+        A further 'save' check should confirm that the date makes sense relative
+        to the visit schedule
+    """    
+       
+    comments = models.TextField(
+        verbose_name="Comment if any additional pertinent information about the participant",
+        max_length=250,
+        blank=True,
+        null=True,   
+        )    
+
+    """
+    next_scheduled_visit_datetime = models.DateTimeField(
+        verbose_name="Next scheduled visit date and time",
+        validators=[
+            datetime_is_after_consent,
+            datetime_is_future,
+            ],       
+        )            
+    """
+        
+    class Meta:
+        abstract = True 
+
---
+++ models/schedule_group.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+from bhp_common.models import MyBasicUuidModel
+
+class ScheduleGroup(MyBasicUuidModel):
+
+    """Model for entry_forms tagged for listing in the visit definition 'tag_for_schedule'."""
+    
+    group_name = models.CharField(
+        max_length=25,
+        unique=True,
+        )
+    
+    membership_form = models.ForeignKey(ContentType)
+        
+    grouping_key = models.CharField(
+        max_length = 25,
+        null=True,
+        blank=True,
+        help_text = "may specify a common value to group a number of tagged forms, not required."        
+        )        
+    
+    def __unicode__(self):
+        return unicode(self.group_name)
+    
+    def get_absolute_url(self):
+        return reverse('admin:bhp_visit_schedulegroup_change', args=(self.id,)) 
+        
+    class Meta:
+        ordering = ['group_name']
+        app_label = 'bhp_visit'         
