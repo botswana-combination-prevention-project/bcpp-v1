60ac12618c2082f8c7c240b2e61ff5ef2741931f
erik van widenfelt <ew2789@gmail.com>
2011-06-12 20:23:03 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@38 a1c8eae8-7d53-4db9-beb7-be5090263015

---
+++ classes/__init__.py
@@ -0,0 +1,1 @@
+from my_model_admin import MyRegisteredSubjectModelAdmin
---
+++ classes/my_model_admin.py
@@ -0,0 +1,44 @@
+from django.db.models import Q
+from django.core.urlresolvers import reverse
+from bhp_common.models import MyModelAdmin
+from bhp_form.models import Appointment, ScheduledEntryBucket
+
+
+class MyRegisteredSubjectModelAdmin (MyModelAdmin):
+   
+    """ModelAdmin subclass for models with a ForeignKey to 'registered_subject'
+    
+    Takes care of updating the bucket and redirecting back to the dashboard after
+    delete()
+    
+    """ 
+    
+    def save_model(self, request, obj, form, change):
+        
+        ScheduledEntryBucket.objects.update_bucket(
+            model = obj,
+            )
+                        
+        return super(MyRegisteredSubjectModelAdmin, self).save_model(request, obj, form, change)
+        
+    def delete_model(self, request, obj):
+
+        ScheduledEntryBucket.objects.update_bucket(
+            model = obj,
+            action = 'delete',
+            )
+            
+        return super(MyRegisteredSubjectModelAdmin, self).delete_model(request, obj)        
+
+    #override, limit dropdown in add_view to id passed in the URL        
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "appointment":
+            kwargs["queryset"] = Appointment.objects.filter(id__exact=request.GET.get('appointment', 0))
+        return super(MyRegisteredSubjectModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)   
+        
+    #override to disallow subject to be changed
+    def get_readonly_fields(self, request, obj = None):
+        if obj: #In edit mode
+            return ('appointment',) + self.readonly_fields
+        else:
+            return self.readonly_fields  
