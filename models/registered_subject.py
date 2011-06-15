c0cd8534aa6e786664f94d9f20eb03e14b260645
erik van widenfelt <ew2789@gmail.com>
2011-06-15 08:54:16 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@49 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/registered_subject.py
+++ models/registered_subject.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
 from base_subject import BaseSubject
+from bhp_variables.models import StudySpecific
 from bhp_registration.models import SubjectIdentifierAuditTrail
 
 
