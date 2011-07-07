466ead6787d3f2e0f746411739e774ec0dcef743
erik van widenfelt <ew2789@gmail.com>
2011-07-07 10:46:48 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@121 bf858008-1fca-4ad0-9432-481439aa1b6d

--- classes/my_model_form.py
+++ classes/my_model_form.py
@@ -17,7 +17,7 @@ class MyModelForm(forms.ModelForm):
     
         return cleaned_data
         
-    def validate_m2m(self, **kwargs ):
+    def validate_m2m(self, **kwargs):
     
         """Validate at form level a triplet of questions lead by a Yes/No for a many to many with other specify.
         
@@ -25,9 +25,9 @@ class MyModelForm(forms.ModelForm):
         The second question is a many to many (select all that apply)
         The third is an 'Other Specify' to be completed if an 'Other' item was selected in the many to many question
 
-        Be sure to cleaned_data for the 'key' of the m2m field first.
+        Be sure to check cleaned_data for the 'key' of the m2m field first.
         
-        For example, in the clean method call:
+        For example, in the ModelForm clean() method call:
 
         if cleaned_data.has_key('chronic_cond'):
             self.validate_m2m(
@@ -39,33 +39,28 @@ class MyModelForm(forms.ModelForm):
 
         """
         
-        label = kwargs.get('label')
+        label = kwargs.get('label', 'items to be selected')
         yesno = kwargs.get('yesno')
         m2m = kwargs.get('m2m')
         other = kwargs.get('other')
         
-        if yesno.lower() == 'yes':
-            for cond in m2m: 
-                if cond.name.lower() == 'not applicable':
-                    raise forms.ValidationError("The participant has " + label + ". You wrote '%s'" %  cond.name)        
+        # if lead question is 'Yes', a m2m item cannot be 'Not applicable'
+        if yesno.lower() == 'yes' and [True for item in m2m if item.name.lower() == 'not applicable']:
+            raise forms.ValidationError("You stated there ARE " + label + "s, yet you selected '%s'" %  item.name)        
 
-        # if yesno == No => only one item in list and item must be 'not applicable'
-        if yesno.lower() == 'no':
-            found = [True for cond in m2m if cond.name.lower() == 'not applicable']
-            if not found:
-               raise forms.ValidationError("The participant has NO " + label + ". Please correct")                
+        # if lead question is 'No', ensure the m2m item is 'not applicable'
+        if yesno.lower() == 'no' and not [True for item in m2m if item.name.lower() == 'not applicable']:
+           raise forms.ValidationError("You stated there are NO " + label + "s. Please correct")                
 
+        # if lead question is 'No', ensure only one m2m item is selected.
         if yesno.lower() == 'no' and len(m2m) > 1:
-            raise forms.ValidationError("The participant has NO " + label + ". Please correct")                
+            raise forms.ValidationError("You stated there are NO " + label + "s. Please correct")                
 
-        """if m2m == 'other, specify', other may not be blank"""           
-        if yesno.lower() == 'yes':        
-            for cond in m2m: 
-                if cond.name.lower() == 'other, specify' and not other:
-                    raise forms.ValidationError("If " + label + " is 'Other', please specify.")
-        
-        if other:
-            found = [True for cond in m2m if cond.name.lower() == 'other, specify']
-            if not found:
-                raise forms.ValidationError("You have specified an 'Other' condition but not selected 'Other, specify'. Please correct.")        
+        # if lead question is 'Yes' and an m2m item is 'other, specify', ensure 'other' attribute has a value
+        if yesno.lower() == 'yes' and not other and [True for item in m2m if item.name.lower() == 'other, specify']:        
+            raise forms.ValidationError("You have selected a '" + label + "' as 'Other', please specify.")
+
+        # if 'other' has a value but no m2m item is 'Other, specify'
+        if other and not [True for item in m2m if item.name.lower() == 'other, specify']:
+            raise forms.ValidationError("You have specified an 'Other' " + label + " but not selected 'Other, specify'. Please correct.")        
 
