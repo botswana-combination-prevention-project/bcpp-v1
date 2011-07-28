564611bc8b894f377390e3ba5c5241c737577add
erik van widenfelt <ew2789@gmail.com>
2011-07-28 07:48:56 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@61 a1c8eae8-7d53-4db9-beb7-be5090263015

--- classes/my_model_admin.py
+++ classes/my_model_admin.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
 from django.core.urlresolvers import reverse
 from bhp_common.models import MyModelAdmin
-from bhp_form.models import Appointment, ScheduledEntryBucket
+from bhp_form.models import Appointment,ScheduledEntryBucket
+from bhp_registration.models import  RegisteredSubject
 
 
 class MyRegisteredSubjectModelAdmin (MyModelAdmin):
@@ -32,13 +33,33 @@ class MyRegisteredSubjectModelAdmin (MyModelAdmin):
 
     #override, limit dropdown in add_view to id passed in the URL        
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+
         if db_field.name == "appointment":
-            kwargs["queryset"] = Appointment.objects.filter(id__exact=request.GET.get('appointment', 0))
-        return super(MyRegisteredSubjectModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)   
-        
+            if request.GET.get('appointment'):
+                kwargs["queryset"] = Appointment.objects.filter(id__exact=request.GET.get('appointment'))
+            elif self.model.objects.filter(pk=object_id):
+                kwargs["queryset"] = Appointment.objects.filter(pk=self.model.objects.get(pk=object_id).appointment.pk)
+            else:
+                kwargs["queryset"] = Appointment.objects.none()
+
+        if db_field.name == "registered_subject":
+            if request.GET.get('registered_subject'):
+                kwargs["queryset"] = RegisteredSubject.objects.filter(pk = request.GET.get('registered_subject'))
+            elif self.model.objects.filter(pk=object_id):
+                kwargs["queryset"] = RegisteredSubject.objects.filter(pk = self.model.objects.get(pk=object_id).appointment.registered_subject.pk)
+            else:
+                kwargs["queryset"] = RegisteredSubject.objects.none()
+
+
+
+        return super(MyRegisteredSubjectModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)                   
+
+
+    """    
     #override to disallow subject to be changed
     def get_readonly_fields(self, request, obj = None):
         if obj: #In edit mode
             return ('appointment',) + self.readonly_fields
         else:
             return self.readonly_fields  
+    """            
