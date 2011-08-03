93f872f82aeda522513a6bda36f006f307228a39
erik van widenfelt <ew2789@gmail.com>
2011-08-03 05:38:14 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@131 bf858008-1fca-4ad0-9432-481439aa1b6d

---
+++ migrations/0007_auto__add_field_identifiertracker_identifier_type.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'IdentifierTracker.identifier_type'
+        db.add_column('bhp_common_identifiertracker', 'identifier_type', self.gf('django.db.models.fields.CharField')(default='unknown', max_length=35), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'IdentifierTracker.identifier_type'
+        db.delete_column('bhp_common_identifiertracker', 'identifier_type')
+
+
+    models = {
+        'bhp_common.contenttypemap': {
+            'Meta': {'object_name': 'ContentTypeMap'},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        },
+        'bhp_common.identifiertracker': {
+            'Meta': {'ordering': "['root_number', 'counter']", 'unique_together': "(['root_number', 'counter'],)", 'object_name': 'IdentifierTracker'},
+            'counter': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '25', 'db_index': 'True'}),
+            'identifier_type': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'root_number': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['bhp_common']
