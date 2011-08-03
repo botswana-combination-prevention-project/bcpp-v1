27f17e5e59e2918c4d4c6473e131f10bbab977b2
erik van widenfelt <ew2789@gmail.com>
2011-08-03 09:44:07 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dashboard@5 12d20db4-2ca3-46d0-ad31-f07335605e40

--- classes/registered_subject_dashboard.py
+++ classes/registered_subject_dashboard.py
@@ -2,4 +2,16 @@ from bhp_dashboard.classes import Dashboard
 
 class RegisteredSubjectDashboard(Dashboard):
 
-    pass    
+    def create(self, **kwargs):
+
+        # call super to initialize default context 
+        super(RegisteredSubjectDashboard, self).create(**kwargs)
+
+        # to see default context, and therefore which variables are already
+        # available to you, see ContextDescriptor in bhp_dashboard.classes.Dashboard or
+        # raise TypeError(self.context)
+        
+        # add extra context specific to this dashboard
+    
+
+
