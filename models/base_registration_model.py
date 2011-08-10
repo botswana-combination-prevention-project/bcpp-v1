4af017f80e65232647ce76be05d455a3b970674c
erik van widenfelt <ew2789@gmail.com>
2011-08-10 11:13:32 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@70 a1c8eae8-7d53-4db9-beb7-be5090263015

---
+++ models/base_registration_model.py
@@ -0,0 +1,20 @@
+from django.db import models
+from bhp_common.models import MyBasicUuidModel
+from bhp_common.validators import datetime_not_before_study_start, datetime_not_future
+from registered_subject import RegisteredSubject
+
+       
+class BaseRegistrationModel (MyBasicUuidModel):
+
+    registered_subject = models.OneToOneField(RegisteredSubject,
+        editable=False  
+        )
+    
+    registration_datetime = models.DateTimeField("Today's date",
+        validators=[
+            datetime_not_before_study_start,
+            datetime_not_future,])
+    
+    class Meta:
+        abstract=True
+    
