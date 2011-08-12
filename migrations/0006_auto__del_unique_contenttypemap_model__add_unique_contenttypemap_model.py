13b94ccb28fe362e190713648debba9b49f0b54e
erik van widenfelt <ew2789@gmail.com>
2011-08-12 19:40:10 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@143 bf858008-1fca-4ad0-9432-481439aa1b6d

---
+++ migrations/0006_auto__del_unique_contenttypemap_model__add_unique_contenttypemap_model.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Removing unique constraint on 'ContentTypeMap', fields ['model']
+        db.delete_unique('bhp_common_contenttypemap', ['model'])
+
+        # Adding unique constraint on 'ContentTypeMap', fields ['model', 'app_label']
+        db.create_unique('bhp_common_contenttypemap', ['model', 'app_label'])
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'ContentTypeMap', fields ['model', 'app_label']
+        db.delete_unique('bhp_common_contenttypemap', ['model', 'app_label'])
+
+        # Adding unique constraint on 'ContentTypeMap', fields ['model']
+        db.create_unique('bhp_common_contenttypemap', ['model'])
+
+
+    models = {
+        'bhp_common.contenttypemap': {
+            'Meta': {'unique_together': "(['app_label', 'model'],)", 'object_name': 'ContentTypeMap'},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'home'", 'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['bhp_common']
