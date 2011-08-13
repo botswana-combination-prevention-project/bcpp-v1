d222ff603f3946e3c175dd3a726029fdfeb0568a
erik van widenfelt <ew2789@gmail.com>
2011-08-13 12:42:33 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@144 bf858008-1fca-4ad0-9432-481439aa1b6d

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from models import ContentTypeMap, MyModelAdmin
-from models import IdentifierTracker
+
 
 class ContentTypeMapAdmin(MyModelAdmin):
     list_display = (  'name', 'content_type','model', 'app_label')
@@ -11,12 +11,4 @@ class ContentTypeMapAdmin(MyModelAdmin):
     
 admin.site.register(ContentTypeMap, ContentTypeMapAdmin)
 
-class IdentifierTrackerAdmin(MyModelAdmin):
 
-    list_display = (  'identifier', 'root_number','counter', 'created')
-    
-    search_fields = ('identifier', 'root_number')
-    
-    list_filter = ('created',)
-    
-admin.site.register(IdentifierTracker, IdentifierTrackerAdmin)
