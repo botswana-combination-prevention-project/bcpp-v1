2c5ec65bdb79689c3cf877f865b7b2d407fd8811
erik van widenfelt <ew2789@gmail.com>
2011-08-20 18:56:18 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@81 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- admin.py
+++
@@ -1,9 +0,0 @@
-"""
-from datetime import datetime, date
-from django.contrib import admin
-from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import force_unicode
-from django.http import HttpResponse, HttpResponseRedirect
-from bhp_common.models import MyModelAdmin, MyStackedInline
-"""
--- urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('bhp_visit.views',
-    (r'^scheduled_forms/$', 'scheduled_forms'),            
-) 
