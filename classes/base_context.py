9087b415b9fbcb47ce7d7dbd14763154ad06ebdf
erik van widenfelt <ew2789@gmail.com>
2011-08-22 06:52:12 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_context@2 c8ba1726-2cb8-4d29-8fda-c2b64553bcb0

--- classes/base_context.py
+++ classes/base_context.py
@@ -1,84 +1,67 @@
+import socket
 from django.conf import settings
+from bhp_common.utils import os_variables
 
-class ExcludeKeysDescriptor(object):
-    
-    def __get__(self, instance, owner):
-        return self.value
-    def __set__(self, instance, arg):
-        if arg and isinstance(arg, list):
-            self.value = arg        
-        else:
-            raise AttributeError, "ExcludeKeysDescriptor expects a list. Got %s" % type(arg)
-        
 
 class ContextDescriptor(object):
     
     """Descriptior for a template context"""    
     
     def __init__(self, **kwargs):
-
-        self.context = {}
-        #self.exclude_keys = []
-        self.initialized = False
-        for k,v in kwargs.items():
-            self.context['k'] = v
-    
+        pass
     def __get__(self, instance, owner):
-        return self.context
-    
+        return self.value
     def __set__(self, instance, arg):
-
-        if not self.initialized and isinstance(arg, dict): 
-            self.context = arg
-
-    def add(self, dct):
-        """ add a k,v pair or update an existing k"""
-        if arg and isinstance(arg, dict):
-            # k,v pairs should be passed only after you have initialized context
-            for k,v in arg.items():
-                self.context[k] = v
+        if isinstance(arg, dict): 
+            self.value = arg
         else:
-            raise AttributeError, "Can't set/add dict key, value pair for attribute \'context\'. Got key=%s, value=%s" % (self.name, key, value)
-
-    def.remove(self, key):
-        """ remove existing key or raise error """
-        if key in self.context.keys():
-            del self.context[key]
-        else:
-            raise AttributeError, "Can't \'remove\' dict key from context. Does not exist. Got key=%s" % key                
-        
-    def.remove_as_dictionary(self, dct):
-        if arg and isinstance(arg, dict):
-            del self.context[j]
-
-    def remove_as_list(self, remove_keys):
-
-        """ remove keys given a list of keys """
-
-        for key in self.context.keys():
-            if key in remove_keys:
-                del self.context[key]    
-        
+            raise AttributeError, "Attribute \'context\' must be type Dict. Got %s" % type(arg)
                 
 
 class BaseContext(object):
 
-    context = ContextDescriptor()
+    values = ContextDescriptor()
 
     def __init__(self):
-        
-        if 'MAIN_APP_LABEL' in settings.keys():
+        try:
             main_app_label = settings.MAIN_APP_LABEL
-        else:
+        except:
             main_app_label = ''    
-            
-        self.context = {
+        self.values = {
             "app_label": main_app_label,
             "hostname": socket.gethostname(),
             "os_variables":os_variables(),
             }        
 
     def add_to_context(self, dct):
+        self.add(dct)
         
-        self.add(dct)    
-    
+    def add(self, **kwargs):
+        """ add a k,v pair or update an existing k"""
+        if kwargs:
+            # k,v pairs should be passed only after you have initialized context
+            for k,v in kwargs.items():
+                self.values[k] = v
+        #else:
+        #    raise AttributeError, "Can't add to context, expected type dict. Got %s" % type(kwargs)
+
+    def remove(self, key):
+        """ remove existing key or raise error """
+        if key in self.values.keys():
+            del self.values[key]
+        else:
+            raise AttributeError, "Can't \'remove\' dict key from context. Does not exist. Got key=%s" % key                
+        
+    def remove_as_dictionary(self, arg):
+        if arg and isinstance(arg, dict):
+            del self.values[j]
+
+    def remove_as_list(self, remove_keys):
+        """ remove keys given a list of keys """
+        if isinstance(remove_keys, list):
+            for key in self.values.keys():
+                if key in remove_keys:
+                    del self.values[key]    
+        else:
+            raise AttributeError, "Method remove_as_list() expects arg to be type list. Got %s" % type(remove_keys)                
+
