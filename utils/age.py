427ae96996ab16632ed8772bdd3cf1b28da5747b
erik van widenfelt <ew2789@gmail.com>
2011-08-23 06:10:21 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@155 bf858008-1fca-4ad0-9432-481439aa1b6d

--- utils/age.py
+++ utils/age.py
@@ -2,19 +2,21 @@ from datetime import *
 from dateutil.relativedelta import *
 
 def formatted_age(born, reference_date):
-    rdelta = relativedelta(reference_date, born)
-    if born > reference_date:
-        return '?'
-    elif rdelta.years == 0 and rdelta.months == 0:
-        return '%sd' % (rdelta.days)
-    elif rdelta.years == 0 and rdelta.months > 0 and rdelta.months <= 2:
-        return '%sm%sd' % (rdelta.months,rdelta.days)
-    elif rdelta.years == 0 and rdelta.months > 2:
-        return '%sm' % (rdelta.months)
-    elif rdelta.years == 1: 
-        m = rdelta.months + 12
-        return '%sm' % (m)        
-    elif rdelta.years > 1:
-        return '%sy' % (rdelta.years)
-    else:
-         raise TypeError('Age template tag missed a case... today - born. redelta = %s and %s' % (rdelta, born)) 
+    
+    if born:
+        rdelta = relativedelta(reference_date, born)
+        if born > reference_date:
+            return '?'
+        elif rdelta.years == 0 and rdelta.months == 0:
+            return '%sd' % (rdelta.days)
+        elif rdelta.years == 0 and rdelta.months > 0 and rdelta.months <= 2:
+            return '%sm%sd' % (rdelta.months,rdelta.days)
+        elif rdelta.years == 0 and rdelta.months > 2:
+            return '%sm' % (rdelta.months)
+        elif rdelta.years == 1: 
+            m = rdelta.months + 12
+            return '%sm' % (m)        
+        elif rdelta.years > 1:
+            return '%sy' % (rdelta.years)
+        else:
+             raise TypeError('Age template tag missed a case... today - born. redelta = %s and %s' % (rdelta, born)) 
