0aba177383b976e7f1cb0f772eb0d74a1747b8d6
erik van widenfelt <ew2789@gmail.com>
2011-08-29 21:47:00 +0000

    git-svn-id: http://192.168.1.50/svn/lab_account@3 175f5fc6-46c7-45fc-b3a3-1d6e88701e56

---
+++ models/account.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from bhp_common.models import MyBasicUuidModel
+from bhp_lab_account.models import AccountHolder
+
+class Account (MyBasicUuidModel):
+    
+    account_name = models.CharField(
+        max_length = 25,
+        unique=True,
+        )
+        
+    account_opendate = models.DateField(
+        verbose_name = 'Open date',
+        )    
+
+    account_closedate = models.DateField(
+        verbose_name = 'Closed date',
+        )    
+
+    account_holder = models.ForeignKey(AccountHolder)
+
+    comment = models.CharField("Comment", 
+        max_length=250, 
+        blank=True,
+        )        
+               
+    def get_absolute_url(self):
+        return "/bhp_lab_account/account/%s/" % self.id   
+
+    def __unicode__(self):
+        return self.account_name
+        
+    class Meta:
+        ordering = ["account_name"]
+        app_label = 'bhp_lab_account'
+        db_table = 'bhp_lab_registration_account'                        
+
---
+++ models/account_holder.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from bhp_common.models import MyBasicUuidModel
+from bhp_common.choices import YES_NO
+from bhp_common.fields import NameField, InitialsField
+
+
+class AccountHolder(MyBasicUuidModel):
+
+    first_name = NameField(
+        verbose_name = _("First name")
+        )
+        
+    last_name = NameField(
+        verbose_name = _("Last name")
+    )
+    
+    initials = InitialsField()
+
+    comment = models.TextField(
+        max_length = 100,
+        blank=True,)
+    
+    def __unicode__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
+           
+    def get_absolute_url(self):
+        return "/bhp_lab_account/accountholder/%s/" % self.id   
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        unique_together = ['last_name', 'first_name']
+        app_label = 'bhp_lab_account'
+        db_table = 'bhp_lab_registration_accountholder'  
