9735e4fdbaf0f9caccd58283227031436519f1e3
erik van widenfelt <ew2789@gmail.com>
2011-08-29 19:42:15 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result@4 afa70ccf-cfc7-4289-b1b8-adaaa15ec0be

--- models/test_code_group.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from bhp_common.models import MyBasicModel, MyBasicListModel
-
-
-class TestCodeGroup(MyBasicListModel):
-
-    class Meta:
-        app_label = 'bhp_lab'  
