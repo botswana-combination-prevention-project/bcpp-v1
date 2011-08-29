ac3728af6567f494759f2449cd38a8b3ffe5ea81
erik van widenfelt <ew2789@gmail.com>
2011-08-29 22:37:54 +0000

    git-svn-id: http://192.168.1.50/svn/lab_common@2 3c07a67b-3389-4fa7-9c45-b2f84f859bbe

---
+++ choices.py
@@ -0,0 +1,24 @@
+ABS_CALC = (
+    ('absolute', 'Absolute'),
+    ('calculated', 'Calculated'),    
+    )
+
+UNITS = (
+    ('%','%'),
+    ('10^3/uL','10^3/uL'),
+    ('10^3uL','10^3uL'),
+    ('10^6/uL','10^6/uL'),
+    ('cells/ul','cells/ul'),
+    ('copies/ml','copies/ml'),
+    ('fL','fL'),
+    ('g/dL','g/dL'),
+    ('g/L','g/L'),
+    ('mg/L','mg/L'),
+    ('mm/H','mm/H'),
+    ('mmol/L','mmol/L'),
+    ('ng/ml','ng/ml'),
+    ('pg','pg'),
+    ('ratio','ratio'),
+    ('U/L','U/L'),
+    ('umol/L','umol/L'),
+)
