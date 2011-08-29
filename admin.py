c9f2ea4534ad1d236983e512118bdb019c29e385
erik van widenfelt <ew2789@gmail.com>
2011-08-29 22:39:25 +0000

    git-svn-id: http://192.168.1.50/svn/lab_order@6 f6c41d23-262b-4918-a957-6735cdbbd5c9

--- admin.py
+++ admin.py
@@ -1,14 +1,19 @@
 from datetime import *
 from django.contrib import admin
+from bhp_common.models import MyModelAdmin, MyStackedInline
 from autocomplete.views import autocomplete, AutocompleteSettings
 from autocomplete.admin import AutocompleteAdmin
 from bhp_lab_order.models import Order
 
+class AliquotAutocomplete(AutocompleteSettings):
+
+    search_fields = ('^aliquot_identifier',)
+autocomplete.register(Order.aliquot, AliquotAutocomplete)
 
 class OrderAutocomplete(AutocompleteSettings):
     search_fields = ('^order_identifier',)
 
-autocomplete.register(Order.aliquot, AliquotAutocomplete)
+#autocomplete.register(Order.aliquot, AliquotAutocomplete)
 
 
 class OrderAdmin(AutocompleteAdmin,MyModelAdmin):
---
+++ choices.py
@@ -0,0 +1,5 @@
+ORDER_STATUS = (
+    ('PENDING','Pending'),
+    ('PARTIAL','Partial'),
+    ('COMPLETE','Complete'),
+)
