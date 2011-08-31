b0862525eeb6da27ae8da29d1298d50dce38bbcf
erik van widenfelt <ew2789@gmail.com>
2011-08-31 05:34:50 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@206 1a682d4c-4399-4169-b676-170dd5b746ab

--- models/specimen_type.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from bhp_common.models import MyBasicListModel
-
-class SpecimenType (MyBasicListModel):
-    pass
-    
-    class Meta:
-        app_label = 'bhp_lab_core'
