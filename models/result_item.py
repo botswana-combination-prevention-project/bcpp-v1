b1c5274712510fdc25bb359935f3465296cb6d90
erik van widenfelt <ew2789@gmail.com>
2011-09-03 23:28:45 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_item@8 94dd4f35-d255-437e-9225-d26d65b3fcea

--- models/result_item.py
+++ models/result_item.py
@@ -1,81 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from audit_trail.audit import AuditTrail
-from bhp_common.models import MyBasicUuidModel
 from lab_result.models import Result, ResultSource
-from lab_test_code.models import TestCode
-from lab_result_item.choices import RESULT_VALIDATION_STATUS, RESULT_QUANTIFIER
+from base_result_item import BaseResultItem
 
 
-class ResultItem(MyBasicUuidModel):
+class ResultItem(BaseResultItem):
 
     result = models.ForeignKey(Result)
-
-    test_code = models.ForeignKey(TestCode)
-
-    result_item_value = models.CharField(
-        verbose_name = 'Result',
-	    max_length = 25,
-	    help_text = '',
-        db_index=True,
-	    )
-
-    result_item_quantifier = models.CharField(
-        verbose_name = 'Quantifier',
-        default = '=',
-        choices = RESULT_QUANTIFIER,
-	    max_length = 25,
-	    help_text = '',
-	    )
-    
-    result_item_datetime = models.DateTimeField(
-        verbose_name = 'Assay date and time',
-        db_index=True,        
-        )
-    result_item_operator = models.CharField(
-        verbose_name = 'Operator',
-        max_length=50,
-	    null=True,
-	    blank=True,
-        db_index=True,	    
-        )
-
-    validation_status = models.CharField(
-        verbose_name = 'Status',
-        default = 'P',
-        choices = RESULT_VALIDATION_STATUS,
-	    max_length = 10,
-	    help_text = 'Default is preliminary',
-        db_index=True,	    
-	    )
-
-    validation_datetime = models.DateTimeField(
-	    null=True,
-	    blank=True,
-        db_index=True,	    
-	    )
-	
-    validation_username = models.CharField(
-        verbose_name = "Validation username",
-        max_length=50,
-	    null=True,
-	    blank=True,
-        db_index=True,	    
-	    )
-    validation_reference = models.CharField(
-        verbose_name = "Validation reference",
-        max_length=50,
-	    null=True,
-	    blank=True,
-	    )
-
-    comment = models.CharField(
-        verbose_name = 'Validation Comment',
-	    max_length = 50,
-	    null = True,
-        blank = True,	    
-	    help_text = ''
-  	    )
 
     result_item_source = models.ForeignKey(ResultSource,
         verbose_name = 'Source',
@@ -91,15 +23,7 @@ class ResultItem(MyBasicUuidModel):
 	    help_text = 'Reference to source, invoice, filename, machine, etc'
   	    )
 
-    
-    error_code = models.CharField(
-        verbose_name = 'Error codes',
-	    max_length = 50,
-	    null = True,
-        blank = True,	    
-	    help_text = ''
-  	    )
-  	    
+
     history = AuditTrail()
   	    
     def __unicode__(self):
@@ -111,4 +35,5 @@ class ResultItem(MyBasicUuidModel):
     class Meta:
         app_label = 'lab_result_item'    
         db_table = 'bhp_lab_core_resultitem'            
+            
        
