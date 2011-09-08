ccdca585419de56e21d31fc9952c3ef1d96d1bef
erik van widenfelt <ew2789@gmail.com>
2011-09-08 18:15:09 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@5 5124d8a1-613d-4708-9420-8c98912e35b2

--- forms.py
+++ forms.py
@@ -25,3 +25,13 @@ class ReviewForm(forms.ModelForm):
     
         return cleaned_data
 
+
+class ResultSearchForm(forms.Form):
+
+    result_search_term = forms.CharField(
+        max_length=35, 
+        label="Search term",
+        help_text="enter all or part of a order number, sample identifier, patient identifier, etc",
+        error_messages={'required': 'Please enter a search term.'},
+        )
+
