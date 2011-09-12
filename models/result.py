9b77e62e0819512a90b9eecdec4e7b5aeb350c39
erik van widenfelt <ew2789@gmail.com>
2011-09-12 12:26:21 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result@13 afa70ccf-cfc7-4289-b1b8-adaaa15ec0be

--- models/result.py
+++ models/result.py
@@ -1,63 +1,12 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from bhp_common.models import MyBasicUuidModel
 from lab_order.models import Order
 from lab_result.choices import RESULT_RELEASE_STATUS
+from base_result import BaseResult
 
-
-class Result(MyBasicUuidModel):
-
-    result_identifier = models.CharField(
-        max_length=25,
-        editable=False,
-        db_index=True,        
-        )
+class Result(BaseResult):
 
     order = models.ForeignKey(Order)
-    
-    result_datetime = models.DateTimeField(
-        help_text = 'Date result added to system.',
-        db_index=True,        
-        )
-
-    release_status = models.CharField(
-        max_length=25,
-        choices=RESULT_RELEASE_STATUS,
-        default='NEW',
-        db_index=True,        
-        )
-
-    release_datetime = models.DateTimeField(
-  	    null = True,
-        blank = True,	    
-        help_text = 'Date result authorized for release. This field will auto-fill if release status is changed',
-        db_index=True,        
-        )
-    
-    release_username = models.CharField(
-        verbose_name = "Release username",
-        max_length=50,
-	    null=True,
-	    blank=True,
-        help_text = 'Username of person authorizing result for release. This field will auto-fill if release status is changed',        	    
-        db_index=True,        
-	    )
-
-    comment = models.CharField(
-        verbose_name = 'Comment',
-	    max_length = 50,
-	    null = True,
-        blank = True,	    
-	    help_text = ''
-  	    )
-  	    
-    dmis_result_guid = models.CharField(
-        max_length=36,
-	    null = True,
-        blank = True,	
-        #editable=False,            
-	    help_text = 'dmis import value. N/A unless data imported from old system'
-        )  	    
     
     def __unicode__(self):
         return '%s' % (self.result_identifier)
@@ -74,4 +23,4 @@ class Result(MyBasicUuidModel):
     class Meta:
         app_label = 'lab_result' 
         db_table = 'bhp_lab_core_result'
-        ordering =['result_identifier','order','result_datetime',]  
+        ordering =['result_identifier','order','result_datetime',]      
