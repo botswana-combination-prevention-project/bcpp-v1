b3556cd4acfa4d70274155491f15872bcfd048a6
erik van widenfelt <ew2789@gmail.com>
2011-09-15 15:24:28 +0000

    git-svn-id: http://192.168.1.50/svn/lab_grading@30 d0c47599-63b2-4c0f-8d21-292bf8f76f30

--- classes/grade_flag.py
+++ classes/grade_flag.py
@@ -53,15 +53,15 @@ flag.flag
                                 test_code=self.test_code, 
                                 gender__icontains=self.gender,
                                 )    
-        grade = {'grade':'', 'range':''}        
+        grade = {'grade':'', 'range':{'lln':'', 'uln':''}}        
         if grading_list_items:
             for reference_list_item in grading_list_items:
                 #find the record for this age 
                 if reference_list_item.age_low_days() <= age_in_days and reference_list_item.age_high_days() >= age_in_days:
                     grade['grade'] = '0'
-                    grade['range'] = '%s - %s'% (round_up(reference_list_item.lln,self.test_code.display_decimal_places), round_up(reference_list_item.uln,self.test_code.display_decimal_places))
                     #see if value is in the of range of a grade
                     if round_up(self.result_item_value, self.test_code.display_decimal_places) >= round_up(reference_list_item.lln, self.test_code.display_decimal_places) and round_up(self.result_item_value, self.test_code.display_decimal_places) <= round_up(reference_list_item.uln, self.test_code.display_decimal_places):
                         grade['grade'] = reference_list_item.grade        
-                        grade['range'] = '%s - %s'% (round_up(reference_list_item.lln,self.test_code.display_decimal_places), round_up(reference_list_item.uln,self.test_code.display_decimal_places))                                               
+                        grade['range']['lln'] = round_up(reference_list_item.lln,self.test_code.display_decimal_places)
+                        grade['range']['uln'] = round_up(reference_list_item.uln,self.test_code.display_decimal_places)
         return grade
