9a474d6d5ef3414a3fce34a8a88fe86eed883dbd
erik van widenfelt <ew2789@gmail.com>
2011-09-22 05:09:39 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@12 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- classes/base_requisition_model_admin.py
+++ classes/base_requisition_model_admin.py
@@ -3,13 +3,14 @@ from lab_panel.models import Panel
 
 class BaseRequisitionModelAdmin(MyModelAdmin):
     
-    #override, limit dropdown in add_view to id passed in the URL        
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         
-        panel = request.GET.get('panel')
+        panel_pk = request.GET.get('panel',0)
+        if db_field.name == 'panel':
+            kwargs["queryset"] = Panel.objects.filter(pk = panel_pk)                                                        
         if db_field.name == 'aliquot_type':
-            if Panel.objects.filter(pk=panel):
-                kwargs["queryset"] = Panel.objects.filter(pk=panel)
+            if Panel.objects.filter(pk=panel_pk):
+                kwargs["queryset"] = Panel.objects.get(pk=panel_pk).aliquot_type.all()
             
         return super(BaseRequisitionModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)   
 
