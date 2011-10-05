f48e282bf4b2698a126887b397f103872ed9457b
erik van widenfelt <ew2789@gmail.com>
2011-10-05 08:15:14 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_code_lists@19 2d834fd6-8cdc-49e2-960a-59c1fb8158c1

--- admin.py
+++ admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from bhp_common.models import MyModelAdmin, MyStackedInline
 from models import DxCode
 
-admin.site.register(DxCode)
+class DxCodeAdmin(MyModelAdmin):
+
+    list_display = ('code', 'short_name')
+    
+    search_fields = ('code', 'short_name', 'long_name')
+    
+    readonly_fields = ('code', 'short_name', 'long_name')
+   
+admin.site.register(DxCode, DxCodeAdmin)
+
