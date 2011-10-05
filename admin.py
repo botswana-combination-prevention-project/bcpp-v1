ea13820979ee63b8f32333b04ab54acaf0d1a7c3
erik van widenfelt <ew2789@gmail.com>
2011-10-05 09:51:07 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_entry@19 ed14d539-c731-4f84-8017-1f76c29e69c2

--- admin.py
+++ admin.py
@@ -53,6 +53,10 @@ admin.site.register(ScheduledEntryBucket, ScheduledEntryBucketAdmin)
 class AdditionalEntryBucketAdmin(MyModelAdmin):
     
     list_display = ('registered_subject', 'content_type_map', 'entry_status', 'fill_datetime', 'due_datetime', 'close_datetime')
+    
+    list_filter = ('entry_status', 'fill_datetime',)    
+    
+    search_fields = ('registered_subject__subject_identifier', 'content_type_map__model')    
 
 admin.site.register(AdditionalEntryBucket, AdditionalEntryBucketAdmin)
 
