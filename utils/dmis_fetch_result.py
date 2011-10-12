4b4b19af8be05882bc653f8f60f1130bb84ba3e4
erik van widenfelt <ew2789@gmail.com>
2011-10-12 11:02:35 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@225 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_result.py
+++ utils/dmis_fetch_result.py
@@ -52,7 +52,10 @@ def create_or_update_result(**kwargs):
     cursor_resultitem = cnxn3.cursor()
 
     if Result.objects.filter(order=order):
+        results = Result.objects.filter(order=order)
         # update
+        for result in results:
+            
         
         pass
     else:
@@ -86,99 +89,134 @@ def create_or_update_result(**kwargs):
                 
             print 'order %s %s result %s' % ( order.order_identifier, order.order_datetime, result.result_identifier)
             
-            # get list of result items for this result from DMIS (LAB21ResponseQ001X0)
-            sql ='select l21d.sample_assay_date, \
-                  utestid, \
-                  result as result_value, \
-                  result_quantifier, \
-                  status, \
-                  mid,\
-                  l21d.validation_ref as validation_reference, \
-                  l21d.datelastmodified as result_item_datetime, \
-                  l21d.keyopcreated as user_created, \
-                  l21.datecreated as created \
-                  from BHPLAB.DBO.LAB21Response as L21 \
-                  left join BHPLAB.DBO.LAB21ResponseQ001X0 as L21D on L21.Q001X0=L21D.QID1X0 \
-                  where l21.id=\'%s\' and l21d.id is not null'  % result.order.order_identifier
-            
-            cursor_resultitem.execute(sql)
-            
             fetch_or_create_resultsource()
             
-            # loop thru result items and create
+            # loop thru result items
             for ritem in cursor_resultitem:
-                
-                code = ritem.utestid.strip(' \t\n\r')
-                
-                test_code = fetch_or_create_testcode(code)
-                
-                # change NT system username to auto
-                if ritem.user_created.strip(' \t\n\r').upper() == 'NT AUTHORITY\SYSTEM':
-                    user_created='auto'
-                else:
-                    user_created=ritem.user_created
-                # evaluate validation_reference
-                if ritem.validation_reference == '-9':
-                    # this is an item from GetResults TCP connected to PSM
-                    result_item_source = fetch_or_create_resultsource(interface='psm_interface')                
-                    result_item_source_reference = ''
-                    validation_reference = 'dmis-auto'
-                elif ritem.validation_reference == 'LAB21:MANUAL':
-                    # manual entry and no validation -- straight to LAB21 tableset
-                    result_item_source = fetch_or_create_resultsource(interface='manual_entry')                
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'auto'                
-                elif re.search('^rad[0-9A-F]{5}\.tmp$', ritem.validation_reference):    
-                    # this is an item from GetResults Flatfile and validated via the LAB05 path
-                    result_item_source = fetch_or_create_resultsource(interface='cd4_interface')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'lab05'                               
-                elif re.search('^LAB23:', ritem.validation_reference):          
-                    # manual entry and validated via the LAB23 validation path                           
-                    result_item_source = fetch_or_create_resultsource(interface='manual_entry')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'lab23'                               
-                elif re.search('^IMPORT', ritem.validation_reference):          
-                    # manual entry and validated via the LAB23 validation path                           
-                    result_item_source = fetch_or_create_resultsource(interface='direct_import')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'auto'     
-                elif re.search('^LB003:', ritem.validation_reference):          
-                    # manual import                           
-                    result_item_source = fetch_or_create_resultsource(interface='direct_import')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'auto'     
-                elif re.search('^LB004:', ritem.validation_reference):          
-                    # manual import                           
-                    result_item_source = fetch_or_create_resultsource(interface='direct_import')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'auto'  
-                elif re.search('^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$', ritem.validation_reference):                   
-                    # manual import                                           
-                    result_item_source = fetch_or_create_resultsource(interface='manual_entry')
-                    result_item_source_reference = 'dmis-%s' % ritem.validation_reference
-                    validation_reference = 'auto'  
-                else:
-                    # missed a case? let's hear about it
-                    raise TypeError('Validation reference \'%s\' was not expected. See dmis_fetch_result.' % ritem.validation_reference)
+                create_or_update_resultitem(ritem)                
 
-                # create a new result item. set validation to 'P', we'll import 
-                # full validation information later 
-                ResultItem.objects.create(
-                    result=result,
-                    test_code=test_code,
-                    result_item_datetime=ritem.result_item_datetime,                
-                    result_item_value=ritem.result_value,
-                    result_item_quantifier=ritem.result_quantifier,
-                    validation_status='P',
-                    validation_reference=validation_reference,
-                    result_item_source=result_item_source,
-                    result_item_source_reference=ritem.mid,
-                    result_item_operator=user_created,                
-                    comment='',
-                    )
     return None
     
+def create_or_update_resultitem(result=result, ritem=ritem)
+    # get list of result items for this result from DMIS (LAB21ResponseQ001X0)
+    sql ='select l21d.sample_assay_date, \
+          utestid, \
+          result as result_value, \
+          result_quantifier, \
+          status, \
+          mid,\
+          l21d.validation_ref as validation_reference, \
+          l21d.datelastmodified as result_item_datetime, \
+          l21d.keyopcreated as user_created, \
+          l21.datecreated as created \
+          from BHPLAB.DBO.LAB21Response as L21 \
+          left join BHPLAB.DBO.LAB21ResponseQ001X0 as L21D on L21.Q001X0=L21D.QID1X0 \
+          where l21.id=\'%s\' and l21d.id is not null'  % result.order.order_identifier
+    
+    cursor_resultitem.execute(sql)
+
+    code = ritem.utestid.strip(' \t\n\r')
+    if ResultItem.objects.filter(result=result, test_code__code=code)
+        # update
+        user = get_ritem_user(ritem.user_modified)            
+        validation = get_ritem_validation(ritem)
+        for result_item in ResultItem.objects.filter(result=result, test_code__code=ritem.code):
+            result_item.result_item_datetime=ritem.result_item_datetime
+            result_item.result_item_value=ritem.result_value
+            result_item.result_item_quantifier=ritem.result_quantifier
+            result_item.validation_status='P'
+            result_item.validation_reference=validation['validation_reference']
+            result_item.result_item_source=validation['result_item_source']
+            result_item.result_item_source_reference=validation['result_item_source_reference']
+            result_item.result_item_operator=user                
+            comment=''
+            result_item.save()
+    else:
+        # create
+        test_code = fetch_or_create_testcode(code)
+        user = get_ritem_user(ritem.user_created)            
+        validation = get_ritem_validation(ritem)
+        # create a new result item. set validation to 'P', we'll import 
+        # full validation information later 
+        ResultItem.objects.create(
+            result=result,
+            test_code=test_code,
+            result_item_datetime=ritem.result_item_datetime,                
+            result_item_value=ritem.result_value,
+            result_item_quantifier=ritem.result_quantifier,
+            validation_status='P',
+            validation_reference=validation['validation_reference'],
+            result_item_source=validation['result_item_source'],
+            result_item_source_reference=validation['result_item_source_reference'],
+            result_item_operator=user,                
+            comment='',
+            )
+        
+        
+def get_ritem_user(dmis_user):
+    # change NT system username to auto
+    if dmis_user.strip(' \t\n\r').upper() == 'NT AUTHORITY\SYSTEM':
+        user = 'auto'
+    else:
+        user = dmis_user    
+    return user
+    
+def get_ritem_validation(ritem):
+
+    result_item_source = ''
+    result_item_source_reference = ''
+    validation_reference = ''
+    
+    # evaluate validation_reference
+    if ritem.validation_reference == '-9':
+        # this is an item from GetResults TCP connected to PSM
+        result_item_source = fetch_or_create_resultsource(interface='psm_interface')                
+        result_item_source_reference = ''
+        validation_reference = 'dmis-auto'
+    elif ritem.validation_reference == 'LAB21:MANUAL':
+        # manual entry and no validation -- straight to LAB21 tableset
+        result_item_source = fetch_or_create_resultsource(interface='manual_entry')                
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'auto'                
+    elif re.search('^rad[0-9A-F]{5}\.tmp$', ritem.validation_reference):    
+        # this is an item from GetResults Flatfile and validated via the LAB05 path
+        result_item_source = fetch_or_create_resultsource(interface='cd4_interface')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'lab05'                               
+    elif re.search('^LAB23:', ritem.validation_reference):          
+        # manual entry and validated via the LAB23 validation path                           
+        result_item_source = fetch_or_create_resultsource(interface='manual_entry')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'lab23'                               
+    elif re.search('^IMPORT', ritem.validation_reference):          
+        # manual entry and validated via the LAB23 validation path                           
+        result_item_source = fetch_or_create_resultsource(interface='direct_import')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'auto'     
+    elif re.search('^LB003:', ritem.validation_reference):          
+        # manual import                           
+        result_item_source = fetch_or_create_resultsource(interface='direct_import')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'auto'     
+    elif re.search('^LB004:', ritem.validation_reference):          
+        # manual import                           
+        result_item_source = fetch_or_create_resultsource(interface='direct_import')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'auto'  
+    elif re.search('^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$', ritem.validation_reference):                   
+        # manual import                                           
+        result_item_source = fetch_or_create_resultsource(interface='manual_entry')
+        result_item_source_reference = 'dmis-%s' % ritem.validation_reference
+        validation_reference = 'auto'  
+    else:
+        # missed a case? let's hear about it
+        raise TypeError('Validation reference \'%s\' was not expected. See dmis_fetch_result.' % ritem.validation_reference)
+
+    result_item_source_reference = '%s %s' % (result_item_source_reference, ritem.mid)
+
+    return {'validation_reference': validation_reference, 'result_item_source':result_item_source,'result_item_source_reference':result_item_source_reference}
+
+
 def fetch_or_create_resultsource( **kwargs ):
 
     interfaces = ['psm_interface', 'cd4_interface','auto', 'manual_entry', 'direct_import',]
