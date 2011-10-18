c787255621b02c1a15c37e8e6659a78ff801cd05
erik van widenfelt <ew2789@gmail.com>
2011-10-18 16:17:06 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@15 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- classes/base_requisition_model_admin.py
+++ classes/base_requisition_model_admin.py
@@ -1,5 +1,7 @@
 from bhp_common.models import MyModelAdmin
 from lab_panel.models import Panel
+from bhp_appointment.classes import BaseVisitModelAdmin
+from bhp_appointment.classes import VisitModelHelper
 
 class BaseRequisitionModelAdmin(MyModelAdmin):
     
@@ -11,6 +13,15 @@ class BaseRequisitionModelAdmin(MyModelAdmin):
         if db_field.name == 'aliquot_type':
             if Panel.objects.filter(pk=panel_pk):
                 kwargs["queryset"] = Panel.objects.get(pk=panel_pk).aliquot_type.all()
+
+        
+        visit_model_helper = VisitModelHelper()
+        if db_field.name == visit_model_helper.get_visit_field(model=self.model, visit_model=self.visit_model):
+            kwargs["queryset"] = visit_model_helper.set_visit_queryset(
+                                                            subject_identifier = request.GET.get('subject_identifier', 0),
+                                                            visit_code = request.GET.get('visit_code', 0),
+                                                            visit_instance = request.GET.get('visit_instance', 0),
+                                                            )
             
         return super(BaseRequisitionModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)   
 
