df9d3195c013e7c920b8dfeb4684e4acd8df491a
erik van widenfelt <ew2789@gmail.com>
2011-10-24 07:28:54 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_dmis@262 1a682d4c-4399-4169-b676-170dd5b746ab

--- utils/dmis_fetch_result.py
+++ utils/dmis_fetch_result.py
@@ -31,7 +31,7 @@ import results for orders already in the system
 def fetch_results_from_dmis(**kwargs):
     subject_identifier = kwargs.get('subject_identifier')
     days = kwargs.get("days")
-    import_tdelta = None
+    import_tdelta = 90
     if days:
         try:
             import_tdelta = int(days)
