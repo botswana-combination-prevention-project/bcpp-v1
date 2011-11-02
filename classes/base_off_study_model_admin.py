4719cf161ebbe942adb9456c34a5c034f43c8b6c
erik van widenfelt <ew2789@gmail.com>
2011-11-02 14:36:58 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@108 a1c8eae8-7d53-4db9-beb7-be5090263015

--- classes/base_off_study_model_admin.py
+++ classes/base_off_study_model_admin.py
@@ -6,9 +6,11 @@ class BaseOffStudyModelAdmin(BaseRegisteredSubjectModelAdmin):
     def save_model(self, request, obj, form, change):
         
         # delete future appointments
+        visit_model_field = '%s__isnull' % self.visit_model_name
         Appointment.objects.filter(
             registered_subject=obj.registered_subject, 
-            appt_datetime__gt=obj.offstudy_datetime, 
-            ).extra(where=[self.visit_model_name+'=%s'], params=[True]).delete()    
-    
+            appt_datetime__gt=obj.offstudy_date, 
+            **{ visit_model_field:True }
+            ).delete()
+
         super(BaseOffStudyModelAdmin, self).save_model(request, obj, form, change)
