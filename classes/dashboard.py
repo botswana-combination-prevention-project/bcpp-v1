1b540321a4b406d82a8f713a588663ae2020c220
erik van widenfelt <ew2789@gmail.com>
2011-11-14 09:37:10 +0000

    changes for mochudi_survey
    git-svn-id: http://192.168.1.50/svn/bhp_dashboard@33 12d20db4-2ca3-46d0-ad31-f07335605e40

--- classes/dashboard.py
+++ classes/dashboard.py
@@ -3,30 +3,40 @@ from bhp_context.classes import BaseContext
 class Dashboard(object):
     
     context = BaseContext()
-    
+    dashboard_type = None    
+    template = None
+    dashboard_identifier = None
+    extra_url_context = None
+
     def __init__(self, **kwargs):
 
-        self.search_name = None    
-        self.dashboard_type = None
-        self.template = None
-        self.dashboard_identifier = None
-        
-        self.search_name = kwargs.get('search_name', self.search_name)
-        self.context.add(search_name = self.search_name ) 
+        self.search_name = None 
         
     def create(self, **kwargs):
 
-        self.dashboard_type = kwargs.get('dashboard_type', self.subject_type.lower())        
-        
-        self.template = kwargs.get('template', self.template)
+        if not self.dashboard_type:
+            self.dashboard_type = kwargs.get('dashboard_type')                        
+            if not self.dashboard_type:
+                raise ValueError, '%s requires a value for attribute dashboard_type.' % (self, )
+
+        if not self.dashboard_identifier:
+            self.dashboard_identifier = kwargs.get('dashboard_identifier')                        
+            if not self.dashboard_identifier:
+                raise ValueError, '%s requires a value for attribute dashboard_identifier. Perhaps set this in the view.' % (self, )
+
+
         if not self.template:
-            self.template = '%s_dashboard.html' % self.dashboard_type
+            self.template = kwargs.get('template','%s_dashboard.html' % self.dashboard_type)
+
+        if not self.extra_url_context:
+            self.extra_url_context = kwargs.get('extra_url_context', {})
 
         self.context.add(
             search_name = self.search_name,
             template = self.template,        
             dashboard_type = self.dashboard_type,
             dashboard_identifier = self.dashboard_identifier,
+            extra_url_context = self.extra_url_context,
             )        
 
     def get_urlpatterns(self, view, regex):
