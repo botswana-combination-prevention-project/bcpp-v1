4885116350dcdb0a3e916f8192690ca1ab4b1509
erik van widenfelt <ew2789@gmail.com>
2011-11-18 06:28:37 +0000

    add option to capture transaction in mybasicuuid save()
    git-svn-id: http://192.168.1.50/svn/bhp_common@171 bf858008-1fca-4ad0-9432-481439aa1b6d

--- models/my_basic_uuid_model.py
+++ models/my_basic_uuid_model.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from django.conf import settings
+from django.core import serializers
+from django.db.models import get_model
 from bhp_common.models import MyBasicModel
 from bhp_common.fields import MyUUIDField
+
 
 class MyBasicUuidModel(MyBasicModel):
 
@@ -7,7 +12,29 @@ class MyBasicUuidModel(MyBasicModel):
     
     id = MyUUIDField(primary_key=True)
 
+    def is_serialized(self):
+        return False
+        
+    def save(self, *args, **kwargs):
 
-
+        super(MyBasicUuidModel, self).save(*args, **kwargs)
+        
+        if self.pk and self.is_serialized() and not self._meta.proxy:
+            # serialize to file
+            # no need to serialize upon 'proxy model' save() as this will get called from the 'model' save()
+            #fname = '%s%s-%s.json' % (settings.MEDIA_ROOT, datetime.today().strftime('%Y%m%d%H%M%S%f'), str(self.pk),) 
+            #fd = open(fname, "w")
+            #serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), stream=fd)
+            #fd.close()
+            transaction = get_model('bhp_sync', 'transaction')
+            json = serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), )            
+            transaction.objects.create(
+                tx_name = self._meta.object_name,
+                tx_pk = self.pk,
+                tx = json,
+                timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
+                )
+            
+    
     class Meta:
         abstract = True
