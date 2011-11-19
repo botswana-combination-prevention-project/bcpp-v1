b50440ca40fe78de384731cd7f7614e572b5e6d9
erik van widenfelt <ew2789@gmail.com>
2011-11-19 16:47:35 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@173 bf858008-1fca-4ad0-9432-481439aa1b6d

--- models/my_basic_uuid_model.py
+++ models/my_basic_uuid_model.py
@@ -4,6 +4,8 @@ from django.core import serializers
 from django.db.models import get_model
 from bhp_common.models import MyBasicModel
 from bhp_common.fields import MyUUIDField
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 class MyBasicUuidModel(MyBasicModel):
@@ -20,16 +22,22 @@ class MyBasicUuidModel(MyBasicModel):
         return False
         
     def save(self, *args, **kwargs):
+    
+        # sneek in the transaction_producer, if called from 
+        # view in bhp_sync.
+        # get value and delete from kwargs before calling super
+        if 'transaction_producer' in kwargs:
+            transaction_producer = kwargs.get('transaction_producer')            
+            del kwargs['transaction_producer']
 
         super(MyBasicUuidModel, self).save(*args, **kwargs)
         
         if self.pk and self.is_serialized() and not self._meta.proxy:
-            # serialize to file
-            # no need to serialize upon 'proxy model' save() as this will get called from the 'model' save()
-            #fname = '%s%s-%s.json' % (settings.MEDIA_ROOT, datetime.today().strftime('%Y%m%d%H%M%S%f'), str(self.pk),) 
-            #fd = open(fname, "w")
-            #serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), stream=fd)
-            #fd.close()
+
+            action = 'I'
+            if self.pk:
+                action = 'U'
+                
             transaction = get_model('bhp_sync', 'transaction')
             json = serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), )            
             transaction.objects.create(
@@ -37,8 +45,48 @@ class MyBasicUuidModel(MyBasicModel):
                 tx_pk = self.pk,
                 tx = json,
                 timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
+                producer = transaction_producer,
+                action = action,                
                 )
-            
+ 
+    def delete(self, *args, **kwargs):
+    
+        if 'transaction_producer' in kwargs:
+            transaction_producer = kwargs.get('transaction_producer')            
+            del kwargs['transaction_producer']
+
+        super(MyBasicUuidModel, self).delete(*args, **kwargs)
+
+        if self.is_serialized() and not self._meta.proxy:
+
+            transaction = get_model('bhp_sync', 'transaction')
+            json = serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), )            
+            transaction.objects.create(
+                tx_name = self._meta.object_name,
+                tx_pk = self.pk,
+                tx = json,
+                timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
+                producer = transaction_producer,
+                action = 'D',
+                )
+
     
     class Meta:
         abstract = True
+        
+#@receiver(pre_delete, sender=MyBasicUuidModel)
+#def serialize_on_delete(sender, **kwargs):
+#    raise TypeError()
+#    if self.pk and self.is_serialized() and not self._meta.proxy:
+#
+#        transaction = get_model('bhp_sync', 'transaction')
+#        json = serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), )            
+#        transaction.objects.create(
+#            tx_name = self._meta.object_name,
+#            tx_pk = self.pk,
+#            tx = json,
+#            timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
+#            producer = transaction_producer,
+#            action = 'D',
+#            )
+
