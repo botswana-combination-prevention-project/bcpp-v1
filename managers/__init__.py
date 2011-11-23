5171aac884f63358b9e92f1b2fef61a28c01c5e9
erik van widenfelt <ew2789@gmail.com>
2011-11-23 09:17:08 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@26 72d6f641-25d3-4f20-903c-4c2123898bce

---
+++ managers/__init__.py
@@ -0,0 +1,1 @@
+from study_specific_manager import StudySpecificManager
---
+++ managers/study_specific_manager.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.query import QuerySet
+from mochudi_survey.models import Survey
+
+class StudySpecificManager(models.Manager):
+
+    def get_query_set(self):
+   
+        qs  = QuerySet(self.model, using=self._db).all()
+        if not qs:
+            raise ValueError, 'Appplication is accessing model StudySpecific but you have not populated it. Please do so before continuing.'
+        
+        return qs[0]
+
