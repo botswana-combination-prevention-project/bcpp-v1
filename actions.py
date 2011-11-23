27d8ee478b22898abcb3062d931fcc02ca67141b
erik van widenfelt <ew2789@gmail.com>
2011-11-23 19:38:05 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_netbook@22 5d149ac0-f06d-41b7-902c-eee1b30ec2e3

---
+++ actions.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from bhp_common.models import MyModelAdmin
+from bhp_nmap.utils import all_uphosts
+from models import Netbook
+
+def netbook_uphosts(modeladmin, request, queryset):
+
+    all_hosts = all_uphosts(network='192.168.11.0/24')    
+    for netbook in Netbook.objects.all():
+        if netbook.name in all_hosts.keys():        
+            netbook.last_seen = datetime.today()
+            netbook.save()
+            
+netbook_uphosts.short_description = "Refresh list of active netbooks"
--- admin.py
+++ admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.utils.translation import ugettext as _
 from bhp_common.models import MyModelAdmin, MyStackedInline
 from bhp_netbook.models import Netbook, NetbookUser
+from actions import netbook_uphosts
+
 
 class NetbookAdmin (MyModelAdmin):
 
     list_display = ('name', 'db_name','make', 'model', 'ip_address', 'is_active', 'last_seen', 'serial_number')
     list_per_page = 25
+
+    actions = [netbook_uphosts,]
 
 admin.site.register(Netbook, NetbookAdmin)
 
