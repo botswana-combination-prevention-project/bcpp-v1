7842596fd25c7998ccff54c2a43c1dc84ff5f1af
erik van widenfelt <ew2789@gmail.com>
2011-11-23 13:56:50 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@27 72d6f641-25d3-4f20-903c-4c2123898bce

--- managers/study_specific_manager.py
+++ managers/study_specific_manager.py
@@ -9,6 +9,8 @@ class StudySpecificManager(models.Manager):
         qs  = QuerySet(self.model, using=self._db).all()
         if not qs:
             raise ValueError, 'Appplication is accessing model StudySpecific but you have not populated it. Please do so before continuing.'
+            
+        qs  = QuerySet(self.model, using=self._db).filter(pk=qs[0].pk)                    
         
-        return qs[0]
+        return qs
 
