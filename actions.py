6fbf3df96b4bc5670890df933edcc89bbb887241
erik van widenfelt <ew2789@gmail.com>
2011-11-23 19:58:41 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_netbook@25 5d149ac0-f06d-41b7-902c-eee1b30ec2e3

--- actions.py
+++ actions.py
@@ -8,7 +8,10 @@ def netbook_uphosts(modeladmin, request, queryset):
     all_hosts = all_uphosts(network='192.168.11.0/24')    
     for netbook in Netbook.objects.all():
         if netbook.name in all_hosts.keys():        
+            netbook.is_alive = True
             netbook.last_seen = datetime.today()
-            netbook.save()
-            
+        else:
+            netbook.is_alive = False                        
+        netbook.save()            
+        
 netbook_uphosts.short_description = "Refresh list of active netbooks"
