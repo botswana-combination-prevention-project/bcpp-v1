71ee41b19ba363c061060752a38d394251717c4b
erik van widenfelt <ew2789@gmail.com>
2012-01-20 07:21:36 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@82 833481ee-4f4c-48f6-8756-2185fbac41e5

--- models/base_locator.py
+++ models/base_locator.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
@@ -16,84 +17,88 @@ from bhp_variables.models import StudySite
 class BaseLocator(MyBasicUuidModel): 
 
     date_signed = models.DateField( 
-        verbose_name = "1.Date Locator Form signed ",
+        verbose_name = "1. Date Locator Form signed ",
+        default = date.today(),
         help_text="",
         ) 
     mail_address = OtherCharField(
         max_length=35,
-        verbose_name="1a. Mailing address ",
+        verbose_name = "Mailing address ",
         help_text="",
         )
     care_clinic = OtherCharField(
         max_length=35,
-        verbose_name="1b. Health clinic where your infant will receive their routine care ",
+        verbose_name = "Health clinic where your infant will receive their routine care ",
         help_text="",
         ) 
     home_visit_permission = models.CharField(
-        max_length=25,
-        choices=YES_NO,
-        verbose_name="2. Has the participant given her permission for study staff to make home visits for follow-up purposes before and during the study?",
-        help_text="if 'No' go to Question 3, otherwise continue",
+        max_length = 25,
+        choices = YES_NO,
+        verbose_name = "Has the participant given her permission for study staff to make home visits for follow-up purposes before and during the study?",
         )
     physical_address = models.TextField(
-        max_length=350,
-        verbose_name="2a. If yes, please provide physical address with detailed description",
+        max_length = 350,
+        verbose_name = "Physical address with detailed description",
+        blank=True,
+        null=True,
         help_text="",
         ) 
     may_follow_up = models.CharField(
         max_length=25,
         choices=YES_NO,
-        verbose_name="3. Has the participant given her permission for study staff to call her  for follow-up purposes before and during the study?", 
-        help_text="if 'No' go to Question 4, otherwise continue",
+        verbose_name="3. Has the participant given her permission for study staff to call her for follow-up purposes before and during the study?", 
         )
+
     subject_cell = models.IntegerField(
-        max_length=8,
-        verbose_name="3a. Cell number",
+        max_length = 8,
+        verbose_name = "Cell number",
         validators = [BWCellNumber,],
         blank=True,
         null=True,
         help_text="",
         )
     subject_cell_alt= models.IntegerField(
-        max_length=8,
-        verbose_name="3b. Cell number (alternate)",
+        max_length = 8,
+        verbose_name = "Cell number (alternate)",
         validators = [BWCellNumber,],
         help_text="",
         blank=True,
         null=True,
         )
     subject_phone = models.IntegerField(
-        max_length=8,
-        verbose_name="3c. Telephone",  
+        max_length = 8,
+        verbose_name = "Telephone",  
         validators = [BWTelephoneNumber,],    
         help_text="",
         blank=True,
         null=True,
         )  
+        
     subject_phone_alt = models.IntegerField(
         max_length=8,
-        verbose_name="3d. Telephone (alternate)",               
+        verbose_name = "Telephone (alternate)",               
         help_text="",
         validators = [BWTelephoneNumber,],
         blank=True,
         null=True,
         )  
+        
     may_call_work = models.CharField(
         max_length=25,
         choices=YES_NO_DOESNT_WORK,
-        verbose_name="4. Has the participant given her permission for study staff to contact her at work for follow up purposes before and during the study?", 
+        verbose_name = "Has the participant given her permission for study staff to contact her at work for follow up purposes before and during the study?", 
         help_text=" if 'No' go to section B, otherwise continue"
     )
     subject_work_place = models.TextField(
         max_length=35,
-        verbose_name="4a. Name and location of work place",
+        verbose_name = "Name and location of work place",
         help_text="",
         blank=True,
         null=True,        
         )
     subject_work_phone = models.IntegerField(
         max_length=8,
-        verbose_name="4b.Work telephone number ",               
+        verbose_name = "Work telephone number ",               
         help_text="",
         validators = [BWTelephoneNumber,],
         blank=True,
@@ -102,28 +107,30 @@ class BaseLocator(MyBasicUuidModel):
     may_contact_someone = models.CharField(
         max_length=25,
         choices=YES_NO,
-        verbose_name="5. Has the participant given her permission for study staff to contact anyone else (e.g partner, spouse, family member, neighbour) for follow-up purposes before and during the study?", 
-        help_text="",
+        verbose_name = "Has the participant given her permission for study staff to contact anyone else for follow-up purposes before and during the study?", 
+        help_text = "For example a partner, spouse, family member, neighbour ...",
         )
     contact_name = OtherCharField(
         max_length=35,
-        verbose_name="5a.Full names of the contact person",
+        verbose_name = "Full names of the contact person",
         help_text="",
         )
     contact_rel = OtherCharField(
         max_length=35,
-        verbose_name="5b.Relationship to participant",
+        verbose_name = "Relationship to participant",
         help_text="",
         
         )
     contact_physical_address = models.TextField(
         max_length=350,
-        verbose_name="5c.Full physical address ",
+        verbose_name = "Full physical address ",
+        blank=True,
+        null=True,
         help_text="",
         )
     contact_cell = models.IntegerField(
         max_length=8,
-        verbose_name="5d. Cell number",
+        verbose_name = "Cell number",
         validators = [BWCellNumber,],
         help_text="",
         blank=True,
@@ -131,7 +138,7 @@ class BaseLocator(MyBasicUuidModel):
         )
     contact_phone = models.IntegerField(
         max_length=8,
-        verbose_name="5e. Telephone number",
+        verbose_name = "Telephone number",
         validators = [BWTelephoneNumber,],
         help_text="", 
         blank=True,
