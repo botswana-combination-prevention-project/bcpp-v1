54ececaf75135abb58924e556e932b62fc112e15
erik van widenfelt <ew2789@gmail.com>
2012-02-06 09:10:29 +0000

    add BW Cell 77, 78
    git-svn-id: http://192.168.1.50/svn/bhp_common@189 bf858008-1fca-4ad0-9432-481439aa1b6d

--- validators.py
+++ validators.py
@@ -20,7 +20,7 @@ def TelephoneNumber(value, pattern, word):
         raise ValidationError(u'Invalid %s number. You entered %s.' % (word, str_value))
 
 def BWCellNumber(value):
-    TelephoneNumber(value, '^[7]{1}[123456]{1}[0-9]{6}$', 'cell')
+    TelephoneNumber(value, '^[7]{1}[12345678]{1}[0-9]{6}$', 'cell')
     
 def BWTelephoneNumber(value):
     TelephoneNumber(value, '^[2-8]{1}[0-9]{6}$', 'telephone')
