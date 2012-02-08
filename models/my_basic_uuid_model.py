9ff09a78a523e196f19700af3f18bf9b65ca8cd0
erik van widenfelt <ew2789@gmail.com>
2012-02-08 10:47:21 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@190 bf858008-1fca-4ad0-9432-481439aa1b6d

--- models/my_basic_uuid_model.py
+++ models/my_basic_uuid_model.py
@@ -1,13 +1,12 @@
 from datetime import datetime
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
 from django.conf import settings
-from django.core import serializers
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
 from django.db.models import get_model
 from bhp_common.models import MyBasicModel
 from bhp_common.fields import MyUUIDField
-from bhp_sync.classes import TransactionProducer
-from bhp_sync.decorators import receiver_subclasses
+from bhp_sync.classes import TransactionProducer, SerializeToTransaction
+
 
 class MyBasicUuidModel(MyBasicModel):
 
@@ -27,10 +26,10 @@ class MyBasicUuidModel(MyBasicModel):
         # sneek in the transaction_producer, if called from 
         # view in bhp_sync.
         # get value and delete from kwargs before calling super
-        transaction_producer = TransactionProducer()
-        if 'transaction_producer' in kwargs:
-            transaction_producer = kwargs.get('transaction_producer')            
-            del kwargs['transaction_producer']
+        #transaction_producer = TransactionProducer()
+        #if 'transaction_producer' in kwargs:
+        #    transaction_producer = kwargs.get('transaction_producer')            
+        #    del kwargs['transaction_producer']
         
         # use 'suppress_autocreate_on_deserialize' to not allow save methods
         # to create new model instances such as appointments, ScheduledEntry, etc
@@ -48,11 +47,13 @@ class MyBasicUuidModel(MyBasicModel):
         # trigger a transaction, but make sure the parent "model" has
         # is_serialized=True, otherwise no transaction will be created.
 
+        """
         if self.pk and self.is_serialized():
             transaction = get_model('bhp_sync', 'transaction')
             use_natural_keys = False
             if 'natural_key' in dir(self):
-                use_natural_keys = True
+                use_natural_keys = Truefrom django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
             #if this is a proxy model, get to the main model
             if self._meta.proxy_for_model:
                 obj = self._meta.proxy_for_model.objects.get(pk=self.pk)
@@ -70,8 +71,8 @@ class MyBasicUuidModel(MyBasicModel):
                 producer = str(transaction_producer),
                 action = action,                
                 )
-            #messages.add_message('', messages.SUCCESS, 'Successfully serialized %s for producer %s' %(unicode(self),str(transaction_producer)))                                                            
- 
+        """            
+         
     def delete(self, *args, **kwargs):
 
         #TODO: get this to work in pre_delete signal
@@ -83,7 +84,8 @@ class MyBasicUuidModel(MyBasicModel):
         if self.is_serialized() and not self._meta.proxy:
 
             transaction = get_model('bhp_sync', 'transaction')
-            json_obj = serializers.serialize("json", self.__class__.objects.filter(pk=self.pk), use_natural_keys=True )            
+            json_obj = serializers.serialize(
+                "json", self.__class__.objects.filter(pk=self.pk), use_natural_keys=True )            
             transaction.objects.create(
                 tx_name = self._meta.object_name,
                 tx_pk = self.pk,
@@ -97,20 +99,23 @@ class MyBasicUuidModel(MyBasicModel):
 
     class Meta:
         abstract = True
-"""        
-@receiver_subclasses(pre_delete, MyBasicUuidModel, "mybasicuuidmodel_pre_delete")
-def serialize_on_delete(sender, instance, **kwargs):
-    if instance.is_serialized() and not instance._meta.proxy:
-        transaction_producer = TransactionProducer()    
-        transaction = get_model('bhp_sync', 'transaction')
-        json = serializers.serialize("json", instance.__class__.objects.filter(pk=instance.pk), )            
-        transaction.objects.create(
-            tx_name = instance._meta.object_name,
-            tx_pk = instance.pk,
-            tx = json,
-            timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
-            producer = str(transaction_producer),
-            action = 'D',
-            )
-"""
+
+
+@receiver(m2m_changed,)
+def serialize_m2m_on_save(sender, instance, **kwargs):
+    if kwargs.get('action') == 'post_add':
+        if isinstance(instance, MyBasicUuidModel):
+            if instance.is_serialized() and not instance._meta.proxy:
+                serialize_to_transaction = SerializeToTransaction()
+                serialize_to_transaction.serialize(sender, instance,**kwargs)
+                   
+@receiver(post_save,)
+def serialize_on_save(sender, instance, **kwargs):
+    if isinstance(instance, MyBasicUuidModel):
+        if instance.is_serialized() and not instance._meta.proxy:
+            serialize_to_transaction = SerializeToTransaction()
+            serialize_to_transaction.serialize(sender, instance,**kwargs)
+
+
+
 
