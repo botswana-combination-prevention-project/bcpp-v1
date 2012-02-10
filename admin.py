5bf0337245f561f3740ec17f3ee70bc3716776db
erik van widenfelt <ew2789@gmail.com>
2012-02-10 15:33:44 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@30 72d6f641-25d3-4f20-903c-4c2123898bce

--- admin.py
+++ admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from bhp_variables.models import StudySpecific, StudySite
 
+class StudySpecificAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "protocol_number", 
+        "study_start_datetime",
+        "hostname_prefix",
+        "device_id",
+        )
+
 admin.site.register(StudySpecific)
 
 admin.site.register(StudySite)
