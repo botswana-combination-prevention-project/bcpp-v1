4ae874fdc14730239397bf000c4641e1fa97dddc
erik van widenfelt <ew2789@gmail.com>
2012-02-10 12:03:04 +0000

    changes to formatted_age utility
    git-svn-id: http://192.168.1.50/svn/bhp_common@193 bf858008-1fca-4ad0-9432-481439aa1b6d

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from my_basic_model import MyBasicModel
 from my_basic_uuid_model import MyBasicUuidModel
+#from my_new_basic_uuid_model import MyNewBasicUuidModel
 from my_basic_list_model import MyBasicListModel
 from my_basic_code_list_model import MyBasicCodeListModel
 from my_model_admin import MyModelAdmin
--- utils/age.py
+++ utils/age.py
@@ -1,15 +1,21 @@
 from datetime import *
 from dateutil.relativedelta import *
 
-def formatted_age(born, reference_date):
+def formatted_age(born, reference_date, month_lower_boundry=0):
+
+    """
+        if month_lower_boundry = 0, first month will ready as 1m, etc
+        if month_lower_boundry = 1, first month will ready as 
+        30/31/28/29 days and second month will be 2mxd  
+    """
     
     if born:
         rdelta = relativedelta(reference_date, born)
         if born > reference_date:
             return '?'
-        elif rdelta.years == 0 and rdelta.months == 0:
+        elif rdelta.years == 0 and rdelta.months <= month_boundry:
             return '%sd' % (rdelta.days)
-        elif rdelta.years == 0 and rdelta.months > 0 and rdelta.months <= 2:
+        elif rdelta.years == 0 and rdelta.months > month_boundry and rdelta.months <= 2:
             return '%sm%sd' % (rdelta.months,rdelta.days)
         elif rdelta.years == 0 and rdelta.months > 2:
             return '%sm' % (rdelta.months)
