56cd3847f8577a7061c840a5a1cd087eb324fb97
erik van widenfelt <ew2789@gmail.com>
2012-02-20 11:06:13 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_netbook@47 5d149ac0-f06d-41b7-902c-eee1b30ec2e3

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
 from bhp_common.models import MyModelAdmin, MyStackedInline
-from bhp_netbook.models import Netbook, NetbookUser, SvnHistory
+from bhp_netbook.models import Netbook, NetbookUser #, SvnHistory
 from actions import netbook_uphosts #, netbook_update_svn
 
 
@@ -21,7 +21,7 @@ class NetbookUserAdmin (MyModelAdmin):
 
 admin.site.register(NetbookUser, NetbookUserAdmin)
 
-
+"""
 class SvnHistoryAdmin (MyModelAdmin):
 
     list_display = ('netbook','repo', 'last_revision_number', 'last_revision_date')
