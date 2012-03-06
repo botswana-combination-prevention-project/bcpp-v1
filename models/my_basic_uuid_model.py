ff2525159c40f0e48cde69ad9a0a3bfb50c9182d
erik van widenfelt <ew2789@gmail.com>
2012-03-06 08:36:06 +0000

    modify signals in Basic UUID
    git-svn-id: http://192.168.1.50/svn/bhp_common@202 bf858008-1fca-4ad0-9432-481439aa1b6d

--- models/my_basic_uuid_model.py
+++ models/my_basic_uuid_model.py
@@ -34,50 +34,14 @@ class MyBasicUuidModel(MyBasicModel):
             #transaction_producer = kwargs.get('transaction_producer')            
             del kwargs['transaction_producer']
         
-        # use 'suppress_autocreate_on_deserialize' to not allow save methods
+        # used 'suppress_autocreate_on_deserialize' to not allow save methods
         # to create new model instances such as appointments, ScheduledEntry, etc
         # as these will be serialized on the producer
         if 'suppress_autocreate_on_deserialize' in kwargs:
             del kwargs['suppress_autocreate_on_deserialize']
-            
-        action = 'I'
-        if self.pk:
-            action = 'U'
 
         super(MyBasicUuidModel, self).save(*args, **kwargs)
-        
-        # note that i do not want both the proxy model and its parent model to 
-        # trigger a transaction, but make sure the parent "model" has
-        # is_serialized=True, otherwise no transaction will be created.
-
-        """
-        if self.pk and self.is_serialized():
-            transaction = get_model('bhp_sync', 'transaction')
-            use_natural_keys = False
-            if 'natural_key' in dir(self):
-                use_natural_keys = Truefrom django.db.models.signals import post_save, m2m_changed
-from django.dispatch import receiver
-            #if this is a proxy model, get to the main model
-            if self._meta.proxy_for_model:
-                obj = self._meta.proxy_for_model.objects.get(pk=self.pk)
-            else:
-                obj = self    
-            #if self.__class__._meta.object_name.lower() == 'qn002sectionone':
-            #    raise TypeError(self.__dict__)    
-            json_tx = serializers.serialize("json", 
-                            obj.__class__.objects.filter(pk=obj.pk),
-                            ensure_ascii=False, 
-                            use_natural_keys=use_natural_keys)            
-            transaction.objects.create(
-                tx_name = obj._meta.object_name,
-                tx_pk = obj.pk,
-                tx = json_tx,
-                timestamp = datetime.today().strftime('%Y%m%d%H%M%S%f'),
-                producer = str(transaction_producer),
-                action = action,                
-                )
-        """            
-         
+                 
     def delete(self, *args, **kwargs):
 
         #TODO: get this to work in pre_delete signal
@@ -105,23 +69,27 @@ from django.dispatch import receiver
     class Meta:
         abstract = True
 
+""" Signals """
 
-@receiver(m2m_changed,)
+@receiver(m2m_changed, weak=False, dispatch_uid='serialize_m2m_on_save')
 def serialize_m2m_on_save(sender, instance, **kwargs):
+    """ part of the serialize transaction process that ensures m2m are serialized correctly """
     if kwargs.get('action') == 'post_add':
         if isinstance(instance, MyBasicUuidModel):
             if instance.is_serialized() and not instance._meta.proxy:
                 serialize_to_transaction = SerializeToTransaction()
                 serialize_to_transaction.serialize(sender, instance,**kwargs)
                    
-@receiver(post_save,)
+@receiver(post_save, weak=False, dispatch_uid='serialize_on_save')
 def serialize_on_save(sender, instance, **kwargs):
+    """ serialize the model instance to the outgoing transaction model for consumption by another application """
     if isinstance(instance, MyBasicUuidModel):
         if instance.is_serialized() and not instance._meta.proxy:
             serialize_to_transaction = SerializeToTransaction()
             serialize_to_transaction.serialize(sender, instance,**kwargs)
 
-@receiver(post_save,)
-def scheduled_entry_on_save(sender, instance, **kwargs):
+@receiver(post_save, weak=False, dispatch_uid='model_bucket_rules_on_save' )
+def model_bucket_rules_on_save(sender, instance, **kwargs):
+    """ update / run model bucket rules, see bhp_bucket """
     if isinstance(instance, MyBasicUuidModel):
         bucket.update(instance)
