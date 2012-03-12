f18846d10c459847afe06998ac26be3b905e8b39
erik van widenfelt <ew2789@gmail.com>
2012-03-12 10:15:38 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@109 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- models/outgoing_transaction.py
+++ models/outgoing_transaction.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver 
 from base_transaction import BaseTransaction
+from bhp_mq.classes import mq_producer_controller
 
 
 class OutgoingTransaction(BaseTransaction):
@@ -12,8 +13,9 @@ class OutgoingTransaction(BaseTransaction):
         ordering = ['timestamp']
         
         
-@receiver(post_save, sender=OutgoingTransaction,  dispatch_uid="deserialize_on_post_save")
-def deserialize_on_post_save(sender, instance, **kwargs):
+@receiver(post_save, sender=OutgoingTransaction,  dispatch_uid="send_to_mq_on_post_save")
+def send_to_mq_on_post_save(sender, instance, **kwargs):
     if isinstance(instance, OutgoingTransaction):
         if not instance.is_consumed and not instance.is_error:
-            pass
+            mq_producer_controller.send_message(instance)
+        
