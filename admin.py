2f7b0f2995c4f5e69392c8b99ad86e8b6148f14c
erik van widenfelt <ew2789@gmail.com>
2012-04-02 09:09:25 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@101 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from bhp_common.models import MyModelAdmin, MyTabularInline
 from django.db.models import Max
 from bhp_registration.models import RegisteredSubject
