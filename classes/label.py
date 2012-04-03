5cd922108942796ac38352ae261bb4008b341ad4
erik van widenfelt <ew2789@gmail.com>
2012-04-03 10:03:02 +0000

    lpr to lp
    git-svn-id: http://192.168.1.50/svn/lab_barcode@30 ca86f7b5-b579-4098-9fdc-afce5cd0fe11

--- classes/label.py
+++ classes/label.py
@@ -137,8 +137,8 @@ class Label(object):
                     try:
                         # note -r will delete the file after printing ...
 
-                        #subprocess.call(['lpr', '-P' ,self.label_printer.cups_printer_name, '-l', self.file_name, '-H', self.label_printer.cups_server_ip], shell=False)                        
-                        subprocess.call(['lp', '-d' ,self.label_printer.cups_printer_name, '', self.file_name, '-h', self.label_printer.cups_server_ip], shell=False)                        
+                        subprocess.call(['lpr', '-P' ,self.label_printer.cups_printer_name, '-l', self.file_name, '-H', self.label_printer.cups_server_ip], shell=False)                        
+                        #subprocess.call(['lp', '-d' ,self.label_printer.cups_printer_name, '', self.file_name, '-h', self.label_printer.cups_server_ip], shell=False)                        
                         self.message = "Label sent to printer %s" % self.label_printer.cups_printer_name
                         self.printer_error = False            
                     except subprocess.CalledProcessError, e:
