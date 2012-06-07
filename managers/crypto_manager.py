0f0cb40f9405168252db11f8d390f1555a8700fc
erik van widenfelt <ew2789@gmail.com>
2012-06-07 12:43:46 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@3 dc6f6662-d1d5-42af-b45d-bfa79074b674

---
+++ managers/crypto_manager.py
@@ -0,0 +1,22 @@
+from django.db import models
+from bhp_crypto.classes import Hasher
+
+
+class CryptoQuerySet(models.query.QuerySet):
+    
+    hasher = Hasher()
+    
+    def _filter_or_exclude(self, negate, *args, **kwargs):
+        
+        cust_lookups = filter(lambda s: s[0].endswith('__hash'), kwargs.items())
+        for lookup in cust_lookups:
+            kwargs.pop(lookup[0])
+            lookup_prefix = lookup[0].rsplit('__',1)[0]
+            kwargs.update({lookup_prefix + '__contains':self.hasher.get_hash(lookup[1]).hexdigest()})
+        return super(CryptoQuerySet, self)._filter_or_exclude(negate, *args, **kwargs)
+    
+    
+class CryptoManager(models.Manager):
+    
+    def get_query_set(self):
+        return CryptoQuerySet(self.model)
