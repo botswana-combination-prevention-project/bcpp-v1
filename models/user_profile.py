bfa1c71c0c1013c910fd0522e4db901e42ec0e54
erik van widenfelt <ew2789@gmail.com>
2012-06-12 09:58:19 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_userprofile@5 286037fc-dbac-43a1-8671-deda17dc685f

--- models/user_profile.py
+++ models/user_profile.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-from bhp_common.fields import NameField, InitialsField
+from bhp_base_model.fields import InitialsField
 from bhp_userprofile.choices import SALUTATION
+
 
 class UserProfile(models.Model):
     # add AUTH_PROFILE_MODULE = "bhp_userprofile.bhpprofile" o settings.py
