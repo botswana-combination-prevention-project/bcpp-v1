abe302d1e7a2438a96b5c52443d63148990fed08
erik van widenfelt <ew2789@gmail.com>
2012-06-12 09:55:28 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@213 bf858008-1fca-4ad0-9432-481439aa1b6d

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,4 +1,3 @@
 from base_template_context import *
 from my_model_form import MyModelForm
 from locking import *
-from logic_check import LogicCheck
--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from index_together import *
 from my_basic_model import MyBasicModel
-from my_basic_uuid_model import MyBasicUuidModel
-#from my_new_basic_uuid_model import MyNewBasicUuidModel
 from my_basic_list_model import MyBasicListModel
 from my_basic_code_list_model import MyBasicCodeListModel
 from my_model_admin import MyModelAdmin
