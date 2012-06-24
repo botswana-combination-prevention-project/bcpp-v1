00711825e8123565eb2ba382fc01d13b657c4a88
erik van widenfelt <ew2789@gmail.com>
2012-06-24 10:21:43 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@152 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from subject_identifier_audit_trail import *
 from base_randomization_list import *
 from registered_subject import *
-from base_off_study import *
 from base_registered_subject_model import BaseRegisteredSubjectModel, delete_unused_appointments
 from base_registration_model import BaseRegistrationModel
-#from base_registered_subject_registration_model import BaseRegisteredSubjectRegistrationModel 
