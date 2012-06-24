7a2d790eba136227fb2a4ef0bde3ebf1c202a708
erik van widenfelt <ew2789@gmail.com>
2012-06-24 20:22:10 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@2 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

---
+++ managers/__init__.py
@@ -0,0 +1,1 @@
+from visit_tracking_manager import VisitTrackingManager
---
+++ managers/visit_tracking_manager.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class VisitTrackingManager(models.Manager):
+
+    def get_by_natural_key(self, registered_subject, visit_definition, visit_instance):
+        return self.get(registered_subject=registered_subject, visit_definition=visit_definition, visit_instance=visit_instance)
+    
+    def get_by_natural_key_with_dict(self, **kwargs):
+        return self.get(**kwargs)
