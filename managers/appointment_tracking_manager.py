f3558af8cc1ce7a05d0f3d10d89f2cdaea0a0bcd
erik van widenfelt <ew2789@gmail.com>
2012-06-24 19:12:45 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@106 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- managers/appointment_tracking_manager.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class AppointmentTrackingManager(models.Manager):
-
-    def get_by_natural_key(self, registered_subject, visit_definition, visit_instance):
-        return self.get(registered_subject=registered_subject, visit_definition=visit_definition, visit_instance=visit_instance)
-    
-    def get_by_natural_key_with_dict(self, **kwargs):
-        return self.get(**kwargs)
