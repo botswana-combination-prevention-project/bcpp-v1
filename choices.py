61f803c5f765fa5183496d6e044037dd0c408f40
erik van widenfelt <ew2789@gmail.com>
2012-06-27 06:23:39 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_adverse@39 89b61199-f52f-466d-8b3a-10004189116d

--- choices.py
+++ choices.py
@@ -6,6 +6,12 @@ GRADING_SCALE = (
     (5, 'Grade 5'),                
 )
 
+GRADING_SCALE_234 = (
+    (2, 'Grade 2'),
+    (3, 'Grade 3'),
+    (4, 'Grade 4'),
+)
+
 GRADING_SCALE_34 = (
     (3, 'Grade 3'),
     (4, 'Grade 4'),
