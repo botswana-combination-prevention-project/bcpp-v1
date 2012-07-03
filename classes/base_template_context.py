2009e31a5e3b544465d8b6eb2be98c24221f617a
erik van widenfelt <ew2789@gmail.com>
2012-07-03 05:28:53 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@217 bf858008-1fca-4ad0-9432-481439aa1b6d

--- classes/base_template_context.py
+++ classes/base_template_context.py
@@ -1,26 +1,31 @@
+from django.conf import settings
+from bhp_common.utils import os_variables
+
+
 class BaseTemplateContext(dict):
    
-    """manage the default template context""" 
+    """Descriptor for the default template context""" 
     
     def __init__(self, **kwargs):
-        
-        from defaults import TEMPLATE_CONTEXT
-
         self.data = {}                                
-
-        if TEMPLATE_CONTEXT is not None: self.update(**TEMPLATE_CONTEXT)
-        
-        """ 
-            section_name, for example 'clinic', 'lab', 'admin', 'surveys' 
-            Comes as a parameter from the Url
-        """
+        defaults = {
+            'extend': "base_site.html", #required, but this default will avoid an error           
+            'template': "", #required
+            'report_title': "",
+            'section_name': "",
+            'os_variables': os_variables,
+            'sql':"",
+            'database': settings.DATABASES['default'],
+            }
+        self.update(**defaults)
+        #section_name, for example 'clinic', 'lab', 'admin', 'surveys' comes as a parameter from the Url
         if kwargs.get('section_name') is None:
             raise TypeError('BaseSearch requires keyword/value for key \'section_name\'. None given.')
         else:
             self['section_name'] =  kwargs.get('section_name')
-
     
-    def context(self): return self.data
+    def context(self): 
+        return self.data
         
     def __getitem__(self, key):
         return self.data[key]
@@ -29,14 +34,12 @@ class BaseTemplateContext(dict):
         self.data[key] = item
     
     def update(self, **kwargs ): 
-
         keys = kwargs.keys()
         for kw in keys:
             self.data.update({ kw:kwargs[kw] })
-    
-        #if dict is not None: self.data.update(dict)
         
-    def clear(self): self.data.clear()         
+    def clear(self): 
+        self.data.clear()         
 
     def copy(self):                            
         if self.__class__ is dict:         
@@ -44,11 +47,12 @@ class BaseTemplateContext(dict):
         import copy                            
         return copy.copy(self)                 
 
-    def keys(self): return self.data.keys()    
+    def keys(self): 
+        return self.data.keys()    
 
-    def items(self): return self.data.items()  
+    def items(self): 
+        return self.data.items()  
 
-    def values(self): return self.data.values()
+    def values(self): 
+        return self.data.values()
     
-    
-        
