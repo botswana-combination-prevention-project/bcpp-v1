a56f0448e919ef0268e126cadf0c30d3650a3da0
erik van widenfelt <ew2789@gmail.com>
2012-07-10 10:49:37 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_common@218 bf858008-1fca-4ad0-9432-481439aa1b6d

--- classes/base_template_context.py
+++ classes/base_template_context.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from bhp_common.utils import os_variables
 
 
-class BaseTemplateContext(dict):
+class BaseContext(dict):
    
     """Descriptor for the default template context""" 
     
     def __init__(self, **kwargs):
-        self.data = {}                                
+        self._context = {}                                
         defaults = {
             'extend': "base_site.html", #required, but this default will avoid an error           
             'template': "", #required
@@ -17,42 +17,13 @@ class BaseTemplateContext(dict):
             'sql':"",
             'database': settings.DATABASES['default'],
             }
-        self.update(**defaults)
+        self._context.update(**defaults)
         #section_name, for example 'clinic', 'lab', 'admin', 'surveys' comes as a parameter from the Url
         if kwargs.get('section_name') is None:
             raise TypeError('BaseSearch requires keyword/value for key \'section_name\'. None given.')
         else:
-            self['section_name'] =  kwargs.get('section_name')
+            self._context['section_name'] =  kwargs.get('section_name')
     
+    @property
     def context(self): 
-        return self.data
-        
-    def __getitem__(self, key):
-        return self.data[key]
-    
-    def __setitem__(self, key, item):
-        self.data[key] = item
-    
-    def update(self, **kwargs ): 
-        keys = kwargs.keys()
-        for kw in keys:
-            self.data.update({ kw:kwargs[kw] })
-        
-    def clear(self): 
-        self.data.clear()         
-
-    def copy(self):                            
-        if self.__class__ is dict:         
-            return dict(self.data)         
-        import copy                            
-        return copy.copy(self)                 
-
-    def keys(self): 
-        return self.data.keys()    
-
-    def items(self): 
-        return self.data.items()  
-
-    def values(self): 
-        return self.data.values()
-    
+        return self._context
