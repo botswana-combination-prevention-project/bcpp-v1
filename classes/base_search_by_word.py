a8299b4abcf05ca9b22de74e238804d0befbdb20
erik van widenfelt <ew2789@gmail.com>
2012-07-10 10:20:47 +0000

    almost a total re-write
    git-svn-id: http://192.168.1.50/svn/bhp_search@29 76f19ef0-b40f-46b0-95e1-91d90dd81aff

--- classes/base_search_by_word.py
+++ classes/base_search_by_word.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.db.models import Q
 from bhp_registration.models import RegisteredSubject
 from bhp_consent.classes import BaseConsent
-from bhp_crypto.classes import BaseEncryptedField, Hasher
+from bhp_crypto.classes import BaseEncryptedField
 #from bhp_crypto.classes import CryptoQ as Q
 from bhp_search.forms import SearchForm
 from base_search import BaseSearch
@@ -69,44 +69,55 @@ class BaseSearchByWord(BaseSearch):
             self.update_context(search_result=self.search_model.get(self.search_model_name).objects.using(self.context.get('dbname')).filter( **{ field_contains : this_value } ).order_by('-created'))                
         else:
             model = self.search_model.get(self.search_model_name)
-            # for fields in the model that are encrypted, convert search_term to a hash
-            terms = {}
+            search_term = {}
             if 'registered_subject' in dir(model()):
-                registered_subject = RegisteredSubject
-                for field in registered_subject._meta.fields:
-                    if isinstance(field, BaseEncryptedField):
-                        terms.update({field.attname: field.crypter.get_stored_hash(self.context.get('search_term'))})
+                # model has a key to registered subject
+                search_term=self.hash_for_encrypted_fields(self.context.get('search_term'), RegisteredSubject()) 
                 self.update_context(order_by='registered_subject__subject_identifier')
+                # prepare the qset, note that 
                 qset = (
                     Q(registered_subject__subject_identifier__icontains=self.context.get('search_term')) |
-                    Q(registered_subject__first_name__exact=terms.get('first_name')) |
-                    Q(registered_subject__initials__icontains=self.context.get('search_term')) |
-                    Q(registered_subject__sid__icontains=self.context.get('search_term'))|      
-                    Q(registered_subject__last_name__exact=terms.get('last_name')) |   
-                    Q(registered_subject__identity__exact=terms.get('identity')) |                                              
+                    Q(registered_subject__first_name__exact=search_term.get('first_name')) |
+                    Q(registered_subject__initials__icontains=search_term.get('initials')) |
+                    Q(registered_subject__sid__icontains=search_term.get('sid'))|      
+                    Q(registered_subject__last_name__exact=search_term.get('last_name')) |   
+                    Q(registered_subject__identity__exact=search_term.get('identity')) |                                              
                     Q(user_created__icontains=self.context.get('search_term')) |
                     Q(user_modified__icontains=self.context.get('search_term'))
                     )
             elif isinstance(model(), BaseConsent):
-                for field in model._meta.fields:
-                    if isinstance(field, BaseEncryptedField):
-                        terms.update({field.attname: field.crypter.get_stored_hash(self.context.get('search_term'))})
+                # model is a subclass of BaseConsent
+                search_term=self.hash_for_encrypted_fields(self.context.get('search_term'), model) 
                 self.update_context(order_by='subject_identifier')
                 qset = (
                     Q(subject_identifier__icontains=self.context.get('search_term')) |
-                    Q(first_name__exact=terms.get('first_name')) |
-                    Q(last_name__exact=terms.get('last_name')) |
-                    Q(identity__exact=terms.get('identity')) |
-                    Q(initials__contains=self.context.get('search_term')) |
+                    Q(first_name__exact=search_term.get('first_name')) |
+                    Q(last_name__exact=search_term.get('last_name')) |
+                    Q(identity__exact=search_term.get('identity')) |
+                    Q(initials__contains=search_term.get('initials')) |
                     Q(user_created__icontains=self.context.get('search_term')) |
                     Q(user_modified__icontains=self.context.get('search_term'))
                     )
             else:
                 raise ImproperlyConfigured('Search models must have a foreign key to model RegisteredSubject or be a subclass of BaseConsent. Got model {0}.'.format(model_name) )
-        search_result=model.objects.using(self.context.get('dbname')).filter(qset).distinct().order_by(self.context.get('order_by'))
+        search_result=model.objects.filter(qset).order_by(self.context.get('order_by'))
         self.update_context(count=0)
         if search_result:
             self.paginate(request.GET.get('page', '1'))    
             self.update_context(search_result=search_result)
             self.update_context(count=search_result.count())
+            
+    def hash_for_encrypted_fields(self, search_term, model_instance):
+        """ Using the model's field objects and the search term, create a dictionary of {field_name, search term}
+            where search term is hashed if this is an encrypted field """
+        terms={}
+        for field in model_instance._meta.fields:
+            if isinstance(field, BaseEncryptedField):
+                # change the search term to a hash using the hasher on the field
+                terms[field.attname]=field.crypter.get_stored_hash(search_term)
+            else:
+                # use the original search term
+                terms[field.attname]=search_term
+        return terms
+        
 
