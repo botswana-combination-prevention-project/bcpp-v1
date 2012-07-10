147502c9529d437da6aec002f71dd33fd41e9f64
erik van widenfelt <ew2789@gmail.com>
2012-07-10 10:49:10 +0000

    improvements while working on search
    git-svn-id: http://192.168.1.50/svn/bhp_crypto@34 dc6f6662-d1d5-42af-b45d-bfa79074b674

---
+++ fields/restricted_rsa_encryption_field.py
@@ -0,0 +1,15 @@
+from restricted_keypair_encryption_field import RestrictedKeyPairEncryptionField
+
+
+class RestrictedRsaEncryptionField(RestrictedKeyPairEncryptionField):
+    
+    """  'local RSA key-pair': same as 'restricted key-pair' but the private key is expected to be available and it's absence is not enforced """
+    
+    def __init__(self, *args, **kwargs):
+        
+        self.algorithm='RSA'
+        self.mode='restricted RSA key-pair'
+        defaults={'encryption_method': self.mode,
+                  'help_text': kwargs.get('help_text', '')+' (Encryption: %s)' % (self.mode,) }
+        kwargs.update(defaults)
+        super(RestrictedRsaEncryptionField, self).__init__(*args, **kwargs)
---
+++ settings.py
@@ -0,0 +1,38 @@
+from django.conf import settings as django_settings
+
+
+class Settings( object ):
+    
+    def __init__(self):
+        self._register = {}        
+        self._register.update(django_settings.BHP_CRYPTO_SETTINGS)
+            
+    @property
+    def SALT(self):
+        return self._register.get('SALT', '')
+    
+    @property
+    def IS_SECURE_DEVICE(self):
+        return self._register.get('IS_SECURE_DEVICE', False)
+
+    @property
+    def PUBLIC_KEY_RESTRICTED(self):
+        return self._register.get('PUBLIC_KEY_RESTRICTED', '')
+
+    @property
+    def PRIVATE_KEY_RESTRICTED(self):
+        return self._register.get('PRIVATE_KEY_RESTRICTED', '')
+
+    @property
+    def PUBLIC_KEY_LOCAL(self):
+        return self._register.get('PUBLIC_KEY_LOCAL', '')
+
+    @property
+    def PRIVATE_KEY_LOCAL(self):
+        return self._register.get('PRIVATE_KEY_LOCAL', '')
+
+    @property
+    def AES_KEY(self):
+        return self._register.get('AES_KEY', '')
+
+settings = Settings()
