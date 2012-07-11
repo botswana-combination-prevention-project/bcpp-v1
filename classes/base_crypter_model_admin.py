4ad901943e835061cdf620efa8f329db16501d8c
erik van widenfelt <ew2789@gmail.com>
2012-07-11 11:41:52 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@37 dc6f6662-d1d5-42af-b45d-bfa79074b674

---
+++ classes/base_crypter_model_admin.py
@@ -0,0 +1,22 @@
+from bhp_base_model.classes import BaseModelAdmin
+from bhp_crypto.actions import encrypt, decrypt
+from bhp_crypto.classes import BaseEncryptedField
+
+
+class BaseCrypterModelAdmin (BaseModelAdmin):
+    
+    """Overide ModelAdmin to force username to be saved on add/change and other stuff""" 
+    def __init__(self, *args, **kwargs):
+        #add cypter actions
+        self.actions.append(encrypt)    
+        self.actions.append(decrypt)                               
+        super(BaseCrypterModelAdmin, self).__init__(*args, **kwargs)
+
+    def get_readonly_fields(self, request, obj = None):
+        # make crypter fields readonly if no private key and in edit mode
+        if obj: #In edit mode
+            for field in obj._meta.fields:
+                if isinstance(field, BaseEncryptedField):
+                    if not field.crypter.private_key and field.attname not in self.readonly_fields:
+                        self.readonly_fields = (field.attname,) + self.readonly_fields 
+        return self.readonly_fields
