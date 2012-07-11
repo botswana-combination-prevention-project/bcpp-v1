86760687e92aef6a234a899fea0f592aa32cd644
erik van widenfelt <ew2789@gmail.com>
2012-07-11 20:28:09 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_search@34 76f19ef0-b40f-46b0-95e1-91d90dd81aff

--- views/search.py
+++ views/search.py
@@ -7,16 +7,20 @@ from mpepu.classes import SearchByWord, SearchByWeek, SearchByDate
 def search(request, **kwargs):   
     
     search_by=kwargs.get('search_by')
+    #assign the correct class to the search instance
     if search_by=='date':
         search = SearchByDate(request, **kwargs)
     elif search_by=='week':
         search = SearchByWeek(request, **kwargs)
     else:
         search = SearchByWord(request, **kwargs)
+    # prepare the instance
     search.prepare(request) 
     search.prepare_form(request, **kwargs)
     if search.ready:
+        #form is ready or "is_valid() = True", so get the search result
         search.search(request, **kwargs)
+        #paginate
         search.paginate(request.GET.get('page', '1'))       
     return render_to_response(
               search.context.get('template'),
