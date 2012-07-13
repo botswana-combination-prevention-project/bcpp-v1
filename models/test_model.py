3bb733d0032906352932afbcdca7ba85a7534000
erik van widenfelt <ew2789@gmail.com>
2012-07-13 06:41:04 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@55 dc6f6662-d1d5-42af-b45d-bfa79074b674

---
+++ models/test_model.py
@@ -0,0 +1,30 @@
+#from django.db import models
+from audit_trail.audit import AuditTrail
+from bhp_base_model.classes import BaseModel
+from bhp_crypto.fields import EncryptedCharField, EncryptedTextField, EncryptedFirstnameField, EncryptedLastnameField
+
+
+class TestModel(BaseModel):     
+            
+    firstname = EncryptedFirstnameField()
+    
+    lastname = EncryptedLastnameField()
+    
+    char1 = EncryptedCharField()
+    
+    char2 = EncryptedCharField()
+    
+    text1 = EncryptedTextField()
+    
+    text2 = EncryptedTextField()
+
+    text3 = EncryptedTextField()
+
+    history=AuditTrail()
+
+    def get_subject_identifier(self):
+        return ''
+
+    class Meta:
+        app_label='bhp_crypto'
+
