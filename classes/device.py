671f416f0e5892ae9f755bc05b84f2cff33786b0
erik van widenfelt <ew2789@gmail.com>
2012-07-17 21:19:05 +0000

    add doc
    git-svn-id: http://192.168.1.50/svn/bhp_device@4 4eff0c70-62bf-466a-ad6b-d4437891b3de

--- classes/device.py
+++ classes/device.py
@@ -7,11 +7,22 @@ from bhp_variables.models import StudySpecific
 class Device(object):
     
     """ Determines the device name, useful to know when identifiers are created by the device.
-    First tries settings.py (with DEVICE_ID settings attribute), then hostname (with HOSTNAME_PREFIX settings attribute), then from the DB.
+    
+    :usage:
+        >>> device = Device()
+        >>> device.device_id
+        >>> 31
+    
+    * First tries settings.py (with DEVICE_ID settings attribute), 
+    * then hostname (with HOSTNAME_PREFIX settings attribute), 
+    * then from the DB.
+    
     Settings attribute DEVICE_ID_LENGTH, if exists, determines the number of digits of the device_id. """
     
     def __init__(self, **kwargs):
+        
         """ Set length and pattern of device_id and call update(). """
+        
         # if settings attribute DEVICE_ID_LENGTH exists, use that value
         self.device_id = None
         self.length = None
---
+++ doc/_build/html/_sources/overview.txt
@@ -0,0 +1,5 @@
+Overview
+========
+
+Provide a consistent method to determine the device id used by applications such as :mod:`bhp_identifier`.
+
