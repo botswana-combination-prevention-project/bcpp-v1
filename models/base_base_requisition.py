be5e494a832f0a6f7dc16f2950eae3555f32bfe8
erik van widenfelt <ew2789@gmail.com>
2012-07-20 11:46:34 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@56 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- models/base_base_requisition.py
+++ models/base_base_requisition.py
@@ -18,162 +18,161 @@ from lab_requisition.classes import RequisitionLabel
 
 
 class BaseBaseRequisition (BaseUuidModel):
-    
+
     """ does not include additional tests """
-    
+
     requisition_identifier = models.CharField(
-        verbose_name = 'Requisition Id',
-        max_length = 25,
+        verbose_name='Requisition Id',
+        max_length=25,
         )
 
-    requisition_datetime =  models.DateTimeField(
+    requisition_datetime = models.DateTimeField(
         verbose_name='Requisition Date'
-        )    
-    
+        )
+
     specimen_identifier = models.CharField(
-        verbose_name = 'Specimen Id',    
-        max_length = 25,
-        null = True,
-        blank = True,
-        editable = False,
-        unique = True,
+        verbose_name='Specimen Id',
+        max_length=25,
+        null=True,
+        blank=True,
+        editable=False,
+        unique=True,
         )
 
     protocol = models.CharField(
-        verbose_name = "Protocol Number",
+        verbose_name="Protocol Number",
         max_length=10,
-        null = True,
-        blank = True,
-        help_text = 'Use three digit code e.g 041, 056, 062, etc'
+        null=True,
+        blank=True,
+        help_text='Use three digit code e.g 041, 056, 062, etc'
         )
 
-    site = models.ForeignKey(StudySite)    
-    
+    site = models.ForeignKey(StudySite)
+
     clinician_initials = InitialsField(
-        default = '--',                               
-        null = True,
-        blank = True,
+        default='--',
+        null=True,
+        blank=True,
         )
-    
+
     aliquot_type = models.ForeignKey(AliquotType,
-        help_text = 'Note: Lists only those types associtaed with the Panel.')
+        help_text='Note: Lists only those types associtaed with the Panel.')
 
     panel = models.ForeignKey(Panel)
-    
-    priority = models.CharField(
-        verbose_name = 'Priority',
-        max_length = 25,
-        choices = PRIORITY,
-        default = 'normal',
-        )
-    
-    is_drawn = models.CharField(
-        verbose_name = 'Was a specimen drawn?',
-        max_length = 3,
-        choices = YES_NO,
-        default = 'Yes',
-        help_text = 'If No, provide a reason below'
-        )
-        
-    reason_not_drawn = models.CharField(
-        verbose_name = 'If not drawn, please explain',
-        max_length = 25,
-        choices = REASON_NOT_DRAWN,
-        null = True,
-        blank = True,
-        )            
 
-    drawn_datetime =  models.DateTimeField(
-        verbose_name = 'Date / Time Specimen Drawn',
-        null = True,
-        blank = True,
-        help_text = 'If not drawn, leave blank. Same as date and time of finger prick in case on DBS.',
+    priority = models.CharField(
+        verbose_name='Priority',
+        max_length=25,
+        choices=PRIORITY,
+        default='normal',
+        )
+
+    is_drawn = models.CharField(
+        verbose_name='Was a specimen drawn?',
+        max_length=3,
+        choices=YES_NO,
+        default='Yes',
+        help_text='If No, provide a reason below'
+        )
+
+    reason_not_drawn = models.CharField(
+        verbose_name='If not drawn, please explain',
+        max_length=25,
+        choices=REASON_NOT_DRAWN,
+        null=True,
+        blank=True,
+        )
+
+    drawn_datetime = models.DateTimeField(
+        verbose_name='Date / Time Specimen Drawn',
+        null=True,
+        blank=True,
+        help_text='If not drawn, leave blank. Same as date and time of finger prick in case on DBS.',
         )
 
     item_type = models.CharField(
-        verbose_name = 'Item collection type',
-        max_length = 25,
-        choices = ITEM_TYPE,
-        default = 'tube',
-        help_text = ''
-        ) 
+        verbose_name='Item collection type',
+        max_length=25,
+        choices=ITEM_TYPE,
+        default='tube',
+        help_text=''
+        )
 
     item_count_total = models.IntegerField(
-        verbose_name = 'Total number of items',
-        default = 1,
-        help_text = 'Number of tubes, samples, cards, etc being sent for this test/order only. Determines number of labels to print',
-        ) 
-    
-    estimated_volume = models.DecimalField(
-        verbose_name = 'Estimated volume in mL',
-        max_digits = 7,
-        decimal_places = 1,
-        default = 5.0,
-        help_text = 'If applicable, estimated volume of sample for this test/order. This is the total volume if number of "tubes" above is greater than 1'
+        verbose_name='Total number of items',
+        default=1,
+        help_text='Number of tubes, samples, cards, etc being sent for this test/order only. Determines number of labels to print',
         )
-        
+
+    estimated_volume = models.DecimalField(
+        verbose_name='Estimated volume in mL',
+        max_digits=7,
+        decimal_places=1,
+        default=5.0,
+        help_text='If applicable, estimated volume of sample for this test/order. This is the total volume if number of "tubes" above is greater than 1'
+        )
+
     comments = models.TextField(
         max_length=25,
-        null = True,
-        blank = True,
+        null=True,
+        blank=True,
         )
-    
+
     is_receive = models.BooleanField(
-        verbose_name = 'received',        
-        default = False,
+        verbose_name='received',
+        default=False,
         )
-    
+
     is_receive_datetime = models.DateTimeField(
-        verbose_name = 'rcv-date',    
-        null = True,
-        blank = True,
+        verbose_name='rcv-date',
+        null=True,
+        blank=True,
         )
 
     is_packed = models.BooleanField(
-        verbose_name = 'packed',    
-        default = False,
+        verbose_name='packed',
+        default=False,
         )
 
     is_labelled = models.BooleanField(
-        verbose_name = 'labelled',
-        default = False,
+        verbose_name='labelled',
+        default=False,
         )
 
     is_labelled_datetime = models.DateTimeField(
-        verbose_name = 'label-date',
-        null = True,
-        blank = True,
+        verbose_name='label-date',
+        null=True,
+        blank=True,
         )
-    
+
     is_lis = models.BooleanField(
-        verbose_name = 'lis',    
-        default = False,
+        verbose_name='lis',
+        default=False,
         )
-        
-    
+
     objects = BaseRequisitionManager()
-    
+
     def __unicode__(self):
         return '%s' % (self.requisition_identifier)
-    
+
     def get_infant_identifier(self):
         return self.get_visit().appointment.registered_subject.subject_identifier
 
     def subject(self):
         return self.get_subject_identifier()
-    
+
     def visit(self):
         return self.get_visit().appointment.visit_definition.code
-    
+
     def get_subject_identifier(self):
         return self.get_visit().appointment.registered_subject.subject_identifier
-    
+
     def save(self, *args, **kwargs):
         if not kwargs.get('suppress_autocreate_on_deserialize', False):
-            if not self.requisition_identifier and self.is_drawn.lower() == 'yes' :
+            if not self.requisition_identifier and self.is_drawn.lower() == 'yes':
                 self.requisition_identifier = self.prepare_requisition_identifier()
             if self.requisition_identifier and not self.specimen_identifier:
-                self.specimen_identifier=self.prepare_specimen_identifier()
+                self.specimen_identifier = self.prepare_specimen_identifier()
 
         return super(BaseBaseRequisition, self).save(*args, **kwargs)
 
@@ -182,58 +181,58 @@ class BaseBaseRequisition (BaseUuidModel):
         study_specific = StudySpecific.objects.get_query_set()[0]
         #check_digit = CheckDigit()
         self.protocol = study_specific.protocol_code
-        opts={}
-        opts.update({'protocol': study_specific.protocol_prefix()})
-        opts.update({'requisition_identifier':self.requisition_identifier, 'site':self.site.site_code})
-        #opts.update({'check_digit':check_digit.calculate('{protocol}{site}{requisition_identifier}'.format(opts), modulus=7)})
-        return '{protocol}-{site}{requisition_identifier}'.format(**opts)
-    
+        opts = {}
+        opts.update({'protocol': study_specific.protocol_prefix})
+        opts.update({'requisition_identifier': self.requisition_identifier, 'site': self.site})
+        opts.update({'check_digit': check_digit.calculate('{protocol}{site}{requisition_identifier}'.format(opts), modulus=7)})
+        return '{protocol}-{site}{requisition_identifier}-{check-digit}'.format(opts)
+
     def prepare_requisition_identifier(self, **kwargs):
-        """Generate and return a locally unique requisition identifier for a device (adds device id)"""        
+        """Generate and return a locally unique requisition identifier for a device (adds device id)"""
         device = Device()
         string = BaseString()
-        length=5
+        length = 5
         template = '{device_id}{random_string}'
-        opts = {'device_id':device.device_id, 'random_string':string.get_safe_random_string(length=length)}
+        opts = {'device_id': device.device_id, 'random_string': string.get_safe_random_string(length=length)}
         requisition_identifier = template.format(**opts)
         # look for a duplicate
         if self.__class__.objects.filter(requisition_identifier=requisition_identifier):
-            n=1
+            n = 1
             while self.__class__.objects.filter(requisition_identifier=requisition_identifier):
                 requisition_identifier = template.format(**opts)
-                n+=1
-                if n==length**len(string.safe_allowed_chars):
-                    raise TypeError('Unable prepare a unique requisition identifier, all are taken. Increase the length of the random string')        
+                n += 1
+                if n == length ** len(string.safe_allowed_chars):
+                    raise TypeError('Unable prepare a unique requisition identifier, '
+                                    'all are taken. Increase the length of the random string')
         return requisition_identifier
-    
+
     def get_label(self, **kwargs):
         """ Return a "ready to print" label object. """
         label = RequisitionLabel()
-        return label       
-    
+        return label
+
     def print_label(self, **kwargs):
         """ print a label using the label class or subclass returned by get_label()"""
         remote_addr = kwargs.get('remote_addr')
         cups_server_ip = kwargs.get('cups_server_ip')
         template_name = kwargs.get('template_name')
-        item_count = kwargs.get('item_count',1), 
-        if self.specimen_identifier:    
+        item_count = kwargs.get('item_count', 1),
+        if self.specimen_identifier:
             for item_count in range(self.item_count_total, 0, -1):
                 try:
                     label = self.get_label()
                     label.prepare_label_options(
-                               template_name = template_name,
-                               requisition = self,        
-                               client_ip = remote_addr,
-                               cups_server_ip = cups_server_ip,
-                               item_count = item_count)
+                               template_name=template_name,
+                               requisition=self,
+                               client_ip=remote_addr,
+                               cups_server_ip=cups_server_ip,
+                               item_count=item_count)
                     label.print_label()
                     self.is_labelled = True
                     self.modified = datetime.today()
-                    self.save()                                             
+                    self.save()
                 except ValueError, err:
                     raise ValueError('Unable to print, is the lab_barcode app configured? %s' % (err,))
-    
+
     class Meta:
-        abstract = True 
-       
+        abstract = True
