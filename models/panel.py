5d8275b03c1910b1ad86b4811d54b5dd68068aa7
erik van widenfelt <ew2789@gmail.com>
2012-07-22 06:24:51 +0000

    git-svn-id: http://192.168.1.50/svn/lab_panel@14 6905788c-99ef-4415-a217-c19abe96c9c9

--- models/panel.py
+++ models/panel.py
@@ -7,48 +7,46 @@ from panel_group import PanelGroup
 
 
 class Panel(BaseModel):
-    
+
     name = models.CharField(
-        verbose_name = "Panel Name", 
-        max_length=50,  
+        verbose_name="Panel Name",
+        max_length=50,
         unique=True,
-        db_index=True,                
+        db_index=True,
         )
 
     panel_group = models.ForeignKey(PanelGroup)
-        
+
     test_code = models.ManyToManyField(TestCode,
         verbose_name='Test Codes',
-        help_text = 'Choose all that apply',
-        )        
+        help_text='Choose all that apply',
+        )
 
     aliquot_type = models.ManyToManyField(AliquotType,
-        help_text = 'Choose all that apply',
-        )        
+        help_text='Choose all that apply',
+        )
 
-    account = models.ManyToManyField(Account, 
+    account = models.ManyToManyField(Account,
         null=True,
         blank=True
         )
-        
+
     comment = models.CharField(
-        verbose_name = "Comment", 
-        max_length=250, 
+        verbose_name="Comment",
+        max_length=250,
         blank=True,
         )
+
     dmis_panel_identifier = models.CharField(
         max_length=25,
         null=True,
         blank=True,
-        )    
+        )
 
     def __unicode__(self):
         return self.name
-        
+
     class Meta:
-        app_label = 'lab_panel'        
+        app_label = 'lab_panel'
         db_table = 'bhp_lab_core_panel'
-        ordering = ['name',]
-
-
-
+        ordering = ['name', ]
