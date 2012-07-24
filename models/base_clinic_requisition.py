33f98cef646df86d7d065fe3fb914bbcc9a045c1
erik van widenfelt <ew2789@gmail.com>
2012-07-24 13:56:38 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@58 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- models/base_clinic_requisition.py
+++ models/base_clinic_requisition.py
@@ -3,22 +3,19 @@ from lab_requisition.models import BaseRequisition
 
 
 class BaseClinicRequisition (BaseRequisition):
-    
+
     def get_visit(self):
-        
         for field in self._meta.fields:
             try:
                 if issubclass(field.rel.to, BaseVisitTracking):
                     return field.rel.to.objects.get(pk=getattr(self, field.attname))
             except:
                 pass
-        raise TypeError('{0} is unable to determine the visit model'.format(self))    
+        raise TypeError('{0} is unable to determine the visit model'.format(self))
         return None
 
     def get_subject_identifier(self):
-        
         return self.get_visit().appointment.registered_subject.subject_identifier
-
 
     class Meta:
         abstract = True
--- models/base_requisition.py
+++ models/base_requisition.py
@@ -2,16 +2,15 @@ from django.db import models
 from lab_test_code.models import TestCode
 from base_base_requisition import BaseBaseRequisition
 
+
 class BaseRequisition (BaseBaseRequisition):
-    
+
     # populate this one based on the selected panel at the dashboard
     test_code = models.ManyToManyField(TestCode,
-        verbose_name = 'Additional tests',
-        null = True,
-        blank = True,
+        verbose_name='Additional tests',
+        null=True,
+        blank=True,
         )
-        
-    class Meta:
-        abstract = True 
-       
 
+    class Meta:
+        abstract = True
