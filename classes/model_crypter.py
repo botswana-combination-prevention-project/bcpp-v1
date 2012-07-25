4882b4734286435507ebc1319a79885a3f2e99ce
erik van widenfelt <ew2789@gmail.com>
2012-07-25 06:15:07 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@87 dc6f6662-d1d5-42af-b45d-bfa79074b674

--- classes/model_crypter.py
+++ classes/model_crypter.py
@@ -3,24 +3,36 @@ from base_encrypted_field import BaseEncryptedField
 
 class ModelCrypter(object):
 
-    def encrypt_instance(self, instance, save=True):
+    def encrypt_instance(self, instance, encrypted_fields=None, save=True):
         """ Returns a modified instance (not saved), encrypt all un-encrypted
         field objects in a given model instance. """
-        for field in instance._meta.fields:
-            if isinstance(field, BaseEncryptedField):
-                field_value = getattr(instance, field.attname)
-                if field_value:
-                    if not field.is_encrypted(field_value):
-                        setattr(instance, field.attname, field.encrypt(field_value))
-                        if save:
-                            instance.save()
+        if not encrypted_fields:
+            encrypted_fields = self.get_encrypted_fields(instance)
+        for field in encrypted_fields:
+            field_value = getattr(instance, field.attname)
+            if field_value:
+                if not field.is_encrypted(field_value):
+                    setattr(instance, field.attname, field.encrypt(field_value))
+                    if save:
+                        instance.save()
         return instance
+
+    def get_encrypted_fields(self, model):
+        encrypted_fields = []
+        for field in model._meta.fields:
+            if isinstance(field, BaseEncryptedField):
+                encrypted_fields.append(field)
+        return encrypted_fields
 
     def encrypt_model(self, model):
         """ Encrypt field objects that are an instance of BaseEncryptedField
         in a given model. """
+        encrypted_fields = []
+        for field in model._meta.fields:
+            if isinstance(field, BaseEncryptedField):
+                encrypted_fields.append(field)
         for instance in model.objects.all():
-            instance = self.encrypt_instance(instance)
+            instance = self.encrypt_instance(instance, encrypted_fields)
 
     def decrypt_instance(self, instance,
                          encrypted_field_object=BaseEncryptedField):
--- management/commands/encrypt_models.py
+++ management/commands/encrypt_models.py
@@ -19,29 +19,34 @@ class Command(BaseCommand):
             except:
                 app = None
             if app:
+                has_encryption_key = False
                 # self.stdout.write(app_name + '\n')
                 for model in get_models(app):
                     # self.stdout.write(model._meta.object_name + '\n')
-                    for field in model._meta.fields:
-                        if isinstance(field, BaseEncryptedField):
-                            if not field.crypter.has_encryption_key:
-                                raise CommandError('Suspect encryption keys are not loaded. Quitting.')
-                            self.stdout.write('Encrypting {app_name}.{model}...\n'.format(app_name=app_name, model=model._meta.object_name.lower()))
-                            model_crypter = ModelCrypter()
-                            count = model.objects.all().count()
-                            instance_count = 0
-                            try:
-                                for instance in model.objects.all().order_by('id'):
-                                    instance_count += 1
-                                    model_crypter.encrypt_instance(instance)
-                                    self.stdout.write('\r\x1b[K {0} / {1} instances '
-                                                      ' ...'.format(instance_count, count))
-                                self.stdout.write('done.\n')
-                                n += 1
-                                break
-                            except:
-                                print "Unexpected error:", sys.exc_info()[0]
-                                raise CommandError('Failed on {app_name}.{model}.{pk}.'.format(app_name=app_name,
+                    if not has_encryption_key:
+                        for field in model._meta.fields:
+                            if isinstance(field, BaseEncryptedField):
+                                if not field.crypter.has_encryption_key:
+                                    raise CommandError('Suspect encryption keys are not loaded. Quitting.')
+                                else:
+                                    has_encryption_key = True
+                    self.stdout.write('Encrypting {app_name}.{model}...\n'.format(app_name=app_name, model=model._meta.object_name.lower()))
+                    model_crypter = ModelCrypter()
+                    count = model.objects.all().count()
+                    instance_count = 0
+                    try:
+                        encrypted_fields = model_crypter.get_encrypted_fields(model)
+                        for instance in model.objects.all().order_by('id'):
+                            instance_count += 1
+                            model_crypter.encrypt_instance(instance, encrypted_fields)
+                            self.stdout.write('\r\x1b[K {0} / {1} instances '
+                                              ' ...'.format(instance_count, count))
+                        self.stdout.write('done.\n')
+                        n += 1
+                        break
+                    except:
+                        print "Unexpected error:", sys.exc_info()[0]
+                        raise CommandError('Failed on {app_name}.{model}.{pk}.'.format(app_name=app_name,
                                                                                             model=model._meta.object_name.lower(),
                                                                                             pk=instance.pk))
                 msg = 'Complete. {0} models encrypted.\n'.format(n)
