c3887b1cd6e358fbfaae3ca9bb5efee5ef02a970
erik van widenfelt <ew2789@gmail.com>
2012-07-26 12:27:20 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_string@3 25168b96-9aa0-4dd9-8557-f9803c00bb68

--- classes/base_string.py
+++ classes/base_string.py
@@ -1,9 +1,10 @@
 
+
 class BaseString(object):
-    
-    def __init__(self, *args, **kwargs):
-        self.safe_allowed_chars='ABCDEFGHKMNPRSTUVWXYZ23456789'
-    
+
+    def __init__(self):
+        self.safe_allowed_chars = 'ABCDEFGHKMNPRSTUVWXYZ23456789'
+
     def get_random_string(self, length=12, allowed_chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*()?<>.,[]{}'):
         """ no dollar signs """
         import random
@@ -12,10 +13,7 @@ class BaseString(object):
         except NotImplementedError:
             pass
         return ''.join([random.choice(allowed_chars) for i in range(length)])
-    
+
     def get_safe_random_string(self, length):
         """ safe for people, no lowercase and no 0OL1J etc."""
-        return self.get_random_string(length, allowed_chars=self.safe_allowed_chars)    
-    
-
-        
+        return self.get_random_string(length, allowed_chars=self.safe_allowed_chars)
