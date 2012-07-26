c17dc8edee7682fa4746342afd8fba1e15c8c5ca
erik van widenfelt <ew2789@gmail.com>
2012-07-26 12:26:25 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_entry_rules@19 22def012-2d7a-4ff2-8462-e3842ca56b02

---
+++ models/signals.py
@@ -0,0 +1,16 @@
+#from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#try:
+#    from bhp_sync.classes import BaseSyncModel
+#except ImportError:
+#    pass
+#from bhp_base_model.classes import BaseUuidModel
+#from bhp_bucket.classes import bucket
+#
+#from rule_history import RuleHistory
+
+#@receiver(post_save, weak=False, dispatch_uid='model_bucket_rules_on_save' )
+#def model_bucket_rules_on_save(sender, instance, **kwargs):
+#    """ update / run model bucket rules, see bhp_bucket """
+#    if isinstance(instance, (BaseUuidModel, BaseSyncModel)):
+#        bucket.update(instance)
