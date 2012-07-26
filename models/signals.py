ddbaf77b4a9d0b540895fb406e82103f46276170
erik van widenfelt <ew2789@gmail.com>
2012-07-26 12:25:40 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@151 3bc1d969-5506-4dd9-9641-5fb303cabccf

---
+++ models/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from bhp_sync.classes import DeserializeFromTransaction
+from incoming_transaction import IncomingTransaction
+
+
+@receiver(post_save, sender=IncomingTransaction, dispatch_uid="deserialize_on_post_save")
+def deserialize_on_post_save(sender, instance, **kwargs):
+
+    """ Callback to deserialize an incoming transaction.
+
+    as long as the transaction is not consumed or in error"""
+
+    if isinstance(instance, IncomingTransaction):
+        if not instance.is_consumed and not instance.is_error:  # and not instance.is_self:
+            deserialize_from_transaction = DeserializeFromTransaction()
+            deserialize_from_transaction.deserialize(sender, instance, **kwargs)
