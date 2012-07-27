55477207b1a291b7f685daf6617214bf8a21158b
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-07-27 13:15:35 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_identifier@19 f71f0ad2-c473-4a78-a10b-80415aeb8028

--- admin.py
+++ admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from bhp_common.models import MyModelAdmin
-from models import IdentifierTracker
-
+from bhp_identifier.models import IdentifierTracker
 
 
 class IdentifierTrackerAdmin(MyModelAdmin):
 
-    list_display = (  'identifier', 'root_number','counter', 'created', 'user_created')
-    
+    list_display = ('identifier', 'root_number', 'counter', 'created', 'user_created')
     search_fields = ('identifier', 'root_number')
-    
-    list_filter = ('created','root_number','user_created' )
-    
+    list_filter = ('created', 'root_number', 'user_created')
+
 admin.site.register(IdentifierTracker, IdentifierTrackerAdmin)
