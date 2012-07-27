99e5256866911814b948467037ceb221de4f19c8
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-07-27 14:32:26 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@112 dc6f6662-d1d5-42af-b45d-bfa79074b674

--- management/commands/encrypt_transactions.py
+++ management/commands/encrypt_transactions.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from bhp_crypto.classes import FieldCrypter
 from django.db.models import get_model
 
-
+#usage: python manage.py encrypt_transactions outgoing/incoming
 class Command(BaseCommand):
     args = '<model_name>'
     help = 'Encrypt Incoming and outgoing transactions.'
@@ -29,22 +29,17 @@ class Command(BaseCommand):
                 self.encrypt_tx(field_crypter, model)
 
     def encrypt_tx(self, field_crypter, model):
-        count = model.objects.all().count()
+        count = model.objects.exclude(tx__startswith='enc1:::').count()
         instance_count = 0
-        already_enc_count = 0
-        self.stdout.write('{0} {1} transactions found.\n'.format(model.__name__, count))
-        for transaction in model.objects.all():
+        self.stdout.write('{0} Unencrypted {1} transactions found.\n'.format(model.__name__,count))
+        for transaction in model.objects.exclude(tx__startswith='enc1:::'):
             instance_count += 1
-            tx = str(transaction.tx)
-            if not "enc1:::" in tx:
-                transaction.tx = field_crypter.encrypt(transaction.tx)
-                transaction.save()
+            #tx = str(transaction.tx)
+            transaction.tx = field_crypter.encrypt(transaction.tx)
+            transaction.save()
 
-            else:
-                already_enc_count += 1
-
-            self.stdout.write('\r\x1b[K {0} / {1} transactions: already encrypted {2} / {1}'
-                                  ' ...'.format(instance_count, count, already_enc_count))
+            self.stdout.write('\r\x1b[K {0} / {1} transactions'
+                                  ' ...'.format(instance_count, count))
             self.stdout.flush()
 
         self.stdout.write('done.\n')
