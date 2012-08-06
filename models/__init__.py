d538d8209bee0cd16abd2d18460156e97a894463
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:04:18 +0000

    git-svn-id: http://192.168.1.50/svn/lab_aliquot@17 c28a7b5e-763c-426f-a5f7-2c8dd379f3b6

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 #from specimen_type import SpecimenType
+from base_aliquot import BaseAliquot
 from aliquot import Aliquot
--- models/aliquot.py
+++ models/aliquot.py
@@ -1,92 +1,14 @@
-import datetime
 from django.db import models
-from bhp_base_model.classes import BaseUuidModel
 from lab_receive.models import Receive
-from lab_aliquot_list.models import AliquotCondition, AliquotType
 from lab_aliquot.managers import AliquotManager
-from lab_aliquot.choices import ALIQUOT_STATUS, SPECIMEN_MEASURE_UNITS, SPECIMEN_MEDIUM
+from base_aliquot import BaseAliquot
 
 
-class Aliquot (BaseUuidModel):
-
-    aliquot_identifier = models.CharField(
-        verbose_name='Aliquot Identifier',
-        max_length=25,
-        unique=True,
-        help_text="Aliquot identifier",
-        editable=False,
-        )
-
-    aliquot_datetime = models.DateTimeField(
-        verbose_name="Date and time aliquot created",
-        default=datetime.datetime.today(),
-        )
+class Aliquot (BaseAliquot):
 
     receive = models.ForeignKey(Receive)
 
-    count = models.IntegerField(
-        editable=False,
-        null=True
-        )
-
-    parent_identifier = models.ForeignKey('self',
-        to_field='aliquot_identifier',
-        blank=True,
-        null=True,
-        )
-
-    aliquot_type = models.ForeignKey(AliquotType,
-        verbose_name="Aliquot Type",
-        )
-
-    medium = models.CharField(
-        verbose_name='Medium',
-        max_length=25,
-        choices=SPECIMEN_MEDIUM,
-        default='TUBE',
-        #help_text = "Indicate such as dbs card, tube, swab, etc",
-        )
-
-    original_measure = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default='5.00',
-        )
-
-    current_measure = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default='5.00',
-        )
-
-    measure_units = models.CharField(
-        max_length=25,
-        choices=SPECIMEN_MEASURE_UNITS,
-        default='mL',
-        )
-
-    condition = models.ForeignKey(AliquotCondition,
-        verbose_name="Aliquot Condition",
-        default=10,
-        null=True,
-        )
-
-    status = models.CharField(
-        max_length=25,
-        choices=ALIQUOT_STATUS,
-        default='available',
-        )
-
-    comment = models.CharField(
-        max_length=50,
-        null=True,
-        blank=True,
-        )
-
     objects = AliquotManager()
-
-    def __unicode__(self):
-        return '%s' % (self.aliquot_identifier)
 
     def get_absolute_url(self):
         return "/lab_aliquot/aliquot/%s/" % self.id
@@ -97,4 +19,3 @@ class Aliquot (BaseUuidModel):
     class Meta:
         app_label = 'lab_aliquot'
         db_table = 'bhp_lab_core_aliquot'
-
