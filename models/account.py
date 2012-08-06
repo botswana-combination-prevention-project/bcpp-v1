fa91379bc3969bd57c4c0aa08bb3df94edfa0a61
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:05:34 +0000

    git-svn-id: http://192.168.1.50/svn/lab_account@11 175f5fc6-46c7-45fc-b3a3-1d6e88701e56

--- models/account.py
+++ models/account.py
@@ -3,38 +3,39 @@ from bhp_base_model.classes import BaseUuidModel
 from lab_account.models import AccountHolder
 
 class Account (BaseUuidModel):
-    
+
     account_name = models.CharField(
-        max_length = 25,
-        unique = True,
+        max_length=25,
+        unique=True,
         )
-        
+
     account_opendate = models.DateField(
-        verbose_name = 'Open date',
-        )    
+        verbose_name='Open date',
+        )
 
     account_closedate = models.DateField(
-        verbose_name = 'Closed date',
-        )    
+        verbose_name='Closed date',
+        )
 
     account_holder = models.ForeignKey(AccountHolder,
-    	null = True,
-    	blank = True,
-    	)
+        null=True,
+        blank=True,
+        )
 
-    comment = models.CharField("Comment", 
-        max_length = 250, 
-        blank = True,
-        )        
-               
+    comment = models.CharField("Comment",
+        max_length=250,
+        blank=True,
+        )
+
+    objects = models.Manager()
+
     def get_absolute_url(self):
-        return "/lab_account/account/%s/" % self.id   
+        return "/lab_account/account/%s/" % self.id
 
     def __unicode__(self):
         return self.account_name
-        
+
     class Meta:
         ordering = ["account_name"]
         app_label = 'lab_account'
-        db_table = 'bhp_lab_registration_account'                        
-
+        db_table = 'bhp_lab_registration_account'
