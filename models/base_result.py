0d9ae675a385e024fe35634a91a7444e47873491
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:04:35 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result@18 afa70ccf-cfc7-4289-b1b8-adaaa15ec0be

--- models/base_result.py
+++ models/base_result.py
@@ -8,52 +8,52 @@ class BaseResult(BaseUuidModel):
     result_identifier = models.CharField(
         max_length=25,
         editable=False,
-        db_index=True,        
+        db_index=True,
         )
 
     result_datetime = models.DateTimeField(
-        help_text = 'Date result added to system.',
-        db_index=True,        
+        help_text='Date result added to system.',
+        db_index=True,
         )
 
     release_status = models.CharField(
         max_length=25,
         choices=RESULT_RELEASE_STATUS,
         default='NEW',
-        db_index=True,        
+        db_index=True,
         )
 
     release_datetime = models.DateTimeField(
-  	    null = True,
-        blank = True,	    
-        help_text = 'Date result authorized for release. This field will auto-fill if release status is changed',
-        db_index=True,        
+        null=True,
+        blank=True,
+        help_text='Date result authorized for release. This field will auto-fill if release status is changed',
+        db_index=True,
         )
-    
+
     release_username = models.CharField(
-        verbose_name = "Release username",
+        verbose_name="Release username",
         max_length=50,
-	    null=True,
-	    blank=True,
-        help_text = 'Username of person authorizing result for release. This field will auto-fill if release status is changed',        	    
-        db_index=True,        
-	    )
+        null=True,
+        blank=True,
+        help_text='Username of person authorizing result for release. This field will auto-fill if release status is changed',
+        db_index=True,
+        )
 
     comment = models.CharField(
-        verbose_name = 'Comment',
-	    max_length = 50,
-	    null = True,
-        blank = True,	    
-	    help_text = ''
-  	    )
-    
+        verbose_name='Comment',
+        max_length=50,
+        null=True,
+          blank=True,
+        help_text=''
+            )
+
     dmis_result_guid = models.CharField(
         max_length=36,
-	    null = True,
-        blank = True,	
-        #editable=False,            
-	    help_text = 'dmis import value. N/A unless data imported from old system'
-        )  	    
-    
+        null=True,
+        blank=True,
+        #editable=False,
+        help_text='dmis import value. N/A unless data imported from old system'
+        )
+
     class Meta:
         abstract = True
--- models/result.py
+++ models/result.py
@@ -2,23 +2,26 @@ from django.db import models
 from lab_order.models import Order
 from base_result import BaseResult
 
+
 class Result(BaseResult):
 
     order = models.ForeignKey(Order)
-    
+
+    objects = models.Manager()
+
     def __unicode__(self):
         return '%s' % (self.result_identifier)
 
     def get_absolute_url(self):
-        return "/lab_result/result/%s/" % self.id   
-        
+        return "/lab_result/result/%s/" % self.id
+
     def get_search_url(self):
-        return "/laboratory/result/search/result/%s/" % self.result_identifier   
+        return "/laboratory/result/search/result/%s/" % self.result_identifier
 
     def get_document_url(self):
-        return "/laboratory/result/document/%s/" % (self.result_identifier)  	
+        return "/laboratory/result/document/%s/" % (self.result_identifier)
 
     class Meta:
-        app_label = 'lab_result' 
+        app_label = 'lab_result'
         db_table = 'bhp_lab_core_result'
-        ordering =['result_identifier','order','result_datetime',]      
+        ordering = ['result_identifier', 'order', 'result_datetime']
