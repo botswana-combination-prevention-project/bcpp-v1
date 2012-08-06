bc5a00fdacfa687be208df23b63026e7acd109bc
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:04:47 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_item@32 94dd4f35-d255-437e-9225-d26d65b3fcea

--- models/result_item.py
+++ models/result_item.py
@@ -14,24 +14,21 @@ class ResultItem(BaseResultItem):
     result = models.ForeignKey(Result)
 
     result_item_source = models.ForeignKey(ResultSource,
-        verbose_name = 'Source',
-	    help_text = 'Reference to source of information, such as interface, manual, outside lab, ...',
-	    db_index = True,
-  	    )
+        verbose_name='Source',
+        help_text='Reference to source of information, such as interface, manual, outside lab, ...',
+        db_index=True)
 
     result_item_source_reference = models.CharField(
-        verbose_name = 'Source Reference',
-	    max_length = 50,
-	    null = True,
-        blank = True,	    
-	    help_text = 'Reference to source, invoice, filename, machine, etc'
-  	    )
+        verbose_name='Source Reference',
+        max_length=50,
+        null=True,
+        blank=True,
+        help_text='Reference to source, invoice, filename, machine, etc')
 
-
+    objects = models.Manager()
     #history = AuditTrail()
-    
+
     def save(self, *args, **kwargs):
-        
         reference = ReferenceFlag()
         reference.dob = self.result.order.aliquot.receive.patient.dob
         reference.gender = self.result.order.aliquot.receive.patient.gender
@@ -39,8 +36,7 @@ class ResultItem(BaseResultItem):
         reference.test_code = self.test_code
         if reference.flag:
             self.reference_range = '%s - %s' % (reference.flag['range']['lln'], reference.flag['range']['uln'])
-            self.reference_flag = reference.flag['flag']        
-
+            self.reference_flag = reference.flag['flag']
         grade = GradeFlag()
         grade.dob = self.result.order.aliquot.receive.patient.dob
         grade.gender = self.result.order.aliquot.receive.patient.gender
@@ -48,27 +44,23 @@ class ResultItem(BaseResultItem):
         grade.test_code = self.test_code
         if grade.flag:
             self.grade_range = '%s - %s' % (grade.flag['range']['lln'], grade.flag['range']['uln'])
-            self.grade_flag = grade.flag['flag']        
+            self.grade_flag = grade.flag['flag']
 
         return super(ResultItem, self).save(*args, **kwargs)
-    
+
     def __unicode__(self):
         return '%s %s' % (unicode(self.result), unicode(self.test_code))
-    
+
     def get_absolute_url(self):
         return "lab_result_item/resultitem/%s/" % (self.id)
-    
+
     def get_result_document_url(self):
-        return "/laboratory/result/document/%s/" % (self.result.result_identifier)  	
+        return "/laboratory/result/document/%s/" % (self.result.result_identifier)
 
     #TODO: get this to return a subject_identifier for the audit trial
     def get_subject_identifier(self,):
         return ''
-    
-    class Meta:
-        app_label = 'lab_result_item'    
-        db_table = 'bhp_lab_core_resultitem'            
-            
-       
-       
 
+    class Meta:
+        app_label = 'lab_result_item'
+        db_table = 'bhp_lab_core_resultitem'
