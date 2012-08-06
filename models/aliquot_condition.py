da08c50be4ca8698bf33beb80a5bf66eba812793
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:05:03 +0000

    git-svn-id: http://192.168.1.50/svn/lab_aliquot_list@7 89cbf902-e6a2-4b1b-b6b2-68351f3bfdfa

--- models/aliquot_condition.py
+++ models/aliquot_condition.py
@@ -1,11 +1,15 @@
+from django.db import models
 from bhp_base_model.classes import BaseListModel
 
 
 class AliquotCondition(BaseListModel):
-    
+
+    objects = models.Manager()
+
     def __unicode__(self):
-        return "%s: %s" % ( self.short_name.upper() ,self.name)
+        return "%s: %s" % (self.short_name.upper(), self.name)
+
     class Meta:
         ordering = ["short_name"]
-        app_label = 'lab_aliquot_list' 
-        db_table = 'bhp_lab_core_aliquotcondition'        
+        app_label = 'lab_aliquot_list'
+        db_table = 'bhp_lab_core_aliquotcondition'
--- models/aliquot_type.py
+++ models/aliquot_type.py
@@ -6,33 +6,35 @@ from bhp_base_model.classes import BaseModel
 class AliquotType(BaseModel):
 
     name = models.CharField(
-        verbose_name = 'Description',
-        max_length=50,        
+        verbose_name='Description',
+        max_length=50,
         )
-    
+
     alpha_code = models.CharField(
-        verbose_name = 'Aplha code',
-        validators = [
+        verbose_name='Aplha code',
+        validators=[
             RegexValidator('^[A-Z]{2,15}$')
             ],
         max_length=15,
-        unique=True,        
+        unique=True,
         )
     numeric_code = models.CharField(
-        verbose_name = 'Numeric code (2-digit)',
-        max_length = 2,
-        validators = [
+        verbose_name='Numeric code (2-digit)',
+        max_length=2,
+        validators=[
             RegexValidator('^[0-9]{2}$')
             ],
-        unique=True,    
+        unique=True,
         )
-        
-    dmis_reference = models.IntegerField()        
-    
+
+    dmis_reference = models.IntegerField()
+
+    objects = models.Manager()
+
     def __unicode__(self):
-        return "%s %s: %s" % ( self.alpha_code, self.numeric_code, self.name.lower())
+        return "%s %s: %s" % (self.alpha_code, self.numeric_code, self.name.lower())
 
     class Meta:
         ordering = ["name"]
-        app_label = 'lab_aliquot_list'   
-        db_table = 'bhp_lab_core_aliquottype'        
+        app_label = 'lab_aliquot_list'
+        db_table = 'bhp_lab_core_aliquottype'
