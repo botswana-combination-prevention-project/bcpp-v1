e12cc89f8f701b35a1cdc5a903872c0367af95b0
erik van widenfelt <ew2789@gmail.com>
2012-08-06 09:04:30 +0000

    git-svn-id: http://192.168.1.50/svn/lab_order@9 f6c41d23-262b-4918-a957-6735cdbbd5c9

--- models/order.py
+++ models/order.py
@@ -1,57 +1,21 @@
 from django.db import models
-from bhp_base_model.classes import BaseUuidModel
-from bhp_base_model.validators import datetime_not_future
-from lab_aliquot.models import Aliquot
-from lab_panel.models import Panel
 from lab_order.managers import OrderManager
+from lab_aliquot.models import Aliquot
+from base_order import BaseOrder
 
 
-class Order(BaseUuidModel):
+class Order(BaseOrder):
 
-    order_identifier = models.CharField(
-        verbose_name = 'Order number',
-        max_length = 25,
-        help_text = 'Allocated internally',
-        db_index=True, 
-        editable = False,               
-        )
+    aliquot = models.ForeignKey(Aliquot)
 
-    order_datetime =  models.DateTimeField(
-        verbose_name = 'Order Date',
-        validators = [
-            datetime_not_future,
-        ],
-        db_index=True,                
-        )
-    
-    aliquot = models.ForeignKey(Aliquot)    
-    
-    panel  = models.ForeignKey(Panel)
-
-    comment = models.CharField(
-        verbose_name = 'Comment',
-        max_length = 150,
-        null = True,
-        blank = True,
-        )
-    
-    dmis_reference = models.IntegerField(
-        null=True,
-        blank = True,        
-        )
-    
     objects = OrderManager()
-    
-    def __unicode__(self):
-        return '%s %s' % (self.order_identifier, self.panel)
-        
+
     def get_absolute_url(self):
-        return "/lab_order/order/%s/" % self.id   
+        return "/lab_order/order/%s/" % self.id
+
     def get_search_url(self):
-        return "/laboratory/order/search/order/byword/%s/" % self.id   
+        return "/laboratory/order/search/order/byword/%s/" % self.id
 
     class Meta:
-        app_label = 'lab_order'    
+        app_label = 'lab_order'
         db_table = 'bhp_lab_core_order'
-        
-
