ceb56fb450e376155e176fcc105181a9cfacf3a5
erik van widenfelt <ew2789@gmail.com>
2012-08-07 13:47:41 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_identifier@23 f71f0ad2-c473-4a78-a10b-80415aeb8028

--- classes/base_identifier_model.py
+++ classes/base_identifier_model.py
@@ -6,7 +6,17 @@ class BaseIdentifierModel(BaseModel):
     """Store identifiers as allocated and use the pk as a unique sequence for the new identifier.
 
     Will not include identifiers derived from other identifiers, for example, infant and partner
-    identifiers are not included in this model."""
+    identifiers are not included in this model.
+
+    To populate for an EDC already in use, for example:
+
+        >>>for rs in RegisteredSubject.objects.filter(subject_type='maternal').order_by('created'):
+        >>>    SubjectIdentifier.objects.create(identifier=rs.subject_identifier, seed='1000')
+
+    If there are records in SubjectIdentifier, delete them and reset the autoincrement like this:
+
+        >>> ALTER TABLE `bhp_identifier_subjectidentifier` AUTO_INCREMENT = 1;
+    """
 
     identifier = models.CharField(max_length=25, unique=True)
     seed = models.IntegerField()
