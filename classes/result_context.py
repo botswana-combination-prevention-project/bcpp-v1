0848c8773506d040e4916ff2d9091708b22a06d9
erik van widenfelt <ew2789@gmail.com>
2012-08-09 12:30:51 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_report@85 20e02364-0de8-4df6-ba75-255d08af7f65

--- classes/result_context.py
+++ classes/result_context.py
@@ -22,38 +22,38 @@ class ContextDescriptor(object):
             'is_dob_estimated': instance.result.order.aliquot.receive.patient.is_dob_estimated,
             'gender': instance.result.order.aliquot.receive.patient.gender,
             'initials': instance.result.order.aliquot.receive.patient.initials,
-            'site_identifier':instance.result.order.aliquot.receive.site.site_identifier,
-            'clinicians_initials': instance.result.order.aliquot.receive.patient.initials,        
+            'site_identifier': instance.result.order.aliquot.receive.site.site_identifier,
+            'clinicians_initials': instance.result.order.aliquot.receive.patient.initials,
             'drawn_datetime': instance.result.order.aliquot.receive.drawn_datetime,
             'panel_name': instance.result.order.panel.name,
-            'receive_identifier': instance.result.order.aliquot.receive.receive_identifier,                    
-            'receive_datetime': instance.result.order.aliquot.receive.receive_datetime,        
+            'receive_identifier': instance.result.order.aliquot.receive.receive_identifier,
+            'receive_datetime': instance.result.order.aliquot.receive.receive_datetime,
             'aliquot_identifier': instance.result.order.aliquot.aliquot_identifier,
-            'order_identifier': instance.result.order.order_identifier,        
+            'order_identifier': instance.result.order.order_identifier,
             'order_datetime': instance.result.order.order_datetime,
-            'condition': instance.result.order.aliquot.condition, 
+            'condition': instance.result.order.aliquot.aliquot_condition,
             #'receive': result.order.aliquot.receive,
             #'order': result.order,
             #'aliquot': result.order.aliquot,
             'result_items': instance.result_items,
-            'action':"view",        
+            'action':"view",
             'section_name': instance.section_name,
-            'search_name': instance.search_name,        
+            'search_name': instance.search_name,
             'result_include_file': "detail.html",
             'receiving_include_file':"receiving.html",
             'orders_include_file': "orders.html",
             'result_items_include_file': "result_items.html",
-            'top_result_include_file': "result_include.html",        
+            'top_result_include_file': "result_include.html",
             }
 
 
 class ResultContext(object):
 
     context = ContextDescriptor()
-    
+
     def __init__(self, **kwargs):
         self.section_name = kwargs.get('section_name')
-        self.search_name = kwargs.get('search_name')        
+        self.search_name = kwargs.get('search_name')
         self.result_identifier = kwargs.get('result_identifier')
         if self.result_identifier:
             self.result = Result.objects.get(result_identifier__exact=self.result_identifier)
--- views/view_result.py
+++ views/view_result.py
@@ -1,37 +1,25 @@
-from settings import DATABASES
-from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
-from bhp_common.utils import os_variables
 from lab_result.models import Result
 from lab_result_item.models import ResultItem
-from lab_result_report.forms import ResultSearchForm
-from laboratory.classes import get_my_limit_queryset
 from lab_result_report.classes import ResultContext
+
 
 @login_required
 def view_result(request, **kwargs):
 
-    section_name = 'result'
-    search_name = 'result'
-
     result_identifier = kwargs.get('result_identifier')
-    limit = 20
     template = 'result_report.html'
-                  
     if result_identifier is not None:
         result = Result.objects.get(result_identifier__exact=result_identifier)
         result_items = ResultItem.objects.filter(result=result)
-        
         result_context = ResultContext(result_identifier=result_identifier)
-        
-        return render_to_response(template, 
-            result_context.context, 
+        return render_to_response(template,
+            result_context.context,
             context_instance=RequestContext(request)
-            )  
-            
-        
+            )
+
+
 def render_search(**kwargs):
     return Result.objects.filter(result_identifier=kwargs['search_term'])
