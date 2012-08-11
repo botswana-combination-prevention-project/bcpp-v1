4f61339b6d5dfdbccd0c267f360b6245aaddcb3a
erik van widenfelt <ew2789@gmail.com>
2012-08-11 16:49:01 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import@3 ac7ad86f-e3ae-41fd-969b-be69b85246f6

--- classes/base_import_history.py
+++ classes/base_import_history.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 from django.db.models import Max
-
+from base_lock import BaseLock
 
 logger = logging.getLogger(__name__)
 
@@ -23,10 +23,11 @@ class BaseImportHistory(object):
     """
 
     def __init__(self, db, lock_name, obj_lock, import_history_model):
-        self.dmis_import_history = None
         self.last_import_datetime = None
         self.conditional_clause = None
         self._lock = None
+        if not issubclass(obj_lock, BaseLock):
+            raise TypeError('Lock must be a subclass of BaseLock.')
         self.obj_lock = obj_lock
         self.lock_name = lock_name
         self.import_history_model = import_history_model
@@ -46,17 +47,19 @@ class BaseImportHistory(object):
         return self._lock is not None
 
     def finish(self):
+        last_import_datetime = None
         if self._lock:
             # only update the history if the lock still exists in the db,
             # as someone may have deleted it to stop the process
             # before was able to completed
             if self.locked:
-                self.dmis_import_history.end_datetime = datetime.today()
-                self.dmis_import_history.save()
+                self.import_history_model.end_datetime = datetime.today()
+                self.import_history_model.save()
             else:
-                self.dmis_import_history.delete()
+                self.import_history_model.delete()
             self._lock.release()
-            self._clean_up()
+            last_import_datetime = self._clean_up()
+        return last_import_datetime
 
     def _prepare(self):
         """ Tries to get or create an instance of self.import_history_model locally and lock the
@@ -67,9 +70,9 @@ class BaseImportHistory(object):
             * protocol -- used as the lock name
         """
         def get_last_import_datetime(lock_name):
-            """ Returns the end datetime of the last successful dmis import for this lock"""
-            if self.import_history_model.objects.using(self.db).filter(lock_name=lock_name, end_datetime__isnull=False):
-                agg = self.import_history_model.objects.using(self.db).filter(lock_name=lock_name).aggregate(Max('end_datetime'))
+            """ Returns the end datetime of the last successful import for this lock"""
+            if self.import_history_model.__class__.objects.using(self.db).filter(lock_name=lock_name, end_datetime__isnull=False):
+                agg = self.import_history_model.__class__.objects.using(self.db).filter(lock_name=lock_name).aggregate(Max('end_datetime'))
                 retval = agg['end_datetime__max']
             else:
                 retval = None
@@ -88,14 +91,14 @@ class BaseImportHistory(object):
                 raise TypeError('Need a lock name. Got None.')
             #if self.import_history_model.objects.filter(lock_name=self.lock_name, end_datetime__isnull=False):
             #    self.last_import_datetime = get_last_import_datetime(self.lock_name)
-            #    self.dmis_import_history = self.import_history_model.objects.get(lock_name=self.lock_name, end_datetime__isnull=False)
+            #    self.import_history_model = self.import_history_model.objects.get(lock_name=self.lock_name, end_datetime__isnull=False)
             #elif self.import_history_model.objects.filter(lock_name=self.lock_name, end_datetime__isnull=True):
             #    # found an open history instance, check if it is locked
-            #    self.dmis_import_history = self.import_history_model.objects.filter(lock_name=self.lock_name, end_datetime__isnull=True).order_by('-start_datetime')[0]
+            #    self.import_history_model = self.import_history_model.objects.filter(lock_name=self.lock_name, end_datetime__isnull=True).order_by('-start_datetime')[0]
             #    self.last_import_datetime = get_last_import_datetime(self.lock_name)
             #else:
-            self.dmis_import_history = self.import_history_model.objects.using(self.db).create(lock_name=self.lock_name, start_datetime=self._lock.created)
-            self.dmis_import_history.save()
+            self.import_history_model = self.import_history_model.objects.using(self.db).create(lock_name=self.lock_name, start_datetime=self._lock.created)
+            self.import_history_model.save()
             self.last_import_datetime = get_last_import_datetime(self.lock_name)
             if self.last_import_datetime:
                 prepare_clause(self.last_import_datetime)
@@ -105,11 +108,16 @@ class BaseImportHistory(object):
         return retval
 
     def _clean_up(self):
-        """ Sets everything to None."""
-        self.dmis_import_history = None
-        self.last_import_datetime = None,
+        """ Sets everything to None and returns the last_import_datetime."""
+        last_import_datetime = self.last_import_datetime
+        self.import_history_model = None
+        self.last_import_datetime = None
         self.conditional_clause = None
         self._lock = None
+        return last_import_datetime
+
+    def force_release(self, lock_name):
+        self.obj_lock(self.db).release(lock_name)
 
     def history(self):
         return self.import_history_model.objects.using(self.db).filter(lock_name=self.lock_name).order_by('-start_datetime')
