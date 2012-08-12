625d73c818effd27138b464159b6cd1b0f803f3b
erik van widenfelt <ew2789@gmail.com>
2012-08-12 23:08:44 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_item@37 94dd4f35-d255-437e-9225-d26d65b3fcea

--- classes/result_item_flag.py
+++ classes/result_item_flag.py
@@ -3,27 +3,29 @@
 class ResultItemFlag(object):
     @classmethod
     def calculate(self, result_item):
-        """Takes a result_item instance and evaluates the reference and grade flags."""
+        """Takes a result_item instance and evaluates the reference and grade flags.
+
+        Gets flag classes from model methods get_cls_reference_flag() and get_grading_list().
+        """
         before = [result_item.reference_range, result_item.reference_flag, result_item.grade_range, result_item.grade_flag]
         value = result_item.result_item_value_as_float
         ReferenceFlag = result_item.get_cls_reference_flag()
         flag = ReferenceFlag(result_item.get_reference_list(), result_item)
         kw = flag.evaluate(value)
+        range_ = '{0}-{1}'.format(kw.get('lower_limit'), kw.get('upper_limit'))
+        if range_.strip() == '-' or kw.get('lower_limit') is None or kw.get('upper_limit') is None:
+            result_item.reference_range = None
+        else:
+            result_item.reference_range = range_
         if kw.get('flag', None):
-            range_ = '{0} - {1}'.format(kw.get('lower_limit'), kw.get('upper_limit'))
-            if range_.strip() == '-':
-                result_item.reference_range = None
-            else:
-                result_item.reference_range = range_
             result_item.reference_flag = kw.get('flag')
         else:
-            result_item.reference_range = None
             result_item.reference_flag = None
         GradeFlag = result_item.get_cls_grade_flag()
         flag = GradeFlag(result_item.get_grading_list(), result_item)
         kw = flag.evaluate(value)
         if kw.get('flag', None):
-            range_ = '{0} - {1}'.format(kw.get('lower_limit'), kw.get('upper_limit'))
+            range_ = '{0}-{1}'.format(kw.get('lower_limit'), kw.get('upper_limit'))
             if range_.strip() == '-':
                 result_item.grade_range = None
             else:
