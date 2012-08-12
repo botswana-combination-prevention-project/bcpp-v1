d91780c1e434f3891a5f65fe97567f79844f42f8
erik van widenfelt <ew2789@gmail.com>
2012-08-12 23:09:23 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@88 5124d8a1-613d-4708-9420-8c98912e35b2

---
+++ management/commands/import_test_codes.py
@@ -0,0 +1,43 @@
+import logging
+from django.core.management.base import BaseCommand
+from lab_test_code.models import TestCode as ListTestCode
+from lab_clinic_api.models import TestCode
+
+logger = logging.getLogger(__name__)
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+nullhandler = logger.addHandler(NullHandler())
+
+
+class Command(BaseCommand):
+
+    args = ()
+    help = 'Recalculate grading and reference range flags for result items.'
+
+    def handle(self, *args, **options):
+        self.db = 'lab_api'
+        count = ListTestCode.objects.using(self.db).all().count()
+        for lis_test_code in ListTestCode.objects.using(self.db).all().order_by('code'):
+            test_code, created = TestCode.objects.get_or_create(code=lis_test_code.code)
+            for field in test_code._meta.fields:
+                if field.name in [fld.name for fld in lis_test_code._meta.fields if fld.name not in ['id', 'code']]:
+                    setattr(test_code, field.name, getattr(lis_test_code, field.name))
+            test_code.save()
+            action = 'Adding'
+            if not created:
+                action = 'Updating'
+            print '{action} {test_code}'.format(action=action, test_code=test_code)
+        lis = [test_code.code for test_code in ListTestCode.objects.using(self.db).all().order_by('code')]
+        local = [test_code.code for test_code in TestCode.objects.all().order_by('code')]
+        diff_set = set(local).difference(set(lis))
+        if diff_set:
+            print 'Warning: found {0} in local but not lis.'.format(', '.join(diff_set))
+        diff_set = set(lis).difference(set(local))
+        if diff_set:
+            print 'Warning: found {0} in lis but not local.'.format(', '.join(diff_set))
+        print 'Done importing {new_count} / {count} test codes on Lis connection {db}.'.format(count=count,
+                                                                                               new_count=TestCode.objects.all().count(),
+                                                                                               db=self.db)
