ea7082153e9e76140f3a4c8b9b256c8b119093cb
erik van widenfelt <ew2789@gmail.com>
2012-08-13 10:13:41 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_reference@7 d0f1680e-b5b9-4295-8319-3d1593bddc0c

--- admin.py
+++ admin.py
@@ -10,7 +10,9 @@ admin.site.register(GradingList, GradingListAdmin)
 
 
 class GradingListItemAdmin(BaseModelAdmin):
-    pass
+    list_display = ('test_code', 'gender', 'hiv_status', 'grade', 'lln', 'uln', 'age_low', 'age_low_unit', 'age_low_quantifier',
+                    'age_high', 'age_high_unit', 'age_high_quantifier', 'grading_list')
+    search_fields = ['grade', 'test_code__code', 'test_code__name', 'lln', 'uln', 'hiv_status']
 admin.site.register(GradingListItem, GradingListItemAdmin)
 
 
