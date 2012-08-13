bdede0014972409cd402fbda40fdf61df7d6217d
erik van widenfelt <ew2789@gmail.com>
2012-08-13 09:14:54 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@119 833481ee-4f4c-48f6-8756-2185fbac41e5

--- classes/consented_subject_identifier.py
+++ classes/consented_subject_identifier.py
@@ -1,13 +1,16 @@
 from django.db.models import get_model
-from bhp_registration.models import RegisteredSubject
+#from bhp_registration.models import RegisteredSubject
 from bhp_identifier.classes import SubjectIdentifier
 
 
 class ConsentedSubjectIdentifier(SubjectIdentifier):
 
-    def get_identifier_post(self, consent, attrname, **kwargs):
+    def get_identifier_post(self, new_identifier, **kwargs):
         """ Updates registered subject after a new subject identifier is created."""
         # the model, for creating a new instance
         RegisteredSubject = get_model('bhp_registration', 'registeredsubject')
         # access registered subject manager method
+        consent = kwargs.get('consent')
+        attrname = kwargs.get('consent_attrname')
+        setattr(consent, attrname, new_identifier)
         RegisteredSubject.objects.update_with(consent, attrname, **kwargs)
