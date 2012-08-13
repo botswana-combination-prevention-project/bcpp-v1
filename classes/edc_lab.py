89bab24b5a2544fbe144275769a8e7b8498dcea1
erik van widenfelt <ew2789@gmail.com>
2012-08-13 00:32:24 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@90 5124d8a1-613d-4708-9420-8c98912e35b2

--- classes/edc_lab.py
+++ classes/edc_lab.py
@@ -1,6 +1,6 @@
 from django.template.loader import render_to_string
-
-from lab_clinic_api.models import Result, Order
+from lab_result_item.classes import ResultItemFlag
+from lab_clinic_api.models import Result, Order, ResultItem
 from lab_clinic_api.classes import Lis
 
 
@@ -17,6 +17,12 @@ class EdcLab(object):
         if update:
             last_updated = self.update(subject_identifier)
         resulted = Result.objects.filter(order__aliquot__receive__registered_subject__subject_identifier=subject_identifier).order_by('-order__aliquot__receive__drawn_datetime')
+        for result in resulted:
+            for result_item in ResultItem.objects.filter(result=result):
+                if result_item.result_item_value_as_float:
+                    result_item, modified = ResultItemFlag().calculate(self)
+                    if modified:
+                        result_item.save()
         ordered = (Order.objects.filter(aliquot__receive__registered_subject__subject_identifier=subject_identifier)
                                 .exclude(order_identifier__in=[result.order.order_identifier for result in resulted])
                                 .order_by('-aliquot__receive__drawn_datetime'))
