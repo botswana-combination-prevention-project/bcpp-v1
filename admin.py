7c23cb66bbde476c4b522f4d49ecec7cb8b7209f
erik van widenfelt <ew2789@gmail.com>
2012-08-13 10:13:49 +0000

    git-svn-id: http://192.168.1.50/svn/lab_grading@41 d0c47599-63b2-4c0f-8d21-292bf8f76f30

--- admin.py
+++ admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from bhp_common.models import MyModelAdmin, MyStackedInline, MyTabularInline
+from bhp_base_model.classes import BaseModelAdmin
 from models import GradingList, GradingListItem
 
-class GradingListAdmin(MyModelAdmin):
+
+class GradingListAdmin(BaseModelAdmin):
     pass
 admin.site.register(GradingList, GradingListAdmin)
 
 
-class GradingListItemAdmin(MyModelAdmin):
-    list_display = ('test_code', 'gender', 'hiv_status','grade', 'lln', 'uln', 'age_low', 'age_low_unit','age_low_quantifier','age_high','age_high_unit','age_high_quantifier',  'grading_list')
-    search_fields = ['grade', 'test_code__code','test_code__name','lln', 'uln', 'hiv_status',]
+class GradingListItemAdmin(BaseModelAdmin):
+    list_display = ('test_code', 'gender', 'hiv_status', 'grade', 'lln', 'uln', 'age_low', 'age_low_unit', 'age_low_quantifier',
+                    'age_high', 'age_high_unit', 'age_high_quantifier', 'grading_list')
+    search_fields = ['grade', 'test_code__code', 'test_code__name', 'lln', 'uln', 'hiv_status']
 admin.site.register(GradingListItem, GradingListItemAdmin)
-
