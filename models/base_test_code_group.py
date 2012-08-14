7c396ad17c17f8629238e54d3019aff381b25d36
erik van widenfelt <ew2789@gmail.com>
2012-08-14 15:14:39 +0000

    git-svn-id: http://192.168.1.50/svn/lab_test_code@33 a3136606-7879-40c6-927c-db30895c4219

---
+++ models/base_test_code_group.py
@@ -0,0 +1,21 @@
+from django.db import models
+from bhp_base_model.classes import BaseModel
+
+
+class BaseTestCodeGroup(BaseModel):
+
+    code = models.CharField(max_length=15, null=True)
+
+    name = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True,
+        )
+
+    objects = models.Manager()
+
+    def __unicode__(self):
+        return self.code
+
+    class Meta:
+        abstract = True
