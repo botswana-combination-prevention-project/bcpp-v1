1209e569e78d4d32bacfbd52cc39a58eae131156
erik van widenfelt <ew2789@gmail.com>
2012-08-15 20:12:24 +0000

    git-svn-id: http://192.168.1.50/svn/lab_common@9 3c07a67b-3389-4fa7-9c45-b2f84f859bbe

--- utils/allocate_result_identifier.py
+++ utils/allocate_result_identifier.py
@@ -2,16 +2,12 @@ from lab_order.models import Order
 from lab_result.models import Result
 
 
-def AllocateResultIdentifier(oOrder): 
-
-    cnt = Result.objects.filter(order=oOrder,).count()
-    
+def get_identifier(order): 
+    cnt = Result.objects.filter(order=order,).count()
     cnt += 1
-    
     if cnt < 10:
         pad = '0'
     else:
-        pad = ''    
-    
-    return '%s-%s%s' % (oOrder.order_identifier, pad, cnt)
+        pad = ''
+    return '%s-%s%s' % (order.order_identifier, pad, cnt)
 
