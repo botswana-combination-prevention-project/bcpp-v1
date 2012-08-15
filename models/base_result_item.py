5ebc5889c858385ba032fa5b784d82d977c1bf87
erik van widenfelt <ew2789@gmail.com>
2012-08-15 20:09:46 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_item@44 94dd4f35-d255-437e-9225-d26d65b3fcea

--- models/base_result_item.py
+++ models/base_result_item.py
@@ -13,106 +13,83 @@ class BaseResultItem(BaseUuidModel):
         verbose_name='Result',
         max_length=25,
         help_text='',
-        db_index=True,
-        )
-
+        db_index=True)
     result_item_value_as_float = models.FloatField(
         verbose_name='Result as float',
         #max_digits = 15,
         help_text='',
         null=True,
         db_index=True,
-        editable=False,
-        )
-
+        editable=False)
     result_item_quantifier = models.CharField(
         verbose_name='Quantifier',
         default='=',
         choices=RESULT_QUANTIFIER,
         max_length=25,
-        help_text='',
-        )
-
+        help_text='')
     result_item_datetime = models.DateTimeField(
         verbose_name='Assay date and time',
-        db_index=True,
-        )
+        db_index=True)
     result_item_operator = models.CharField(
         verbose_name='Operator',
         max_length=50,
         null=True,
         blank=True,
-        db_index=True,
-        )
-
+        db_index=True)
     grade_range = models.CharField(
         max_length=25,
         null=True,
-        blank=True,
-        )
-
+        blank=True)
     grade_flag = models.CharField(
         max_length=5,
         null=True,
-        blank=True,
-        )
-
+        blank=True)
     reference_flag = models.CharField(
         max_length=5,
         null=True,
-        blank=True,
-        )
-
+        blank=True)
     reference_range = models.CharField(
         max_length=25,
         null=True,
-        blank=True,
-        )
-
+        blank=True)
     validation_status = models.CharField(
         verbose_name='Status',
         default='P',
         choices=RESULT_VALIDATION_STATUS,
         max_length=10,
         help_text='Default is preliminary',
-        db_index=True,
-        )
-
+        db_index=True)
     validation_datetime = models.DateTimeField(
         null=True,
         blank=True,
-        db_index=True,
-        )
-
+        db_index=True)
     validation_username = models.CharField(
         verbose_name="Validation username",
         max_length=50,
         null=True,
         blank=True,
-        db_index=True,
-        )
+        db_index=True)
     validation_reference = models.CharField(
         verbose_name="Validation reference",
         max_length=50,
-      null=True,
-      blank=True,
-      )
-
+        null=True,
+        blank=True,)
     comment = models.CharField(
         verbose_name='Validation Comment',
-      max_length=50,
-      null=True,
+        max_length=50,
+        null=True,
         blank=True,
-      help_text=''
-        )
-
+        help_text='')
     error_code = models.CharField(
         verbose_name='Error codes',
-      max_length=50,
-      null=True,
+        max_length=50,
+        null=True,
         blank=True,
-      help_text=''
-        )
+        help_text='')
+    receive_identifier = models.CharField(
+        max_length=25, editable=False, null=True, db_index=True,
+        help_text="non-user helper field to simplify search and filter")
+    import_datetime = models.DateTimeField(null=True)
 
     def get_subject_identifier(self):
         return ''
@@ -137,7 +114,7 @@ class BaseResultItem(BaseUuidModel):
         return None
 
     def save(self, *args, **kwargs):
-
+        self.receive_identifier = self.result.order.aliquot.receive.receive_identifier
         if re.search(r'\d+\.?\d*', self.result_item_value):
             try:
                 self.result_item_value_as_float = float(self.result_item_value)
