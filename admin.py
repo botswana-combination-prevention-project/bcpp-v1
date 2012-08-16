ac97858336ca669dec78fc4283a98f59dbacec84
erik van widenfelt <ew2789@gmail.com>
2012-08-16 21:16:25 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@131 5124d8a1-613d-4708-9420-8c98912e35b2

--- admin.py
+++ admin.py
@@ -11,6 +11,7 @@ class ReceiveAdmin(BaseModelAdmin):
     list_display = ('registered_subject', 'to_order', "receive_identifier", "receive_datetime", "requisition_identifier", "drawn_datetime", 'created', 'modified', 'import_datetime')
     search_fields = ('registered_subject__subject_identifier', "receive_identifier", "requisition_identifier",)
     list_filter = ('created', "receive_datetime", "drawn_datetime", 'modified', 'import_datetime', )
+    list_per_page = 15
 
     def get_readonly_fields(self, request, obj):
         return [field.name for field in obj._meta.fields if field.editable and field.name not in ['requisition_identifier']]
@@ -22,6 +23,7 @@ class AliquotAdmin(BaseModelAdmin):
     list_display = ("aliquot_identifier", 'to_receive', 'subject_identifier', 'drawn', "aliquot_type", 'aliquot_condition', 'created', 'modified', 'import_datetime')
     search_fields = ('aliquot_identifier', 'receive', )
     list_filter = ('created', 'import_datetime', 'aliquot_type', 'aliquot_condition')
+    list_per_page = 15
 
     def get_readonly_fields(self, request, obj):
         return [field.name for field in obj._meta.fields if field.editable]
@@ -33,18 +35,11 @@ class OrderAdmin(BaseModelAdmin):
     list_display = ("order_identifier", "to_receive", "to_result", "subject_identifier", "panel", "order_datetime", 'created', 'modified', 'import_datetime')
     search_fields = ('aliquot__receive__registered_subject__subject_identifier', "order_identifier", "aliquot__receive__receive_identifier")
     list_filter = ('status', 'import_datetime', 'panel__edc_name')
+    list_per_page = 15
 
     def get_readonly_fields(self, request, obj):
         return ['aliquot', 'panel', 'status', 'order_datetime', 'comment']
 admin.site.register(Order, OrderAdmin)
-
-
-#class UpdateLogAdmin(BaseModelAdmin):
-#    list_display = ('subject_identifier', 'update_datetime')
-#    fields = ('subject_identifier', 'update_datetime')
-#    list_filter = ('update_datetime',)
-#    search_fields = ('subject_identifier',)
-#admin.site.register(UpdateLog, UpdateLogAdmin)
 
 
 class AliquotTypeAdmin(BaseModelAdmin):
@@ -82,16 +77,15 @@ admin.site.register(Review, ReviewAdmin)
 class ResultAdmin(BaseModelAdmin):
 
     def __init__(self, *args, **kwargs):
+        super(ResultAdmin, self).__init__(*args, **kwargs)
         self.actions.append(recalculate_grading)
         self.actions.append(flag_as_reviewed)
         self.actions.append(unflag_as_reviewed)
-        super(ResultAdmin, self).__init__(*args, **kwargs)
 
     form = ResultForm
-    search_fields = ("result_identifier", "order__aliquot__receive__registered_subject__subject_identifier",
-                     "order__aliquot__receive__receive_identifier",
+    search_fields = ("result_identifier", "subject_identifier",
+                     "receive_identifier",
                      "order__order_identifier")
-
     list_display = (
         "result_identifier",
         'report',
@@ -104,7 +98,6 @@ class ResultAdmin(BaseModelAdmin):
         "release_status",
         "release_datetime",
         'import_datetime')
-
     radio_fields = {"release_status": admin.VERTICAL}
     list_filter = ("release_status", 'reviewed', "result_datetime", 'import_datetime')
     list_per_page = 15
@@ -117,8 +110,11 @@ admin.site.register(Result, ResultAdmin)
 
 class ResultItemAdmin(BaseModelAdmin):
 
-    form = ResultItemForm
+    def __init__(self, *args, **kwargs):
+        super(ResultItemAdmin, self).__init__(*args, **kwargs)
+        self.actions.append(recalculate_grading)
 
+    form = ResultItemForm
     list_display = (
         "test_code",
         "result",
@@ -133,13 +129,11 @@ class ResultItemAdmin(BaseModelAdmin):
         "reference_range",
         "created",
         "modified",
-        'import_datetime'
-        )
-
+        'import_datetime')
     list_filter = ('grade_flag', 'reference_flag', "result_item_datetime", "created", "modified", 'import_datetime', "test_code")
     search_fields = ('test_code__code', 'result__result_identifier',
-                     "result__order__aliquot__receive__registered_subject__subject_identifier",
-                     "result__order__aliquot__receive__receive_identifier")
+                     "subject_identifier",
+                     "receive_identifier")
     radio_fields = {
         "result_item_quantifier": admin.VERTICAL,
         "validation_status": admin.VERTICAL}
@@ -147,9 +141,8 @@ class ResultItemAdmin(BaseModelAdmin):
         fields=[],
         exclude=['id', ],
         extra_fields=[
-            {'subject_identifier': 'result__lab__subject_identifier'},
-            {'gender': 'result__lab__gender'},
-            {'dob': 'result__lab__dob'},
+            {'gender': 'result__order__aliquot__receive__registered_subject__gender'},
+            {'dob': 'result__order__aliquot__receive__registered_subject__dob'},
             ]), ]
     list_per_page = 35
 
