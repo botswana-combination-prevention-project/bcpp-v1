6f55741284645d9b35e85f0c093002f20e25faad
erik van widenfelt <ew2789@gmail.com>
2012-08-17 05:14:30 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_userprofile@6 286037fc-dbac-43a1-8671-deda17dc685f

--- admin.py
+++ admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from bhp_common.models import MyModelAdmin, MyStackedInline, MyTabularInline
+from bhp_base_model.classes import BaseModelAdmin
 from bhp_userprofile.models import UserProfile
 
-class UserProfileAdmin(MyModelAdmin):
+
+class UserProfileAdmin(BaseModelAdmin):
     list_display = ('user', 'initials')
     search_fields = ['user__username', 'user__first_name', 'user__last_name']
 admin.site.register(UserProfile, UserProfileAdmin)
-
