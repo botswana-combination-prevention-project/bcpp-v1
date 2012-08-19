5c52c6d64244d98ff82dd90068e9951e36801b2b
erik van widenfelt <ew2789@gmail.com>
2012-08-19 18:41:05 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_lab_tracker@7 5f62ce6e-2ac5-4da8-806c-9e97803e0885

--- models/history_model.py
+++ models/history_model.py
@@ -5,21 +5,11 @@ from bhp_base_model.classes import BaseUuidModel
 class HistoryModel(BaseUuidModel):
 
     subject_identifier = models.CharField(max_length=25)
-
     test_key = models.CharField(max_length=25)
-
     test_code = models.CharField(max_length=25)
-
     value = models.CharField(max_length=25)
-
     value_datetime = models.DateTimeField()
-
-    current = models.CharField(max_length=50, null=True)
-
     objects = models.Manager()
-
-#    def save(self, *args, **kwargs):
-#        super(HistoryModel, self).save(*args, **kwargs)
 
     class Meta:
         app_label = 'lab_longitudinal'
