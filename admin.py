911cb4b98ded91c9f0e0c98f67ffcd465bcbc9b8
erik van widenfelt <ew2789@gmail.com>
2012-08-20 19:07:05 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_content_type_map@12 179f8c61-186b-4a6f-a3fc-937048475198

--- admin.py
+++ admin.py
@@ -1,25 +1,12 @@
 from django.contrib import admin
 from bhp_common.models import MyModelAdmin
 from models import ContentTypeMap
-# Add any missing content types
-from django.contrib.contenttypes.management \
-    import update_all_contenttypes
-
-
-def pop_and_sync(modeladmin, request, queryset):
-    update_all_contenttypes()
-    ContentTypeMap.objects.populate()
-    ContentTypeMap.objects.sync()
-pop_and_sync.short_description = "Re-populate and sync content type"
+from actions import pop_and_sync
 
 
 class ContentTypeMapAdmin(MyModelAdmin):
     list_display = ('name', 'content_type', 'model', 'app_label')
-
     search_fields = ('name', 'app_label', 'model')
-
     list_filter = ('app_label',)
-
-    actions = [pop_and_sync, ]
-
+    actions = [pop_and_sync]
 admin.site.register(ContentTypeMap, ContentTypeMapAdmin)
