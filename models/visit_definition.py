ad6a8884d3a5515e1d542ce4b2325495de58928f
erik van widenfelt <ew2789@gmail.com>
2012-08-22 19:13:51 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@107 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- models/visit_definition.py
+++ models/visit_definition.py
@@ -9,35 +9,27 @@ from bhp_visit.managers import VisitDefinitionManager
 
 
 class VisitDefinition(BaseWindowPeriodItem):
-
     """Model to define a visit code, title, windows, schedule_group, etc."""
-
     code = models.CharField(
         max_length=4,
-        validators = [MinLengthValidator(4),]        
-        )        
-
+        validators=[MinLengthValidator(4)],
+        db_index=True)
     title = models.CharField(
         verbose_name="Title",
-        max_length=35,
-        )
-
+        max_length=35)
     schedule_group = models.ManyToManyField(ScheduleGroup,
-        help_text = "Visit definition may be used in more than one schedule_group")
-
+        help_text="Visit definition may be used in more than one schedule_group")
     instruction = models.TextField(
         verbose_name="Instructions",
         max_length=255,
-        blank=True
-        )    
-
+        blank=True)
     objects = VisitDefinitionManager()
 
     def get_lower_window_datetime(self, appt_datetime):
         days = get_lower_window_days(self.lower_window, self.lower_window_unit)
         td = timedelta(days=days)
         return appt_datetime - td
-        
+
     def get_upper_window_datetime(self, appt_datetime):
         days = get_upper_window_days(self.upper_window, self.upper_window_unit)
         td = timedelta(days=days)
@@ -47,9 +39,9 @@ class VisitDefinition(BaseWindowPeriodItem):
         return '{0}: {1}'.format(self.code, self.title)
 
     def get_absolute_url(self):
-        return reverse('admin:bhp_visit_visitdefinition_change', args=(self.id,)) 
+        return reverse('admin:bhp_visit_visitdefinition_change', args=(self.id,))
 
     class Meta:
-        ordering = ['code', 'time_point']  
+        ordering = ['code', 'time_point']
         app_label = "bhp_visit"
         db_table = 'bhp_form_visitdefinition'
