850e4c1a0d8d6a31e1c96d6a922c2acc88bb649d
erik van widenfelt <ew2789@gmail.com>
2012-08-23 12:13:09 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_subject_summary@6 b3b67fa8-016b-4e13-b55d-735f2f5aa35e

--- models/link.py
+++ models/link.py
@@ -3,33 +3,24 @@ from bhp_base_model.classes import BaseUuidModel
 
 
 class Link (BaseUuidModel):
-    
+
     label = models.CharField(
-        max_length = 25,
-        help_text = 'Button or link label',
-        )
-
+        max_length=25,
+        help_text='Button or link label')
     app_label = models.CharField(
-        max_length = 25,
-        help_text = 'app containing the ajax.py',
-        )
-
+        max_length=25,
+        help_text='app containing the ajax.py')
     ajax_method = models.CharField(
-        max_length = 25,
-        help_text = 'name of method in ajax.py',
-        )
-
+        max_length=25,
+        help_text='name of method in ajax.py')
     dashboard_type = models.CharField(
-        max_length = 25,
-        )
-        
+        max_length=25)
     is_active = models.BooleanField(
-        default=True,
-        )    
+        default=True)
+    objects = models.Manager()
 
     def __unicode__(self):
         return self.label
-        
+
     class Meta:
         app_label = 'bhp_subject_summary'
-        
