cc5b393862310c5670bc2ce2b8faea3a95a07545
erik van widenfelt <ew2789@gmail.com>
2012-08-24 03:33:39 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@181 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/base_registered_subject_model.py
+++ models/base_registered_subject_model.py
@@ -7,28 +7,28 @@ try:
 except ImportError:
     from bhp_base_model.classes import BaseUuidModel
 from bhp_visit_tracking.models.base_visit_tracking import BaseVisitTracking
- 
-        
+
+
 class BaseRegisteredSubjectModel (BaseUuidModel):
 
-    """ Base model for models that need a key to RegisteredSubject. 
-    
+    """ Base model for models that need a key to RegisteredSubject.
+
     Such models may be listed by name in the ScheduledGroup model and thus
     trigger the creation of appointments. Other instances may be Additional
-    forms which are link to a subject but not a time point (for example, 
-    a Death model or OffStudy model (see also AdditionalEntryBucket) 
-    
+    forms which are link to a subject but not a time point (for example,
+    a Death model or OffStudy model (see also AdditionalEntryBucket)
+
     Use this along with BaseRegisteredSubjectModelAdmin()
-    
+
     """
-    registered_subject = models.OneToOneField(RegisteredSubject)            
-    
+    registered_subject = models.OneToOneField(RegisteredSubject)
+
     def get_visit_model(self, instance):
         for model in get_models(get_app(instance._meta.app_label)):
             if isinstance(model(), BaseVisitTracking):
                 return model
         raise TypeError('Unable to determine the visit model from instance {0} for app {1}'.format(instance._meta.model_name, instance._meta.app_label))
-        
+
     def save(self, *args, **kwargs):
 
         super(BaseRegisteredSubjectModel, self).save(*args, **kwargs)
@@ -39,20 +39,15 @@ class BaseRegisteredSubjectModel (BaseUuidModel):
         # for the base_appt_datetime, see Appointment.objects.create_appointments()
 
         if not kwargs.get('suppress_autocreate_on_deserialize', False):
-            Appointment.objects.create_appointments( 
-                registered_subject = self.registered_subject, 
-                model_name = self.__class__.__name__.lower(),
+            Appointment.objects.create_appointments(
+                registered_subject=self.registered_subject,
+                model_name=self.__class__.__name__.lower(),
                 )
-            AdditionalEntryBucket=get_model('bhp_entry', 'additionalentrybucket')
-            AdditionalEntryBucket.objects.update_status(
-                registered_subject = self.registered_subject,    
-                model_instance = self,
-                )
+            #AdditionalEntryBucket = get_model('bhp_entry', 'additionalentrybucket')
+            #AdditionalEntryBucket.objects.update_status(
+            #    registered_subject=self.registered_subject,
+            #    model_instance=self,
+            #    )
 
     class Meta:
-        abstract=True
-
-
-
-
-
+        abstract = True
--- models/base_registration_model.py
+++ models/base_registration_model.py
@@ -2,18 +2,18 @@ from django.db import models
 from bhp_base_model.validators import datetime_not_before_study_start, datetime_not_future
 from base_registered_subject_model import BaseRegisteredSubjectModel
 
-       
+
 class BaseRegistrationModel (BaseRegisteredSubjectModel):
 
     #registered_subject = models.OneToOneField(RegisteredSubject,
     #    #editable=False  
     #    )
-    
+
     registration_datetime = models.DateTimeField("Today's date",
         validators=[
             datetime_not_before_study_start,
-            datetime_not_future,])
-    
+            datetime_not_future, ])
+
     class Meta:
-        abstract=True
-    
+        abstract = True
+
