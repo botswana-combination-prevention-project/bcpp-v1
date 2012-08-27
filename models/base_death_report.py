28a58486412f5c508959e487a46a3a2b1d4c0208
erik van widenfelt <ew2789@gmail.com>
2012-08-27 07:59:57 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_adverse@40 89b61199-f52f-466d-8b3a-10004189116d

--- models/base_death_report.py
+++ models/base_death_report.py
@@ -9,49 +9,53 @@ from bhp_registration.models import BaseRegisteredSubjectModel
 
 class DeathCauseInfo (BaseListModel):
     class Meta:
-        ordering = ['display_index']  
+        ordering = ['display_index']
         app_label = 'bhp_adverse'
-                
+
+
 class DeathCauseCategory (BaseListModel):
     class Meta:
-        ordering = ['display_index']  
+        ordering = ['display_index']
         app_label = 'bhp_adverse'
-                
+
+
 class DeathMedicalResponsibility (BaseListModel):
     class Meta:
-        ordering = ['display_index']  
-        app_label = 'bhp_adverse'        
-        
+        ordering = ['display_index']
+        app_label = 'bhp_adverse'
+
+
 class DeathReasonHospitalized (BaseListModel):
     class Meta:
-        ordering = ['display_index']  
+        ordering = ['display_index']
         app_label = 'bhp_adverse'
-        
+
+
 class BaseDeathReport(BaseRegisteredSubjectModel):
-    
+
     """
     Death form / AF005
     """
-    
+
     #registered_subject = models.OneToOneField(RegisteredSubject)
-    
-    death_date = models.DateField( 
+
+    death_date = models.DateField(
         verbose_name="1. Date of Death:",
-        validators = [
-            date_not_before_study_start, 
+        validators=[
+            date_not_before_study_start,
             date_not_future,
             ],
         help_text="",
-        )   
-    death_cause_info = models.ForeignKey(DeathCauseInfo, 
+        )
+    death_cause_info = models.ForeignKey(DeathCauseInfo,
         verbose_name="2. What is the primary source of cause of death information? (if multiple source of information, list one with the smallest number closest to the top of the list) ",
         help_text="",
-        )   
+        )
     death_cause_info_other = OtherCharField(
         verbose_name="2a. if other specify...",
         blank=True,
-        null=True,     
-        )   
+        null=True,
+        )
     perform_autopsy = models.CharField(
         max_length=3,
         choices=YES_NO,
@@ -61,17 +65,17 @@ class BaseDeathReport(BaseRegisteredSubjectModel):
     death_cause = models.TextField(
         max_length=1000,
         verbose_name="4. Describe the major cause of death(including pertinent autopsy information if available),starting with the first noticeable illness thought to be related to death,continuing to time of death. ",
-        help_text = "Note: Cardiac and pulmonary arrest are not major reasons and should not be used to describe major cause)"  
-        )    
-    death_cause_category = models.ForeignKey(DeathCauseCategory, 
+        help_text="Note: Cardiac and pulmonary arrest are not major reasons and should not be used to describe major cause)"
+        )
+    death_cause_category = models.ForeignKey(DeathCauseCategory,
         verbose_name="4a. Based on the above description, what category best defines the major cause of death? ",
         help_text="",
-        )   
+        )
     death_cause_other = OtherCharField(
         verbose_name="4b. if other specify...",
         blank=True,
-        null=True,     
-        )   
+        null=True,
+        )
     dx_code = models.ForeignKey(DxCode,
         max_length=25,
         verbose_name="4c. Please code the cause of death as one of the following:",
@@ -81,35 +85,33 @@ class BaseDeathReport(BaseRegisteredSubjectModel):
         verbose_name="4d. Duration of acute illness directly causing death   ",
         help_text="If unknown enter -1",
         )
-    death_medical_responsibility = models.ForeignKey(DeathMedicalResponsibility, 
+    death_medical_responsibility = models.ForeignKey(DeathMedicalResponsibility,
         verbose_name="5. Who was responsible for primary medical care of the participant during the month prior to death?",
-        help_text="",
-        )    
+        help_text="")
     participant_hospitalized = models.CharField(
         max_length=3,
         choices=YES_NO,
         verbose_name="6. Was the participant hospitalised before death?",
         help_text="",
         )
-    death_reason_hospitalized = models.ForeignKey(DeathReasonHospitalized, 
+    death_reason_hospitalized = models.ForeignKey(DeathReasonHospitalized,
         verbose_name="6a. if yes, hospitalized, what was the primary reason for hospitalisation? ",
         help_text="",
         blank=True,
         null=True,
-        )   
+        )
     days_hospitalized = models.IntegerField(
         verbose_name="6b. if yes, hospitalized, for how many days was the participant hospitalised during the illness immediately before death? ",
         help_text="",
-        default = 0,
+        default=0,
         )
-    
+
     comment = models.TextField(
-        max_length = 500,
+        max_length=500,
         verbose_name="8. Comments",
         blank=True,
-        null=True,   
-        ) 
-        
+        null=True,
+        )
+
     class Meta:
         abstract = True
-
