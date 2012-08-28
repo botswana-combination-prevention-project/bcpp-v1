2a434db9eac49066892392ec1ae18d9ad51fd6e8
erik van widenfelt <ew2789@gmail.com>
2012-08-28 22:36:45 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_model@19 69677513-6300-4d4b-a1c0-2a4462c8f59b

--- validators/age.py
+++ validators/age.py
@@ -3,22 +3,28 @@ from django.core.exceptions import ValidationError
 from bhp_variables.models import StudySpecific
 
 
-def MinConsentAge (value):
-    now  = date.today()
-    ss = StudySpecific.objects.all()[0]
-    
+def MinConsentAge(value):
+    now = date.today()
+    try:
+        ss = StudySpecific.objects.all()[0]
+    except IndexError as e:
+        raise IndexError('{0}. Have you filled in the required information in bhp_variables.StudySpecific?'.format(e))
+
     min_consent_age_years = ss.minimum_age_of_consent
-    min_consent_age_days = 365*min_consent_age_years
+    min_consent_age_days = 365 * min_consent_age_years
     age_in_days = timedelta(days=min_consent_age_days)
     if value > now - age_in_days:
         raise ValidationError(u'Participant must be %syrs or older. Date of birth suggests otherwise. You entered %s ' % (min_consent_age_years, value))
 
-def MaxConsentAge (value):
-    now  = date.today()
-    ss = StudySpecific.objects.all()[0]
+
+def MaxConsentAge(value):
+    now = date.today()
+    try:
+        ss = StudySpecific.objects.all()[0]
+    except IndexError as e:
+        raise IndexError('{0}. Have you filled in the required information in bhp_variables.StudySpecific?'.format(e))
     max_consent_age_years = ss.maximum_age_of_consent
-    max_consent_age_days = 365*max_consent_age_years
+    max_consent_age_days = 365 * max_consent_age_years
     age_in_days = timedelta(days=max_consent_age_days)
     if value < now - age_in_days:
         raise ValidationError(u'Participant must be no older than %syrs. Date of birth suggests otherwise. You entered %s ' % (max_consent_age_years, value))
-
