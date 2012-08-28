a6cd260d7dc574335b6e7f04807a3f0c15eee2c5
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-08-28 07:33:39 +0000

    changed bhp_bucket to bhp_entry_rules
    git-svn-id: http://192.168.1.50/svn/bhp_entry_rules@28 22def012-2d7a-4ff2-8462-e3842ca56b02

--- admin.py
+++ admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from bhp_bucket.models import RuleHistory
+from bhp_entry_rules.models import RuleHistory
 
 
-class RuleHistoryAdmin(admin.ModelAdmin):     
-      
+class RuleHistoryAdmin(admin.ModelAdmin):
+
     list_display = ('action', 'model', 'predicate', 'rule', 'timestamp')
-    
+
 admin.site.register(RuleHistory, RuleHistoryAdmin)
