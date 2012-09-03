31093f39e8ac839c41254be9a0b80d428d5e9181
erik van widenfelt <ew2789@gmail.com>
2012-09-03 11:57:48 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@112 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- managers/visit_definition_manager.py
+++ managers/visit_definition_manager.py
@@ -1,84 +1,76 @@
 import inspect
 from dateutil.relativedelta import relativedelta
 from django.db import models
-from bhp_visit.models import MembershipForm
+from django.db.models import Count
+#from bhp_visit.models import MembershipForm
+
 
 class VisitDefinitionManager(models.Manager):
-    
+
     """
     schedule_group=ScheduleGroup.objects.all()
     schedule_group=schedule_group[4]
     visit_definition = VisitDefinition.objects.filter(code='2010',schedule_group=schedule_group).order_by('time_point')[0]
     VisitDefinition.objects.next_visit_definition(visit_definition=visit_definition)
     """
-    
-    
+
     def next_visit_definition(self, **kwargs):
 
-        """ return next visit_definition for the given visit_definition """        
+        """ return next visit_definition for the given visit_definition """
 
         if kwargs.get('visit_definition'):
             visit_definition = kwargs.get('visit_definition')
             schedule_group = visit_definition
-            #if not isinstance(visit_definition, super(VisitDefinitionManager, self)):
-            #    raise AttributeError, '%s method %s requires visit_definition to be an instance of VisitDefintion' %  (self.__name__, inspect.stack()[0][3],)
         elif kwargs.get('schedule_group') and kwargs.get('code'):
-            schedule_group = kwargs.get('schedule_group')       
-            code = kwargs.get('code')       
+            schedule_group = kwargs.get('schedule_group')
+            code = kwargs.get('code')
             if super(VisitDefinitionManager, self).filter(schedule_group=schedule_group, code=code):
                 visit_definition = super(VisitDefinitionManager, self).get(schedule_group=schedule_group, code=code)
             else:
-                raise ValueError, '%s method %s cannot determine the visit_definition given schedule_group=\'%s\' and code=\'%s\''   % (self.__name__, inspect.stack()[0][3],schedule_group, code,)
+                raise ValueError, '%s method %s cannot determine the visit_definition given schedule_group=\'%s\' and code=\'%s\'' % (self.__name__, inspect.stack()[0][3], schedule_group, code,)
         else:
-            raise AttributeError, '%s method %s requires a visit_definition instance OR schedule_group and code' % (self.__name__, inspect.stack()[0][3], )             
+            raise AttributeError, '%s method %s requires a visit_definition instance OR schedule_group and code' % (self.__name__, inspect.stack()[0][3],)
 
-        visit_definitions = super(VisitDefinitionManager, self).filter(schedule_group=visit_definition.schedule_group).exclude(id=visit_definition.id).order_by('time_point') 
+        visit_definitions = super(VisitDefinitionManager, self).filter(schedule_group=visit_definition.schedule_group).exclude(id=visit_definition.id).order_by('time_point')
 
         if visit_definitions:
             next_visit_def = visit_definitions[0]
         else:
             next_visit_def = None
         return next_visit_def
-        
-    def relativedelta_from_base(self, **kwargs):        
-    
+
+    def relativedelta_from_base(self, **kwargs):
+
         """ return the relativedelta from the zero time_point visit_definition """
 
         if kwargs.get('visit_definition'):
             visit_definition = kwargs.get('visit_definition')
         else:
-            raise AttributeError, '%s method %s requires a visit_definition instance' % (self.__name__, inspect.stack()[0][3], )             
+            raise AttributeError, '%s method %s requires a visit_definition instance' % (self.__name__, inspect.stack()[0][3],)
 
         interval = visit_definition.base_interval
         unit = visit_definition.base_interval_unit
         if interval == 0:
             rdelta = relativedelta(days=0)
-        else:    
+        else:
             if unit == 'Y':
                 rdelta = relativedelta(years=interval)
             elif unit == 'M':
-                rdelta = relativedelta(months=interval)        
+                rdelta = relativedelta(months=interval)
             elif unit == 'D':
-                rdelta = relativedelta(days=interval)        
-            elif unit == 'H':        
+                rdelta = relativedelta(days=interval)
+            elif unit == 'H':
                 rdelta = relativedelta(hours=interval)
             else:
-                raise AttributeError, "Cannot calculate relativedelta, visit_definition.base_interval_unit must be Y,M,D or H. Got %s" % (unit,)  
-        return rdelta 
-        
+                raise AttributeError, "Cannot calculate relativedelta, visit_definition.base_interval_unit must be Y,M,D or H. Got %s" % (unit,)
+        return rdelta
+
     def codes_for_membership_form_category(self, **kwargs):
-        
+        """Returns a list of visit codes for a membership form category."""
         if not kwargs.get('membership_form_category'):
-            raise AttributeError, '%s method %s requires attribute \'membership_form_category\'. Got None' % (self.__name__, inspect.stack()[0][3], )
-        membership_forms = MembershipForm.objects.filter(category=kwargs.get('membership_form_category'))
-        visit_definition_codes = set()
-        for membership_form in membership_forms:
-            for visit_definition in super(VisitDefinitionManager, self).filter(schedule_group__membership_form=membership_form):
-                visit_definition_codes.add(visit_definition.code)
-
-        return list(visit_definition_codes)
-
-        
-        
-        
-
+            raise AttributeError('%s method %s requires attribute \'membership_form_category\'. Got None' % (self.__name__, inspect.stack()[0][3],))
+        codes = []
+        agg = super(VisitDefinitionManager, self).values('code').filter(schedule_group__membership_form__category=kwargs.get('membership_form_category')).annotate(cnt=Count('code'))
+        for dct in agg:
+            codes.append(dct['code'])
+        return codes
