30c52762c7a4541d02464419809c68d6957de820
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-09-07 08:03:59 +0000

    unicode should return a string
    git-svn-id: http://192.168.1.50/svn/bhp_off_study@3 7aed250d-f3cc-4085-b232-d4bc89341959

--- models/base_off_study.py
+++ models/base_off_study.py
@@ -1,7 +1,7 @@
 from django.db import models
 from bhp_base_model.fields import OtherCharField
 from bhp_registration.models import BaseRegisteredSubjectModel
-
+from bhp_crypto.utils import mask_encrypted
 
 class BaseOffStudy(BaseRegisteredSubjectModel):
 
@@ -22,14 +22,15 @@ class BaseOffStudy(BaseRegisteredSubjectModel):
         max_length=250,
         verbose_name="Comments:",
         blank=True,
-        null=True,   
+        null=True,
         )
 
     def __unicode__(self):
-        return self.registered_subject
-    
+        return "{0} {1} ({2})".format(self.registered_subject.subject_identifier,
+                                          self.registered_subject.subject_type,
+                                          mask_encrypted(self.registered_subject.first_name))
+
     class Meta:
         app_label = 'bhp_off_study'
         abstract = True
-        
 
