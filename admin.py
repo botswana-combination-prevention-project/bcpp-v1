dbb2eba461c767edaf5d0a11a4bc8b92f6b0001e
erik van widenfelt <ew2789@gmail.com>
2012-09-09 08:56:14 +0000

    git-svn-id: http://192.168.1.50/svn/lab_import_lis@31 e53f047f-6857-484a-85a2-23d11a799c7e

--- admin.py
+++ admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from bhp_base_model.classes import BaseModelAdmin
 from models import LisImportError
+from actions import clear_stale_error_messages
 
 
 class LisImportErrorAdmin(BaseModelAdmin):
     list_display = ('model_name', "identifier", 'created', "error_message")
     list_filter = ('model_name', 'created')
     search_fields = ("identifier", "error_message")
+    actions = [clear_stale_error_messages]
 admin.site.register(LisImportError, LisImportErrorAdmin)
