4c46419073059c0d128a1ec1bf2f2c9f66d83027
erik van widenfelt <ew2789@gmail.com>
2012-09-20 12:17:29 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment_helper@1 be5f3245-f14f-42d7-95c3-3126846531f0

---
+++ classes/appointment_date_helper.py
@@ -0,0 +1,138 @@
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from bhp_visit.models import VisitDefinition
+from bhp_appointment.models import Holiday, Configuration
+
+
+class AppointmentDateHelper(object):
+    """ """
+    def __init__(self):
+        if not "APPOINTMENTS_PER_DAY_MAX" in dir(settings):
+            raise ImproperlyConfigured('Appointment requires settings attribute APPOINTMENTS_PER_DAY_MAX. Please add to your settings.py')
+        # number of appointments to set per day before moveing to an alternative appointment date
+        self.appointments_per_day_max = settings.APPT_PER_DAY_MAX
+        # number of days to look forward for an alternative appointment date
+        self.days_forward = 8
+        # not used
+        self.allow_backwards = False
+        # True if appointments should land on the same day for a subject
+        config = Configuration.objects.get_configuration()
+        self.use_same_weekday = config.use_same_weekday
+
+    def get_best_datetime(self, appt_datetime, weekday=None):
+        """ Gets the appointment datetime on insert.
+
+        For example, may be configured to be on the same day as the base, not on holiday, etc.
+
+        Note, appt_datetime comes from the membership_form model method get_registration_datetime"""
+
+        if not isinstance(appt_datetime, datetime):
+            raise AttributeError('Expected parameter \'appt_datetime\' to be an instance of datetime')
+        if weekday and self.use_same_weekday:
+            # force to use same week day for every appointment
+            appt_datetime = self._move_to_same_weekday(appt_datetime, weekday)
+        return self._check(appt_datetime)
+
+    def change_datetime(self, best_appt_datetime, new_appt_datetime, days_forward=None):
+        """Checks if an appointment datetime from the user is OK to accept."""
+        if not days_forward:
+            days_forward = self.days_forward
+        td = best_appt_datetime - new_appt_datetime
+        if abs(td.days) <= days_forward and new_appt_datetime >= datetime.today():
+            # return changed datetime +/- a possible adjustment in _check
+            appt_datetime = self._check(new_appt_datetime)
+        else:
+            # return unchanged appt_datetime
+            appt_datetime = best_appt_datetime
+        return appt_datetime
+
+    def get_relative_datetime(self, base_appt_datetime, visit_definition):
+        """ Returns appointment datetime relative to the base_appointment_datetime."""
+        appt_datetime = base_appt_datetime + VisitDefinition.objects.relativedelta_from_base(visit_definition=visit_definition)
+        return self.get_best_datetime(appt_datetime, base_appt_datetime.isoweekday())
+
+    def _check(self, appt_datetime):
+        appt_datetime = self._check_if_allowed_isoweekday(appt_datetime)
+        appt_datetime = self._check_if_holiday(appt_datetime)
+        appt_datetime = self._move_on_appt_max_exceeded(appt_datetime)
+        return appt_datetime
+
+    def _check_if_allowed_isoweekday(self, appt_datetime):
+        """ Checks if weekday is allowed, otherwise adjust forward or backward """
+        # check if is allowable isoweekday based on integer value in
+        # study_specific.allowed_iso_weekdays (e.g. 12345)
+        allowed_iso_weekdays = [int(num) for num in str(self.get_appointment_configuration().allowed_iso_weekdays)]
+        forward = appt_datetime
+        while forward.isoweekday() not in allowed_iso_weekdays:
+            forward = forward + timedelta(days=+1)
+        backward = appt_datetime
+        while backward.isoweekday() not in allowed_iso_weekdays:
+            backward = backward + timedelta(days=-1)
+        # which is closer to the original appt_datetime
+        td_forward = abs(appt_datetime - forward)
+        td_backward = abs(appt_datetime - backward)
+        if td_forward <= td_backward:
+            appt_datetime = forward
+        else:
+            appt_datetime = backward
+        return appt_datetime
+
+    def _check_if_holiday(self, appt_datetime):
+        """ Checks if appt_datetime lands on a holiday, if so, move forward """
+        while appt_datetime.date() in [holiday.holiday_date for holiday in Holiday.objects.all()]:
+            appt_datetime = appt_datetime + timedelta(days=+1)
+            appt_datetime = self._check_if_allowed_isoweekday(appt_datetime)
+        return appt_datetime
+
+    def _move_to_same_weekday(self, appt_datetime, weekday=1):
+        """ Moves appointment to use same weekday for each subject appointment."""
+        if weekday not in range(1, 8):
+            raise ValueError('Weekday must be a number between 1-7, Got %s' % (weekday, ))
+        # make all appointments land on the same isoweekday,
+        # if possible as date may change becuase of holiday and/or iso_weekday checks below)
+        forward = appt_datetime
+        while not forward.isoweekday() == weekday:
+            forward = forward + timedelta(days=+1)
+        backward = appt_datetime
+        while not backward.isoweekday() == weekday:
+            backward = backward - timedelta(days=+1)
+        # which is closer to the original appt_datetime
+        td_forward = abs(appt_datetime - forward)
+        td_backward = abs(appt_datetime - backward)
+        if td_forward <= td_backward:
+            appt_datetime = forward
+        else:
+            appt_datetime = backward
+        return appt_datetime
+
+    def _move_on_appt_max_exceeded(self, appt_datetime, appointments_per_day_max=None, days_forward=None):
+        """Moves appointment date to another date if the appointments_per_day_max is exceeded."""
+        from bhp_appointment.models import Appointment
+        if not appointments_per_day_max:
+            appointments_per_day_max = self.appointments_per_day_max
+        if not days_forward:
+            days_forward = self.days_forward
+        my_appt_date = appt_datetime.date()
+        # get a list of appointments in the date range from 'appt_datetime' to 'appt_datetime'+days_forward
+        # use model field appointment.best_appt_datetime not appointment.appt_datetime
+        # TODO: change this query to allow the search to go to the beginning of the week
+        appointments = Appointment.objects.filter(
+            best_appt_datetime__gte=appt_datetime,
+            best_appt_datetime__lte=appt_datetime + timedelta(days=self.days_forward))
+        if appointments:
+            # looking for appointments per day
+            # create dictionary of { day: count, ... }
+            appt_dates = [appointment.appt_datetime.date() for appointment in appointments]
+            appt_date_counts = dict((i, appt_dates.count(i)) for i in appt_dates)
+            # if desired date is not maxed out, use it
+            if appt_date_counts(my_appt_date) < appointments_per_day_max:
+                appt_date = my_appt_date
+            else:
+                # look for an alternative date
+                for appt_date, cnt in dict((i, appt_dates.count(i)) for i in appt_dates).iteritems():
+                    # only looking forward at the moment unless Appointments query is changed above
+                    if cnt < appointments_per_day_max and appt_date > my_appt_date:
+                        break
+            # return an appointment datetime that uses the time from the originally desrired datetime
+            return datetime(appt_date.year, appt_date.month, appt_date.day, appt_datetime.hour, appt_datetime.minute)
---
+++ classes/appointment_helper.py
@@ -0,0 +1,128 @@
+import inspect
+from django.db.models import get_model, Max
+from bhp_visit.models import VisitDefinition, ScheduleGroup
+from bhp_visit.classes import VisitDefinitionHelper
+from appointment_date_helper import AppointmentDateHelper
+from bhp_appointment.models import Appointment
+
+
+class AppointmentHelper(object):
+    """ """
+    def __init__(self):
+        pass
+
+    def create_all(self, registered_subject, model_name, base_appt_datetime=None, dashboard_type=None):
+        """Creates appointments for a registered subject based on a list of visit definitions if given model_name is a member of a schedule group.
+
+            Args:
+                registered_subject: current subject
+                model_name: model of the membership_form
+                dashboard_type:
+
+            1. Only create for visit_instance = 0
+            2. If appointment exists, just update the appt_datetime
+
+            visit_definition contains schedule group contains membership form
+        """
+        # base_appt_datetime must come from the membership_form model and not from the appt_datetime
+        # of the first appointment as the user may change this.
+        # base_appt_datetime = kwargs.get("base_appt_datetime")
+        if ScheduleGroup.objects.filter(membership_form__content_type_map__model=model_name):
+            # get list of visits for scheduled group containing this model
+            # get schedule_group
+            schedule_group = ScheduleGroup.objects.get(membership_form__content_type_map__model=model_name)
+            # get membership_form model class of this schedule_group for this registered_subject
+            membership_form_model = get_model(app_label=schedule_group.membership_form.content_type_map.app_label, model_name=model_name)
+            if membership_form_model.objects.filter(registered_subject=registered_subject):
+                # in some cases, send the base_appt_datetime, such as when
+                # the visit_datetime is a next appt datetime.
+                # But ideally, it is better to get this from
+                # get_registration_datetime() on the model.
+                if not base_appt_datetime:
+                    # determine base_appt_datetime using the membership_form instance
+                    # for an existing membership form
+                    membership_form = membership_form_model.objects.get(registered_subject=registered_subject)
+                    base_appt_datetime = membership_form.get_registration_datetime()
+            else:
+                # we may be calling this method as a new membership form is being inserted
+                # once the instance is saved, the created attribute will = datetime.today()
+                #base_appt_datetime = datetime.today()
+
+                # i have decided that it is safer for the model_instance to return the
+                # base_appt_datetime instead of assuming datetime.today(),
+                # thus, the model save() method must have been called already.
+                # ...see BaseRegisteredSubjectModel.save()
+                # so if you get here throw an error.
+                raise AttributeError("%s method %s cannot determine the registration model_instance. "
+                                     "This is needed to call get_registration_datetime()." % (self, inspect.stack()[0][3],))
+            visit_definitions = VisitDefinition.objects.filter(schedule_group=schedule_group)
+            appointment_date_helper = AppointmentDateHelper()
+            for visit_definition in visit_definitions:
+                # calculate the appointment date
+                if visit_definition.time_point == 0:
+                    #appt_datetime = self.best_appointment_datetime(appt_datetime=base_appt_datetime)
+                    appt_datetime = appointment_date_helper.get_best_datetime(base_appt_datetime)
+                else:
+                    appt_datetime = appointment_date_helper.get_relative_datetime(base_appt_datetime, visit_definition)
+                # if appt exists, update appt_datetime
+                if Appointment.objects.filter(
+                            registered_subject=registered_subject,
+                            visit_definition=visit_definition,
+                            visit_instance=0):
+                    appt = Appointment.objects.get(
+                                registered_subject=registered_subject,
+                                visit_definition=visit_definition,
+                                visit_instance=0)
+                    appt.appt_datetime = appt_datetime
+                    appt.save()
+                # else create a new appointment
+                else:
+                    Appointment.objects.create(
+                        registered_subject=registered_subject,
+                        visit_definition=visit_definition,
+                        visit_instance=0,
+                        appt_datetime=appt_datetime,
+                        timepoint_datetime=appt_datetime,
+                        dashboard_type=dashboard_type)
+
+    def delete_appointments_for_instance(self, model_instance):
+        """ Delete appointments for this registered_subject for this model_instance but only if visit report not yet submitted """
+        #visit_definitions = self.list_visit_definitions_for_model(model_instance.registered_subject, model_instance._meta.object_name.lower())
+        visit_definitions = VisitDefinitionHelper.list_all_for_model(model_instance.registered_subject, model_instance._meta.object_name.lower())
+
+        # only delete appointments without a visit model
+        appointments = Appointment.objects.filter(registered_subject=model_instance.registered_subject, visit_definition__in=visit_definitions)
+        count = 0
+        visit_model = model_instance.get_visit_model(model_instance)
+        for appointment in appointments:
+            if not visit_model.objects.filter(appointment=appointment):
+                appointment.delete()
+                count += 1
+        return count
+
+    def create_next_instance(self, base_appointment_instance, next_appt_datetime):
+        """ Creates a continuation appointment given the base appointment instance (.0) and the next appt_datetime """
+        appointment = base_appointment_instance
+        if not Appointment.objects.filter(
+            registered_subject=appointment.registered_subject,
+            visit_definition=appointment.visit_definition,
+            appt_datetime=next_appt_datetime):
+            aggr = Appointment.objects.filter(
+                registeredsubject=appointment.registered_subject,
+                visit_definition=appointment.visit_definition
+                ).aggregate(Max('visit_instance'))
+            if aggr:
+                appointment_date_helper = AppointmentDateHelper()
+                # check if there are rules to determine a better appt_datetime
+                appt_datetime = appointment_date_helper.get_best_datetime(next_appt_datetime)
+                next_visit_instance = int(aggr['visit_instance__max'] + 1.0)
+                Appointment.objects.create(
+                    registered_subject=appointment.registered_subject,
+                    visit_definition=appointment.visit_definition,
+                    visit_instance=next_visit_instance,
+                    appt_datetime=appt_datetime)
+
+    def list_appointments_for_model(self, registered_subject, model_name):
+        """ Lists created appointments for this registered_subject for this model_name """
+        visit_definitions = VisitDefinitionHelper.list_all_for_model(registered_subject, model_name)
+        return Appointment.objects.filter(registered_subject=registered_subject, visit_definition__in=visit_definitions)
