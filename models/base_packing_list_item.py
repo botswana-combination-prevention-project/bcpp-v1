e59b387a8cd7bbf70b03c19902eeb7fe99398fe2
erik van widenfelt <ew2789@gmail.com>
2012-09-24 05:16:56 +0000

    git-svn-id: http://192.168.1.50/svn/lab_packing@24 95d7b449-3884-4939-8fbe-e73e6bee94f8

--- models/base_packing_list_item.py
+++ models/base_packing_list_item.py
@@ -11,87 +11,96 @@ from base_packing_list import BasePackingList
 class BasePackingListItem(BaseUuidModel):
 
     requisition = models.CharField(
-        max_length = 35,
-        null = True,
-        blank = False,
-        help_text = "",
+        max_length=35,
+        null=True,
+        blank=False,
+        help_text="",
         )
-    
+
     item_reference = models.CharField(
-        max_length = 25,
+        max_length=25,
         )
 
     item_datetime = models.DateTimeField(
-        null = True,
-        blank = True,
-        )    
+        null=True,
+        blank=True,
+        )
 
     item_description = models.TextField(
-        max_length = 100,
-        null = True,
-        blank = True,
-        )  
-    
-    item_priority = models.CharField(
-        max_length = 35,
-        choices = (('normal','Normal'),('urgent','Urgent')),
-        null = True,
-        blank = False,
-        help_text = "",
+        max_length=100,
+        null=True,
+        blank=True,
         )
-      
+
+    item_priority = models.CharField(
+        max_length=35,
+        choices=(('normal', 'Normal'), ('urgent', 'Urgent')),
+        null=True,
+        blank=False,
+        help_text="",
+        )
+
     panel = models.ForeignKey(Panel,
-        null = True,
-        blank = True,
-        )  
-    
-    #history = AuditTrail()    
+        null=True,
+        blank=True,
+        )
+
+    #history = AuditTrail()
+
+    def gender(self):
+        """Users should override."""
+        return '?'
+
+    def clinician(self):
+        """Users should override."""
+        return '?'
+
+    def drawn_datetime(self):
+        return None
+
     def packing_list_model(self):
         for field in self._meta.fields:
             try:
                 if issubclass(field.rel.to, BasePackingList):
                     return (field.attname, field.rel.to)
             except:
-                pass    
-        return (None, None)   
-    
+                pass
+        return (None, None)
+
     def view_packing_list(self):
         packing_list_field_attname, packing_list_model = self.packing_list_model()
         if packing_list_model:
-            return '<a href="/admin/{app_label}/{object_name}/?q={pk}">{timestamp}</a>'.format(app_label = packing_list_model._meta.app_label,
-                                                                                    object_name = packing_list_model._meta.object_name.lower(),
-                                                                                    timestamp = packing_list_model.objects.get(pk=getattr(self, packing_list_field_attname)).timestamp,
-                                                                                    pk = getattr(self, packing_list_field_attname),
+            return '<a href="/admin/{app_label}/{object_name}/?q={pk}">{timestamp}</a>'.format(app_label=packing_list_model._meta.app_label,
+                                                                                    object_name=packing_list_model._meta.object_name.lower(),
+                                                                                    timestamp=packing_list_model.objects.get(pk=getattr(self, packing_list_field_attname)).timestamp,
+                                                                                    pk=getattr(self, packing_list_field_attname),
                                                                                     )
         else:
             return 'packing list'
     view_packing_list.allow_tags = True
 
     def priority(self):
-        style=''
-        priority = self.item_priority.lower() 
+        style = ''
+        priority = self.item_priority.lower()
         if priority == 'urgent':
             style = 'color:red;font-weight:700'
         if priority == 'normal':
-            priority = ''    
+            priority = ''
         return '<span style="{style}">{priority}</span>'.format(style=style, priority=priority)
     priority.allow_tags = True
-     
+
     def specimen(self):
-        return '{item_reference}<BR>{requisition}</a>'.format(item_reference = self.item_reference,
-                                                              requisition = self.requisition.replace('requisition', ''))
-    specimen.allow_tags=True
-       
+        return '{item_reference}<BR>{requisition}</a>'.format(item_reference=self.item_reference,
+                                                              requisition=self.requisition.replace('requisition', ''))
+    specimen.allow_tags = True
+
     def description(self):
         return self.item_description.replace(' ', '<BR>')
     description.allow_tags = True
-    
+
     def get_subject_identifier(self):
         return ''
-    
+
     class Meta:
         abstract = True
-        #app_label = 'lab_packing'    
-        ordering = ['created',]            
-        
-    
+        ordering = ['created', ]
