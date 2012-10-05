68d5f54843361d9f35084c2e51f02104cb547ecf
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-10-05 11:56:20 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@183 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
-from base_transaction import BaseTransaction
 from sync_lock_model import SyncLockModel
 from sync_import_history_model import SyncImportHistoryModel
+from base_transaction import BaseTransaction
 from incoming_transaction import IncomingTransaction
 from outgoing_transaction import OutgoingTransaction
 from transaction import Transaction
