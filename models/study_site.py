349ef12c11df12729e17fdfef82686b3e3c8f46a
erik van widenfelt <ew2789@gmail.com>
2012-10-16 11:48:14 +0000

    natural key
    git-svn-id: http://192.168.1.50/svn/bhp_variables@42 72d6f641-25d3-4f20-903c-4c2123898bce

--- models/study_site.py
+++ models/study_site.py
@@ -3,16 +3,18 @@ from bhp_base_model.classes import BaseUuidModel
 
 
 class StudySite(BaseUuidModel):
-    
-    site_code = models.CharField(max_length=4)        
-    
-    site_name = models.CharField(max_length=35)
-    
+
+    site_code = models.CharField(max_length=4, unique=True)
+
+    site_name = models.CharField(max_length=35, unique=True)
+
+    def natural_key(self):
+        return (self.site_code, )
+
     def __unicode__(self):
         return "%s %s" % (self.site_code, self.site_name)
-        
+
     class Meta:
-        unique_together = [('site_code', 'site_name')]   
-        ordering = ['site_code',] 
-        app_label='bhp_variables'
-        
+        unique_together = [('site_code', 'site_name')]
+        ordering = ['site_code', ]
+        app_label = 'bhp_variables'
