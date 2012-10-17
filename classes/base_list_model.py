e4659ea2d456db76cc270622c78fece17374c7e5
erik van widenfelt <ew2789@gmail.com>
2012-10-17 06:19:31 +0000

    add natural key manager for list
    git-svn-id: http://192.168.1.50/svn/bhp_base_model@21 69677513-6300-4d4b-a1c0-2a4462c8f59b

--- classes/base_list_model.py
+++ classes/base_list_model.py
@@ -46,6 +46,9 @@ class BaseListModel(BaseModel):
     def __unicode__(self):
         return self.name
 
+    def natural_key(self):
+        return (self.name, )
+
     class Meta:
         abstract = True
         ordering = ['display_index']
