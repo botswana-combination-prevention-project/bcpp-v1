53d0c78009fad60aed2d33016e78b312bc190c29
erik van widenfelt <ew2789@gmail.com>
2012-10-18 16:56:32 +0000

    enabled signal
    git-svn-id: http://192.168.1.50/svn/bhp_lab_tracker@22 5f62ce6e-2ac5-4da8-806c-9e97803e0885

--- models/signals.py
+++ models/signals.py
@@ -1,10 +1,10 @@
-#from django.db.models.signals import post_save
-#from django.dispatch import receiver
-#from lab_tracker.classes import tracker
-#
-#
-#@receiver(post_save, weak=False, dispatch_uid="tracker_on_post_save")
-#def tracker_on_post_save(sender, instance, **kwargs):
-#    for model, history_cls in tracker.iteritems():
-#        if sender == model:
-#            history_cls.update(sender._meta.object_name)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from lab_tracker.classes import tracker
+
+
+@receiver(post_save, weak=False, dispatch_uid="tracker_on_post_save")
+def tracker_on_post_save(sender, instance, **kwargs):
+    for model, history_cls in tracker.iteritems():
+        if sender == model:
+            history_cls.update(sender._meta.object_name)
