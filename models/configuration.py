c8a03b77fa4a0c9d40f7547dcd38344630b60cbc
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-10-24 08:06:39 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@143 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- models/configuration.py
+++ models/configuration.py
@@ -7,28 +7,32 @@ from bhp_appointment.managers import ConfigurationManager
 class Configuration(BaseUuidModel):
 
     allowed_iso_weekdays = models.IntegerField(
-        default = 12345,
-        help_text = 'List ISO weekdays to which appointments may be scheduled. For example, 12345 where 1 is Monday.' ,
+        default=12345,
+        help_text='List ISO weekdays to which appointments may be scheduled. For example, 12345 where 1 is Monday.' ,
         )
 
     use_same_weekday = models.BooleanField(
-        max_length = 3,
-        default = True,
-        help_text = 'If Ticked, adjust the appointment date to have appointments always land on the same weekday.',
+        max_length=3,
+        default=True,
+        help_text='If Ticked, adjust the appointment date to have appointments always land on the same weekday.',
         )
-        
-    objects = ConfigurationManager()        
+
+    objects = ConfigurationManager()
 
     def save(self, *args, **kwargs):
-        
         if not self.id and self.__class__.objects.all().count() == 1:
-            raise ValueError, 'Configuration model may only have one record and you are trying to add a second. Edit the first record instead.'
+            raise ValueError('Configuration model may only have one record and you are trying to add a second. Edit the first record instead.')
         else:
-            super(Configuration, self).save(*args, **kwargs)            
+            super(Configuration, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('admin:bhp_appointment_configuration_change', args=(self.id,))
+        return reverse(
+            'admin:bhp_appointment_configuration_change',
+            args=(self.id,)
+            )
+
+    def __unicode__(self):
+        return "{0}".format(self.allowed_iso_weekdays)
 
     class Meta:
-        app_label = 'bhp_appointment' 
-    
+        app_label = 'bhp_appointment'
