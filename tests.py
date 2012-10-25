297a5ae8d03b7d8894a0512d7454c91b0e9fd177
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-25 12:38:30 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@182 5124d8a1-613d-4708-9420-8c98912e35b2

--- tests.py
+++ tests.py
@@ -22,37 +22,40 @@ class ResultItemTestCase(unittest.TestCase):
 
     def test_Haemaglobin_grade_flag(self):      
             for flag_args in TRUE_G1_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 1,str(self.get_result_item_flag(flag_args))+" is not==1")
+                self.assertTrue(self.get_result_item_flag(flag_args) == 1,"TRUE_G1 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G1_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 1)
+                self.assertFalse(self.get_result_item_flag(flag_args) == 1,"FALSE_G1 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in TRUE_G2_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 2)
+                self.assertTrue(self.get_result_item_flag(flag_args) == 2,"TRUE_G2 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G2_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 2)
+                self.assertFalse(self.get_result_item_flag(flag_args) == 2,"FALSE_G2 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in TRUE_G3_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 3)
+                self.assertTrue(self.get_result_item_flag(flag_args) == 3,"TRUE_G3 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G3_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 3)
+                self.assertFalse(self.get_result_item_flag(flag_args) == 3,"FALSE_G3 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in TRUE_G4_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 4)
+                self.assertTrue(self.get_result_item_flag(flag_args) == 4,"TRUE_G4 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G4_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 4)
+                self.assertFalse(self.get_result_item_flag(flag_args) == 4,"FALSE_G4 "+str(flag_args['result_value'])+" FAILED")
 #            print "--------------------------------------------------------------------"
 
     def get_result_item_flag(self,flag_args):
         result_item = ResultItem()
         subject = RegisteredSubject()
         test_code = TestCode()
-        test_code.pk = 3
+        test_code.pk = 3 #TODO : Query this value from DB
+        #test_code_temp = TestCode.objects.filter(code='HGB')
+        #test_code.pk = test_code_temp[0].pk
+        #print test_code_temp[0].pk
         test_code_group = TestCodeGroup()
-        test_code_group.code = 301
+        test_code_group.code = 301#TODO : Query this value from DB
         test_code_group.pk = 1
         test_code.test_code_group = test_code_group
         result_item.result_item_value_as_float = flag_args['result_value']
