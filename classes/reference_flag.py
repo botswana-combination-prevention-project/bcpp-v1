895cfa403331bfcd3bea103bbcb5cce34f359ef4
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-25 12:42:27 +0000

    git-svn-id: http://192.168.1.50/svn/lab_reference@24 d0d547b7-54e7-4bd0-bed5-421763f9147c

--- classes/reference_flag.py
+++ classes/reference_flag.py
@@ -16,22 +16,65 @@ class ReferenceFlag(Flag):
     def get_evaluate_prep(self, value, list_item):
         """ Determines if the value falls outside of the reference range after all values are rounded up."""
         flag, lln, uln = None, None, None
-        if list_item.age_low_days() <= self.age_in_days and list_item.age_high_days() >= self.age_in_days:
-            # round up all values
-            places = self.test_code.display_decimal_places or 0  # might be worth a warning if None
-            lln = ceil(list_item.lln * (10 ** places)) / (10 ** places)
-            uln = ceil(list_item.uln * (10 ** places)) / (10 ** places)
-            value = ceil(value * (10 ** places)) / (10 ** places)
-            # evaluate
-            if not flag:
-                flag = ''
-            if value < lln:
-                flag += 'LO'
-            if value > uln:
-                flag += 'HI'
-            if len(flag) > 2:
-                raise ValueError('Reference ranges overlap (HI/LO) for test code {0} age {1} gender {2}.'.format(self.test_code.code, self.age_in_days, self.gender))
-            if flag == '':
-                flag = None
+        if str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('< '):
+            if self.age_in_days > list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
+                val, lln, uln = self.round_off(value,list_item)
+                return self.get_HI_LO(val,lln,uln,flag)
+        elif str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('<='):
+            if self.age_in_days > list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
+                val, lln, uln = self.round_off(value,list_item)
+                return self.get_HI_LO(val,lln,uln,flag)
+                #flag should be None, LO or HI -- but not ''
+                return flag, lln, uln
+        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('< '):
+            if self.age_in_days >= list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
+                val, lln, uln = self.round_off(value,list_item)
+                return self.get_HI_LO(val,lln,uln,flag)
+                #flag should be None, LO or HI -- but not ''
+                return flag, lln, uln
+        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('<='):
+            if self.age_in_days >= list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
+                val, lln, uln = self.round_off(value,list_item)
+                return self.get_HI_LO(val,lln,uln,flag)
+#        if list_item.age_low_days() <= self.age_in_days and list_item.age_high_days() >= self.age_in_days:
+#            # round up all values
+#            places = self.test_code.display_decimal_places or 0  # might be worth a warning if None
+#            lln = ceil(list_item.lln * (10 ** places)) / (10 ** places)
+#            uln = ceil(list_item.uln * (10 ** places)) / (10 ** places)
+#            value = ceil(value * (10 ** places)) / (10 ** places)
+#            # evaluate
+#            if not flag:
+#                flag = ''
+#            if value < lln:
+#                flag += 'LO'
+#            if value > uln:
+#                flag += 'HI'
+#            if len(flag) > 2:
+#                raise ValueError('Reference ranges overlap (HI/LO) for test code {0} age {1} gender {2}.'.format(self.test_code.code, self.age_in_days, self.gender))
+#            if flag == '':
+#                flag = None
         #flag should be None, LO or HI -- but not ''
         return flag, lln, uln
+
+    def get_HI_LO(self, val, lln, uln, flag):
+        if not flag:
+            flag = ''
+        if val < lln:
+            flag += 'LO'
+        if val > uln:
+            flag += 'HI'
+        if len(flag) > 2:
+            raise ValueError('Reference ranges overlap (HI/LO) for test code {0} age {1} gender {2}.'.format(self.test_code.code, self.age_in_days, self.gender))
+        if flag == '':
+            flag = None
+        #flag should be None, LO or HI -- but not ''
+        return flag, lln, uln
+    
+#    def round_off(self, value, list_item):
+#        flag, lln, uln = None, None, None
+#        places = self.test_code.display_decimal_places or 0  # might be worth a warning if None
+#        lln = ceil(list_item.lln * (10 ** places)) / (10 ** places)
+#        uln = ceil(list_item.uln * (10 ** places)) / (10 ** places)
+#        value = ceil(value * (10 ** places)) / (10 ** places)
+        # evaluate
+        
