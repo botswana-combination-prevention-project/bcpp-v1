4063208fc5e5eecfbeeb692c4d49f46fb449c656
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-25 12:42:11 +0000

    git-svn-id: http://192.168.1.50/svn/lab_grading@43 d0c47599-63b2-4c0f-8d21-292bf8f76f30

--- classes/grade_flag.py
+++ classes/grade_flag.py
@@ -2,7 +2,7 @@ from math import ceil
 from django.db.models import Q
 from django.conf import settings
 from lab_flag.classes import Flag
-
+import pdb
 
 class GradeFlag(Flag):
 
@@ -22,13 +22,42 @@ class GradeFlag(Flag):
 
     def get_evaluate_prep(self, value, list_item):
         """ Determines if the value falls within one of the graded ranges."""
+        #pdb.set_trace()
         flag, lower_limit, upper_limit = None, None, None
-        if list_item.age_low_days() <= self.age_in_days and list_item.age_high_days() >= self.age_in_days:
-            # round up all values
-            places = self.test_code.display_decimal_places or 0  # this might be worth a warning in None
-            lower_limit = ceil(list_item.lln * (10 ** places)) / (10 ** places)
-            upper_limit = ceil(list_item.uln * (10 ** places)) / (10 ** places)
-            value = ceil(value * (10 ** places)) / (10 ** places)
-            if value >= lower_limit and value <= upper_limit:
-                flag = list_item.grade
+        if str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('< '):
+            if self.age_in_days > list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
+                val, lower_limit, upper_limit = self.round_off(value,list_item)
+                if val >= lower_limit and val <= upper_limit:
+                    flag = list_item.grade
+                return flag, lower_limit, upper_limit
+        elif str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('<='):
+            if self.age_in_days > list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
+                val, lower_limit, upper_limit = self.round_off(value,list_item)
+                if val >= lower_limit and val <= upper_limit:
+                    flag = list_item.grade
+                return flag, lower_limit, upper_limit
+        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('< '):
+            if self.age_in_days >= list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
+                val, lower_limit, upper_limit = self.round_off(value,list_item)
+                if val >= lower_limit and val <= upper_limit:
+                    flag = list_item.grade
+                return flag, lower_limit, upper_limit
+        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('<='):
+            if self.age_in_days >= list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
+                val, lower_limit, upper_limit = self.round_off(value,list_item)
+                if val >= lower_limit and val <= upper_limit:
+                    flag = list_item.grade
+                return flag, lower_limit, upper_limit
         return flag, lower_limit, upper_limit
+        
+#        if list_item.age_low_days() <= self.age_in_days and list_item.age_high_days() >= self.age_in_days:
+#            # round up all values
+#            places = self.test_code.display_decimal_places or 0  # this might be worth a warning in None
+#            lower_limit = ceil(list_item.lln * (10 ** places)) / (10 ** places)
+#            upper_limit = ceil(list_item.uln * (10 ** places)) / (10 ** places)
+#            value = ceil(value * (10 ** places)) / (10 ** places)
+#            if value >= lower_limit and value <= upper_limit:
+#                flag = list_item.grade
+#        return flag, lower_limit, upper_limit
+
+    
