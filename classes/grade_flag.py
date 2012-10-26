d1767879985893a0c1b4653811c3176294de3d83
erik van widenfelt <ew2789@gmail.com>
2012-10-26 13:56:00 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_reference@12 d0f1680e-b5b9-4295-8319-3d1593bddc0c

--- classes/grade_flag.py
+++ classes/grade_flag.py
@@ -12,7 +12,13 @@ class GradeFlag(BaseGradeFlag):
         subject_identifier = result_item.result.order.aliquot.receive.registered_subject.subject_identifier
         hiv_status = kwargs.get('hiv_status', None)
         if not hiv_status:
-            subject_identifier, hiv_status, reference_datetime = lab_tracker.get_value('HIV', subject_identifier, reference_datetime)
+            subject_identifier, hiv_status, reference_datetime = lab_tracker.get_value(self.get_lab_tracker_group_name(), subject_identifier, reference_datetime)
         if not hiv_status:
             raise TypeError('hiv_status cannot be None.')
         super(GradeFlag, self).__init__(reference_list, test_code, gender, dob, reference_datetime, hiv_status)
+
+    def get_lab_tracker_group_name(self):
+        """Returns a group name to use when filtering on values in the lab_tracker class.
+
+        See :mode:bhp_lab_tracker"""
+        return 'HIV'
