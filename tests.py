5c7e0b5a8feaae23a8614c57e91cb4be9a46b601
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-26 13:23:55 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@186 5124d8a1-613d-4708-9420-8c98912e35b2

--- tests.py
+++ tests.py
@@ -20,43 +20,51 @@ class ResultItemTestCase(unittest.TestCase):
     def test_print_no_returned(self):
         return self.test_data.count()
 
-    def test_haemaglobin_grade_flag(self):
-            for flag_args in TRUE_G1_ASSERTIONS:
-                if not self.assertTrue(self.get_result_item_flag(flag_args) == 1, "TRUE_G1 " + str(flag_args['result_value']) + " FAILED"):
-                    print 'pass'
+    def test_Haemaglobin_grade_flag(self):      
+#            for flag_args in TRUE_G1_ASSERTIONS:
+#                grade = self.get_result_item_flag(flag_args)
+#                self.assertTrue(grade == 1,"TRUE_G1 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G1_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 1, "FALSE_G1 " + str(flag_args['result_value']) + " FAILED")
+                grade = self.get_result_item_flag(flag_args)
+                self.assertFalse(grade == 1,"FALSE_G1 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
             for flag_args in TRUE_G2_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 2, "TRUE_G2 " + str(flag_args['result_value']) + " FAILED")
+                grade = self.get_result_item_flag(flag_args)
+                self.assertTrue(grade == 2,"TRUE_G2 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G2_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 2, "FALSE_G2 " + str(flag_args['result_value']) + " FAILED")
+                grade = self.get_result_item_flag(flag_args)
+                self.assertFalse(grade == 2,"FALSE_G2 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
             for flag_args in TRUE_G3_ASSERTIONS:
-                self.assertTrue(self.get_result_item_flag(flag_args) == 3, "TRUE_G3 " + str(flag_args['result_value']) + " FAILED")
+                grade = self.get_result_item_flag(flag_args)
+                self.assertTrue(grade == 3,"TRUE_G3 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
             for flag_args in FALSE_G3_ASSERTIONS:
-                self.assertFalse(self.get_result_item_flag(flag_args) == 3, "FALSE_G3 " + str(flag_args['result_value']) + " FAILED")
+                grade = self.get_result_item_flag(flag_args)
+                self.assertFalse(grade == 3,"FALSE_G3 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
-            #for flag_args in TRUE_G4_ASSERTIONS:
-            #    self.assertTrue(self.get_result_item_flag(flag_args) == 4, "TRUE_G4 " + str(flag_args['result_value']) + " FAILED")
+            for flag_args in TRUE_G4_ASSERTIONS:
+                grade = self.get_result_item_flag(flag_args)
+                self.assertTrue(grade == 4,"TRUE_G4 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
-            #for flag_args in FALSE_G4_ASSERTIONS:
-            #    self.assertFalse(self.get_result_item_flag(flag_args) == 4, "FALSE_G4 " + str(flag_args['result_value']) + " FAILED")
+            for flag_args in FALSE_G4_ASSERTIONS:
+                grade = self.get_result_item_flag(flag_args)
+                self.assertFalse(grade == 4,"FALSE_G4 "+str(flag_args['result_value'])+" FAILED. Returned Grade="+str(grade))
 #            print "--------------------------------------------------------------------"
 
-    def get_result_item_flag(self, flag_args):
+    def get_result_item_flag(self,flag_args):
         result_item = ResultItem()
         subject = RegisteredSubject()
         test_code = TestCode()
-        test_code.pk = 3  # TODO : Query this value from DB
-        # test_code_temp = TestCode.objects.filter(code='HGB')
-        # test_code.pk = test_code_temp[0].pk
-        # print test_code_temp[0].pk
+        test_code.pk = 3 #TODO : Query this value from DB
+        #test_code_temp = TestCode.objects.filter(code='HGB')
+        #test_code.pk = test_code_temp[0].pk
+        #print test_code_temp[0].pk
         test_code_group = TestCodeGroup()
-        test_code_group.code = 301  # TODO : Query this value from DB
+        #test_code_group_temp = TestCodeGroup.objects.filter(code='HGB')
+        test_code_group.code = 301#TODO : Query this value from DB
         test_code_group.pk = 1
         test_code.test_code_group = test_code_group
         result_item.result_item_value_as_float = flag_args['result_value']
@@ -67,7 +75,7 @@ class ResultItemTestCase(unittest.TestCase):
         result_item.result_item_datetime = flag_args['datetime_drawn']
         subject.dob = flag_args['dob']
         subject.gender = flag_args['gender']
-        subject.hiv_status = flag_args['hiv_status']
+        subject.hiv_status = flag_args['hiv_status']                
         receive = Receive()
         receive.registered_subject = subject
         aliquot = Aliquot()
@@ -77,13 +85,13 @@ class ResultItemTestCase(unittest.TestCase):
         result = Result()
         result.order = order
         result_item.result = result
-        # pdb.set_trace()
-        reference_range, reference_flag, grade_range, grade_flag = ResultItemFlag().calculate(result_item, hiv_status=flag_args.get('hiv_status'))
-        return grade_flag
-
+        #pdb.set_trace()
+        reference_range, reference_flag, grade_range, grade_flag = ResultItemFlag().calculate(result_item)
+        return result_item.grade_flag
+    
     def main(self):
         rs = ResultItemTestCase()
-        rs.test_haemaglobin_grade_flag()
-
+        rs.test_Haemaglobin_grade_flag()
+        
     if __name__ == '__main__':
             main()
