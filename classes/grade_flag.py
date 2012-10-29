ccdb02d19863867fee9cc0a3ce9eb9969013bfc4
erik van widenfelt <ew2789@gmail.com>
2012-10-29 07:40:33 +0000

    git-svn-id: http://192.168.1.50/svn/lab_grading@46 d0c47599-63b2-4c0f-8d21-292bf8f76f30

--- classes/grade_flag.py
+++ classes/grade_flag.py
@@ -1,29 +1,42 @@
-from math import ceil
 from django.db.models import Q
 from django.conf import settings
+from bhp_lab_tracker.classes import lab_tracker
 from lab_flag.classes import Flag
-import pdb
 
 
 class GradeFlag(Flag):
 
-    def get_list_prep(self):
+    def __init__(self, reference_list, test_code, subject_identifier, gender, dob, reference_datetime, hiv_status, **kwargs):
+        hiv_status = kwargs.get('hiv_status', None)
+        if not hiv_status:
+            subject_identifier, hiv_status, reference_datetime, is_default_hiv_status = lab_tracker.get_value(self.get_lab_tracker_group_name(), subject_identifier, reference_datetime)
+        if not hiv_status:
+            raise TypeError('hiv_status cannot be None.')
+        self.is_default_hiv_status = kwargs.get('is_default_hiv_status', None)
+        super(GradeFlag, self).__init__(reference_list, test_code, gender, dob, reference_datetime, hiv_status, **kwargs)
+
+    def get_lab_tracker_group_name(self):
+        """Returns a group name to use when filtering on values in the lab_tracker class.
+
+        See :mode:bhp_lab_tracker"""
+        return 'HIV'
+
+    def get_list_prep(self, test_code, gender, hiv_status):
         """Returns a filtered list of GradingListItem for this ."""
-        if self.hiv_status:
-            qset = (Q(hiv_status__iexact=self.hiv_status.lower()) | Q(hiv_status__iexact='ANY'))
+        if hiv_status:
+            qset = (Q(hiv_status__iexact=hiv_status.lower()) | Q(hiv_status__iexact='ANY'))
         else:
             qset = (Q(hiv_status__iexact='ANY'))
         list_items = self.list_item_model_cls.objects.filter(
             qset,
             **{'{0}__name__iexact'.format(self.list_name): settings.GRADING_LIST,
                'grading_list__name__iexact': settings.GRADING_LIST,
-               'test_code': self.test_code,
-               'gender__icontains': self.gender})
+               'test_code': test_code,
+               'gender__icontains': gender})
         return list_items
 
     def get_evaluate_prep(self, value, list_item):
         """ Determines if the value falls within one of the graded ranges."""
-        # pdb.set_trace()
         flag, lower_limit, upper_limit = None, None, None
         if str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('< '):
             if self.age_in_days > list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
