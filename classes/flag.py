db19acbb4b17d36735d5c7cfb784302e5f446f7e
erik van widenfelt <ew2789@gmail.com>
2012-10-29 07:40:26 +0000

    git-svn-id: http://192.168.1.50/svn/lab_flag@16 1e68f827-a8a8-4a6c-9325-e8b4dfff610c

--- classes/flag.py
+++ classes/flag.py
@@ -2,7 +2,6 @@ import logging
 from bhp_common.utils import get_age_in_days
 from lab_test_code.models import BaseTestCode
 from lab_reference.models import BaseReferenceListItem
-import pdb
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +14,7 @@ nullhandler = logger.addHandler(NullHandler())
 
 class Flag(object):
 
-    def __init__(self, reference_list, test_code, gender, dob, reference_datetime, hiv_status):
+    def __init__(self, reference_list, test_code, gender, dob, reference_datetime, hiv_status, is_default_hiv_status):
         self.dirty = False
         self.list_name, self.list_item_model_cls = reference_list
         self.test_code = test_code
@@ -25,13 +24,14 @@ class Flag(object):
         self.dob = dob
         self.reference_datetime = reference_datetime
         self.hiv_status = hiv_status
+        self.is_default_hiv_status = is_default_hiv_status
         self.age_in_days = get_age_in_days(self.reference_datetime, self.dob)
 
-    def get_list_prep(self):
+    def get_list_prep(self, *args):
         """Returns a filtered list of list items .
 
         Users should override this."""
-        return None
+        raise TypeError('No reference list has been associated with this class. Cannot continue')
 
     def get_evaluate_prep(self, value, list_item):
         """Returns a tuple of the calculated flag, lower limit, upper limit.
@@ -40,39 +40,40 @@ class Flag(object):
         return None, None, None
 
     def evaluate(self, value):
-        """ Determines the flag for value."""
+        """ Determines the flag for value and returns a with the flag and related parameters."""
         if self.dirty:
             raise ValueError('Instance has already been evaluated. Initialize a new instance before evaluating again.')
         if not isinstance(value, (int, float, long)):
             raise TypeError('Value must be an instance of int, float, long.')
         retdict = {}
-        list_items = self.get_list_prep()
+        retdict.update({'is_default_hiv_status': self.is_default_hiv_status})
+        # get the reference list from the user defined method
+        list_items = self.get_list_prep(test_code, gender, hiv_status)
         if not list_items:
+            # nothing in the reference list for this 
             logger.warning('    No {0} items for {1}.'.format(self.list_name, self.test_code.code))
         else:
-            #pdb.set_trace()
             for list_item in list_items:
                 if not isinstance(list_item, BaseReferenceListItem):
                     raise TypeError('List item must be an instance of BaseReferenceListItem.')
-                flag, lower_limit, upper_limit = self.get_evaluate_prep(value, list_item)
-                retdict['lower_limit'], retdict['upper_limit'] = lower_limit, upper_limit
-                if flag:
-                    retdict['flag'] = flag
+                # call user defined evaluate
+                retdict['flag'], retdict['lower_limit'], retdict['upper_limit'] = self.get_evaluate_prep(value, list_item)
+                if retdict['flag']:
                     break
         self._cleanup()
         return retdict
-    
+
     def round_off(self, value, list_item):
-        flag, lower_limit, upper_limit = None, None, None
+        """Rounds off value and reference range to the number of places from "test code" for valid comparison."""
+        #flag, lower_limit, upper_limit = None, None, None
         places = self.test_code.display_decimal_places or 0  # this might be worth a warning in None
-        #lower_limit = ceil(list_item.lln * (10 ** places)) / (10 ** places)
-        #upper_limit = ceil(list_item.uln * (10 ** places)) / (10 ** places)
-        #value = ceil(value * (10 ** places)) / (10 ** places)
-        lower_limit = round(list_item.lln ,places)
-        upper_limit = round(list_item.uln,places)
-        value = round(value,places)
+        # lower_limit = ceil(list_item.lln * (10 ** places)) / (10 ** places)
+        # upper_limit = ceil(list_item.uln * (10 ** places)) / (10 ** places)
+        # value = ceil(value * (10 ** places)) / (10 ** places)
+        lower_limit = round(list_item.lln, places)
+        upper_limit = round(list_item.uln, places)
+        value = round(value, places)
         return value, lower_limit, upper_limit
-        
 
     def _cleanup(self):
         """ Clean up instance variables in case you forget to re-init."""
@@ -82,4 +83,5 @@ class Flag(object):
         self.dob = None
         self.reference_datetime = None
         self.hiv_status = None
+        self.is_default_hiv_status = None
         self.age_in_days = None
