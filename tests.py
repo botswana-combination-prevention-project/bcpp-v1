5519ea2199a5bd962e73eb9938209b1e5197b039
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-30 10:56:06 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@191 5124d8a1-613d-4708-9420-8c98912e35b2

--- tests.py
+++ tests.py
@@ -17,8 +17,8 @@ class ResultItemTestCase(unittest.TestCase):
 
     # def setUp(self):
         # self.item = ResultItem.objects.create(subject_identifier="xxx", receive_identifier="xxxxx")
-    def test_print_no_returned(self):
-        return self.test_data.count()
+    #def test_print_no_returned(self):
+        #return self.test_data.count()
 
     def test_Haemaglobin_grade_flag(self):      
             for flag_args in TRUE_G1_ASSERTIONS:
@@ -57,20 +57,8 @@ class ResultItemTestCase(unittest.TestCase):
     def get_result_item_flag(self,flag_args):
         result_item = ResultItem()
         subject = RegisteredSubject()
-        test_code = TestCode()
-        test_code.pk = 3 #TODO : Query this value from DB
-        #test_code_temp = TestCode.objects.filter(code='HGB')
-        #test_code.pk = test_code_temp[0].pk
-        #print test_code_temp[0].pk
-        test_code_group = TestCodeGroup()
-        #test_code_group_temp = TestCodeGroup.objects.filter(code='HGB')
-        test_code_group.code = 301#TODO : Query this value from DB
-        test_code_group.pk = 1
-        test_code.test_code_group = test_code_group
+        test_code = TestCode.objects.filter(code='HGB')[0]
         result_item.result_item_value_as_float = flag_args['result_value']
-        test_code.code = flag_args['test_code']
-        test_code.display_decimal_places = 1
-        test_code.units = 'g/dL'
         result_item.test_code = test_code
         result_item.result_item_datetime = flag_args['datetime_drawn']
         subject.dob = flag_args['dob']
@@ -93,5 +81,7 @@ class ResultItemTestCase(unittest.TestCase):
         rs = ResultItemTestCase()
         rs.test_Haemaglobin_grade_flag()
         
-    if __name__ == '__main__':
-            main()
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(ResultItemTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    #unittest.main()
