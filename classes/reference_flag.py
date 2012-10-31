92e26344030765ce4417ab823874b9088b3bbf09
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2012-10-31 13:55:09 +0000

    git-svn-id: http://192.168.1.50/svn/lab_reference@31 d0d547b7-54e7-4bd0-bed5-421763f9147c

--- classes/reference_flag.py
+++ classes/reference_flag.py
@@ -1,59 +1,46 @@
+import re
 from django.conf import settings
 from lab_flag.classes import Flag
-
+import pdb
 
 class ReferenceFlag(Flag):
+
+    def check_list_prep(self, list_items):
+        """Runs additional checks for the reference table."""
+        """For reference_flag only a single comparison item should be exist."""
+        ranges = []
+        for list_item in list_items:
+            ranges.append(str(list_item.lln)+'-'+str(list_item.uln))
+            ranges = list(ranges)
+        if len(ranges) != 1:
+            raise TypeError('Multiple reference ranges for test code {0} gender {2}. Got ranges for "{3}".'.format(self.test_code, self.gender, ranges))
 
     def get_list_prep(self, test_code, gender, hiv_status, age_in_days):
         """Returns a filtered list of list items."""
         list_items = self.list_item_model_cls.objects.filter(
             **{'{0}__name__iexact'.format(self.list_name): settings.REFERENCE_RANGE_LIST,
                'test_code': test_code,
-               'gender__icontains': gender})
-        return list_items
+               'gender__iexact': gender})
+        my_list_items = []
+        eval_str = '{age_in_days} {age_low_quantifier} {age_low_days} and {age_in_days} {age_high_quantifier} {age_high_days}'
+        for list_item in list_items:
+            if not re.match('^\>$|^\>\=$', list_item.age_low_quantifier.strip(' \t\n\r')):
+                raise TypeError('Invalid age_low_quantifier in reference list. Got {0}.'.format(list_item.age_low_quantifier))
+            if not re.match('^\<$|^\<\=$', list_item.age_high_quantifier.strip(' \t\n\r')):
+                raise TypeError('Invalid age_high_quantifier in reference list. Got {0}.'.format(list_item.age_high_quantifier))
+            if eval(eval_str.format(age_in_days=self.age_in_days,
+                                    age_low_quantifier=list_item.age_low_quantifier,
+                                    age_low_days=list_item.age_low_days(),
+                                    age_high_quantifier=list_item.age_high_quantifier,
+                                    age_high_days=list_item.age_high_days())):
+                my_list_items.append(list_item)
+        return my_list_items
 
     def get_evaluate_prep(self, value, list_item):
         """ Determines if the value falls outside of the reference range after all values are rounded up."""
-        flag, lln, uln = None, None, None
-        if str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('< '):
-            if self.age_in_days > list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
-                val, lln, uln = self.round_off(value, list_item)
-                return self.get_flg(val, lln, uln, flag)
-        elif str(list_item.age_low_quantifier) == str('> ') and str(list_item.age_high_quantifier) == str('<='):
-            if self.age_in_days > list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
-                val, lln, uln = self.round_off(value, list_item)
-                return self.get_flg(val, lln, uln, flag)
-                # flag should be None, LO or HI -- but not ''
-                return flag, lln, uln
-        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('< '):
-            if self.age_in_days >= list_item.age_low_days() and self.age_in_days < list_item.age_high_days():
-                val, lln, uln = self.round_off(value, list_item)
-                return self.get_flg(val, lln, uln, flag)
-                # flag should be None, LO or HI -- but not ''
-                return flag, lln, uln
-        elif str(list_item.age_low_quantifier) == str('>=') and str(list_item.age_high_quantifier) == str('<='):
-            if self.age_in_days >= list_item.age_low_days() and self.age_in_days <= list_item.age_high_days():
-                val, lln, uln = self.round_off(value, list_item)
-                return self.get_flg(val, lln, uln, flag)
-#        if list_item.age_low_days() <= self.age_in_days and list_item.age_high_days() >= self.age_in_days:
-#            # round up all values
-#            places = self.test_code.display_decimal_places or 0  # might be worth a warning if None
-#            lln = ceil(list_item.lln * (10 ** places)) / (10 ** places)
-#            uln = ceil(list_item.uln * (10 ** places)) / (10 ** places)
-#            value = ceil(value * (10 ** places)) / (10 ** places)
-#            # evaluate
-#            if not flag:
-#                flag = ''
-#            if value < lln:
-#                flag += 'LO'
-#            if value > uln:
-#                flag += 'HI'
-#            if len(flag) > 2:
-#                raise ValueError('Reference ranges overlap (HI/LO) for test code {0} age {1} gender {2}.'.format(self.test_code.code, self.age_in_days, self.gender))
-#            if flag == '':
-#                flag = None
-        # flag should be None, LO or HI -- but not ''
-        return flag, lln, uln
+        flag = None
+        val, lln, uln = self.round_off(value, list_item)
+        return self.get_flg(val, lln, uln, flag)
 
     def get_flg(self, val, lln, uln, flg):
         if not flg:
