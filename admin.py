36f3626a41a8d40b1b1b61276c578ae8cb7787e4
erik van widenfelt <ew2789@gmail.com>
2012-10-31 14:13:36 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_reference@32 d0f1680e-b5b9-4295-8319-3d1593bddc0c

--- admin.py
+++ admin.py
@@ -11,9 +11,9 @@ admin.site.register(GradingList, GradingListAdmin)
 
 
 class GradingListItemAdmin(BaseModelAdmin):
-    list_display = ('test_code', 'grade', 'active', 'describe', 'hiv_status', 'gender', 'lln', 'uln', 'age_low', 'age_low_unit', 'age_low_quantifier',
+    list_display = ('test_code', 'grade', 'active', 'describe', 'hiv_status', 'gender', 'value_low', 'value_high', 'age_low', 'age_low_unit', 'age_low_quantifier',
                     'age_high', 'age_high_unit', 'age_high_quantifier', 'grading_list', 'scale')
-    search_fields = ['grade', 'test_code__code', 'test_code__name', 'lln', 'uln', 'hiv_status']
+    search_fields = ['grade', 'test_code__code', 'test_code__name', 'value_low', 'value_high', 'hiv_status']
     list_filter = ('grade', 'hiv_status', 'grading_list', 'scale', 'active', 'test_code')
     actions = [flag_as_active, flag_as_inactive]
 admin.site.register(GradingListItem, GradingListItemAdmin)
