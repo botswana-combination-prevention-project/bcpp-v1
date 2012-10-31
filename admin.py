a7cc8d43918bd4093bbcae84c6fd58ff712d196e
erik van widenfelt <ew2789@gmail.com>
2012-10-31 12:45:02 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_reference@30 d0f1680e-b5b9-4295-8319-3d1593bddc0c

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 #from bhp_export_data.actions import export_as_csv_action
 from bhp_base_model.classes import BaseModelAdmin
+from actions import flag_as_active, flag_as_inactive
 from models import GradingList, GradingListItem, ReferenceRangeList, ReferenceRangeListItem
 
 
@@ -10,10 +11,11 @@ admin.site.register(GradingList, GradingListAdmin)
 
 
 class GradingListItemAdmin(BaseModelAdmin):
-    list_display = ('test_code', 'grade', 'describe', 'hiv_status', 'gender', 'lln', 'uln', 'age_low', 'age_low_unit', 'age_low_quantifier',
+    list_display = ('test_code', 'grade', 'active', 'describe', 'hiv_status', 'gender', 'lln', 'uln', 'age_low', 'age_low_unit', 'age_low_quantifier',
                     'age_high', 'age_high_unit', 'age_high_quantifier', 'grading_list', 'scale')
     search_fields = ['grade', 'test_code__code', 'test_code__name', 'lln', 'uln', 'hiv_status']
     list_filter = ('grade', 'hiv_status', 'grading_list', 'scale', 'active', 'test_code')
+    actions = [flag_as_active, flag_as_inactive]
 admin.site.register(GradingListItem, GradingListItemAdmin)
 
 
