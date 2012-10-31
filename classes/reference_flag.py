99ed8fc77f57289d54ced68776402b616cf0b1da
erik van widenfelt <ew2789@gmail.com>
2012-10-31 14:13:58 +0000

    refactor uln, lln
    git-svn-id: http://192.168.1.50/svn/lab_reference@32 d0d547b7-54e7-4bd0-bed5-421763f9147c

--- classes/reference_flag.py
+++ classes/reference_flag.py
@@ -1,7 +1,7 @@
 import re
 from django.conf import settings
 from lab_flag.classes import Flag
-import pdb
+
 
 class ReferenceFlag(Flag):
 
@@ -10,7 +10,7 @@ class ReferenceFlag(Flag):
         """For reference_flag only a single comparison item should be exist."""
         ranges = []
         for list_item in list_items:
-            ranges.append(str(list_item.lln)+'-'+str(list_item.uln))
+            ranges.append(str(list_item.value_low) + '-' + str(list_item.value_high))
             ranges = list(ranges)
         if len(ranges) != 1:
             raise TypeError('Multiple reference ranges for test code {0} gender {2}. Got ranges for "{3}".'.format(self.test_code, self.gender, ranges))
@@ -21,45 +21,25 @@ class ReferenceFlag(Flag):
             **{'{0}__name__iexact'.format(self.list_name): settings.REFERENCE_RANGE_LIST,
                'test_code': test_code,
                'gender__iexact': gender})
-        my_list_items = []
-        eval_str = '{age_in_days} {age_low_quantifier} {age_low_days} and {age_in_days} {age_high_quantifier} {age_high_days}'
-        for list_item in list_items:
-            if not re.match('^\>$|^\>\=$', list_item.age_low_quantifier.strip(' \t\n\r')):
-                raise TypeError('Invalid age_low_quantifier in reference list. Got {0}.'.format(list_item.age_low_quantifier))
-            if not re.match('^\<$|^\<\=$', list_item.age_high_quantifier.strip(' \t\n\r')):
-                raise TypeError('Invalid age_high_quantifier in reference list. Got {0}.'.format(list_item.age_high_quantifier))
-            if eval(eval_str.format(age_in_days=self.age_in_days,
-                                    age_low_quantifier=list_item.age_low_quantifier,
-                                    age_low_days=list_item.age_low_days(),
-                                    age_high_quantifier=list_item.age_high_quantifier,
-                                    age_high_days=list_item.age_high_days())):
-                my_list_items.append(list_item)
-        return my_list_items
+        # return a filtered list of list_item instances
+        return self.filter_list_items_by_age(list_items, self.age_in_days)
 
     def get_evaluate_prep(self, value, list_item):
         """ Determines if the value falls outside of the reference range after all values are rounded up."""
         flag = None
-        val, lln, uln = self.round_off(value, list_item)
-        return self.get_flg(val, lln, uln, flag)
+        val, value_low, value_high = self.round_off(value, list_item)
+        return self.get_flg(val, value_low, value_high, flag)
 
-    def get_flg(self, val, lln, uln, flg):
+    def get_flg(self, val, value_low, value_high, flg):
         if not flg:
             flg = ''
-        if val < lln:
+        if val < value_low:
             flg += 'LO'
-        if val > uln:
+        if val > value_high:
             flg += 'HI'
         if len(flg) > 2:
             raise ValueError('Reference ranges overlap (HI/LO) for test code {0} age {1} gender {2}.'.format(self.test_code.code, self.age_in_days, self.gender))
         if flg == '':
             flg = None
         # flag should be None, LO or HI -- but not ''
-        return flg, lln, uln
-
-#    def round_off(self, value, list_item):
-#        flag, lln, uln = None, None, None
-#        places = self.test_code.display_decimal_places or 0  # might be worth a warning if None
-#        lln = ceil(list_item.lln * (10 ** places)) / (10 ** places)
-#        uln = ceil(list_item.uln * (10 ** places)) / (10 ** places)
-#        value = ceil(value * (10 ** places)) / (10 ** places)
-        # evaluate
+        return flg, value_low, value_high
--- models/base_reference_list_item.py
+++ models/base_reference_list_item.py
@@ -18,23 +18,23 @@ class BaseReferenceListItem(BaseModel):
         max_length=10,
         )
 
-    lln = models.DecimalField(
+    value_low = models.DecimalField(
         verbose_name='lower',
         null=True,
         max_digits=12,
         decimal_places=4,
         blank=True)
 
-    lln_quantifier = models.CharField(max_length=10, default='>=')
+    value_low_quantifier = models.CharField(max_length=10, default='>=')
 
-    uln = models.DecimalField(
+    value_high = models.DecimalField(
         verbose_name='upper',
         null=True,
         max_digits=12,
         decimal_places=4,
         blank=True)
 
-    uln_quantifier = models.CharField(max_length=10, default='<=')
+    value_high_quantifier = models.CharField(max_length=10, default='<=')
 
     age_low = models.IntegerField(
         null=True,
@@ -64,40 +64,6 @@ class BaseReferenceListItem(BaseModel):
         max_length=250,
         blank=True,
         )
-
-    """
-        lower |	upper
-        ------|-------------
-        m*30  |	(1+m)*30)-1
-        y*365 |	(1+y)*365)-1
-    """
-
-    """
-    def age_low_days(self):
-        if self.age_low_unit.upper() == 'D':
-            days = self.age_low * 1
-        elif self.age_low_unit.upper() == 'M':
-            days = self.age_low * 30
-        elif self.age_low_unit.upper() == 'Y':
-            days = self.age_low * 365
-        else:
-            raise TypeError('Invalid age_low_unit in model TestCodeReference, You have the value \'%s\' stored' % (self.age_low_unit.upper()) )
-        return days
-
-    def age_high_days(self):
-        if self.age_high_unit.upper() == 'D':
-            days = self.age_high * 1
-        elif self.age_high_unit.upper() == 'M':
-            days = ((1+self.age_high) * 30)-1
-        elif self.age_high_unit.upper() == 'Y':
-            days = ((1+self.age_high) * 365)-1
-        else:
-            raise TypeError('Invalid age_high_unit in model TestCodeReference, You have the value \'%s\' stored' % (self.age_high_unit.upper()) )
-        return days
-
-    def __unicode__(self):
-        return "%s" % (self.test_code)
-    """
 
     objects = models.Manager()
 
