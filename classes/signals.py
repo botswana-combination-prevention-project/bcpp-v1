1c26911a1bf3e8f7d630b1a8fdaf10983e6234d1
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-11-06 10:01:01 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@193 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- classes/signals.py
+++ classes/signals.py
@@ -7,7 +7,9 @@ from base_sync_model import BaseSyncModel
 
 @receiver(m2m_changed, weak=False, dispatch_uid='serialize_m2m_on_save')
 def serialize_m2m_on_save(sender, instance, **kwargs):
-    """ Part of the serialize transaction process that ensures m2m are serialized correctly."""
+    """ Part of the serialize transaction process that ensures m2m are
+    serialized correctly.
+    """
     if kwargs.get('action') == 'post_add':
         if isinstance(instance, BaseSyncModel):
             if instance.is_serialized() and not instance._meta.proxy:
@@ -17,11 +19,12 @@ def serialize_m2m_on_save(sender, instance, **kwargs):
 
 @receiver(post_save, weak=False, dispatch_uid='serialize_on_save')
 def serialize_on_save(sender, instance, **kwargs):
-    """ Serialize the model instance to the outgoing transaction model for consumption by another application. """
+    """ Serialize the model instance to the outgoing transaction
+    model for consumption by another application.
+    """
     if isinstance(instance, BaseSyncModel):
         hostname = socket.gethostname()
         if (instance.hostname_created == hostname and not instance.hostname_modified) or (instance.hostname_modified == hostname):
             if instance.is_serialized() and not instance._meta.proxy:
                 serialize_to_transaction = SerializeToTransaction()
                 serialize_to_transaction.serialize(sender, instance, **kwargs)
-
