34437027578fe17a6031391d163e2f79b055b593
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-11-06 09:58:14 +0000

    initial
    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@7 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

---
+++ classes/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+@receiver(pre_save, weak=False, dispatch_uid='is_checked_out_on_save')
+def is_checked_out_on_save(sender, instance, **kwargs):
+    """ Before saving, check if instance has not been checked out to a netbook. """
+    #if isinstance(instance, BaseSyncModel):
+    if 'subject_visit' in dir(instance):
+        if instance.subject_visit.is_locked():
+            message = "{0} is currently dispatched to a netbook NOT synced with the server".format(instance.subject_visit.household_structure_member)
+            raise ValueError(message)
+
+    elif 'household_structure_member' in dir(instance):
+        if instance.household_structure_member.is_locked():
+            message = "{0} is currently dispatched to a netbook NOT synced with the server".format(instance.household_structure_member)
+            raise ValueError(message)
