eede039a13aa106a297bd0b543eae4b4ffc1b438
erik van widenfelt <ew2789@gmail.com>
2012-11-08 13:38:12 +0000

    git-svn-id: http://192.168.1.50/svn/lab_result_item@58 94dd4f35-d255-437e-9225-d26d65b3fcea

--- classes/reference_range_flag.py
+++ classes/reference_range_flag.py
@@ -7,6 +7,7 @@ class ReferenceRangeFlag(ReferenceFlag):
         test_code = result_item.test_code
         gender = result_item.result.order.aliquot.receive.patient.gender
         dob = result_item.result.order.aliquot.receive.patient.dob
-        reference_datetime = result_item.result.order.aliquot.receive.receive_datetime
+        drawn_datetime = result_item.result.order.aliquot.receive.receive_datetime
+        release_datetime = result_item.result_item_datetime
         hiv_status = result_item.result.order.aliquot.receive.patient.hiv_status
-        super(ReferenceRangeFlag, self).__init__(reference_list, test_code, gender, dob, reference_datetime, hiv_status, **kwargs)
+        super(ReferenceRangeFlag, self).__init__(reference_list, test_code, gender, dob, drawn_datetime, release_datetime, hiv_status, **kwargs)
