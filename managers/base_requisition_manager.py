c903963709d62abe98c2b801616d21f2310d10c0
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-11-09 12:01:38 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@72 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- managers/base_requisition_manager.py
+++ managers/base_requisition_manager.py
@@ -5,6 +5,10 @@ from bhp_identifier.classes import Identifier
 
 
 class BaseRequisitionManager(models.Manager):
+    def get_by_natural_key(self, requisition_identifier, specimen_identifier):
+        return self.get(requisition_identifier=requisition_identifier,
+                        specimen_identifier=specimen_identifier,
+            )
 
     def get_global_identifier(self, **kwargs):
 
