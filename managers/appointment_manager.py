e75a68984611c0bd821e46b358bfe2955e39187c
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-11-12 14:58:27 +0000

    natural keys
    git-svn-id: http://192.168.1.50/svn/bhp_appointment@146 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- managers/appointment_manager.py
+++ managers/appointment_manager.py
@@ -1,7 +1,22 @@
 from django.db import models
+from bhp_registration.models import RegisteredSubject
+from bhp_visit.models import VisitDefinition
 
 
 class AppointmentManager(models.Manager):
 
-    def get_by_natural_key(self, registered_subject, visit_definition, visit_instance):
-        return self.get(registered_subject=registered_subject, visit_definition=visit_definition, visit_instance=visit_instance)
+    def get_by_natural_key(self, visit_instance, code, identity, first_name, dob, initials, registration_identifier):
+        registered_subject = RegisteredSubject.objects.get(
+            identity=identity,
+            first_name=first_name,
+            dob=dob,
+            initials=initials,
+            registration_identifier=registration_identifier
+            )
+
+        visit_definition = VisitDefinition.objects.get(code=code)
+        return self.get(
+            registered_subject=registered_subject,
+            visit_definition=visit_definition,
+            visit_instance=visit_instance
+            )
