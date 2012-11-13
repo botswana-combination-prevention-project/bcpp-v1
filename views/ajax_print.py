a991d8d8e3417c808d3a3c7ec411ca6d3eddfa2d
erik van widenfelt <ew2789@gmail.com>
2012-11-13 05:15:23 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@74 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- views/ajax_print.py
+++ views/ajax_print.py
@@ -1,22 +1,15 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 from lab_requisition.classes import ClinicRequisitionLabel
 
 
 @login_required
-def ajax_print( request, requisition_identifier, requisition_model ):
-
+def ajax_print(request, requisition_identifier, requisition_model):
     if request.is_ajax():
-        if requisition_identifier is not None:            
-            requisition = requisition_model.objects.get(requisition_identifier = requisition_identifier)
-            
-            for cnt in range(self.item_count_total, 0, -1):
+        if requisition_identifier is not None:
+            requisition = requisition_model.objects.get(requisition_identifier=requisition_identifier)
+            for cnt in range(requisition.item_count_total, 0, -1):
                 label = ClinicRequisitionLabel(
-                                item_count = cnt, 
-                                requisition = self,
-                                )
-                label.print_label() 
-    return None                
-
+                    item_count=cnt,
+                    requisition=requisition)
+                label.print_label()
+    return None
