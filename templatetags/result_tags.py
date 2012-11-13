7cbefec797a2b9bd301696247c9fdc1cced86fef
erik van widenfelt <ew2789@gmail.com>
2012-11-13 05:15:45 +0000

    git-svn-id: http://192.168.1.50/svn/lab_common@12 3c07a67b-3389-4fa7-9c45-b2f84f859bbe

--- templatetags/result_tags.py
+++ templatetags/result_tags.py
@@ -1,8 +1,5 @@
-from datetime import *
-from dateutil.relativedelta import *
 from django import template
 from bhp_common.utils import formatted_age
-from bhp_research_protocol.models import Protocol
 register = template.Library()
 
 
@@ -11,6 +8,7 @@ def result_age(born, collection_date):
     reference_date = collection_date.date()
     return formatted_age(born, reference_date)
 
+
 @register.filter(name='result_clinic_name')
 def result_clinic_name(site_identifier, protocol_identifier):
 
@@ -18,16 +16,16 @@ def result_clinic_name(site_identifier, protocol_identifier):
 
     """
     if Protocol.objects.filter(protocol_identifier__iexact=protocol_identifier):
-        oProtocol = Protocol.objects.get(protocol_identifier__iexact=protocol_identifier)        
+        oProtocol = Protocol.objects.get(protocol_identifier__iexact=protocol_identifier)
         for site in oProtocol.site.all():
             if site.site_identifier == site_identifier:
                 site_name = '%s %s %s' % (site_identifier, oProtocol.site_name_fragment, site.location)
     """
-    
+
     if not site_name:
-        site_name = site_identifier        
+        site_name = site_identifier
     return site_name
-    
+
 
 @register.filter(name='filter_validation_by_status')
 def filter_validation_by_status(value, status):
@@ -36,28 +34,28 @@ def filter_validation_by_status(value, status):
     elif status == 'P':
         return 'Preliminary'
     else:
-        return value  
-
+        return value
 
 
 @register.filter(name='status_flag')
 def status_flag(value):
-    if value=='F':
+    if value == 'F':
         return ''
     else:
-        return value    
-
-@register.filter(name='hide_not_final')        
-def hide_not_final(value, validation_status):
-    if validation_status=='F':
         return value
-    else:    
+
+
+@register.filter(name='hide_not_final')
+def hide_not_final(value, validation_status):
+    if validation_status == 'F':
+        return value
+    else:
         return '****'
-    
+
+
 @register.filter(name='quantifier')
 def quantifier(value):
     if value == '=':
         return ''
     else:
-        return value    
-    
+        return value
