23b2403cc6260f55e86fc8e142491909be04cb74
erik van widenfelt <ew2789@gmail.com>
2012-11-21 11:00:03 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@26 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,4 +1,3 @@
-#from base_dispatch import BaseDispatch
 from dispatch_controller import DispatchController
 from hbc_dispatch_helper import HBCDispatchHelper
 from edc_device_prep import EdcDevicePrep
