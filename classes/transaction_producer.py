4e64fb7eed5949cf12fcc1dc045a8dc18b599e26
erik van widenfelt <ew2789@gmail.com>
2012-11-21 11:00:35 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@200 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- classes/transaction_producer.py
+++ classes/transaction_producer.py
@@ -1,21 +1,32 @@
 import socket
-import settings
+from django.conf import settings
+from django.db.models import get_model
 
 
 class TransactionProducer(object):
-    
+
     def __init__(self, **kwargs):
 
         if kwargs.get('hostname'):
             self.value = kwargs.get('hostname')
-        else:            
-            self.value = '%s-%s' % ( socket.gethostname().lower(),settings.DATABASES['default']['NAME'].lower())
+        else:
+            self.value = '%s-%s' % (socket.gethostname().lower(), settings.DATABASES['default']['NAME'].lower())
         if len(self.value) > 25:
             raise ValueError('Transaction Producer name cannot exceed 25 characters.(hostname + settings.DATABASES[\'default\'][\'NAME\'])')
 
     def __get__(self, instance, owner):
         return self.value
-        
+
     def __str__(self):
         return self.value
 
+    def has_outgoing_transactions(self, producer_name=None, using=None):
+        retval = False
+        if not using:
+            using = 'default'
+        OutgoingTransaction = get_model('bhp_sync', 'outgoingtransaction')
+        if not producer_name:
+            producer_name = self.value
+        if OutgoingTransaction.objects.using(using).filter(producer=producer_name, is_consumed=False):
+            retval = True
+        return retval
