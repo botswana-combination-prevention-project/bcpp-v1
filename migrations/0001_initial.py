3a9fc3b1f775b01b83a96db9262c5fee48685386
erik van widenfelt <ew2789@gmail.com>
2012-12-01 14:01:08 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@58 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

---
+++ migrations/0001_initial.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Dispatch'
+        db.create_table('bhp_dispatch_dispatch', (
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('user_created', self.gf('django.db.models.fields.CharField')(default='', max_length=250)),
+            ('user_modified', self.gf('django.db.models.fields.CharField')(default='', max_length=250)),
+            ('hostname_created', self.gf('django.db.models.fields.CharField')(default='mac.local', max_length=50, db_index=True, blank=True)),
+            ('hostname_modified', self.gf('django.db.models.fields.CharField')(default='mac.local', max_length=50, db_index=True, blank=True)),
+            ('id', self.gf('django.db.models.fields.CharField')(max_length=36, primary_key=True)),
+            ('producer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['bhp_sync.Producer'])),
+            ('is_dispatched', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('dispatched_datetime', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('dispatch_items', self.gf('django.db.models.fields.TextField')(max_length=500)),
+        ))
+        db.send_create_signal('bhp_dispatch', ['Dispatch'])
+
+        # Adding model 'DispatchItem'
+        db.create_table('bhp_dispatch_dispatchitem', (
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('user_created', self.gf('django.db.models.fields.CharField')(default='', max_length=250)),
+            ('user_modified', self.gf('django.db.models.fields.CharField')(default='', max_length=250)),
+            ('hostname_created', self.gf('django.db.models.fields.CharField')(default='mac.local', max_length=50, db_index=True, blank=True)),
+            ('hostname_modified', self.gf('django.db.models.fields.CharField')(default='mac.local', max_length=50, db_index=True, blank=True)),
+            ('id', self.gf('django.db.models.fields.CharField')(max_length=36, primary_key=True)),
+            ('producer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['bhp_sync.Producer'])),
+            ('is_dispatched', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('dispatched_datetime', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('dispatch', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['bhp_dispatch.Dispatch'], null=True)),
+            ('item_identifier', self.gf('django.db.models.fields.CharField')(max_length=25)),
+        ))
+        db.send_create_signal('bhp_dispatch', ['DispatchItem'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Dispatch'
+        db.delete_table('bhp_dispatch_dispatch')
+
+        # Deleting model 'DispatchItem'
+        db.delete_table('bhp_dispatch_dispatchitem')
+
+
+    models = {
+        'bhp_dispatch.dispatch': {
+            'Meta': {'object_name': 'Dispatch'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'dispatch_items': ('django.db.models.fields.TextField', [], {'max_length': '500'}),
+            'dispatched_datetime': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'primary_key': 'True'}),
+            'is_dispatched': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'producer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bhp_sync.Producer']"}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        },
+        'bhp_dispatch.dispatchitem': {
+            'Meta': {'object_name': 'DispatchItem'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'dispatch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bhp_dispatch.Dispatch']", 'null': 'True'}),
+            'dispatched_datetime': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'primary_key': 'True'}),
+            'is_dispatched': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'item_identifier': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'producer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bhp_sync.Producer']"}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        },
+        'bhp_sync.producer': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Producer'},
+            'comment': ('django.db.models.fields.TextField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'mac.local'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'json_limit': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'json_total_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'settings_key': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True'}),
+            'sync_datetime': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'sync_status': ('django.db.models.fields.CharField', [], {'default': "'-'", 'max_length': '250', 'null': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250'})
+        }
+    }
+
+    complete_apps = ['bhp_dispatch']
