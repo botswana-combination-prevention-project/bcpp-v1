5d4c49c5fe0a42c2bc99eed04be4890428ca8c9a
erik van widenfelt <ew2789@gmail.com>
2012-12-02 14:36:20 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@82 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- forms.py
+++ forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class DispatchForm(forms.Form):
     producer = forms.CharField(max_length=100)
-    dispatch_items = forms.Textarea(max_length=500)
+    dispatch_items = forms.Textarea()
