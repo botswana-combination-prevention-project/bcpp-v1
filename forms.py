c3ab34118a499450db709739b59e11b2bad858f3
erik van widenfelt <ew2789@gmail.com>
2012-12-02 17:53:39 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@90 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- forms.py
+++ forms.py
@@ -4,5 +4,6 @@ from bhp_sync.models import Producer
 
 class DispatchForm(forms.Form):
     producer = forms.ModelChoiceField(
-        queryset=Producer.objects.all().order_by('name'))
+        queryset=Producer.objects.all().order_by('name'),
+        required=False)
     dispatch_items = forms.MultipleChoiceField()
