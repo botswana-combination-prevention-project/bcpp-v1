5521905d482a67c9f9089e8ac0a944dbabe38df4
erik van widenfelt <ew2789@gmail.com>
2012-12-04 09:00:11 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_crypto@152 dc6f6662-d1d5-42af-b45d-bfa79074b674

--- admin.py
+++ admin.py
@@ -4,7 +4,8 @@ from bhp_crypto.models import Crypt, TestModel
 
 class CryptAdmin (admin.ModelAdmin):
 
-    list_display = ('hash',)
+    list_display = ('hash', 'created', 'modified')
+    list_filter = ('created', 'modified')
     search_fields = ('hash',)
 
 admin.site.register(Crypt, CryptAdmin)
