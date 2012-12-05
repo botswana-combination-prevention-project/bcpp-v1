59698a9e8b92bae13eefb5bcd4e8f050275256f6
erik van widenfelt <ew2789@gmail.com>
2012-12-05 08:27:24 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_code_lists@25 2d834fd6-8cdc-49e2-960a-59c1fb8158c1

--- models/my_models.py
+++ models/my_models.py
@@ -1,65 +1,76 @@
 from django.db import models
 from base_code_list import BaseCodeList
 
-#dx and ssx        
+
 class WcsDxAdult(BaseCodeList):
-    
+
     """WhoClinicalStagingDxAdult"""
-    
+
     list_ref = models.CharField("List Reference",
-        max_length = 100,
-        blank = True,        
-        )    
+        max_length=100,
+        blank=True,
+        )
 
     class Meta:
         app_label = "bhp_code_lists"
 
+
 class WcsDxPed(BaseCodeList):
-    
+
     """WhoClinicalStagingDxPediatric"""
 
     list_ref = models.CharField("List Reference",
-        max_length = 100,
-        blank = True,
-        )    
+        max_length=100,
+        blank=True,
+        )
 
     class Meta:
         app_label = "bhp_code_lists"
 
+
 class MedicationCode (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
+        max_length=35)
+
     class Meta:
-        app_label="bhp_code_lists"
+        app_label = "bhp_code_lists"
+
 
 class BodySiteCode (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
+        max_length=35)
+
     class Meta:
-        app_label="bhp_code_lists"
+        app_label = "bhp_code_lists"
+
 
 class OrganismCode (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
-    class Meta:
-        app_label="bhp_code_lists"
+        max_length=35)
 
-#ARV medications
+    class Meta:
+        app_label = "bhp_code_lists"
+
+
 class ArvCode (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
+        max_length=35)
+
     class Meta:
-        app_label="bhp_code_lists"
+        app_label = "bhp_code_lists"
+
 
 class ArvDoseStatus (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
+        max_length=35)
+
     class Meta:
-        app_label="bhp_code_lists"
+        app_label = "bhp_code_lists"
+
 
 class ArvModificationCode (BaseCodeList):
     list_ref = models.CharField("List Reference",
-        max_length=35)    
-    class Meta:
-        app_label="bhp_code_lists"
+        max_length=35)
 
+    class Meta:
+        app_label = "bhp_code_lists"
