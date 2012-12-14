86fcb4406a188ea4b3fe171d8e6b08207674e605
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-12-14 13:46:27 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@213 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- views/index.py
+++ views/index.py
@@ -2,19 +2,13 @@ import socket
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
 from bhp_sync.models import Producer
 
 
 @login_required
 def index(request, **kwargs):
-    show_checkin_link = False
-    if settings.ALLOW_MODEL_CHECKOUT:
-        show_checkin_link = True
     producers = Producer.objects.filter(is_active=True)
     return render_to_response('sync.html', {
         'producers': producers,
-        'hostname': socket.gethostname(),
-        'show_checkin_link': show_checkin_link,
-        'CHECKOUT_APP': settings.CHECKOUT_APP
+        'hostname': socket.gethostname()
     }, context_instance=RequestContext(request))
