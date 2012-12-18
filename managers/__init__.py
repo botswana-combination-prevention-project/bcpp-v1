bd39ffa200cc3bf10d7e154cff0beb89e50b770e
panto motshegwa <pmotshegwa@bhp.org.bw>
2012-12-18 13:23:01 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@217 5124d8a1-613d-4708-9420-8c98912e35b2

--- managers/__init__.py
+++ managers/__init__.py
@@ -4,3 +4,4 @@ from aliquot_type_manager import AliquotTypeManager
 from aliquot_condition_manager import AliquotConditionManager
 from order_manager import OrderManager
 from panel_manager import PanelManager
+from panel_group_manager import PanelGroupManager
