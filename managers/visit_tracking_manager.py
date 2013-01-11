7f1338087c27a2dace0cd8df3ae6dcf48a7b2b33
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-01-11 05:42:25 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@23 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

--- managers/visit_tracking_manager.py
+++ managers/visit_tracking_manager.py
@@ -1,22 +1,22 @@
 from django.db import models
-#from bhp_registration.models import RegisteredSubject
-#from bhp_visit.models import VisitDefinition
-#from bhp_appointment.models import Appointment
+from bhp_registration.models import RegisteredSubject
+from bhp_visit.models import VisitDefinition
+from bhp_appointment.models import Appointment
 
 
 class VisitTrackingManager(models.Manager):
-#    def get_by_natural_key(self, visit_instance, visit_definition, subject_identifier):
-#        registered_subject = RegisteredSubject.objects.get(
-#            subject_identifier=subject_identifier
-#            )
-#        visit_definition = VisitDefinition.objects.get(code=visit_definition)
-#
-#        appointment = Appointment.objects.get(
-#            registered_subject=registered_subject,
-#            visit_definition=visit_definition,
-#            visit_instance=visit_instance
-#            )
-#        return self.get(appointment=appointment)
+    def get_by_natural_key(self, visit_instance, visit_definition, subject_identifier):
+        registered_subject = RegisteredSubject.objects.get(
+            subject_identifier=subject_identifier
+            )
+        visit_definition = VisitDefinition.objects.get(code=visit_definition)
+
+        appointment = Appointment.objects.get(
+            registered_subject=registered_subject,
+            visit_definition=visit_definition,
+            visit_instance=visit_instance
+            )
+        return self.get(appointment=appointment)
 
     def get_by_natural_key_with_dict(self, **kwargs):
         return self.get(**kwargs)
