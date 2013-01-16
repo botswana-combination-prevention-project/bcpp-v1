ee47e8615d454a1685eb753ab103cde3748dc636
erik van widenfelt <ew2789@gmail.com>
2013-01-16 13:55:02 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@143 833481ee-4f4c-48f6-8756-2185fbac41e5

---
+++ models/base_consent_update.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from django.core.validators import RegexValidator
+try:
+    from bhp_sync.classes import BaseSyncModel as BaseUuidModel
+except ImportError:
+    from bhp_base_model.classes import BaseUuidModel
+from bhp_common.choices import YES_NO
+from bhp_crypto.fields import EncryptedLastnameField, EncryptedTextField
+from bhp_base_model.validators import datetime_not_future, datetime_not_before_study_start
+from bhp_variables.models import StudySite
+
+
+class BaseConsentUpdate(BaseUuidModel):
+    """Tracks updates to the original consent.
+
+    In the subclass:
+        1. add your consent as a OneToOneField()::
+
+            maternal_consent = models.OneToOneField(MaternalConsent)
+
+        2. add a save method to update the main consent's most recent version number::
+
+            def save(self, *args, **kwargs):
+                #After save, updates the most recent consent version on the "entry" consent.
+                super(MaternalConsentUpdate, self).save(*args, **kwargs)
+                self.maternal_consent.consent_version_recent = self.consent_version
+    """
+
+    study_site = models.ForeignKey(StudySite,
+        verbose_name='Site',
+        help_text="This refers to the site or 'clinic area' where the subject is being consented."
+        )
+
+    consent_datetime = models.DateTimeField("Consent date and time",
+        validators=[
+            datetime_not_before_study_start,
+            datetime_not_future, ],
+        )
+
+    guardian_name = EncryptedLastnameField(
+        verbose_name=_("Guardian\'s Last and first name (minors only)"),
+        validators=[
+            RegexValidator('^[A-Z]{1,50}\,[A-Z]{1,50}$', 'Invalid format. Format is \'LASTNAME,FIRSTNAME\'. All uppercase separated by a comma'),
+            ],
+        blank=True,
+        null=True,
+        help_text=_('Required only if subject is a minor. Format is \'LASTNAME,FIRSTNAME\'. All uppercase separated by a comma'),
+        )
+
+    may_store_samples = models.CharField(
+        verbose_name=_("Sample storage after closure?"),
+        max_length=3,
+        choices=YES_NO,
+        help_text=_("Subject agrees for samples to be stored after the study closure")
+        )
+
+    comment = EncryptedTextField("Comment",
+        max_length=250,
+        blank=True,
+        null=True
+        )
+
+    consent_version = models.IntegerField(
+        editable=False,
+        )
+
+    def save(self, *args, **kwargs):
+        self.consent_version = self.get_current_consent_version(self.consent_datetime)
+        super(BaseConsentUpdate, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
