57decbd19f429e427a659d00e8f006d8e9c30fad
erik van widenfelt <ew2789@gmail.com>
2013-01-18 09:54:23 +0000

    git-svn-id: http://192.168.1.50/svn/lab_reference@46 d0d547b7-54e7-4bd0-bed5-421763f9147c

--- models/base_reference_list_item.py
+++ models/base_reference_list_item.py
@@ -1,5 +1,5 @@
 from django.db import models
-from bhp_base_model.classes import BaseModel
+from bhp_base_model.models import BaseModel
 from bhp_common.choices import POS_NEG_ANY
 from lab_common.choices import UNITS
 from lab_reference.choices import GENDER_OF_REFERENCE
