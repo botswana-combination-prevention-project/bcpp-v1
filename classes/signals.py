fbc21c499cdec751a007302b929bc83ad31b5526
erik van widenfelt <ew2789@gmail.com>
2013-01-18 10:00:15 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@223 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- classes/signals.py
+++ classes/signals.py
@@ -2,7 +2,7 @@ import socket
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from serialize_to_transaction import SerializeToTransaction
-from base_sync_model import BaseSyncModel
+from bhp_sync.models import BaseSyncUuidModel
 
 
 @receiver(m2m_changed, weak=False, dispatch_uid='serialize_m2m_on_save')
@@ -11,7 +11,7 @@ def serialize_m2m_on_save(sender, instance, **kwargs):
     serialized correctly.
     """
     if kwargs.get('action') == 'post_add':
-        if isinstance(instance, BaseSyncModel):
+        if isinstance(instance, BaseSyncUuidModel):
             if instance.is_serialized() and not instance._meta.proxy:
                 serialize_to_transaction = SerializeToTransaction()
                 serialize_to_transaction.serialize(sender, instance, **kwargs)
@@ -22,7 +22,7 @@ def serialize_on_save(sender, instance, **kwargs):
     """ Serialize the model instance to the outgoing transaction
     model for consumption by another application.
     """
-    if isinstance(instance, BaseSyncModel):
+    if isinstance(instance, BaseSyncUuidModel):
         hostname = socket.gethostname()
         if (instance.hostname_created == hostname and not instance.hostname_modified) or (instance.hostname_modified == hostname):
             if instance.is_serialized() and not instance._meta.proxy:
