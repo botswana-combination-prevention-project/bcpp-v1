7ad871da311fafdf11e43d060d8afd70eacf8fb8
erik van widenfelt <ew2789@gmail.com>
2013-01-18 10:00:09 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_actg_reference@8 283b8d84-d4af-4b97-893f-627e6529052c

--- models/base_appendix.py
+++ models/base_appendix.py
@@ -1,21 +1,21 @@
 from django.db import models
-from bhp_base_model.classes import BaseUuidModel
+from bhp_base_model.models import BaseUuidModel
 
 
 class BaseAppendix(BaseUuidModel):
-    
+
     code = models.CharField(
-        max_length = 25,
-        unique = True,
+        max_length=25,
+        unique=True,
         )
-    
+
     short_description = models.CharField(
-        max_length = 300,
+        max_length=300,
         )
-        
+
     full_description = models.TextField(
-        max_length = 600
-        )        
-        
+        max_length=600
+        )
+
     class Meta:
-        abstract = True        
+        abstract = True
