35cb29fed600bc7b182275797684d6098cef0d1a
erik van widenfelt <ew2789@gmail.com>
2013-01-18 09:22:28 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_userprofile@9 286037fc-dbac-43a1-8671-deda17dc685f

--- models/user_profile.py
+++ models/user_profile.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from bhp_base_model.fields import InitialsField
 from bhp_userprofile.choices import SALUTATION
