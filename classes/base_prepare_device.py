99478f97bfced5ccea701e936d3ae50aaad6adb6
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-01-22 08:09:42 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@153 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- classes/base_prepare_device.py
+++ classes/base_prepare_device.py
@@ -7,7 +7,7 @@ from django.db import IntegrityError
 from django.db.models import Model, get_model, get_models, get_app, Max, ForeignKey, OneToOneField
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User, Group, Permission
-from bhp_base_model.classes import BaseListModel
+from bhp_base_model.models import BaseListModel
 from bhp_userprofile.models import UserProfile
 from bhp_content_type_map.classes import ContentTypeMapHelper
 from base import Base
