f73c3ce5938f579b431445bd5dff056572b48834
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-02-14 09:29:09 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@231 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- classes/consumer.py
+++ classes/consumer.py
@@ -79,7 +79,7 @@ class Consumer(object):
         deserialize_from_transaction = DeserializeFromTransaction()
         n = 0
         tot = IncomingTransaction.objects.filter(is_consumed=False).count()
-        for incoming_transaction in IncomingTransaction.objects.filter(is_consumed=False).order_by('producer', 'timestamp'):
+        for incoming_transaction in IncomingTransaction.objects.filter(is_consumed=False, is_ignored=False).order_by('producer', 'timestamp'):
             n += 1
             action = ''
             print '{0} / {1} {2} {3}'.format(n, tot, incoming_transaction.producer, incoming_transaction.tx_name)
