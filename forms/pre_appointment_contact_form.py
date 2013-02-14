51ec3334f8996a6a79df22fb3db6064b14c59e1d
erik van widenfelt <ew2789@gmail.com>
2013-02-14 07:00:17 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@169 0d19f2bb-31f9-471a-9829-a57abbf0ef71

---
+++ forms/pre_appointment_contact_form.py
@@ -0,0 +1,10 @@
+from bhp_base_form.classes import BaseModelForm
+from bhp_appointment.models import PreAppointmentContact
+
+
+class PreAppointmentContactForm (BaseModelForm):
+
+    pass
+
+    class Meta:
+        model = PreAppointmentContact
