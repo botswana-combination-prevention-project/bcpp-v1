b50958fdd3949d4e6658785c7e6e5792fd0f04eb
erik van widenfelt <ew2789@gmail.com>
2013-02-15 06:40:15 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@177 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- admin/pre_appointment_contact_admin.py
+++ admin/pre_appointment_contact_admin.py
@@ -10,12 +10,18 @@ class PreAppointmentContactInlineAdmin(MyTabularInline):
     model = PreAppointmentContact
     form = PreAppointmentContactForm
     extra = 1
+    fields = ('contact_datetime', 'is_contacted', 'information_provider', 'is_confirmed', 'comment')
     radio_fields = {"is_contacted": admin.VERTICAL,
                     "is_confirmed": admin.VERTICAL}
 
 
 class PreAppointmentContactAdmin(BaseModelAdmin):
     form = PreAppointmentContactForm
+    date_hierarchy = 'contact_datetime'
+    fields = ('contact_datetime', 'is_contacted', 'information_provider', 'is_confirmed', 'comment')
+    list_display = ('appointment', 'contact_datetime', 'is_contacted', 'information_provider', 'is_confirmed')
+    list_filter = ('contact_datetime', 'is_contacted', 'information_provider', 'is_confirmed')
+    search_fields = ('appointment__registered_subject__subject_identifier', 'id')
     radio_fields = {"is_contacted": admin.VERTICAL,
                     "is_confirmed": admin.VERTICAL}
 admin.site.register(PreAppointmentContact, PreAppointmentContactAdmin)
