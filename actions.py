9f22c833ff0137a738d62eb675fd549242c65d3a
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-02-20 06:53:07 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@161 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- actions.py
+++ actions.py
@@ -1,8 +1,12 @@
-#from datetime import datetime
-#from bhp_dispatch.models import DispatchItem
-#from bhp_dispatch.classes import DispatchHelper
-#
-#
+def set_is_dispatched(modeladmin, request, queryset, **kwargs):
+    """Sets is dispatched to True"""
+    for qs in queryset:
+        if qs.is_dispatched == False:
+            qs.is_dispatched = True
+            qs.return_datetime = None
+            qs.save()
+set_is_dispatched.short_description = "Set is_dispatched to True."
+
 #def process_dispatch(modeladmin, request, queryset, **kwargs):
 #
 #    """Checkout all selected households to specified netbooks.
