7bd3d5bc19fc01357a02dda57d0bbfc0f6f9bc96
erik van widenfelt <ew2789@gmail.com>
2013-02-21 11:02:12 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@240 3bc1d969-5506-4dd9-9641-5fb303cabccf

---
+++ helpers/transaction_helper.py
@@ -0,0 +1,19 @@
+from bhp_sync.models import OutgoingTransaction, IncomingTransaction
+
+
+class TransactionHelper(object):
+
+    def has_incoming_for_producer(self, producer, using=None):
+        if not using:
+            using = 'default'
+        return IncomingTransaction.objects.using(using).filter(producer=producer, is_consumed=False).exists()
+
+    def has_incoming_for_model(self, models, using=None):
+        if not using:
+            using = 'default'
+        if not isinstance(models, (list, tuple)):
+            models = [models]
+        return IncomingTransaction.objects.using(using).filter(tx_name__in=models, is_consumed=False).exists()
+
+    def has_outgoing(self, using=None):
+        return OutgoingTransaction.objects.using(using).filter(is_consumed=False).exists()
