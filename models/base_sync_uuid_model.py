0d5a923cc94be5f35ea4b095f13afcb621f426a3
erik van widenfelt <ew2789@gmail.com>
2013-02-21 15:24:56 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@242 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- models/base_sync_uuid_model.py
+++ models/base_sync_uuid_model.py
@@ -16,9 +16,6 @@ class NullHandler(logging.Handler):
         pass
 nullhandler = logger.addHandler(NullHandler())
 
-class AlreadyDispatched(Exception):
-    pass
-
 
 class BaseSyncUuidModel(BaseUuidModel):
 
@@ -34,17 +31,29 @@ class BaseSyncUuidModel(BaseUuidModel):
         ..note:: only use this for models that exist in an app listed in the settings.DISPATCH_APP_LABELS but need to be ignored (which should not be very often)."""
         return False
 
-    def get_registered_subject(self):
-        """Primarily needed by bhp_dispatch to return the registered_subject instance.
+    def include_for_dispatch(self):
+        """Flgas a model to be included by the dispatch infrastructure.
 
-        Users must override either this or the container method."""
-        if self.is_dispatch_container_model():
-            return None
+        ..note:: only use this for models that do not exist in an app listed in the settings.DISPATCH_APP_LABELS but need to be included (which should not be very often)."""
+        return False
+
+    def is_dispatchable_model(self):
         if self.ignore_for_dispatch():
-            return None
+            return False
         if not self._meta.app_label in settings.DISPATCH_APP_LABELS:
-            return None
-        raise ImproperlyConfigured('Model method get_registered_subject() not configured. Are you attempting to dispatch? Items included for dispatch must have access to registered_subject through this method.')
+            if self.include_for_dispatch():
+                return True
+            else:
+                return False
+        return True
+
+#    def get_registered_subject(self):
+#        """Primarily needed by bhp_dispatch to return the registered_subject instance.
+#
+#        Users must override either this or the container method."""
+#        if not is_dispatchable_model():
+#            return None
+#        raise ImproperlyConfigured('Model method get_registered_subject() not configured. Are you attempting to dispatch? Items included for dispatch must have access to registered_subject through this method.')
 
     def is_serialized(self, serialize=True):
         if 'ALLOW_MODEL_SERIALIZATION' in dir(settings):
@@ -72,82 +81,62 @@ class BaseSyncUuidModel(BaseUuidModel):
 
     @property
     def is_dispatched(self):
-        """Returns lock status as a boolean needed when using this model with bhp_dispatch."""
-        from bhp_dispatch.exceptions import AlreadyDispatched
-        is_dispatched, producer = self.is_dispatched_to_producer()
-        if is_dispatched:
-            raise AlreadyDispatched('Model instance {0}-{1} is currently dispatched'.format(self._meta.object_name, self.pk))
-        return is_dispatched
+        """Returns True if the model is tracked/is_dispatched=True in the DispatchItem model.
 
-    def unlocking_prep(self):
-        """Override in subclass to run the specific checks for each subclass before unlocking its instance on the server."""
-        pass
+        ..note:: Unlike, :func:`_is_dispatched_to_producer_as_container`, this method does NOT consider
+            DispacthContainer. So if the instance is referred to in DispacthContainer but
+            not yet tracked in the DispatchItem model, the return value is False."""
+        if self.id:
+            if self.is_dispatchable_model():
+                if self.is_dispatched_to_producer():
+                    return True
+        return False
+
+#    def unlocking_prep(self):
+#        """Override in subclass to run the specific checks for each subclass before unlocking its instance on the server."""
+#        pass
 
     def dispatched_as_container_identifier_attr(self):
-        """Override to return the field attrname of the identifier used for dispatch."""
+        """Override to return the field attrname of the identifier used for the dispatch container."""
         raise ImproperlyConfigured('Method must be overridden on model {0}'.format(self._meta.object_name))
 
-#    def is_dispatch_container_model(self):
-#        """Flags a model used as a container model as dispatched.
-#
-#        Override to include on your dispatch container model.
-#        """
-#        return False
-
-    def _is_dispatched_as_container(self):
+    def _is_dispatched_to_producer_as_container(self):
         is_dispatched = False
-        producer = None
         DispatchContainer = get_model('bhp_dispatch', 'DispatchContainer')
-        attrname = self.dispatched_as_container_identifier_attr()
         if DispatchContainer:
-            if DispatchContainer.objects.filter(
-                    container_identifier=getattr(self, attrname),
-                    is_dispatched=True,
-                    return_datetime__isnull=True).exists():
-                dispatch_container = DispatchContainer.objects.get(
-                    container_identifier=getattr(self, attrname),
-                    is_dispatched=True,
-                    return_datetime__isnull=True)
-                producer = dispatch_container.producer
-                is_dispatched = True
-        return (is_dispatched, producer)
+            is_dispatched = DispatchContainer.objects.filter(
+                container_identifier=getattr(self, self.dispatched_as_container_identifier_attr()),
+                is_dispatched=True,
+                return_datetime__isnull=True).exists()
+        return is_dispatched
 
-    def is_dispatched_to_producer(self, registered_subject=None):
+    def is_dispatched_to_producer(self):
         """Returns lock status as a boolean needed when using this model with bhp_dispatch."""
         is_dispatched = False
-        producer = None
-        if self.is_dispatch_container_model():
-            is_dispatched, producer = self._is_dispatched_as_container()
-        else:
-            if not registered_subject:
-                if 'get_registered_subject' in dir(self):
-                    registered_subject = self.get_registered_subject()
-            if registered_subject:
-                DispatchItem = get_model('bhp_dispatch', 'DispatchItem')
-                if DispatchItem and registered_subject:
-                    if DispatchItem.objects.filter(
-                            subject_identifiers__icontains=registered_subject,
-                            is_dispatched=True).exists():
-                        dispatch_item = DispatchItem.objects.get(
-                            subject_identifiers__icontains=registered_subject,
-                            is_dispatched=True)
-                        producer = dispatch_item.producer
-                        is_dispatched = True
-        return (is_dispatched, producer)
+        if self.id:
+            DispatchItem = get_model('bhp_dispatch', 'DispatchItem')
+            if DispatchItem:
+                is_dispatched = DispatchItem.objects.filter(
+                    item_app_label=self._meta.app_label,
+                    item_model_name=self._meta.object_name,
+                    item_pk=self.pk,
+                    is_dispatched=True).exists()
+        return is_dispatched
 
     def save(self, *args, **kwargs):
         if 'transaction_producer' in kwargs:
             #transaction_producer = kwargs.get('transaction_producer')
             del kwargs['transaction_producer']
-        # for bhp_dispatch, catch instances that may not be saved
         if 'bhp_dispatch' in settings.INSTALLED_APPS:
-            is_dispatched, producer = self.is_dispatched_to_producer()
-            if is_dispatched:
-                raise ValidationError('Save not allowed. Model {0} for {1} is currently dispatched to {3}.'
-                                      '(You should catch this in the form validation.)'.format(self._meta.object_name,
-                                                                                               'subject \'{0}\' with registered_subject.pk=\'{1}\''.format(self.get_registered_subject().subject_identifier(),
-                                                                                                                                                           self.get_registered_subject()),
-                                                                                               producer))
+            from bhp_dispatch.exceptions import AlreadyDispatched
+            if self.id:
+                if self.is_dispatchable_model():
+                    if self.is_dispatch_container_model():
+                        if self._is_dispatched_to_producer_as_container():
+                            raise AlreadyDispatched('Model {0}-{1} is currently dispatched as a container for other dispatched items.'.format(self._meta.object_name, self.pk))
+                    if self.is_dispatched_to_producer():
+                        raise AlreadyDispatched('Model {0}-{1} is currently dispatched'.format(self._meta.object_name, self.pk))
+
         super(BaseSyncUuidModel, self).save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
