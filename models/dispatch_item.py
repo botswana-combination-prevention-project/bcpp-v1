c5378bb806d7d9226ad04024bca3a9b19b6f9624
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-02-25 11:02:20 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@193 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- models/dispatch_item.py
+++ models/dispatch_item.py
@@ -21,33 +21,22 @@ class DispatchItem(BaseDispatch):
         )
     objects = models.Manager()
 
-#    def run_pre_unlocking_checks(self):
-#        consumer = Consumer()
-#        consumer.check_all_synched_from_producer(self.dispatch_container.producer.name)
-#        consumer.check_all_consumed_in_server(self.dispatch_container.producer.name)
-#
-#    def unlock_dispatch_item(self):
-#        self.is_dispatched = False
-#        self.save()
 
-    def unlock_dispatch_item(self):
-        self.is_dispatched = False
-        self.save()
 
 # temp removed - erikvw (fails on unknown producer when setting dispatched to False)
 # no longer necessary to check if the instance is dispatched, as this is done by
 # the controller class.
-#    def save(self, *args, **kwargs):
-#        """Confirms an instance does not exist for this item_identifier."""
-#        if self.__class__.objects.filter(
-#                item_identifier=self.item_identifier,
-#                is_dispatched=True,
-#                ).exclude(pk=self.pk).exists():
-#            dispatch_item = self.__class__.objects.get(
-#                item_identifier=self.item_identifier,
-#                is_dispatched=True,
-#                ).exclude(pk=self.pk)
-#            raise ValueError("Cannot dispatch. The item \'{0}\' is already dispatched to \'{1}\'.".format(dispatch_item.item_identifier, dispatch_item.dispatch_container.producer))
+    def save(self, *args, **kwargs):
+        """Confirms an instance does not exist for this item_identifier."""
+        if self.__class__.objects.filter(
+                item_identifier=self.item_identifier,
+                is_dispatched=True,
+                ).exclude(pk=self.pk).exists():
+            dispatch_item = self.__class__.objects.get(
+                item_identifier=self.item_identifier,
+                is_dispatched=True,
+                ).exclude(pk=self.pk)
+            raise ValueError("Cannot dispatch. The item \'{0}\' is already dispatched to \'{1}\'.".format(dispatch_item.item_identifier, dispatch_item.dispatch_container.producer))
 #        if not self.is_dispatched:
 #            self.run_pre_unlocking_checks()
         super(DispatchItem, self).save(*args, **kwargs)
