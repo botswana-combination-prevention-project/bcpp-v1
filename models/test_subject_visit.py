c8201c890c2fbea245c748122e8d549cba3e8ccf
erik van widenfelt <ew2789@gmail.com>
2013-02-25 11:18:27 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@40 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

--- models/test_subject_visit.py
+++ models/test_subject_visit.py
@@ -3,5 +3,8 @@ from base_visit_tracking import BaseVisitTracking
 
 class TestSubjectVisit(BaseVisitTracking):
 
+    def get_requires_consent(self):
+        return False
+
     class Meta:
         app_label = 'bhp_visit_tracking'
