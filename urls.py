79fbe278bf749a46919176920fd7a6475ec9caf0
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-02-25 12:10:04 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@245 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- urls.py
+++ urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
 urlpatterns += patterns('bhp_sync.views',
     # fetch unsent transactions from a producer (GET)
     url(r'^consume/(?P<producer>[a-z0-9\-\_\.]+)/', 'consume_transactions',),
-    url(r'^view/(?P<model_name>incomingtransaction)/(?P<pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/', 'view_transaction', name='view_transaction_url'),
-    url(r'^', 'index',),
+    url(r'^view/(?P<model_name>incomingtransaction)/(?P<pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/', 'view_transaction', name='view_transaction_url',),
+    url(r'^$', 'index',),
     )
