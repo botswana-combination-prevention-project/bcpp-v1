ad8bd653d1382a4507b31029df859662ad27808a
erik van widenfelt <ew2789@gmail.com>
2013-02-25 11:18:19 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@210 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/base_registered_subject_model.py
+++ models/base_registered_subject_model.py
@@ -25,13 +25,24 @@ class BaseRegisteredSubjectModel (BaseAppointmentHelperModel):
         """Returns the subject_identifier."""
         return self.registered_subject.subject_identifier
 
-    def get_visit_model_cls(self):
+    def get_visit_model_app(self):
+        """Returns the app that contains the visit model for the RARE case where it is not in the same app as this model."""
+        return None
+
+    def get_visit_model_cls(self, instance=None):
         """Returns the visit model which is a subclass of :class:`BaseVisitTracking`."""
         from bhp_visit_tracking.models.base_visit_tracking import BaseVisitTracking
-        for model in get_models(get_app(self._meta.app_label)):
+        if not instance:
+            instance = self
+        if instance.get_visit_model_app():
+            app_label = instance.get_visit_model_app()
+        else:
+            app_label = instance._meta.app_label
+        for model in get_models(get_app(app_label)):
             if isinstance(model(), BaseVisitTracking):
                 return model
-        raise TypeError('Unable to determine the visit model for app {1} from instance {0}. Needed to clear future appointments.'.format(self._meta.model_name, self._meta.app_label))
+        raise TypeError('Unable to determine the visit model for app {1} from instance {0}. Visit model and Off Study model are expected to be in the same app. '
+                        'If not use model method \'get_visit_model_app()\''.format(instance._meta.object_name, instance._meta.app_label))
 
     def get_visit_model(self, instance):
         """Returns the visit model which is a subclass of :class:`BaseVisitTracking`."""
@@ -39,7 +50,7 @@ class BaseRegisteredSubjectModel (BaseAppointmentHelperModel):
         for model in get_models(get_app(instance._meta.app_label)):
             if isinstance(model(), BaseVisitTracking):
                 return model
-        raise TypeError('Unable to determine the visit model from instance {0} for app {1}'.format(instance._meta.model_name, instance._meta.app_label))
+        raise TypeError('Unable to determine the visit model from instance {0} for app {1}'.format(instance._meta.object_name, instance._meta.app_label))
 
     def get_registered_subject(self):
         return self.registered_subject
