7fd76d11b0982a6b7c7610c49cf7fe9cc304a9fc
erik van widenfelt <ew2789@gmail.com>
2013-02-25 10:13:41 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_off_study@12 7aed250d-f3cc-4085-b232-d4bc89341959

---
+++ models/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from base_off_study import BaseOffStudy
+
+
+@receiver(post_save, weak=False, dispatch_uid="base_off_study_post_save")
+def base_off_study_post_save(sender, instance, **kwargs):
+    """Calls post_save method."""
+    if isinstance(instance, BaseOffStudy):
+        instance.post_save()
