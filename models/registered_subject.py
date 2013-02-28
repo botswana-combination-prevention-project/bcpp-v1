0070dbbf16c05809aef43d1b4905bdc161d830af
erik van widenfelt <ew2789@gmail.com>
2013-02-28 11:07:39 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@217 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/registered_subject.py
+++ models/registered_subject.py
@@ -119,8 +119,6 @@ class RegisteredSubject(BaseSubject):
 
     def natural_key(self):
         return (self.identity, self.first_name, self.dob, self.initials, self.registration_identifier, self.subject_identifier, )
-    #def natural_key(self):
-        #return (self.subject_identifier, )
 
     def is_serialized(self):
         return super(RegisteredSubject, self).is_serialized(True)
