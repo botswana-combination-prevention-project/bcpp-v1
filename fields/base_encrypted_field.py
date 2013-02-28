9aefb8bfa5d458557e92b49e9800ab435bd26f40
erik van widenfelt <ew2789@gmail.com>
2013-02-28 10:37:47 +0000

    refactor admin and forms
    git-svn-id: http://192.168.1.50/svn/bhp_crypto@159 dc6f6662-d1d5-42af-b45d-bfa79074b674

--- fields/base_encrypted_field.py
+++ fields/base_encrypted_field.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from field_cryptor import FieldCryptor
+from bhp_crypto.classes import FieldCryptor
 
 
 class BaseEncryptedField(models.Field):
