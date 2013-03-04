b5af485b6aa4e09670e4d7be9b7d9f31ea54a156
erik van widenfelt <ew2789@gmail.com>
2013-03-04 12:52:57 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_content_type_map@22 179f8c61-186b-4a6f-a3fc-937048475198

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from actions import pop_and_sync
 
 
 class ContentTypeMapAdmin(MyModelAdmin):
-    list_display = ('name', 'content_type', 'model', 'app_label')
+    list_display = ('name', 'content_type', 'id', 'model', 'app_label')
     search_fields = ('name', 'app_label', 'model')
     list_filter = ('app_label',)
     actions = [pop_and_sync]
