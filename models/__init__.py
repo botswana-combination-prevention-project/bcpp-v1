215c997210059a2977f5ff859dac1954b9172b8e
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-03-14 16:04:19 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_lab_tracker@77 5f62ce6e-2ac5-4da8-806c-9e97803e0885

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from history_model import HistoryModel
+from base_history_model import BaseHistoryModel
 from history_model_error import HistoryModelError
 from default_value_log import DefaultValueLog
 from signals import *
--- models/history_model.py
+++ models/history_model.py
@@ -5,7 +5,7 @@ from base_history_model import BaseHistoryModel
 class HistoryModel(BaseHistoryModel):
 
     objects = models.Manager()
-
+    
     class Meta:
         app_label = 'bhp_lab_tracker'
         #unique_together = (('subject_identifier', 'group_name', 'test_code', 'value_datetime'), ('source', 'source_identifier'), )
