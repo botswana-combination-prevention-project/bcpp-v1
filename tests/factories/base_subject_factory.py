0ca9924805a03baee6913d8ad9b89f6d0423bf1d
erik van widenfelt <ew2789@gmail.com>
2013-03-17 23:08:36 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_subject@21 f056b393-b364-42d3-9b15-75ddb560184a

---
+++ tests/factories/base_subject_factory.py
@@ -0,0 +1,18 @@
+import factory
+from datetime import date
+from django.conf import settings
+from bhp_base_model.tests.factories import BaseUuidModelFactory
+
+starting_seq_num = 1000
+
+
+class BaseSubjectFactory(BaseUuidModelFactory):
+    ABSTRACT_FACTORY = True
+
+    subject_identifier = factory.Sequence(lambda n: '{0}-{1}{2}-0'.format(settings.PROJECT_IDENTIFIER_PREFIX, settings.DEVICE_ID, n.rjust(5, '0')))
+    first_name = 'ERIK'
+    initials = 'EV'
+    dob = date(date.today().year - 20, 1, 1)
+    is_dob_estimated = '-'
+    gender = 'M'
+    subject_type = 'subject'
