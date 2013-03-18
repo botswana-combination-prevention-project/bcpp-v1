754873b0e25f2bf40601470f15bff28586831fc7
erik van widenfelt <ew2789@gmail.com>
2013-03-18 14:19:45 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_subject@23 f056b393-b364-42d3-9b15-75ddb560184a

--- tests/factories/base_subject_factory.py
+++ tests/factories/base_subject_factory.py
@@ -9,7 +9,9 @@ starting_seq_num = 1000
 class BaseSubjectFactory(BaseUuidModelFactory):
     ABSTRACT_FACTORY = True
 
-    subject_identifier = factory.Sequence(lambda n: '{0}-{1}{2}-0'.format(settings.PROJECT_IDENTIFIER_PREFIX, settings.DEVICE_ID, n.rjust(5, '0')))
+    # if you set subject_identifier here, filling in a consent, for example, will not generate 
+    # an identifier and not update registered subject
+    #subject_identifier = factory.Sequence(lambda n: '{0}-{1}{2}-0'.format(settings.PROJECT_IDENTIFIER_PREFIX, settings.DEVICE_ID, n.rjust(5, '0')))
     first_name = 'ERIK'
     initials = 'EV'
     dob = date(date.today().year - 20, 1, 1)
