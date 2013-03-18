216033ee6d592b1dda86995e2d98324ec05ee810
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-03-18 06:22:34 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@270 3bc1d969-5506-4dd9-9641-5fb303cabccf

---
+++ admin/__init__.py
@@ -0,0 +1,5 @@
+from admin import TransactionAdmin
+from admin import RequestLogAdmin
+from incoming_transaction_admin import IncomingTransactionAdmin
+from outgoing_transaction_admin import OutgoingTransactionAdmin
+from producer_admin import ProducerAdmin
