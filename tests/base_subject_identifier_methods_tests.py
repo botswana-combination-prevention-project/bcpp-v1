4537cf7ed4db3187da2dab202297ba8de810f981
erik van widenfelt <ew2789@gmail.com>
2013-03-19 22:58:22 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_identifier@91 f71f0ad2-c473-4a78-a10b-80415aeb8028

--- tests/base_subject_identifier_methods_tests.py
+++ tests/base_subject_identifier_methods_tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
+from django.conf import settings
 from bhp_identifier.classes import BaseSubjectIdentifier
 from bhp_identifier.exceptions import CheckDigitError, IdentifierEncodingError, IdentifierDecodingError
 
 
 class BaseSubjectIdentifierMethodsTests(TestCase):
-    
-    subject_identifier = BaseSubjectIdentifier()
-    print subject_identifier.get_identifier()
+
+    def test_p1(self):
+        x = 0
+        while x < 50:
+            subject_identifier = BaseSubjectIdentifier()
+            print subject_identifier.get_identifier()
+            self.assertTrue(subject_identifier.get_identifier().startswith(settings.PROJECT_IDENTIFIER_PREFIX))
+            self.assertTrue(subject_identifier.get_identifier().startswith(settings.PROJECT_IDENTIFIER_PREFIX + '-' + settings.DEVICE_ID))
+            x += 1
