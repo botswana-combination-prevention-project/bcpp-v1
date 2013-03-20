0da14eb835b2d2bf242513a57a064494b1ce51f4
erik van widenfelt <ew2789@gmail.com>
2013-03-20 07:05:00 +0000

    fix natural key
    git-svn-id: http://192.168.1.50/svn/bhp_appointment@201 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- managers/appointment_manager.py
+++ managers/appointment_manager.py
@@ -5,12 +5,8 @@ from django.db import models
 
 class AppointmentManager(models.Manager):
 
-    def get_by_natural_key(self, visit_instance, code, identity, first_name, dob, initials, subject_identifier):
+    def get_by_natural_key(self, visit_instance, code, subject_identifier):
         return self.get(
-            registered_subject__identity=identity,
-            registered_subject__first_name=first_name,
-            registered_subject__dob=dob,
-            registered_subject__initials=initials,
             registered_subject__subject_identifier=subject_identifier,
             visit_definition__code=code,
             visit_instance=visit_instance
