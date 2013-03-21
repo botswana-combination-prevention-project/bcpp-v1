80525409979009a56654b95f6997cbcca3e714cc
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-03-21 09:47:51 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@203 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- managers/appointment_manager.py
+++ managers/appointment_manager.py
@@ -5,9 +5,10 @@ from django.db import models
 
 class AppointmentManager(models.Manager):
 
-    def get_by_natural_key(self, visit_instance, code, subject_identifier):
+    def get_by_natural_key(self, visit_instance, appt_status, code, subject_identifier):
         return self.get(
             registered_subject__subject_identifier=subject_identifier,
             visit_definition__code=code,
-            visit_instance=visit_instance
+            visit_instance=visit_instance,
+            appt_status=appt_status,
             )
