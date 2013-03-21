18002666fa1379c1256aa76f958ce925a1bd52fa
erik van widenfelt <ew2789@gmail.com>
2013-03-21 18:20:40 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dashboard_registered_subject@119 af6e21aa-e2a9-484a-a4e1-7d0be244359e

--- tests/dashboard_tests.py
+++ tests/dashboard_tests.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
+from bhp_content_type_map.classes import ContentTypeMapHelper
+
 from bhp_registration.tests.factories import RegisteredSubjectFactory
 from bhp_registration.models import RegisteredSubject
 from bhp_consent.models import TestSubjectConsent
@@ -11,19 +13,39 @@ from bhp_content_type_map.tests.factories import ContentTypeMapFactory
 from bhp_visit.tests.factories import VisitDefinitionFactory, ScheduleGroupFactory, MembershipFormFactory
 from bhp_visit.models import MembershipForm, ScheduleGroup, VisitDefinition
 from bhp_dashboard_registered_subject.classes import RegisteredSubjectDashboard
+from bhp_lab_tracker.classes import lab_tracker
+from lab_requisition.models import TestRequisition
+from bhp_visit_tracking.models import TestSubjectVisit
 
 
 class DashboardTests(TestCase):
 
     def test_p1(self):
+        lab_tracker.autodiscover()
+        content_type_map_helper = ContentTypeMapHelper()
+        content_type_map_helper.populate()
+        content_type_map_helper.sync()
         # setup consent
         # setup bhp_visit
         content_type = ContentType.objects.get(app_label='bhp_consent', model='testsubjectconsent')
-        content_type_map = ContentTypeMapFactory(content_type=content_type)
-        membership_form = MembershipFormFactory(content_type_map=content_type_map)
+        content_type_map = ContentTypeMap.objects.get(content_type=content_type)
+        #content_type_map = ContentTypeMapFactory(content_type=content_type)
+        membership_form = MembershipFormFactory(content_type_map=content_type_map, category='subject')
         schedule_group = ScheduleGroupFactory(membership_form=membership_form)
         visit_definition = VisitDefinitionFactory()
         visit_definition.schedule_group.add(schedule_group)
-        rs = RegisteredSubject()
+        rs = RegisteredSubjectFactory(subject_type='subject')
+        
+        #create appointments
+        
+        
         dash = RegisteredSubjectDashboard()
+        print rs.subject_identifier
+        dash.create(dashboard_type='subject',
+                    dashboard_identifier=rs.subject_identifier,
+                    registered_subject=rs,
+                    requisition_model=TestRequisition,
+                    visit_model=TestSubjectVisit)
         dash._set_appointments(visit_code=visit_definition.code, visit_instance=None)
+        print dash._get_membership_form_category()
+        
