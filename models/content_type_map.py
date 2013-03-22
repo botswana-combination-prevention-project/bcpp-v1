7a4f56f8a4ef2d5dc593c3416b1991af57ae1fbc
erik van widenfelt <ew2789@gmail.com>
2013-03-22 06:34:57 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_content_type_map@25 179f8c61-186b-4a6f-a3fc-937048475198

--- models/content_type_map.py
+++ models/content_type_map.py
@@ -36,19 +36,10 @@ class ContentTypeMap(BaseModel):
         return (self.app_label, self.model, )
 
     def model_class(self):
-
-        """
         if not self.content_type.name == self.name:
-            c = ContentTypeMapManager()
-            c.sync()
+            raise TypeError('ContentTypeMap is not in sync with ContentType for verbose_name %s. Run sync_content_type management command.' % self.name)
         if not self.content_type.model == self.model:
-            c = ContentTypeMapManager()
-            c.sync()
-        """
-        if not self.content_type.name == self.name:
-            raise TypeError('ContentTypeMap is not in sync with ContentType for verbose_name %s' % self.name)
-        if not self.content_type.model == self.model:
-            raise TypeError('ContentTypeMap is not in sync with ContentType for model %s' % self.model)
+            raise TypeError('ContentTypeMap is not in sync with ContentType for model %s. Run sync_content_type management command.' % self.model)
 
         return self.content_type.model_class()
 
