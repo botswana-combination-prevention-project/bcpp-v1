b3f6158012500670e5cff3c27082820d1be33d84
erik van widenfelt <ew2789@gmail.com>
2013-03-22 09:32:22 +0000

    fix get_by_natural_key, appt_status
    git-svn-id: http://192.168.1.50/svn/bhp_appointment@205 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- managers/appointment_manager.py
+++ managers/appointment_manager.py
@@ -1,14 +1,14 @@
 from django.db import models
-#from bhp_visit.models import VisitDefinition
-#from bhp_registration.models import RegisteredSubject
 
 
 class AppointmentManager(models.Manager):
-
-    def get_by_natural_key(self, visit_instance, appt_status, code, subject_identifier):
+    def get_by_natural_key(self, visit_instance, appt_status, visit_definition_code, subject_identifier_as_pk):
+        RegisteredSubject = models.get_model('bhp_registration', 'RegisteredSubject')
+        VisitDefinition = models.get_model('bhp_visit', 'VisitDefinition')
+        registered_subject = RegisteredSubject.objects.get_by_natural_key(subject_identifier_as_pk)
+        visit_definition = VisitDefinition.objects.get_by_natural_key(visit_definition_code)
         return self.get(
-            registered_subject__subject_identifier=subject_identifier,
-            visit_definition__code=code,
+            registered_subject=registered_subject,
+            visit_definition=visit_definition,
             visit_instance=visit_instance,
-            appt_status=appt_status,
-            )
+            appt_status=appt_status)
