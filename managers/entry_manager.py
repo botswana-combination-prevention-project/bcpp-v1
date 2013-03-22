809a4890e4f29ce1840f23b6099784466faa6931
erik van widenfelt <ew2789@gmail.com>
2013-03-22 09:34:17 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_entry@74 ed14d539-c731-4f84-8017-1f76c29e69c2

--- managers/entry_manager.py
+++ managers/entry_manager.py
@@ -7,6 +7,6 @@ class EntryBucketManager(models.Manager):
 
     def get_by_natural_key(self, visit_definition_code, app_label, model):
         """Returns the instance using the natural key."""
-        visit_definition = VisitDefinition.objects.get_by_natural_key(code=visit_definition_code)
-        content_map_type = ContentTypeMap.objects.get_by_natural_key(app_label=app_label, model=model)
+        visit_definition = VisitDefinition.objects.get_by_natural_key(visit_definition_code)
+        content_map_type = ContentTypeMap.objects.get_by_natural_key(app_label, model)
         return self.get(content_map_type=content_map_type, visit_definition=visit_definition)
