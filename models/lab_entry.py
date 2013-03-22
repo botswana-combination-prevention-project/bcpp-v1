0a4484017ff70362cdc5dc6bd60c1aa7d3e78113
erik van widenfelt <ew2789@gmail.com>
2013-03-22 10:03:32 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_lab_entry@26 ecd7a240-9b5c-4260-8afc-8fffe4488176

--- models/lab_entry.py
+++ models/lab_entry.py
@@ -10,7 +10,6 @@ class LabEntry(BaseLabEntry):
     This model lists entry forms by visit definition used to fill
     the scheduled entry bucket for a subject once a visit is reported
 
-    Important: this is linked to / depends on the ContentTypeMap
     """
 
     visit_definition = models.ForeignKey(VisitDefinition)
