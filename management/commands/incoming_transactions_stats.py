fd776ceca8936ad5e456afb09bc90890a203f060
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-03-26 12:26:41 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@282 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- management/commands/incoming_transactions_stats.py
+++ management/commands/incoming_transactions_stats.py
@@ -1,7 +1,6 @@
 from optparse import make_option
 from django.db.models import Count
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
 from bhp_sync.models import IncomingTransaction
 from bhp_lab_tracker.classes import lab_tracker
 
@@ -24,11 +23,13 @@ class Command(BaseCommand):
         return IncomingTransaction.objects.values('tx_name').filter(is_consumed=False,is_ignored=False).annotate(tx_count=Count('tx_name'))
         
     def print_stats (self, stats):
-        print "\nPrint a summary of unconsumed incoming transactions\n"
-        print "Total:\t:Transaction name"
-        print "__________________________"
-
-        for stat in stats:
-            print "{0}\t:{1}".format(stat['tx_count'],stat['tx_name'])
-        print "\t -------------------\t"
-
+        tot = stats.count()    
+        print "\nA summary of unconsumed incoming transactions\n"  
+        print "\nA {0} Incoming transactions\n".format(tot)
+        if stats:
+            print "Total:\t:Transaction name"
+            print "__________________________"
+    
+            for stat in stats:
+                print "{0}\t:{1}".format(stat['tx_count'],stat['tx_name'])
+            print "\t -------------------\t"
