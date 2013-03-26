87b752981c0fc1a009bd442c0cb782cc91027b28
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-03-26 10:33:27 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@277 3bc1d969-5506-4dd9-9641-5fb303cabccf

---
+++ management/commands/incoming_transactions_stats.py
@@ -0,0 +1,30 @@
+from optparse import make_option
+from django.db.models import Count
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from bhp_sync.models import IncomingTransaction
+from bhp_lab_tracker.classes import lab_tracker
+
+lab_tracker.autodiscover()
+
+
+class Command(BaseCommand):
+
+    args = ()
+    help = 'Print uncomsumed incoming transactions breakdown by tx_name. '
+
+    def handle(self, *args, **options):
+        if not args:
+            args = [None]
+        stats = self.prepare_stats()
+        
+
+    def prepare_stats(self):
+        return IncomingTransaction.objects.values('tx_name').filter(is_consumed=False,is_ignored=False).annotate(tx_count=Count('tx_name'))
+        
+    def print_stats (self, stats):
+        print "Total:\t:Transaction name"
+
+        for stat in stats:
+            print "{0}\t:{1}".format(stat['tx_count'],stat['tx_name'])
+
