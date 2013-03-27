b75ca347d1e7ffc273ee5d97b3a14d8732033a98
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-03-27 14:40:21 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@334 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- helpers/is_dispatched.py
+++ helpers/is_dispatched.py
@@ -1,12 +1,11 @@
-#from bhp_dispatch.models import DispatchItem
-#
-#
-#def is_dispatched(item_identifier):
-#        """Returns dispatch status if the item based on the identifier."""
-#        raise TypeError('Method is not used. Access the model method.')
-#        locked = False
-#        if DispatchItem.objects.filter(
-#                item_identifier=item_identifier,
-#                is_dispatched=True).exists():
-#            locked = True
-#        return locked
+from bhp_dispatch.models import DispatchContainerRegister
+
+
+def is_dispatched(container_identifier):
+    """Returns dispatch status if the item based on the container_identifier."""
+    locked = False
+    if DispatchContainerRegister.objects.filter(
+            container_identifier=container_identifier,
+            is_dispatched=True).exists():
+        locked = True
+    return locked
