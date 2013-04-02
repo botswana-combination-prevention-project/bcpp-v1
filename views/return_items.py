3f68723fb29ec132bfca7990d2a9b0edc908d1e5
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-04-02 08:25:45 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@341 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- views/return_items.py
+++ views/return_items.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 #from django.core.exceptions import ObjectDoesNotExist
 #from django.http import Http404
-from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from bhp_sync.models import Producer
@@ -13,15 +12,12 @@ from bhp_dispatch.classes import ReturnController
 def return_items(request, **kwargs):
     """ Return items from the producer to the source."""
     msg = None
-    if settings.DEVICE_ID != '99':
-        msg = 'Cannot UNLOCK households from a NETBOOK'
-    else:
-        producer = Producer.objects.get(name__iexact=kwargs.get('producer', None))
-        container_model = request.GET.getlist('container_model')
-        if producer and request.GET.getlist(container_model[0],None):
-            msg = ReturnController('default', producer.name).return_selected_items(request.GET.getlist(container_model[0]))
-        elif producer:
-            msg = ReturnController('default', producer.name).return_dispatched_items()
+    producer = Producer.objects.get(name__iexact=kwargs.get('producer', None))
+    container_model = request.GET.getlist('container_model')
+    if producer and request.GET.getlist(container_model[0],None):
+        msg = ReturnController('default', producer.name).return_selected_items(request.GET.getlist(container_model[0]))
+    elif producer:
+        msg = ReturnController('default', producer.name).return_dispatched_items()
     messages.add_message(request, messages.INFO, msg)
     return render_to_response(
         'checkin_households.html', {'producer': producer, },
