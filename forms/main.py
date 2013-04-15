4a2b730ebe4ab0a0066a3aa7d8458e00430a5e4a
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-15 15:32:36 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@7 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -1,6 +1,6 @@
 from django import forms
 from base_subject_model_form import BaseSubjectModelForm
-from bcpp_subject.models import SubjectLocator
+from bcpp_subject.models import SubjectLocator, SubjectDeath, RecentPartner, SecondPartner, ThirdPartner
 
 
 # SubjectLocator
@@ -8,3 +8,46 @@ class SubjectLocatorForm (BaseSubjectModelForm):
 
     class Meta:
         model = SubjectLocator
+
+
+# SubjectDeath
+class SubjectDeathForm (BaseSubjectModelForm):
+
+    class Meta:
+        model = SubjectDeath
+
+
+#RecentPartner
+class RecentPartnerForm (BaseSubjectModelForm):
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+
+        return cleaned_data
+
+    class Meta:
+        model = RecentPartner
+
+
+# SecondPartner
+class SecondPartnerForm (BaseSubjectModelForm):
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+
+        return cleaned_data
+
+    class Meta:
+        model = SecondPartner
+
+
+# ThirdPartner
+class ThirdPartnerForm (BaseSubjectModelForm):
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+
+        return cleaned_data
+
+    class Meta:
+        model = ThirdPartner
