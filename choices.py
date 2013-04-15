d12c1fd4cabf3bc99335aeb82569c224d006d41a
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-15 15:32:28 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@6 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- choices.py
+++ choices.py
@@ -4,3 +4,32 @@ VISIT_UNSCHEDULED_REASON = (
     ('Patient called', 'Patient called to come for visit'),
     ('OTHER', 'Other, specify:'),
 )
+
+
+RELATIONSHIP_TYPE = (
+    ('Longterm partner', 'Longterm partner (>2 years) or spouse'),
+    ('Boyfriend/Girlfriend', 'Boyfriend/Girlfriend'),
+    ('Casual', 'Casual (known) partner'),
+    ('One time partner', 'One time partner (previously unknown)'),
+    ('Commercial sex worker', 'Commercial sex worker'),
+    ('Other, specify', 'Other, specify'), 
+)
+
+MAIN_PARTNER_RESIDENCY = (
+    ('In this community', 'In this community'),
+    ('On farm/cattle post', 'On farm/cattle post'),
+    ('Outside this community', 'Outside this community'),
+    ('Don\'t want to answer', 'Don\'t want to answer'),
+)
+
+SEX_REGULARITY = (
+    ('All of the time', 'All of the time'),
+    ('Sometimes', 'Sometimes'),
+    ('Never', 'Never'),
+)
+
+INTERCOURSE_TYPE = (
+    ('Vaginal', 'Vaginal sex'),
+    ('Anal', 'Anal sex'),
+    ('Both', 'Both vaginal and anal sex'),
+)
