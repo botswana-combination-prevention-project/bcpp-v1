2c43bbc116b007a96aae3b71dd8fcae1bc3f77a0
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-16 14:37:13 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@14 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,15 @@ from subject_locator import SubjectLocator
 from subject_death import SubjectDeath
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
+#CS003
 from recent_partner import RecentPartner
 from second_partner import SecondPartner
 from third_partner import ThirdPartner
+
+#CE001
+from quality_of_life import QualityOfLife
+from resource_utilization import ResourceUtilization
+from outpatient_care import OutpatientCare
+from hospital_admission import HospitalAdmission
+from hiv_health_care_costs import HivHealthCareCosts
+from labour_market_wages import LabourMarketWages, Grant
