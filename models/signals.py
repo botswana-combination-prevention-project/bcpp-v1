f16ea88a9185b922f1052f06b6dbd67aba115606
erik van widenfelt <ew2789@gmail.com>
2013-04-16 10:19:42 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_appointment@210 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- models/signals.py
+++ models/signals.py
@@ -3,14 +3,14 @@ from django.dispatch import receiver
 from pre_appointment_contact import PreAppointmentContact
 
 
-@receiver(post_save, weak=False, dispatch_uid="PreAppointmentContact_on_post_save")
+@receiver(post_save, weak=False, dispatch_uid="pre_appointment_contact_on_post_save")
 def pre_appointment_contact_on_post_save(sender, instance, **kwargs):
     """Calls post_save method which will only call save."""
     if isinstance(instance, PreAppointmentContact):
         instance.post_save()
 
 
-@receiver(post_delete, weak=False, dispatch_uid="PreAppointmentContact_on_post_delete")
+@receiver(post_delete, weak=False, dispatch_uid="pre_appointment_contact_on_post_delete")
 def pre_appointment_contact_on_post_delete(sender, instance, **kwargs):
     """Calls post_delete method."""
     if isinstance(instance, PreAppointmentContact):
