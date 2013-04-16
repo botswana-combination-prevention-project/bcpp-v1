709c4a9c23ee481b17b764d74f25de844aa0ad37
erik van widenfelt <ew2789@gmail.com>
2013-04-16 10:39:31 +0000

    rename signal
    git-svn-id: http://192.168.1.50/svn/bhp_subject@31 f056b393-b364-42d3-9b15-75ddb560184a

--- models/signals.py
+++ models/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from base_subject import BaseSubject
 
 
-@receiver(post_save, weak=False, dispatch_uid='base_subject_on_post_save')
-def base_subject_on_post_save(sender, instance, **kwargs):
+@receiver(post_save, weak=False, dispatch_uid='base_subject_get_or_create_registered_subject_on_post_save')
+def base_subject_get_or_create_registered_subject_on_post_save(sender, instance, **kwargs):
     if isinstance(instance, (BaseSubject)):
         instance.post_save_get_or_create_registered_subject()
