84ef5d033e75288ffc1ec1df9823b11556607a2f
erik van widenfelt <ew2789@gmail.com>
2013-04-16 10:13:46 +0000

    add management command for signals
    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@345 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- classes/signal_manager.py
+++ classes/signal_manager.py
@@ -4,6 +4,7 @@ from bhp_sync.models.signals import serialize_on_save, serialize_m2m_on_save
 from bhp_visit_tracking.models.signals import base_visit_tracking_add_or_update_entry_buckets_on_post_save, base_visit_tracking_on_post_save
 from bhp_lab_tracker.models.signals import tracker_on_post_save
 from bhp_consent.models.signals import is_consented_instance_on_pre_save
+from bhp_subject.models.signals import base_subject_on_post_save
 
 
 class SignalManager(object):
@@ -17,11 +18,13 @@ class SignalManager(object):
         signals.post_save.disconnect(tracker_on_post_save, weak=False, dispatch_uid="tracker_on_post_save")
         signals.post_save.disconnect(base_visit_tracking_add_or_update_entry_buckets_on_post_save, weak=False, dispatch_uid="base_visit_tracking_add_or_update_entry_buckets_on_post_save")
         signals.post_save.disconnect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_on_post_save")
+        signals.pre_save.disconnect(base_subject_on_post_save, weak=False, dispatch_uid="base_subject_on_post_save")
         signals.pre_save.disconnect(is_consented_instance_on_pre_save, weak=False, dispatch_uid="is_consented_instance_on_pre_save")
         self.disconnect_audit_trail_signals(obj)
 
     def reconnect(self):
         signals.pre_save.connect(is_consented_instance_on_pre_save, weak=False, dispatch_uid="is_consented_instance_on_pre_save")
+        signals.pre_save.connect(base_subject_on_post_save, weak=False, dispatch_uid="base_subject_on_post_save")
         signals.post_save.connect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_on_post_save")
         signals.post_save.connect(base_visit_tracking_add_or_update_entry_buckets_on_post_save, weak=False, dispatch_uid="base_visit_tracking_add_or_update_entry_buckets_on_post_save")
         signals.post_save.connect(tracker_on_post_save, weak=False, dispatch_uid="tracker_on_post_save")
