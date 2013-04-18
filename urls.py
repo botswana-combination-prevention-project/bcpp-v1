e9eeb18aad7bd40b14eee7224777e229d034373c
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-18 13:25:14 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_dashboard@2 6d3fde56-e3b9-4da8-b725-c35ce0fc21a7

--- urls.py
+++ urls.py
@@ -4,10 +4,10 @@ admin.autodiscover()
 
 regex = {}
 regex['dashboard_type'] = 'subject'
-regex['subject_identifier'] = '066-[0-9]{9,11}-[0-9]{1}'
+regex['subject_identifier'] = '066\-[0-9]{6,11}\-[0-9]{1}'
 regex['visit_code'] = '\w+'
 regex['visit_instance'] = '[0-9]{1}'
 subject_dashboard = SubjectDashboard()
 urlpatterns = subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit'])
 # regex['subject_identifier'] = '066-[0-9]{3}-[0-9]{4}'
-# urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
+# urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit'])
