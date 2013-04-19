b5dab3fc3fff84c4cc429cc47fe0622e6a75c22b
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-19 15:31:23 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@12 674fa781-39cf-41df-9450-c12409787013

---
+++ models/neighbourhood_problems.py
@@ -0,0 +1,9 @@
+from bhp_base_model.models import BaseListModel
+
+
+class NeighbourhoodProblems (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Neighbourhood Problems"
