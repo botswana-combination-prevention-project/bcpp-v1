b13d617dbe2c443fda19c8b79a0e3bbce56619a3
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-19 15:31:57 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@15 674fa781-39cf-41df-9450-c12409787013

--- admin/main.py
+++ admin/main.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from bhp_base_model.classes import BaseModelAdmin
-from bcpp_list.models import ElectricalAppliances, TransportMode
+from bcpp_list.models import ElectricalAppliances, TransportMode, LiveWith, NeighbourhoodProblems
 
 
 class ElectricalAppliancesAdmin(BaseModelAdmin):
@@ -11,3 +11,13 @@ admin.site.register(ElectricalAppliances, ElectricalAppliancesAdmin)
 class TransportModeAdmin(BaseModelAdmin):
     pass
 admin.site.register(TransportMode, TransportModeAdmin)
+
+
+class LiveWithAdmin(BaseModelAdmin):
+    pass
+admin.site.register(LiveWith, LiveWithAdmin)
+
+
+class NeighbourhoodProblemsAdmin(BaseModelAdmin):
+    pass
+admin.site.register(NeighbourhoodProblems, NeighbourhoodProblemsAdmin)
