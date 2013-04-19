4b2be1b60f374d16e13f8c99d027af661619101f
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-19 15:31:14 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@11 674fa781-39cf-41df-9450-c12409787013

---
+++ models/live_with.py
@@ -0,0 +1,9 @@
+from bhp_base_model.models import BaseListModel
+
+
+class LiveWith (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Living With"
