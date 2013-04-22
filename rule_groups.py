baa2080b284a1acca3b9b283a0a5ea80bb9a495b
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-22 12:21:04 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@49 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- rule_groups.py
+++ rule_groups.py
@@ -1,6 +1,6 @@
 from bhp_entry_rules.classes import RuleGroup, rule_groups, ScheduledDataRule, AdditionalDataRule, Logic
-# from bhp_registration.models import RegisteredSubject
-from models import SubjectVisit, ResourceUtilization, HivTestingHistory, SexualBehaviour
+from bhp_registration.models import RegisteredSubject
+from models import SubjectVisit, ResourceUtilization, HivTestingHistory, SexualBehaviour, MonthsRecentPartner, MonthsSecondPartner, HivCareAdherence, Circumcision
 
 
 class ResourceUtilizationRuleGroup(RuleGroup):
@@ -64,3 +64,98 @@ class SexualBehaviourRuleGroup(RuleGroup):
         source_model = SexualBehaviour
 rule_groups.register(SexualBehaviourRuleGroup)
 
+
+class SexualPartnersRuleGroup(RuleGroup):
+    
+    concurrent = ScheduledDataRule(
+        logic=Logic(
+            predicate=('concurrent', 'equals', 'Yes'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['monthssecondpartner'])
+    
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = MonthsRecentPartner
+rule_groups.register(SexualPartnersRuleGroup)
+    
+
+class MoreSexualPartnersRuleGroup(RuleGroup):
+    
+    concurrent = ScheduledDataRule(
+        logic=Logic(
+            predicate=('concurrent', 'equals', 'Yes'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['monthsthirdpartner'])
+    
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = MonthsSecondPartner
+rule_groups.register(MoreSexualPartnersRuleGroup)
+
+
+class MedicalCareRuleGroup(RuleGroup):
+    
+    medical_care = ScheduledDataRule(
+        logic=Logic(
+            predicate=('medical_care', 'equals', 'Yes'),
+            consequence='new',
+            alternative='not_required'),
+        target_model=['hivmedicalcare'])
+    
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = HivCareAdherence
+rule_groups.register(MedicalCareRuleGroup)
+
+
+class MaleCircumcisionRuleGroup(RuleGroup):
+
+    gender = ScheduledDataRule(
+        logic=Logic(
+            predicate=('gender', 'equals', 'f'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['circumcision', 'circumcised', 'uncircumcised'])
+
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = RegisteredSubject
+rule_groups.register(MaleCircumcisionRuleGroup)
+
+
+class CircumcisionRuleGroup(RuleGroup):
+    
+    circumcised = ScheduledDataRule(
+        logic=Logic(
+            predicate=('circumcised', 'equals', 'Yes'),
+            consequence='new',
+            alternative='not_required'),
+        target_model=['circumcised'])
+    
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = Circumcision
+rule_groups.register(CircumcisionRuleGroup)
+
+
+class NoCircumcisionRuleGroup(RuleGroup):
+    
+    circumcised = ScheduledDataRule(
+        logic=Logic(
+            predicate=('circumcised', 'equals', 'No'),
+            consequence='new',
+            alternative='not_required'),
+        target_model=['uncircumcised'])
+    
+    class Meta:
+        app_label = 'bcpp_subject'
+        filter_model = (SubjectVisit, 'subject_visit')
+        source_model = Circumcision
+rule_groups.register(NoCircumcisionRuleGroup)
