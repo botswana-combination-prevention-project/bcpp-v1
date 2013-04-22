410df3876c8f00606c367146a866a33a3f4f1d55
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-22 12:20:15 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@23 674fa781-39cf-41df-9450-c12409787013

--- forms/main.py
+++ forms/main.py
@@ -1,5 +1,5 @@
 from bhp_base_form.classes import BaseModelForm
-from bcpp_list.models import ElectricalAppliances, TransportMode, LiveWith, NeighbourhoodProblems
+from bcpp_list.models import ElectricalAppliances, TransportMode, LiveWith, NeighbourhoodProblems, CicumcisionBenefits
 
 
 # ElectricalAppliances
@@ -49,3 +49,15 @@ class NeighbourhoodProblemsForm (BaseModelForm):
 
     class Meta:
         model = NeighbourhoodProblems
+
+
+#CicumcisionBenefits
+class CicumcisionBenefitsForm (BaseModelForm):
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        return cleaned_data
+
+    class Meta:
+        model = CicumcisionBenefits
