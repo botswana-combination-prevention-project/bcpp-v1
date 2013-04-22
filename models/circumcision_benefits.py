e9d4965382296c53efb3652f1c60d3a77d309934
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-22 12:19:56 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@21 674fa781-39cf-41df-9450-c12409787013

---
+++ models/circumcision_benefits.py
@@ -0,0 +1,10 @@
+from bhp_base_model.models import BaseListModel
+
+
+class CicumcisionBenefits (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Cicumcision Benefits"
+        verbose_name_plural = "Cicumcision Benefits"
