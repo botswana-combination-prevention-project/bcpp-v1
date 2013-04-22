7ce576301724a50d9a434e212e0e22a7f78365c3
erik van widenfelt <ew2789@gmail.com>
2013-04-22 09:00:03 +0000

    add trap for missing site code
    git-svn-id: http://192.168.1.50/svn/bhp_identifier@95 f71f0ad2-c473-4a78-a10b-80415aeb8028

--- classes/base_subject_identifier.py
+++ classes/base_subject_identifier.py
@@ -12,6 +12,9 @@ class BaseSubjectIdentifier(BaseIdentifier):
         app_name = app_name or 'bhp_identifier'
         model_name = model_name or 'subjectidentifier'
         identifier_format = identifier_format or "{identifier_prefix}-{site_code}{device_id}{sequence}"
+        if 'site_code' in identifier_format:
+            if not site_code:
+                raise ImproperlyConfigured('Attribute site_code cannot be None for a subject identifier. Specify the site_code when instantiating the identifier class.')
         super(BaseSubjectIdentifier, self).__init__(identifier_format=identifier_format,
                                                     app_name=app_name,
                                                     model_name=model_name,
--- tests/base_subject_identifier_methods_tests.py
+++ tests/base_subject_identifier_methods_tests.py
@@ -7,20 +7,21 @@ from bhp_identifier.exceptions import CheckDigitError, IdentifierEncodingError, 
 class BaseSubjectIdentifierMethodsTests(TestCase):
 
     def test_p1(self):
+        site_code = '20'
         x = 0
         while x < 50:
-            subject_identifier = BaseSubjectIdentifier()
+            subject_identifier = BaseSubjectIdentifier(site_code=site_code)
             print subject_identifier.get_identifier()
             self.assertTrue(subject_identifier.get_identifier().startswith(settings.PROJECT_IDENTIFIER_PREFIX))
-            self.assertTrue(subject_identifier.get_identifier().startswith(settings.PROJECT_IDENTIFIER_PREFIX + '-' + settings.DEVICE_ID))
+            self.assertTrue(subject_identifier.get_identifier().startswith('{0}-{1}{2}'.format(settings.PROJECT_IDENTIFIER_PREFIX, site_code, settings.DEVICE_ID)))
             x += 1
 
         print 'assert raises error if format has fewer keys that the values dictionary'
         subject_identifier = BaseSubjectIdentifier(identifier_format='{prefix}')
         self.assertRaises(IndentifierFormatError, subject_identifier.get_identifier)
         print 'assert raises error if format has more keys that the values dictionary'
-        subject_identifier = BaseSubjectIdentifier(identifier_format='{identifier_prefix}-{site_code}{device_id}{sequence}{prefix}')
+        subject_identifier = BaseSubjectIdentifier(identifier_format='{identifier_prefix}-{site_code}{device_id}{sequence}{prefix}', site_code=site_code)
         self.assertRaises(IndentifierFormatError, subject_identifier.get_identifier)
         print 'assert raises error if get_identifier gets a value not in the format'
-        subject_identifier = BaseSubjectIdentifier(identifier_format='{identifier_prefix}-{site_code}{device_id}{sequence}{prefix}')
+        subject_identifier = BaseSubjectIdentifier(identifier_format='{identifier_prefix}-{site_code}{device_id}{sequence}{prefix}', site_code=site_code)
         self.assertRaises(IndentifierFormatError, subject_identifier.get_identifier, erik=32)
