644e89448d9dede9e352550b50f35482551d05b5
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-23 10:14:18 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@31 674fa781-39cf-41df-9450-c12409787013

---
+++ models/medical_care_access.py
@@ -0,0 +1,10 @@
+from bhp_base_model.models import BaseListModel
+
+
+class MedicalCareAccess (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Medical Care Access"
+        verbose_name_plural = "Medical Care Access"
