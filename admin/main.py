045acf2a4fdedf116d670888d82db4a8ad66935b
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-25 14:13:07 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@48 674fa781-39cf-41df-9450-c12409787013

--- admin/main.py
+++ admin/main.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from bhp_base_model.classes import BaseModelAdmin
-from bcpp_list.models import ElectricalAppliances, TransportMode, LiveWith, NeighbourhoodProblems, CicumcisionBenefits, FamilyPlanning, MedicalCareAccess
+from bcpp_list.models import ElectricalAppliances, TransportMode, LiveWith, NeighbourhoodProblems, CicumcisionBenefits, FamilyPlanning, MedicalCareAccess, HouseholdSurveyCode, HouseholdSurveyReason, HouseholdSurveySource, SurveyGroup, HouseholdStructureRelation, HouseholdSurveyStatus, SubjectAbsenteeReason, SubjectMovedReason
 
 
 class ElectricalAppliancesAdmin(BaseModelAdmin):
@@ -36,3 +36,43 @@ admin.site.register(FamilyPlanning, FamilyPlanningAdmin)
 class MedicalCareAccessAdmin(BaseModelAdmin):
     pass
 admin.site.register(MedicalCareAccess, MedicalCareAccessAdmin)
+
+
+class HouseholdSurveyCodeAdmin(BaseModelAdmin):
+    pass
+admin.site.register(HouseholdSurveyCode, HouseholdSurveyCodeAdmin)
+
+
+class HouseholdSurveyReasonAdmin(BaseModelAdmin):
+    pass
+admin.site.register(HouseholdSurveyReason, HouseholdSurveyReasonAdmin)
+
+
+class HouseholdSurveySourceAdmin(BaseModelAdmin):
+    pass
+admin.site.register(HouseholdSurveySource, HouseholdSurveySourceAdmin)
+
+
+class SurveyGroupAdmin(BaseModelAdmin):
+    pass
+admin.site.register(SurveyGroup, SurveyGroupAdmin)
+
+
+class HouseholdStructureRelationAdmin(BaseModelAdmin):
+    pass
+admin.site.register(HouseholdStructureRelation, HouseholdStructureRelationAdmin)
+
+
+class HouseholdSurveyStatusAdmin(BaseModelAdmin):
+    pass
+admin.site.register(HouseholdSurveyStatus, HouseholdSurveyStatusAdmin)
+
+
+class SubjectAbsenteeReasonAdmin(BaseModelAdmin):
+    pass
+admin.site.register(SubjectAbsenteeReason, SubjectAbsenteeReasonAdmin)
+
+
+class SubjectMovedReasonAdmin(BaseModelAdmin):
+    pass
+admin.site.register(SubjectMovedReason, SubjectMovedReasonAdmin)
