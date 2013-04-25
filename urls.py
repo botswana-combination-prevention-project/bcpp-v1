f7d8f63f638884f668a4b1668312994b63906244
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-25 15:22:08 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_dashboard@6 6d3fde56-e3b9-4da8-b725-c35ce0fc21a7

--- urls.py
+++ urls.py
@@ -1,13 +1,48 @@
 from django.contrib import admin
-from bcpp_dashboard.classes import SubjectDashboard
+from django.conf.urls.defaults import patterns, url
+
+from bcpp_dashboard.classes import SubjectDashboard, HouseholdDashboard
+
 admin.autodiscover()
 
 regex = {}
 regex['dashboard_type'] = 'subject'
-regex['subject_identifier'] = '066\-[0-9]{6,11}\-[0-9]{1}'
-regex['visit_code'] = '\w+'
+regex['subject_identifier'] = 'S[0-9]{6}\-[0-9]{2}\-[A-Z]{3}[0-9]{2}'
+regex['visit_code'] = '[A-Z0-9]+'
 regex['visit_instance'] = '[0-9]{1}'
 subject_dashboard = SubjectDashboard()
-urlpatterns = subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit'])
-# regex['subject_identifier'] = '066-[0-9]{3}-[0-9]{4}'
-# urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit'])
+urlpatterns = subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
+
+regex = {}
+regex['dashboard_type'] = 'subject'
+regex['subject_identifier'] = 'S[0-9]{3}\-[0-9]{3}'
+regex['visit_code'] = '[A-Z0-9]+'
+regex['visit_instance'] = '[0-9]{1}'
+subject_dashboard = SubjectDashboard()
+urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
+
+regex = {}
+regex['dashboard_type'] = 'subject'
+regex['subject_identifier'] = '066\-[0-9]{6,8}\-[0-9]{1}'
+regex['visit_code'] = '[A-Z0-9]+'
+regex['visit_instance'] = '[0-9]{1}'
+subject_dashboard = SubjectDashboard()
+urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
+
+
+regex = {}
+regex['dashboard_type'] = 'subject'
+regex['subject_identifier'] = '[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}'
+regex['visit_code'] = '[A-Z0-9]+'
+regex['visit_instance'] = '[0-9]{1}'
+subject_dashboard = SubjectDashboard()
+urlpatterns += subject_dashboard.get_urlpatterns('bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
+
+regex = {}
+regex['dashboard_type'] = 'household'
+regex['household_identifier'] = 'H[0-9]{3,6}\-[0-9]{1,3}'
+household_dashboard = HouseholdDashboard()
+urlpatterns += household_dashboard.get_urlpatterns('bcpp_dashboard.views', regex,)
+
+urlpatterns += patterns('bcpp_dashboard.views',
+    url(r'participation/', 'participation',))
