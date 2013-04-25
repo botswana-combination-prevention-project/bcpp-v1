87016db8bac0ea4bd120b5dcbee1f08d91ea0672
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-25 14:12:39 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@46 674fa781-39cf-41df-9450-c12409787013

---
+++ models/household_survey_code.py
@@ -0,0 +1,10 @@
+from bhp_base_model.models import BaseListModel
+
+
+class HouseholdSurveyCode (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Household Survey Code"
+        verbose_name_plural = "Household Survey Code"
