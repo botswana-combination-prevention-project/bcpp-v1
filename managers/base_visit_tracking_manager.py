705513901c6ba826a769cb6da6f0c916f288da16
erik van widenfelt <ew2789@gmail.com>
2013-04-25 11:00:19 +0000

    manager reportdatetime
    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@57 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

--- managers/base_visit_tracking_manager.py
+++ managers/base_visit_tracking_manager.py
@@ -8,7 +8,8 @@ class BaseVisitTrackingManager(models.Manager):
 
     def get_by_natural_key(self, report_datetime, visit_instance, appt_status, visit_definition_code, subject_identifier_as_pk):
         # deserialized date follows ECMA-262 specification which has less precision than that reported by mysql
-        report_datetime = dateutil.parser.parse(report_datetime)
+        if isinstance(report_datetime, basestring):
+            report_datetime = dateutil.parser.parse(report_datetime)
         margin = timedelta(microseconds=999)
         appointment = Appointment.objects.get_by_natural_key(visit_instance, appt_status, visit_definition_code, subject_identifier_as_pk)
         return self.get(report_datetime__range=(report_datetime - margin, report_datetime + margin), appointment=appointment)
