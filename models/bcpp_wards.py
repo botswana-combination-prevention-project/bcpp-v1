02706601eb43e9cd6030602c6b054a6af7ba4846
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-04-25 14:11:44 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@42 674fa781-39cf-41df-9450-c12409787013

---
+++ models/bcpp_wards.py
@@ -0,0 +1,11 @@
+from bhp_base_model.models import BaseListModel
+
+
+class BcppWards (BaseListModel):
+    pass
+
+    class Meta:
+        app_label = "bcpp_list"
+        verbose_name = "Bcpp Wards"
+        verbose_name_plural = "Bcpp Wards"
+
