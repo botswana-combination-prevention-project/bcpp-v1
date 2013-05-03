efbb4c053df8e6eb0a5fe379464947d10b4d71d1
erik van widenfelt <ew2789@gmail.com>
2013-05-03 06:51:22 +0000

    admin
    git-svn-id: http://192.168.1.50/svn/lab_order@30 f6c41d23-262b-4918-a957-6735cdbbd5c9

--- admin/order_admin.py
+++ admin/order_admin.py
@@ -34,7 +34,7 @@ class OrderAdmin(AutocompleteAdmin, BaseModelAdmin):
             return self.readonly_fields
     fields = ('order_datetime', 'panel', 'aliquot', 'comment', 'dmis_reference', )
     list_display = ('order_identifier', 'order_datetime', 'panel', 'aliquot', 'dmis_reference')
-    list_filter = ('order_datetime', 'panel', 'protocol')
+    list_filter = ('order_datetime', 'panel', )
     search_fields = ['order_identifier', 'dmis_reference', 'aliquot__aliquot_identifier', 'aliquot__receive__receive_identifier', 'aliquot__receive__protocol__protocol_identifier',]
     date_hierarchy = 'order_datetime'
 admin.site.register(Order, OrderAdmin)
