8c588ff2a7b9929f225adcc7f187c423f1572fa4
erik van widenfelt <ew2789@gmail.com>
2013-05-06 13:49:39 +0000

    handle visit and RS on instance for edit in admin
    git-svn-id: http://192.168.1.50/svn/bhp_base_form@11 5a461858-af1e-4e6f-938f-33a009643501

--- classes/base_model_form.py
+++ classes/base_model_form.py
@@ -11,6 +11,19 @@ class BaseModelForm(forms.ModelForm):
 
         super(BaseModelForm, self).__init__(*args, **kwargs)
         self.logic = LogicCheck(self._meta.model)
+        if 'get_visit' in dir(self.instance):
+            if self.instance.get_visit():
+                try:
+                    attr = self.instance.get_visit()._meta.object_name.lower()
+                    self.fields[attr].queryset = self.instance.get_visit().__class__.objects.filter(pk=self.instance.subject_visit.pk)
+                except KeyError:
+                    pass
+        if 'registered_subject' in dir(self.instance):
+            try:
+                if self.instance.registered_subject:
+                    self.fields['registered_subject'].queryset = self.instance.registered_subject.__class__.objects.filter(pk=self.instance.registered_subject.pk)
+            except:
+                pass
 
     def get_subject_identifier(self, cleaned_data):
         subject_identifier = None
