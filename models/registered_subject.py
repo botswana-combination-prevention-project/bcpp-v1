2236ad9d2620cd39b5099c212dda3f9c5995f785
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-05-08 11:25:02 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_registration@240 a1c8eae8-7d53-4db9-beb7-be5090263015

--- models/registered_subject.py
+++ models/registered_subject.py
@@ -135,7 +135,7 @@ class RegisteredSubject(BaseSubject):
         # requery myself
         obj = self.__class__.objects.get(pk=self.pk)
         #dont allow values in these fields to change if dispatched
-        may_not_change_these_fields = [(k, v) for k, v in obj.__dict__.iteritems() if k not in ['study_site_id','registration_status', 'modified', 'identity']]
+        may_not_change_these_fields = [(k, v) for k, v in obj.__dict__.iteritems() if k not in ['study_site_id','registration_status', 'modified']]
         for k, v in may_not_change_these_fields:
             if k[0] != '_':
                 if getattr(self, k) != v:
