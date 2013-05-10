a9ab3f4f952344350be085815e2d5f7542152fc4
erik van widenfelt <ew2789@gmail.com>
2013-05-10 06:19:04 +0000

    admin and forms base import
    git-svn-id: http://192.168.1.50/svn/bhp_model_describer@24 7242b4bf-b897-43ef-8160-6f6da1028fca

--- admin.py
+++ admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-from bhp_common.models import MyModelAdmin
+from bhp_base_admin.admin import BaseModelAdmin
 from bhp_model_describer.models import Related, GroupingHint
 
-class RelatedAdmin(MyModelAdmin):
-    
+
+class RelatedAdmin(BaseModelAdmin):
+
     list_filter = ('app_label', 'model_name')
 
     list_display = ('app_label', 'model_name', 'field_name', 'related_to_model', 'related_to_field_name')
 admin.site.register(Related, RelatedAdmin)
 
-class GroupingHintAdmin(MyModelAdmin):
-    
+
+class GroupingHintAdmin(BaseModelAdmin):
+
     list_filter = ('app_label', 'model_name')
 
     list_display = ('app_label', 'model_name', 'field_name')
