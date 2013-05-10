7a372418c4b0ea25973834cb8a28cf21cd08c667
erik van widenfelt <ew2789@gmail.com>
2013-05-10 06:20:35 +0000

    admin and forms base import
    git-svn-id: http://192.168.1.50/svn/bhp_content_type_map@27 179f8c61-186b-4a6f-a3fc-937048475198

--- admin.py
+++ admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from bhp_common.models import MyModelAdmin
+from bhp_base_admin.admin import BaseModelAdmin
 from models import ContentTypeMap
 from actions import pop_and_sync
 
 
-class ContentTypeMapAdmin(MyModelAdmin):
+class ContentTypeMapAdmin(BaseModelAdmin):
     list_display = ('name', 'content_type', 'id', 'model', 'app_label')
     search_fields = ('name', 'app_label', 'model')
     list_filter = ('app_label',)
