457d2cab8847a8e19c1e7737b433c1054d1e0a74
erik van widenfelt <ew2789@gmail.com>
2013-05-10 06:23:08 +0000

    admin and forms base import
    git-svn-id: http://192.168.1.50/svn/lab_aliquot_list@17 89cbf902-e6a2-4b1b-b6b2-68351f3bfdfa

--- admin.py
+++ admin.py
@@ -1,22 +1,15 @@
-from datetime import *
 from django.contrib import admin
-from bhp_common.models import MyModelAdmin, MyStackedInline
+from bhp_base_admin.admin import BaseModelAdmin
 from models import AliquotCondition, AliquotType
 
-class AliquotTypeAdmin(MyModelAdmin):
 
+class AliquotTypeAdmin(BaseModelAdmin):
     list_display = ('alpha_code', 'numeric_code', 'name', 'created', 'modified')
-    
     ordering = ['name']
-    
-admin.site.register(AliquotType,AliquotTypeAdmin)
+admin.site.register(AliquotType, AliquotTypeAdmin)
 
 
-class AliquotConditionAdmin(MyModelAdmin):
-
+class AliquotConditionAdmin(BaseModelAdmin):
     list_display = ('display_index', 'name', 'short_name', 'field_name', 'created', 'modified')
-
     ordering = ['display_index']
-
-admin.site.register(AliquotCondition,AliquotConditionAdmin)
-
+admin.site.register(AliquotCondition, AliquotConditionAdmin)
