8cb2b3dbda92543767aed8d30de1cf006c2eb0ea
erik van widenfelt <ew2789@gmail.com>
2013-05-15 22:19:22 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_admin@2 eb779c7d-545f-4eec-81f9-8628f4f85584

--- admin/base_model_admin.py
+++ admin/base_model_admin.py
@@ -98,3 +98,41 @@ class BaseModelAdmin (admin.ModelAdmin):
                 except:
                     raise
         return http_response_redirect
+
+    def add_view(self, request, *args, **kwargs):
+        """ Deletes the session key, since we want the user to be directed to all listings
+        after a save on a new object.
+
+        from http://djangosnippets.org/snippets/2849/
+        """
+        result = super(BaseModelAdmin, self).add_view(request, *args, **kwargs)
+        request.session['filtered'] = None
+        return result
+    """
+    Used to redirect users back to their filtered list of locations if there were any
+    """
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """ Saves the referer of the page to return to the filtered
+        change_list after saving the page.
+
+        from http://djangosnippets.org/snippets/2849/
+        """
+        result = super(BaseModelAdmin, self).change_view(request, object_id, form_url, extra_context)
+        # Look at the referer for a query string '^.*\?.*$'
+        ref = request.META.get('HTTP_REFERER', '')
+        if ref.find('?') != -1:
+            # We've got a query string, set the session value
+            request.session['filtered'] = ref
+        if '_save' in request.POST:
+            """
+            We only kick into action if we've saved and if
+            there is a session key of 'filtered', then we
+            delete the key.
+            """
+            try:
+                if request.session['filtered'] is not None:
+                    result['Location'] = request.session['filtered']
+                    request.session['filtered'] = None
+            except:
+                pass
+        return result
