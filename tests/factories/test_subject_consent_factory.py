8a2b8434b2ccfda1493bfe63638c4ce055143208
erik van widenfelt <ew2789@gmail.com>
2013-05-26 18:31:36 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@255 833481ee-4f4c-48f6-8756-2185fbac41e5

--- tests/factories/test_subject_consent_factory.py
+++ tests/factories/test_subject_consent_factory.py
@@ -13,6 +13,8 @@ class TestSubjectConsentFactory(BaseConsentFactory):
     identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
     identity_type = factory.Iterator(IDENTITY_TYPE, getter=lambda c: c[0])
     confirm_identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    first_name = factory.Sequence(lambda n: 'ERIK{0}'.format(n))
+    initials = factory.Sequence(lambda n: 'E{0}W'.format(n))
 
 
 class TestSubjectConsentNoRSFactory(BaseConsentFactory):
@@ -22,3 +24,5 @@ class TestSubjectConsentNoRSFactory(BaseConsentFactory):
     identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
     identity_type = factory.Iterator(IDENTITY_TYPE, getter=lambda c: c[0])
     confirm_identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    first_name = factory.Sequence(lambda n: 'JASON{0}'.format(n))
+    initials = factory.Sequence(lambda n: 'JASON{0}W'.format(n))
