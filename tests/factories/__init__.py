53a59c3dea65e28fa4c201ae58f4a0294c1bd353
erik van widenfelt <ew2789@gmail.com>
2013-05-26 18:32:32 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_test@4 b80f8652-8d5a-498f-a521-a51f72815f6d

---
+++ tests/factories/__init__.py
@@ -0,0 +1,4 @@
+from test_consent_factory import TestConsentFactory
+from test_base_off_study_factory import TestBaseOffStudyFactory
+from test_visit_factory import TestVisitFactory
+from test_registration_factory import TestRegistrationFactory
---
+++ tests/factories/test_consent_factory.py
@@ -0,0 +1,17 @@
+import factory
+from bhp_base_test.models import TestConsent
+from bhp_consent.tests.factories import BaseConsentFactory
+from bhp_registration.tests.factories import RegisteredSubjectFactory
+from bhp_common.choices import IDENTITY_TYPE
+
+
+class TestConsentFactory(BaseConsentFactory):
+    FACTORY_FOR = TestConsent
+
+    registered_subject = factory.SubFactory(RegisteredSubjectFactory)
+    user_provided_subject_identifier = None
+    identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    identity_type = factory.Iterator(IDENTITY_TYPE, getter=lambda c: c[0])
+    confirm_identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    first_name = factory.Sequence(lambda n: 'ERIK{0}'.format(n))
+    initials = factory.Sequence(lambda n: 'E{0}W'.format(n))
