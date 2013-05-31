e3da97609241629369b427521bec0ef1878147b2
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-05-31 14:47:31 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@93 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/base_membership_form.py
+++ forms/base_membership_form.py
@@ -1,5 +1,5 @@
 from django import forms
-from bhp_base_form.classes import BaseModelForm
+from bhp_base_form.forms import BaseModelForm
 from bcpp_subject.models import SubjectConsentYearOne, SubjectConsentYearTwo, SubjectConsentYearThree, SubjectConsentYearFour, SubjectConsentYearFive
 
 
