9a97f02e72afc5dd9a58a0e90af7566f1c0d9928
erik van widenfelt <ew2789@gmail.com>
2013-06-05 19:10:13 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_model@45 69677513-6300-4d4b-a1c0-2a4462c8f59b

--- validators/date.py
+++ validators/date.py
@@ -5,14 +5,14 @@ from bhp_variables.models import StudySpecific
 
 def IsTodayDateTime (value):
     pass
-    
+
 def datetime_not_future (value):
     time_error = timedelta(minutes=10)
     if value > datetime.now() + time_error:
         raise ValidationError(u'Date cannot be a future date and time. You entered %s' % (value,))
 
 def date_not_future (value):
-    now  = date.today()
+    now = date.today()
     if value > now:
         raise ValidationError(u'Date cannot be a future date. You entered %s' % (value,))
 
@@ -22,27 +22,31 @@ def datetime_is_future (value):
         raise ValidationError(u'Date must be a future date and time. You entered %s' % (value,))
 
 def date_is_future (value):
-    now  = date.today()
+    now = date.today()
     if value < now:
         raise ValidationError(u'Date must be a future date. You entered %s' % (value,))
-    
+
 def datetime_is_after_consent (value):
     """ not working..."""
-    now  = value
+    now = value
     if value != now:
         raise ValidationError(u'Date and time cannot be prior to consent date. You entered %s' % (value,))
 
-def date_not_before_study_start (value):
-    dte = StudySpecific.objects.all()[0]
-    value_datetime = datetime(value.year, value.month, value.day, 0,0)
-    started  = dte.study_start_datetime
-    protocol_number = dte.protocol_number
+
+def date_not_before_study_start(value):
+    if StudySpecific.objects.all().count() == 0:
+        raise ValidationError('bhp_variables form StudySpecific has not been completed. Please do this first in admin section bhp_variables.')
+    study_specific = StudySpecific.objects.all()[0]
+    value_datetime = datetime(value.year, value.month, value.day, 0, 0)
+    started = study_specific.study_start_datetime
+    protocol_number = study_specific.protocol_number
     if value_datetime < started:
         raise ValidationError(u'Date cannot be before the study started. %s started on %s. You entered %s.' % (protocol_number, started, value,))
 
+
 def datetime_not_before_study_start (value):
     dte = StudySpecific.objects.all()[0]
-    started  = dte.study_start_datetime
+    started = dte.study_start_datetime
     protocol_number = dte.protocol_number
     if value < started:
         raise ValidationError(u'Date and time cannot be before the study started. %s started on %s. You entered %s.' % (protocol_number, started, value,))
