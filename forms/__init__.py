510da098ee6b408f4fbcd76816f1b7f3b782450a
erik van widenfelt <ew2789@gmail.com>
2013-06-10 16:52:10 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_search@53 76f19ef0-b40f-46b0-95e1-91d90dd81aff

--- forms/__init__.py
+++ forms/__init__.py
@@ -1,1 +1,4 @@
-from main import *
+from search_form import SearchForm
+from date_range_search_form import DateRangeSearchForm
+from week_number_search_form import WeekNumberSearchForm
+from week_year_search_form import WeekYearSearchForm
