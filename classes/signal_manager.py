0013752f524fa7aa4dfc10f40dd466020b94b988
erik van widenfelt <ew2789@gmail.com>
2013-06-12 06:04:56 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_dispatch@366 f4864c45-e38a-4eae-8a0a-e093f1d4e12c

--- classes/signal_manager.py
+++ classes/signal_manager.py
@@ -1,7 +1,7 @@
 from django.db.models import signals
 from bhp_base_model.models import BaseModel
 from bhp_sync.models.signals import serialize_on_save, serialize_m2m_on_save
-from bhp_visit_tracking.models.signals import base_visit_tracking_add_or_update_entry_buckets_on_post_save, base_visit_tracking_on_post_save
+from bhp_visit_tracking.models.signals import base_visit_tracking_add_or_update_entry_buckets_on_post_save #  , base_visit_tracking_on_post_save
 from bhp_lab_tracker.models.signals import tracker_on_post_save
 from bhp_consent.models.signals import is_consented_instance_on_pre_save
 from bhp_subject.models.signals import base_subject_get_or_create_registered_subject_on_post_save
@@ -34,7 +34,7 @@ class SignalManager(object):
         signals.post_save.disconnect(serialize_on_save, weak=False, dispatch_uid="serialize_on_save")
         signals.post_save.disconnect(tracker_on_post_save, weak=False, dispatch_uid="tracker_on_post_save")
         signals.post_save.disconnect(base_visit_tracking_add_or_update_entry_buckets_on_post_save, weak=False, dispatch_uid="base_visit_tracking_add_or_update_entry_buckets_on_post_save")
-        signals.post_save.disconnect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_check_in_progress_on_post_save")
+        #signals.post_save.disconnect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_check_in_progress_on_post_save")
         signals.pre_save.disconnect(base_subject_get_or_create_registered_subject_on_post_save, weak=False, dispatch_uid="base_subject_get_or_create_registered_subject_on_post_save")
         signals.pre_save.disconnect(is_consented_instance_on_pre_save, weak=False, dispatch_uid="is_consented_instance_on_pre_save")
         signals.post_save.disconnect(pre_appointment_contact_on_post_delete, weak=False, dispatch_uid="pre_appointment_contact_on_post_delete")
@@ -48,7 +48,7 @@ class SignalManager(object):
         signals.post_save.connect(pre_appointment_contact_on_post_delete, weak=False, dispatch_uid="pre_appointment_contact_on_post_delete")
         signals.pre_save.connect(is_consented_instance_on_pre_save, weak=False, dispatch_uid="is_consented_instance_on_pre_save")
         signals.pre_save.connect(base_subject_get_or_create_registered_subject_on_post_save, weak=False, dispatch_uid="base_subject_get_or_create_registered_subject_on_post_save")
-        signals.post_save.connect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_check_in_progress_on_post_save")
+        #signals.post_save.connect(base_visit_tracking_on_post_save, weak=False, dispatch_uid="base_visit_tracking_check_in_progress_on_post_save")
         signals.post_save.connect(base_visit_tracking_add_or_update_entry_buckets_on_post_save, weak=False, dispatch_uid="base_visit_tracking_add_or_update_entry_buckets_on_post_save")
         signals.post_save.connect(tracker_on_post_save, weak=False, dispatch_uid="tracker_on_post_save")
         signals.post_save.connect(serialize_on_save, weak=False, dispatch_uid="serialize_on_save")
