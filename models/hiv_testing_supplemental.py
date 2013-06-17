384475d43fae33716464b8139ad65160bdfc9652
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-17 10:11:53 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@101 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/hiv_testing_supplemental.py
+++ models/hiv_testing_supplemental.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
-from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, WHYNOHIVTEST_CHOICE, YES_NO_UNSURE
+from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, HIVTEST_PREFEREDWEEK, HIVTEST_PREFEREDYEAR, WHYNOHIVTESTING_CHOICE, HIVTEST_PREFEREDTIME, HIVTESTPREFERENCE_CHOICE, YES_NO_UNSURE
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
 
@@ -10,71 +10,77 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
     """CS002"""
     
     numhivtests = models.IntegerField(
-        verbose_name = "Supplemental HT1. How many times before today have you had an HIV test?",
-        max_length = 2,
+        verbose_name="Supplemental HT1. How many times before today have you had an HIV test?",
+        max_length=2,
+        null=True,
+        blank=True,
         help_text="Note:Leave blank if participant does not want to respond.",
         )
 
     wherehivtest = models.CharField(
-        verbose_name = "Supplemental HT2. Not including today's HIV test, where were you tested for HIV, the last [most recent] time you were tested?",
-        max_length = 15,
-        choices = WHEREHIVTEST_CHOICE,
+        verbose_name="Supplemental HT2. Not including today's HIV test, where were you tested for HIV, the last [most recent] time you were tested?",
+        max_length=85,
+        null=True,
+        blank=True,
+        choices=WHEREHIVTEST_CHOICE,
         help_text="",
         )
 
     whyhivtest = models.CharField(
-        verbose_name = "Supplemental HT3. Not including today's HIV test, which of the following statements best describes the reason you were tested the last [most recent] time you were tested before today?",
-        max_length = 15,
-        choices = WHYHIVTEST_CHOICE,
+        verbose_name="Supplemental HT3. Not including today's HIV test, which of the following statements best describes the reason you were tested the last [most recent] time you were tested before today?",
+        max_length=105,
+        null=True, 
+        blank=True,
+        choices=WHYHIVTEST_CHOICE,
         help_text="",
         )
 
     whynohivtest = models.CharField(
-        verbose_name = "Supplemental HT4. If you were not tested for HIV in the 12 months prior to today, what is the main reason why not?",
-        max_length = 15,
-        choices = WHYNOHIVTEST_CHOICE,
+        verbose_name="Supplemental HT4. If you were not tested for HIV in the 12 months prior to today, what is the main reason why not?",
+        max_length=55,
+        choices=WHYNOHIVTESTING_CHOICE,
         help_text="",
         )
 
     hiv_pills = models.CharField(
-        verbose_name = "Supplemental HT5. Have you ever heard about treatment for HIV with pills called antiretroviral therapy or ARVs [or HAART]?",
-        max_length = 15,
-        choices = YES_NO_UNSURE,
+        verbose_name="Supplemental HT5. Have you ever heard about treatment for HIV with pills called antiretroviral therapy or ARVs [or HAART]?",
+        max_length=25,
+        choices=YES_NO_UNSURE,
         help_text="",
         )
 
     arvshivtest = models.CharField(
-        verbose_name = "Supplemental HT6. Do you believe that treatment for HIV with antiretroviral therapy (or ARVs) can help HIV-positive people to live longer?",
-        max_length = 15,
-        choices = YES_NO_UNSURE,
+        verbose_name="Supplemental HT6. Do you believe that treatment for HIV with antiretroviral therapy (or ARVs) can help HIV-positive people to live longer?",
+        max_length=25,
+        choices=YES_NO_UNSURE,
         help_text="",
         )
 
     prefer_hivtest = models.CharField(
-        verbose_name = "Supplemental HT7. In the future, where would you prefer to get tested for HIV?",
-        max_length = 15,
-        choices = WHYNOHIVTEST_CHOICE,
+        verbose_name="Supplemental HT7. In the future, where would you prefer to get tested for HIV?",
+        max_length=70,
+        choices=HIVTESTPREFERENCE_CHOICE,
         help_text="",
         )
 
     hivtest_time = models.CharField(
-        verbose_name = "Supplemental HT8. In the future, is there a particular time of day that you would prefer to receive your next HIV test ?",
-        max_length = 15,
-        choices = WHYNOHIVTEST_CHOICE,
+        verbose_name="Supplemental HT8. In the future, is there a particular time of day that you would prefer to receive your next HIV test ?",
+        max_length=35,
+        choices=HIVTEST_PREFEREDTIME,
         help_text="",
         )
 
     hivtest_week = models.CharField(
-        verbose_name = "Supplemental HT9. In the future, is there a particular day of the week that you would prefer to receive your next HIV test ?",
-        max_length = 15,
-        choices = WHYNOHIVTEST_CHOICE,
+        verbose_name="Supplemental HT9. In the future, is there a particular day of the week that you would prefer to receive your next HIV test ?",
+        max_length=35,
+        choices=HIVTEST_PREFEREDWEEK,
         help_text="",
         )
 
     hivtest_year = models.CharField(
-        verbose_name = "Supplemental HT10. In the future, is there a particular time of year that you would prefer to receive your next HIV test?",
-        max_length = 15,
-        choices = WHYNOHIVTEST_CHOICE,
+        verbose_name="Supplemental HT10. In the future, is there a particular time of year that you would prefer to receive your next HIV test?",
+        max_length=25,
+        choices=HIVTEST_PREFEREDYEAR,
         help_text="",
         )
     
