a422ce3c9cb84ad9fbedf0394aa8102df5461b4c
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-06-18 07:46:53 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_birt_reports@4 b789da29-60d9-447c-b67e-ccf55bb91ea3

---
+++ views/generate_report.py
@@ -0,0 +1,36 @@
+import os
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+@login_required
+def generate_report(request, **kwargs):
+    """ Return items from the producer to the source."""
+    data = request.POST
+    report_name = None
+    token_string = '\"'
+    run_string = 'java -jar'+' '+settings.REPORTS_JAR_PATH+' '+settings.REPORTS_TEMPLATES_PATH+' '+settings.REPORTS_OUTPUT_PATH+' '+request.user.username
+    for key, value in data.iteritems():
+        if key == 'report':
+            report_name = value
+            continue 
+        if key != 'csrfmiddlewaretoken':
+            tokens = value.split(' ')
+            if len(tokens) > 1:
+                for tk in tokens:
+                    token_string = token_string + tk + ' '
+                token_string = token_string.rstrip()
+                token_string = token_string+'\"'
+            value = token_string
+            run_string = run_string +' '+key+' '+value
+    run_string +=' '+report_name
+    #print run_string
+    result = os.system(run_string)
+    #java -jar ~/Documents/birtreport_generator.jar structures_yr_2 STARTING_DATE 2013-05-01 csrfmiddlewaretoken yRyoq2DbHHRlcpmWT7AedhOQZs33sUxS SECTION Section A WARD Ntshinoge
+    if result != 0:
+        raise TypeError('java jar file did not complete successfully')       
+    return render_to_response(
+        'report_'+request.user.username+'_'+report_name+'.html', {},
+        context_instance=RequestContext(request)
+        )
