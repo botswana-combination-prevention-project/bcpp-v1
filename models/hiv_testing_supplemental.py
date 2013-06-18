36a2f8d2d05d937009f5ad1b2c443b3252514c2c
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-18 06:58:20 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@106 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/hiv_testing_supplemental.py
+++ models/hiv_testing_supplemental.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
+from bhp_base_model.fields import OtherCharField
 from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, HIVTEST_PREFEREDWEEK, HIVTEST_PREFEREDYEAR, WHYNOHIVTESTING_CHOICE, HIVTEST_PREFEREDTIME, HIVTESTPREFERENCE_CHOICE, YES_NO_UNSURE
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
@@ -69,6 +70,9 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
         choices=HIVTEST_PREFEREDTIME,
         help_text="",
         )
+    hivtest_time_other = OtherCharField(
+        verbose_name="If yes, specify:",
+        )
 
     hivtest_week = models.CharField(
         verbose_name="Supplemental HT9. In the future, is there a particular day of the week that you would prefer to receive your next HIV test ?",
@@ -76,12 +80,18 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
         choices=HIVTEST_PREFEREDWEEK,
         help_text="",
         )
+    hivtest_week_other = OtherCharField(
+        verbose_name="If yes, specify:",
+        )
 
     hivtest_year = models.CharField(
         verbose_name="Supplemental HT10. In the future, is there a particular time of year that you would prefer to receive your next HIV test?",
         max_length=25,
         choices=HIVTEST_PREFEREDYEAR,
         help_text="",
+        )
+    hivtest_year_other = OtherCharField(
+        verbose_name="If yes, specify:",
         )
     
     history = AuditTrail()
