58348b1a80ea76130b034c21461ebb2bfaffee4c
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-18 14:12:25 +0000

    pep8
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@128 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/base_circumcision.py
+++ models/base_circumcision.py
@@ -9,15 +9,15 @@ class BaseCircumcision (BaseScheduledVisitModel):
     """CS002"""
     
     circumcised = models.CharField(
-        verbose_name = "Supplemental MC1. Do you believe that male circumcision has any health benefits for you?",
-        max_length = 15,
-        choices = YES_NO_UNSURE,
+        verbose_name="Supplemental MC1. Do you believe that male circumcision has any health benefits for you?",
+        max_length=15,
+        choices=YES_NO_UNSURE,
         help_text="",
         )
     
     healthbenefitsSMC = models.ManyToManyField(CicumcisionBenefits,
-        verbose_name = "Supplemental MC2. What do you believe are the health benefits of male circumcision?",
-        max_length = 25,
+        verbose_name="Supplemental MC2. What do you believe are the health benefits of male circumcision?",
+        max_length=25,
         help_text="Indicate all that apply.",
         )
 
