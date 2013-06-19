a784ef1033efcdf43e7c063ccc0bc92f44887c16
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-19 06:26:12 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@130 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/reproductive_health.py
+++ models/reproductive_health.py
@@ -15,51 +15,52 @@ class ReproductiveHealth (BaseScheduledVisitModel):
                       " children that may have died at (stillbirth) or after birth. "
                       "Do not include any current pregnancies or miscarriages that occur"
                       " early in pregnancy (prior to 20 weeks)."),
-        max_length = 2,
+        max_length=2,
         default=0,
         help_text=("Note: If participant does not want to answer, please record 0. "
                    "If no children, skip questions 84-87."),
         )
 
     morechildren = models.CharField(
-        verbose_name = "76. Do you wish to have a child now or in the future?",
-        max_length = 15,
-        choices = YES_NO_DONT_ANSWER,
+        verbose_name="76. Do you wish to have a child now or in the future?",
+        max_length=25,
+        choices=YES_NO_DONT_ANSWER,
         help_text="",
         )
 
     wherecirc = models.CharField(
         verbose_name=("77. Is it possible that you could become pregnant? "
                       "[If no, please indicate reason why you cannot become pregnant]"),
-        max_length = 15,
-        choices = WHERECIRC_CHOICE,
+        max_length=70,
+        choices=WHERECIRC_CHOICE,
         help_text="",
         )
 
     familyplanning = models.ManyToManyField(FamilyPlanning,
         verbose_name=("78. In the past 12 months, have you used any methods to prevent"
                       " pregnancy ?"),
-        max_length = 25,
         help_text="check all that apply",
         )
 
     currentpregnant = models.CharField(
-        verbose_name = "79. Are you currently pregnant?",
-        max_length = 15,
-        choices = YES_NO_UNSURE,
+        verbose_name="79. Are you currently pregnant?",
+        max_length=25,
+        choices=YES_NO_UNSURE,
         help_text="",
         )
 
     ancreg = models.CharField(
-        verbose_name = "80. Have you registered for antenatal care?",
-        max_length = 15,
-        choices = ANCREG_CHOICE,
+        verbose_name="80. Have you registered for antenatal care?",
+        max_length=15,
+        null=True,
+        blank=True,
+        choices=ANCREG_CHOICE,
         help_text="",
         )
 
-    lnmp = models.DateTimeField(
-        verbose_name = "81. When was your last normal menstrual period?",
-        max_length = 25,
+    lnmp = models.DateField(
+        verbose_name="81. When was your last normal menstrual period?",
+        max_length=25,
         null=True,
         blank=True,
         help_text=("Note:If participant does not want to answer, leave blank.  "
@@ -67,32 +68,37 @@ class ReproductiveHealth (BaseScheduledVisitModel):
         )
 
     lastbirth = models.DateField(
-        verbose_name = "82. When did you last (most recently) give birth?",
-        max_length = 25,
+        verbose_name="82. When did you last (most recently) give birth?",
         null=True,
         blank=True,
         help_text=("Note:Leave blank if participant does not want to respond. "
-                   "If participant is unable to estimate date, record -4."),
+                   "If participant is unable to estimate date, leave blank"),
         )
 
     anclastpregnancy = models.CharField(
-        verbose_name = "83. During your last pregnancy (not current pregnancy) did you go for antenatal care?",
-        max_length = 15,
-        choices = YES_NO_DONT_ANSWER,
+        verbose_name="83. During your last pregnancy (not current pregnancy) did you go for antenatal care?",
+        max_length=25,
+        choices=YES_NO_DONT_ANSWER,
+        null=True,
+        blank=True,
         help_text="",
         )
 
     hivlastpregnancy = models.CharField(
-        verbose_name = "84. During your last pregnancy (not current pregnancy) were you tested for HIV?",
-        max_length = 15,
-        choices = YES_NO_UNSURE,
-        help_text="",
+        verbose_name="84. During your last pregnancy (not current pregnancy) were you tested for HIV?",
+        max_length=25,
+        null=True,
+        blank=True,
+        choices=YES_NO_UNSURE,
+        help_text="If respondent was aware that she was HIV-positive prior to last pregnancy",
         )
 
     pregARV = models.CharField(
-        verbose_name = "85. Were you given antiretroviral medications to protect the baby?",
-        max_length = 15,
-        choices = PREGARV_CHOICE,
+        verbose_name="85. Were you given antiretroviral medications to protect the baby?",
+        max_length=15,
+        choices=PREGARV_CHOICE,
+        null=True,
+        blank=True,
         help_text="",
         )
 
