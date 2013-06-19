d74fef3a5e01dd3326f9ab7e2f2a3f66a9cbc826
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-19 09:06:48 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@147 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/access_to_care.py
+++ models/access_to_care.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
-from bcpp.choices import AGREE_STRONGLY
+from bhp_base_model.fields import OtherCharField 
+from bcpp.choices import AGREE_STRONGLY, WHEREACCESS_CHOICE
 from bcpp_list.models import MedicalCareAccess
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
@@ -13,10 +14,11 @@ class AccessToCare (BaseScheduledVisitModel):
     often_medicalcare = models.CharField(
         verbose_name=("Supplemental AC1. In the past year, where do you MOST OFTEN get"
                       " medical care or treatment when you or someone in your family is sick or hurt?"),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=50,
+        choices=WHEREACCESS_CHOICE,
         help_text="",
         )
+    often_medicalcare_other = OtherCharField()
 
     whereaccess = models.ManyToManyField(MedicalCareAccess,
         verbose_name=("Supplemental AC2. In the past year, where else have you obtained"
@@ -24,40 +26,41 @@ class AccessToCare (BaseScheduledVisitModel):
                       " is sick or hurt?"),
         help_text="(check all that apply)",
         )
+    whereaccess_other = OtherCharField()
 
     overallaccess = models.CharField(
         verbose_name=("Supplemental AC3. If I need medical care, I can get seen by an"
                         " appropriate health professional without any trouble."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     emergencyaccess = models.CharField(
-        verbose_name = "Supplemental AC4. It is hard for me to get medical care in an emergency",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        verbose_name="Supplemental AC4. It is hard for me to get medical care in an emergency",
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     expensiveaccess = models.CharField(
-        verbose_name = "Supplemental AC5. Sometimes I go without the medical care I need because it is too expensive.",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        verbose_name="Supplemental AC5. Sometimes I go without the medical care I need because it is too expensive.",
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     convenientaccess = models.CharField(
-        verbose_name = "Supplemental AC6. Places where I can get medical care are very conveniently located.",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        verbose_name="Supplemental AC6. Places where I can get medical care are very conveniently located.",
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     wheneverlaccess = models.CharField(
-        verbose_name = "Supplemental AC7. I am able to get medical care whenever I need it.",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        verbose_name="Supplemental AC7. I am able to get medical care whenever I need it.",
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
     
