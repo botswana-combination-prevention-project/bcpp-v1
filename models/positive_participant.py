89817b7409d476c6a06a023d0b3a542aaba70c9b
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-19 08:42:26 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@142 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/positive_participant.py
+++ models/positive_participant.py
@@ -14,52 +14,52 @@ class PositiveParticipant (BaseScheduledVisitModel):
     
     internalize1stigma = models.CharField(
         verbose_name="Supplemental ST13. I think less of myself.",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     internalized2stigma = models.CharField(
         verbose_name=("Supplemental ST14. I have felt ashamed because of having HIV."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     friendstigma = models.CharField(
-        verbose_name=("Supplemental ST15. I fear that if I disclosed my HIV status to my"
+        verbose_name=("Supplemental ST25. I fear that if I disclosed my HIV status to my"
                       " friends, they would lose respect for me."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     familystigma = models.CharField(
         verbose_name=("Supplemental ST16. I fear that if I disclosed my HIV status to my family,"
                       " they would exclude me from usual family activities."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     enactedtalkstigma = models.CharField(
-        verbose_name = "Supplemental ST17People have talked badly about me.",
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        verbose_name="Supplemental ST17People have talked badly about me.",
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     enactedrespectstigma = models.CharField(
         verbose_name=("Supplemental ST18. I have lost respect or standing in the community."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
 
     enactedjobstigma = models.CharField(
         verbose_name=("Supplemental ST19. I have lost a job because of having HIV."),
-        max_length = 15,
-        choices = AGREE_STRONGLY,
+        max_length=25,
+        choices=AGREE_STRONGLY,
         help_text="",
         )
     
