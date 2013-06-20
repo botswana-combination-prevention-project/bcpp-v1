3c820bdb5b17223e31595b61e54c808cf57507ab
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-20 13:25:23 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@163 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -126,6 +126,18 @@ class CsEnrolmentChecklistForm (BaseSubjectModelForm):
 
 #ResidencyMobility
 class ResidencyMobilityForm (BaseSubjectModelForm):
+    
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        #validating if other community, you specify
+        if cleaned_data['cattlepostlands'] == 'Other community' and not cleaned_data['cattlepostlands_other']:
+            raise forms.ValidationError('If participant was staying in another community, specify the community')
+        #if reason for staying away is OTHER, specify reason
+        if cleaned_data['reasonaway'] == 'Other' and not cleaned_data['reasonaway_other']:
+            raise forms.ValidationError('If participant was away from community for \'OTHER\' reason, provide/specify reason')
+        cleaned_data = super(ResidencyMobilityForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = ResidencyMobility
