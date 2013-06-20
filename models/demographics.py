c56361ffb28a87c816f6f04e7a1d0f48dd298a12
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-20 13:24:46 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@162 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/demographics.py
+++ models/demographics.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
+from bhp_base_model.fields import OtherCharField
 from bcpp_list.models import LiveWith
 from bcpp.choices import RELIGION_CHOICE, ETHNIC_CHOICE, MARITALSTATUS_CHOICE
 from base_scheduled_visit_model import BaseScheduledVisitModel
@@ -11,38 +12,40 @@ class Demographics (BaseScheduledVisitModel):
     """CS002"""
     
     religion = models.CharField(
-        verbose_name = "7. What is your religion affiliation?",
-        max_length = 15,
-        choices = RELIGION_CHOICE,
+        verbose_name="7. What is your religion affiliation?",
+        max_length=35,
+        choices=RELIGION_CHOICE,
         help_text="",
         )
+    religion_other = OtherCharField()
 
     ethnic = models.CharField(
-        verbose_name = "8. What is your ethnic group?",
-        max_length = 15,
-        choices = ETHNIC_CHOICE,
+        verbose_name="8. What is your ethnic group?",
+        max_length=35,
+        choices=ETHNIC_CHOICE,
         help_text="",
         )
-
+    other = OtherCharField()
+    
     maritalstatus = models.CharField(
-        verbose_name = "9. What is your current marital status?",
-        max_length = 15,
-        choices = MARITALSTATUS_CHOICE,
+        verbose_name="9. What is your current marital status?",
+        max_length=55,
+        choices=MARITALSTATUS_CHOICE,
         help_text="",
         )
 
     numwives = models.IntegerField(
-        verbose_name = ("10. How many wives do (you/your husband) have (including traditional marriage),"
+        verbose_name=("10. How many wives do (you/your husband) have (including traditional marriage),"
                         " including yourself?"),
-        max_length = 2,
-        null=True, 
+        max_length=2,
+        null=True,
         blank=True,
         help_text="Note:leave blank if participant does not want to respond.",
         )
 
     livewith = models.ManyToManyField(LiveWith,
-        verbose_name = "11. Who do you currently live with ?",
-        null=True, 
+        verbose_name="11. Who do you currently live with ?",
+        null=True,
         blank=True,
         help_text="[indicate all that apply]",
         )
