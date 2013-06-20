570e1b227e99f8e80bf396785ea42d34003f6601
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-20 13:23:57 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@160 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/community_engagement.py
+++ models/community_engagement.py
@@ -11,30 +11,29 @@ class CommunityEngagement (BaseScheduledVisitModel):
     """CS002"""
     
     communityengagement = models.CharField(
-        verbose_name = "CE1. How active are you in community activities such as celebrations, health campaigns and development of the community that surrounds you??",
-        max_length = 15,
-        choices = COMMUNITYENGAGEMENT_CHOICE,
+        verbose_name="CE1. How active are you in community activities such as celebrations, health campaigns and development of the community that surrounds you??",
+        max_length=25,
+        choices=COMMUNITYENGAGEMENT_CHOICE,
         help_text="",
         )
 
     voteengagement = models.CharField(
-        verbose_name = "CE2. Did you vote in the last local government election?",
-        max_length = 15,
-        choices = VOTEENGAGEMENT_CHOICE,
+        verbose_name="CE2. Did you vote in the last local government election?",
+        max_length=25,
+        choices=VOTEENGAGEMENT_CHOICE,
         help_text="",
         )
 
     problemsengagement = models.ManyToManyField(NeighbourhoodProblems,
-        verbose_name = "CE3. What are the major problems in this neighbourhood??",
-        max_length = 25,
+        verbose_name="CE3. What are the major problems in this neighbourhood??",
         help_text=("Note:Interviewer to read question but NOT the responses. Check the boxes of"
                    " any of problems mentioned."),
         )
 
     solveengagement = models.CharField(
-        verbose_name = "CE4. If there is a problem in this neighbourhood, do the adults work together in solving it?",
-        max_length = 15,
-        choices = SOLVEENGAGEMENT_CHOICE,
+        verbose_name="CE4. If there is a problem in this neighbourhood, do the adults work together in solving it?",
+        max_length=25,
+        choices=SOLVEENGAGEMENT_CHOICE,
         help_text="",
         )
     
