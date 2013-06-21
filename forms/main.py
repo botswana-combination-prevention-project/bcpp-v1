bae0e9e17a19551f96b915a7027a052f7a199658
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-21 10:40:33 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@170 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -105,6 +105,19 @@ class GrantForm (BaseSubjectModelForm):
 
 # BaselineHouseholdSurvey
 class BaselineHouseholdSurveyForm (BaseSubjectModelForm):
+    
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        #validating if other, specify
+        if cleaned_data['flooring_type'] == 'OTHER' and not cleaned_data['flooring_type_other']:
+            raise forms.ValidationError('If participant has a different flooring type from what is on the list, provide the flooring type')
+        if cleaned_data['water_source'] == 'OTHER' and not cleaned_data['water_source_other']:
+            raise forms.ValidationError('If participant uses a different water source, specify it')
+        if cleaned_data['energy_source'] == 'OTHER' and not cleaned_data['energy_source_other']:
+            raise forms.ValidationError('If a different energy source is used, specify it')
+        cleaned_data = super(BaselineHouseholdSurveyForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = BaselineHouseholdSurvey
