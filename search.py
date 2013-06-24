3908c6ea91137891ae573802a184ec6b3506b18f
erik van widenfelt <ew2789@gmail.com>
2013-06-24 09:35:15 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@186 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ search.py
@@ -0,0 +1,25 @@
+from bhp_search.classes import BaseSearchByWord, search
+#from bccp_household.models import Household, HouseholdStructure, HouseholdStructureMember
+from models import SubjectConsent
+
+
+class SearchByWord(BaseSearchByWord):
+
+    section_name = 'subject'
+    search_model = SubjectConsent  # (SubjectConsent, Household, HouseholdStructure, HouseholdStructureMember)
+
+search.register(SearchByWord)
+
+# from bhp_search.classes import BaseSearchByWord, search
+#
+# class SearchByWord(BaseSearchByWord):
+#
+#     def get_search_models_prep(self):
+#         return {'householdstructuremember': ('bcpp_household', 'householdstructuremember', 'household'),
+#                 'householdstructure': ('bcpp_household', 'householdstructure', 'household'),
+#                 'household': ('bcpp_household', 'household', 'household'),
+#                 'subjectconsentyearzero': ('bcpp_subject', 'subjectconsentyearzero', 'subject'),
+#                 }
+#
+#
+# search.register(SearchByWord)
---
+++ section.py
@@ -0,0 +1,12 @@
+from bhp_section.classes import BaseSectionView, site_sections
+from models import SubjectConsent
+
+
+class SectionSubjectView(BaseSectionView):
+    section_name = 'subject'
+    section_display_name = 'Subjects'
+    section_display_index = 10
+    section_template = 'section_subject.html'
+    add_model = SubjectConsent
+
+site_sections.register(SectionSubjectView)
