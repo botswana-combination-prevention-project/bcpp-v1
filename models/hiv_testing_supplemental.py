3110c1f5f630e673a094f307802dbf0d62070a51
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-24 09:05:28 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@177 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/hiv_testing_supplemental.py
+++ models/hiv_testing_supplemental.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
-from bhp_base_model.fields import OtherCharField
-from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, HIVTEST_PREFEREDWEEK, HIVTEST_PREFEREDYEAR, WHYNOHIVTESTING_CHOICE, HIVTEST_PREFEREDTIME, HIVTESTPREFERENCE_CHOICE, YES_NO_UNSURE
+from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, WHYNOHIVTESTING_CHOICE, YES_NO_UNSURE
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
 
@@ -19,7 +18,7 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
         )
 
     wherehivtest = models.CharField(
-        verbose_name="Supplemental HT2. Not including today's HIV test, where were you tested for HIV, the last [most recent] time you were tested?",
+        verbose_name="Supplemental HT2. Where were you tested for HIV, the last [most recent] time you were tested?",
         max_length=85,
         null=True,
         blank=True,
@@ -55,47 +54,12 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
     arvshivtest = models.CharField(
         verbose_name="Supplemental HT6. Do you believe that treatment for HIV with antiretroviral therapy (or ARVs) can help HIV-positive people to live longer?",
         max_length=25,
+        null=True, 
+        blank=True,
         choices=YES_NO_UNSURE,
         help_text="",
         )
 
-    prefer_hivtest = models.CharField(
-        verbose_name="Supplemental HT7. In the future, where would you prefer to get tested for HIV?",
-        max_length=70,
-        choices=HIVTESTPREFERENCE_CHOICE,
-        help_text="",
-        )
-
-    hivtest_time = models.CharField(
-        verbose_name="Supplemental HT8. In the future, is there a particular time of day that you would prefer to receive your next HIV test ?",
-        max_length=35,
-        choices=HIVTEST_PREFEREDTIME,
-        help_text="",
-        )
-    hivtest_time_other = OtherCharField(
-        verbose_name="If yes, specify:",
-        )
-
-    hivtest_week = models.CharField(
-        verbose_name="Supplemental HT9. In the future, is there a particular day of the week that you would prefer to receive your next HIV test ?",
-        max_length=35,
-        choices=HIVTEST_PREFEREDWEEK,
-        help_text="",
-        )
-    hivtest_week_other = OtherCharField(
-        verbose_name="If yes, specify:",
-        )
-
-    hivtest_year = models.CharField(
-        verbose_name="Supplemental HT10. In the future, is there a particular time of year that you would prefer to receive your next HIV test?",
-        max_length=25,
-        choices=HIVTEST_PREFEREDYEAR,
-        help_text="",
-        )
-    hivtest_year_other = OtherCharField(
-        verbose_name="If yes, specify:",
-        )
-    
     history = AuditTrail()
 
     def get_absolute_url(self):
