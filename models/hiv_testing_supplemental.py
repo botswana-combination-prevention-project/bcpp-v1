38662105328580f8915618954975eefe94aff660
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-25 08:44:34 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@190 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/hiv_testing_supplemental.py
+++ models/hiv_testing_supplemental.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
-from bcpp.choices import WHEREHIVTEST_CHOICE, WHYHIVTEST_CHOICE, WHYNOHIVTESTING_CHOICE, YES_NO_UNSURE
+from bcpp.choices import YES_NO_UNSURE
 from base_scheduled_visit_model import BaseScheduledVisitModel
 
 
@@ -9,40 +8,40 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
     
     """CS002"""
     
-    numhivtests = models.IntegerField(
-        verbose_name="Supplemental HT1. How many times before today have you had an HIV test?",
-        max_length=2,
-        null=True,
-        blank=True,
-        help_text="Note:Leave blank if participant does not want to respond.",
-        )
-
-    wherehivtest = models.CharField(
-        verbose_name="Supplemental HT2. Where were you tested for HIV, the last [most recent] time you were tested?",
-        max_length=85,
-        null=True,
-        blank=True,
-        choices=WHEREHIVTEST_CHOICE,
-        help_text="",
-        )
-
-    whyhivtest = models.CharField(
-        verbose_name="Supplemental HT3. Not including today's HIV test, which of the following statements best describes the reason you were tested the last [most recent] time you were tested before today?",
-        max_length=105,
-        null=True, 
-        blank=True,
-        choices=WHYHIVTEST_CHOICE,
-        help_text="",
-        )
-
-    whynohivtest = models.CharField(
-        verbose_name="Supplemental HT4. If you were not tested for HIV in the 12 months prior to today, what is the main reason why not?",
-        max_length=55,
-        null=True, 
-        blank=True,
-        choices=WHYNOHIVTESTING_CHOICE,
-        help_text="",
-        )
+#     numhivtests = models.IntegerField(
+#         verbose_name="Supplemental HT1. How many times before today have you had an HIV test?",
+#         max_length=2,
+#         null=True,
+#         blank=True,
+#         help_text="Note:Leave blank if participant does not want to respond.",
+#         )
+# 
+#     wherehivtest = models.CharField(
+#         verbose_name="Supplemental HT2. Where were you tested for HIV, the last [most recent] time you were tested?",
+#         max_length=85,
+#         null=True,
+#         blank=True,
+#         choices=WHEREHIVTEST_CHOICE,
+#         help_text="",
+#         )
+# 
+#     whyhivtest = models.CharField(
+#         verbose_name="Supplemental HT3. Not including today's HIV test, which of the following statements best describes the reason you were tested the last [most recent] time you were tested before today?",
+#         max_length=105,
+#         null=True, 
+#         blank=True,
+#         choices=WHYHIVTEST_CHOICE,
+#         help_text="",
+#         )
+# 
+#     whynohivtest = models.CharField(
+#         verbose_name="Supplemental HT4. If you were not tested for HIV in the 12 months prior to today, what is the main reason why not?",
+#         max_length=55,
+#         null=True, 
+#         blank=True,
+#         choices=WHYNOHIVTESTING_CHOICE,
+#         help_text="",
+#         )
 
     hiv_pills = models.CharField(
         verbose_name="Supplemental HT5. Have you ever heard about treatment for HIV with pills called antiretroviral therapy or ARVs [or HAART]?",
@@ -62,10 +61,5 @@ class HivTestingSupplemental (BaseScheduledVisitModel):
 
     history = AuditTrail()
 
-    def get_absolute_url(self):
-        return reverse('admin:bcpp_subject_supplementalht_change', args=(self.id,))
-
     class Meta:
-        app_label = 'bcpp_subject'
-        verbose_name = "Supplemental HT"
-        verbose_name_plural = "Supplemental HT"
+        abstract = True
