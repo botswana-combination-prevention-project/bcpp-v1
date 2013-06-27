794a5ec4181f57e0713fc6a9d3ab2993b90e1860
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-27 12:24:15 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@204 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/community_engagement.py
+++ models/community_engagement.py
@@ -1,5 +1,6 @@
 from django.db import models
 from audit_trail.audit import AuditTrail
+from bhp_base_model.fields import OtherCharField
 from bcpp_list.models import NeighbourhoodProblems
 from bcpp.choices import COMMUNITYENGAGEMENT_CHOICE, VOTEENGAGEMENT_CHOICE, SOLVEENGAGEMENT_CHOICE
 from base_scheduled_visit_model import BaseScheduledVisitModel
@@ -9,27 +10,28 @@ class CommunityEngagement (BaseScheduledVisitModel):
 
     """CS002"""
     
-    communityengagement = models.CharField(
+    community_engagement = models.CharField(
         verbose_name="CE1. How active are you in community activities such as celebrations, health campaigns and development of the community that surrounds you??",
         max_length=25,
         choices=COMMUNITYENGAGEMENT_CHOICE,
         help_text="",
         )
 
-    voteengagement = models.CharField(
+    vote_engagement = models.CharField(
         verbose_name="CE2. Did you vote in the last local government election?",
         max_length=25,
         choices=VOTEENGAGEMENT_CHOICE,
         help_text="",
         )
 
-    problemsengagement = models.ManyToManyField(NeighbourhoodProblems,
+    problems_engagement = models.ManyToManyField(NeighbourhoodProblems,
         verbose_name="CE3. What are the major problems in this neighbourhood??",
         help_text=("Note:Interviewer to read question but NOT the responses. Check the boxes of"
                    " any of problems mentioned."),
         )
+    problems_engagement_other = OtherCharField()
 
-    solveengagement = models.CharField(
+    solve_engagement = models.CharField(
         verbose_name="CE4. If there is a problem in this neighbourhood, do the adults work together in solving it?",
         max_length=25,
         choices=SOLVEENGAGEMENT_CHOICE,
