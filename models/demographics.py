3f8ff12a00ca541b18654d3c655a8ffe773e46bd
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-27 12:24:30 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@206 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/demographics.py
+++ models/demographics.py
@@ -22,18 +22,18 @@ class Demographics (BaseScheduledVisitModel):
         verbose_name="8. What is your ethnic group?",
         max_length=35,
         choices=ETHNIC_CHOICE,
-        help_text="",
+        help_text="Ask for the original ethnic group",
         )
     other = OtherCharField()
     
-    maritalstatus = models.CharField(
+    marital_status = models.CharField(
         verbose_name="9. What is your current marital status?",
         max_length=55,
         choices=MARITALSTATUS_CHOICE,
         help_text="",
         )
 
-    numwives = models.IntegerField(
+    num_wives = models.IntegerField(
         verbose_name=("10. How many wives do (you/your husband) have (including traditional marriage),"
                         " including yourself?"),
         max_length=2,
@@ -42,7 +42,7 @@ class Demographics (BaseScheduledVisitModel):
         help_text="Leave blank if participant does not want to respond.",
         )
 
-    livewith = models.ManyToManyField(LiveWith,
+    live_with = models.ManyToManyField(LiveWith,
         verbose_name="11. Who do you currently live with ?",
         null=True,
         blank=True,
