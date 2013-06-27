f922ef6a55adb34bdb67b708ec5c9ab5d5f8aa24
erik van widenfelt <ew2789@gmail.com>
2013-06-27 10:17:36 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_test@7 b80f8652-8d5a-498f-a521-a51f72815f6d

--- tests/factories/test_consent_factory.py
+++ tests/factories/test_consent_factory.py
@@ -10,8 +10,8 @@ class TestConsentFactory(BaseConsentFactory):
 
     registered_subject = factory.SubFactory(RegisteredSubjectFactory)
     user_provided_subject_identifier = None
-    identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    identity = factory.Sequence(lambda n: '{0}2{1}'.format(str(n).rjust(4, '0'), str(n).rjust(4, '0')))
     identity_type = factory.Iterator(IDENTITY_TYPE, getter=lambda c: c[0])
-    confirm_identity = factory.Sequence(lambda n: '{0}2{1}'.format(n.rjust(4, '0'), n.rjust(4, '0')))
+    confirm_identity = factory.Sequence(lambda n: '{0}2{1}'.format(str(n).rjust(4, '0'), str(n).rjust(4, '0')))
     first_name = factory.Sequence(lambda n: 'ERIK{0}'.format(n))
     initials = factory.Sequence(lambda n: 'E{0}W'.format(n))
