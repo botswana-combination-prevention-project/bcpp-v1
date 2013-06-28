36822f95a6b0ddfd2d2f26c41735a58678e49fe5
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-06-28 10:12:23 +0000

    overiding get_form() to implement randomising questions
    git-svn-id: http://192.168.1.50/svn/bhp_base_admin@6 eb779c7d-545f-4eec-81f9-8628f4f85584

--- admin/base_model_admin.py
+++ admin/base_model_admin.py
@@ -1,3 +1,4 @@
+import random
 from django.contrib import admin
 from bhp_base_admin.mixin import SiteMixin
 
@@ -7,3 +8,17 @@ class BaseModelAdmin (SiteMixin, admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         self.update_modified_stamp(request, obj, change)
         super(BaseModelAdmin, self).save_model(request, obj, form, change)
+    
+    def get_form(self, request, obj=None, **kwargs):
+        if 'randomise' in dir(self.form.Meta):#make sure the field is nullable or has a default value if not nullable
+            for random_element in self.form.Meta.randomise:
+                if self.form.base_fields[random_element[0]].required:
+                    #need both null=True and blank=True in field declaration in model for field.required to be false
+                    raise TypeError('Randomization field \'{0}\' cannot be a required field.'.format(random_element[0]))
+                rand_num = random.randint(0, 1)
+                if random_element[0] in self.fields and rand_num == 0:#remove field
+                    self.fields.remove(random_element[0])
+                elif random_element[0] not in self.fields and rand_num == 1:#add field
+                    self.fields.insert(random_element[1]-1,random_element[0])
+        form = super(BaseModelAdmin, self).get_form(request, obj, **kwargs)
+        return form
