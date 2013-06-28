4afcdb1c9ce2507f2b66a1db5e244780f777f753
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-06-28 13:22:31 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_admin@7 eb779c7d-545f-4eec-81f9-8628f4f85584

--- admin/base_model_admin.py
+++ admin/base_model_admin.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 from django.contrib import admin
 from bhp_base_admin.mixin import SiteMixin
 
@@ -10,12 +10,13 @@ class BaseModelAdmin (SiteMixin, admin.ModelAdmin):
         super(BaseModelAdmin, self).save_model(request, obj, form, change)
     
     def get_form(self, request, obj=None, **kwargs):
-        if 'randomise' in dir(self.form.Meta):#make sure the field is nullable or has a default value if not nullable
+        if 'Meta' in dir(self.form) and 'randomise' in dir(self.form.Meta):#make sure the field is nullable or has a default value if not nullable
             for random_element in self.form.Meta.randomise:
                 if self.form.base_fields[random_element[0]].required:
                     #need both null=True and blank=True in field declaration in model for field.required to be false
                     raise TypeError('Randomization field \'{0}\' cannot be a required field.'.format(random_element[0]))
-                rand_num = random.randint(0, 1)
+                #rand_num = random.randint(0, 1)
+                rand_num = choice(random_element[2])
                 if random_element[0] in self.fields and rand_num == 0:#remove field
                     self.fields.remove(random_element[0])
                 elif random_element[0] not in self.fields and rand_num == 1:#add field
