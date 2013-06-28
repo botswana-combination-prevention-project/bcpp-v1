3dfb4a764f8e816494d4ec2fe9f9977dd88260c0
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-06-28 05:52:00 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@215 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/hiv_test_review.py
+++ models/hiv_test_review.py
@@ -7,18 +7,18 @@ from base_scheduled_visit_model import BaseScheduledVisitModel
 
 class HivTestReview (BaseScheduledVisitModel):
     
-    """CS002"""
+    """CS002 - [numbering here has been re-arranged. Not same as in Wiki. sl note. ]"""
     
-    hivtestdate = models.DateField(
-        verbose_name="20. What was the recorded date of the last HIV test?",
+    hiv_test_date = models.DateField(
+        verbose_name="22. What was the recorded date of the last HIV test?",
         null=True,
         blank=True,
         help_text=("Note:If participant does not want to answer, leave blank.  "
                    "If participant is unable to estimate date, record estimation."),
         )
 
-    recordedhivresult = models.CharField(
-        verbose_name="21. What was the recorded HIV test result?",
+    recorded_hiv_result = models.CharField(
+        verbose_name="23. What was the recorded HIV test result?",
         max_length=30,
         null=True,
         blank=True,
@@ -26,16 +26,16 @@ class HivTestReview (BaseScheduledVisitModel):
         help_text="",
         )
 
-    whenhivtest = models.CharField(
-        verbose_name=("22. When was the last [most recent]"
+    when_hiv_test = models.CharField(
+        verbose_name=("20. When was the last [most recent]"
                         " time you were tested for HIV?"),
         max_length=25,
         choices=WHENHIVTEST_CHOICE,
         help_text="",
         )
 
-    verbalhivresult = models.CharField(
-        verbose_name="23. Please tell me the results of your last [most recent] HIV test?",
+    verbal_hiv_result = models.CharField(
+        verbose_name="21. Please tell me the results of your last [most recent] HIV test?",
         max_length=30,
         choices=VERBALHIVRESULT_CHOICE,
         help_text="",
