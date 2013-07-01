292642b48dea610a3d10eedae69d449e568e5a23
erik van widenfelt <ew2789@gmail.com>
2013-07-01 06:01:14 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_test@8 b80f8652-8d5a-498f-a521-a51f72815f6d

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,7 @@ from test_off_study_mixin import TestOffStudyMixin
 from test_base_off_study import TestBaseOffStudy
 from test_registration import TestRegistration
 from test_consent import TestConsent
+from test_consent_no_rs import TestConsentNoRs
 from test_visit import TestVisit
 from test_scheduled_model import TestScheduledModel
+from test_subject_uuid_model import TestSubjectUuidModel
--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -1,5 +1,7 @@
 from test_consent_factory import TestConsentFactory
+from test_consent_no_rs_factory import TestConsentNoRsFactory
 from test_base_off_study_factory import TestBaseOffStudyFactory
 from test_visit_factory import TestVisitFactory
 from test_registration_factory import TestRegistrationFactory
 from test_scheduled_model_factory import TestScheduledModelFactory
+from test_subject_uuid_model_factory import TestSubjectUuidModelFactory
