ed885c701d5acc70f5fefeccb13f28ab380f2d72
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-02 09:18:06 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@233 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -458,6 +458,13 @@ class UncircumcisedForm (BaseSubjectModelForm):
     def clean(self):
 
         cleaned_data = self.cleaned_data
+        #validate other
+        if cleaned_data.get('circumcision_day') and not cleaned_data.get('circumcision_day_other'):
+            raise forms.ValidationError('if \'YES\', specify the day preferred.')
+        if cleaned_data.get('circumcision_week') and not cleaned_data.get('circumcision_week_other'):
+            raise forms.ValidationError('if \'YES\', specify the week preferred.')
+        if cleaned_data.get('circumcision_year') and not cleaned_data.get('circumcision_year_other'):
+            raise forms.ValidationError('if \'YES\', specify the year preferred.')
 
         return cleaned_data
 
