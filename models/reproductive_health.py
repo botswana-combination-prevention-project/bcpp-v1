12c30daddf510913264cc210cb34b21c2d267ca7
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-02 12:28:13 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@237 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/reproductive_health.py
+++ models/reproductive_health.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from audit_trail.audit import AuditTrail
+from bhp_base_model.fields import OtherCharField
 from bcpp_list.models import FamilyPlanning
 from bcpp.choices import YES_NO_DONT_ANSWER, WHERECIRC_CHOICE, ANCREG_CHOICE, PREGARV_CHOICE, YES_NO_UNSURE
 from base_scheduled_visit_model import BaseScheduledVisitModel
@@ -10,7 +11,7 @@ class ReproductiveHealth (BaseScheduledVisitModel):
     
     """CS002"""
     
-    numberchildren = models.IntegerField(
+    number_children = models.IntegerField(
         verbose_name=("75. How many children have you given birth to? Please include any"
                       " children that may have died at (stillbirth) or after birth. "
                       "Do not include any current pregnancies or miscarriages that occur"
@@ -21,14 +22,14 @@ class ReproductiveHealth (BaseScheduledVisitModel):
                    "If no children, skip questions 84-87."),
         )
 
-    morechildren = models.CharField(
+    more_children = models.CharField(
         verbose_name="76. Do you wish to have a child now or in the future?",
         max_length=25,
-        choices=YES_NO_DONT_ANSWER,
+        choices=YES_NO_UNSURE,
         help_text="",
         )
 
-    wherecirc = models.CharField(
+    where_circ = models.CharField(
         verbose_name=("77. Is it possible that you could become pregnant? "
                       "[If no, please indicate reason why you cannot become pregnant]"),
         max_length=70,
@@ -36,20 +37,21 @@ class ReproductiveHealth (BaseScheduledVisitModel):
         help_text="",
         )
 
-    familyplanning = models.ManyToManyField(FamilyPlanning,
+    family_planning = models.ManyToManyField(FamilyPlanning,
         verbose_name=("78. In the past 12 months, have you used any methods to prevent"
                       " pregnancy ?"),
         help_text="check all that apply",
         )
+    family_planning_other = OtherCharField()
 
-    currentpregnant = models.CharField(
+    current_pregnant = models.CharField(
         verbose_name="79. Are you currently pregnant?",
         max_length=25,
         choices=YES_NO_UNSURE,
         help_text="",
         )
 
-    ancreg = models.CharField(
+    anc_reg = models.CharField(
         verbose_name="80. Have you registered for antenatal care?",
         max_length=55,
         null=True,
@@ -67,7 +69,7 @@ class ReproductiveHealth (BaseScheduledVisitModel):
                    "If participant is unable to estimate date, leave blank"),
         )
 
-    lastbirth = models.DateField(
+    last_birth = models.DateField(
         verbose_name="82. When did you last (most recently) give birth?",
         null=True,
         blank=True,
@@ -75,7 +77,7 @@ class ReproductiveHealth (BaseScheduledVisitModel):
                    "If participant is unable to estimate date, leave blank"),
         )
 
-    anclastpregnancy = models.CharField(
+    anc_last_pregnancy = models.CharField(
         verbose_name="83. During your last pregnancy (not current pregnancy) did you go for antenatal care?",
         max_length=25,
         choices=YES_NO_DONT_ANSWER,
@@ -84,7 +86,7 @@ class ReproductiveHealth (BaseScheduledVisitModel):
         help_text="",
         )
 
-    hivlastpregnancy = models.CharField(
+    hiv_last_pregnancy = models.CharField(
         verbose_name="84. During your last pregnancy (not current pregnancy) were you tested for HIV?",
         max_length=25,
         null=True,
@@ -93,7 +95,7 @@ class ReproductiveHealth (BaseScheduledVisitModel):
         help_text="If respondent was aware that she was HIV-positive prior to last pregnancy",
         )
 
-    pregARV = models.CharField(
+    preg_arv = models.CharField(
         verbose_name="85. Were you given antiretroviral medications to protect the baby?",
         max_length=95,
         choices=PREGARV_CHOICE,
