04b2f8a7d4585ebdf7c60dcdeb88e0b3688a46b6
erik van widenfelt <ew2789@gmail.com>
2013-07-07 18:10:42 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_test@12 b80f8652-8d5a-498f-a521-a51f72815f6d

--- admin/test_model_admin.py
+++ admin/test_model_admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from bhp_base_admin.admin import BaseModelAdmin
-from bhp_supplimental_fields.classes import SupplimentalFields
+from bhp_supplemental_fields.classes import SupplementalFields
 from bhp_base_test.models import TestModel
 from bhp_base_test.forms import TestModelForm
 
@@ -9,6 +9,6 @@ class TestModelAdmin(BaseModelAdmin):
 
     form = TestModelForm
     fields = ('f1', 'f2', 'f3', 'f4', 'f5')
-    supplimental_fields = SupplimentalFields(('f3', 'f4'), p=0.5)
+    supplimental_fields = SupplementalFields(('f3', 'f4'), p=0.5)
 
 admin.site.register(TestModel, TestModelAdmin)
