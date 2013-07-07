96b866d0f1206393413fca5d0644a545ed32f9c1
erik van widenfelt <ew2789@gmail.com>
2013-07-07 07:51:22 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_admin@8 eb779c7d-545f-4eec-81f9-8628f4f85584

--- admin/base_model_admin.py
+++ admin/base_model_admin.py
@@ -1,6 +1,6 @@
-from random import choice
 from django.contrib import admin
 from bhp_base_admin.mixin import SiteMixin
+from bhp_supplimental_fields.models import Excluded
 
 
 class BaseModelAdmin (SiteMixin, admin.ModelAdmin):
@@ -8,18 +8,21 @@ class BaseModelAdmin (SiteMixin, admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         self.update_modified_stamp(request, obj, change)
         super(BaseModelAdmin, self).save_model(request, obj, form, change)
-    
+        if 'supplimental_fields' in dir(self): 
+            if self.form._meta.exclude:
+                # record which instances were selected for excluded fields
+                Excluded.objects.get_or_create(app_label=obj._meta.app_label, object_name=obj._meta.object_name, model_pk=obj.pk, excluded=self.form._meta.exclude)
+            self.form._meta.exclude = None
+
     def get_form(self, request, obj=None, **kwargs):
-        if 'Meta' in dir(self.form) and 'randomise' in dir(self.form.Meta):#make sure the field is nullable or has a default value if not nullable
-            for random_element in self.form.Meta.randomise:
-                if self.form.base_fields[random_element[0]].required:
-                    #need both null=True and blank=True in field declaration in model for field.required to be false
-                    raise TypeError('Randomization field \'{0}\' cannot be a required field.'.format(random_element[0]))
-                #rand_num = random.randint(0, 1)
-                rand_num = choice(random_element[2])
-                if random_element[0] in self.fields and rand_num == 0:#remove field
-                    self.fields.remove(random_element[0])
-                elif random_element[0] not in self.fields and rand_num == 1:#add field
-                    self.fields.insert(random_element[1]-1,random_element[0])
-        form = super(BaseModelAdmin, self).get_form(request, obj, **kwargs)
-        return form
+        if 'supplimental_fields' in dir(self) and not request.method == 'POST':
+            # we are using form._meta.exclude, so make sure it was not set in the form.Meta class
+            if 'exclude' in dir(self.form.Meta):
+                raise AttributeError('The form.Meta attribute exclude cannot be used with \'supplimental_fields\'. See {0}.'.format(self.form))
+            # always set to None first
+            self.form._meta.exclude = None
+            self.fields, exclude_supplimental_fields = self.supplimental_fields.update_fields(self.fields, self.form._meta.model, obj)
+            if exclude_supplimental_fields:
+                # set exclude so that when the form is saved we use the same excluded fields from when the form was rendered
+                self.form._meta.exclude = exclude_supplimental_fields
+        return super(BaseModelAdmin, self).get_form(request, obj, **kwargs)
