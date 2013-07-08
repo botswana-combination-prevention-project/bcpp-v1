5510b98ba137e5fe0a5117f80935f313a5d72f16
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-08 13:02:27 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@281 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from base_subject_model_form import BaseSubjectModelForm
 from bcpp_subject.models import (SubjectLocator, SubjectDeath, RecentPartner, SecondPartner, ThirdPartner, 
                                  QualityOfLife, ResourceUtilization, OutpatientCare, HospitalAdmission, 
-                                 HivHealthCareCosts, LabourMarketWages, Grant, BaselineHouseholdSurvey, 
+                                 HivHealthCareCosts, LabourMarketWages, Grant,
                                  CeaEnrolmentChecklist, CsEnrolmentChecklist, 
-                                 Demographics, CommunityEngagement, Education,
-                                 HivTestReview, HivTested, HivUntested, FutureHivTesting, SexualBehaviour, 
+                                 CommunityEngagement, Education,
+                                 HivTestReview, HivTested, HivUntested, FutureHivTesting, 
                                  MonthsRecentPartner, MonthsSecondPartner, MonthsThirdPartner, 
                                  HivCareAdherence, HivMedicalCare, Circumcision, Circumcised, Uncircumcised, 
                                  ReproductiveHealth, MedicalDiagnoses, SubstanceUse, Stigma, StigmaOpinion, 
@@ -113,26 +113,6 @@ class GrantForm (BaseSubjectModelForm):
         model = Grant
 
 
-# BaselineHouseholdSurvey
-class BaselineHouseholdSurveyForm (BaseSubjectModelForm):
-    
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        #validating if other, specify
-        if cleaned_data.get('flooring_type') == 'OTHER' and not cleaned_data.get('flooring_type_other'):
-            raise forms.ValidationError('If participant has a different flooring type from what is on the list, provide the flooring type')
-        if cleaned_data.get('water_source') == 'OTHER' and not cleaned_data.get('water_source_other'):
-            raise forms.ValidationError('If participant uses a different water source, specify it')
-        if cleaned_data.get('energy_source') == 'OTHER' and not cleaned_data.get('energy_source_other'):
-            raise forms.ValidationError('If a different energy source is used, specify it')
-        cleaned_data = super(BaselineHouseholdSurveyForm, self).clean()
-        return cleaned_data
-
-    class Meta:
-        model = BaselineHouseholdSurvey
-
-
 # CeaEnrollmentChecklist
 class CeaEnrolmentChecklistForm (BaseSubjectModelForm):
 
@@ -145,31 +125,6 @@ class CsEnrolmentChecklistForm (BaseSubjectModelForm):
 
     class Meta:
         model = CsEnrolmentChecklist
-
-
-#Demographics
-class DemographicsForm (BaseSubjectModelForm):
-    
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        #validating religion affiliation
-        if cleaned_data.get('religion') == 'Christian' and not cleaned_data.get('religion_other'):
-            raise forms.ValidationError('If participant is a Christian, specify denomination.')
-        #validating ethnicity
-        if cleaned_data.get('ethnic') == 'Tswana' and not cleaned_data.get('other'):
-            raise forms.ValidationError('If participant is Tswana, specify the ethnic group.')
-        #validating Other
-        if cleaned_data.get('ethnic') == 'Other' and not cleaned_data.get('other'):
-            raise forms.ValidationError('If participant ethnic group not given in list-of-options, specify the ethnic group.')
-        #validating marital status
-        if cleaned_data.get('marital_status') == 'Married' and not cleaned_data.get('num_wives'):
-            raise forms.ValidationError('If participant is married, give number of wives')
-        cleaned_data = super(DemographicsForm, self).clean()
-        return cleaned_data
-    
-    class Meta:
-        model = Demographics
 
 
 #CommunityEngagement
@@ -260,38 +215,6 @@ class HivUntestedForm (BaseSubjectModelForm):
     class Meta:
         model = HivUntested
 
-#SexualBehaviour
-class SexualBehaviourForm (BaseSubjectModelForm):
-    
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        #if respondent has had sex, answer all following questions on form
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('last_year_partners'):
-            raise forms.ValidationError('If participant has had sex, how many people has he/she had sex with')
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('more_sex'):
-            raise forms.ValidationError('If participant has had sex, we need to know if this person lives outside community')
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('first_sex'):
-            raise forms.ValidationError('If participant has had sex, how old was he/she when he/she first had sex')
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('condom'):
-            raise forms.ValidationError('If participant has had sex, was a condom used the last time he/she had sex?')
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('alcohol_sex'):
-            raise forms.ValidationError('If participant has had sex, did he/she or partner have alcohol?')
-        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('last_sex'):
-            raise forms.ValidationError('If participant has had sex, when was the last time he/she had sex?')
-        if cleaned_data.get('last_sex') == 'Days' and not cleaned_data.get('last_sex_calc'):
-            raise forms.ValidationError('If participant has had sex, and indicated a time point when last had sex, provide number of days')
-        if cleaned_data.get('last_sex') == 'Months' and not cleaned_data.get('last_sex_calc'):
-            raise forms.ValidationError('If participant has had sex, and indicated a time point when last had sex, provide number of months')
-        if cleaned_data.get('last_sex') == 'Years' and not cleaned_data.get('last_sex_calc'):
-            raise forms.ValidationError('If participant has had sex, and indicated a time point when last had sex, provide number of years')
-        
-        cleaned_data = super(SexualBehaviourForm, self).clean()
-        return cleaned_data
-
-    class Meta:
-        model = SexualBehaviour
-        
 
 #MonthsRecentPartner
 class MonthsRecentPartnerForm (BaseSubjectModelForm):
@@ -345,41 +268,6 @@ class MonthsThirdPartnerForm (BaseSubjectModelForm):
     class Meta:
         model = MonthsThirdPartner
 
-
-
-#HivCareAdherence
-class HivCareAdherenceForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        #if no medical care, explain why not
-        if cleaned_data.get('medical_care') == 'No' and not cleaned_data.get('no_medical_care'):
-            raise forms.ValidationError('If participant has not received any medical care, please give reason why not')
-        #if never taken arv's give reason why
-        if cleaned_data.get('ever_recommended_arv') == 'No' and not cleaned_data.get('why_no_arv'):
-            raise forms.ValidationError('If participant has not taken any ARV\'s, give the main reason why not')
-        if cleaned_data.get('why_no_arv') == 'Other' and not cleaned_data.get('why_no_arv_other'):
-            raise forms.ValidationError('If participant NO ARV\'s is \'OTHER\', specify reason why not started ARV\'s?')
-        #if partipant has taken arv's, give date when these were started
-        if cleaned_data.get('ever_recommended_arv') == 'Yes' and not cleaned_data.get('first_arv'):
-            raise forms.ValidationError('If participant has taken ARV\'s, give the date when these were first started.')
-        #if taking arv's have you missed any
-        if cleaned_data.get('on_arv') == 'Yes' and not cleaned_data.get('adherence_4_day'):
-            raise forms.ValidationError('If participant is taking ARV\'s, have they skipped/ missed taking any? Pleae indicate')
-        #if you are not taking any arv's do not indicate that you have missed taking medication
-        if cleaned_data.get('on_arv') == 'No' and cleaned_data.get('adherence_4_day'):
-            raise forms.ValidationError('You do not have to indicate missed medication (70) because you are not taking any ARV\'s (68)')
-        #if currently taking arv's, how well has participant been taking medication
-        if cleaned_data.get('on_arv') == 'Yes' and cleaned_data.get('adherence_4_wk'):
-            raise forms.ValidationError('If participant is currently taking ARV\'s, how well has he/she been taking the medication this past week?')
-        if cleaned_data.get('arv_stop') == 'Other' and not cleaned_data.get('arv_stop_other'):
-            raise forms.ValidationError('If participant reason for stopping ARV\'s is \'OTHER\', specify reason why stopped taking ARV\'s?')
-
-        return cleaned_data
-
-    class Meta:
-        model = HivCareAdherence
-        
 
 #HivMedicalCare
 class HivMedicalCareForm (BaseSubjectModelForm):
