d643e3387e999659010b7d1ec6608a0de97def16
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-08 12:27:53 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@264 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/__init__.py
+++ forms/__init__.py
@@ -3,3 +3,7 @@ from subject_consent_form import SubjectConsentForm
 from subject_refusal_form import SubjectRefusalForm
 from subject_absentee_form import SubjectAbsenteeForm, SubjectAbsenteeEntryForm
 from subject_moved_form import SubjectMovedForm
+
+from residency_mobility_form import ResidencyMobilityForm
+from hiv_testing_history_form import HivTestingHistoryForm
+from pregnancy_form import PregnancyForm
