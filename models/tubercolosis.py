10598b9cbb7405dd31002d4d0887497720ab0292
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-09 10:40:14 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@294 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ models/tubercolosis.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from audit_trail.audit import AuditTrail
+from bcpp.choices import DXTB_CHOICE   
+from base_scheduled_visit_model import BaseScheduledVisitModel
+
+
+class Tubercolosis (BaseScheduledVisitModel):
+    
+    """CS002 - Medical Diagnoses- Tubercolosis"""
+    
+    date_tb = models.DateField(
+        verbose_name="97. Date of the diagnosis of tuberculosis:",
+        help_text="",
+        )
+
+    dx_tb = models.CharField(
+        verbose_name="98. [Interviewer:]What is the tuberculosis diagnosis as recorded?",
+        max_length=50,
+        choices=DXTB_CHOICE,
+        help_text="",
+        )
+    
+    history = AuditTrail()
+    
+    def get_absolute_url(self):
+        return reverse('admin:bcpp_subject_tubercolosis_change', args=(self.id,))
+
+    class Meta:
+        app_label = 'bcpp_subject'
+        verbose_name = "Tubercolosis"
+        verbose_name_plural = "Tubercolosis"
