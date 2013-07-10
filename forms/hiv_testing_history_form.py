775ba95630e81aea82a2324298f8f54c092bd0d3
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-10 14:43:25 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@323 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/hiv_testing_history_form.py
+++ forms/hiv_testing_history_form.py
@@ -9,14 +9,13 @@ class HivTestingHistoryForm (BaseSubjectModelForm):
     def clean(self):
 
         cleaned_data = self.cleaned_data
-        #validating when testing declined
-        if cleaned_data.get('hiv_result') == 'Declined' and not cleaned_data.get('why_not_tested'):
-            raise forms.ValidationError('If participant has declined testing, provide reason participant declined testing (2)')
         #validating prior hiv testing
         if cleaned_data.get('has_tested') == 'Yes' and not cleaned_data.get('when_hiv_test'):
-            raise forms.ValidationError('If participant has tested before, let us know the last time he/she tested (20).')
-        if cleaned_data.get('has_tested') == 'Yes' and not cleaned_data.get('verbal_hiv_result'):
-            raise forms.ValidationError('If participant has tested before, provide what the result was (21).')
+            raise forms.ValidationError('If participant has tested before, let us know the last time he/she tested.')
+        if cleaned_data.get('has_tested') == 'Yes' and not cleaned_data.get('has_record'):
+            raise forms.ValidationError('If participant has tested before, we need to know if a record is available.')
+        if cleaned_data.get('has_record') == 'No' and not cleaned_data.get('verbal_hiv_result'):
+            raise forms.ValidationError('If participant has tested before, let us know the result of the last HIV test.')
         cleaned_data = super(HivTestingHistoryForm, self).clean()
         return cleaned_data
 
