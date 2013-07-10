e720d967e96099eb47592f4458a36c763b2c62de
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-10 14:43:02 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@322 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- admin/hiv_testing_history_admin.py
+++ admin/hiv_testing_history_admin.py
@@ -4,22 +4,19 @@ from bcpp_subject.models import HivTestingHistory
 from bcpp_subject.forms import HivTestingHistoryForm
 
 
-# HivTestingHistory
 class HivTestingHistoryAdmin(SubjectVisitModelAdmin):
 
     form = HivTestingHistoryForm
     fields = (
         "subject_visit",
-        'take_hiv_testing',
-        'why_not_tested',
         'has_tested',
-        'has_record',
         "when_hiv_test",
-        'verbal_hiv_result',)
+        'has_record',
+        'verbal_hiv_result',
+        'other_record',)
     radio_fields = {
-        "take_hiv_testing": admin.VERTICAL,
-        "why_not_tested": admin.VERTICAL,
         "has_tested": admin.VERTICAL,
         "has_record": admin.VERTICAL,
-        "verbal_hiv_result": admin.VERTICAL,}
+        "verbal_hiv_result": admin.VERTICAL,
+        'other_record': admin.VERTICAL,}
 admin.site.register(HivTestingHistory, HivTestingHistoryAdmin)
