b7eeee67db17de3725684cf3a1b3f7235d683703
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-10 14:38:48 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@313 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ models/hiv_result_documentation.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from audit_trail.audit import AuditTrail
+from bcpp.choices import RECORDEDHIVRESULT_CHOICE
+from bcpp_subject.choices import HIV_DOC_TYPE
+from base_scheduled_visit_model import BaseScheduledVisitModel
+
+
+class HivResultDocumentation (BaseScheduledVisitModel):
+    
+    """CS002 - for usage only and if only patient has other result for HIV"""
+    
+    result_date = models.DateField(
+        verbose_name="What is the recorded date of the HIV test?",
+        help_text="",
+        )
+
+    result_recorded = models.CharField(
+        verbose_name="What is the recorded HIV test result?",
+        max_length=30,
+        choices=RECORDEDHIVRESULT_CHOICE,
+        help_text="",
+        )
+    
+    result_doc_type = models.CharField(
+        verbose_name="What is the type of document used?",
+        max_length=35,
+        choices=HIV_DOC_TYPE,
+        help_text="",
+        )
+    
+    history = AuditTrail()
+
+    def get_absolute_url(self):
+        return reverse('admin:bcpp_subject_hivresultdocumentation_change', args=(self.id,))
+
+    class Meta:
+        app_label = 'bcpp_subject'
+        verbose_name = "HIV result documentation"
+        verbose_name_plural = "HIV result documentation"
