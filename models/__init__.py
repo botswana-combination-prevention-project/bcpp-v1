bd3e187eea7eca95bc53c80ed568f332d7948a5b
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-10 14:41:33 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_list@76 674fa781-39cf-41df-9450-c12409787013

--- models/__init__.py
+++ models/__init__.py
@@ -18,3 +18,4 @@ from subject_moved_reason import SubjectMovedReason
 from partner_residency import PartnerResidency
 from heart_disease import HeartDisease
 from diagnoses import Diagnoses
+from religion import Religion
