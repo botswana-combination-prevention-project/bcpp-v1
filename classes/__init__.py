99837fbbaffa1c8e287a577b2c4ba744a93667ec
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-11 13:52:23 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp@39 55627d75-56d3-4d04-9d37-21f252a47b4a

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,3 +1,3 @@
-#from search_by_word import SearchByWord
-#from search_by_date import SearchByDate
-#from search_by_week import SearchByWeek
+from search_by_word import SearchByWord
+from search_by_date import SearchByDate
+from search_by_week import SearchByWeek
