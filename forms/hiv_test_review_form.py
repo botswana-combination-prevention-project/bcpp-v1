687ad7e88b915eed065c6bf2d77e306156c0f874
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-12 09:35:23 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@337 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ forms/hiv_test_review_form.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from django import forms
+from base_subject_model_form import BaseSubjectModelForm
+from bcpp_subject.models import HivTestReview
+
+
+class HivTestReviewForm (BaseSubjectModelForm):
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        # to ensure that HIV test date is not greater than today
+        if cleaned_data.get('hiv_test_date'):
+            if cleaned_data.get('hiv_test_date') > datetime.today():
+                raise forms.ValidationError('The last recorded HIV test date cannot be greater than today\'s date. Please correct.')
+
+        return super(HivTestReviewForm, self).clean()
+
+    class Meta:
+        model = HivTestReview
