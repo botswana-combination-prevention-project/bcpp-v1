be761ea021bf1453cedbddc3981aab23e22120d1
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-12 09:40:52 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@348 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ admin/uncircumcised_admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from subject_visit_model_admin import SubjectVisitModelAdmin
+from bcpp_subject.models import Uncircumcised
+from bcpp_subject.forms import UncircumcisedForm
+
+
+class UncircumcisedAdmin(SubjectVisitModelAdmin):
+
+    form = UncircumcisedForm
+    fields = [
+        "subject_visit",
+        "circumcised",
+        "health_benefits_smc",
+        'reason_circ',
+        'future_circ',
+        'circumcision_day',
+        'circumcision_day_other',
+        'circumcision_week',
+        'circumcision_week_other',
+        'circumcision_year',
+        'circumcision_year_other',
+        'future_reasons_smc',
+        'service_facilities',
+        'aware_free',
+    ]
+    radio_fields = {
+        "circumcised": admin.VERTICAL,
+        "reason_circ": admin.VERTICAL,
+        "future_circ": admin.VERTICAL,
+        "circumcision_day": admin.VERTICAL,
+        "circumcision_week": admin.VERTICAL,
+        "circumcision_year": admin.VERTICAL,
+        "future_reasons_smc": admin.VERTICAL,
+        "service_facilities": admin.VERTICAL,
+        "aware_free": admin.VERTICAL}
+    filter_horizontal = ("health_benefits_smc",)
+admin.site.register(Uncircumcised, UncircumcisedAdmin)
