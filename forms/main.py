c5a69040496155b2202718f52ad5d19578fa40dc
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-12 09:38:44 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@344 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/main.py
+++ forms/main.py
@@ -1,256 +1,96 @@
 from django import forms
-from datetime import datetime
 from base_subject_model_form import BaseSubjectModelForm
-from bcpp_subject.models import (SubjectLocator, SubjectDeath, RecentPartner, SecondPartner,
-                                 ThirdPartner, QualityOfLife, ResourceUtilization,
-                                 OutpatientCare, HospitalAdmission,
+from bcpp_subject.models import (SubjectLocator, SubjectDeath, QualityOfLife, 
+                                 ResourceUtilization, OutpatientCare, HospitalAdmission,
                                  HivHealthCareCosts, LabourMarketWages, Grant,
                                  CeaEnrolmentChecklist, CsEnrolmentChecklist,
                                  CommunityEngagement, Education,
-                                 HivTestReview, HivTested, HivUntested, FutureHivTesting,
-                                 MonthsRecentPartner, MonthsSecondPartner, MonthsThirdPartner,
-                                 HivMedicalCare, Circumcision, Circumcised, Uncircumcised,
-                                 ReproductiveHealth, MedicalDiagnoses, HeartAttack, Cancer,
-                                 Tubercolosis, SubstanceUse, Stigma, StigmaOpinion,
-                                 PositiveParticipant, AccessToCare, HouseholdComposition,
-                                 Respondent, TodaysHivResult, HivResultDocumentation)
+                                 HivMedicalCare, Circumcision, Circumcised,
+                                 ReproductiveHealth, MedicalDiagnoses, HeartAttack,
+                                 Cancer, Tubercolosis, SubstanceUse, Stigma, 
+                                 StigmaOpinion, PositiveParticipant, 
+                                 HouseholdComposition, Respondent, 
+                                 HivResultDocumentation)
 
 
-# SubjectLocator
 class SubjectLocatorForm (BaseSubjectModelForm):
 
     class Meta:
         model = SubjectLocator
 
 
-# SubjectDeath
 class SubjectDeathForm (BaseSubjectModelForm):
 
     class Meta:
         model = SubjectDeath
 
 
-# RecentPartner
-class RecentPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-
-        return cleaned_data
-
-    class Meta:
-        model = RecentPartner
-
-
-# SecondPartner
-class SecondPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-
-        return cleaned_data
-
-    class Meta:
-        model = SecondPartner
-
-
-# ThirdPartner
-class ThirdPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-
-        return cleaned_data
-
-    class Meta:
-        model = ThirdPartner
-
-
-# QualityOfLife
 class QualityOfLifeForm (BaseSubjectModelForm):
 
     class Meta:
         model = QualityOfLife
 
 
-# ResourceUtilization
 class ResourceUtilizationForm (BaseSubjectModelForm):
 
     class Meta:
         model = ResourceUtilization
 
 
-# OutpatientCare
 class OutpatientCareForm (BaseSubjectModelForm):
 
     class Meta:
         model = OutpatientCare
 
 
-# HospitalAdmission
 class HospitalAdmissionForm (BaseSubjectModelForm):
 
     class Meta:
         model = HospitalAdmission
 
 
-# HivHealthCareCosts
 class HivHealthCareCostsForm (BaseSubjectModelForm):
 
     class Meta:
         model = HivHealthCareCosts
 
 
-# LabourMarketWages
 class LabourMarketWagesForm (BaseSubjectModelForm):
 
     class Meta:
         model = LabourMarketWages
 
 
-# Grant
 class GrantForm (BaseSubjectModelForm):
 
     class Meta:
         model = Grant
 
 
-# CeaEnrollmentChecklist
 class CeaEnrolmentChecklistForm (BaseSubjectModelForm):
 
     class Meta:
         model = CeaEnrolmentChecklist
 
 
-# CsEnrolmentChecklist
 class CsEnrolmentChecklistForm (BaseSubjectModelForm):
 
     class Meta:
         model = CsEnrolmentChecklist
 
 
-# CommunityEngagement
 class CommunityEngagementForm (BaseSubjectModelForm):
 
     class Meta:
         model = CommunityEngagement
 
 
-# Education
 class EducationForm (BaseSubjectModelForm):
 
     class Meta:
         model = Education
 
 
-# HivTestReview
-class HivTestReviewForm (BaseSubjectModelForm):
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        # to ensure that HIV test date is not greater than today
-        if cleaned_data.get('hiv_test_date'):
-            if cleaned_data.get('hiv_test_date') > datetime.today():
-                raise forms.ValidationError('The last recorded HIV test date cannot be greater than today\'s date. Please correct.')
-
-        return super(HivTestReviewForm, self).clean()
-
-    class Meta:
-        model = HivTestReview
-
-
-class HivTestedForm (BaseSubjectModelForm):
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        if cleaned_data.get('num_hiv_tests') == 0:
-            raise forms.ValidationError('if participant has tested before, number of HIV tests before today cannot be zero. Please correct')
-        # if no, don't answer next question
-        if cleaned_data.get('hiv_pills') == 'No' and  cleaned_data.get('arvs_hiv_test'):
-            raise forms.ValidationError('You are answering information about ARV\'s yet have answered \'NO\', patient has never heard about ARV\'s. Please correct')
-        if cleaned_data.get('hiv_pills') == 'Yes' or cleaned_data.get('hiv_pills') == 'not sure' and not cleaned_data.get('arvs_hiv_test'):
-            raise forms.ValidationError('if "%s", answer whether participant believes that HIV positive can live longer if taking ARV\'s. (Q Supplemental HT6)')
-
-        return cleaned_data
-
-    class Meta:
-        model = HivTested
-
-
-# HivUntested
-class HivUntestedForm (BaseSubjectModelForm):
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        # if no, don't answer next question
-        if cleaned_data.get('hiv_pills') == 'No' and  cleaned_data.get('arvs_hiv_test'):
-            raise forms.ValidationError('You are answering information about ARV\'s yet have answered \'NO\', patient has never heard about ARV\'s. Please correct')
-        if cleaned_data.get('hiv_pills') == 'Yes' or cleaned_data.get('hiv_pills') == 'not sure' and not cleaned_data.get('arvs_hiv_test'):
-            raise forms.ValidationError('if "%s", answer whether participant believes that HIV positive can live longer if taking ARV\'s. (Q Supplemental HT6)')
-
-        return cleaned_data
-
-    class Meta:
-        model = HivUntested
-
-
-# MonthsRecentPartner
-class MonthsRecentPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        # ensuring that question about antiretrovirals is not answered if partner is known to be HIV negative
-        if cleaned_data.get('firstpartnerhiv') == 'negative' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('Do not answer this question if partners HIV status is known to be negative')
-        if cleaned_data.get('firstpartnerhiv') == 'I am not sure' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('If partner status is not known, do not give information about status of ARV\'s')
-        cleaned_data = super(MonthsRecentPartnerForm, self).clean()
-        return cleaned_data
-
-    class Meta:
-        model = MonthsRecentPartner
-
-
-# MonthsSecondPartner
-class MonthsSecondPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        # ensuring that question about antiretrovirals is not answered if partner is known to be HIV negative
-        if cleaned_data.get('firstpartnerhiv') == 'negative' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('Do not answer this question if partners HIV status is known to be negative')
-        if cleaned_data.get('firstpartnerhiv') == 'I am not sure' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('If partner status is not known, do not give information about status of ARV\'s')
-        cleaned_data = super(MonthsSecondPartnerForm, self).clean()
-
-        return cleaned_data
-
-    class Meta:
-        model = MonthsSecondPartner
-
-
-# MonthsThirdPartner
-class MonthsThirdPartnerForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        # ensuring that question about antiretrovirals is not answered if partner is known to be HIV negative
-        if cleaned_data.get('firstpartnerhiv') == 'negative' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('Do not answer this question if partners HIV status is known to be negative')
-        if cleaned_data.get('firstpartnerhiv') == 'I am not sure' and cleaned_data.get('firsthaart') == 'Yes' or cleaned_data.get('firsthaart') == 'No' or cleaned_data.get('firsthaart') == 'not sure' or cleaned_data.get('firsthaart') == 'Don\'t want to answer':
-            raise forms.ValidationError('If partner status is not known, do not give information about status of ARV\'s')
-        cleaned_data = super(MonthsThirdPartnerForm, self).clean()
-
-        return cleaned_data
-
-    class Meta:
-        model = MonthsThirdPartner
-
-
-# HivMedicalCare
 class HivMedicalCareForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -262,7 +102,6 @@ class HivMedicalCareForm (BaseSubjectModelForm):
         model = HivMedicalCare
 
 
-# Circumcision
 class CircumcisionForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -274,7 +113,6 @@ class CircumcisionForm (BaseSubjectModelForm):
         model = Circumcision
 
 
-# Circumcised
 class CircumcisedForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -284,26 +122,6 @@ class CircumcisedForm (BaseSubjectModelForm):
 
     class Meta:
         model = Circumcised
-
-
-# Uncircumcised
-class UncircumcisedForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-        # validate other
-        if cleaned_data.get('circumcision_day') and not cleaned_data.get('circumcision_day_other'):
-            raise forms.ValidationError('if \'YES\', specify the day preferred.')
-        if cleaned_data.get('circumcision_week') and not cleaned_data.get('circumcision_week_other'):
-            raise forms.ValidationError('if \'YES\', specify the week preferred.')
-        if cleaned_data.get('circumcision_year') and not cleaned_data.get('circumcision_year_other'):
-            raise forms.ValidationError('if \'YES\', specify the year preferred.')
-
-        return cleaned_data
-
-    class Meta:
-        model = Uncircumcised
-        randomise = [('aware_free', 14, [1, 1, 0, 0, 0, 0, 0, 0, 0, 0])]
 
 
 class ReproductiveHealthForm (BaseSubjectModelForm):
@@ -322,7 +140,6 @@ class MedicalDiagnosesForm (BaseSubjectModelForm):
         model = MedicalDiagnoses
 
 
-# SubstanceUse
 class SubstanceUseForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -334,7 +151,6 @@ class SubstanceUseForm (BaseSubjectModelForm):
         model = SubstanceUse
 
 
-# Stigma
 class StigmaForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -346,7 +162,6 @@ class StigmaForm (BaseSubjectModelForm):
         model = Stigma
 
 
-# StigmaOpinion
 class StigmaOpinionForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -358,7 +173,6 @@ class StigmaOpinionForm (BaseSubjectModelForm):
         model = StigmaOpinion
 
 
-# PositiveParticipant
 class PositiveParticipantForm (BaseSubjectModelForm):
     def clean(self):
 
@@ -370,78 +184,16 @@ class PositiveParticipantForm (BaseSubjectModelForm):
         model = PositiveParticipant
 
 
-# AccessToCare
-class AccessToCareForm (BaseSubjectModelForm):
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-
-        # if other, specify
-        if cleaned_data.get('often_medicalcare') == 'OTHER' and not cleaned_data.get('often_medicalcare_other'):
-            raise forms.ValidationError('if other medical care is used, specify the kind of medical care received')
-        if cleaned_data.get('whereaccess') == 'Other, specify' and not cleaned_data.get('whereaccess_other'):
-            raise forms.ValidationError('if medical access is \'OTHER\', provide the type of medical access obtained')
-
-        return cleaned_data
-
-    class Meta:
-        model = AccessToCare
-
-
-# HouseholdComposition
 class HouseholdCompositionForm (BaseSubjectModelForm):
 
     class Meta:
         model = HouseholdComposition
 
 
-# Respondent
 class RespondentForm (BaseSubjectModelForm):
 
     class Meta:
         model = Respondent
-
-
-# FutureHivTesting
-class FutureHivTestingForm (BaseSubjectModelForm):
-
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-
-        # validating a need to specify the participant's preference
-        if cleaned_data.get('hiv_test_time') == 'Yes, specify' and not cleaned_data.get('hiv_test_time_other'):
-            raise forms.ValidationError('If participant prefers a different test date/time than what is indicated, indicate the preference.')
-
-        if cleaned_data.get('hiv_test_week') == 'Yes, specify' and not cleaned_data.get('hiv_test_week_other'):
-            raise forms.ValidationError('If participant has preference for testing on a particular day of the week, indicate the preference.')
-
-        if cleaned_data.get('hiv_test_year') == 'Yes, specify' and not cleaned_data.get('hiv_test_year_other'):
-            raise forms.ValidationError('If participant prefers time of the year than the options given, indicate the preference.')
-
-        cleaned_data = super(FutureHivTestingForm, self).clean()
-
-        return cleaned_data
-
-    class Meta:
-        model = FutureHivTesting
-
-
-class TodaysHivResultForm(BaseSubjectModelForm):
-
-    def clean(self):
-
-        cleaned_data = self.cleaned_data
-    # validating when testing declined
-        if cleaned_data.get('hiv_result') == 'Declined' and not cleaned_data.get('why_not_tested'):
-            raise forms.ValidationError('If participant has declined testing, provide reason participant declined testing (2)')
-
-        cleaned_data = super(TodaysHivResultForm, self).clean()
-
-        return cleaned_data
-
-    class Meta:
-        model = TodaysHivResult
 
 
 class HeartAttackForm (BaseSubjectModelForm):
