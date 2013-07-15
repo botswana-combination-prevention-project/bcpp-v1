c58bd82e4ccbba6efc06cc2acc863f74dd5f4048
erik van widenfelt <ew2789@gmail.com>
2013-07-15 11:58:10 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_subject@44 f056b393-b364-42d3-9b15-75ddb560184a

--- models/signals.py
+++ models/signals.py
@@ -6,4 +6,9 @@ from base_subject import BaseSubject
 @receiver(post_save, weak=False, dispatch_uid='base_subject_get_or_create_registered_subject_on_post_save')
 def base_subject_get_or_create_registered_subject_on_post_save(sender, instance, **kwargs):
     if isinstance(instance, (BaseSubject)):
-        instance.post_save_get_or_create_registered_subject()
+        registered_subject = instance.post_save_get_or_create_registered_subject()
+        try:
+            if not instance.registered_subject:
+                instance.registered_subject = registered_subject
+        except:
+            pass
