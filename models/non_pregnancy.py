2c9750d9afe6223c488e73c28d294adc8867e538
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-17 07:41:31 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@387 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ models/non_pregnancy.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from audit_trail.audit import AuditTrail
+from bcpp.choices import YES_NO_UNSURE
+from base_pregnancy import BasePregnancy
+
+
+class NonPregnancy (BasePregnancy):
+
+    """CS002"""
+
+    more_children = models.CharField(
+        verbose_name="Do you wish to have a child now or in the future?",
+        max_length=25,
+        choices=YES_NO_UNSURE,
+        help_text="",
+        )
+
+    history = AuditTrail()
+
+    def get_absolute_url(self):
+        return reverse('admin:bcpp_subject_nonpregnancy_change', args=(self.id,))
+
+    class Meta:
+        app_label = 'bcpp_subject'
+        verbose_name = "Non Pregnancy"
+        verbose_name_plural = "Non Pregnancy"
