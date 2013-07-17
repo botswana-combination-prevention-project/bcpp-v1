6f9d06fe1762e17f5d3a63a335fda372fed50780
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-17 07:44:07 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@392 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ forms/reproductive_health_form.py
@@ -0,0 +1,25 @@
+from django import forms
+from base_subject_model_form import BaseSubjectModelForm
+from bcpp_subject.models import ReproductiveHealth
+
+
+class ReproductiveHealthForm (BaseSubjectModelForm):
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        if cleaned_data.get('menopause') == 'Yes' and cleaned_data.get('family_planning'): 
+            raise forms.ValidationError('if participant has reached menopause, you should not be giving details about family planning')
+        if cleaned_data.get('menopause') == 'No' and not cleaned_data.get('family_planning'): 
+            raise forms.ValidationError('if participant has not reached menopause, provide the family planning details')
+        # if no, don't answer next question
+        if cleaned_data.get('menopause') == 'Yes' and  cleaned_data.get('current_pregnant'):
+            raise forms.ValidationError('If participant has reached menopause, do not give details about current pregnancy')
+        if cleaned_data.get('menopause') == 'No' and  not cleaned_data.get('current_pregnant'):
+            raise forms.ValidationError('If participant has not reached menopause, we need to know if partipant is currently pregnant or not.')
+
+        return cleaned_data
+
+
+    class Meta:
+        model = ReproductiveHealth
