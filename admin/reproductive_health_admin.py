bf668e082351590e78295284e628b45ae9414242
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-17 07:45:26 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@393 5066620e-b066-4cb9-a883-3cd329b6f9ec

---
+++ admin/reproductive_health_admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from subject_visit_model_admin import SubjectVisitModelAdmin
+from bcpp_subject.models import ReproductiveHealth
+from bcpp_subject.forms import ReproductiveHealthForm
+
+
+
+class ReproductiveHealthAdmin(SubjectVisitModelAdmin):
+
+    form = ReproductiveHealthForm
+    fields = (
+        "subject_visit",
+        "number_children",
+        "menopause",
+        "family_planning",
+        "family_planning_other",
+        "currently_pregnant",
+        )
+    radio_fields = {
+        "menopause": admin.VERTICAL,
+        "currently_pregnant": admin.VERTICAL,}
+    filter_horizontal = ("family_planning",)
+    required_instructions = ("Note to Interviewer: This section is to be"
+                             " completed by female participants. SKIP for male participants."
+                             "Read to Participant: I am now going to ask you questions"
+                             " about reproductive health and pregnancy.")
+admin.site.register(ReproductiveHealth, ReproductiveHealthAdmin)
