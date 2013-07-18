646a52f421624f1d47b502c4d185b6f3c6ffd5b8
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-07-18 11:34:48 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_contact@4 1f107553-88b7-42ae-a9bf-5c8de24fe7ab

--- forms/base_contact_log_item_form_cleaner.py
+++ forms/base_contact_log_item_form_cleaner.py
@@ -17,6 +17,8 @@ class BaseContactLogItemFormCleaner (object):
 #        )
 
     def clean(self, cleaned_data):
+        if not cleaned_data.get('is_contacted', None):
+            raise forms.ValidationError('Please select Yes or No')
         if cleaned_data.get('is_contacted').lower() == 'no' and cleaned_data.get('information_provider'):
             raise forms.ValidationError('You wrote contact was NOT made yet have recorded the information provider. Please correct.')
         if cleaned_data.get('is_contacted').lower() == 'yes' and not cleaned_data.get('information_provider'):
