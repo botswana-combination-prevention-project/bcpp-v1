d10ccc85c8e448357c7ae1a145749c88d9974241
erik van widenfelt <ew2789@gmail.com>
2013-07-18 05:17:41 +0000

    reset migrations
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@405 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- section.py
+++ section.py
@@ -7,6 +7,6 @@ class SectionSubjectView(BaseSectionView):
     section_display_name = 'Subjects'
     section_display_index = 20
     section_template = 'section_subject.html'
-    add_model = SubjectConsent
+    #add_model = SubjectConsent
 
 site_sections.register(SectionSubjectView)
