ea7cea8eebbd52f6c91bd604bd246f1816953242
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-07-19 06:29:55 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@415 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- admin/__init__.py
+++ admin/__init__.py
@@ -3,7 +3,9 @@ from subject_consent_admin import SubjectConsentAdmin
 from subject_locator_admin import SubjectLocatorAdmin
 from subject_refusal_admin import SubjectRefusalAdmin
 from subject_absentee_admin import *
+from subject_undecided_admin import *
 from subject_moved_admin import SubjectMovedAdmin
+
 from subject_death_admin import SubjectDeathAdmin
 from partner_admin import RecentPartnerAdmin, SecondPartnerAdmin, ThirdPartnerAdmin
 from residency_mobility_admin import ResidencyMobilityAdmin
