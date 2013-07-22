1730c7178833078cedaac875d86bb4538f5b0abb
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-07-22 14:58:33 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_consent@268 833481ee-4f4c-48f6-8756-2185fbac41e5

--- models/base_consent_update.py
+++ models/base_consent_update.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.core.validators import RegexValidator
 from django.core.exceptions import ImproperlyConfigured
 from bhp_sync.models import BaseSyncUuidModel
