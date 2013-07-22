2db864ed6d9bdb3475d88b681dfbda12114cd1c5
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-07-22 15:09:02 +0000

    git-svn-id: http://192.168.1.50/svn/lab_requisition@101 9a8ae1fb-4a67-40b5-aa50-fceefa09fc97

--- filters.py
+++ filters.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.contrib.admin import SimpleListFilter
 
 
