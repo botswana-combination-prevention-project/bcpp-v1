b57e459f49f402d1d99ab0dc63b649c6ba7a84b8
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-07-22 14:54:25 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_adverse@63 89b61199-f52f-466d-8b3a-10004189116d

--- models/simple_adverse_event.py
+++ models/simple_adverse_event.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 from bhp_base_model.models import BaseListModel
 from bhp_consent.models import BaseConsentedUuidModel
