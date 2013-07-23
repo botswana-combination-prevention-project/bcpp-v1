18a2a9cba68f8fe6d80eb19d739f441837083e3c
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-07-23 05:46:28 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household@42 30821849-012b-4b24-87a7-59ccb7ae08db

--- models/signals.py
+++ models/signals.py
@@ -1,21 +1,6 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from household_member import HouseholdMember, HouseholdStructure
-
-
-@receiver(pre_save, weak=False, dispatch_uid="household_member_on_pre_save")
-def household_member_on_pre_save(sender, instance, **kwargs):
-    if not kwargs.get('raw', False):
-        if isinstance(instance, HouseholdMember):
-            instance.update_hiv_history_on_pre_save(**kwargs)
-
-
-@receiver(post_save, weak=False, dispatch_uid="household_member_on_post_save")
-def household_member_on_post_save(sender, instance, **kwargs):
-    if not kwargs.get('raw', False):
-        if isinstance(instance, HouseholdMember):
-            instance.update_registered_subject_on_post_save(**kwargs)
-
+from household_member import HouseholdStructure
 
 @receiver(post_save, weak=False, dispatch_uid="household_structure_on_post_save")
 def household_structure_on_post_save(sender, instance, **kwargs):
