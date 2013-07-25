16cb08390059c51a3bdc22e9ced4d776536b5e27
erik van widenfelt <ew2789@gmail.com>
2013-07-25 15:18:05 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_model@50 69677513-6300-4d4b-a1c0-2a4462c8f59b

--- validators/date.py
+++ validators/date.py
@@ -3,30 +3,35 @@ from django.core.exceptions import ValidationError
 from bhp_variables.models import StudySpecific
 
 
-def IsTodayDateTime (value):
+def IsTodayDateTime(value):
     pass
 
-def datetime_not_future (value):
+
+def datetime_not_future(value):
     time_error = timedelta(minutes=10)
     if value > datetime.now() + time_error:
         raise ValidationError(u'Date cannot be a future date and time. You entered %s' % (value,))
 
-def date_not_future (value):
+
+def date_not_future(value):
     now = date.today()
     if value > now:
         raise ValidationError(u'Date cannot be a future date. You entered %s' % (value,))
 
-def datetime_is_future (value):
+
+def datetime_is_future(value):
     time_error = timedelta(minutes=10)
     if value < datetime.now() + time_error:
         raise ValidationError(u'Date must be a future date and time. You entered %s' % (value,))
 
-def date_is_future (value):
+
+def date_is_future(value):
     now = date.today()
     if value < now:
         raise ValidationError(u'Date must be a future date. You entered %s' % (value,))
 
-def datetime_is_after_consent (value):
+
+def datetime_is_after_consent(value):
     """ not working..."""
     now = value
     if value != now:
@@ -36,7 +41,10 @@ def datetime_is_after_consent (value):
 def date_not_before_study_start(value):
     if StudySpecific.objects.all().count() == 0:
         raise ValidationError('bhp_variables form StudySpecific has not been completed. Please do this first in admin section bhp_variables.')
-    study_specific = StudySpecific.objects.all()[0]
+    try:
+        study_specific = StudySpecific.objects.all()[0]
+    except IndexError:
+        raise ValidationError(u'Study Specific configuration data not available. Please update the config table. See bhp_variables.')
     value_datetime = datetime(value.year, value.month, value.day, 0, 0)
     started = study_specific.study_start_datetime
     protocol_number = study_specific.protocol_number
@@ -44,9 +52,14 @@ def date_not_before_study_start(value):
         raise ValidationError(u'Date cannot be before the study started. %s started on %s. You entered %s.' % (protocol_number, started, value,))
 
 
-def datetime_not_before_study_start (value):
-    dte = StudySpecific.objects.all()[0]
-    started = dte.study_start_datetime
-    protocol_number = dte.protocol_number
+def datetime_not_before_study_start(value):
+    if StudySpecific.objects.all().count() == 0:
+        raise ValidationError('bhp_variables form StudySpecific has not been completed. Please do this first in admin section bhp_variables.')
+    try:
+        study_specific = StudySpecific.objects.all()[0]
+    except IndexError:
+        raise ValidationError(u'Study Specific configuration data not available. Please update the config table. See bhp_variables.')
+    started = study_specific.study_start_datetime
+    protocol_number = study_specific.protocol_number
     if value < started:
         raise ValidationError(u'Date and time cannot be before the study started. %s started on %s. You entered %s.' % (protocol_number, started, value,))
