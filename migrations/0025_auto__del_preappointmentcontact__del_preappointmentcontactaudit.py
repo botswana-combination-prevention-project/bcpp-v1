5da75a5843d8a6206f206c4e3b8c6beb2a6f734f
erik van widenfelt <ew2789@gmail.com>
2013-07-25 08:25:43 +0000

    migrations 25/26
    git-svn-id: http://192.168.1.50/svn/bhp_appointment@232 0d19f2bb-31f9-471a-9829-a57abbf0ef71

--- migrations/0025_auto__del_preappointmentcontact__del_preappointmentcontactaudit.py
+++ migrations/0025_auto__del_preappointmentcontact__del_preappointmentcontactaudit.py
@@ -8,7 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        
         # Adding index on 'AppointmentAudit', fields ['_audit_subject_identifier']
         db.create_index('bhp_appointment_appointment_audit', ['_audit_subject_identifier'])
         
