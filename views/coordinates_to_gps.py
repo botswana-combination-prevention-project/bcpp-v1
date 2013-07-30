88878865ab5f7beefd2172d72ee5296fd795ea1c
coulson kgathi <ckgathi@bhp.org.bw>
2013-07-30 10:27:32 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_map@67 87996a1b-1222-4716-82a7-21449bde0c3d

--- views/coordinates_to_gps.py
+++ views/coordinates_to_gps.py
@@ -1,5 +1,6 @@
 import os
 # Import django modules
+from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from bhp_map.classes import site_mappers
@@ -12,22 +13,6 @@ def coordinates_to_gps(request, **kwargs):
     
     fname: is an already existing file
     """
-    FNAME = '/Users/ckgathi/source/bhp066/Current.gpx'
-    file_to_gps = '/Volumes/GARMIN/GPX/Current.gpx'
-    
-    f = open(FNAME, 'r')
-    line = f.readline()
-    lines = f.read()
-    f.close()
-    
-    
-    try:
-        os.remove(file_to_gps)
-    except OSError:
-        pass
-    wf = open(file_to_gps, 'a')
-    wf.write(line)
-
     template = 'sent.html'
     mapper_item_label = kwargs.get('mapper_item_label', '')
     mapper_name = kwargs.get('mapper_name', '')
@@ -37,28 +22,43 @@ def coordinates_to_gps(request, **kwargs):
     else:
         m = site_mappers.get_registry(mapper_name)()
         
-        #This values need to come from the edc   
-        items = m.get_item_model_cls().objects.all()
-        print str(getattr(items[0], m.get_identifier_field_attr()))
-        for item in items:
-            identifier_name = str(getattr(item, m.get_identifier_field_attr()))
-            lat = item.lat 
-            lon = item.lon
-            ele = 0.0
-            city_village = m.get_map_area()
-            str_from_edc = '<name>' + str(identifier_name) + '</name><sym>Waypoint</sym><extensions><gpxx:WaypointExtension><gpxx:Categories><gpxx:Category>Map Points and Coordinates</gpxx:Category></gpxx:Categories><gpxx:Address><gpxx:City>' + str(city_village) + '</gpxx:City><gpxx:State>South Eastern</gpxx:State></gpxx:Address></gpxx:WaypointExtension></extensions>' + '</wpt>' + '<wpt lat="' + str(lat) + '" lon="' + str(lon) + '"><ele>' + str(ele) + '</ele>'
+        if settings.DEVICE_ID == '99':
+            raise MapperError('You are in the server, You can\'t dispatch the whole server data to a GPS receiver.')
+        else:    
+            FNAME = os.path.abspath('bhp_map/static/gpx/Current.gpx')
+            f = open(FNAME, 'r')
+            line = f.readline()
+            lines = f.read()
+            f.close()
             
-            #write to gps file from database
-            wf.write(str_from_edc)
-        wf.write(lines)
-        wf.close()
-    return render_to_response(
-            template, {
-                'mapper_name': mapper_name,
-                'file_to_gps': os.path.exists(file_to_gps)
-            },
-            context_instance=RequestContext(request)
-        ) 
-        
+            try:
+                os.remove(settings.GPS_FILE_PATH)
+            except OSError:
+                pass
+            wf = open(settings.GPS_FILE_PATH, 'a')
+            wf.write(line)
+            
+            #This values need to come from the edc   
+            items = m.get_item_model_cls().objects.all()
+            print str(getattr(items[0], m.get_identifier_field_attr()))
+            for item in items:
+                identifier_name = str(getattr(item, m.get_identifier_field_attr()))
+                lat = item.lat 
+                lon = item.lon
+                ele = 0.0
+                city_village = m.get_map_area()
+                str_from_edc = '<name>' + str(identifier_name) + '</name><sym>Waypoint</sym><extensions><gpxx:WaypointExtension><gpxx:Categories><gpxx:Category>Map Points and Coordinates</gpxx:Category></gpxx:Categories><gpxx:Address><gpxx:City>' + str(city_village) + '</gpxx:City><gpxx:State>South Eastern</gpxx:State></gpxx:Address></gpxx:WaypointExtension></extensions>' + '</wpt>' + '<wpt lat="' + str(lat) + '" lon="' + str(lon) + '"><ele>' + str(ele) + '</ele>'
+                
+                #write to gps file from database
+                wf.write(str_from_edc)
+            wf.write(lines)
+            wf.close()
+        return render_to_response(
+                template, {
+                    'mapper_name': mapper_name,
+                    'file_to_gps': os.path.exists(settings.GPS_FILE_PATH)
+                },
+                context_instance=RequestContext(request)
+            )
         
         
