67b3036a2b9e49f200e8d570c2b4e0686bf17d9b
panto motshegwa <pmotshegwa@bhp.org.bw>
2013-07-31 10:21:00 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household_member@32 0e2d9bf9-3001-43ce-ab37-2eca76889215

---
+++ tests/natural_key_tests.py
@@ -0,0 +1,34 @@
+import pprint
+from django.test import TestCase
+from django.core import serializers
+from bhp_crypto.classes import FieldCryptor
+
+from django.db.models import get_app, get_models
+from bhp_lab_tracker.classes import lab_tracker
+from bhp_sync.classes import SerializeToTransaction
+from bhp_variables.tests.factories import StudySpecificFactory, StudySiteFactory
+from bhp_registration.models import RegisteredSubject
+from bhp_consent.tests.factories import ConsentCatalogueFactory
+from bhp_appointment.models import Appointment
+from bhp_appointment.tests.factories import ConfigurationFactory
+from bhp_visit.tests.factories import MembershipFormFactory, ScheduleGroupFactory, VisitDefinitionFactory
+from bhp_content_type_map.classes import ContentTypeMapHelper
+from bhp_content_type_map.models import ContentTypeMap
+
+class NaturalKeyTests(TestCase):
+
+    def test_p1(self):
+        """Confirms all models have a natural_key method (except Audit models)"""
+        app = get_app('bcpp_household_member')
+        for model in get_models(app):
+            if 'Audit' not in model._meta.object_name:
+                print 'checking for natural key on {0}.'.format(model._meta.object_name)
+                self.assertTrue('natural_key' in dir(model), 'natural key not found in {0}'.format(model._meta.object_name))
+
+    def test_p2(self):
+        """Confirms all models have a get_by_natural_key manager method."""
+        app = get_app('bcpp_household_member')
+        for model in get_models(app):
+            if 'Audit' not in model._meta.object_name:
+                print 'checking for get_by_natural_key manager method key on {0}.'.format(model._meta.object_name)
+                self.assertTrue('get_by_natural_key' in dir(model.objects), 'get_by_natural_key key not found in {0}'.format(model._meta.object_name))
