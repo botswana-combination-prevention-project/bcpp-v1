9cff22eb508a147423bf2d6818fdb509f8b2a1f2
erik van widenfelt <ew2789@gmail.com>
2013-07-31 09:58:31 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household@100 30821849-012b-4b24-87a7-59ccb7ae08db

--- search.py
+++ search.py
@@ -7,4 +7,5 @@ class HouseholdSearchByWord(BaseSearchByWord):
     section_name = 'household'
     search_model = Household  # , HouseholdStructure, HouseholdStructureMember)
     order_by = 'household_identifier'
+    template = 'search_result_include.html'
 site_search.register(HouseholdSearchByWord)
