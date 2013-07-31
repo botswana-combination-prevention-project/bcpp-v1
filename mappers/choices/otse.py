6a26a81ec4f2e8b2318300ce6455084039ca05ef
erik van widenfelt <ew2789@gmail.com>
2013-07-31 05:37:20 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household@86 30821849-012b-4b24-87a7-59ccb7ae08db

---
+++ mappers/choices/otse.py
@@ -0,0 +1,14 @@
+
+OTSE_SECTIONS = (
+    ('A', 'A'),
+)
+
+OTSE_SUB_SECTIONS = (
+    ('1', '1'),
+)
+
+
+OTSE_LANDMARKS = (
+    ('Otse center', -25.033194, 25.747132),
+    ('Otse Stadium', -25.026117, 25.746608)
+)
