435c6e2c281fb2e38b25d6880eae3332d1c46d69
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-02 09:48:06 +0000

    should validation be omitted for fields that could be left blank if participant does not want to answer?
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@480 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/sexual_behaviour_form.py
+++ forms/sexual_behaviour_form.py
@@ -10,9 +10,18 @@ class SexualBehaviourForm (BaseSubjectModelForm):
         cleaned_data = self.cleaned_data
         #validating having had sex and no sex
         if cleaned_data.get('ever_sex') == 'No' and cleaned_data.get('lifetime_sex_partners') and cleaned_data.get('last_year_partners'):
-            raise forms.ValidationError('If participant has NEVER had sex, do not provide details about sexual partners')
+            raise forms.ValidationError('If participant has NEVER had sex, DO NOT provide details about sexual partners')
         if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('lifetime_sex_partners') and not cleaned_data.get('last_year_partners'):
             raise forms.ValidationError('If participant has had sex at some point in their life, give details about sexual partners')
+        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('more_sex'):
+            raise forms.ValidationError('If participant has had sex at some point in their life, has participant had sex with anyone outside community?')
+        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('first_sex'):
+            raise forms.ValidationError('If participant has had sex at some point in their life, how old was the participant when he/she first had sex?')
+        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('condom'):
+            raise forms.ValidationError('If participant has had sex at some point in their life, did participant use a condom the last time he/she had sex?')
+        if cleaned_data.get('ever_sex') == 'Yes' and not cleaned_data.get('alcohol_sex'):
+            raise forms.ValidationError('If participant has had sex at some point in their life, did participant drink alcohol before sex last time?')
+        
 
         cleaned_data = super(SexualBehaviourForm, self).clean()
         return cleaned_data
