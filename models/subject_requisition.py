851463990ee1c9d8b105c8cfe1cea64105a19c07
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-02 09:51:26 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_lab@9 b3748bed-73d4-46e5-98ad-97fdbb4f1fbe

--- models/subject_requisition.py
+++ models/subject_requisition.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.conf import settings
 from audit_trail.audit import AuditTrail
 from lab_requisition.models import BaseRequisition
 from bcpp_subject.models import SubjectVisit
 from packing_list import PackingList
+from bcpp_inspector.models import SubjectRequisitionInspector
 
 
 class SubjectRequisition(BaseRequisition):
@@ -17,7 +19,18 @@ class SubjectRequisition(BaseRequisition):
         editable=False)
 
     history = AuditTrail()
-
+    
+    def save_to_inspector(self):
+        SubjectRequisitionInspector.objects.create(
+                subject_identifier = self.subject_identifier,
+                requisition_datetime = self.requisition_datetime,
+                requisition_identifier = self.requisition_identifier,
+                specimen_identifier = self.specimen_identifier,
+                device_id = settings.DEVICE_ID,
+                app_name = 'bcpp_lab',
+                model_name = 'SubjectRequisition'                
+                )
+    
     def save(self, *args, **kwargs):
         self.subject_identifier = self.get_visit().get_subject_identifier()
         super(SubjectRequisition, self).save(*args, **kwargs)
