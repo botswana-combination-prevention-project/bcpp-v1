90b370831112fadd6e2819a4647182e6cba19a8b
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-02 09:00:00 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@313 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- models/middle_man_transaction.py
+++ models/middle_man_transaction.py
@@ -1,6 +1,8 @@
 from django.db import models
 from base_transaction import BaseTransaction
-
+from django.conf import settings
+from datetime import datetime
+from bhp_sync.classes import TransactionProducer, DeserializeFromTransaction
 
 class MiddleManTransaction(BaseTransaction):
 
@@ -16,6 +18,19 @@ class MiddleManTransaction(BaseTransaction):
         db_index=True,
         )
     
+    def save(self, *args, **kwargs):
+        if self.is_consumed_server and not self.consumed_datetime:
+            self.consumed_datetime = datetime.today()
+        if not 'MIDDLE_MAN' in dir(settings) or not settings.MIDDLE_MAN:
+            raise TypeError('\'{0}\' is not configured to be a MiddleMan, so you cannot save MiddleMan transanctions here.'.format(settings.DEVICE_ID))
+        super(MiddleManTransaction, self).save(*args, **kwargs)
+        
+    def deserialize_to_inspector_on_post_save(self, **kwargs):
+        object = DeserializeFromTransaction().deserialize_and_not_save(self)
+        if object and 'save_to_inspector' in dir(object):
+            object.save_to_inspector()
+            #pass
+            
     objects = models.Manager() 
     class Meta:
         app_label = 'bhp_sync'
