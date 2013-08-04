ee1fc8363af6d0be50369ebcde6b8823222e289a
erik van widenfelt <ew2789@gmail.com>
2013-08-04 19:05:58 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit@155 c3dbc7c3-30f2-42a5-aac4-8163f1b0d3d7

--- classes/membership_form_helper.py
+++ classes/membership_form_helper.py
@@ -1,13 +1,157 @@
-from bhp_visit.models import MembershipForm, VisitDefinition
+from django.db import models
+from django.core.exceptions import ImproperlyConfigured
+from bhp_base_model.models import BaseModel
+from bhp_appointment_helper.models import BaseAppointmentMixin
 
 
 class MembershipFormHelper(object):
 
-    def codes_for_category(self, membership_form_category):
-        """ Lists visit codes for this membership form category."""
-        membership_forms = MembershipForm.objects.filter(category=membership_form_category)
-        visit_definition_codes = set()
-        for membership_form in membership_forms:
-            for visit_definition in VisitDefinition.objects.filter(schedule_group__membership_form=membership_form):
-                visit_definition_codes.add(visit_definition.code)
-        return list(visit_definition_codes)
+    def __init__(self, *args, **kwargs):
+        super(MembershipFormHelper, self).__init__(*args, **kwargs)
+        self._keyed = None
+        self._unkeyed = None
+        self._grouping_key = None
+        self._category = None
+        self._model = None
+
+    def get_membership_models_for(self, registered_subject, membership_form_category, **kwargs):
+
+        """ Returns dict of keyed model instances and unkeyed model classes or "membership forms" for a given registered_subject.
+
+        Specify the registered_subject and the membership_form_category.
+        """
+        ScheduleGroup = models.get_model('bhp_visit', 'ScheduleGroup')
+        extra_grouping_key = kwargs.get("exclude_others_if_keyed_model_name", None)
+        self._set_keyed()
+        self._set_unkeyed()
+        self._set_category(membership_form_category)
+        for schedule_group in ScheduleGroup.objects.filter(membership_form__category__iexact=self._get_category()):
+            self._set_model(schedule_group=schedule_group)
+            if self._get_model().objects.filter(registered_subject_id=registered_subject.pk):
+                self._add_keyed(schedule_group.grouping_key, self._get_model().objects.get(registered_subject=registered_subject))
+            else:
+                self._add_unkeyed(schedule_group.grouping_key, self._get_model())
+        self._remove_unkeyed_by_grouping_key()
+        self._remove_unkeyed_by_extra_grouping_key(extra_grouping_key)
+        return self._format_for_return()
+
+    def _set_keyed(self):
+        self._keyed = {}
+
+    def _get_keyed(self):
+        if self._keyed == None:
+            self._set_keyed()
+        return self._keyed
+
+    def _add_keyed(self, group, obj):
+        if not group:
+            group = 'no_group'
+        if not isinstance(group, basestring):
+            raise TypeError('Expected parameter group to be a string')
+        if not isinstance(obj, BaseAppointmentMixin):
+            raise TypeError('Expected an instance of a model class using mixin BaseAppointmentMixin')
+        if group in self._get_keyed():
+            self._get_keyed()[group].append(obj)
+        else:
+            self._get_keyed().update({group: [obj]})
+
+    def _set_unkeyed(self):
+        self._unkeyed = {}
+
+    def _add_unkeyed(self, group, cls):
+        if not group:
+            group = 'no_group'
+        if not isinstance(group, basestring):
+            raise TypeError('Expected parameter group to be a string')
+        if not issubclass(cls, BaseAppointmentMixin):
+            raise TypeError('Expected a model class using mixin BaseAppointmentMixin')
+        if group in self._get_unkeyed():
+            self._get_unkeyed()[group].append(cls)
+        else:
+            self._get_unkeyed().update({group: [cls]})
+
+    def _get_unkeyed(self):
+        if self._unkeyed == None:
+            self._set_unkeyed()
+        return self._unkeyed
+
+    def _set_category(self, value=None):
+        """Sets the category (MembershipForm.category)."""
+        self._category = None
+        if self._is_configured_for_category(value):
+            self._category = value
+        if not self._category:
+            raise TypeError('Attribute category may not be None.')
+
+    def _get_category(self):
+        if not self._category:
+            self._set_category()
+        return self._category
+
+    def _set_model(self, cls=None, schedule_group=None):
+        """Sets the model class of the model that is either keyed or unkeyed.
+
+        Either uses the content_type associated with the MembershipForm instance
+        or uses the cls parameter.
+
+        Model class must have a key to registered_subject and may not be None."""
+        self._model = None
+        ScheduleGroup = models.get_model('bhp_visit', 'ScheduleGroup')
+        if isinstance(schedule_group, ScheduleGroup):
+            if not schedule_group.membership_form.content_type_map.model_class():
+                raise ImproperlyConfigured('Cannot get model class from content_type_map for schedule group \'{0}\' using \'{1}\'. Update content_type_map?'.format(schedule_group, schedule_group.membership_form))
+            else:
+                self._model = schedule_group.membership_form.content_type_map.model_class()
+        if isinstance(cls, BaseModel):
+            self._model = cls
+        if 'registered_subject' not in dir(self._model):
+            raise ImproperlyConfigured('Model require attribute \'registered_subject\'. Model \'%s\' does not have this attribute but is listed as a membership form.' % schedule_group.membership_form.content_type_map.name)
+        if not self._model:
+            raise TypeError('Attribute _model may not be None.')
+
+    def _get_model(self):
+        if not self._model:
+            self._set_model()
+        return self._model
+
+    def _is_configured_for_category(self, category=None):
+        """Confirms membership forms exist for this category."""
+        MembershipForm = models.get_model('bhp_visit', 'MembershipForm')
+        ScheduleGroup = models.get_model('bhp_visit', 'ScheduleGroup')
+        if not MembershipForm.objects.filter(category__iexact=category).exists():
+            raise ImproperlyConfigured('Can\'t find any membership forms! Have you configured any for category \'{0}\'.'.format(category))
+        if not ScheduleGroup.objects.filter(membership_form__category__iexact=category).exists():
+            raise ImproperlyConfigured('Can\'t find any schedule groups! Have you configured any for category \'{0}\'.'.format(category))
+        return True
+
+    def _remove_unkeyed_by_grouping_key(self):
+        """Removes from unkeyed if any member of group is in keyed."""
+        for grouping_key in self._get_keyed():
+            if grouping_key in self._get_unkeyed():
+                del self._get_unkeyed()[grouping_key]
+
+    def _remove_unkeyed_by_extra_grouping_key(self, extra_grouping_key):
+        """Removes from unkeyed if the unkeyed object name startswith string extra_grouping_key."""
+        if extra_grouping_key:
+            for inst in self._get_keyed().itervalues():
+                if inst._meta.object_name.lower() == extra_grouping_key:
+                    for grouping_key, cls in self._get_unkeyed().iteritems():
+                        if cls._meta.object_name.lower().startswith(extra_grouping_key):
+                            del self._get_unkeyed()[grouping_key]
+
+    def _format_for_return(self):
+        keyed = []
+        for inst in [inst for inst in self._get_keyed().itervalues()]:
+            if isinstance(inst, list):
+                for i in inst:
+                    keyed.append(i)
+            else:
+                keyed.append(inst)
+        unkeyed = []
+        for cls in [cls for cls in self._get_unkeyed().itervalues()]:
+            if isinstance(cls, list):
+                for c in cls:
+                    unkeyed.append(c)
+            else:
+                unkeyed.append(cls)
+        return {'keyed': keyed, 'unkeyed': unkeyed}
--- managers/schedule_group_manager.py
+++ managers/schedule_group_manager.py
@@ -1,10 +1,5 @@
-import re
 import logging
-from django.core.exceptions import FieldError
 from django.db import models
-from django.db.models import Q
-from bhp_dashboard_registered_subject.exceptions import DashboardError, DashboardFieldError
-
 
 logger = logging.getLogger(__name__)
 
@@ -20,78 +15,3 @@ class ScheduleGroupManager(models.Manager):
     def get_by_natural_key(self, group_name):
         """Returns the natural key for serialization."""
         return self.get(group_name=group_name)
-
-    def get_membership_forms_for(self, registered_subject, membership_form_category, **kwargs):
-
-        """ Returns dict of keyed and unkeyed schedule group membership forms for a given registered_subject
-
-        Specify the registered_subject and the membership_form_category. Include forms
-        of the specified membership_form__category AND those that have no category (null) or blank.
-        """
-        MembershipForm = models.get_model('bhp_visit', 'membershipform')
-        # if this model is keyed, exclude all other UNKEYED models LIKE this from the list
-        # for example, if consented, no other membership forms apply and
-        # the links need not show on the dashboard
-        exclude_others_if_keyed_model_name = kwargs.get("exclude_others_if_keyed_model_name", None)
-
-        # TODO: implement 'include_after_exclusion_model_keyed'
-        # ...not working yet because the query would be complicated
-        # if the "exclude" model (above) is keyed, remove all unkeyed models, except those listed here
-        # for example, if the model in 'exclude_others_if_keyed_model_name' is KEYED,
-        # still show links to the models listed here
-        # (naming needs work ...)
-        # include_after_exclusion_model_keyed = kwargs.get("include_after_exclusion_model_keyed", None)
-        # category of the membership form. Can be any value as  long as
-        # it helps link membership forms in some way. For example,
-        # to distinguish 'maternal' from 'infant' forms. Specified at the form level
-
-        #  membership form 'category' should appear in the category field of membership_form.
-        if not MembershipForm.objects.filter(category__iexact=membership_form_category).exists():
-            raise DashboardError('Can\'t find any membership forms! Have you configured any for category \'{0}\'.'.format(membership_form_category))
-        if not super(ScheduleGroupManager, self).filter(membership_form__category__iexact=membership_form_category).exists():
-            raise DashboardError('Can\'t find any schedule groups! Have you configured any for category \'{0}\'.'.format(membership_form_category))
-        # a list of "keys" that link like membership forms together. If they share this
-        # key it means that only one form should be KEYED per subject.
-        # If form is KEYED for subject, there is no need to list the others as UNKEYED
-        # Specified at the scheduledgroup level
-        grouping_keys = []
-        # a list of KEYED forms
-        keyed_membership_forms = {}
-        #get KEYED schedule group membership forms
-        for schedule_group in super(ScheduleGroupManager, self).filter(membership_form__category__iexact=membership_form_category):
-            membership_form_model = schedule_group.membership_form.content_type_map.model_class()
-            if not membership_form_model:
-                raise DashboardError('Cannot get the membership_form model class from content_type_map for schedule group \'{0}\' using \'{1}\'. Update content_type_map?'.format(schedule_group, schedule_group.membership_form))
-            try:
-                if membership_form_model.objects.filter(registered_subject_id=registered_subject.pk):
-                    # append grouping key for schedule groups
-                    # where the membership_form is KEYED and the schedule group
-                    # has more than one membership_form
-                    if schedule_group.grouping_key:
-                        grouping_keys.append(schedule_group.grouping_key)
-                    # add KEYED model
-                    keyed_membership_forms.update({schedule_group.membership_form.content_type_map.name: membership_form_model.objects.get(registered_subject=registered_subject)})
-            except FieldError:
-                # raise error is attribute is missing
-                raise DashboardFieldError('Membership forms require attribute \'registered_subject\'. Model \'%s\' does not have this attribute but is listed as a membership form.' % schedule_group.membership_form.content_type_map.name)
-        #get UNKEYED schedule group membership forms
-        # ...use the grouping key to eliminate membership forms related to a KEYED membership form from above
-        qset = (
-            (Q(membership_form__category__iexact=membership_form_category) |
-            Q(membership_form__category__isnull=True) |
-            Q(membership_form__category__exact=''))
-            )
-        # if 'exclude_others_if_keyed_model_name' is set it might contain the exact or part of
-        # the module name. For example, subjectconsent should match subjectconsentyearzero
-        # If True, add this to the filter for the list of UNKEYED membership forms
-        exclude = False
-        for v in keyed_membership_forms.values():
-            if re.search(exclude_others_if_keyed_model_name.replace('_', ''), v._meta.object_name.lower()):
-                exclude = True
-        if exclude_others_if_keyed_model_name and exclude:
-            qset.add(Q(membership_form__content_type_map__name__icontains=exclude_others_if_keyed_model_name), Q.AND)
-        schedule_groups = super(ScheduleGroupManager, self).filter(qset)
-        unkeyed_membership_forms = [schedule_group.membership_form.content_type_map
-                                    for schedule_group in schedule_groups
-                                        if schedule_group.grouping_key not in grouping_keys and schedule_group.membership_form.content_type_map.name not in keyed_membership_forms]
-        return {'keyed': keyed_membership_forms, 'unkeyed': unkeyed_membership_forms}
