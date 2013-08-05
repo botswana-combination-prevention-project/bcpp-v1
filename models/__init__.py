4acc43dac4e3e0e02ee78cb068c7b23e7c6d4d86
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-05 11:01:38 +0000

    HTC
    git-svn-id: http://192.168.1.50/svn/bcpp_household_member@42 0e2d9bf9-3001-43ce-ab37-2eca76889215

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from household_member import HouseholdMember
 from enrolment_checklist import EnrolmentChecklist
+from htc_data import HtcData
 from contact_log import ContactLog
 from signals import *
