2fe1e31af531ca662250fdee827cb99ee176a489
erik van widenfelt <ew2789@gmail.com>
2013-08-05 08:14:24 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_templatetags@8 bd461fbe-67f1-4b81-a55f-d9b538d9b96f

--- templatetags/bhp_admin_modify.py
+++ templatetags/bhp_admin_modify.py
@@ -1,37 +1,40 @@
 from django import template
-#from django.contrib.admin.templatetags.admin_modify import submit_row as original_submit_row
+from django.contrib.admin.templatetags.admin_modify import submit_row as original_submit_row
 
 register = template.Library()
 
-# @register.inclusion_tag('admin/submit_line.html', takes_context=True)
-# def submit_row(context):
-#     ctx = original_submit_row(context)
-#     ctx.update({
-#         'show_save_and_add_another': context.get('show_save_and_add_another', ctx['show_save_and_add_another']),
-#         'show_save_and_continue': context.get('show_save_and_continue', ctx['show_save_and_continue'])
-#         })
-#     return ctx
-
 
 @register.inclusion_tag('admin/submit_line.html', takes_context=True)
-def bhp_submit_row(context):
-    """
-    Displays the row of buttons for delete and save.
-    """
-    opts = context['opts']
-    change = context['change']
+def submit_row(context):
+    ctx = original_submit_row(context)
     is_popup = context['is_popup']
-    save_as = context['save_as']
-    return {
-        'onclick_attrib': (opts.get_ordered_objects() and change
-                            and 'onclick="submitOrderForm();"' or ''),
-        'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
-        'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': context['has_add_permission'] and
-                            not is_popup and (not save_as or context['add']),
-        'show_save_and_continue': not is_popup and context['has_change_permission'],
-        'is_popup': is_popup,
-        'show_save': True,
+    ctx.update({
+        #'show_save_and_add_another': context.get('show_save_and_add_another', ctx['show_save_and_add_another']),
+        #'show_save_and_continue': context.get('show_save_and_continue', ctx['show_save_and_continue'])
         'show_savenext': (not is_popup and context['add'])
-    }
+        })
+    return ctx
+
+
+# @register.inclusion_tag('admin/submit_line.html', takes_context=True)
+# def bhp_submit_row(context):
+#     """
+#     Displays the row of buttons for delete and save.
+#     """
+#     opts = context['opts']
+#     change = context['change']
+#     is_popup = context['is_popup']
+#     save_as = context['save_as']
+#     return {
+#         'onclick_attrib': (opts.get_ordered_objects() and change
+#                             and 'onclick="submitOrderForm();"' or ''),
+#         'show_delete_link': (not is_popup and context['has_delete_permission']
+#                               and (change or context['show_delete'])),
+#         'show_save_as_new': not is_popup and change and save_as,
+#         'show_save_and_add_another': context['has_add_permission'] and
+#                             not is_popup and (not save_as or context['add']),
+#         'show_save_and_continue': not is_popup and context['has_change_permission'],
+#         'is_popup': is_popup,
+#         'show_save': True,
+#         'show_savenext': (not is_popup and context['add'])
+#     }
