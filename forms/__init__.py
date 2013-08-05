74549f78b9cbc04cd9b91f875db7b3fa25247758
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-05 11:03:01 +0000

    HTC
    git-svn-id: http://192.168.1.50/svn/bcpp_household_member@45 0e2d9bf9-3001-43ce-ab37-2eca76889215

--- forms/__init__.py
+++ forms/__init__.py
@@ -1,4 +1,4 @@
 from household_member_form import HouseholdMemberForm
 from participation_form import ParticipationForm
 from enrolment_checklist_form import EnrolmentChecklistForm
-from htc_data_from import HtcDataForm
+from htc_data_form import HtcDataForm
