677b8f51fcc3e3944de5901dd97f00330401ae01
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-06 14:34:20 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@509 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/education_form.py
+++ forms/education_form.py
@@ -7,18 +7,22 @@ from bcpp_subject.models import Education
 class EducationForm (BaseSubjectModelForm):
     
     def clean(self):
-
+ 
         cleaned_data = self.cleaned_data
         # validating not working
-        if cleaned_data.get('job_type') == 'not working' and not cleaned_data.get('reason_unemployed'):
+        if cleaned_data.get('working') == 'No' and cleaned_data.get('job_type'):
+            raise forms.ValidationError('If participant is not working, do not give job type')
+        if cleaned_data.get('working') == 'No' and not cleaned_data.get('reason_unemployed'):
             raise forms.ValidationError('If participant is not working, provide reason for unemployment')
-        if cleaned_data.get('job_type') != 'not working' and not cleaned_data.get('job_description'):
+        if cleaned_data.get('working') == 'Yes' and not cleaned_data.get('job_type'):
+            raise forms.ValidationError('If participant is working, provide the job type')
+        if cleaned_data.get('working') != 'Yes' and not cleaned_data.get('job_description'):
             raise forms.ValidationError('If participant is employed, what is the job description')
-        if cleaned_data.get('job_type') != 'not working' and not cleaned_data.get('monthly_income'):
+        if cleaned_data.get('working') != 'Yes' and not cleaned_data.get('monthly_income'):
             raise forms.ValidationError('If participant is employed, what is his/her monthly income?')
-
+ 
         cleaned_data = super(EducationForm, self).clean()
-
+ 
         return cleaned_data
 
 
