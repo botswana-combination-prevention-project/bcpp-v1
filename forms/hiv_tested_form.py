55200e02159b7092992e93272ed1d417b8152a12
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-06 13:25:59 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@507 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/hiv_tested_form.py
+++ forms/hiv_tested_form.py
@@ -13,7 +13,7 @@ class HivTestedForm (BaseSubjectModelForm):
         # if no, don't answer next question
         if cleaned_data.get('hiv_pills') == 'No' and  cleaned_data.get('arvs_hiv_test'):
             raise forms.ValidationError('You are answering information about ARV\'s yet have answered \'NO\', patient has never heard about ARV\'s. Please correct')
-        if cleaned_data.get('hiv_pills') == 'Yes' or cleaned_data.get('hiv_pills') == 'not sure' and not cleaned_data.get('arvs_hiv_test'):
+        if (cleaned_data.get('hiv_pills') == 'Yes' or cleaned_data.get('hiv_pills') == 'not sure') and not cleaned_data.get('arvs_hiv_test'):
             raise forms.ValidationError('if participant has heard about ARV\'s, provide information whether he/she believes that HIV positive can live longer if taking ARV\'s')
 
         return cleaned_data
