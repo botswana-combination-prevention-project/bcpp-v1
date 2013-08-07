0b84c83426d91dc1727ffa36b14b83dd195b0a90
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-07 08:13:46 +0000

    changed
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@514 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- models/subject_refusal.py
+++ models/subject_refusal.py
@@ -23,12 +23,12 @@ class SubjectRefusal (BaseMemberStatusModel):
         help_text="Leave blank If participant does not want to answer.",
         )
 
-    length_residence = models.CharField(
-        verbose_name="How long have your lived in this community?",
-        max_length=25,
-        choices=LENGTHRESIDENCE_CHOICE,
-        help_text="",
-        )
+#     length_residence = models.CharField(
+#         verbose_name="How long have your lived in this community?",
+#         max_length=25,
+#         choices=LENGTHRESIDENCE_CHOICE,
+#         help_text="",
+#         )
 
     refusal_date = models.DateField(
         verbose_name="Date subject refused participation",
@@ -36,10 +36,10 @@ class SubjectRefusal (BaseMemberStatusModel):
         help_text="Date format is YYYY-MM-DD")
 
     why_no_participate = models.CharField(
-        verbose_name="What was the main reason you do not want to participate in the study?",
+        verbose_name="What was the main reason you do not want to participate in this study?",
         max_length=50,
         choices=WHYNOPARTICIPATE_CHOICE,
-        help_text="Note: Only asked of individuals declining HIV testing during this visit.",
+        help_text="",
         )
     why_no_participate_other = OtherCharField()
 
@@ -50,22 +50,22 @@ class SubjectRefusal (BaseMemberStatusModel):
                    " when the subject changes their mind"),
         default='REFUSED',
         editable=False)
-
-    hiv_test_today = models.CharField(
-        verbose_name="[For interviewer] Did resident complete HIV testing today?",
-        max_length=15,
-        choices=YES_NO_UNSURE,
-        help_text="",
-        )
-
-    why_no_hivtest = models.CharField(
-        verbose_name="What was the main reason why you did not want HIV testing as part of today's visit?",
-        max_length=50,
-        null=True,
-        blank=True,
-        choices=WHYNOHIVTESTING_CHOICE,
-        help_text="if 'Yes or Unsure' END survey",
-        )
+# 
+#     hiv_test_today = models.CharField(
+#         verbose_name="[For interviewer] Did resident complete HIV testing today?",
+#         max_length=15,
+#         choices=YES_NO_UNSURE,
+#         help_text="",
+#         )
+# 
+#     why_no_hivtest = models.CharField(
+#         verbose_name="What was the main reason why you did not want HIV testing as part of today's visit?",
+#         max_length=50,
+#         null=True,
+#         blank=True,
+#         choices=WHYNOHIVTESTING_CHOICE,
+#         help_text="if 'Yes or Unsure' END survey",
+#         )
 
     comment = models.CharField(
         verbose_name="Comment",
@@ -91,6 +91,6 @@ class SubjectRefusal (BaseMemberStatusModel):
 
     class Meta:
         app_label = "bcpp_subject"
-        verbose_name = "Subject Refusal"
-        verbose_name_plural = "Subject Refusal"
+        verbose_name = "Refusal Log"
+        verbose_name_plural = "Refusal Log"
         ordering = ['household_member']
