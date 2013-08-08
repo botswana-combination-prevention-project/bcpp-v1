1422cfc559b56b01165d5845a0bac102af094b43
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-08 08:18:20 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@531 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/blood_draw_form.py
+++ forms/blood_draw_form.py
@@ -4,9 +4,20 @@ from bcpp_subject.models import BloodDraw
 
 
 class BloodDrawForm (BaseSubjectModelForm):
+    
     def clean(self):
 
         cleaned_data = self.cleaned_data
+        
+        if cleaned_data.get('is_blood_drawn') == 'No' and not cleaned_data.get('is_blood_drawn_other'):
+            raise forms.ValidationError('If blood was not drawn today, please explain why')
+        if cleaned_data.get('is_blood_drawn') == 'Yes' and not cleaned_data.get('draw_date'):
+            raise forms.ValidationError('If blood was drawn today, please provide the date and time of the blood draw')
+        if cleaned_data.get('record_available') == 'Yes' and not cleaned_data.get('last_cd4_count') and not cleaned_data.get('last_cd4_drawn_date'):
+            raise forms.ValidationError('If last known record of CD4 count is available or known, please provide the CD4count and the CD4date')
+
+
+        cleaned_data = super(BloodDrawForm, self).clean()
 
         return cleaned_data
 
