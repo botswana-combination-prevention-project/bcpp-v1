a6a5d4307defe9f4dc24b41a8ae29635ce29877b
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-12 13:04:01 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@550 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- managers/subject_absentee_entry_manager.py
+++ managers/subject_absentee_entry_manager.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class SubjectAbsenteeEntryManager(models.Manager):
-    def get_by_natural_key(self, report_datetime, report_datetime1, household_identifier, survey_name, subject_identifier_as_pk):
+    def get_by_natural_key(self, report_datetime, household_identifier, survey_name, subject_identifier_as_pk):
         report_datetime = dateutil.parser.parse(report_datetime)
         margin = timedelta(microseconds=999)
         SubjectAbsentee = models.get_model('bcpp_subject', 'SubjectAbsentee')
