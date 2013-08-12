88d498c39811b0a06a771f0771fe122be2d7c8e3
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-12 12:59:10 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household_member@72 0e2d9bf9-3001-43ce-ab37-2eca76889215

--- managers/household_member_manager.py
+++ managers/household_member_manager.py
@@ -4,7 +4,9 @@ from django.db.models import get_model
 
 class HouseholdMemberManager(models.Manager):
 
-    def get_by_natural_key(self, household_identifier, survey_name):
+    def get_by_natural_key(self, household_identifier, survey_name, subject_identifier_as_pk):
         HouseholdStructure = get_model('bcpp_household', 'HouseholdStructure')
+        RegisteredSubject = get_model('bhp_registration', 'RegisteredSubject')
         household_structure = HouseholdStructure.objects.get_by_natural_key(household_identifier, survey_name)
-        return self.get(household_structure=household_structure)
+        registered_subject = RegisteredSubject.objects.get_by_natural_key(subject_identifier_as_pk)
+        return self.get(household_structure=household_structure, registered_subject=registered_subject)
