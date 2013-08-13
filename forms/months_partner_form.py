251db2aac96afcd61ae2b315399ebbcf5f17ea0c
erik van widenfelt <ew2789@gmail.com>
2013-08-13 16:04:48 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@567 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/months_partner_form.py
+++ forms/months_partner_form.py
@@ -6,16 +6,19 @@ from bcpp.choices import FIRSTPARTNERHIV_CHOICE, YES_NO_UNSURE
 
 class BaseMonthsPartnerForm (BaseSubjectModelForm):
 
+    yes_no_unsure_options = ['Yes', 'No', 'not sure', 'Don\'t want to answer']
+
+    def check_tuples(self):
+        # check tuples have not changed
+        self.options_in_tuple(YES_NO_UNSURE, self.yes_no_unsure_options)
+        self.options_in_tuple(FIRSTPARTNERHIV_CHOICE, ['negative', 'I am not sure'])
+
     def clean(self):
         """Ensures that question about antiretrovirals is not answered if partner is known to be HIV negative."""
         cleaned_data = super(MonthsRecentPartnerForm, self).clean()
-        # check tuples have not changed
-        options = ['Yes', 'No', 'not sure', 'Don\'t want to answer']
-        self.options_in_tuple(YES_NO_UNSURE, options)
-        self.options_in_tuple(FIRSTPARTNERHIV_CHOICE, ['negative', 'I am not sure'])
-        if cleaned_data.get('firstpartnerhiv') == 'negative' and cleaned_data.get('firsthaart') in options:
+        if cleaned_data.get('firstpartnerhiv') == 'negative' and cleaned_data.get('firsthaart') in self.yes_no_unsure_options:
             raise forms.ValidationError('Do not answer this question if partners HIV status is known to be negative')
-        if cleaned_data.get('firstpartnerhiv') == 'I am not sure' and cleaned_data.get('firsthaart') in options:
+        if cleaned_data.get('firstpartnerhiv') == 'I am not sure' and cleaned_data.get('firsthaart') in self.yes_no_unsure_options:
             raise forms.ValidationError('If partner status is not known, do not give information about status of ARV\'s')
         return cleaned_data
 
