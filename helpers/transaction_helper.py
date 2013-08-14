94ce78073870b22412af58c7b48d2f3dd16e55d7
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-14 06:33:03 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@327 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- helpers/transaction_helper.py
+++ helpers/transaction_helper.py
@@ -6,7 +6,7 @@ class TransactionHelper(object):
     def has_incoming_for_producer(self, producer, using=None):
         if not using:
             using = 'default'
-        return IncomingTransaction.objects.using(using).filter(producer=producer, is_consumed=False).exists()
+        return IncomingTransaction.objects.using(using).filter(producer=producer, is_consumed=False).exclude(is_ignored=True).exists()
 
     def has_incoming_for_model(self, models, using=None):
         """Checks if incoming transactions exist for the given model(s)."""
@@ -14,7 +14,7 @@ class TransactionHelper(object):
             return False
         if not isinstance(models, (list, tuple)):
             models = [models]
-        return IncomingTransaction.objects.using(using).filter(tx_name__in=models, is_consumed=False).exists()
+        return IncomingTransaction.objects.using(using).filter(tx_name__in=models, is_consumed=False).exclude(is_ignored=True).exists()
 
     def has_outgoing(self, using=None):
         return OutgoingTransaction.objects.using(using).filter(is_consumed_server=False).exists()
