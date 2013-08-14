94431abaa483f717b7d1b2a50876c221db88dd0a
erik van widenfelt <ew2789@gmail.com>
2013-08-14 09:17:10 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_base_form@26 5a461858-af1e-4e6f-938f-33a009643501

--- forms/base_model_form.py
+++ forms/base_model_form.py
@@ -1,4 +1,6 @@
+import re
 from django import forms
+from django.db.models.query import QuerySet
 from django.conf import settings
 from django.db.models import OneToOneField, ForeignKey, get_model
 from bhp_visit_tracking.models import BaseVisitTracking
@@ -27,7 +29,6 @@ class BaseModelForm(forms.ModelForm):
                 except:
                     pass
         # if in admin edit mode, populate registered_subject's queryset
-
         if 'registered_subject' in self.fields:
             try:
                 if 'registered_subject' in dir(self.instance):
@@ -39,13 +40,6 @@ class BaseModelForm(forms.ModelForm):
                 if 'registered_subject' not in self.initial and 'registered_subject' not in self.data:
                     RegisteredSubject = get_model('bhp_registration', 'RegisteredSubject')
                     self.fields['registered_subject'].queryset = RegisteredSubject.objects.none()
-        # translation
-        #self.insert_translation_help_text_attributes()
-
-#     def insert_translation_help_text_attributes(self):
-#         for k in self.fields.iterkeys():
-#             self.fields[k].translation_help_text = 'This is translation help text'
-#         return None
 
     def get_subject_identifier(self, cleaned_data):
         subject_identifier = None
@@ -66,7 +60,7 @@ class BaseModelForm(forms.ModelForm):
         return subject_identifier
 
     def clean(self):
-
+        """Calls crypto clean methods, OTHER/Specify and some functionality for bhp_dispatch."""
         cleaned_data = self.cleaned_data
         # check if dispatched
         if 'bhp_dispatch' in settings.INSTALLED_APPS:
@@ -76,23 +70,51 @@ class BaseModelForm(forms.ModelForm):
         # encrypted fields may have their own validation code to run.
         # See the custom field objects in bhp_crypto.
         try:
-            from bhp_crypto.classes import BaseEncryptedField
+            from bhp_crypto.fields import BaseEncryptedField
             for field in self._meta.model._meta.fields:
                 if isinstance(field, BaseEncryptedField):
                     field.validate_with_cleaned_data(field.attname, cleaned_data)
         except ImportError:
             pass
+        # check for OTHER in choices tuples, if selected, should have a value on Other specify.
         other = []
-        [other.append(k) for k in cleaned_data.iterkeys() if cleaned_data[k] == 'OTHER']
+        [other.append(k) for k in cleaned_data.iterkeys() if cleaned_data.get(k, None) in ['other', 'Other', 'OTHER']]
         for k in other:
             if '{0}_other'.format(k) in cleaned_data:
-                if not cleaned_data['{0}_other'.format(k)]:
+                if not cleaned_data.get('{0}_other'.format(k)):
                     raise forms.ValidationError('If {0} is \'OTHER\', please specify. '
                                                 'You wrote \'{1}\''.format(k, cleaned_data['{0}_other'.format(k)]))
+        # m2m with OTHER
+        self.check_for_other_in_m2m(cleaned_data)
         return super(BaseModelForm, self).clean()
 
-    def validate_m2m(self, **kwargs):
+    def check_for_other_in_m2m(self, cleaned_data):
+        """Raises ValidtionError for an m2m if it cannot confirm \'Other Specify\' is paired with a value in a following \'other\' field."""
+        for field_name, field_value in cleaned_data.iteritems():
+            self.check_for_value_in_m2m(cleaned_data, field_name, field_value, '{0}_other'.format(field_name), ['specify', 'explain'], 'other')
+#             if isinstance(value, QuerySet):
+#                 words = ['specify', 'explain']
+#                 answers = [l.name.lower() for l in cleaned_data.get(key, [])]
+#                 if answers:
+#                     for ans in answers:
+#                         if any([word in ans for word in words]) and 'other' in ans.lower():
+#                             if not cleaned_data.get('{0}_other'.format(key), None):
+#                                 raise forms.ValidationError('You have selected \'{0}\' as an answer for {1}. Please specify.'.format(ans, key))
 
+    def check_for_value_in_m2m(self, cleaned_data, m2m_name, m2m_qs, optional_field_name, optional_words, required_word=None):
+        """Raises ValidtionError for an m2m if it cannot confirm \'Other Specify\' is paired with a value in a following \'other\' field."""
+        if not required_word:
+            required_word = ''
+        if isinstance(m2m_qs, QuerySet):
+            answers = [l.name.lower() for l in cleaned_data.get(m2m_name, [])]
+            if answers:
+                for ans in answers:
+                    if any([word in ans for word in optional_words]) and required_word in ans.lower():
+                        if not cleaned_data.get(optional_field_name, None):
+                            raise forms.ValidationError('You have selected \'{0}\' as an answer for {1}. Please specify in {2}.'.format(ans, m2m_name, optional_field_name))
+
+    # TODO: is this method used??
+    def validate_m2m(self, **kwargs):
         """Validates at form level a triplet of questions lead by a Yes/No for a many to many with other specify.
 
             * The first question is a Yes/No question indicating if any items in the many to many will be selected
@@ -110,7 +132,6 @@ class BaseModelForm(forms.ModelForm):
                             m2m = cleaned_data['chronic_cond'],
                             other = cleaned_data['chronic_cond_other'])
         """
-
         label = kwargs.get('label', 'items to be selected')
         leading = kwargs.get('leading')
         m2m = kwargs.get('m2m')
@@ -135,3 +156,26 @@ class BaseModelForm(forms.ModelForm):
         # if 'other' has a value but no m2m item is 'Other, specify'
         if other and not [True for item in m2m if 'other' in item.name.lower()]:
             raise forms.ValidationError("You have specified an 'Other' {0} but not selected 'Other, specify'. Please correct.".format(label))
+
+    def options_in_tuple(self, choices, options):
+        """Confirms options exist in choices tuple."""
+        if not isinstance(choices, tuple):
+            raise TypeError('Parameter \'choices\' must be a tuple.')
+        if not isinstance(options, list):
+            raise TypeError('Parameter \'options\' must be a list.')
+        choices = list(choices)
+        choices.sort()
+        lst = list(set(list(options) + choices))
+        lst.sort()
+        if not lst == choices:
+            raise TypeError('Options {0} are not in choices tuple {1}. Has the choices tuple changed?'.format(options, choices))
+
+    def verify_tuples(self):
+        """Override to verify tuple values referenced in the valifation checks.
+
+        For example, validation checks refer to values from these choices::
+            options = ['Yes', 'No', 'not sure', 'Don\'t want to answer']
+            self.options_in_tuple(YES_NO_UNSURE, options)
+            self.options_in_tuple(FIRSTPARTNERHIV_CHOICE, ['negative', 'I am not sure'])
+        """
+        pass
