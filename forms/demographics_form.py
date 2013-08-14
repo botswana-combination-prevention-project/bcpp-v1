46ce26e675cba2a89e8ceda34bd3b8d2be1c88dc
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-14 09:56:06 +0000

    marriage validation removed for now
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@572 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- forms/demographics_form.py
+++ forms/demographics_form.py
@@ -5,23 +5,17 @@ from bcpp_subject.models import Demographics
 
 class DemographicsForm (BaseSubjectModelForm):
 
-    def clean(self):
-
-        cleaned_data = super(DemographicsForm, self).clean()
-        #validation religion
-#         if cleaned_data.get('religion')[0].name == 'Other' and not cleaned_data.get('religion_other'):
-#             raise forms.ValidationError('If participant\'s religion not in given list, name the religion')
-#         #validation- ethnic group
-#         if cleaned_data.get('ethnic')[0].name == 'Other, specify' and not cleaned_data.get('other'):
-#             raise forms.ValidationError('If participant\'s ethnic group is not in the given list, name the ethnic group')
-#         #validating marital status
-        #asked if female
-        if cleaned_data.get('marital_status') == 'Married' and not cleaned_data.get('num_wives'):
-            raise forms.ValidationError('If participant is married, give number of wives')
-        #asked if male
-        if cleaned_data.get('marital_status') == 'Married' and not cleaned_data.get('husband_wives'):
-            raise forms.ValidationError('If participant is married, how many wives is he married to, including traditional marriages?')
-        return cleaned_data
+#     def clean(self):
+# 
+#         cleaned_data = super(DemographicsForm, self).clean()
+#         #This will later be used under conditional fields. For now its commented out and help text is given on the model form
+#         #asked if female
+# #         if cleaned_data.get('marital_status') == 'Married' and not cleaned_data.get('num_wives'):
+# #             raise forms.ValidationError('If participant is married, give number of wives')
+#         #asked if male
+# #         if cleaned_data.get('marital_status') == 'Married' and not cleaned_data.get('husband_wives'):
+# #             raise forms.ValidationError('If participant is married, how many wives is he married to, including traditional marriages?')
+#         return cleaned_data
 
     class Meta:
         model = Demographics
