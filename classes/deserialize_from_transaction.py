1295a27bc720165de91cd86abab176e556db0663
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-16 13:46:24 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_sync@330 3bc1d969-5506-4dd9-9641-5fb303cabccf

--- classes/deserialize_from_transaction.py
+++ classes/deserialize_from_transaction.py
@@ -17,8 +17,7 @@ class DeserializeFromTransaction(object):
     def deserialize(self, incoming_transaction, using, **kwargs):
         # may bypass this check for for testing ...
         check_hostname = kwargs.get('check_hostname', True)
-        #for obj in serializers.deserialize("json", FieldCryptor('aes', 'local').decrypt(incoming_transaction.tx)):
-        for obj in serializers.deserialize("json", incoming_transaction):
+        for obj in serializers.deserialize("json", FieldCryptor('aes', 'local').decrypt(incoming_transaction.tx)):
             # if you get an error deserializing a datetime, confirm dev version of json.py
             if incoming_transaction.action == 'I' or incoming_transaction.action == 'U':
                 # check if tx originanted from me
