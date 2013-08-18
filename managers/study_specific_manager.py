0b54d8f386663c5861fbc42c1e2b9454103780c2
erik van widenfelt <ew2789@gmail.com>
2013-08-18 19:56:43 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_variables@60 72d6f641-25d3-4f20-903c-4c2123898bce

--- managers/study_specific_manager.py
+++ managers/study_specific_manager.py
@@ -8,9 +8,14 @@ class StudySpecificManager(models.Manager):
         qs = QuerySet(self.model, using=self._db).all()
         if not qs:
             return qs
-            raise ValueError('Application is accessing model StudySpecific' \
-                             'you have not populated it. Please do so before' \
-                             ' continuing.')
-
+            raise ValueError('Application is accessing model StudySpecific but you have not populated it. Please do so before continuing.')
         qs = QuerySet(self.model, using=self._db).filter(pk=qs[0].pk)
         return qs
+
+    def get_subject_types(self):
+        if super(StudySpecificManager, self).all():
+            if not super(StudySpecificManager, self).all()[0].subject_type:
+                raise TypeError('Please indicate the subject types for this protocol in model StudySpecific. e.g. \'subject\' or \'maternal, infant\', etc')
+        else:
+            raise ValueError('Application is accessing model StudySpecific but you have not populated it. Please do so before continuing.')
+        return super(StudySpecificManager, self).all()[0].subject_type.split(', ')
