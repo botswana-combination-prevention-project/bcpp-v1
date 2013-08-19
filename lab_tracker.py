33facc23fc5a8538171986630f086f88c54c0a23
erik van widenfelt <ew2789@gmail.com>
2013-08-19 10:17:36 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@578 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- lab_tracker.py
+++ lab_tracker.py
@@ -4,6 +4,7 @@ from models import HivTestReview, HivResult
 
 
 class SubjectHivLabTracker(HivLabTracker):
+
     trackers = [(HivTestReview, 'recorded_hiv_result', 'hiv_test_date', ),
               (HivResult, 'hiv_result', 'hiv_result_datetime', )]
 
