bd21fd1a8596ecf555413d8b3b6d971a85d0aea1
erik van widenfelt <ew2789@gmail.com>
2013-08-21 19:39:17 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_subject@589 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- search.py
+++ search.py
@@ -1,10 +1,11 @@
 from bhp_search.classes import BaseSearchByWord, site_search
 from models import SubjectConsent
+from section import SectionSubjectView
 
 
 class SubjectSearchByWord(BaseSearchByWord):
 
-    section_name = 'subject'
+    section = SectionSubjectView
     search_model = SubjectConsent
     order_by = '-created'
     template = 'subjectconsent_include.html'
