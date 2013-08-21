2c500ef14620d5755fbb6e8fc72bb6c54d593f19
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-21 13:48:39 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_htc_subject@25 c9949dc8-c471-4645-b440-cbfc7d707ab4

--- forms/followup_contact_permission_form.py
+++
@@ -1,8 +0,0 @@
-from base_htc_model_form import BaseHtcModelForm
-from bcpp_htc.models import FollowupContactPermission
-
-
-class FollowupContactPermissionForm (BaseHtcModelForm):
-
-    class Meta:
-        model = FollowupContactPermission
