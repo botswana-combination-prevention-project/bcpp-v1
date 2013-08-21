074e9505472e54fce86f19588513206b4275ff39
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-21 14:53:15 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_htc_subject@31 c9949dc8-c471-4645-b440-cbfc7d707ab4

--- rule_groups.py
+++ rule_groups.py
@@ -1,22 +1,29 @@
 from bhp_entry_rules.classes import RuleGroup, rule_groups, ScheduledDataRule, AdditionalDataRule, Logic
 from bhp_registration.models import RegisteredSubject
-from models import HtcVisit, HivTestingHistory
+from models import HtcVisit, HivTestingHistory, HivResult, HivTestingConsent
 
 
-class MaleCircumcisionRuleGroup(RuleGroup):
+class GenderRuleGroup(RuleGroup):
 
-    gender = ScheduledDataRule(
+    female = ScheduledDataRule(
         logic=Logic(
             predicate=('gender', 'equals', 'f'),
             consequence='not_required',
             alternative='new'),
-        target_model=['circumcision'])
+        target_model=['circumcision', 'malefollowupconsent', 'circumcisionappointment'])
+
+    male = ScheduledDataRule(
+        logic=Logic(
+            predicate=('gender', 'equals', 'm'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['pregnantfollowupconsent'])
 
     class Meta:
         app_label = 'bcpp_htc'
         filter_model = (HtcVisit, 'htc_visit')
         source_model = RegisteredSubject
-rule_groups.register(MaleCircumcisionRuleGroup)
+rule_groups.register(GenderRuleGroup)
 
 
 class HivTestingHistoryRuleGroup(RuleGroup):
@@ -33,3 +40,35 @@ class HivTestingHistoryRuleGroup(RuleGroup):
         filter_model = (HtcVisit, 'htc_visit')
         source_model = HivTestingHistory
 rule_groups.register(HivTestingHistoryRuleGroup)
+
+
+class HivResultRuleGroup(RuleGroup):
+    
+    todays_result = ScheduledDataRule(
+        logic=Logic(
+            predicate=('todays_result', 'equals', 'NEG'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['Cd4test', 'positivefollowupconsent'])
+    
+    class Meta:
+        app_label = 'bcpp_htc'
+        filter_model = (HtcVisit, 'htc_visit')
+        source_model = HivResult
+rule_groups.register(HivResultRuleGroup)
+
+
+class HivTestingConsentRuleGroup(RuleGroup):
+    
+    todays_result = ScheduledDataRule(
+        logic=Logic(
+            predicate=('testing_today', 'equals', 'No'),
+            consequence='not_required',
+            alternative='new'),
+        target_model=['Cd4test', 'positivefollowupconsent', 'pregnantfollowupconsent', 'malefollowupconsent', 'circumcisionappointment'])
+    
+    class Meta:
+        app_label = 'bcpp_htc'
+        filter_model = (HtcVisit, 'htc_visit')
+        source_model = HivTestingConsent
+rule_groups.register(HivTestingConsentRuleGroup)
