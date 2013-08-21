cfbaf10c7689ba257f85e2395ea164415613ba93
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-21 13:47:02 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_htc_subject@21 c9949dc8-c471-4645-b440-cbfc7d707ab4

--- forms/hiv_testing_counseling_form.py
+++
@@ -1,8 +0,0 @@
-from base_htc_model_form import BaseHtcModelForm
-from bcpp_htc.models import HivTestingCounseling
-
-
-class HivTestingCounselingForm (BaseHtcModelForm):
-
-    class Meta:
-        model = HivTestingCounseling
