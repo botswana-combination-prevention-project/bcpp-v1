2e152235052fb97abc0fecba069a7670708abe2f
erik van widenfelt <ew2789@gmail.com>
2013-08-22 10:31:58 +0000

    add validate to forms
    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@84 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

--- forms/base_visit_tracking_form.py
+++ forms/base_visit_tracking_form.py
@@ -5,6 +5,10 @@ from bhp_consent.forms import BaseConsentedModelForm
 
 class BaseVisitTrackingForm(BaseConsentedModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(BaseVisitTrackingForm, self).__init__(*args, **kwargs)
+        self._validation_error = None
+
     def clean(self):
         cleaned_data = self.cleaned_data
         if 'bhp_dispatch' in dir(settings):
@@ -15,4 +19,27 @@ class BaseVisitTrackingForm(BaseConsentedModelForm):
                     forms.ValidationError("Data for {0} is currently dispatched to netbook {1}. "
                                           "This form may not be modified.".format(appointment.registered_subject.subject_identifier,
                                                                                   dispatch_item.producer.name))
+        self._validate_cleaned_data(cleaned_data)
         return cleaned_data
+
+    def set_validation_error(self, value=None):
+        if value:
+            if not isinstance(value, dict):
+                raise TypeError('parameter value must be a dictionary.')
+        self._validation_error = value
+
+    def get_validation_error(self):
+        if not self._validation_error:
+            self.set_validation_error()
+        return self._validation_error
+
+    def _validate_cleaned_data(self, cleaned_data, supress_exception=None):
+        self.validate_cleaned_data(cleaned_data)
+        if self.get_validation_error() and not supress_exception:
+            for message in self.get_validation_error().itervalues():
+                raise forms.ValidationError(message)
+        return self.get_validation_error()
+
+    def validate_cleaned_data(self, cleaned_data, suppress_exception=None):
+        """Override to add validation code."""
+        pass
