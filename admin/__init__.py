a0ae4156b15bc64713fedc46e4fc8f5ccc78879b
erik van widenfelt <ew2789@gmail.com>
2013-08-22 06:15:55 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_section@58 9ea92a1f-d274-4fd2-88dd-3dac8f9b37c0

--- admin/__init__.py
+++
@@ -1,1 +0,0 @@
-from section_admin import SectionAdmin
--- admin/section_admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-#from bhp_site_edc import edc as admin
-from bhp_section.models import Section
-
-
-class SectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'display_name', 'display_index')
-admin.site.register(Section, SectionAdmin)
