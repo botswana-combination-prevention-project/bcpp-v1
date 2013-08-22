a5ef85e63fb0ddc673ab84f5b49ab60f90153cdf
one pharatlhatlhe <opharatlhatlhe@bhp.org.bw>
2013-08-22 14:19:17 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household_member@89 0e2d9bf9-3001-43ce-ab37-2eca76889215

--- tests/factories/household_member_factory.py
+++ tests/factories/household_member_factory.py
@@ -3,11 +3,14 @@ from datetime import date, datetime
 from bhp_base_model.tests.factories import BaseUuidModelFactory
 from bcpp_household_member.models import HouseholdMember
 from bcpp_survey.tests.factories import SurveyFactory
+from bcpp_household.tests.factories import HouseholdStructureFactory
+from bhp_registration.tests.factories import RegisteredSubjectFactory
 
 
 class HouseholdMemberFactory(BaseUuidModelFactory):
     FACTORY_FOR = HouseholdMember
 
+    registered_subject = factory.SubFactory(RegisteredSubjectFactory)
     first_name = factory.Sequence(lambda n: 'first_name{0}'.format(n))
     initials = factory.Sequence(lambda n: 'initials{0}'.format(n))
     gender = (('M', 'Male'), ('F', 'Female'))[0][0]
@@ -17,4 +20,5 @@ class HouseholdMemberFactory(BaseUuidModelFactory):
     is_eligible_member = True
     target = 2
     survey = factory.SubFactory(SurveyFactory)
+    household_structure = factory.SubFactory(HouseholdStructureFactory)
     nights_out = 2
