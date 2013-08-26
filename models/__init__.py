e0bbf48e07fa340ce4305ec3dd469b18a6ea3ca2
erik van widenfelt <ew2789@gmail.com>
2013-08-26 10:10:42 +0000

    git-svn-id: http://192.168.1.50/svn/bhp_visit_tracking@87 5b0d96ef-e3d5-4248-adb5-3ee6ab0ec093

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,2 @@
 from base_visit_tracking import BaseVisitTracking
-from test_subject_visit import *
 from signals import *
--- tests/factories/test_scheduled_model_factory.py
+++
@@ -1,7 +0,0 @@
-from bhp_base_model.tests.factories import BaseUuidModelFactory
-
-from bhp_visit_tracking.models import TestScheduledModel
-
-
-class TestScheduledModelFactory(BaseUuidModelFactory):
-    FACTORY_FOR = TestScheduledModel
