11b16531735585e965cde1a29874841796fdfee5
coulson kgathi <ckgathi@bhp.org.bw>
2013-08-28 13:02:23 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_household@150 30821849-012b-4b24-87a7-59ccb7ae08db

--- forms/current_gps_form.py
+++ forms/current_gps_form.py
@@ -4,11 +4,11 @@ from django import forms
 class CurrentGpsForm(forms.Form):
 
     degrees_s = forms.IntegerField(label='S', required=True)
-    minutes_s = forms.IntegerField(label='.', required=True)
+    minutes_s = forms.DecimalField(label='.', required=True)
     degrees_e = forms.IntegerField(label='E', required=True)
-    minutes_e = forms.IntegerField(label='.', required=True)
+    minutes_e = forms.DecimalField(label='.', required=True)
     radius = forms.DecimalField(label='R (m)', required=True)
-    community = forms.CharField(widget=forms.HiddenInput())
+    #community = forms.CharField(widget=forms.HiddenInput())
 
     def clean_degree_s(self):
         degrees_s = self.cleaned_data['degrees_s']
