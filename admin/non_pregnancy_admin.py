7a366f76876c199da98c276384a16e0baf6786ed
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-08-29 10:22:03 +0000

    updated
    git-svn-id: http://192.168.1.50/svn/bcpp_subject@602 5066620e-b066-4cb9-a883-3cd329b6f9ec

--- admin/non_pregnancy_admin.py
+++ admin/non_pregnancy_admin.py
@@ -10,13 +10,15 @@ class NonPregnancyAdmin(SubjectVisitModelAdmin):
     fields = (
         "subject_visit",
         'more_children',
-        'last_birth',
-        'anc_last_pregnancy',
-        'hiv_last_pregnancy',
-        'preg_arv',)
+#         'last_birth',
+#         'anc_last_pregnancy',
+#         'hiv_last_pregnancy',
+#         'preg_arv',
+            )
     radio_fields = {
         "more_children": admin.VERTICAL,
-        "anc_last_pregnancy": admin.VERTICAL,
-        "hiv_last_pregnancy": admin.VERTICAL,
-        "preg_arv": admin.VERTICAL, }
+#         "anc_last_pregnancy": admin.VERTICAL,
+#         "hiv_last_pregnancy": admin.VERTICAL,
+#         "preg_arv": admin.VERTICAL, 
+        }
 admin.site.register(NonPregnancy, NonPregnancyAdmin)
