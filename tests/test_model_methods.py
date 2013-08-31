8f216c9690803d7440fcd932260801e481d5faa6
erik van widenfelt <ew2789@gmail.com>
2013-08-31 10:02:23 +0000

    git-svn-id: http://192.168.1.50/svn/lab_clinic_api@239 5124d8a1-613d-4708-9420-8c98912e35b2

--- tests/test_model_methods.py
+++ tests/test_model_methods.py
@@ -3,15 +3,16 @@ from bhp_registration.tests.factories import RegisteredSubjectFactory
 from factories import AliquotFactory, AliquotConditionFactory, AliquotTypeFactory, ReceiveFactory, OrderFactory, ResultFactory, ResultItemFactory
 from bhp_lab_tracker.classes import site_lab_tracker
 
-site_lab_tracker.autodiscover()
 
 
 class TestModelMethods(TestCase):
 
     def test_aliquot(self):
+        site_lab_tracker.autodiscover()
         aliquot = AliquotFactory()
 
     def test_order(self):
+        site_lab_tracker.autodiscover()
         registered_subject = RegisteredSubjectFactory()
         receive = ReceiveFactory(registered_subject=registered_subject)
         aliquot_condition = AliquotConditionFactory(short_name='10')
