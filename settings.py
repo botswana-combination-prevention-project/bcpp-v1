ed1357feb92c5fef60aef5d9b12a0b4c56b6acc1
erik van widenfelt <ew2789@gmail.com>
2013-09-02 05:04:41 +0000

    git-svn-id: http://192.168.1.50/svn/bhp066_project@64 dd0f4ffa-7f17-495f-ba89-20ae773efdaf

--- settings.py
+++ settings.py
@@ -82,7 +82,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066',
+            'NAME': 'bhp066_survey',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
@@ -138,7 +138,7 @@ TIME_ZONE = 'Africa/Gaborone'
 ugettext = lambda s: s
 LANGUAGES = (
     ('tn', 'Setswana'),
-    ('en', 'English'),   
+    ('en', 'English'),
 )
 
 LOCALE_PATHS = (os.path.join(DIRNAME, 'locale'), )
@@ -193,16 +193,17 @@ SECRET_KEY = '0$q&@p=jz(+_r^+phzenyqi49#y2^3ot3h#jru+32z&+cm&j51'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'django.template.loaders.eggs.Loader',
+    ('django.template.loaders.cached.Loader', (
+     'django.template.loaders.filesystem.Loader',
+     'django.template.loaders.app_directories.Loader',
+     'django.template.loaders.eggs.Loader',
+     )),
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -236,9 +237,10 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django_extensions',
+    'django-databrowse',
     'dajaxice',
     'dajax',
-#    'south',
+    'south',
     'audit_trail',
     'autocomplete',
     'bhp_templates',
@@ -330,6 +332,7 @@ INSTALLED_APPS = (
     'bhp_subject_summary',
     'bhp_entry_rules',
     'bhp_dispatch',
+    'bhp_inspector',
     'bhp_netbook',
     'bhp_household',
     'bhp_household_member',
@@ -339,13 +342,14 @@ INSTALLED_APPS = (
     'bcpp_lab',
     'bcpp_list',
     'bcpp_subject',
+    'bcpp_htc',
     'bcpp_dashboard',
     'bcpp_stats',
     'bcpp_household',
     'bcpp_household_member',
     'bcpp_survey',
     'bcpp_inspector',
-    'tastypie',
+#    'tastypie',
 #     'bcpp_survey_dashboard',
 #     'bcpp_survey_lab',
 )
@@ -387,7 +391,8 @@ LABDB = 'bhplab'
 SESSION_COOKIE_AGE = 3000
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 DEVICE_ID = '99'
-MAX_SUBJECTS = 3000
+SUBJECT_TYPES = ['subject']
+MAX_SUBJECTS = {'subject': 3000}
 APPOINTMENTS_PER_DAY_MAX = 20
 APPOINTMENTS_DAYS_FORWARD = 15
 
@@ -396,23 +401,26 @@ DISPATCH_APP_LABELS = ['bcpp_subject', 'bcpp_household', 'bcpp_household_member'
 
 #Reports settings
 REPORTS_TEMPLATES_PATH = os.path.join(DIRNAME)
-REPORTS_JAR_PATH = os.path.join(DIRNAME,'birtreport_generator.jar')
-REPORTS_OUTPUT_PATH = os.path.join(DIRNAME,'bhp_birt_reports/templates/')
+REPORTS_JAR_PATH = os.path.join(DIRNAME, 'birtreport_generator.jar')
+REPORTS_OUTPUT_PATH = os.path.join(DIRNAME, 'bhp_birt_reports/templates/')
 
 #BHP_CRYPTO_SETTINGS
 IS_SECURE_DEVICE = False
 MAY_CREATE_NEW_KEYS = True
 KEY_PATH = os.path.join(DIRNAME, 'keys')
 MAP_DIR = os.path.join(DIRNAME, 'static/img/')
-#KEY_PATH = '/Volumes/bhp066/keys'
+if platform.system() == 'Darwin':
+    KEY_PATH = '/Volumes/bhp066/keys'
+else:
+    KEY_PATH = os.path.join(DIRNAME, 'keys')
+
 GPS_FILE_PATH = '/Volumes/GARMIN/GPX/Current.gpx'
-#FIELD_MAX_LENGTH='default'
+VERIFY_GPS = False
 FIELD_MAX_LENGTH = 'migration'
 
 # LAB REFERENCE AND GRADING
 REFERENCE_RANGE_LIST = 'BHPLAB_NORMAL_RANGES_201005'
 GRADING_LIST = 'DAIDS_2004'
-
 # for bhp_import_dmis
 if platform.system() == 'Darwin':
     LAB_IMPORT_DMIS_DATA_SOURCE = ('DRIVER=/usr/local/lib/libtdsodbc.so;SERVER=192.168.1.141;'
