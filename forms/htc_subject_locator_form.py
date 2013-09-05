b144f706cd2786eab33c659a07ef6c6ceab8ea4a
erik van widenfelt <ew2789@gmail.com>
2013-09-05 12:50:58 +0000

    git-svn-id: http://192.168.1.50/svn/bcpp_htc_subject@67 c9949dc8-c471-4645-b440-cbfc7d707ab4

---
+++ forms/htc_subject_locator_form.py
@@ -0,0 +1,8 @@
+from bcpp_htc_subject.models import HtcSubjectLocator
+from base_htc_scheduled_model_form import BaseHtcScheduledModelForm
+
+
+class HtcSubjectLocatorForm (BaseHtcScheduledModelForm):
+
+    class Meta:
+        model = HtcSubjectLocator
