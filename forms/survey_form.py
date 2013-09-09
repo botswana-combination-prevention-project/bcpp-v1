a3c5685d3154b4bf7466f072d78cf9cf84d4c77e
erik van widenfelt <ew2789@gmail.com>
2013-09-10 01:26:46 +0200

    Squashed 'bcpp_survey/' content from commit 223ff94
    git-subtree-dir: bcpp_survey
    git-subtree-split: 223ff947a8bade48701934ebc7c84f1c9842db52

---
+++ forms/survey_form.py
@@ -0,0 +1,11 @@
+from django import forms
+from bcpp_survey.models import Survey
+
+
+class SurveyForm(forms.Form):
+    survey = forms.ChoiceField(
+        choices=[[survey.survey_name, survey.survey_name] for survey in Survey.objects.all()],
+        label="Survey ",
+        initial=None,
+        help_text="",
+        )
---
+++ views/__init__.py
@@ -0,0 +1,4 @@
+from index import *
+from section_index import *
+from find_household import find_household
+from search_response import *
