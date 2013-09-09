2350d8ed7638b78478da1638222ab950b7ae1d29
erik van widenfelt <ew2789@gmail.com>
2013-09-10 01:26:08 +0200

    Squashed 'bcpp_dashboard/' content from commit cc91def
    git-subtree-dir: bcpp_dashboard
    git-subtree-split: cc91def149ad561357ee0d7cb3d1667f19bc8036

---
+++ classes/household_dashboard.py
@@ -0,0 +1,257 @@
+import re
+from datetime import datetime, date
+from django.conf.urls.defaults import patterns, url
+from django.conf import settings
+from django.db.models import get_model
+from bhp_dashboard.classes import Dashboard
+from bhp_registration.models import RegisteredSubject
+from bcpp_household.models import Household, HouseholdStructure, HouseholdLogEntry, HouseholdLog
+from bcpp_household_member.models import HouseholdMember, EnrolmentChecklist, HouseholdInfo
+from bcpp_survey.models import Survey
+from bcpp_household.choices import HOUSEHOLD_MEMBER_ACTION
+
+
+class HouseholdDashboard(Dashboard):
+
+    view = 'household_dashboard'
+    dashboard_name = 'Household Dashboard'
+    dashboard_url_name = 'household_dasborad_url'
+
+    def __init__(self, dashboard_type, dashboard_id, dashboard_model, dashboard_type_list=None, dashboard_models=None, **kwargs):
+        self._household_members = None
+        self._household_structure = None
+        self._household_log = None
+        self._current_member_count = None
+        self._enrolment_checklist = None
+        self._household_info = None
+        self._survey = None
+        self._surveys = None
+        self._first_name = None
+        kwargs.update({'dashboard_models': {'household': Household, 'household_structure': HouseholdStructure}})
+        super(HouseholdDashboard, self).__init__(dashboard_type, dashboard_id, dashboard_model, dashboard_type_list, dashboard_models=kwargs.get('dashboard_models'))
+        self.set_survey(kwargs.get('survey'))
+        self.set_household()
+        self.set_household_structure()
+        self.set_first_name(kwargs.get('first_name', None))
+        # TODO: is this still necessary?
+        self.check_members_have_registered_subject()
+        self.set_mapper_name(kwargs.get('mapper_name'))
+
+    def add_to_context(self):
+        super(HouseholdDashboard, self).add_to_context()
+
+        self.context.add(
+            home='bcpp_survey',
+            household_member_actions=[action[0] for action in HOUSEHOLD_MEMBER_ACTION],
+            #membership_forms={'ABSENT': get_model('bcpp_subject', 'subjectabsentee')},
+            title='',  # 'A. Household Composition',
+            household_meta=Household._meta,
+            household_member_meta=HouseholdMember._meta,
+            household_structure_meta=HouseholdStructure._meta,
+            household_log_entry_meta=HouseholdLogEntry._meta,
+            enrolment_checklist_meta=EnrolmentChecklist._meta,
+            household_info_meta=HouseholdInfo._meta,
+            household=self.get_household(),
+            household_identifier=self.get_household().household_identifier,
+            household_structure=self.get_household_structure(),
+            household_members=self.get_household_members(),
+            household_log=self.get_household_log(),
+            household_log_entries=self.get_household_log_entries(),
+            first_name=self.get_first_name(),
+            current_member_count=self.get_current_member_count(),
+            survey=self.get_survey(),
+            surveys=self.get_surveys(),
+            allow_edit_members=self.allow_edit_members(),
+            has_household_log_entry=self.has_household_log_entry(),
+            household_info=self.get_household_info(),
+            mapper_name=self.get_mapper_name()
+            )
+
+    def set_dashboard_type_list(self):
+        self._dashboard_type_list = ['household']
+
+    def set_first_name(self, value=None):
+        self._first_name = value
+
+    def get_first_name(self):
+        if not self._first_name:
+            self.set_first_name()
+        return self._first_name
+
+    def has_household_log_entry(self):
+        """Confirms there is an househol_log_entry for today."""
+        today = date.today()
+        if self.get_household_log():
+            if not HouseholdLogEntry.objects.filter(
+                household_log=self.get_household_log(),
+                report_datetime__year=today.year,
+                report_datetime__month=today.month,
+                report_datetime__day=today.day):
+                return False
+        return True
+
+    def set_household_info(self):
+        self._household_info = None
+        if HouseholdInfo.objects.filter(household_structure=self.get_household_structure()):
+            self._household_info = HouseholdInfo.objects.get(household_structure=self.get_household_structure())
+
+    def get_household_info(self):
+        if not self._household_info:
+            self.set_household_info()
+        return self._household_info
+
+    def set_mapper_name(self, value=None):
+        self._mapper_name = value
+        if not self._mapper_name:
+            if self.get_household():
+                if not 'mapper_name' in dir(self.get_household()):
+                    raise AttributeError('Expected model Household to have attribute \'mapper_name\'.')
+                self._mapper_name = self.get_household().mapper_name
+
+    def get_mapper_name(self):
+        if not self._mapper_name:
+            self.set_mapper_name()
+        return self._mapper_name
+
+    def set_survey(self, value=None):
+        """Sets to the survey model class given a survey pk, slug or name or tries to determine the survey based on today\'s date.
+
+        .. warning:: this will fail if no survey name is provided and there is more than one survey defined where
+                     the survey dates overlap.
+        """
+        re_pk = re.compile('[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}')
+        re_survey_slug = re.compile('bcpp\-year\-[0-9]{1}')
+        re_survey_name = re.compile('BCPP\ Year\ [0-9]{1}')
+        if re_pk.match(str(value)):
+            self._survey = Survey.objects.get(pk=value)
+        elif re_survey_slug.match(str(value)):
+            self._survey = Survey.objects.get(survey_slug=value)
+        elif re_survey_name.match(str(value)):
+            self._survey = Survey.objects.get(survey_name=value)
+        elif Survey.objects.filter(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today()).count() == 1:
+            # assume only one survey
+            self._survey = Survey.objects.get(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today())
+        elif Survey.objects.filter(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today()).count() >= 1:
+            raise TypeError('Unable to set attribute _survey given survey=None and today\'s date. More than one survey exists for the given datetime (today). Either specify a survey or given a different date. Got {0}.'.format(Survey.objects.filter(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today())))
+        if not self._survey:
+            raise TypeError('Dashboard attribute _survey may not be null.')
+
+    def get_survey(self):
+        if not self._survey:
+            self.set_survey()
+        return self._survey
+
+    def set_surveys(self, value=None):
+        self._surveys = Survey.objects.all().order_by('survey_name')
+
+    def get_surveys(self):
+        if not self._surveys:
+            self.set_surveys()
+        return self._surveys
+
+    def get_current_member_count(self):
+        try:
+            self._current_member_count = self.household_members.count()
+        except:
+            self._current_member_count = 0
+
+    def set_household_structure(self, value=None, pk=None):
+        self._household_structure = value
+        if not self._household_structure:
+            if issubclass(self.get_dashboard_model(), HouseholdStructure):
+                self._household_structure = self.get_dashboard_model()
+            if self.get_household():
+                self._household_structure = HouseholdStructure.objects.get(household__pk=self.get_household().pk, survey=self.get_survey())
+            if HouseholdStructure.objects.filter(pk=pk, survey=self.get_survey()):
+                self._household_structure = HouseholdStructure.objects.get(pk=pk, survey=self.get_survey())
+        if not self._household_structure:
+            raise TypeError('Household_structure cannot be None. Using {0}.'.format((value, pk)))
+
+    def get_household_structure(self):
+        if not self._household_structure:
+            self.set_household_structure()
+        return self._household_structure
+
+    def set_household(self, pk=None, household_identifier=None, household_structure=None):
+        """Sets the household instance using household attributes pk or household_identifier or household_strcuture."""
+        self._household = None
+        if isinstance(self.get_dashboard_model_instance(), Household):
+            self._household = self.get_dashboard_model_instance()
+        if isinstance(self.get_dashboard_model_instance(), HouseholdStructure):
+            self._household = self.get_dashboard_model_instance().household
+        if household_identifier and not self._household:
+            if not Household.objects.filter(household_identifier=household_identifier):
+                raise TypeError('Cannot set Household using the household_identifier. Got {0}'.format(household_identifier))
+            self._household = Household.objects.get(household_identifier=household_identifier)
+        if pk and not self._household:
+            if not Household.objects.filter(pk=pk):
+                raise TypeError('Cannot set Household using the pk. Got {0}'.format(pk))
+            self._household = Household.objects.get(pk=pk)
+        if household_structure and not self._household:
+            self._household = household_structure.household
+        if not self._household:
+            raise TypeError('Household cannot be None. Using {0}'.format(pk, household_identifier, household_structure))
+
+    def get_household(self):
+        if not self._household:
+            raise TypeError('Dashboard attribute _household may not be null. Set this from kwargs in method create.')
+        return self._household
+
+    def set_household_members(self):
+        self._household_members = HouseholdMember.objects.filter(
+            household_structure__household=self.get_household(),
+            household_structure__survey=self.get_survey(),
+            ).order_by('first_name')
+
+    def get_household_members(self):
+        if not self._household_members:
+            self.set_household_members()
+        return self._household_members
+
+    def set_household_log(self):
+        if not HouseholdLog.objects.filter(household_structure=self.get_household_structure()):
+            self._household_log = HouseholdLog.objects.create(household_structure=self.get_household_structure())
+        else:
+            self._household_log = HouseholdLog.objects.get(household_structure=self.get_household_structure())
+
+    def get_household_log(self):
+        if not self._household_log:
+            self.set_household_log()
+        return self._household_log
+
+    def allow_edit_members(self):
+        allow_edit_members = False
+        if hasattr(settings, 'ALLOW_CHANGES_OTHER_SURVEYS') and settings.ALLOW_CHANGES_OTHER_SURVEYS:
+            allow_edit_members = True
+        else:
+            if self.get_survey():
+                if self.get_survey().datetime_start <= datetime.today() and datetime.today() <= self.get_survey().datetime_end:
+                    allow_edit_members = True
+        return allow_edit_members
+
+    def check_members_have_registered_subject(self):
+        for household_member in self.get_household_members():
+            if not RegisteredSubject.objects.filter(registration_identifier=household_member.internal_identifier):
+                raise ValueError('{0} expects all household_members to have '
+                                 'an entry in RegisterSubject. Got None for {1}.'.format(self, household_member,))
+
+    def get_household_log_entries(self):
+        return HouseholdLogEntry.objects.filter(household_log__household_structure=self.get_household_structure())
+
+#     def get_urlpatterns(self, view, regex, **kwargs):
+#         """Gets the url_patterns for the dashboard view.
+# 
+#         Called in the urls.py"""
+#         regex.update({'pk': '[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}'})
+#         regex.update({'dashboard_model': 'household|household_structure|registered_subject'})
+#         regex.update({'dashboard_type': 'household'})
+#         urlpatterns = patterns(view,
+#             url(r'^(?P<dashboard_type>{dashboard_type})/(?P<dashboard_model>{dashboard_model})/(?P<dashboard_id>{pk})/(?P<show>any)/$'.format(**regex),
+#               'household_dashboard',
+#                 name="household_dashboard_url"
+#                 ),
+#             url(r'^(?P<dashboard_type>{dashboard_type})/(?P<dashboard_model>{dashboard_model})/(?P<dashboard_id>{pk})/$'.format(**regex),
+#               'household_dashboard',
+#                 name="household_dashboard_url"
+#                 ))
+#         return urlpatterns
