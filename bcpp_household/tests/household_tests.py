b27b91136e31bbbaab811f61d3625864d838f887
erik van widenfelt <ew2789@gmail.com>
2013-09-11 17:16:04 +0200

    household now creates valid identifiers

--- bcpp_household/tests/household_tests.py
+++ bcpp_household/tests/household_tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from bhp_map.classes import site_mappers
 from bhp_identifier.exceptions import IdentifierError
 from bcpp_household.classes  import HouseholdIdentifier
-from bcpp_household.models import HouseholdIdentifierHistory
+from bcpp_household.models import HouseholdIdentifierHistory, Household
 from bcpp_survey.tests.factories import SurveyFactory
 from factories import PlotFactory, HouseholdFactory
 
@@ -11,6 +11,8 @@ class HouseholdTests(TestCase):
 
     def test_identifier(self):
 
+        print 'create a survey'
+        SurveyFactory()
         print 'get site mappers'
         site_mappers.autodiscover()
         print 'get one mapper'
@@ -31,17 +33,34 @@ class HouseholdTests(TestCase):
         self.assertEqual(HouseholdIdentifierHistory.objects.all().count(), 1)
         print 'clear history to start over'
         HouseholdIdentifierHistory.objects.all().delete()
-        print 'create a survey'
-        SurveyFactory()
+        print 'confirm no households yet'
+        self.assertEqual(Household.objects.all().count(), 0)
         print 'create a household'
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        HouseholdFactory(plot=plot)
-        print 'assert fails on tenth household'
-        self.assertRaises(IdentifierError, HouseholdFactory(plot=plot))
+        h1 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 1)
+        print h1
+        h2 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 2)
+        print h2
+        h3 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 3)
+        print h3
+        h4 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 4)
+        print h4
+        h5 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 5)
+        print h5
+        h6 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 6)
+        print h6
+        h7 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 7)
+        print h7
+        h8 = HouseholdFactory(plot=plot)
+        self.assertEqual(Household.objects.all().count(), 8)
+        print h8
+        h9 = HouseholdFactory(plot=plot)
+        print h9
+        print 'assert that you cannot create a tenth household in the same plot'
+        self.assertRaises(IdentifierError, HouseholdFactory, plot=plot)
