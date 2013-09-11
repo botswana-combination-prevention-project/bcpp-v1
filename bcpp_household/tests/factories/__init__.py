e64fd6a886c8b38970c01d2f6458121039648087
erik van widenfelt <ew2789@gmail.com>
2013-09-11 13:25:26 +0200

    start writing tests for plot model
    Former-commit-id: 249a596a60c8472853084c7896015af5ed173037

--- bcpp_household/tests/factories/__init__.py
+++ bcpp_household/tests/factories/__init__.py
@@ -3,3 +3,4 @@ from household_log_factory import HouseholdLogFactory
 from household_log_entry_factory import HouseholdLogEntryFactory
 from gps_device_factory import GpsDeviceFactory
 from household_structure_factory import HouseholdStructureFactory
+from plot_factory import PlotFactory
