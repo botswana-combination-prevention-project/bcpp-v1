441d20e3fc30ae11a2777678ebe23ed4e05b9fed
erik van widenfelt <ew2789@gmail.com>
2013-09-11 08:47:13 +0200

    remove ac from admin

--- admin/receive_admin.py
+++ admin/receive_admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from bhp_base_admin.admin import BaseModelAdmin
-from autocomplete.views import AutocompleteSettings
-from autocomplete.admin import AutocompleteAdmin
 from lab_receive.models import Receive
-
-
-class ReceiveAutocomplete(AutocompleteSettings):
-    search_fields = ('^receive_identifier',)
 
 
 class ReceiveAdmin(AutocompleteAdmin, BaseModelAdmin):
@@ -16,17 +10,6 @@ class ReceiveAdmin(AutocompleteAdmin, BaseModelAdmin):
             return ('receive_identifier',) + self.readonly_fields
         else:
             return self.readonly_fields
-
-#    def change_view(self, request, object_id, extra_context=None):
-#
-#        result = super(ReceiveAdmin, self).change_view(request, object_id, extra_context)
-#        if request.GET.get('return_object') == 'result':
-#            try:
-#                result = Result.objects.get(pk=request.GET.get('object_id'))
-#                result['Location'] = result.get_document_url()
-#            except:
-#                pass
-#        return result
 
     list_display = ('receive_identifier', 'patient', 'drawn_datetime', 'receive_datetime', 'protocol')
     search_fields = ('receive_identifier', 'patient__subject_identifier', 'protocol')
