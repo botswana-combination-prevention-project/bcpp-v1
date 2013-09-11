5a0a9fe486e761861ddcbce8a7e1b227564adb5b
erik van widenfelt <ew2789@gmail.com>
2013-09-11 14:32:10 +0200

    test and some corrections with plot identifier
    Former-commit-id: af713aec970eaf537dc7990cb84bc124bee9077e

--- bcpp_household/tests/plot_tests.py
+++ bcpp_household/tests/plot_tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from bhp_map.classes import site_mappers
 from bcpp_household.classes  import PlotIdentifier
+from bcpp_household.models import PlotIdentifier as PlotIdentifierHistory
 from factories import PlotFactory
 
 
@@ -15,11 +16,15 @@ class PlotTests(TestCase):
         print 'mapper is {0}'.format(mapper().get_map_area())
         print 'init plot identifier class'
         plot_identifier = PlotIdentifier(community=mapper().get_map_code())
+        self.assertEqual(PlotIdentifierHistory.objects.all().count(), 0)
         print 'get the plot_identifier, mode times than the modulus'
         id1 = plot_identifier.get_identifier()
-        for i in range(0, 20):
+        self.assertEqual(PlotIdentifierHistory.objects.all().count(), 1)
+        n = 20
+        for i in range(0, n):
             plot_identifier.get_identifier()
         id_last = plot_identifier.get_identifier()
+        self.assertEqual(PlotIdentifierHistory.objects.all().count(), n + 2)
         print 'length is consistent'
         self.assertEqual(len(id1), len(id_last))
         print 'create a plot model instance for community {0}'.format(mapper().get_map_area())
@@ -27,3 +32,5 @@ class PlotTests(TestCase):
         print 'assert plot_identifier is set'
         self.assertIsNotNone(plot.plot_identifier)
         print plot.plot_identifier
+        print 'assert expected number of instances in PlotIdentifierHistory ({0})'.format(n + 3)
+        self.assertEqual(PlotIdentifierHistory.objects.all().count(), n + 3)
