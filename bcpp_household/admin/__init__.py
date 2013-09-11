d51844d8e045b2e747e0f92874dfa193d540943f
erik van widenfelt <ew2789@gmail.com>
2013-09-11 09:39:21 +0200

    got plot_admin
    Former-commit-id: def16dff31046bf063ef44cc1c94cc3e231504ba

--- bcpp_household/admin/__init__.py
+++ bcpp_household/admin/__init__.py
@@ -3,4 +3,4 @@ from gps_device_admin import GpsDeviceAdmin
 from household_admin import HouseholdAdmin
 from household_log_admin import HouseholdLogAdmin, HouseholdLogEntryAdmin
 from household_structure_admin import HouseholdStructureAdmin
-#from plot_admin import PlotAdmin
+from plot_admin import PlotAdmin
