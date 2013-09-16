bcf6538bf79f51c7a4f185e0961b3930a1ebccd8
erik van widenfelt <ew2789@gmail.com>
2013-09-16 03:10:08 +0200

    changed subclass with dashboard methods from base subject to base dashboard
    Former-commit-id: 7656d459150a5a6ff78b24b2ef3beb5762b9fbe9

--- bhp_section/classes/base_section_for_dashboard_view.py
+++ bhp_section/classes/base_section_for_dashboard_view.py
@@ -3,12 +3,14 @@ from base_section_view import BaseSectionView
 
 
 # class Section(View):  # 1.5
-class BaseSubjectSectionView(BaseSectionView):
+class BaseSectionForDashboardView(BaseSectionView):
+
+    """Adds dashboard url methods to section views that show links to a dashboard."""
 
     dashboard_url_name = None
 
     def __init__(self):
-        super(BaseSubjectSectionView, self).__init__()
+        super(BaseSectionForDashboardView, self).__init__()
         self._dashboard_url_name = None
 
     def set_dashboard_url_name(self, value=None):
@@ -31,6 +33,6 @@ class BaseSubjectSectionView(BaseSectionView):
 
         .. note:: Overriding this method instead of :func:`contribute_to_context` so that users of the
                   class won't need to call super when overriding :func:`contribute_to_context`."""
-        context = super(BaseSubjectSectionView, self)._contribute_to_context(context, request, *args, **kwargs)
+        context = super(BaseSectionForDashboardView, self)._contribute_to_context(context, request, *args, **kwargs)
         context.update({'subject_dashboard_url': self.get_dashboard_url_name()})
         return context
