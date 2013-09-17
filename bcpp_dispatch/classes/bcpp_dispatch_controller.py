f6bd93f068e51fc1afe693b309648c4473f122c6
SirOne Pharatlhatlhe <sirone@One.local>
2013-09-17 15:04:02 +0200

    Fixing dispatch_container_lookup in subject_locator

--- bcpp_dispatch/classes/bcpp_dispatch_controller.py
+++ bcpp_dispatch/classes/bcpp_dispatch_controller.py
@@ -59,12 +59,12 @@ class BcppDispatchController(DispatchController):
         """Disconnects signals before saving the serialized object in _to_json."""
         signals.pre_save.disconnect(household_member_on_pre_save, weak=False, dispatch_uid="household_member_on_pre_save")
         signals.post_save.disconnect(household_member_on_post_save, weak=False, dispatch_uid="household_member_on_post_save")
-        signals.post_save.connect(base_household_member_consent_on_post_save, weak=False, dispatch_uid="base_household_member_consent_on_post_save")
+        signals.post_save.disconnect(base_household_member_consent_on_post_save, weak=False, dispatch_uid="base_household_member_consent_on_post_save")
         signals.post_save.disconnect(post_save_create_household, weak=False, dispatch_uid="post_save_create_household")
         signals.post_save.disconnect(base_subject_get_or_create_registered_subject_on_post_save, weak=False, dispatch_uid="base_subject_get_or_create_registered_subject_on_post_save")
-        signals.post_save.connect(create_household_structure_on_post_save, weak=False, dispatch_uid="create_household_structure_on_post_save")
-        signals.post_save.connect(household_structure_on_post_save, weak=False, dispatch_uid="household_structure_on_post_save")
-        signals.pre_save.connect(check_for_survey_on_pre_save, weak=False, dispatch_uid="check_for_survey_on_pre_save")
+        signals.post_save.disconnect(create_household_structure_on_post_save, weak=False, dispatch_uid="create_household_structure_on_post_save")
+        signals.post_save.disconnect(household_structure_on_post_save, weak=False, dispatch_uid="household_structure_on_post_save")
+        signals.pre_save.disconnect(check_for_survey_on_pre_save, weak=False, dispatch_uid="check_for_survey_on_pre_save")
         
     def reconnect_signals(self):
         """Reconnects signals after saving the serialized object in _to_json."""
