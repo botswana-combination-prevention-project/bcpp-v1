a63d894476d412a18494f66934963e678c8369be
ckgathi <ckgathi@bhp.org.bw>
2013-09-17 16:50:09 +0200

    fix import of signal creating a household from plot

--- bcpp_dispatch/classes/bcpp_dispatch_controller.py
+++ bcpp_dispatch/classes/bcpp_dispatch_controller.py
@@ -10,7 +10,7 @@ from bhp_dispatch.exceptions import DispatchError
 from bhp_dispatch.models import BaseDispatchSyncUuidModel
 from bcpp_subject.models import  BaseMemberStatusModel
 from bcpp_household_member.models import household_member_on_pre_save, household_member_on_post_save, base_household_member_consent_on_post_save
-from bcpp_household.models import post_save_create_household, create_household_structure_on_post_save, household_structure_on_post_save, \
+from bcpp_household.models import create_household_on_post_save, create_household_structure_on_post_save, household_structure_on_post_save, \
                                   check_for_survey_on_pre_save
 from bcpp_survey.models import Survey
 from bhp_registration.models import RegisteredSubject
@@ -60,7 +60,7 @@ class BcppDispatchController(DispatchController):
         signals.pre_save.disconnect(household_member_on_pre_save, weak=False, dispatch_uid="household_member_on_pre_save")
         signals.post_save.disconnect(household_member_on_post_save, weak=False, dispatch_uid="household_member_on_post_save")
         signals.post_save.disconnect(base_household_member_consent_on_post_save, weak=False, dispatch_uid="base_household_member_consent_on_post_save")
-        signals.post_save.disconnect(post_save_create_household, weak=False, dispatch_uid="post_save_create_household")
+        signals.post_save.disconnect(create_household_on_post_save, weak=False, dispatch_uid="create_household_on_post_save")
         signals.post_save.disconnect(base_subject_get_or_create_registered_subject_on_post_save, weak=False, dispatch_uid="base_subject_get_or_create_registered_subject_on_post_save")
         signals.post_save.disconnect(create_household_structure_on_post_save, weak=False, dispatch_uid="create_household_structure_on_post_save")
         signals.post_save.disconnect(household_structure_on_post_save, weak=False, dispatch_uid="household_structure_on_post_save")
@@ -71,7 +71,7 @@ class BcppDispatchController(DispatchController):
         signals.post_save.connect(household_member_on_post_save, weak=False, dispatch_uid="household_member_on_post_save")
         signals.pre_save.connect(household_member_on_pre_save, weak=False, dispatch_uid="household_member_on_pre_save")
         signals.post_save.connect(base_household_member_consent_on_post_save, weak=False, dispatch_uid="base_household_member_consent_on_post_save")
-        signals.post_save.connect(post_save_create_household, weak=False, dispatch_uid="post_save_create_household")
+        signals.post_save.connect(create_household_on_post_save, weak=False, dispatch_uid="create_household_on_post_save")
         signals.post_save.connect(base_subject_get_or_create_registered_subject_on_post_save, weak=False, dispatch_uid="base_subject_get_or_create_registered_subject_on_post_save")
         signals.post_save.connect(create_household_structure_on_post_save, weak=False, dispatch_uid="create_household_structure_on_post_save")
         signals.post_save.connect(household_structure_on_post_save, weak=False, dispatch_uid="household_structure_on_post_save")
