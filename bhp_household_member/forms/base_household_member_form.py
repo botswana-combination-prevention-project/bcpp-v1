d22e8138bf686ebaa7294e5053fb76261e7a6d8f
erik van widenfelt <ew2789@gmail.com>
2013-09-21 16:41:42 +0200

    FIX: first_name and initials only checked if there is a last_name in encrypted field.
    Former-commit-id: e66d973454d191e9ad18f456a1e31a3792d8c0df

--- bhp_household_member/forms/base_household_member_form.py
+++ bhp_household_member/forms/base_household_member_form.py
@@ -1,20 +1,21 @@
 from django import forms
 from bhp_base_form.forms import BaseModelForm
-from bhp_common.utils import check_initials_field
 
 
 class BaseHouseholdMemberForm(BaseModelForm):
 
-    def validate_cleaned_data(self, cleaned_data):
+    pass
 
-        cleaned_data = self.cleaned_data
-        #check if dispatched
-        household_structure = cleaned_data.get('household_structure', None)
-        if household_structure:
-            if household_structure.is_dispatched_as_item():
-                raise forms.ValidationError("Household is currently dispatched. Data may not be changed.")
-        my_initials = cleaned_data.get("initials")
-        my_first_name = cleaned_data.get("first_name")
-        check_initials_field(my_first_name, None, my_initials)
-        # Always return the full collection of cleaned data.
-        return cleaned_data
+#     def validate_cleaned_data(self, cleaned_data):
+# 
+#         cleaned_data = self.cleaned_data
+#         #check if dispatched. TODO: is this dispatch stuff necessary?
+#         household_structure = cleaned_data.get('household_structure', None)
+#         first_name = cleaned_data.get("first_name")
+# #         if household_structure:
+# #             if household_structure.is_dispatched_as_item():
+# #                 raise forms.ValidationError("Household is currently dispatched. Data may not be changed.")
+#         #check name not repeated
+#         if self.model.objects.get(household_structure_id=household_structure, first_name=first_name).exists():
+#             raise forms.ValidationError("{0} is already listed as a household member.".format(first_name))
+#         return cleaned_data
