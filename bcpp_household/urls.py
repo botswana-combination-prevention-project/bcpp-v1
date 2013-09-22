53b6806ec93e43af48408bec5ccc5f556ffad9e0
erik van widenfelt <ew2789@gmail.com>
2013-09-22 18:26:43 +0200

    FIX: for 1.5, remove reference to django.conf.urls.defaults
    Former-commit-id: 21f193af1ca767f7dfed5295325e17d04a19512d

--- bcpp_household/urls.py
+++ bcpp_household/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('bcpp_household.views',
     url(r'^return/(?P<household>[A-Z0-9\-0-9]+)/', 'return_households')
