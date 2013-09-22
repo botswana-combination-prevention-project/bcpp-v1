fcce636d0abe6f121f47db94e6fadd8a4e9fd6c9
erik van widenfelt <ew2789@gmail.com>
2013-09-22 18:26:43 +0200

    FIX: for 1.5, remove reference to django.conf.urls.defaults
    Former-commit-id: 21f193af1ca767f7dfed5295325e17d04a19512d

--- bhp_dispatch/urls.py
+++ bhp_dispatch/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 
 admin.autodiscover()
@@ -6,6 +6,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^return/(?P<producer>[a-z0-9\-\_\.]+)/', 'return_items'),
     url(r'^return/(?P<identifier>\w+)/', 'return_households', name='return_household'),
-    #url(r'^return_selectively/(?P<producer>[a-z0-9\-\_\.]+)/', 'return_selectively'),
-    #url(r'^', 'return_items',),
     )
