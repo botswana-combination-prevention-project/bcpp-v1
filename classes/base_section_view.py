072b4bac34e6d75e0ad134a187a998f1f3988349
erik van widenfelt <ew2789@gmail.com>
2013-09-24 14:27:50 +0200

    Squashed 'bhp_section/' changes from 3591da5..e286c04
    e286c04 merge conflict bhp_section.
    
    git-subtree-dir: bhp_section
    git-subtree-split: e286c0476f9b5798f8bf048dabc40b017338e8b2

--- classes/base_section_view.py
+++ classes/base_section_view.py
@@ -330,10 +330,6 @@ class BaseSectionView(object):
             * search_model_admin_url: 'url',
             * search_result: search_result,
             * search_result_include_file: search_result_include_file,
-<<<<<<< HEAD
-
-=======
->>>>>>> 575351681bf093f96ba476dc5d2500f1410c5543
         """
         self.set_section_name(kwargs.get('section_name'))
         search_term = kwargs.get('search_term', '')
