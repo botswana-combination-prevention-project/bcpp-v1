4bcd743a683c90ba3645f24889c7ae1a54afad51
Twice Tshwenyane <twicet@gmail.com>
2013-09-25 09:39:04 +0200

    updated the database settings
    Former-commit-id: 11e31e75b9bbcc602383991a04d635d1201211d3

--- settings.py
+++ settings.py
@@ -83,7 +83,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066_migrated',
+            'NAME': 'bhp066',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
