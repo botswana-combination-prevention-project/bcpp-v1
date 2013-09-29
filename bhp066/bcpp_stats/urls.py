fccf6cec710c23f29122b666695038af0b60ed3e
erik van widenfelt <ew2789@gmail.com>
2013-09-29 20:50:37 +0200

    project restructure
    Former-commit-id: fca83cc3ea0a5c83ef49f7d22dc9d920b39c6a61

--- bhp066/bcpp_stats/urls.py
+++ bhp066/bcpp_stats/urls.py
@@ -1,7 +1,4 @@
 from django.conf.urls import patterns, url
-from django.contrib import admin
-
-admin.autodiscover()
 
 urlpatterns = patterns('bhp_model_describer.views',
     url(r'^model_describer/', 'model_describer', name="describer_url_name"),
