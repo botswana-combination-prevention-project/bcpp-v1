d9a6af5d480d9da08cd3de312f55df8dfbda2135
SirOne Pharatlhatlhe <sirone@One.local>
2013-10-03 14:30:24 +0200

    sync and consume in one step

--- bhp066/bhp066/settings/dev_sirone.py
+++ bhp066/bhp066/settings/dev_sirone.py
@@ -8,7 +8,7 @@ from .dev import *
 mysql_db = DBConfig(mysql_base_config, PASSWORD=env('DEV_DB_PASSWORD'))
 
 DATABASES = {
-    'default': mysql_db(NAME='bhp066_migrated'),
+    'default': mysql_db(NAME='bhp066'),
     'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
     'mpp15-bhp066': mysql_db(NAME='bhp066_survey', HOST='192.168.1.36'),
 }
@@ -16,9 +16,7 @@ DATABASES = {
 
 #Things to put in your .bashrc or .bash_profile file. If you don't the app will blow up
 # export TEST_DB_PASSWORD=cc3721b
-# export DEV_DB_PASSWORD='cc3721b'
+# export DEV_DB_PASSWORD=cc3721b
 # export PROD_DB_PASSWORD=cc3721b
-# export SECRET_KEY=0$q&@p=jz(+_r^+phzenyqi49#y2^3ot3h#jru+32z&+cm&j51
-# exportEMAIL_HOST_PASSWORD=paeH#ie9
-
-# alias='export DJANGO_SETTINGS_MODULE=bhp066; python manage.py test'
+# export SECRET_KEY='0$q&@p=jz(+_r^+phzenyqi49#y2^3ot3h#jru+32z&+cm&j51'
+# export EMAIL_HOST_PASSWORD=paeH#ie9
