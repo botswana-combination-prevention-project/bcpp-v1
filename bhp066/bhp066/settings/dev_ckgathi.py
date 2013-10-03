b90e0a4ce294e88d6074f7a1d47f06c7167478f2
ckgathi <ckgathi@bhp.org.bw>
2013-10-03 08:47:30 +0200

    settings file
    Former-commit-id: f8d4eacb2d8e3e5150925c2aedcc96eb58ea69aa

--- bhp066/bhp066/settings/dev_ckgathi.py
+++ bhp066/bhp066/settings/dev_ckgathi.py
@@ -8,7 +8,7 @@ from .dev import *
 mysql_db = DBConfig(mysql_base_config, PASSWORD=env('DEV_DB_PASSWORD'))
 
 DATABASES = {
-    'default': mysql_db(NAME='bhp066_migrated'),
+    'default': mysql_db(NAME='bhp066'),
     'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
     'mpp15-bhp066': mysql_db(NAME='bhp066_survey', HOST='192.168.1.36'),
 }
@@ -19,6 +19,6 @@ DATABASES = {
 # export DEV_DB_PASSWORD='cc3721b'
 # export PROD_DB_PASSWORD=cc3721b
 # export SECRET_KEY=0$q&@p=jz(+_r^+phzenyqi49#y2^3ot3h#jru+32z&+cm&j51
-# exportEMAIL_HOST_PASSWORD=paeH#ie9
+# export EMAIL_HOST_PASSWORD=paeH#ie9
 
 # alias='export DJANGO_SETTINGS_MODULE=bhp066; python manage.py test'
