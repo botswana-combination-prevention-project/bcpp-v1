8845622d0868e90986ce0c307f6df273bef715ff
ckgathi <ckgathi@bhp.org.bw>
2013-10-08 15:09:26 +0200

    urls for adding plot
    Former-commit-id: 95bc37154f7599f67d7d28e369e1f7f5b34bc5c5

--- bhp066/apps/bcpp_household/urls.py
+++ bhp066/apps/bcpp_household/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from .views import *
 
-urlpatterns = patterns('bcpp_household.views',
+urlpatterns = patterns('',
     url(r'^return/(?P<household>[A-Z0-9\-0-9]+)/', 'return_households'),
     url(r'^add_household_index/(?P<plot>[0-9\-0-9]+)/', add_household_index, name='add_household_index_url'),
     url(r'^add_household/(?P<plot>[0-9\-0-9]+)/', add_household, name='add_household_url')
