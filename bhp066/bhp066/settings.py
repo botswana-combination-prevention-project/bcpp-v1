27240c1776092301c8368f465f3f6be1a2ac582d
ckgathi <ckgathi@bhp.org.bw>
2013-10-08 15:09:26 +0200

    urls for adding plot
    Former-commit-id: 95bc37154f7599f67d7d28e369e1f7f5b34bc5c5

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -97,7 +97,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066_migrated',
+            'NAME': 'bhp066',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
