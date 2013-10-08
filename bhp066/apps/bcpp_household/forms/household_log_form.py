d044e5ac4b44d9d9b56ef11cb47fc1ec60b4e034
erik van widenfelt <ew2789@gmail.com>
2013-10-08 23:49:12 +0200

    add QA to eligibility checklist, remove save.

--- bhp066/apps/bcpp_household/forms/household_log_form.py
+++ bhp066/apps/bcpp_household/forms/household_log_form.py
@@ -1,14 +1,9 @@
+from django import forms
 from edc.base.form.forms import BaseModelForm
 from ..models import HouseholdLog, HouseholdLogEntry
 
 
 class HouseholdLogForm(BaseModelForm):
-
-    def clean(self):
-
-        cleaned_data = super(HouseholdLogForm, self).clean()
-
-        return cleaned_data
 
     class Meta:
         model = HouseholdLog
@@ -17,9 +12,11 @@ class HouseholdLogForm(BaseModelForm):
 class HouseholdLogEntryForm(BaseModelForm):
 
     def clean(self):
-
         cleaned_data = super(HouseholdLogEntryForm, self).clean()
-
+        # confirm next_appt_datetime on on or after report datetime
+        if cleaned_data.get('next_appt_datetime'):
+            if not cleaned_data.get('next_appt_datetime') >= cleaned_data.get('report_datetime'):
+                raise forms.ValidationError('The next appointment date must be on or after the report datetime. You entered {0}'.format(cleaned_data.get('next_appt_datetime').strftime('%Y-%m-%d')))
         return cleaned_data
 
     class Meta:
