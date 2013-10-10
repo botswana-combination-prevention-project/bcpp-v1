a47f9a4ccf6228a1b712282ada3bfad9bda51018
erik van widenfelt <ew2789@gmail.com>
2013-10-10 07:39:39 +0200

    can only be one head of household
    Former-commit-id: c97bfc47b5fdcf7cce949d7b7c34a50bdeb73ba3

--- bhp066/apps/bcpp_household_member/forms/household_member_form.py
+++ bhp066/apps/bcpp_household_member/forms/household_member_form.py
@@ -1,8 +1,21 @@
+from django import forms
+
 from base_household_member_form import BaseHouseholdMemberForm
 from ..models import HouseholdMember
 
 
 class HouseholdMemberForm(BaseHouseholdMemberForm):
+
+    def clean(self):
+        cleaned_data = super(HouseholdMemberForm, self).clean()
+        # only allow one person to be Head of Household
+        household_structure = cleaned_data.get('household_structure')
+        initials = cleaned_data.get('initials')
+        if cleaned_data.get('relation') == 'Head':
+            if HouseholdMember.objects.filter(household_structure=household_structure, relation='Head').exclude(initials=initials):
+                household_member = HouseholdMember.objects.get(household_structure=household_structure, relation='Head')
+                raise forms.ValidationError('The Head of Household member has already been added. See {0}'.format(household_member))
+        return cleaned_data
 
     class Meta:
         model = HouseholdMember
