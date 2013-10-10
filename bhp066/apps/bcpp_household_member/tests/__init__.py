38d918c6b50a2dd2519c06024259e59c727aad99
erik van widenfelt <ew2789@gmail.com>
2013-10-10 07:39:39 +0200

    can only be one head of household

--- bhp066/apps/bcpp_household_member/tests/__init__.py
+++ bhp066/apps/bcpp_household_member/tests/__init__.py
@@ -2,3 +2,4 @@
 # from .natural_key_tests import NaturalKeyTests
 # from .household_composition_tests import HouseholdCompositionTests
 from .eligibility_checklist_tests import EligibilityChecklistTests
+from .household_member_tests import HouseholdMemberTests
