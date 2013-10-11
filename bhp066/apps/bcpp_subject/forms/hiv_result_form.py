08d3db70ebf82ba6c9ddad19462878274ce684fd
SirOne Pharatlhatlhe <sirone@One.local>
2013-10-11 10:20:38 +0200

    Fixing minor issues that prevented forms from saving in bcpp_subject
    Former-commit-id: f79bce9f919a9299ca40472bc67e6ee7de714936

--- bhp066/apps/bcpp_subject/forms/hiv_result_form.py
+++ bhp066/apps/bcpp_subject/forms/hiv_result_form.py
@@ -14,15 +14,15 @@ class HivResultForm (BaseSubjectModelForm):
             raise forms.ValidationError('If participant has declined testing, provide reason participant declined testing')
         
         #testing declined but giving test date
-        if ((cleaned_data.get('hiv_result', None) == 'Declined' or 'Not performed') and cleaned_data.get('hiv_result_datetime', None)):
+        if ((cleaned_data.get('hiv_result', None) == 'Declined') or (cleaned_data.get('hiv_result', None) =='Not performed')) and (cleaned_data.get('hiv_result_datetime', None)):
             raise forms.ValidationError('If testing was NOT done, DO NOT give date and time of testing')
         
         #testing done but giving reason why not done
-        if ((cleaned_data.get('hiv_result', None) != 'Declined' or 'Not performed') and cleaned_data.get('why_not_tested', None)):
+        if ((cleaned_data.get('hiv_result', None) != 'Declined') or (cleaned_data.get('hiv_result', None) != 'Not performed')) and (cleaned_data.get('why_not_tested', None)):
             raise forms.ValidationError('If testing is performed, DO NOT provide reason for declining test')
         
         #testing done but not providing date
-        if ((cleaned_data.get('hiv_result', None) != 'Declined' or 'Not performed') and not cleaned_data.get('hiv_result_datetime', None)):
+        if ((cleaned_data.get('hiv_result', None) != 'Declined') or (cleaned_data.get('hiv_result', None) != 'Not performed')) and not (cleaned_data.get('hiv_result_datetime', None)):
             raise forms.ValidationError('If test has been performed, what is the test result date time?')
         
         return cleaned_data
