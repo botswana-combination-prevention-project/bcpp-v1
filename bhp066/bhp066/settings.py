158aa8e6536fdea89d39c432af6ab4b3b00bfb54
erik van widenfelt <ew2789@gmail.com>
2013-10-12 10:12:35 +0200

    FIX #151: age on dashboard.
    Former-commit-id: 8ac1ba344e8e94008cbdf7b54f693a5605b5c317

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -22,13 +22,10 @@ TEMPLATE_DIRS = (
     )
 STATICFILES_DIRS = ()
 CONFIG_DIR = PROJECT_DIR.child('bhp066')
-
 #KEY_PATH = PROJECT_DIR.child('keys')
-
 #KEY_PATH = '/Users/ckgathi/source/mappers/bhp066/keys'
 #KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
 KEY_PATH = '/Volumes/bhp066/keys'
-
 MAP_DIR = STATIC_ROOT.child('img')
 
 MANAGERS = ADMINS
