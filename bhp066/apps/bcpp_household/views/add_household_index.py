4d656ba5615f7b9ea3170bbc5982f29f27f8347c
erik van widenfelt <ew2789@gmail.com>
2013-10-13 21:20:54 +0200

    improve plot form, model, tests
    Former-commit-id: 5cf89b0a2c3a96246018f448a0e3a68c6b91de7f

--- bhp066/apps/bcpp_household/views/add_household_index.py
+++ bhp066/apps/bcpp_household/views/add_household_index.py
@@ -3,15 +3,14 @@ from django.template import RequestContext
 
 from ..models import Plot, Household
 
-def add_household_index(request, **kwargs):
-    """Allows to add household in a plot.
-    """
 
+def add_household_index(request, **kwargs):
+    """Allows to add household in a plot."""
     template = 'add_household.html'
     plot_identifier = kwargs.get('plot')
-    message =  ""
+    message = ""
     plot = Plot.objects.filter(plot_identifier=plot_identifier)
-    households = Household.objects.filter(plot__plot_identifier=plot_identifier).order_by('household_sequence')
+    households = Household.objects.filter(plot__plot_identifier=plot_identifier).order_by('household_identifier')
     household_meta = Household._meta
     return render_to_response(
             template, {
