d16750f4e355e47e907e278fafc48b72a80fa518
erik van widenfelt <ew2789@gmail.com>
2013-10-14 16:07:30 +0200

    rename num_household, work on get_most_recent in search, update plot and plot tests.
    Former-commit-id: 6c9ce05fc462465639827a7e6367a35d46feb15e

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -25,7 +25,7 @@ CONFIG_DIR = PROJECT_DIR.child('bhp066')
 #KEY_PATH = PROJECT_DIR.child('keys')
 #KEY_PATH = '/Users/ckgathi/source/mappers/bhp066/keys'
 #KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
-KEY_PATH = '/Volumes/keys'
+KEY_PATH = '/Volumes/bhp066/keys'
 MAP_DIR = STATIC_ROOT.child('img')
 
 MANAGERS = ADMINS
@@ -99,7 +99,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066',
+            'NAME': 'bhp066_v123',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
