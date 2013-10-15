505b9dce959436520d837f6cfa1c6d01acf30ba4
erik van widenfelt <ew2789@gmail.com>
2013-10-15 13:54:37 +0200

    changes to search and section

--- bhp066/apps/bcpp_household/section/section_household_view.py
+++ bhp066/apps/bcpp_household/section/section_household_view.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from edc.dashboard.section.classes import BaseSectionForDashboardView, site_sections
 from edc.map.classes import site_mappers
 from apps.bcpp_survey.forms import SurveyForm
-from ..forms.current_gps_form import CurrentGpsForm
 from ..models import Household, Plot
 from ..forms import CommunityForm
 
@@ -32,66 +31,67 @@ class SectionHouseholdView(BaseSectionForDashboardView):
             community_form = None
         else:
             community_form = CommunityForm()
-        gps_search_form = CurrentGpsForm(initial={'community': current_community, 'radius': 100})
+#         gps_search_form = CurrentGpsForm(initial={'community': current_community, 'radius': 100})
         context.update({'community_form': community_form,
                         'current_community': current_community,
                         'mapper_name': current_community,
-                        'gps_search_form': gps_search_form})
+#                         'gps_search_form': gps_search_form,
+                        })
         return context
 
-    def get_search_result(self, request, **kwargs):
-        search_result = None
-        community = settings.CURRENT_COMMUNITY
-        mapper = site_mappers.get(community)()
-        if request.method == 'POST':
-            gps_form = CurrentGpsForm(request.POST)
-            if gps_form.is_valid():
-                degrees_s = gps_form.cleaned_data.get('degrees_s')
-                degrees_e = gps_form.cleaned_data.get('degrees_e')
-                minutes_s = float('{0}'.format(gps_form.cleaned_data.get('minutes_s')))
-                minutes_e = float('{0}'.format(gps_form.cleaned_data.get('minutes_e')))
-                radius = gps_form.cleaned_data.get('radius') / 1000
-                lat = mapper.get_gps_lat(degrees_s, minutes_s)
-                lon = mapper.get_gps_lon(degrees_e, minutes_e)
-                search_result = []
-                lst = []
-                dct = {}
-                for household in Household.objects.filter(**{mapper.map_area_field_attr: community}):
-                    dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
-                    if dist <= radius:
-                        household.relative_distance = dist
-                        while dist in lst:
-                            dist += .0001
-                        lst.append(dist)
-                        dct.update({dist: household})
-                lst.sort()
-                for dist in lst:
-                    search_result.append(dct[dist])
-                request.session['search_result'] = search_result
-            else:
-                self.context.update({'gps_search_form': gps_form})
-        elif request.GET.get('plot'):
-            search_result = []
-            lst = []
-            dct = {}
-            plot = Plot.objects.filter(plot_identifier=request.GET.get('plot'))
-            lat = plot[0].gps_target_lat
-            lon = plot[0].gps_target_lon
-            radius = 1
-            for household in Household.objects.filter(plot__plot_identifier=request.GET.get('plot')):
-                dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
-                if dist <= radius:
-                    household.relative_distance = dist
-                    while dist in lst:
-                        dist += .0001
-                    lst.append(dist)
-                    dct.update({dist: household})
-            lst.sort()
-            for dist in lst:
-                search_result.append(dct[dist])
-            request.session['search_result'] = search_result
-        else:
-            if request.GET.get('page'):
-                search_result = request.session['search_result']
-        return search_result
+#     def get_search_result(self, request, **kwargs):
+#         search_result = None
+#         community = settings.CURRENT_COMMUNITY
+#         mapper = site_mappers.get(community)()
+#         if request.method == 'POST':
+#             gps_form = CurrentGpsForm(request.POST)
+#             if gps_form.is_valid():
+#                 degrees_s = gps_form.cleaned_data.get('degrees_s')
+#                 degrees_e = gps_form.cleaned_data.get('degrees_e')
+#                 minutes_s = float('{0}'.format(gps_form.cleaned_data.get('minutes_s')))
+#                 minutes_e = float('{0}'.format(gps_form.cleaned_data.get('minutes_e')))
+#                 radius = gps_form.cleaned_data.get('radius') / 1000
+#                 lat = mapper.get_gps_lat(degrees_s, minutes_s)
+#                 lon = mapper.get_gps_lon(degrees_e, minutes_e)
+#                 search_result = []
+#                 lst = []
+#                 dct = {}
+#                 for household in Household.objects.filter(**{mapper.map_area_field_attr: community}):
+#                     dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
+#                     if dist <= radius:
+#                         household.relative_distance = dist
+#                         while dist in lst:
+#                             dist += .0001
+#                         lst.append(dist)
+#                         dct.update({dist: household})
+#                 lst.sort()
+#                 for dist in lst:
+#                     search_result.append(dct[dist])
+#                 request.session['search_result'] = search_result
+#             else:
+#                 self.context.update({'gps_search_form': gps_form})
+#         elif request.GET.get('plot'):
+#             search_result = []
+#             lst = []
+#             dct = {}
+#             plot = Plot.objects.filter(plot_identifier=request.GET.get('plot'))
+#             lat = plot[0].gps_target_lat
+#             lon = plot[0].gps_target_lon
+#             radius = 1
+#             for household in Household.objects.filter(plot__plot_identifier=request.GET.get('plot')):
+#                 dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
+#                 if dist <= radius:
+#                     household.relative_distance = dist
+#                     while dist in lst:
+#                         dist += .0001
+#                     lst.append(dist)
+#                     dct.update({dist: household})
+#             lst.sort()
+#             for dist in lst:
+#                 search_result.append(dct[dist])
+#             request.session['search_result'] = search_result
+#         else:
+#             if request.GET.get('page'):
+#                 search_result = request.session['search_result']
+#         return search_result
 site_sections.register(SectionHouseholdView)
