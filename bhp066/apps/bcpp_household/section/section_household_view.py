84b7e1c75a9903d1bba087b0598487c8688197a5
erik van widenfelt <ew2789@gmail.com>
2013-10-19 19:44:28 +0200

    changes to section, search and related templates.
    Former-commit-id: 70f593042a5d9356ce36bc609435c8f1fdef141e

--- bhp066/apps/bcpp_household/section/section_household_view.py
+++ bhp066/apps/bcpp_household/section/section_household_view.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from edc.dashboard.section.classes import BaseSectionForDashboardView, site_sections
 from edc.map.classes import site_mappers
-from apps.bcpp_survey.forms import SurveyForm
-from ..models import Household, Plot
-from ..forms import CommunityForm
+from apps.bcpp_survey.models import Survey
+
+from ..forms import GpsSearchForm
+from ..search import HouseholdSearchByWord, HouseholdSearchByGps
 
 
 site_mappers.autodiscover()
@@ -12,86 +13,21 @@ site_mappers.autodiscover()
 class SectionHouseholdView(BaseSectionForDashboardView):
     section_name = 'household'
     section_display_name = 'Households'
-    add_model = Household
     section_display_index = 20
     section_template = 'section_bcpp_household.html'
     dashboard_url_name = 'household_dashboard_url'
+    search = {'word': HouseholdSearchByWord, 'gps': HouseholdSearchByGps}
 
     def contribute_to_context(self, context, request, *args, **kwargs):
-        current_survey = None
-        if 'CURRENT_SURVEY' in dir(settings):
-            current_survey = settings.CURRENT_SURVEY
-            survey_form = None
-        else:
-            survey_form = SurveyForm()
-        context.update({'survey_form': survey_form, 'current_survey': current_survey})
-        current_community = None
-        if 'CURRENT_COMMUNITY' in dir(settings):
-            current_community = settings.CURRENT_COMMUNITY
-            community_form = None
-        else:
-            community_form = CommunityForm()
-#         gps_search_form = CurrentGpsForm(initial={'community': current_community, 'radius': 100})
-        context.update({'community_form': community_form,
-                        'current_community': current_community,
-                        'mapper_name': current_community,
-#                         'gps_search_form': gps_search_form,
-                        })
+        current_community = settings.CURRENT_COMMUNITY
+        context.update({
+            'current_survey': Survey.objects.current_survey(),
+            'current_community': self.get_current_community(),
+            'mapper_name': current_community,
+            'gps_search_form': GpsSearchForm(initial={'radius': 100})})
         return context
 
-#     def get_search_result(self, request, **kwargs):
-#         search_result = None
-#         community = settings.CURRENT_COMMUNITY
-#         mapper = site_mappers.get(community)()
-#         if request.method == 'POST':
-#             gps_form = CurrentGpsForm(request.POST)
-#             if gps_form.is_valid():
-#                 degrees_s = gps_form.cleaned_data.get('degrees_s')
-#                 degrees_e = gps_form.cleaned_data.get('degrees_e')
-#                 minutes_s = float('{0}'.format(gps_form.cleaned_data.get('minutes_s')))
-#                 minutes_e = float('{0}'.format(gps_form.cleaned_data.get('minutes_e')))
-#                 radius = gps_form.cleaned_data.get('radius') / 1000
-#                 lat = mapper.get_gps_lat(degrees_s, minutes_s)
-#                 lon = mapper.get_gps_lon(degrees_e, minutes_e)
-#                 search_result = []
-#                 lst = []
-#                 dct = {}
-#                 for household in Household.objects.filter(**{mapper.map_area_field_attr: community}):
-#                     dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
-#                     if dist <= radius:
-#                         household.relative_distance = dist
-#                         while dist in lst:
-#                             dist += .0001
-#                         lst.append(dist)
-#                         dct.update({dist: household})
-#                 lst.sort()
-#                 for dist in lst:
-#                     search_result.append(dct[dist])
-#                 request.session['search_result'] = search_result
-#             else:
-#                 self.context.update({'gps_search_form': gps_form})
-#         elif request.GET.get('plot'):
-#             search_result = []
-#             lst = []
-#             dct = {}
-#             plot = Plot.objects.filter(plot_identifier=request.GET.get('plot'))
-#             lat = plot[0].gps_target_lat
-#             lon = plot[0].gps_target_lon
-#             radius = 1
-#             for household in Household.objects.filter(plot__plot_identifier=request.GET.get('plot')):
-#                 dist = mapper.gps_distance_between_points(lat, lon, household.gps_target_lat, household.gps_target_lon, radius)
-#                 if dist <= radius:
-#                     household.relative_distance = dist
-#                     while dist in lst:
-#                         dist += .0001
-#                     lst.append(dist)
-#                     dct.update({dist: household})
-#             lst.sort()
-#             for dist in lst:
-#                 search_result.append(dct[dist])
-#             request.session['search_result'] = search_result
-#         else:
-#             if request.GET.get('page'):
-#                 search_result = request.session['search_result']
-#         return search_result
+    def get_current_community(self):
+        return settings.CURRENT_COMMUNITY
+
 site_sections.register(SectionHouseholdView)
