0d8c122f2d7514873915b369f859c93e56a765d6
erik van widenfelt <ew2789@gmail.com>
2013-10-20 18:00:40 +0200

    fixes #203 by adding url to search context.
    Former-commit-id: f86a1747338f780c837b04349b94006b496ecd4e

--- bhp066/apps/bcpp_subject/search/subject_search_by_word.py
+++ bhp066/apps/bcpp_subject/search/subject_search_by_word.py
@@ -9,3 +9,10 @@ class SubjectSearchByWord(BaseSearchByWord):
     search_model = SubjectConsent
     order_by = '-created'
     template = 'subjectconsent_include.html'
+
+    def contribute_to_context(self, context):
+        context = super(BaseSearchByWord, self).contribute_to_context(context)
+        # FIXME: this should not be hard coded, get it from the section class somehow.
+        context.update({
+            'subject_dashboard_url': 'subject_dashboard_url'})
+        return context
