bdf2dd9ef665e8499fc1323e5ee9039f18c9ba0e
erik van widenfelt <ew2789@gmail.com>
2013-10-21 18:37:46 +0200

    fix imports
    Former-commit-id: dbfb92dac9e32bd3cd51a1cc3e90390671b624fe

--- bhp066/apps/bcpp_htc_subject/tests/factories/htc_recent_partner_factory.py
+++ bhp066/apps/bcpp_htc_subject/tests/factories/htc_recent_partner_factory.py
@@ -1,7 +1,7 @@
 import factory
 from datetime import date, datetime
 from edc.base.model.tests.factories import BaseUuidModelFactory
-from ..models import HtcRecentPartner
+from ...models import HtcRecentPartner
 from htc_subject_visit_factory import HtcSubjectVisitFactory
 
 
