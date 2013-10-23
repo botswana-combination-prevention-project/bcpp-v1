075215ea6f6564f0ca2131ec7877b9b88fb408e0
erik van widenfelt <ew2789@gmail.com>
2013-10-23 10:05:52 +0200

    add subject_referral and tests.
    Former-commit-id: a9db25015215b018b7b01eb7e8db5a530e6423a3

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -25,8 +25,9 @@ CONFIG_DIR = PROJECT_DIR.child('bhp066')
 
 # KEY_PATH = 'keys'
 # KEY_PATH = '/Users/ckgathi/source/mappers/bhp066/keys'
-KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
+# KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
 #KEY_PATH = '/Volumes/keys'
+KEY_PATH = '/Volumes/bhp066/keys'
 
 MAP_DIR = STATIC_ROOT.child('img')
 
@@ -245,7 +246,7 @@ INSTALLED_APPS = (
     'django_databrowse',
     'dajaxice',
     'dajax',
-    #'south',
+    'south',
 
     'edc.audit',
 
