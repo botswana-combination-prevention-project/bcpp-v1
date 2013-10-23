d79eb4c5bdb1f0801b2ae29c86d369dd296f73a6
Twice Tshwenyane <twicet@gmail.com>
2013-10-23 16:10:23 +0200

    fixed settings for netbooks
    Former-commit-id: 580d8f1393908445c2e21aac8780f6f98d597632

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -23,11 +23,11 @@ TEMPLATE_DIRS = (
 STATICFILES_DIRS = ()
 CONFIG_DIR = PROJECT_DIR.child('bhp066')
 
-# KEY_PATH = 'keys'
+#KEY_PATH = 'keys'
 # KEY_PATH = '/Users/ckgathi/source/mappers/bhp066/keys'
 # KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
-#KEY_PATH = '/Volumes/keys'
-KEY_PATH = '/Volumes/bhp066/keys'
+KEY_PATH = '/Volumes/keys'
+#KEY_PATH = '/Volumes/bhp066/keys'
 
 MAP_DIR = STATIC_ROOT.child('img')
 
@@ -102,7 +102,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066_v124',
+            'NAME': 'bhp066',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
