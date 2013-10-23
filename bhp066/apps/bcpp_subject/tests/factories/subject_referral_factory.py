55f6d73cebb029511851a1d22491128c73510913
erik van widenfelt <ew2789@gmail.com>
2013-10-23 10:05:52 +0200

    add subject_referral and tests.

--- bhp066/apps/bcpp_subject/tests/factories/subject_referral_factory.py
+++ bhp066/apps/bcpp_subject/tests/factories/subject_referral_factory.py
@@ -9,12 +9,5 @@ from edc.subject.registration.tests.factories import RegisteredSubjectFactory
 
 class SubjectReferralFactory(BaseUuidModelFactory):
     FACTORY_FOR = SubjectReferral
-    
-    registered_subject = factory.SubFactory(RegisteredSubjectFactory)
-    household_member = factory.SubFactory(HouseholdMemberFactory)
+
     report_datetime = datetime.today()
-    survey = factory.SubFactory(SurveyFactory)
-    subject_referral_reason = (('receive', u'Referred to receive HIV result in clinic'), ('test', u'Referred to test in clinic'), ('protocol', u'Referred as per protocol'))[0][0]
-    subject_referral_reason_other = factory.Sequence(lambda n: 'subject_referral_reason_other{0}'.format(n))
-    in_clinic = (('Yes', '<django.utils.functional.__proxy__ object at 0x1021b8810>'), ('No', '<django.utils.functional.__proxy__ object at 0x1021b8850>'))[0][0]
-    comment = factory.Sequence(lambda n: 'comment{0}'.format(n))
