68787216d3fdb1937be4fd27482671169827da76
erik van widenfelt <ew2789@gmail.com>
2013-10-30 17:04:08 +0200

    update referral to pass tests.
    Former-commit-id: 9dfb1cdb7d9794ab28119620abf25764c1a74b2c

--- bhp066/apps/bcpp_subject/forms/subject_referral_form.py
+++ bhp066/apps/bcpp_subject/forms/subject_referral_form.py
@@ -4,5 +4,10 @@ from .base_subject_model_form import BaseSubjectModelForm
 
 class SubjectReferralForm(BaseSubjectModelForm):
 
+#     def clean(self):
+#         cleaned_data = super(SubjectReferralForm, self).clean()
+#
+#         return cleaned_data
+
     class Meta:
         model = SubjectReferral
