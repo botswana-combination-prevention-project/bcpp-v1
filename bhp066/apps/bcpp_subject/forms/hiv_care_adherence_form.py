c808b71d5e0850ca28774897dfa3cbff53be2d68
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-10-31 08:33:25 +0200

    adherence form update
    Former-commit-id: f4eebd15b386764772e7ae17bf3eafefc5936e79

--- bhp066/apps/bcpp_subject/forms/hiv_care_adherence_form.py
+++ bhp066/apps/bcpp_subject/forms/hiv_care_adherence_form.py
@@ -27,7 +27,7 @@ class HivCareAdherenceForm (BaseSubjectModelForm):
         if cleaned_data.get('on_arv', None) == 'No' and cleaned_data.get('adherence_4_day'):
             raise forms.ValidationError('You do not have to indicate missed medication because you are not taking any ARV\'s')
         # if currently taking arv's, how well has participant been taking medication
-        if cleaned_data.get('on_arv', None) == 'Yes' and cleaned_data.get('adherence_4_wk'):
+        if cleaned_data.get('on_arv', None) == 'Yes' and not cleaned_data.get('adherence_4_wk'):
             raise forms.ValidationError('If participant is currently taking ARV\'s, how well has he/she been taking the medication this past week?')
         if cleaned_data.get('arv_stop', None) == 'Other' and not cleaned_data.get('arv_stop_other'):
             raise forms.ValidationError('If participant reason for stopping ARV\'s is \'OTHER\', specify reason why stopped taking ARV\'s?')
