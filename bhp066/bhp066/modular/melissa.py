4d08d63f5094adc2b7e3bf9b69bbfd91b2742978
Twice Tshwenyane <twicet@gmail.com>
2013-11-05 14:02:52 +0200

    Added LOCALE_PATHS to each individual settings file
    Former-commit-id: e310be4b7d9f3c8d0ac3eded5c117a1cc9d82674

--- bhp066/bhp066/modular/melissa.py
+++ bhp066/bhp066/modular/melissa.py
@@ -8,6 +8,8 @@ TEMPLATE_DEBUG = DEBUG
 
 KEY_PATH = join(SETTINGS_DIR, '..', '..', 'keys')
 
+LOCALE_PATHS = ('locale', )
+
 DATABASES = {
     'default': mysql_db(NAME='bhp066'),
     'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
