7ebd34fb850495c18098c954f2d6dda241fc0cc6
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-11-12 10:55:02 +0200

    update instructions, locale, questions
    Former-commit-id: de28606b3ac021fea8a2f49dae770a5dc7e553b8

---
+++ bhp066/apps/bcpp_subject/admin/hiv_result_documentation_admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from ..models import HivResultDocumentation
+from ..forms import HivResultDocumentationForm
+from .subject_visit_model_admin import SubjectVisitModelAdmin
+
+
+class HivResultDocumentationAdmin (SubjectVisitModelAdmin):
+
+    form = HivResultDocumentationForm
+    fields = (
+        'subject_visit',
+        'result_date',
+        'result_recorded',
+        'result_doc_type',)
+    radio_fields = {
+        "result_recorded": admin.VERTICAL,
+        'result_doc_type': admin.VERTICAL, }
+    instructions = [("This section collects information on whether or not the"
+                     " participant has either:"
+                     " a) documentation of an HIV test result other than the"
+                     " most recent HIV test (positive, negative, or "
+                     " indeterminate); OR "
+                     " b) documentation that supports a previous diagnosis of"
+                     " HIV, if record of positive HIV test is not available. "),]
+admin.site.register(HivResultDocumentation, HivResultDocumentationAdmin)
