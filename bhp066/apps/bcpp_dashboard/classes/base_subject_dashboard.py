51f678dd325e6d7c0f11d56733433e6744a217dc
erik van widenfelt <ew2789@gmail.com>
2013-11-12 11:08:34 +0200

    rework meta data in dashboard and rules and meta data.
    Former-commit-id: 2d3f953fd555d15b62113fe5177ef91e8ae732ab

--- bhp066/apps/bcpp_dashboard/classes/base_subject_dashboard.py
+++ bhp066/apps/bcpp_dashboard/classes/base_subject_dashboard.py
@@ -1,10 +1,7 @@
 import re
 
-from django.core.exceptions import ImproperlyConfigured
-
 from edc.dashboard.subject.classes import RegisteredSubjectDashboard
 from edc.subject.registration.models import RegisteredSubject
-from edc.subject.visit_schedule.models import MembershipForm
 from edc.subject.appointment.models import Appointment
 
 from apps.bcpp_household_member.models import HouseholdMember
@@ -13,21 +10,17 @@ from apps.bcpp_household_member.models import HouseholdMember
 class BaseSubjectDashboard(RegisteredSubjectDashboard):
 
     def __init__(self, *args, **kwargs):
+
         dashboard_models = kwargs.get('dashboard_models', {})
         dashboard_models.update({'household_member': HouseholdMember})
         kwargs.update({'dashboard_models': dashboard_models})
-        self.set_household_member(kwargs.get('dashboard_model'), kwargs.get('dashboard_id'))
-        self.set_survey()
+
+        self.household_member = (kwargs.get('dashboard_model'), kwargs.get('dashboard_id'))
+        kwargs.update({'membership_form_category': self.survey.survey_slug})
+
         super(BaseSubjectDashboard, self).__init__(*args, **kwargs)
-#         self._survey = None
-#         self._household_member = None
-#         self._household_members = None
-#         self._household = None
-#         self._household_structure = None
-        self.set_household_structure()
-        self.set_household()
-        self.set_household_members()
-        self.add_to_extra_url_context()
+
+        self.extra_url_context = '&household_member={0}'.format(self.household_member.pk)
 
     def add_to_context(self):
         super(BaseSubjectDashboard, self).add_to_context()
@@ -36,84 +29,68 @@ class BaseSubjectDashboard(RegisteredSubjectDashboard):
             search_name='subject',
             title='Subject Dashboard',
             household_dashboard_url='household_dashboard_url',
-            household_member=self.get_household_member(),
-            subject_consent=self.get_household_member().consent(),
-            household=self.get_household(),
-            survey=self.get_survey(),
-            household_members=self.get_household_members(),
-            household_structure=self.get_household_member().household_structure,
+            household_member=self.household_member,
+            subject_consent=self.consent,
+            household=self.household,
+            survey=self.survey,
+            household_members=self.household_members,
+            household_structure=self.household_member.household_structure,
             )
 
-    def add_to_extra_url_context(self):
-        self._extra_url_context += '&household_member={0}'.format(self.get_household_member().pk)
+    @property
+    def survey(self):
+        return self.household_member.survey
 
-    def set_membership_form_category(self):
-        self._membership_form_category = (self.get_survey().survey_slug)
-        if MembershipForm.objects.filter(category=self._membership_form_category).count() == 0:
-            raise ImproperlyConfigured('Cannot find a membership form for category \'{0}\'. '
-                                       'Please correct on visit.MembershipForm.'.format(self._membership_form_category))
+    @property
+    def household_member(self):
+        return self._household_member
 
-    def set_survey(self):
-        self._survey = self.get_household_member().survey
-
-    def get_survey(self):
-        return self._survey
-
-    def set_household_member(self, dashboard_model_name, dashboard_id):
+    @household_member.setter
+    def household_member(self, (dashboard_model_name, dashboard_id)):
         self._household_member = None
         re_pk = re.compile('[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}')
         if re_pk.match(dashboard_id or ''):
             if dashboard_model_name == 'household_member':
                 self._household_member = HouseholdMember.objects.get(pk=dashboard_id)
             elif dashboard_model_name == 'visit':
-                self._household_member = self.get_visit_model().objects.get(pk=dashboard_id).household_member
+                self._household_member = self.visit_model.objects.get(pk=dashboard_id).household_member
             elif dashboard_model_name == 'appointment':
-                #if self.get_visit_model_instance():
-                #    self._household_member = self.get_visit_model_instance().household_member
-                #elif self.get_appointment():
-                    # may have more than on, so take most recent
                 appointment = Appointment.objects.get(pk=dashboard_id)
                 # TODO: should I set appointment now?
                 self._household_member = HouseholdMember.objects.filter(registered_subject=appointment.registered_subject).order_by('-created')[0]
-#                 else:
-#                     pass
             elif dashboard_model_name == 'registered_subject':
                 # may have more than on, so take most recent
                 self._household_member = HouseholdMember.objects.filter(registered_subject=dashboard_id).order_by('-created')[0]
         if not self._household_member:
             raise TypeError('Attribute _household_member may not be None. Using dashboard_model={0}, dashboard_id={1}'.format(dashboard_model_name, dashboard_id))
 
-    def get_household_member(self):
-        return self._household_member
+    @property
+    def household(self):
+        return self.household_structure.household
 
-    def set_household(self):
-        self._household = self.get_household_structure().household
+    @property
+    def household_structure(self):
+        return self.household_member.household_structure
 
-    def get_household(self):
-        return self._household
+    @property
+    def registered_subject(self, pk=None):
+        return self._registered_subject
 
-    def set_household_structure(self):
-        self._household_structure = self.get_household_member().household_structure
-
-    def get_household_structure(self):
-        return self._household_structure
-
-    def set_registered_subject(self, pk=None):
+    @registered_subject.setter
+    def registered_subject(self, pk=None):
         """Sets the registered subject using a given pk or from household member."""
         re_pk = re.compile('[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}')
         self._registered_subject = None
         if re_pk.match(str(pk)):
             self._registered_subject = RegisteredSubject.objects.get(pk=pk)
-        elif RegisteredSubject.objects.filter(registration_identifier=self.get_household_member().internal_identifier).exists():
-            self._registered_subject = RegisteredSubject.objects.get(registration_identifier=self.get_household_member().internal_identifier)
-        elif self.get_appointment():
-            self._registered_subject = self.get_appointment().registered_subject
+        elif RegisteredSubject.objects.filter(registration_identifier=self.household_member.internal_identifier).exists():
+            self._registered_subject = RegisteredSubject.objects.get(registration_identifier=self.household_member.internal_identifier)
+        elif self.appointment:
+            self._registered_subject = self.appointment.registered_subject
         else:
-            raise ValueError('Expect all household_members to have an entry in RegisterSubject. Got None for member {0}.'.format(self.get_household_member()))
+            raise ValueError('Expect all household_members to have an entry in RegisterSubject. Got None for member {0}.'.format(self.household_member))
 
-    def set_household_members(self):
-        self._household_members = HouseholdMember.objects.filter(
-            household_structure=self.get_household_member().household_structure).order_by('household_structure', 'first_name')
-
-    def get_household_members(self):
-        return self._household_members
+    @property
+    def household_members(self):
+        return HouseholdMember.objects.filter(
+            household_structure=self.household_member.household_structure).order_by('household_structure', 'first_name')
--- bhp066/apps/bcpp_dashboard/classes/subject_dashboard.py
+++ bhp066/apps/bcpp_dashboard/classes/subject_dashboard.py
@@ -13,7 +13,10 @@ class SubjectDashboard(BaseSubjectDashboard):
     dashboard_name = 'Participant Dashboard'
 
     def __init__(self, *args, **kwargs):
+        self.household_dashboard_url = 'household_dashboard_url'
+        self.dashboard_type_list = ['subject']
         kwargs.update({'dashboard_models': {'subject_consent': SubjectConsent}})
+        self.visit_model = SubjectVisit
         super(SubjectDashboard, self).__init__(*args, **kwargs)
 
     def add_to_context(self):
@@ -21,44 +24,42 @@ class SubjectDashboard(BaseSubjectDashboard):
         self.context.add(
             home='bcpp',
             search_name='subject',
-            household_dashboard_url=self.get_household_dashboard_url(),
+            household_dashboard_url=self.household_dashboard_url,
             title='Research Subject Dashboard',
-            subject_consent=self.get_consent(),
-            subject_referral=self.get_subject_referral(),
+            subject_consent=self.consent,
+            subject_referral=self.subject_referral,
             rendered_household_members_sidebar=self.render_household_members_sidebar(),
             )
 
-    def get_household_dashboard_url(self):
-        return 'household_dashboard_url'
-
-    def set_dashboard_type_list(self):
-        self._dashboard_type_list = ['subject']
-
-    def set_consent(self, subject_identifier):
-        """Sets to the subject consent, if it has been completed."""
+    @property
+    def consent(self):
+        """Returns to the subject consent, if it has been completed."""
         self._consent = None
-        if SubjectConsent.objects.filter(subject_identifier=subject_identifier):
-            self._consent = SubjectConsent.objects.get(subject_identifier=subject_identifier)
+        if SubjectConsent.objects.filter(subject_identifier=self.subject_identifier):
+            self._consent = SubjectConsent.objects.get(subject_identifier=self.subject_identifier)
+        return self._consent
 
-    def get_subject_referral(self):
-        if SubjectReferral.objects.filter(subject_visit=self.get_visit_model_instance()):
-            return SubjectReferral.objects.get(subject_visit=self.get_visit_model_instance())
+    @property
+    def subject_referral(self):
+        if SubjectReferral.objects.filter(subject_visit=self.visit_model_instance):
+            return SubjectReferral.objects.get(subject_visit=self.visit_model_instance)
         return 'unknown referral'
 
-    def get_visit_model(self):
-        return SubjectVisit
-
-    def get_requisition_model(self):
+    @property
+    def requisition_model(self):
         return SubjectRequisition
 
-    def get_locator_model(self):
+    @property
+    def locator_model(self):
         return SubjectLocator
 
-    def get_locator_scheduled_visit_code(self):
+    @property
+    def locator_scheduled_visit_code(self):
         """ Returns visit where the locator is scheduled, TODO: maybe search visit definition for this?."""
         return '1000'
 
-    def get_packing_list_model(self):
+    @property
+    def packing_list_model(self):
         return PackingList
 
     def render_labs(self, update=False):
@@ -67,6 +68,6 @@ class SubjectDashboard(BaseSubjectDashboard):
     def render_household_members_sidebar(self):
         """Renders to string the household members sidebar."""
         return render_to_string('household_members_sidebar.html',
-            {'household_members': self.get_household_members(),
-             'household_dashboard_url': self.get_household_dashboard_url(),
-             'household_structure': self.get_household_structure()})
+            {'household_members': self.household_members,
+             'household_dashboard_url': self.household_dashboard_url,
+             'household_structure': self.household_structure})
