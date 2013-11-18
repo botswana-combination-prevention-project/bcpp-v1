fbcf6bbc13aa34070426dd29d8691653cfd529c0
ckgathi <ckgathi@bhp.org.bw>
2013-11-18 15:36:31 +0200

    replace CURRENT_COMMUNITY WITH CUURENT_MAPPER

--- bhp066/apps/bcpp_household/models/plot.py
+++ bhp066/apps/bcpp_household/models/plot.py
@@ -342,7 +342,7 @@ class Plot(BaseDispatchSyncUuidModel):
         """Sets the community number to use for the plot identifier."""
         community_number = None
         for commun in BCPP_VILLAGES:
-            if commun[1] == (settings.CURRENT_COMMUNITY).title():
+            if commun[1] == (settings.CURRENT_MAPPER).title():
                 community_number = commun[0]
                 return community_number
         return community_number
