42740e9b4d4f66f38f15baf1c1a039624167c36a
ckgathi <ckgathi@bhp.org.bw>
2013-11-18 15:37:12 +0200

    replace CURRENT_COMMUNITY WITH CUURENT_MAPPER
    Former-commit-id: d865b811db0ddf4281a730f2fe2344b98ffbfa9c

--- misc/twice_settings/common.py
+++ misc/twice_settings/common.py
@@ -187,7 +187,7 @@ else:
                                    'DATABASE=BHPLAB')
 VAR_ROOT = '/var'
 LOGGING = logger.LOGGING
-CURRENT_COMMUNITY = 'mochudi'
+CURRENT_MAPPER = 'mochudi'
 CURRENT_SURVEY = 'baseline'
 
 SUBJECT_IDENTIFIER_UNIQUE_ON_CONSENT = False  # set to False so that the constraint can be expanded to subject_identifier + survey
