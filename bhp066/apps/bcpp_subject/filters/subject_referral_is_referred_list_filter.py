13b4032df0f5d30f5d2cc73903daa885397c06f7
erik van widenfelt <ew2789@gmail.com>
2013-11-19 01:15:51 +0200

    add referral filter

---
+++ bhp066/apps/bcpp_subject/filters/subject_referral_is_referred_list_filter.py
@@ -0,0 +1,18 @@
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
+
+
+class SubjectReferralIsReferredListFilter(SimpleListFilter):
+
+    title = _('referred')
+
+    parameter_name = 'referred'
+
+    def lookups(self, request, model_admin):
+        return tuple(True, False)
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.exclude(referral_code='NOT-REF', referral_code='ERROR')
+        if self.value() == False:
+            return queryset.filter(referral_code='NOT-REF', referral_code='ERROR')
