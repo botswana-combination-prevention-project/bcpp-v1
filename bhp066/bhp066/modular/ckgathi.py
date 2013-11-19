10bae256c2f42ea25ba3b95ca899a3b2607a8da6
ckgathi <ckgathi@bhp.org.bw>
2013-11-19 16:49:31 +0200

    add netbook to dispatch
    Former-commit-id: b681e7cacc7a623d990ff430f63fe95d18ee1537

--- bhp066/bhp066/modular/ckgathi.py
+++ bhp066/bhp066/modular/ckgathi.py
@@ -14,5 +14,7 @@ LOCALE_PATHS = ('locale', )
 
 DATABASES = {
     'default': mysql_db(NAME='bhp066'),
-    'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50')
+    'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
+    'bcpp038-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.174'),
+    'bcpp035-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.216'),
 }
