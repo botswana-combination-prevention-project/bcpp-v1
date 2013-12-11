abd9f8bacdad685f29e9facf609ad79074249f1b
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2013-12-11 11:08:44 +0200

    refactored bcpp_signal_manager and added absentee_visit_attempts
    Former-commit-id: b6a87487874b18138fa3e78ac9d2f83103cc0775

--- bhp066/apps/bcpp_household_member/admin/household_member_admin.py
+++ bhp066/apps/bcpp_household_member/admin/household_member_admin.py
@@ -6,6 +6,42 @@ from ..models import HouseholdMember
 from ..forms import HouseholdMemberForm
 
 
+class AbsenteeVisitAtempts(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Current Absentee visit attempts'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'visit_attempts'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        values = ['0', '1', '2', '3']
+        visit_attempts_tuples = []
+        visit_attempts_tuples.append((values[0], values[0]))
+        visit_attempts_tuples.append((values[1], values[1]))
+        visit_attempts_tuples.append((values[2], values[2]))
+        visit_attempts_tuples.append((values[3], values[3]))
+        return visit_attempts_tuples
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        print 'VALUE='+self.value()
+        if self.value():
+            return queryset.filter(member_status='ABSENT', absentee_visit_attempts=self.value())
+        else:
+            return queryset
+
 class HouseholdMemberInline(BaseTabularInline):
     model = HouseholdMember
     extra = 3
@@ -44,6 +80,7 @@ class HouseholdMemberAdmin(BaseModelAdmin):
                     'eligible_subject',
                     'is_consented',
                     'member_status',
+                    'absentee_visit_attempts',
                     'created',
                     'hostname_created')
 
@@ -58,6 +95,8 @@ class HouseholdMemberAdmin(BaseModelAdmin):
 
     list_filter = ('household_structure__survey__survey_name', 'present_today', 'study_resident', 'member_status',
                    'eligible_member', 'eligible_subject', 'target', 'hiv_history', 'household_structure__household__community',
-                    'modified', 'hostname_created')
+                    'modified', 'hostname_created', AbsenteeVisitAtempts)
+
+    #readonly_fields = ('absentee_visit_attempts', )
     list_per_page = 15
 admin.site.register(HouseholdMember, HouseholdMemberAdmin)
