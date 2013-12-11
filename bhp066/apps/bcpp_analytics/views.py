55a4aff156fdc7a442cc11f8e4bc5c9b19433eb4
Twice Tshwenyane <twicet@gmail.com>
2013-12-11 12:25:40 +0200

    finished first page of accrual reports

--- bhp066/apps/bcpp_analytics/views.py
+++ bhp066/apps/bcpp_analytics/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Sum
+from django.db.models import Q
 
 import datetime
 
@@ -51,9 +52,20 @@ class HouseholdMemberQuery(object):
         self.study_eligible = self.study_eligible_qs()
         self.unreached_after_2_visits = self.unreached_after_visits_qs(2).count()
         self.hiv_positives = self.hiv_positive_qs().count()
+        self.new_infections = self.new_infections_qs().count()
+        self.already_infected = self.already_infected_qs().count()
+        self.self_reported_infected = self.self_reported_positive_qs().count()
 
     def refused_qs(self):
         return self.community_members.filter(member_status='REFUSED')
+
+    def absentee_stratified(self):
+        absentee_strat = {}
+        absentee_strat['absentees'] = self.absentee_qs().count()
+        absentee_strat['visit_1'] = self.unreached_after_visits_qs(1).count()
+        absentee_strat['visit_2'] = self.unreached_after_visits_qs(2).count()
+        absentee_strat['visit_3'] = self.unreached_after_visits_qs(3).count()
+        return absentee_strat
 
     def first_time_testers_qs(self):
         return self.community_members.filter(subjectvisit__hivtestinghistory__has_tested='No')
@@ -71,6 +83,9 @@ class HouseholdMemberQuery(object):
     def reached_stats(self):
         return self._residents_demographics('REFUSED', 'UNDECIDED', 'RESEARCH')
 
+    def absentee_qs(self):
+        return self.community_members.filter(member_status='ABSENT')
+
     def enrolled_stats(self):
         return self._residents_demographics('RESEARCH')
 
@@ -80,7 +95,7 @@ class HouseholdMemberQuery(object):
         demographics['count'] = demo_query.count()
         demographics['males'] = demo_query.filter(gender='M').count()
         demographics['females'] = demo_query.filter(gender='F').count()
-        age_ranges = [(16, 19), (20, 29), (30, 39), (40, 49), (50, 59), (60, 79)]
+        age_ranges = [(16, 19), (20, 29), (30, 39), (40, 49), (50, 79)]
         groups = {}
         for(a, b) in age_ranges:
             groups["{0}-{1}".format(a, b)] = demo_query.filter(age_in_years__range=(a, b)).count()
@@ -88,11 +103,42 @@ class HouseholdMemberQuery(object):
         return demographics
 
     def unreached_after_visits_qs(self, no_of_visits):
-        absentee_query = self.community_members.annotate(absentee_count=Count('subjectabsentee__subjectabsenteeentry'))
-        return absentee_query.filter(absentee_count__gte=no_of_visits)
+        absentee_count_query = self.absentee_qs().annotate(absentee_count=Count('subjectabsentee__subjectabsenteeentry'))
+        return absentee_count_query.filter(absentee_count=no_of_visits)
+
+    def self_reported_stats(self):
+        stat = dict(count=self.self_reported_infected)
+        stat['on_arv'] = self.on_ART(self.self_reported_positive_qs()).count()
+        stat['not_on_arv'] = self.not_on_ART(self.self_reported_positive_qs()).count()
+        return stat
+
+    def documented_positive_stats(self):
+        stat = dict(count=self.already_infected)
+        stat['on_arv'] = self.on_ART(self.already_infected_qs()).count()
+        stat['not_on_arv'] = self.not_on_ART(self.already_infected_qs()).count()
+        return stat
 
     def hiv_positive_qs(self):
         return self.community_members.filter(subjectvisit__hivresult__hiv_result='POS')
+
+    def new_infections_qs(self):
+        untested_Q = Q(subjectvisit__hivtestinghistory__has_tested='No')
+        tested_Q = Q(subjectvisit__hivtestinghistory__verbal_hiv_result__in=['NEG', 'UNK', 'IND'])
+        return self.hiv_positive_qs().filter(untested_Q | tested_Q)
+
+    def already_infected_qs(self):
+        review_Q = Q(subjectvisit__hivtestreview__recorded_hiv_result='POS')
+        document_Q = Q(subjectvisit__hivresultdocumentation__result_recorded='POS')
+        return self.hiv_positive_qs().filter(review_Q | document_Q)
+
+    def self_reported_positive_qs(self):
+        return self.hiv_positive_qs().filter(subjectvisit__hivtestinghistory__verbal_hiv_result='POS')
+
+    def on_ART(self, hiv_positive_qs):
+        return hiv_positive_qs.filter(subjectvisit__hivcareadherence__on_arv='Yes')
+
+    def not_on_ART(self, hiv_positive_qs):
+        return hiv_positive_qs.filter(subjectvisit__hivcareadherence__on_arv='No')
 
 
 class PlotReportQuery(object):
