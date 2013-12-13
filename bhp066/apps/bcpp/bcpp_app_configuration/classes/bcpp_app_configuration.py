d9b01072b9c413a7087457d518c0c3e821616b5f
erik van widenfelt <ew2789@gmail.com>
2013-12-13 08:15:28 +0200

    add update_survey
    Former-commit-id: e221f8dcf5aa2fed0e97b84d08e431fc00f2f948

--- bhp066/apps/bcpp/bcpp_app_configuration/classes/bcpp_app_configuration.py
+++ bhp066/apps/bcpp/bcpp_app_configuration/classes/bcpp_app_configuration.py
@@ -1,11 +1,19 @@
 from datetime import datetime
-from django.db.models import get_model
 
 from edc.apps.app_configuration.classes import BaseAppConfiguration
-# from edc.core.bhp_content_type_map.models import ContentTypeMap
+
+from apps.bcpp_survey.models import Survey
+
+
+study_start_datetime = datetime(2013, 10, 29, 10, 30, 00)
+study_end_datetime = datetime(2013, 10, 29, 10, 30, 00)
 
 
 class BcppAppConfiguration(BaseAppConfiguration):
+
+    def __init__(self):
+        super(BcppAppConfiguration, self).__init__()
+        self.update_or_create_survey()
 
     appointment_configuration = {
                 'allowed_iso_weekdays': '1234567',
@@ -17,7 +25,7 @@ class BcppAppConfiguration(BaseAppConfiguration):
                 'protocol_code': '066',
                 'protocol_title': 'BCPP',
                 'research_title': 'Botswana Combination Prevention Project',
-                'study_start_datetime': datetime(2013, 10, 29, 10, 30, 00),
+                'study_start_datetime': study_start_datetime,
                 'minimum_age_of_consent': 16,
                 'maximum_age_of_consent': 64,
                 'gender_of_consent': 'MF',
@@ -35,12 +43,22 @@ class BcppAppConfiguration(BaseAppConfiguration):
 #                 'content_type_map': '',
                 'consent_type': 'study',
                 'version': 1,
-                'start_datetime': datetime(2013, 10, 29, 10, 30, 00),
+                'start_datetime': study_start_datetime,
                 'end_datetime': datetime(2014, 10, 29, 16, 30, 00),
                 'add_for_app': 'bcpp_subject'}
 
-    set_survey = {
+    survey_setup = {
                 'survey_name': 'BCPP Year 1',
                 'survey_slug': 'bcpp-year-1',
-                'datetime_start': datetime(2013, 10, 29, 10, 30, 00),
+                'datetime_start': study_start_datetime,
                 'datetime_end': datetime(2014, 10, 29, 16, 30, 00)}
+
+    def update_or_create_survey(self):
+        if Survey.objects.all().count() == 0:
+            Survey.objects.create(**self.survey_setup)
+        else:
+            try:
+                # mat fail on tests if surveys created there
+                Survey.objects.all().update(**self.survey_setup)
+            except:
+                pass
