d887482f60910e0ad09d2d793f373c6cb52b4dc9
Twice Tshwenyane <twicet@gmail.com>
2013-12-16 11:52:57 +0200

    moved the report queries objects into their own modules
    Former-commit-id: 29f0f329ced399d06e2031a8be3c52ccfa8948c1

--- bhp066/apps/bcpp_analytics/urls.py
+++ bhp066/apps/bcpp_analytics/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-from .views import operational_report_view
+#from .views import operational_report_view
 from .views import index, accrual
 
-urlpatterns = patterns('',
-    url(r'^operational/', operational_report_view,),
-    )
+#urlpatterns = patterns('',
+#    url(r'^operational/', operational_report_view,),
+#    )
 
 urlpatterns = patterns('',
     url(r'^reports/$', index),
