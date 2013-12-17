59d55101d1511a3a90085d468fc59e39cb5390af
Twice Tshwenyane <twicet@gmail.com>
2013-12-17 09:48:42 +0200

    made operational report to be accessible from analytic app
    Former-commit-id: a247b9185e9099564aed9a2cada526ead4b2db0c

--- bhp066/apps/bcpp_analytics/urls.py
+++ bhp066/apps/bcpp_analytics/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, url
-#from .views import operational_report_view
 from .views import index, accrual
+from .views import operational_report_view
 
-#urlpatterns = patterns('',
-#    url(r'^operational/', operational_report_view,),
-#    )
 
 urlpatterns = patterns('',
-    url(r'^reports/$', index),
-    url(r'^report/community_accrual/$', accrual, name="accrual"),
-)
+                       url(r'^reports/$', index),
+                       url(r'^report/community_accrual/$', accrual, name="accrual"),
+                       url(r'^report/operational/', operational_report_view, name="operational"),
+                       )
