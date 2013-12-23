de715400699b4e74add9fc8840104530c03cc48a
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-12-23 15:54:14 +0200

    medical diagnoses, sti, heartattack forms
    Former-commit-id: 80f7b93b94580dd26e2687ec7b168e5693b5f996

--- bhp066/apps/bcpp_subject/forms/medical_diagnoses_form.py
+++ bhp066/apps/bcpp_subject/forms/medical_diagnoses_form.py
@@ -12,12 +12,19 @@ class MedicalDiagnosesForm (BaseSubjectModelForm):
         #cancer
         if cleaned_data.get('diagnoses')[0].name == 'Cancer' and not cleaned_data.get('cancer_record'):
             raise forms.ValidationError('If participant has ever been diagnosed with cancer, is there a record available?')
-        #sti
-        if cleaned_data.get('diagnoses')[0].name == 'STI (Sexually Transmitted Infection)' and not cleaned_data.get('sti_record'):
-            raise forms.ValidationError('If participant has ever been diagnosed with an STI, is there a record available?')
+        #sti - this has changed. no longer triggers the sti form - leaving it incase we revert
+#         if cleaned_data.get('diagnoses')[0].name == 'STI (Sexually Transmitted Infection)' and not cleaned_data.get('sti_record'):
+#             raise forms.ValidationError('If participant has ever been diagnosed with an STI, is there a record available?')
         #TB
         if cleaned_data.get('diagnoses')[0].name == 'Tubercolosis' and not cleaned_data.get('tb_record'):
             raise forms.ValidationError('If participant has ever been diagnosed with TB, is there a record available?')
+        #validating inavailablity of records
+        if cleaned_data.get('heart_attack_record') == 'No' and not cleaned_data.get('heart_attack_comment'):
+            raise forms.ValidationError('if \'NO heart attack record\', please provide the VERBAL response of the diagnosis and date of diagnosis from the participant.')
+        if cleaned_data.get('cancer_record') == 'No' and not cleaned_data.get('cancer_record_comment'):
+            raise forms.ValidationError('if \'NO cancer record\', please provide the VERBAL response of the diagnosis and date of diagnosis from the participant.')
+        if cleaned_data.get('tb_record') == 'No' and not cleaned_data.get('tb_record_comment'):
+            raise forms.ValidationError('if \'NO TB record\', please provide the VERBAL response of the diagnosis and date of diagnosis from the participant.')
 
         return cleaned_data
 
