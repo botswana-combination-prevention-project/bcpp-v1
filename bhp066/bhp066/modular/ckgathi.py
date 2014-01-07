12e8d5ac7f5a3b5130a0162b0960fd78006a95ed
ckgathi <ckgathi@bhp.org.bw>
2014-01-07 08:52:04 +0200

    coulson settings file
    Former-commit-id: 08137621b821458eca352ab839824380ee8442e4

--- bhp066/bhp066/modular/ckgathi.py
+++ bhp066/bhp066/modular/ckgathi.py
@@ -15,6 +15,6 @@ LOCALE_PATHS = ('locale', )
 DATABASES = {
     'default': mysql_db(NAME='bhp066'),
     'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
-    'bcpp012-bhp066': mysql_db(NAME='bhp066', HOST='10.42.43.12'),
+    'bcpp012-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.26'),
 #     'bcpp035-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.216'),
 }
