7404ad82fe9c8b5f22b215c74d9e724f86607bb9
ckgathi <ckgathi@bhp.org.bw>
2014-01-10 08:31:01 +0200

    prepare base
    Former-commit-id: 836d14cde3ce3a4fa4639a46588f9d5d7f53d0e3

--- bhp066/bhp066/modular/ckgathi.py
+++ bhp066/bhp066/modular/ckgathi.py
@@ -13,8 +13,8 @@ KEY_PATH = join(SETTINGS_DIR, '..', '..', 'keys')
 LOCALE_PATHS = ('locale', )
 
 DATABASES = {
-    'default': mysql_db(NAME='bhp066'),
+    'default': mysql_db(NAME='bhp066_molapowabojang'),
     'lab_api': mysql_db(NAME='lab', HOST='192.168.1.50'),
-    'bcpp012-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.26'),
+#     'bcpp012-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.26'),
 #     'bcpp035-bhp066': mysql_db(NAME='bhp066', HOST='192.168.1.216'),
 }
