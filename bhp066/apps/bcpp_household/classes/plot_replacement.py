cd7179254340a29322e9573b742f3bf92f75ada8
SirOne Pharatlhatlhe <sirone@sirone.lan>
2014-01-17 13:58:41 +0200

    simplified code in plot replacement

--- bhp066/apps/bcpp_household/classes/plot_replacement.py
+++ bhp066/apps/bcpp_household/classes/plot_replacement.py
@@ -1,109 +1,49 @@
-from ...bcpp_household_member.models import HouseholdInfo, HouseholdMember
+from ...bcpp_household_member.models import HouseholdMember
 from ..models import Household, HouseholdStructure
 from apps.bcpp_household_member.models import SubjectAbsentee, SubjectAbsenteeEntry
- 
- 
- 
+
+
 class PlotReplacement(object):
- 
+
     def __init__(self, *args, **kwargs):
         self._members = None
         self._h_structure = None
         self._replacement_plot = None
- 
+
     def replace_refusal_plot(self, plot):
         """Check if a plot has refusal that would make it be replaced."""
+        replaced = None
         if plot.household_count == 1:
             household = Household.objects.get(plot=plot)
-            if HouseholdStructure.objects.get(household=household):
-                h_structure = HouseholdStructure.objects.get(household=household)
-                if HouseholdMember.objects.filter(household_structure=h_structure):
-                    members = HouseholdMember.objects.filter(household_structure=h_structure)
-                #All eligible members refused
-                members_status_list = []
-                if members:
-                    for member in members:
-                        if member.eligible_member:
-                            members_status_list.append(member.member_status)
-                if members_status_list:
-                    if all(map(lambda x: x == members_status_list[0], members_status_list)):
-                        if members_status_list[0] == "REFUSED":
-                            replacement_plot = plot
-                            return replacement_plot
+            return self.evaluate_refusals(plot, household)
         if plot.household_count >= 2:
             households = Household.objects.filter(plot=plot)
             for household in households:
-                if HouseholdStructure.objects.get(household=household):
-                    h_structure = HouseholdStructure.objects.get(household=household)
-                    if HouseholdMember.objects.filter(household_structure=h_structure):
-                        members = HouseholdMember.objects.filter(household_structure=h_structure)
-                #At least one household refused
-                members_status_list = []
-                if members:
-                    for member in members:
-                        if member.eligible_member:
-                            members_status_list.append(member.member_status)
-                if members_status_list:
-                    if all(map(lambda x: x == members_status_list[0], members_status_list)):
-                        if members_status_list[0] == "REFUSED":
-                            replacement_plot = plot
-                            return replacement_plot
-        return replacement_plot
- 
+                #Does this current household qualify the plot to be replaced?
+                replaced = self.evaluate_refusals(plot, household)
+                if replaced:
+                    #If a single household qualifies a plot to be replaced, then replace the whole plot
+                    return replaced
+        #We will return None if the plot passed does not qualify to be replaced
+        return None
+
     def replacement_absentee(self, plot):
         """Check if a plot has absentees that would make it be replaced."""
-        if plot.household_count == 1:
+        replaced = None
+        if plot.household_count == 1:#We will replace if all eligible members are absent 3 times
             household = Household.objects.get(plot=plot)
-            if HouseholdStructure.objects.get(household=household):
-                h_structure = HouseholdStructure.objects.get(household=household)
-                if HouseholdMember.objects.filter(household_structure=h_structure):
-                    members = HouseholdMember.objects.filter(household_structure=h_structure)
-            #All eligible absent after 3 individual visits each
-            members_status_list = []
-            absentee_entries = []
-            if members:
-                for member in members:
-                    if member.eligible_member:
-                        members_status_list.append(member.member_status)
-                        if members_status_list:
-                            if all(map(lambda x: x == members_status_list[0], members_status_list)):
-                                if members_status_list[0] == "ABSENT":
-                                    sub_absentee = SubjectAbsentee.objects.get(household_member=member)
-                                    num_absentee_entries = SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee)
-                                    absentee_entries.append(SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee).count())
-            if all(map(lambda x: x == member_absentee_entries_list[0], absentee_entries)):
-                if absentee_entries:
-                    if absentee_entries[0] == 3:
-                        replacement_plot = plot
-                        return replacement_plot
-        if plot.household_count >= 2:
+            return self.evaluate_absentees(plot, household)
+        if plot.household_count >= 2:#We will replace if all eligible members in each household are absent 3 times
             households = Household.objects.filter(plot=plot)
             for household in households:
-                if HouseholdStructure.objects.get(household=household):
-                    h_structure = HouseholdStructure.objects.get(household=household)
-                    if HouseholdMember.objects.filter(household_structure=h_structure):
-                        members = HouseholdMember.objects.filter(household_structure=h_structure)
-                #All eligible absent after 3 individual visits each
-                members_status_list = []
-                member_absentee_entries_list = []
-                if members:
-                    for member in members:
-                        if member.eligible_member:
-                            members_status_list.append(member.member_status)
-                            if members_status_list:
-                                if all(map(lambda x: x == members_status_list[0], members_status_list)):
-                                    if members_status_list[0] == "ABSENT":
-                                        sub_absentee = SubjectAbsentee.objects.get(household_member=member)
-                                        num_absentee_entries = SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee)
-                                        member_absentee_entries_list.append(SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee).count())
-                if all(map(lambda x: x == member_absentee_entries_list[0], member_absentee_entries_list)):
-                    if member_absentee_entries_list:
-                        if member_absentee_entries_list[0]:
-                            if member_absentee_entries_list[0] == 3:
-                                replacement_plot = plot
-                                return replacement_plot
-        return replacement_plot
- 
+                #Does this current household qualify the plot to be replaced?
+                replaced = self.evaluate_absentees(plot, household)
+                if replaced:
+                    #If a single household qualifies a plot to be replaced, then replace the whole plot
+                    return replaced
+        #We will return None if the plot passed does not qualify to be replaced
+        return None
+
     def replacement_none_consented(self, plot):
         """Check if a plot has no consents to make it be replaced."""
         #A plot with more than one household.
@@ -124,4 +64,47 @@ class PlotReplacement(object):
                     if consented_check_list[0] == False:
                         replacement_plot = plot
                         return replacement_plot
+        return replacement_plot
+
+    def evaluate_refusals(self, plot, household):
+        replacement_plot = None
+        dont_replace = False#Assume a plot should be replaced untill we find a reason not to replace it
+        if HouseholdStructure.objects.filter(household=household).exists():
+                h_structure = HouseholdStructure.objects.get(household=household)
+                if HouseholdMember.objects.filter(household_structure=h_structure):
+                    members = HouseholdMember.objects.filter(household_structure=h_structure)
+                #All eligible members refused
+                members_status_list = []
+                if members:
+                    for member in members:
+                        if member.eligible_member:
+                            #accumulate the statuses of all eligible members
+                            members_status_list.append(member.member_status)
+                if members_status_list:
+                    for status in members_status_list:
+                        if status != 'REFUSED':
+                            #All statuses must be 'REFUSED' for this to qualify for replacement, otherwise we dont replace.
+                            dont_replace = True
+                            break
+                if not dont_replace:
+                    #If any member had a status that is not 'REFUSE' then this plot does not qualify for replacement
+                    replacement_plot = plot
+                return replacement_plot
+
+    def evaluate_absentees(self, plot, household):
+        replacement_plot = None
+        dont_replace = False#Assume a plot should be replaced untill we find a reason not to replace it
+        if HouseholdStructure.objects.filter(household=household).exists():
+            h_structure = HouseholdStructure.objects.get(household=household)
+            if HouseholdMember.objects.filter(household_structure=h_structure):
+                members = HouseholdMember.objects.filter(household_structure=h_structure)
+        if members:
+            for member in members:
+                if member.eligible_member and member.member_status == 'ABSENT':
+                    sub_absentee = SubjectAbsentee.objects.get(household_member=member)
+                    num_absentee_entries = SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee).count()
+                    if num_absentee_entries <= '3':#Then we have found a reason not to replace this plot
+                        dont_replace = True
+        if not dont_replace:
+            replacement_plot = plot
         return replacement_plot
