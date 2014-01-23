45dba47e2e1da1330a4089ef2da23871cc2917fc
melissa kewagamang <mkewagamang@bhp.org.bw>
2014-01-23 14:05:25 +0200

    validations + url_context + migration + update dashboardtype admin
    Former-commit-id: 5138e6023e7964ddfcfda8ce5bde077f53868acc

--- bhp066/apps/bcpp_clinic/forms/clinic_visit_form.py
+++ bhp066/apps/bcpp_clinic/forms/clinic_visit_form.py
@@ -1,8 +1,19 @@
+from django import forms
+from django.contrib.admin.widgets import AdminRadioSelect, AdminRadioFieldRenderer
+
 from edc.subject.consent.forms import BaseConsentedModelForm
+
 from ..models import ClinicVisit
+from ..choices import VISIT_REASON
 
 
 class ClinicVisitForm (BaseConsentedModelForm):
+
+    reason = forms.ChoiceField(
+        label='Reason for clinic visit',
+        choices=[choice for choice in VISIT_REASON],
+        help_text="",
+        widget=AdminRadioSelect(renderer=AdminRadioFieldRenderer))
 
     class Meta:
         model = ClinicVisit
