4851dd2320bc745b72f32ea42e5b80160083f25b
Twice Tshwenyane <twicet@gmail.com>
2014-01-24 14:32:32 +0200

    implemented the key_indicators report
    Former-commit-id: 491e599e9768b591e37a517fb5b9f6e95133607d

--- bhp066/apps/bcpp_analytics/urls.py
+++ bhp066/apps/bcpp_analytics/urls.py
@@ -1,13 +1,14 @@
 from datetime import date
 from django.conf.urls import patterns, url
-from .views import index, accrual, accrual_pdf
+from .views import index, accrual, accrual_pdf, key_indicators
 from .views import operational_report_view
 
 
 urlpatterns = patterns('',
-                       url(r'^reports/$', index),
+                       url(r'^reports/$', index, name="index"),
                        url(r'^report/community_accrual/$', accrual, name="accrual"),
                        url(r'^report/community_accrual/pdf/(?P<com1>\w+)/(?P<com2>\w+)/(?P<start>[a-zA-Z0-9_., ]+)/(?P<to>[a-zA-Z0-9_., ]+)/$',
                            accrual_pdf, name="accrual_pdf"),
                        url(r'^report/operational/$', operational_report_view, name="operational"),
+                       url(r'^report/key_indicators/$', key_indicators, name="indicators"),
                        )
