3d0d3c3f5d1dce4fbc5f6ea5050d720cdfe05bf8
ckgathi <ckgathi@bhp.org.bw>
2014-01-27 15:45:08 +0200

    replacement fix

--- bhp066/apps/bcpp_household/urls.py
+++ bhp066/apps/bcpp_household/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
+from .views import replacement_data, return_data
 
 urlpatterns = patterns('',
     url(r'^return/(?P<household>[A-Z0-9\-0-9]+)/', 'return_households'),
+    url(r'^replacement_data/', replacement_data, name='replacement_data_url'),
+    url(r'^return_data/', return_data, name='return_data_url')
     )
