39eed8cd11be197b53621829ed34f97d6f9ccba6
ckgathi <ckgathi@bhp.org.bw>
2014-01-29 08:29:16 +0200

    replacements fix
    Former-commit-id: fc2a199d1ca80270c1ff11b4d09b67588c946fe5

--- bhp066/apps/bcpp_household/views/return_data.py
+++ bhp066/apps/bcpp_household/views/return_data.py
@@ -1,5 +1,13 @@
+import itertools
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import redirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+from django.http import HttpResponseRedirect
+
 from django.db.models import Q
 
 from ..models import Plot, Household
@@ -14,15 +22,19 @@ def return_data(request):
 
     if request.GET.get('replacement_data'):
         replacement_data = request.GET.get('replacement_data')
+        print replacement_data
+        
         #return households
         #TODO: Call return method to return households
         #get replacement plots
         replacement_plots = Plot.objects.filter(selected=2)
         replacement_count = 0
+        content_type = None
         while replacement_count < len(replacement_data):
             for plot, household in replacement_data:
                 if replacement_plots[replacement_count]:
                     replacement_data_list.append(replacement_plots[replacement_count].plot_identifier)
+                    print replacement_data_list
                     plot.replacement = True
                     plot.save
                     household.replacement = True
@@ -35,10 +47,10 @@ def return_data(request):
                     replacement_count += 1
                 else:
                     raise ReplacementError("There are no more Plots available to replace with.")
-                pks = replacement_data_list.values_list('pk')
-                selected = list(itertools.chain(*pks))
-                content_type = ContentType.objects.get_for_model('Plot')
-                return HttpResponseRedirect("/dispatch/bcpp/?ct={0}&items={1}".format(content_type.pk, ",".join(selected)))
+    selected = replacement_data_list
+    content_type = ContentType.objects.get_for_model(Plot)
+    return HttpResponseRedirect("/dispatch/bcpp/?ct={0}&items={1}".format(content_type.pk, ",".join(selected)))
+
 
 #     elif request.GET.get('plot') and request.GET.get('household'):
 #         plot = request.GET.get('plot')
