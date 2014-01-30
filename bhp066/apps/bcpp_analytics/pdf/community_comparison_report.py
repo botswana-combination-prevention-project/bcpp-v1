b1c618d2722364797471910b46d58d19ac0987de
Twice Tshwenyane <twicet@gmail.com>
2014-01-30 17:46:41 +0200

    refactored and redesigned the look of community accrual pdf report
    Former-commit-id: 0505f0862b5efd226ca29f406f63250023174592

---
+++ bhp066/apps/bcpp_analytics/pdf/community_comparison_report.py
@@ -0,0 +1,136 @@
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import mm
+
+
+class CommunityComparisonPDF(object):
+
+    def __init__(self, pdf_data, title):
+        self.community1_data = pdf_data['community1_data']
+        self.community2_data = pdf_data['community2_data']
+        self.community1_name = self.community1_data[0].community
+        self.community2_name = self.community2_data[0].community
+        self.communities = "%s - %s" % (self.community1_name, self.community2_name)
+        self.report_period = "%s - %s" % (pdf_data["start_date"], pdf_data["end_date"])
+        self.report_title = title
+        self.style = getSampleStyleSheet()
+        self.p_style = self._paragraph_styles()
+
+    def draw(self, output, story):
+        from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate
+
+        PAGE_SIZE = A4
+        MARGIN_SIZE = 15 * mm
+        doc = BaseDocTemplate(output, pagesize=PAGE_SIZE, leftMargin=MARGIN_SIZE, rightMargin=MARGIN_SIZE,
+                              topMargin=MARGIN_SIZE, bottomMargin=MARGIN_SIZE)
+        main_frame = Frame(MARGIN_SIZE, MARGIN_SIZE, PAGE_SIZE[0] - 2 * MARGIN_SIZE, PAGE_SIZE[1] - 2 * MARGIN_SIZE,
+                           leftPadding=0, rightPadding=0, bottomPadding=0, topPadding=0, id='main_frame')
+        main_template = PageTemplate(id='main_template', frames=[main_frame])
+        doc.addPageTemplates([main_template])
+        doc.build(story)
+
+    def build(self, output):
+        from .lib.spreadsheet_table import SpreadsheetTable
+        from reportlab.platypus.flowables import Spacer
+        from reportlab.platypus import Paragraph
+        from itertools import izip
+
+        story = []
+        story.append(Paragraph("Community Accrual Report", self.p_style['ReportName']))
+        story.append(Paragraph(self.communities, self.p_style['CommunityName']))
+        story.append(Paragraph(self.report_period, self.p_style['report_period']))
+        story.append(Spacer(0, 3 * mm))
+        report_data = []
+        report_table_headers = [Paragraph("Accrual Report Variables", self.p_style['report_vars'])]
+        for header in [self.community1_name, self.community2_name]:
+            report_table_headers.append(Paragraph(header, self.p_style['community_column']))
+        report_data.append(report_table_headers)
+
+        row_no = 0
+        report_contents = []
+        extra_styles = []
+        empty_column = " "
+        for sect1, sect2 in izip(self.community1_data, self.community2_data):
+            report_contents.append([sect1.display_title(), empty_column, empty_column])
+            row_no += 1
+            extra_styles.append(('BACKGROUND', (0, row_no), (2, row_no), colors.HexColor('#ffeb94')))
+            extra_styles.append(('LINEBELOW', (0, row_no), (2, row_no), 1, colors.HexColor('#1f8dd6')))
+            extra_styles.append(('FONT', (0, row_no), (1, row_no),  'Helvetica-Bold'))
+            for comm1_row, comm2_row in izip(sect1.data, sect2.data):
+                row_no += 1
+                if row_no % 2 != 0:
+                    extra_styles.append(('BACKGROUND', (0, row_no), (-1, row_no), colors.HexColor('#e1f2fa')))
+                value1 = Paragraph(comm1_row.display_value(), self.p_style['value'])
+                value2 = Paragraph(comm2_row.display_value(), self.p_style['value'])
+                row_data = [comm1_row.label, value1, value2]
+                report_contents.append(row_data)
+        report_data.extend(report_contents)
+        report_table = SpreadsheetTable(report_data, repeatRows=1)
+        table_style = [
+            ('LINEABOVE', (0, 0), (-1, 0), 0.25, colors.orange),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ]
+        table_style.extend(extra_styles)
+        report_table.setStyle(table_style)
+        story.append(report_table)
+        self.draw(output, story)
+
+    def _paragraph_styles(self):
+        from reportlab.lib.styles import ParagraphStyle, StyleSheet1
+        from reportlab.lib.enums import TA_CENTER, TA_RIGHT
+
+        stylesheet = StyleSheet1()
+        stylesheet.add(ParagraphStyle(
+            name='CommunityName',
+            fontName='Helvetica',
+            fontSize=16,
+            alignment=TA_CENTER,
+            textColor=colors.HexColor('#505050'),
+            leading=24,
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='ReportName',
+            fontName='Helvetica',
+            fontSize=18,
+            leading=28,
+            alignment=TA_CENTER,
+            textColor=colors.HexColor('#565656'),
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='community_column',
+            fontName='Helvetica',
+            fontSize=14,
+            leading=30,
+            alignment=TA_RIGHT,
+            textColor=colors.HexColor('#565656'),
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='report_period',
+            fontName='Helvetica',
+            fontSize=11,
+            leading=22,
+            alignment=TA_CENTER,
+            textColor=colors.HexColor('#333333'),
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='report_vars',
+            fontName='Helvetica',
+            fontSize=14,
+            leading=26,
+            textColor=colors.HexColor('#565656'),
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='normal',
+            fontName='Helvetica',
+            fontSize=8,
+            textColor=colors.HexColor('#333333'),
+        ))
+        stylesheet.add(ParagraphStyle(
+            name='value',
+            alignment=TA_RIGHT,
+            fontName='Helvetica',
+            fontSize=9,
+            textColor=colors.HexColor('#444444'),
+        ))
+        return stylesheet
