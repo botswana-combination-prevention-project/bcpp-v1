30b014d426a6e62e910c28f7b28b1ab0bcb2599a
Twice Tshwenyane <twicet@gmail.com>
2014-02-03 09:29:10 +0200

    added pdf template for key indicators report
    Former-commit-id: 49999cb25a4be867d44a34578dd92f18bef81255

--- bhp066/apps/bcpp_analytics/pdf/community_comparison_report.py
+++ bhp066/apps/bcpp_analytics/pdf/community_comparison_report.py
@@ -2,18 +2,19 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import mm
+from reportlab.platypus import Paragraph
 
 
 class CommunityComparisonPDF(object):
 
-    def __init__(self, pdf_data, title):
-        self.community1_data = pdf_data['community1_data']
-        self.community2_data = pdf_data['community2_data']
-        self.community1_name = self.community1_data[0].community
-        self.community2_name = self.community2_data[0].community
+    def __init__(self, pdf_data):
+        self.community1_name = pdf_data['community1']
+        self.community2_name = pdf_data['community2']
         self.communities = "%s - %s" % (self.community1_name, self.community2_name)
         self.report_period = "%s - %s" % (pdf_data["start_date"], pdf_data["end_date"])
-        self.report_title = title
+        self.report_title = "%s Report" % pdf_data['title']
+        self.report_variables = pdf_data['report_variables']
+        self.data = pdf_data['data']
         self.style = getSampleStyleSheet()
         self.p_style = self._paragraph_styles()
 
@@ -32,26 +33,45 @@ class CommunityComparisonPDF(object):
 
     def build(self, output):
         from .lib.spreadsheet_table import SpreadsheetTable
-        from reportlab.platypus.flowables import Spacer
-        from reportlab.platypus import Paragraph
-        from itertools import izip
 
         story = []
-        story.append(Paragraph("Community Accrual Report", self.p_style['ReportName']))
+        self._populate_report_title(story)
+        report_data = []
+        self._populate_table_header(report_data)
+        report_contents, extra_styles = self._populate_report()
+        report_data.extend(report_contents)
+        report_table = SpreadsheetTable(report_data, repeatRows=1)
+        table_style = [
+            ('LINEABOVE', (0, 0), (-1, 0), 0.25, colors.orange),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ]
+        table_style.extend(extra_styles)
+        report_table.setStyle(table_style)
+        story.append(report_table)
+        self.draw(output, story)
+
+    def _populate_report_title(self, story):
+        from reportlab.platypus.flowables import Spacer
+
+        story.append(Paragraph(self.report_title, self.p_style['ReportName']))
         story.append(Paragraph(self.communities, self.p_style['CommunityName']))
         story.append(Paragraph(self.report_period, self.p_style['report_period']))
         story.append(Spacer(0, 3 * mm))
-        report_data = []
-        report_table_headers = [Paragraph("Accrual Report Variables", self.p_style['report_vars'])]
+
+    def _populate_table_header(self, report_data):
+        report_table_headers = [Paragraph(self.report_variables, self.p_style['report_vars'])]
         for header in [self.community1_name, self.community2_name]:
             report_table_headers.append(Paragraph(header, self.p_style['community_column']))
         report_data.append(report_table_headers)
+
+    def _populate_report(self):
+        from itertools import izip
 
         row_no = 0
         report_contents = []
         extra_styles = []
         empty_column = " "
-        for sect1, sect2 in izip(self.community1_data, self.community2_data):
+        for sect1, sect2 in self.data:
             report_contents.append([sect1.display_title(), empty_column, empty_column])
             row_no += 1
             extra_styles.append(('BACKGROUND', (0, row_no), (2, row_no), colors.HexColor('#ffeb94')))
@@ -65,16 +85,7 @@ class CommunityComparisonPDF(object):
                 value2 = Paragraph(comm2_row.display_value(), self.p_style['value'])
                 row_data = [comm1_row.label, value1, value2]
                 report_contents.append(row_data)
-        report_data.extend(report_contents)
-        report_table = SpreadsheetTable(report_data, repeatRows=1)
-        table_style = [
-            ('LINEABOVE', (0, 0), (-1, 0), 0.25, colors.orange),
-            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ]
-        table_style.extend(extra_styles)
-        report_table.setStyle(table_style)
-        story.append(report_table)
-        self.draw(output, story)
+        return (report_contents, extra_styles)
 
     def _paragraph_styles(self):
         from reportlab.lib.styles import ParagraphStyle, StyleSheet1
