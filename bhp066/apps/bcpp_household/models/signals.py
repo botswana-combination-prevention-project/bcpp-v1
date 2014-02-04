59c675f62f9b2d7c7be9ad9b22f44c5e1b250332
ckgathi <ckgathi@bhp.org.bw>
2014-02-04 14:03:09 +0200

    Plot replacement

--- bhp066/apps/bcpp_household/models/signals.py
+++ bhp066/apps/bcpp_household/models/signals.py
@@ -17,7 +17,7 @@ def post_save_on_household(sender, instance, created, **kwargs):
         if isinstance(instance, Household):
             instance.post_save_update_identifier(instance, created)
             instance.post_save_create_household_structure(instance, created)
-            instance.post_save_plot_allowed_to_enumerate(instance, created)
+#             instance.post_save_plot_allowed_to_enumerate(instance, created)
 
 
 @receiver(post_save, weak=False, dispatch_uid="household_structure_on_post_save")
