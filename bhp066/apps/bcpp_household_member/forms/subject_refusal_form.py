5fc1d6c9536128d524dbe195962c7fb08594f539
SirOne Pharatlhatlhe <sirone@One.local>
2014-02-17 08:27:41 +0200

    refusal form validations

--- bhp066/apps/bcpp_household_member/forms/subject_refusal_form.py
+++ bhp066/apps/bcpp_household_member/forms/subject_refusal_form.py
@@ -1,8 +1,16 @@
+from django import forms
 from ..models import SubjectRefusal
 from .base_membership_form import BaseMembershipForm
 
 
 class SubjectRefusalForm(BaseMembershipForm):
+
+    def clean(self):
+        cleaned_data = super(SubjectRefusalForm, self).clean()
+        offered_htc = cleaned_data.get('participant_offered_htc')
+        if offered_htc is None:
+            raise forms.ValidationError('The answer to question 6, must be either \'Yes\' or \' No\''.format(offered_htc))
+        return cleaned_data
 
     class Meta:
         model = SubjectRefusal
