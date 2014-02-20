9702bf279c603771847e8f0a463adb095b51c493
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-02-20 15:56:07 +0200

    research blood draw
    Former-commit-id: 27dfcf700c7fe6e1c94d889eaf4cc777b2c1fd7c

--- bhp066/apps/bcpp_rbd_subject/admin/__init__.py
+++ bhp066/apps/bcpp_rbd_subject/admin/__init__.py
@@ -3,3 +3,4 @@ from .subject_locator_rbd_admin import SubjectLocatorRBDAdmin
 from .subject_visit_rbd_admin import SubjectVisitRBDAdmin
 from .subject_visit_model_rbd_admin import SubjectVisitModelRBDAdmin
 from .rbd_eligibility_admin import RBDEligibilityAdmin
+from .rbd_demographics_admin import RBDDemographicsAdmin
