7a49142f7438ef3e9bbe53d7fd39b904aadfe246
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-03-10 15:54:31 +0200

    allowing consent and eligibility for above 64 in RBD only consent
    Former-commit-id: 17066574d6d8eb6fe3363791dd6e231b52966335

--- bhp066/apps/bcpp_rbd_subject/forms/subject_consent_rbd_form.py
+++ bhp066/apps/bcpp_rbd_subject/forms/subject_consent_rbd_form.py
@@ -1,3 +1,5 @@
+from dateutil.relativedelta import relativedelta
+
 from django import forms
 from apps.bcpp_subject.forms import MainConsentForm
 from apps.bcpp_rbd_subject.models import SubjectConsentRBDonly
@@ -7,6 +9,12 @@ class SubjectConsentRBDForm(MainConsentForm):
     def check_elligibility_filled(self, cleaned_data):
         if not cleaned_data.get('household_member').eligible_rbd_subject == True:
             raise forms.ValidationError('Subject is not eligible or has not been confirmed eligible. Complete the eligibility checklist first. Got {0}'.format(cleaned_data.get('household_member')))
+
+    def study_specifics_checks(self, obj, cleaned_data):
+        consent_datetime = cleaned_data.get('consent_datetime').date()
+        rdelta = relativedelta(consent_datetime, cleaned_data.get('dob'))
+        if rdelta.years < obj.minimum_age_of_consent:
+            raise forms.ValidationError(u'Subject is too young to consent. Got {0} years'.format(rdelta.years))
 
     class Meta:
         model = SubjectConsentRBDonly
