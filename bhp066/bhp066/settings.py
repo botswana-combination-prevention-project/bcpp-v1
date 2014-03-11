f2e3f997e5dcdbe7c26fa2c1d14e9b9b99c6bef4
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-03-11 15:48:14 +0200

    settings for netbook deployment
    Former-commit-id: 37c7f5117b2e948fedf8406941e907dc80c4dad5

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -24,12 +24,12 @@ CONFIG_DIR = PROJECT_DIR.child('bhp066')
 MAP_DIR = STATIC_ROOT.child('img')
 
 # edc.crytpo_fields encryption keys
-KEY_PATH = '/Volumes/bhp066/keys'  # DONT DELETE ME!!, just comment out
+#KEY_PATH = '/Volumes/bhp066/keys'  # DONT DELETE ME!!, just comment out
 #KEY_PATH = '/Users/melissa/Documents/git/bhp066/bhp066/keys'
 # KEY_PATH = '/Users/twicet/dev/bhp/projs/git/bhp066_settings/bhp066/keys'
 #KEY_PATH = '/Users/ckgathi/source/confirm_plots/bhp066/keys'
 #KEY_PATH = '/Users/sirone/Documents/workspace/git_projects/bhp066_git/bhp066/keys'
-#KEY_PATH = '/Volumes/keys'
+KEY_PATH = '/Volumes/keys'
 
 # DB_FILES = {
 #     'table': 'FILES',
@@ -107,7 +107,7 @@ else:
             'OPTIONS': {
                 'init_command': 'SET storage_engine=INNODB',
             },
-            'NAME': 'bhp066_140310',
+            'NAME': 'bhp066',
             'USER': 'root',
             'PASSWORD': 'cc3721b',
             'HOST': '',
