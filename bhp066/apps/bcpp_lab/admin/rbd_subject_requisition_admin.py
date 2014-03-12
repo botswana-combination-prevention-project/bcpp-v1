61072eeb433d0fd6d0e59a6a297d73bcfa6a6693
erik van widenfelt <ew2789@gmail.com>
2014-03-12 10:48:12 +0200

    rename rbd requisition
    Former-commit-id: 8a1eb39576367b924c357196fd6b5bea788c8818

--- bhp066/apps/bcpp_lab/admin/rbd_subject_requisition_admin.py
+++ bhp066/apps/bcpp_lab/admin/rbd_subject_requisition_admin.py
@@ -3,16 +3,16 @@ from django.contrib import admin
 from apps.bcpp_rbd_subject.models import SubjectVisitRBD
 
 from ..classes import SubjectRequisitionModelAdmin
-from ..forms import RBDSubjectRequisitionForm
-from ..models import RBDSubjectRequisition
+from ..forms import SubjectRequisitionRBDForm
+from ..models import SubjectRequisitionRBD
 
 
-class RBDSubjectRequisitionAdmin(SubjectRequisitionModelAdmin):
+class SubjectRequisitionRBDAdmin(SubjectRequisitionModelAdmin):
 
     visit_model = SubjectVisitRBD
     visit_fieldname = 'subject_visit_rbd'
     dashboard_type = 'rbd_subject'
 
-    form = RBDSubjectRequisitionForm
+    form = SubjectRequisitionRBDForm
 
-admin.site.register(RBDSubjectRequisition, RBDSubjectRequisitionAdmin)
+admin.site.register(SubjectRequisitionRBD, SubjectRequisitionRBDAdmin)
