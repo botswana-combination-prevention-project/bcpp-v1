6114be1be4342c6bf531ec16faaa0ae6dee6da3e
SirOne Pharatlhatlhe <sirone@One.local>
2014-03-13 21:51:38 +0200

    fixed bcpp_rbd

--- bhp066/apps/bcpp_rbd/managers/base_scheduled_rbd_visit_model_manager.py
+++ bhp066/apps/bcpp_rbd/managers/base_scheduled_rbd_visit_model_manager.py
@@ -4,6 +4,6 @@ from django.db import models
 class ScheduledRBDModelManager(models.Manager):
     """Manager for all scheduled models (those with a subject_visit fk)."""
     def get_by_natural_key(self, report_datetime, visit_instance, code, subject_identifier_as_pk):
-        RBDVisit = models.get_model('bcpp_RBD_subject', 'RBDVisit')
+        RBDVisit = models.get_model('bcpp_rbd', 'RBDVisit')
         rbd_visit = RBDVisit.objects.get_by_natural_key(report_datetime, visit_instance, code, subject_identifier_as_pk)
         return self.get(rbd_visit=rbd_visit)
