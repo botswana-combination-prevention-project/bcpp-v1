4d8881d6466cff7eda259d84e4586b9d9b2ebe89
ckgathi <ckgathi@bhp.org.bw>
2014-03-15 18:42:26 +0200

    replacement algorithm bug fix
    Former-commit-id: 7d337118fa8420f741a12190027dc8cac4b4fd8f

--- bhp066/apps/bcpp_household/views/replace_data.py
+++ bhp066/apps/bcpp_household/views/replace_data.py
@@ -7,6 +7,7 @@ from edc.device.dispatch.models import DispatchContainerRegister
 from ..models import Plot, Household
 from ..classes import ReplacementData
 
+
 def replace_data(request):
     """Get all plots to be replaced.
 
@@ -16,26 +17,30 @@ def replace_data(request):
     replacement_producer = []
     plot_identifiers = []
     replace_str = ''
-    replacement_count = 0
     template = 'replacement_data.html'
     plots = Plot.objects.filter(Q(selected=2) | Q(selected=1))
     #Get all household to be replaced
     for plot in plots:
         if ReplacementData().replace_refusals(plot):
-            replacement_data = replacement_data + ReplacementData().replace_refusals(plot)
+            replacement_data = replacement_data + ReplacementData().replace_refusals(plot)  # replacement of refusals.
             plot_identifiers.append(plot.plot_identifier)
         if ReplacementData().replacement_absentees_ineligibles(plot):
-            replacement_data = replacement_data + ReplacementData().replacement_absentees_ineligibles(plot)
+            replacement_data = replacement_data + ReplacementData().replacement_absentees_ineligibles(plot)  # replacement of absentees.
             plot_identifiers.append(plot.plot_identifier)
+        if ReplacementData().is_replacement_valid(plot):
+            replacement_data = replacement_data + ReplacementData().is_replacement_valid(plot)  # replacement of an invalid replacement.
     replacement_count = len(replacement_data)
-    container_items = DispatchContainerRegister.objects.filter(Q(**{'{0}__in'.format('container_identifier'): plot_identifiers}))
+    # For all items attach it to the produce where it has been dispatch to.
     for identifier in plot_identifiers:
-        container =  DispatchContainerRegister.objects.get(container_identifier=identifier)
+        container = DispatchContainerRegister.objects.get(container_identifier=identifier)
         plot = Plot.objects.get(plot_identifier=identifier)
         household = Household.objects.get(plot=plot)
         replacement_producer.append([household.household_identifier, container.producer])
-    for household in replacement_data:
-        replace_str = replace_str + ',' + household.household_identifier
+    for item in replacement_data:
+        if isinstance(item, Plot):
+            replace_str = replace_str + ',' + item.plot_identifier
+        elif isinstance(item, Household):
+            replace_str = replace_str + ',' + item.household_identifier
     return render_to_response(
             template, {
                 'replacement_data': replacement_data,
