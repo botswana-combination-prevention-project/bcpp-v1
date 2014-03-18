7073ddf048fb7b6c696e26e5d2e5f83ad5923798
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-03-18 14:22:25 +0200

    tests
    Former-commit-id: dd678fdcaf713d3da4f607b1335a3316d6c8987b

--- bhp066/apps/bcpp_household_member/tests/__init__.py
+++ bhp066/apps/bcpp_household_member/tests/__init__.py
@@ -2,4 +2,5 @@
 from .natural_key_tests import NaturalKeyTests
 # from .household_composition_tests import HouseholdCompositionTests
 from .eligibility_tests import EligibilityTests
+from .enrollment_checklist_tests import EnrollmentChecklistTests
 from .household_member_tests import HouseholdMemberTests
