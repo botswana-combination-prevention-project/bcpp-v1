2df6796ab285b42b75704a07dc22e8666f1e1078
ckgathi <ckgathi@bhp.org.bw>
2014-03-18 12:35:28 +0200

    household refusal manager

--- bhp066/apps/bcpp_household/managers/household_refusal_manager.py
+++ bhp066/apps/bcpp_household/managers/household_refusal_manager.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-from apps.bcpp_survey.models import Survey
-
 
 class HouseholdRefusalManager(models.Manager):
 
-    def get_by_natural_key(self, household_identifier):
-        Household = models.get_model('bcpp_household', 'Household')
-        household = Household.objects.get_by_natural_key(household_identifier)
-        return self.get(household=household)
+    def natural_key(self):
+        return self.household.natural_key() + self.survey.natural_key()
+    natural_key.dependencies = ['bcpp_household.household']
