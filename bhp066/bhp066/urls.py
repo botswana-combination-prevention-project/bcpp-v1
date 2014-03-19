320e4b9f3e6ed70c74c4f84bc5992a067868f222
ckgathi <ckgathi@bhp.org.bw>
2014-03-19 08:17:05 +0200

    add a base replacement class
    Former-commit-id: b376f59d7b52e391d111f5c5ea9d1ca909ac8ecb

--- bhp066/bhp066/urls.py
+++ bhp066/bhp066/urls.py
@@ -15,16 +15,16 @@ from edc.subject.lab_tracker.classes import site_lab_tracker
 from edc.subject.visit_schedule.classes import site_visit_schedules
 from edc.core.bhp_data_manager.classes import data_manager
 from edc.dashboard.section.classes import site_sections
-from edc.lab.lab_profile.classes import site_lab_profiles
-
-from apps.bcpp.app_configuration.classes import BcppAppConfiguration
-# from apps.clinic.bcpp_clinic_configuration.classes import BcppClinicConfiguration
+# from edc.lab.lab_profile.classes import site_lab_profiles
+# 
+# from apps.bcpp.app_configuration.classes import BcppAppConfiguration
+from apps.clinic.bcpp_clinic_configuration.classes import BcppClinicConfiguration
 
 admin.autodiscover()
-site_lab_profiles.autodiscover()
+# site_lab_profiles.autodiscover()
 dajaxice_autodiscover()
-#BcppClinicConfiguration()
-BcppAppConfiguration()
+BcppClinicConfiguration()
+# BcppAppConfiguration()
 site_mappers.autodiscover()
 site_visit_schedules.autodiscover()
 site_visit_schedules.build_all()
