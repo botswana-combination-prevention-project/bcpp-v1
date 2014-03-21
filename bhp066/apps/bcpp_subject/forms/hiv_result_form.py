a55b80e8e3b2c1e4669ea7888c38fed35c1dba44
erik van widenfelt <ew2789@gmail.com>
2014-03-21 11:05:45 +0200

    req rules, hic enrolment check fails, add fields to hiv result
    Former-commit-id: 1405219db6125716bbd0e3b6dd9d9c5c0a9a3c17

--- bhp066/apps/bcpp_subject/forms/hiv_result_form.py
+++ bhp066/apps/bcpp_subject/forms/hiv_result_form.py
@@ -1,5 +1,5 @@
 from django import forms
-from ..models import HivResult, HicEnrollment
+from ..models import HivResult
 from .base_subject_model_form import BaseSubjectModelForm
 
 
@@ -10,7 +10,8 @@ class HivResultForm (BaseSubjectModelForm):
         cleaned_data = super(HivResultForm, self).clean()
 
         # validating that hiv_result is not changed after HicEnrollment is filled
-        self.instance.hic_enrollment_checks(forms.ValidationError)
+        if self.instance:
+            self.instance.hic_enrollment_checks(self.instance, cleaned_data.get('subject_visit'), forms.ValidationError)
         # validating when testing declined
         if cleaned_data.get('hiv_result', None) == 'Declined' and not cleaned_data.get('why_not_tested', None):
             raise forms.ValidationError('If participant has declined testing, provide reason participant declined testing')
@@ -26,14 +27,11 @@ class HivResultForm (BaseSubjectModelForm):
         # testing done but giving reason why not done
         if ((cleaned_data.get('hiv_result', None) == 'POS') or (cleaned_data.get('hiv_result', None) == 'NEG') or (cleaned_data.get('hiv_result', None) == 'IND')) and (cleaned_data.get('why_not_tested', None)):
             raise forms.ValidationError('If testing is performed, DO NOT provide reason for declining test')
- 
+
         # testing done but not providing date
         if ((cleaned_data.get('hiv_result', None) == 'POS') or (cleaned_data.get('hiv_result', None) == 'NEG') or (cleaned_data.get('hiv_result', None) == 'IND')) and not (cleaned_data.get('hiv_result_datetime', None)):
             raise forms.ValidationError('If test has been performed, what is the test result date time?')
-
-
         return cleaned_data
-
 
     class Meta:
         model = HivResult
