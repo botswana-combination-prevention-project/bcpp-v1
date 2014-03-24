96a2b925374ca667e802b464005d7addff4300d5
ckgathi <ckgathi@bhp.org.bw>
2014-03-24 16:06:36 +0200

    replacement fixes
    Former-commit-id: 8bbfc7380295ab5dce1a11ff48d47d539202b72a

--- bhp066/apps/bcpp_household/models/base_replacement.py
+++ bhp066/apps/bcpp_household/models/base_replacement.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from edc.device.dispatch.models import BaseDispatchSyncUuidModel
 
 from ..classes import ReplacementData
@@ -13,10 +15,7 @@ class BaseReplacement(BaseDispatchSyncUuidModel):
         return False
 
     def replaced(self, using=None):
-        if self.is_household() or self.is_plot():
-            if self.replacement:
-                return True
-        return False
+        return None
 
     def potential_replacement(self, using=None):
         plot = None
@@ -41,19 +40,30 @@ class BaseReplacement(BaseDispatchSyncUuidModel):
                 item[0].save()
 
     def replacement_container(self, using=None):
-        if self.is_plot() or self.is_household():
+        if self.is_household():
             return self
         return None
 
     def save(self, *args, **kwargs):
         using = kwargs.get('using', None)
         if not (self.is_plot() or self.is_household()):
-            self.potential_replacement(using=None)
-            # TODO: figure out a way here.
-#         if self.replacement_container(using).replacement:
-#             raise AlreadyReplaced('Model {0}-{1} has its container replaced.'.format(self._meta.object_name, self.pk))
-#         if self.replaced(using):
-#             raise AlreadyReplaced('Model {0}-{1} is replaced.'.format(self._meta.object_name, self.pk))
+            self.potential_replacement(using)
+        if self.replacement_container(using):  # returns a household instance
+            if self.id:
+                try:
+                    household = self.replacement_container(using).__class__.objects.get(household_identifier=self.replacement_container(using).household_identifier)
+                    if household.replacement and household.replaceble:
+                        raise AlreadyReplaced('Model {0}-{1} has its container replaced.'.format(self._meta.object_name, self.pk))
+                except ObjectDoesNotExist:
+                    pass
+        if self.replaced(using):  # returns a plot instance
+            if self.id:
+                try:
+                    plot = self.replaced(using).__class__.objects.get(plot_identifier=self.replaced(using).plot_identifier)
+                    if plot.replacement and plot.replaceble:
+                        raise AlreadyReplaced('Model {0}-{1} is replaced.'.format(self._meta.object_name, self.pk))
+                except ObjectDoesNotExist:
+                    pass
         super(BaseReplacement, self).save(*args, **kwargs)
 
     class Meta:
