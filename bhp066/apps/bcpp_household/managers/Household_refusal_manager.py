1f3498923a825da497942620080e876982ed710f
erik van widenfelt <ew2789@gmail.com>
2014-03-24 15:26:16 +0200

    fix problem in git with household_refusal_manager
    Former-commit-id: 283c214e3a91b8b11d4023068f46548ead166378

--- bhp066/apps/bcpp_household/managers/Household_refusal_manager.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class HouseholdRefusalManager(models.Manager):
-
-    def natural_key(self):
-        return self.household.natural_key() + self.survey.natural_key()
-    natural_key.dependencies = ['bcpp_household.household']
