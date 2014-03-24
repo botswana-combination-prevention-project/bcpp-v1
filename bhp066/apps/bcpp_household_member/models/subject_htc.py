d6587779333f314d25620f8088ec51a0e3d17d48
erik van widenfelt <ew2789@gmail.com>
2014-03-24 09:32:54 +0200

    update subject htc form base class
    Former-commit-id: 72577e9f7d2d1af7e70458f77d3ed2b7da1d2b8e

--- bhp066/apps/bcpp_household_member/models/subject_htc.py
+++ bhp066/apps/bcpp_household_member/models/subject_htc.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 from edc.audit.audit_trail import AuditTrail
-from edc.choices import YES_NO
+from edc.choices import YES_NO, YES_NO_NA
 
 from apps.bcpp_household_member.constants import HTC, HTC_ELIGIBLE, REFUSED_HTC
 from apps.bcpp_household_member.exceptions import MemberStatusError
+from apps.bcpp.choices import HIV_RESULT
 
 from .base_member_status_model import BaseMemberStatusModel
+
+
+HIV_RESULT = list(HIV_RESULT)
+HIV_RESULT.append(('N/A', 'Not applicable'))
+HIV_RESULT = tuple(HIV_RESULT)
 
 
 class SubjectHtc(BaseMemberStatusModel):
@@ -16,11 +22,39 @@ class SubjectHtc(BaseMemberStatusModel):
         max_length=10,
         choices=YES_NO)
 
-    outcome = models.CharField(
-        verbose_name="The subject:",
+    accepted = models.CharField(
+        verbose_name="Did the subject accept HTC",
         max_length=25,
-        choices=(('accepted', 'Accepted HTC'), ('refused', 'Refused HTC'), ('N/A', 'Not applicable'), ),
+        choices=YES_NO_NA,
+        null=True,
+        blank=False,
         )
+
+    refusal_reason = models.CharField(
+        verbose_name="If the subject did not accept HTC, please explain",
+        max_length=50,
+        null=True,
+        blank=True,
+        help_text='Required if subject did not accepted HTC')
+
+    hiv_result = models.CharField(
+        verbose_name="What was the result of todays\'s HIV test",
+        max_length=10,
+        choices=HIV_RESULT,
+        help_text='Required if subject accepted HTC')
+
+    referred = models.CharField(
+        verbose_name="Was the subject referred",
+        max_length=10,
+        choices=YES_NO_NA,
+        help_text='Required if subject accepted HTC')
+
+    referral_clinic = models.CharField(
+        verbose_name="If referred, which clinic",
+        max_length=25,
+        blank=True,
+        null=True,
+        help_text='Required if subject was referred')
 
     comment = models.TextField(max_length=250, null=True, blank=True)
 
@@ -29,7 +63,6 @@ class SubjectHtc(BaseMemberStatusModel):
     def save(self, *args, **kwargs):
         if self.household_member.member_status not in [HTC, HTC_ELIGIBLE, REFUSED_HTC]:
             raise MemberStatusError('Expected member status to be on of {0}. Got {1}'.format([HTC, HTC_ELIGIBLE, REFUSED_HTC], self.household_member.member_status))
-        self.survey = self.household_member.survey
         self.survey = self.household_member.survey
         self.registered_subject = self.household_member.registered_subject
         self.household_member.htc = True
