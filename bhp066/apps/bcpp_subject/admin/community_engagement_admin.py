cf170d0ab89fc3faa6a6f828894005acfa4b466c
erik van widenfelt <ew2789@gmail.com>
2014-03-26 07:10:04 +0200

    remove reference to admin_supplemental_fields
    Former-commit-id: 946867d949545f90a094424a36b2aec7e0f63cef

--- bhp066/apps/bcpp_subject/admin/community_engagement_admin.py
+++ bhp066/apps/bcpp_subject/admin/community_engagement_admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from edc.apps.admin_supplemental_fields.admin import SupplementalModelAdminMixin
-from edc.apps.admin_supplemental_fields.classes import SupplementalFields
+# from edc.apps.admin_supplemental_fields.admin import SupplementalModelAdminMixin
+# from edc.apps.admin_supplemental_fields.classes import SupplementalFields
 
 from ..forms import CommunityEngagementForm
 from ..models import CommunityEngagement
@@ -9,15 +9,15 @@ from ..models import CommunityEngagement
 from .subject_visit_model_admin import SubjectVisitModelAdmin
 
 
-class CommunityEngagementAdmin(SupplementalModelAdminMixin, SubjectVisitModelAdmin):
+class CommunityEngagementAdmin(SubjectVisitModelAdmin):
 
     form = CommunityEngagementForm
-    supplemental_fields = SupplementalFields(
-        ('community_engagement',
-        'vote_engagement',
-        'problems_engagement',
-        'problems_engagement_other',
-        'solve_engagement',), p=0.09, group='CE', grouping_field='subject_visit')
+#     supplemental_fields = SupplementalFields(
+#         ('community_engagement',
+#         'vote_engagement',
+#         'problems_engagement',
+#         'problems_engagement_other',
+#         'solve_engagement',), p=0.09, group='CE', grouping_field='subject_visit')
     fields = (
         "subject_visit",
         'community_engagement',
