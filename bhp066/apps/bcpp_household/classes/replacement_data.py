83108f531f0264f230d12e917625c6c8704a85a7
ckgathi <ckgathi@bhp.org.bw>
2014-03-27 10:29:31 +0200

    fix replacement methods add add tests
    Former-commit-id: 0b4b218b6d84d1fba39fa600e5e93e54f5f71188

--- bhp066/apps/bcpp_household/classes/replacement_data.py
+++ bhp066/apps/bcpp_household/classes/replacement_data.py
@@ -10,11 +10,10 @@ class ReplacementData(object):
         """Check if a plot has household refusals that would make it be replaced."""
         replaced = []
         if plot.status == 'residential_habitable':
-                if self.is_hoh_refused(household):
+                if self.is_hoh_refused(household_structure):
                     replaced.append([household, 'HOH refusal', self.producer(plot)])
-                else:
-                    if self.is_refusal(household, household_structure):
-                        replaced.append([self.is_refusal(household, household_structure), 'all members refused', self.producer(plot)])
+                elif self.is_refusal(household, household_structure):
+                    replaced.append([self.is_refusal(household, household_structure), 'all members refused', self.producer(plot)])
         return replaced
 
     def producer(self, plot):
@@ -33,15 +32,15 @@ class ReplacementData(object):
         if plot.status == 'residential_habitable':
             if self.is_absent(household, household_structure):
                 replaced.append([self.is_absent(household, household_structure), 'all members are absent', self.producer(plot)])
-            if self.no_informant(household):
-                replaced.append([self.no_informant(household), 'no household informant', self.producer(plot)])
+            if self.no_informant(household, household_structure):
+                replaced.append([self.no_informant(household, household_structure), 'no household informant', self.producer(plot)])
             if self.no_eligible_rep(household):
                 replaced.append([self.no_eligible_rep(household), 'no eligible members', self.producer(plot)])
         return replaced
 
-    def is_hoh_refused(self, household):
+    def is_hoh_refused(self, household_structure):
         """Check if head of household refused members to participate."""
-        if household.refused:
+        if household_structure.refused_enumerated:
             return True
         return False
 
@@ -60,29 +59,21 @@ class ReplacementData(object):
         members = None
         if household.enumerated:
             members = HouseholdMember.objects.filter(household_structure=household_structure)
-            #All eligible members refused
             members_status_list = []
             for member in members:
-                if member.age_in_years >= 16 and member.study_resident == 'Yes':
-                    #accumulate the statuses of all eligible members
-                    members_status_list.append(member.member_status)
+                if member.age_in_years >= 16 and member.study_resident == 'Yes':  # All eligible members refused
+                    members_status_list = list(set(members_status_list + [member.refused]))
             if members_status_list:
-                #Ckeck if all the values in the member status list are the same.
-                if all(map(lambda x: x == members_status_list[0], members_status_list)):
-                    if members_status_list[0] == 'REFUSED':
-                        #If any member had a status that is not 'REFUSE' then this plot does not qualify for replacement
+                if members_status_list[0] and len(members_status_list):
                         replacement_household = household
         return replacement_household
 
-    def no_informant(self, household):
+    def no_informant(self, household, household_structure):
         """Check if there is anyone to give information living in the household"""
-        from ..models import HouseholdAssessment
         replacement_household = None
         if household.enumeration_attempts == 3:
-            household_assessment = HouseholdAssessment.objects.filter(household=household)
-            if household_assessment:
-                    if household.reason_not_enumerated == 'no_household_informant' and household_assessment[0].vdc_househould_status in ['rarely_there', 'seasonally_there']:
-                        replacement_household = household
+            if household_structure.no_informant:
+                replacement_household = household
         return replacement_household
 
     def is_absent(self, household, household_structure):
@@ -92,12 +83,15 @@ class ReplacementData(object):
         members = None
         if household.enumerated:
             members = HouseholdMember.objects.filter(household_structure=household_structure)
+            absentee_entry_count_list = []
             for member in members:
-                if member.age_in_years >= 16 and member.study_resident == 'Yes' and member.member_status == 'ABSENT':
+                if member.age_in_years >= 16 and member.study_resident == 'Yes' and member.member_status == 'ABSENT':  # All eligible members absent
                     sub_absentee = SubjectAbsentee.objects.get(household_member=member)
                     num_absentee_entries = SubjectAbsenteeEntry.objects.filter(subject_absentee=sub_absentee).count()
-                    if num_absentee_entries == 3:  # Then we have found a reason not to replace this plot
-                        replacement_household = household
+                    absentee_entry_count_list = list(set(absentee_entry_count_list + [num_absentee_entries]))
+            if absentee_entry_count_list:
+                if absentee_entry_count_list[0] == 3 and len(absentee_entry_count_list) == 1:  # Then we have found a reason not to replace this plot
+                    replacement_household = household
         return replacement_household
 
     def replacement_reason(self, household, household_structure):
