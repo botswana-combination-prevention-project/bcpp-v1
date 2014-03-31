24db6c78bf5b474f33750c4617879158d055c0a2
erik van widenfelt <ew2789@gmail.com>
2014-03-31 08:49:28 +0200

    subject referal helper MASA-DF case

--- bhp066/apps/bcpp_subject/models/signals.py
+++ bhp066/apps/bcpp_subject/models/signals.py
@@ -1,7 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from edc.subject.appointment.constants import COMPLETE
+
 from .subject_consent import SubjectConsent
+
+from ..classes import SubjectReferralHelper
+from ..models import SubjectReferral
 
 
 @receiver(post_save, weak=False, dispatch_uid='subject_consent_on_post_save')
@@ -10,3 +15,15 @@ def subject_consent_on_post_save(sender, instance, **kwargs):
         if isinstance(instance, (SubjectConsent)):
             instance.post_save_update_registered_subject()  # HM values must either be changed or match that provided on the consent
             instance.household_member.enroll_household()
+
+
+def update_subject_referral_on_post_save(sender, instance, **kwargs):
+    """Updates the subject referral if a model that is part of the referral data is changed."""
+    if not kwargs.get('raw', False):
+        if isinstance(instance, SubjectReferralHelper.models):
+            if 'subject_visit' in dir(instance):
+                try:
+                    subject_referral = SubjectReferral.objects.get(subject_visit=instance.subject_visit)
+                    subject_referral.save()
+                except SubjectReferral.DoesNotExist:
+                    pass
