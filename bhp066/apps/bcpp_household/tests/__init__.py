d13643fe77a812d6f68046eaf7d7fe0643159631
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-03-31 11:42:29 +0200

    bcpp_household natural key tests
    Former-commit-id: 13bdb9e1c8f3227f2946435bcf7b1c8b2e26c6e9

--- bhp066/apps/bcpp_household/tests/__init__.py
+++ bhp066/apps/bcpp_household/tests/__init__.py
@@ -1,7 +1,7 @@
 # from .household_confirmation_tests import HouseholdConfirmTests
 # from .household_mapper_tests import HouseholdMapperTests
 # from .household_tests import HouseholdTests
-# from .natural_key_tests import NaturalKeyTests
+from .natural_key_tests import NaturalKeyTests
 from .plot_replacement_tests import PlotReplacementTests
 # from .plot_tests import PlotTests
 from .replacement_tests import ReplacementTests
