744fbdfb94fb0708ab306222bd1a8b8f5a6fccbc
ckgathi <ckgathi@bhp.org.bw>
2014-04-07 08:35:07 +0200

    using perameter and filter by producer
    Former-commit-id: 6261636533009ed693ee3701d18ef8b669529ac9

--- bhp066/apps/bcpp_household/urls.py
+++ bhp066/apps/bcpp_household/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import replace_data, return_data
+from .views import replace_data, return_data, replacement_index
 
 urlpatterns = patterns('',
+    url(r'replacement_index/', replacement_index, name='replacement_index_url'),
     url(r'^return/(?P<household>[A-Z0-9\-0-9]+)/', 'return_households'),
     url(r'^replace_data/', replace_data, name='replace_data_url'),
     url(r'^return_data/', return_data, name='return_data_url')
