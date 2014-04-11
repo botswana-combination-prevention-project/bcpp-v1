a8430b1f62c80ad987b6984418e0cf6dd6a9cf97
erik van widenfelt <ew2789@gmail.com>
2014-04-11 17:24:01 +0200

    remove refusal manager after release/finish

--- bhp066/apps/bcpp_household/managers/household_refusal_manager.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class HouseholdRefusalManager(models.Manager):
-
-    def get_by_natural_key(self, household_identifier, survey_name):
-        HouseholdStructure = models.get_model('bcpp_household', 'HouseholdStructure')
-        household_structure = HouseholdStructure.objects.get_by_natural_key(household_identifier, survey_name)
-        return self.get(household_structure=household_structure)
