15e328b93de26227c8d93751f01fbf197b55e2ad
erik van widenfelt <ew2789@gmail.com>
2014-04-11 18:07:30 +0200

    add back household refusal manager
    Former-commit-id: 4af4fc9fc2f6c370de9b2eb09d69cdddcf409824

---
+++ bhp066/apps/bcpp_household/managers/household_refusal_manager.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class HouseholdRefusalManager(models.Manager):
+
+    def natural_key(self):
+        return self.household.natural_key() + self.survey.natural_key()
+    natural_key.dependencies = ['bcpp_household.household']
