db9d6e90df062fa87285480a29c39ba7b98de971
SirOne Pharatlhatlhe <sirone@One.local>
2014-04-12 17:03:32 +0200

    fixing entering of multiple head of households
    Former-commit-id: 392f2e473009b413003e0854744d1e95565c88b2

--- bhp066/apps/bcpp_household_member/forms/head_household_eligibility_form.py
+++ bhp066/apps/bcpp_household_member/forms/head_household_eligibility_form.py
@@ -7,6 +7,8 @@ class HouseholdHeadEligibilityForm(BaseModelForm):
 
     def clean(self):
         cleaned_data = super(HouseholdHeadEligibilityForm, self).clean()
+        if HouseholdHeadEligibility.objects.filter(household_structure=cleaned_data.get('household_structure'), aged_over_18='Yes', verbal_script='Yes').exists():
+            raise forms.ValidationError('You have already entered an eligible head of household. DO NOT enter another one. Continue with Survey.')
         if cleaned_data.get('household_member', None) is None:
             raise forms.ValidationError('You have to select a household member in order to save.')
         self.instance.matches_household_member_values(cleaned_data.get('household_member'), forms.ValidationError)
