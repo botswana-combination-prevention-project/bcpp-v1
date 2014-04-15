1240c3f50992b26e8f9ca0b6f8962765e5924828
erik van widenfelt <ew2789@gmail.com>
2014-04-15 12:12:10 +0200

    next clinic day now cannot be today.
    Former-commit-id: eb9216f2011e114fa3924d136267d4b8343a4c3f

--- bhp066/apps/bcpp_subject/utils/next_clinic_date.py
+++ bhp066/apps/bcpp_subject/utils/next_clinic_date.py
@@ -1,42 +1,47 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta, MO, TU, WE
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta, MO, TU, WE  # , TH, FR
 
 from django.conf import settings
 
 clinic_days = {
-    '11': MO,
-    '12': MO,
-    '13': TU,
-    '14': MO,
-    '15': MO,
-    '16': MO,
-    '17': MO,
-    '18': MO,
-    '19': MO,
-    '20': MO,
-    '21': MO,
-    '22': MO,
-    '23': MO,
-    '24': MO,
-    '25': MO,
-    '26': MO,
-    '27': MO,
-    '28': MO,
-    '29': MO,
-    '30': MO,
-    '31': MO,
-    '32': MO,
-    '33': MO,
-    '34': MO,
-    '35': MO,
-    '36': MO,
-    '37': MO,
-    '38': MO,
-    '39': MO,
-    '40': MO,
+    '11': (MO, WE),
+    '12': (MO, WE),
+    '13': (TU, WE),
+    '14': (MO, WE),
+    '15': (MO, WE),
+    '16': (MO, WE),
+    '17': (MO, WE),
+    '18': (MO, WE),
+    '19': (MO, WE),
+    '20': (MO, WE),
+    '21': (MO, WE),
+    '22': (MO, WE),
+    '23': (MO, WE),
+    '24': (MO, WE),
+    '25': (MO, WE),
+    '26': (MO, WE),
+    '27': (MO, WE),
+    '28': (MO, WE),
+    '29': (MO, WE),
+    '30': (MO, WE),
+    '31': (MO, WE),
+    '32': (MO, WE),
+    '33': (MO, WE),
+    '34': (MO, WE),
+    '35': (MO, WE),
+    '36': (MO, WE),
+    '37': (MO, WE),
+    '38': (MO, WE),
+    '39': (MO, WE),
+    '40': (MO, WE),
     }
 
 
 def next_clinic_date():
-    D = clinic_days.get(settings.SITE_CODE)
-    return datetime.today() + relativedelta(D(+1))
+    """Returns next clinic date that is not today."""
+    TODAY = date.today()
+    for D in clinic_days.get(settings.SITE_CODE):
+        clinic_date = date.today() + relativedelta(weekday=D(+1))
+        if relativedelta(TODAY, clinic_date).days > 0:
+            break
+    return datetime(clinic_date.year, clinic_date.month, clinic_date.day, 7, 30, 0)
