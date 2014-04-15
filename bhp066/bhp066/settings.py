5005a58ee2eab26676851c79f5f0827cd24200ed
erik van widenfelt <ew2789@gmail.com>
2014-04-15 09:22:31 +0200

    fix rule group for pima, add export filter for subject locator
    Former-commit-id: 2d01848a36f0157d4c750caef28dfdd4e8221c82

--- bhp066/bhp066/settings.py
+++ bhp066/bhp066/settings.py
@@ -377,7 +377,7 @@ SHORT_DATETIME_FORMAT = 'Y-m-d H:i'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # django email settings
-EMAIL_HOST = '192.168.1.48'
+EMAIL_HOST = 'mail.bhp.org.bw'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = 'edcdev'
 EMAIL_HOST_PASSWORD = 'cc3721b'
