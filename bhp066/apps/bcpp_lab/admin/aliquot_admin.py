600376ecc31c002b3ce419c3ec64dbbef79fe011
erik van widenfelt <ew2789@gmail.com>
2014-04-30 09:59:14 +0200

    add ordering to lab
    Former-commit-id: 3e72828d6196fa535411f66d9762fc38e80b3b79

--- bhp066/apps/bcpp_lab/admin/aliquot_admin.py
+++ bhp066/apps/bcpp_lab/admin/aliquot_admin.py
@@ -4,15 +4,16 @@ from edc.base.admin.admin import BaseModelAdmin
 
 from lis.labeling.actions import print_aliquot_label
 
+from ..actions import create_order
 from ..models import Aliquot
 
 
 class AliquotAdmin(BaseModelAdmin):
     date_hierarchy = 'created'
 
-    actions = [print_aliquot_label]
+    actions = [print_aliquot_label, create_order]
 
-    list_display = ("aliquot_identifier", 'subject_identifier', 'to_process', 'to_receive', 'drawn', "aliquot_type", 'aliquot_condition', 'is_packed', 'created', 'user_created', 'hostname_created')
+    list_display = ("aliquot_identifier", 'subject_identifier', 'processing', 'related', 'to_receive', 'drawn', "aliquot_type", 'aliquot_condition', 'is_packed', 'created', 'user_created', 'hostname_created')
 
     search_fields = ('aliquot_identifier', 'receive__receive_identifier', 'receive__registered_subject__subject_identifier')
 
--- bhp066/apps/bcpp_lab/models/aliquot_profile.py
+++ bhp066/apps/bcpp_lab/models/aliquot_profile.py
@@ -5,10 +5,11 @@ from edc.lab.lab_profile.models import BaseProfile
 from .aliquot_type import AliquotType
 
 
-class Profile(BaseProfile):
+class AliquotProfile(BaseProfile):
 
     aliquot_type = models.ForeignKey(AliquotType,
         verbose_name='Source aliquot type')
 
     class Meta:
         app_label = 'bcpp_lab'
+        db_table = 'bcpp_lab_profile'
