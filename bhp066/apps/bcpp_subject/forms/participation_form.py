f54c368634e667049a9343ebf410b4ccefcd6f74
SirOne Pharatlhatlhe <sirone@One.local>
2014-07-14 16:26:39 +0200

    settings

--- bhp066/apps/bcpp_subject/forms/participation_form.py
+++ bhp066/apps/bcpp_subject/forms/participation_form.py
@@ -1,9 +1,17 @@
-from ..models import Participation
+from django import forms
 
+from ..models import Participation
 from .base_subject_model_form import BaseSubjectModelForm
 
 
 class ParticipationForm (BaseSubjectModelForm):
+    def clean(self):
+
+        cleaned_data = super(ParticipationForm, self).clean()
+        if cleaned_data.get('full').lower() == 'yes' and not cleaned_data.get('description'):
+            raise forms.ValidationError('If partial participation is chosen, you need to fill in the description field.')
+
+        return cleaned_data
 
     class Meta:
         model = Participation
