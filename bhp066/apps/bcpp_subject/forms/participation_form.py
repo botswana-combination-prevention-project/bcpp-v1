e75ea3063e70f3c2893eee1eb4303a33847a1e43
SirOne Pharatlhatlhe <sirone@One.local>
2014-07-15 08:47:11 +0200

    fixing participation form bugs

--- bhp066/apps/bcpp_subject/forms/participation_form.py
+++ bhp066/apps/bcpp_subject/forms/participation_form.py
@@ -8,8 +8,10 @@ class ParticipationForm (BaseSubjectModelForm):
     def clean(self):
 
         cleaned_data = super(ParticipationForm, self).clean()
-        if cleaned_data.get('full').lower() == 'yes' and not cleaned_data.get('description'):
+        if cleaned_data.get('full').lower() == 'no' and not cleaned_data.get('description'):
             raise forms.ValidationError('If partial participation is chosen, you need to fill in the description field.')
+        if cleaned_data.get('full').lower() == 'yes' and cleaned_data.get('description'):
+            raise forms.ValidationError('If full participation is chosen, do not fill anything in the description field.')
 
         return cleaned_data
 
