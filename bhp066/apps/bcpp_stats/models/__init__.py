5946865fb0fe52fb9970655799221d080f64c46e
erik van widenfelt <ew2789@gmail.com>
2014-08-04 05:45:25 +0200

    initial
    Former-commit-id: e6ad102a14252eaefb89ebc90200d7f7f5ca022d

--- bhp066/apps/bcpp_stats/models/__init__.py
+++ bhp066/apps/bcpp_stats/models/__init__.py
@@ -1,1 +1,5 @@
-from reports import Reports
+from .reports import Reports
+from .cdc_smc import CdcSmc
+from .cdc_htc import CdcHtc
+from .cdc_htc_intake import CdcHtcIntake
+from .cdc_smc_digawana import CdcSmcDigawana
