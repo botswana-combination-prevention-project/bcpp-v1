14f7c3db9bc5934b9371ab32c65a178e96662b9e
melissa <mrbkew@gmail.com>
2014-08-05 16:29:49 +0200

    fix participation form, update rulegroups, constants, choices
    Former-commit-id: 93837e591ec9c6ae1bdd486f7426a37968c1f7ae

--- bhp066/apps/bcpp_subject/forms/participation_form.py
+++ bhp066/apps/bcpp_subject/forms/participation_form.py
@@ -8,10 +8,10 @@ class ParticipationForm (BaseSubjectModelForm):
     def clean(self):
 
         cleaned_data = super(ParticipationForm, self).clean()
-        if cleaned_data.get('full').lower() == 'no' and cleaned_data.get('participation_type').lower() == 'not applicable':
+        if cleaned_data.get('full') == 'No' and cleaned_data.get('participation_type') == 'Not Applicable':
             raise forms.ValidationError('If partial participation is chosen, you cannot choose \'Not Applicable\' in type of participation.')
-        if cleaned_data.get('full').lower() == 'yes' and cleaned_data.get('participation_type').lower() != 'not applicable':
-            raise forms.ValidationError('If full participation is chosen, you should choose \'Not Applicable\' in type of participation.')
+        if cleaned_data.get('full') == 'Yes' and cleaned_data.get('participation_type') != 'Not Applicable':
+            raise forms.ValidationError('If full participation is chosen, type of participation should be \'Not Applicable\'.')
 
         return cleaned_data
 
