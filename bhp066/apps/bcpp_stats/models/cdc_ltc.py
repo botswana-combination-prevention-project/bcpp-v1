3210c4c01e2490866d410434d83117bfc4ab1619
erik van widenfelt <ew2789@gmail.com>
2014-08-06 11:50:56 +0200

    add additional models, add commands
    Former-commit-id: 54b2ec2d48059d377a87ee98f721daf1e5f924ab

---
+++ bhp066/apps/bcpp_stats/models/cdc_ltc.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# from edc.core.crypto_fields.fields import EncryptedCharField
+
+from .base_cdc import BaseCdc
+
+
+class CdcLtc(BaseCdc):
+
+    subject_id = models.CharField(max_length=25, null=True)
+    appointment_date = models.DateField(null=True)
+    followup_appt_date = models.DateField(null=True)
+
+    class Meta:
+        app_label = 'bcpp_stats'
