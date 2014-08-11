da580b586e85385515eaa8ddaa7fe70925f8e140
erik van widenfelt <ew2789@gmail.com>
2014-08-11 08:59:36 +0200

    adjust import as incoming data more consistent.
    Former-commit-id: b1df2c94807469a7607e025d73cca1f644a4f439

--- bhp066/apps/bcpp_stats/models/cdc_htc_intake_for_clinic.py
+++ bhp066/apps/bcpp_stats/models/cdc_htc_intake_for_clinic.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from edc.core.crypto_fields.fields import EncryptedCharField
+from edc.core.crypto_fields.fields import EncryptedIdentityField
 
 from .base_cdc import BaseCdc
 
@@ -8,16 +8,16 @@ from .base_cdc import BaseCdc
 class CdcHtcIntakeForClinic(BaseCdc):
 
     community_name = models.IntegerField(null=True)
-    omang_nbr = EncryptedCharField()
+    omang_nbr = EncryptedIdentityField(null=True)
     pregnant_ind = models.IntegerField(null=True)
     prior_hiv_result = models.IntegerField(null=True)
-    prior_hiv_test_date = models.IntegerField(null=True)
+    prior_hiv_test_date = models.DateField(null=True)
     prior_hiv_test_record_available = models.IntegerField(null=True)
     referral_appt_datetime = models.DateField(null=True)
-    referred_for = models.IntegerField(null=True)
+    referred_for = models.CharField(max_length=25, null=True)
     referred_for_desc = models.CharField(max_length=250, null=True)
-    subject_id = models.IntegerField(null=True)
-    subject_id_reformatted = models.IntegerField(null=True)
+    subject_id = models.CharField(max_length=25, null=True)
+    subject_id_reformatted = models.CharField(max_length=25, null=True)
 
     class Meta:
         app_label = 'bcpp_stats'
