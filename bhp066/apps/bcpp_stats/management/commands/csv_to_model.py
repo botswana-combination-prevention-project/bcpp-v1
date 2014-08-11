71b39b085380d04b8eedac7ce984cfc92d13d4f5
erik van widenfelt <ew2789@gmail.com>
2014-08-11 08:59:36 +0200

    adjust import as incoming data more consistent.

--- bhp066/apps/bcpp_stats/management/commands/csv_to_model.py
+++ bhp066/apps/bcpp_stats/management/commands/csv_to_model.py
@@ -1,44 +1,40 @@
 import pandas as pd
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import get_model
 
+from ...utils import strip_underscore
+
 
 class Command(BaseCommand):
-
+    """A Management command that generates the syntax for a model file based on a given CSV file."""
     args = '<source_path> <destination_model_name>'
     help = 'Inspect a CDC csv file and output the syntax for a basic model file'
     option_list = BaseCommand.option_list
 
-    def strip_underscore(self, item):
-        """Returns  a string stripped of a leading or tailing underscore."""
-        if item.startswith('_'):
-            item = item[1:]
-        if item.endswith('_'):
-            item = item[:-1]
-        return item
-
     def handle(self, *args, **options):
+        """Prints model syntax  based on the given CSV file.
+
+        All fields are assumed to be IntegerFields unless the column name
+        has the word 'date' in it. Some manual editing of the proposed model
+        syntax will be necessary.
+
+        Usage: python manage.py csv_to_model <source_path> <destination_model_name>."""
         source_path = args[0]
         model_name = args[1]
         if get_model('bcpp_stats', model_name):
             raise CommandError('Model class {0} already exists.'.format(model_name))
-        df = pd.read_csv(source_path)
-        dct = df.to_dict()
-        keys = dct.keys()
+        keys = pd.read_csv(source_path).to_dict().keys()
         keys.sort()
         attr = []
         for key in keys:
             if 'date' in key.lower():
-                attr.append('{0} = models.DateField(null=True)'.format(self.strip_underscore(key)))
+                attr.append('{0} = models.DateField(null=True)'.format(strip_underscore(key)))
             else:
-                attr.append('{0} = models.IntegerField(null=True)'.format(self.strip_underscore(key)))
+                attr.append('{0} = models.IntegerField(null=True)'.format(strip_underscore(key)))
         print """from django.db import models
 
-from edc.core.crypto_fields.fields import EncryptedCharField
+from edc.core.crypto_fields.fields import EncryptedIdentityField
 
 from .base_cdc import BaseCdc
 
--- bhp066/apps/bcpp_stats/management/commands/export_as_csv.py
+++ bhp066/apps/bcpp_stats/management/commands/export_as_csv.py
@@ -1,37 +1,20 @@
-import csv
+from django.core.management.base import BaseCommand
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-
-from django.core.management.base import BaseCommand, CommandError
-from django.db.models import get_model
+from ...utils import export_model_as_csv
 
 
 class Command(BaseCommand):
-
-    args = '<model_name> <destination_path>'
+    """A management command to exports data from a model to a csv file."""
+    args = '<destination_path> <model_name>'
     help = 'Output data from a model to a destination file in CSV format'
     option_list = BaseCommand.option_list
 
-    def strip_underscore(self, item):
-        """Returns  a string stripped of a leading or tailing underscore."""
-        if item.startswith('_'):
-            item = item[1:]
-        if item.endswith('_'):
-            item = item[:-1]
-        return item
-
     def handle(self, *args, **options):
+        """Exports a model as CSV.
+
+        Usage: python manage.py export_as_csv  <source_model> <destination_path>."""
         model_name = args[0]
         destination_path = args[1]
-        excluded_fields = ['modified', 'created', 'user_created', 'user_modified', 'hostname_created', 'hostname_modified', '_community', '_title', '_description']
-        model = get_model('bcpp_stats', model_name)
-        values_row = [field.name for field in model._meta.fields if field.name not in excluded_fields]
-        header_row = [self.strip_underscore(x) for x in values_row]
-        print 'Writing data from {0} to file {1}'.format(model_name, destination_path)
-        with open(destination_path, 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(header_row)
-            for row in model.objects.values_list(*values_row).order_by('id'):
-                writer.writerow(row)
+        export_model_as_csv('bcpp_stats', model_name, destination_path, exclude_system_fields=True,
+                            additional_exclude_fields=['_community', '_title', '_description'])
         print 'Done.'
