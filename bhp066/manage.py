b6eb85b51dc69ea87b12402f0a14a93a61886340
erik van widenfelt <ew2789@gmail.com>
2014-08-17 16:20:11 +0200

    correct spelling error on import, edit manage.py for development.
    Former-commit-id: 50b058471eaaed07b543b4c692931ce8c0c19f47

--- bhp066/manage.py
+++ bhp066/manage.py
@@ -2,12 +2,14 @@
 import os
 import sys
 
+HOME = os.path.expanduser('~/')
+
 if __name__ == "__main__":
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-    sys.path.insert(1, '/Users/erikvw/source/edc_project/')
-    sys.path.insert(1, '/Users/erikvw/source/lis_project/') 
+    sys.path.insert(1, os.path.join(HOME, 'source/edc_project/'))
+    sys.path.insert(1, os.path.join(HOME, 'source/lis_project/'))
 
     from django.core.management import execute_from_command_line
 
