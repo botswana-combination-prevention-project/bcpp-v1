f2631327c7101a7dfbeed13aaa56933b8112b2be
melissa <mrbkew@gmail.com>
2014-08-19 14:13:24 +0200

    added validation for hiv_test_review
    Former-commit-id: 4f93770257bcc1f00dafe42189adabc1f5923bf7

--- bhp066/apps/bcpp_subject/forms/medical_diagnoses_form.py
+++ bhp066/apps/bcpp_subject/forms/medical_diagnoses_form.py
@@ -19,7 +19,7 @@ class MedicalDiagnosesForm (BaseSubjectModelForm):
         if 'Tubercolosis' in diagnoses_list and not cleaned_data.get('tb_record'):
             raise forms.ValidationError('If participant has ever been diagnosed with TB, is there a record available?')
         if 'None' in diagnoses_list and len(cleaned_data.get('diagnoses')) > 1:
-            raise forms.ValidationError('The diagnosis can not be None and other diagonosis at the same time.')
+            raise forms.ValidationError('The diagnosis can not be None and other diagnosis at the same time.')
 
         return cleaned_data
 
