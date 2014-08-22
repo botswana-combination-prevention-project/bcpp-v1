32f4dd52693e75c681f22c9fa64dc0d127ac3120
erik van widenfelt <ew2789@gmail.com>
2014-08-22 17:09:38 +0200

    more work on referral appt-date
    Former-commit-id: 9211ff48369aa5e9a2979712e6d4a726945ce74b

--- bhp066/apps/bcpp_subject/utils/next_clinic_date.py
+++ bhp066/apps/bcpp_subject/utils/next_clinic_date.py
@@ -10,7 +10,7 @@ def next_clinic_date(community_code, clinic, today=None, community_clinic_days=N
     community_clinic_days = community_clinic_days or settings.CLINIC_DAYS
     DAYS = community_clinic_days.get(community_code).get(clinic)
     clinic_dates = []
-    for DAY in DAYS:
+    for DAY in DAYS[0]:
         if today + relativedelta(weekday=DAY(+1)) != today:
             clinic_dates.append(today + relativedelta(weekday=DAY(+1)))
     return datetime(min(clinic_dates).year, min(clinic_dates).month, min(clinic_dates).day, 7, 30, 0)
