1b88c28ba3f93b8b61f6b66f72e05027f6419393
erik van widenfelt <ew2789@gmail.com>
2014-08-24 16:40:53 +0200

    minor, will diff after pull
    Former-commit-id: a6ac7715b0a4432de4e428c74d68fa7f28282cf8

--- bhp066/apps/bcpp_subject/tests/factories/subject_consent_factory.py
+++ bhp066/apps/bcpp_subject/tests/factories/subject_consent_factory.py
@@ -10,17 +10,21 @@ class SubjectConsentFactory(BaseConsentFactory):
     class Meta:
         model = SubjectConsent
 
+    # need to set these (if using more than one)
     household_member = factory.SubFactory(HouseholdMemberFactory)
+    gender = 'M'
+    dob = date(1980, 01, 01)
+    initials = 'XX'
+    # study_site =  # set the study site or a new site is created in the base factory!
+    # OK as defaults
     subject_identifier = None
     consent_datetime = datetime.today()
     may_store_samples = (('Yes', 'Yes'), ('No', 'No'))[0][0]
     is_literate = (('Yes', 'Yes'), ('No', 'No'))[0][0]
     consent_version_on_entry = 1
     consent_version_recent = 1
-    gender = 'M'
-    dob = date(1980, 01, 01)
     citizen = 'Yes'
-    is_verified = True
+    is_verified = False
     identity = factory.Sequence(lambda n: 'identity{0}'.format(n))
     identity_type = (('OMANG', 'Omang'), ('DRIVERS', "Driver's License"), ('PASSPORT', 'Passport'), ('OMANG_RCPT', 'Omang Receipt'), ('OTHER', 'Other'))[0][0]
     is_signed = True
