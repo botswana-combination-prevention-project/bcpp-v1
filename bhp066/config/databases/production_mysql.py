c458d0bf54ea1b8f3ab13e7302b826e0c610c306
erik van widenfelt <ew2789@gmail.com>
2014-08-25 22:52:36 +0200

    move mysql settings to a my.cnf file to prepare to keep in /etc

--- bhp066/config/databases/production_mysql.py
+++ bhp066/config/databases/production_mysql.py
@@ -1,24 +1,25 @@
+import os
+
+# this could be /etc/mysql/django/ for security
+PATH = os.path.expanduser('~/source/bhp066_project/bhp066/config/etc/')
+
 PRODUCTION_MYSQL = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB',
+            'read_default_file': os.path.join(PATH, 'default.cnf'),
         },
-        'NAME': 'bhp066',
-        'USER': 'root',
-        'PASSWORD': 'cc3721b',
         'HOST': '',
         'PORT': '',
+        'ATOMIC_REQUESTS': True,
     },
     'lab_api': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB',
+            'read_default_file': os.path.join(PATH, 'lab_api.cnf'),
         },
-        'NAME': 'lab',
-        'USER': 'root',
-        'PASSWORD': 'cc3721b',
         'HOST': '',
         'PORT': '',
+        'ATOMIC_REQUESTS': True,
     },
 }
