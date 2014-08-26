aec5d0c99c06917fe1e5cbed1da6b48f5d2cb62f
erik van widenfelt <ew2789@gmail.com>
2014-08-26 22:55:51 +0200

    add properties to referral helper from appt helper and ref in forms
    Former-commit-id: 583252e473bc0fc0d548c759f2d17fb2186353cc

--- bhp066/apps/bcpp_subject/forms/subject_referral_form.py
+++ bhp066/apps/bcpp_subject/forms/subject_referral_form.py
@@ -11,18 +11,15 @@ class SubjectReferralForm(BaseSubjectModelForm):
     def clean(self):
         cleaned_data = super(SubjectReferralForm, self).clean()
         subject_referral_helper = SubjectReferralHelper(SubjectReferral(**cleaned_data))
-        if not subject_referral_helper.validate_referral_appt_date(forms.ValidationError) and not cleaned_data.get('comment'):
-            raise forms.ValidationError('If you changed the default Referral Appointment Date, '
-                                        'then you must give a comment as to why?')
         if subject_referral_helper.missing_data:
             raise forms.ValidationError('Some data is missing for the referral. Complete \'{0}\' first '
                                         'and try again.'.format(subject_referral_helper.missing_data._meta.verbose_name))
-#         if subject_referral_helper.referral_code and not (
-#                 subject_referral_helper.referral_appt_datetime or subject_referral_helper.referral_clinic_type):
-#             raise forms.ValidationError('Need referral_code, referral_appt_date and referral_clinic_type to continue. '
-#                                         'Got {0}.'.format([subject_referral_helper.referral_code,
-#                                                           subject_referral_helper.referral_appt_datetime,
-#                                                           subject_referral_helper.referral_clinic_type]))
+        if subject_referral_helper.referral_code and not (
+                subject_referral_helper.referral_appt_datetime or subject_referral_helper.referral_clinic_type):
+            raise forms.ValidationError('Need referral_code, referral_appt_date and referral_clinic_type to continue. '
+                                        'Got {0}.'.format([subject_referral_helper.referral_code,
+                                                          subject_referral_helper.referral_appt_datetime,
+                                                          subject_referral_helper.referral_clinic_type]))
         return cleaned_data
 
     class Meta:
