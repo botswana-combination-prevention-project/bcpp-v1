a67ce613322c195178d7ff975c467834968b48b7
erik van widenfelt <ew2789@gmail.com>
2014-08-28 14:09:52 +0200

    add new variables to context
    Former-commit-id: 59cb12016fd4503da66328c733c56016a91c36bf

--- bhp066/config/settings.py
+++ bhp066/config/settings.py
@@ -176,7 +176,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.media",
                                "django.core.context_processors.static",
                                "django.core.context_processors.request",
-                               #"django.contrib.messages.context_processors.messages",
+                               "django.contrib.messages.context_processors.messages",
                                )
 
 ROOT_URLCONF = 'config.urls'
