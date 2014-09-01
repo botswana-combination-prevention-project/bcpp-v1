6178bc5bae430c8447954425198075baa0b50fd9
erik van widenfelt <ew2789@gmail.com>
2014-09-01 15:27:16 +0200

    use constants
    Former-commit-id: 5973f02cd9d469408b9f827e1123aa73e8e435bf

--- bhp066/apps/bcpp_household_member/forms/subject_htc_form.py
+++ bhp066/apps/bcpp_household_member/forms/subject_htc_form.py
@@ -3,6 +3,7 @@ from django import forms
 from ..models import SubjectHtc
 
 from edc.base.form.forms import BaseModelForm
+from edc.constants import NOT_APPLICABLE
 
 
 class SubjectHtcForm(BaseModelForm):
@@ -15,7 +16,7 @@ class SubjectHtcForm(BaseModelForm):
                 raise forms.ValidationError('You wrote HTC was accepted. A refusal reason is not applicable.')
             if cleaned_data.get('accepted') == 'No' and not cleaned_data.get('refusal_reason'):
                 raise forms.ValidationError('You wrote HTC was not accepted. A refusal reason is required.')
-            if cleaned_data.get('referred') == 'N/A':
+            if cleaned_data.get('referred') == NOT_APPLICABLE:
                 raise forms.ValidationError('Please indicate whether the subject was referred.')
             if cleaned_data.get('referred') == 'Yes' and not cleaned_data.get('referral_clinic'):
                 raise forms.ValidationError('Please indicate the referral clinic.')
@@ -25,15 +26,15 @@ class SubjectHtcForm(BaseModelForm):
                 raise forms.ValidationError('You wrote subject accepted HTC but declined testing. Please correct.')
 
         else:
-            if cleaned_data.get('accepted') != 'N/A':
+            if cleaned_data.get('accepted') != NOT_APPLICABLE:
                 raise forms.ValidationError('HTC was not offered HTC. Whether subject accepted is not applicable.')
             if cleaned_data.get('refusal_reason'):
                 raise forms.ValidationError('You wrote HTC was not offered. Refusal reason is not applicable.')
-            if cleaned_data.get('referred') != 'N/A':
+            if cleaned_data.get('referred') != NOT_APPLICABLE:
                 raise forms.ValidationError('HTC was not offered HTC. Whether subject was referred is not applicable.')
             if cleaned_data.get('referral_clinic'):
                 raise forms.ValidationError('HTC was not offered HTC. A referral clinic is not applicable.')
-            if cleaned_data.get('hiv_result') != 'N/A':
+            if cleaned_data.get('hiv_result') != NOT_APPLICABLE:
                 raise forms.ValidationError('HTC was not offered HTC. An HIV result is not applicable.')
         return cleaned_data
 
