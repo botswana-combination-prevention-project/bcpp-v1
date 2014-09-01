0c8c6c77a713459ad59e8e510e01f09cf63767bc
erik van widenfelt <ew2789@gmail.com>
2014-09-01 15:33:22 +0200

    add contrinute to context to add constants
    Former-commit-id: 82af26a407f7a0a33e1de11cc413af1fcf197300

--- bhp066/apps/bcpp_household_member/section/section_member_view.py
+++ bhp066/apps/bcpp_household_member/section/section_member_view.py
@@ -1,6 +1,8 @@
 from edc.dashboard.section.classes import BaseSectionView, site_sections
 from edc.map.classes import site_mappers
 
+from apps.bcpp_household.constants import CONFIRMED
+
 from ..search import MemberSearchByWord
 
 site_mappers.autodiscover()
@@ -9,9 +11,13 @@ site_mappers.autodiscover()
 class SectionMemberView(BaseSectionView):
     section_name = 'member'
     section_display_name = 'Members'
-    #add_model = HouseholdMember
     section_display_index = 30
     section_template = 'section_member.html'
     search = [MemberSearchByWord]
+
+    def contribute_to_context(self, context, request, *args, **kwargs):
+        """Users may override to update the template context with {key, value} pairs."""
+        context.update({'CONFIRMED': CONFIRMED})
+        return context
 
 site_sections.register(SectionMemberView)
