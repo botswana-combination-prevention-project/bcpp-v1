f04836352c4a8d18f00caac05c45b0ce81d82770
erik van widenfelt <ew2789@gmail.com>
2014-09-01 15:23:45 +0200

    use constants
    Former-commit-id: da98464f93d73873448f67d6d3c2d297aedd3080

--- bhp066/apps/bcpp_household/search/plot_search_by_word.py
+++ bhp066/apps/bcpp_household/search/plot_search_by_word.py
@@ -4,6 +4,7 @@ from edc.dashboard.search.classes import BaseSearchByWord
 
 from ..models import Plot
 from .base_search_by_mixin import BaseSearchByMixin
+from apps.bcpp_household.constants import CONFIRMED, UNCONFIRMED, RESIDENTIAL_HABITABLE, NON_RESIDENTIAL, RESIDENTIAL_NOT_HABITABLE
 
 
 class PlotSearchByWord(BaseSearchByMixin, BaseSearchByWord):
@@ -13,18 +14,23 @@ class PlotSearchByWord(BaseSearchByMixin, BaseSearchByWord):
     order_by = 'plot_identifier'
     template = 'search_plot_result_include.html'
 
+    def contribute_to_context(self, context):
+        context = super(PlotSearchByWord, self).contribute_to_context(context)
+        return context
+
     def get_qset_by_filter_keyword(self):
         """Returns a qset based on matching keyword.
 
         If you predefine keywords, the search term will be intercepted and used to select a query instead."""
         qset_filter = Q()
         qset_exclude = Q()
-        if self.get_search_term() in ['confirmed', '-confirmed', 'unconfirmed']:
+        if self.get_search_term() in [CONFIRMED, '-{}'.format(CONFIRMED), UNCONFIRMED]:
             if self.get_search_term()[0] == '-':
                 qset_exclude = Q(action=self.get_search_term()[1:])
             else:
                 qset_filter = Q(action=self.get_search_term())
-        if self.get_search_term() in ['occupied', '-occupied', 'vacant', 'non-residential']:
+        if self.get_search_term() in [RESIDENTIAL_HABITABLE, '-'.format(RESIDENTIAL_HABITABLE), 
+                                      RESIDENTIAL_NOT_HABITABLE, NON_RESIDENTIAL]:
             if self.get_search_term()[0] == '-':
                 qset_exclude = Q(status=self.get_search_term()[1:])
             else:
