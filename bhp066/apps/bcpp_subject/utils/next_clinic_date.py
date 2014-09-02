7f2531a579429e9c3736c5a1b60d1fc10cdc3dbc
erik van widenfelt <ew2789@gmail.com>
2014-09-02 03:58:48 +0200

    add feature to allow next-appt to calculate backwards to a clinic day, update tests. POS-PR ... referred to IDCC
    Former-commit-id: ff19cc9b3bfade11fae02b6c7149754a99ad578d

--- bhp066/apps/bcpp_subject/utils/next_clinic_date.py
+++ bhp066/apps/bcpp_subject/utils/next_clinic_date.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-def next_clinic_date(community_clinic_days, base_datetime=None, allow_same_day=None):
+def next_clinic_date(community_clinic_days, base_datetime=None, allow_same_day=None, subtract=None):
     """Returns next clinic date that is not today or None.
 
     community_clinic_days format is a ClinicDaysTuple. See bcpp_household.mappers for format.
@@ -18,4 +18,15 @@ def next_clinic_date(community_clinic_days, base_datetime=None, allow_same_day=N
             elif base_datetime + relativedelta(weekday=DAY(+1)) != base_datetime:
                 clinic_dates.append(base_datetime + relativedelta(weekday=DAY(+1)))
         next_clinic_datetime = datetime(min(clinic_dates).year, min(clinic_dates).month, min(clinic_dates).day, 7, 30, 0)
+        if subtract:
+            # work back to a clinic day, e.g the nearest clinic day within two weeks
+            days = list(community_clinic_days.days)
+            days.reverse()
+            base_datetime = datetime(base_datetime.year, base_datetime.month, base_datetime.day, 7, 30, 0)
+            for DAY in days:
+                next_clinic_datetime = next_clinic_datetime + relativedelta(weekday=DAY(-1))
+                if allow_same_day and next_clinic_datetime == base_datetime:
+                    break
+                elif next_clinic_datetime < base_datetime:
+                    break
     return next_clinic_datetime
