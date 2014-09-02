a6d5deb8eeae74f0e4c841df804c2e4b38444e9e
erik van widenfelt <ew2789@gmail.com>
2014-09-03 01:55:36 +0200

    get timepointstatus to generated a referral on CLOSED
    Former-commit-id: 528363e3ec4c8d7ffec934335c6f2de2a9204280

--- bhp066/apps/bcpp_subject/models/signals.py
+++ bhp066/apps/bcpp_subject/models/signals.py
@@ -1,15 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.bcpp_household_member.models import HouseholdMember
+from edc.core.bhp_data_manager.models import TimePointStatus
+from edc.constants import CLOSED
 
 from .subject_consent import SubjectConsent
 
 from ..classes import SubjectReferralHelper
 
-from ..models import SubjectReferral
-
-from apps.bcpp_household.models import HouseholdStructure
+from ..models import SubjectReferral, SubjectVisit
 
 
 @receiver(post_save, weak=False, dispatch_uid='subject_consent_on_post_save')
@@ -29,6 +28,7 @@ def subject_consent_on_post_save(sender, instance, raw, created, using, **kwargs
                 instance.post_save_update_registered_subject(using)
                 instance.household_member.is_consented = True
                 instance.household_member.save(using=using, update_fields=['is_consented'])
+                # update household_structure if enrolled
                 instance.household_member.household_structure.enrolled = True
                 instance.household_member.household_structure.enrolled_household_member = instance.pk
                 instance.household_member.household_structure.enrolled_datetime = instance.consent_datetime
@@ -36,18 +36,57 @@ def subject_consent_on_post_save(sender, instance, raw, created, using, **kwargs
                                                                    update_fields=['enrolled',
                                                                                   'enrolled_household_member',
                                                                                   'enrolled_datetime'])
+                # update household if enrolled
+                instance.household_member.household_structure.household.enrolled = True
+                instance.household_member.household_structure.household.enrolled_datetime = \
+                    instance.consent_datetime
+                instance.household_member.household_structure.household.save(
+                    using=using, update_fields=['enrolled', 'enrolled_datetime'])
+                # update plot if enrolled
+                instance.household_member.household_structure.household.plot.bhs = True
+                instance.household_member.household_structure.household.plot.enrolled_datetime = \
+                    instance.consent_datetime
+                instance.household_member.household_structure.household.plot.save(
+                    using=using, update_fields=['bhs', 'enrolled_datetime'])
 
 
 @receiver(post_save, weak=False, dispatch_uid='update_subject_referral_on_post_save')
 def update_subject_referral_on_post_save(sender, instance, raw, created, using, **kwargs):
-    """Updates the subject referral if a model that is part of the referral data is changed."""
+    """Updates the subject referral if a model that is part of the referral data is changed.
+
+    The sender classes are listed in the SubjectReferralHelper."""
     if not raw:
         try:
-            subject_visit = instance.visit
-            if instance.__class__ in SubjectReferralHelper(instance).models.values():
-                subject_referral = SubjectReferral.objects.using(using).get(subject_visit=subject_visit)
+            if sender in SubjectReferralHelper.models.values():
+                subject_referral = SubjectReferral.objects.using(using).get(subject_visit=instance.visit)
                 subject_referral.save(using=using)
         except AttributeError:
             pass
         except SubjectReferral.DoesNotExist:
             pass
+
+
+@receiver(post_save, weak=False, dispatch_uid='time_point_status_on_post_save')
+def time_point_status_on_post_save(sender, instance, raw, created, using, **kwargs):
+    """Attempt to save the subject referral to refer participants that 
+    do not complete data collection (partial participation)."""
+    if not raw:
+        if isinstance(instance, (TimePointStatus, )):
+            if instance.status == CLOSED:
+                try:
+                    # subject visit should always exist
+                    subject_visit = SubjectVisit.objects.get(appointment=instance.appointment)
+                    # refresh an existing referral document
+                    subject_referral = SubjectReferral.objects.using(using).get(
+                        subject_visit=subject_visit)
+                    subject_referral.comment = '{} ({})'.format(
+                        subject_referral.comment,
+                        'Partial participation. Auto generated when time point closed.')
+                    subject_referral.save(using=using)
+                except SubjectReferral.DoesNotExist:
+                    # create a new document
+                    SubjectReferral.objects.using(using).create(
+                        subject_visit=subject_visit,
+                        subject_referred='No',
+                        comment='(Partial participation. Auto generated when time point closed.)'
+                        )
