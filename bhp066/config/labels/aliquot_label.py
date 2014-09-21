a4d119aa65fb19a28eece4115fb5f93c8a3cdab0
erik van widenfelt <ew2789@gmail.com>
2014-09-21 13:49:48 +0200

    fix plot allow_enrollment, reconfig dajaxice, remove bhp_nmap
    Former-commit-id: d1bbde58f016bfee5e312cdef40b70802a064087

--- bhp066/config/labels/aliquot_label.py
+++ bhp066/config/labels/aliquot_label.py
@@ -5,12 +5,12 @@ from lis.labeling.classes import ZplTemplateTuple
 aliquot_label = ZplTemplateTuple(
     'aliquot_label', (
         ('^XA\n'
-         '^FO270,15^A0N,20,20^FD${protocol} Site ${site} ${clinician_initials}   ${aliquot_type} ${aliquot_count}${primary}^FS\n'
-         '^FO270,34^BY1,3.0^BCN,50,N,N,N\n'
+         '^FO260,15^A0N,20,20^FD${protocol} Site ${site} ${clinician_initials}   ${aliquot_type} ${aliquot_count}${primary}^FS\n'
+         '^FO260,34^BY1,3.0^BCN,50,N,N,N\n'
          '^BY^FD${aliquot_identifier}^FS\n'
-         '^FO270,92^A0N,20,20^FD${aliquot_identifier}^FS\n'
-         '^FO270,112^A0N,20,20^FD${subject_identifier} (${initials})^FS\n'
-         '^FO270,132^A0N,20,20^FDDOB: ${dob} ${gender}^FS\n'
-         '^FO270,152^A0N,25,20^FD${drawn_datetime}^FS\n'
+         '^FO260,92^A0N,20,20^FD${aliquot_identifier}^FS\n'
+         '^FO260,112^A0N,20,20^FD${subject_identifier} (${initials})^FS\n'
+         '^FO260,132^A0N,20,20^FDDOB: ${dob} ${gender}^FS\n'
+         '^FO260,152^A0N,25,20^FD${drawn_datetime}^FS\n'
          '^XZ')
     ), True)
