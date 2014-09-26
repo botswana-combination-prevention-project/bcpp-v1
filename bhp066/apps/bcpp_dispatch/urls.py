9f812a84aea0eab25e493f54ef906fdb75406be6
erik van widenfelt <ew2789@gmail.com>
2014-09-26 15:01:05 +0200

    replacement_helper, pep8, load producers, change views to use django messages, replaceable is not NullBoolean
    Former-commit-id: 953caa728984a99715c58c96c8bc68f4449633ab

--- bhp066/apps/bcpp_dispatch/urls.py
+++ bhp066/apps/bcpp_dispatch/urls.py
@@ -1,5 +1,15 @@
 from django.conf.urls import patterns, url
+
+from edc.device.sync.exceptions import ProducerError
+from edc.device.sync.utils import load_producer_db_settings
+
 from .views import load_producer_databases
+
+
+try:
+    load_producer_db_settings()
+except ProducerError:
+    pass
 
 urlpatterns = patterns('apps.bcpp_dispatch.views',
     url(r'^play_transactions/', 'play_transactions', name='bccp_dispatch_play_url'),
