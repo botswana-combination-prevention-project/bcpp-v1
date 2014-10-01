586fc8514e54014a8c2517709601df6eadbd1198
erik van widenfelt <ew2789@gmail.com>
2014-10-01 09:23:18 +0200

    minor fix on dispatch/sync views, reverse template urls
    Former-commit-id: 7d50fa4cfe8f4a43aa74b01072132e5a266d1d07

--- bhp066/apps/bcpp_dispatch/urls.py
+++ bhp066/apps/bcpp_dispatch/urls.py
@@ -14,7 +14,7 @@ except ProducerError:
 urlpatterns = patterns('apps.bcpp_dispatch.views',
     url(r'^play_transactions/', 'play_transactions', name='bccp_dispatch_play_url'),
     url(r'^sync/(?P<selected_producer>[a-z0-9\-\_\.]+)/', 'bcpp_sync', name='bccp_sync_producer_url'),
-    url(r'^sync/', 'bcpp_sync', name='bccp_sync_url'),
+    url(r'^sync/', 'bcpp_sync', name='bcpp_sync_url'),
     url(r'^load_producer_databases/$', load_producer_databases, name='load_producer_databases_url'),
     url(r'^', 'bcpp_dispatch_view', name='bccp_dispatch_url'),
     )
