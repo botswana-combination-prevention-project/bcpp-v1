479cb44f8d30534052c8c62dd4ba9f37a27b15bb
erik van widenfelt <ew2789@gmail.com>
2014-10-11 12:10:26 +0200

    increate plot radius
    Former-commit-id: fd1905ded1e4ad59f2460b601c005df8cd716587

---
+++ bhp066/apps/bcpp_household/filters/action_filter.py
@@ -0,0 +1,18 @@
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
+from apps.bcpp_household.constants import CONFIRMED, UNCONFIRMED
+
+
+class ActionFilter(SimpleListFilter):
+
+    title = _('action')
+    parameter_name = 'action'
+
+    def lookups(self, request, model_admin):
+        return ((CONFIRMED, CONFIRMED), (UNCONFIRMED, UNCONFIRMED), )
+
+    def queryset(self, request, queryset):
+        if self.value() == CONFIRMED:
+            return queryset.filter(plot__action=CONFIRMED)
+        if self.value() == UNCONFIRMED:
+            return queryset.filter(plot__action=UNCONFIRMED)
