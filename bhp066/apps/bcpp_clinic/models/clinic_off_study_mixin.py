655465d6556e6123d7651366d4fd72def6509bc0
melissa kewagamang <mrbkew@gmail.com>
2014-10-14 10:29:17 +0200

    remove old clinic code
    Former-commit-id: b5248499a813770264f4e0bcb9a52ff47179ab27

--- bhp066/apps/bcpp_clinic/models/clinic_off_study_mixin.py
+++
@@ -1,8 +0,0 @@
-from edc.subject.off_study.mixins import OffStudyMixin
-from .clinic_off_study import ClinicOffStudy
-
-
-class ClinicOffStudyMixin(OffStudyMixin):
-
-    def get_off_study_cls(self):
-        return ClinicOffStudy
