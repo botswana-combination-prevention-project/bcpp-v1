651f6bcfe9eb206cc60f52f71e3db82a54ac3105
melissa kewagamang <mrbkew@gmail.com>
2014-10-21 16:08:09 +0200

    update suject_type, dashboard_type, dashboard_type_list + template
    Former-commit-id: b730c8e798e0cded502f454bc338628a957cfbe2

--- bhp066/apps/bcpp_clinic_dashboard/urls.py
+++ bhp066/apps/bcpp_clinic_dashboard/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .classes import ClinicDashboard
 
 regex = {}
-regex['dashboard_type'] = 'subject'
+regex['dashboard_type'] = 'clinic'
 regex['dashboard_model'] = 'clinic_consent'
 urlpatterns = ClinicDashboard.get_urlpatterns('apps.bcpp_clinic_dashboard.views', regex, visit_field_names=['clinic_visit', ])
 
