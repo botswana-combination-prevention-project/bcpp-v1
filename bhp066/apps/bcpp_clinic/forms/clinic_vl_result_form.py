1861b3657a4a05d75a0ba2a6431bf64300496235
melissa kewagamang <mrbkew@gmail.com>
2014-10-23 08:07:49 +0200

    add missing lab_clinic_api panel + update visit_schedule

--- bhp066/apps/bcpp_clinic/forms/clinic_vl_result_form.py
+++ bhp066/apps/bcpp_clinic/forms/clinic_vl_result_form.py
@@ -12,7 +12,7 @@ class ClinicVLResultForm (BaseModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        vl_requisition_metadata = RequisitionMetaData.objects.get(appointment=cleaned_data.get('clinic_visit').appointment, lab_entry=LabEntry.objects.get(requisition_panel__name='Viral Load Clinic'))
+        vl_requisition_metadata = RequisitionMetaData.objects.get(appointment=cleaned_data.get('clinic_visit').appointment, lab_entry=LabEntry.objects.get(requisition_panel__name='Viral Load (clinic)'))
         if vl_requisition_metadata.entry_status == NEW:
             raise forms.ValidationError('If a Viral Load Clinic Requisition is required, then you have to fill it before this form.')
 
