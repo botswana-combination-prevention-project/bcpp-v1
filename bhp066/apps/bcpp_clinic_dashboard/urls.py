0bcefb3d94a8df04e5831566e3ac6f307b0a7f92
melissa kewagamang <mrbkew@gmail.com>
2014-10-29 16:17:14 +0200

    update dashboard_type on dashboard urls
    Former-commit-id: 5076d5a38aa77e2262028b00d44f67c355149def

--- bhp066/apps/bcpp_clinic_dashboard/urls.py
+++ bhp066/apps/bcpp_clinic_dashboard/urls.py
@@ -1,13 +1,6 @@
-from django.conf.urls import patterns, url
-
 from .classes import ClinicDashboard
 
 regex = {}
 regex['dashboard_type'] = 'clinic'
 regex['dashboard_model'] = 'clinic_eligibility'
 urlpatterns = ClinicDashboard.get_urlpatterns('apps.bcpp_clinic_dashboard.views', regex, visit_field_names=['clinic_visit', ])
-
-# regex = {}
-# regex['dashboard_type'] = 'subject'
-# regex['dashboard_model'] = 'household_member'
-# urlpatterns = SubjectDashboard.get_urlpatterns('apps.bcpp_dashboard.views', regex, visit_field_names=['subject_visit', ])
