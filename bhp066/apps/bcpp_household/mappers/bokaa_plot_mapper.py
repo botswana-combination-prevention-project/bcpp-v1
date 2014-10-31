45dac455f56c909271d26b982986f81db48c0dfd
SirOne Pharatlhatlhe <sirone@One-2.local>
2014-10-31 13:15:35 +0200

    validation of bcpp key dates against survey dates
    Former-commit-id: 3d4ee13321d18efe062b7eff229f338ffbba6466

--- bhp066/apps/bcpp_household/mappers/bokaa_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/bokaa_plot_mapper.py
@@ -1,6 +1,11 @@
+from dateutil.relativedelta import MO, TU, WE, TH, FR
+
+from datetime import date
 from edc.map.classes import site_mappers
 from .base_plot_mapper import BasePlotMapper
 from .choices import SECTIONS, SUB_SECTIONS, BOKAA_LANDMARKS
+
+from ..utils import ClinicDaysTuple
 
 
 class BokaaPlotMapper(BasePlotMapper):
@@ -18,5 +23,16 @@ class BokaaPlotMapper(BasePlotMapper):
     gps_center_lon = 26.021626
     radius = 5.5
     location_boundary = ()
+
+    bhs_start_date = date(2014, 11, 5)
+    bhs_full_enrollment_date = date(2014, 11, 28)
+    bhs_end_date = date(2014, 12, 20)
+    smc_start_date = date(2014, 12, 5)  # referenced by bcpp mappers
+
+    clinic_days = {
+        'IDCC': ClinicDaysTuple((WE, ), None),
+        'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+        'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+        'SMC': ClinicDaysTuple((FR, ), smc_start_date)}
 
 site_mappers.register(BokaaPlotMapper)
