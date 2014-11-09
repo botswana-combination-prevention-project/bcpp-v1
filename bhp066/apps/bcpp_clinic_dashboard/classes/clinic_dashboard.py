a1908833309da24f0c50d2e6ff9e73b2c825bd99
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:01:54 +0200

    slight improvements
    Former-commit-id: 3b554b9fe792b2b5f0bd715bb3d739755137f942

--- bhp066/apps/bcpp_clinic_dashboard/classes/clinic_dashboard.py
+++ bhp066/apps/bcpp_clinic_dashboard/classes/clinic_dashboard.py
@@ -1,10 +1,10 @@
-import re
+# import re
+
+from edc.dashboard.subject.classes import RegisteredSubjectDashboard
+# from edc.subject.registration.models import RegisteredSubject
 
 from apps.bcpp_clinic.models import ClinicConsent, ClinicVisit, ClinicSubjectLocator, ClinicEligibility
 from apps.bcpp_lab.models import ClinicRequisition, PackingList
-
-from edc.dashboard.subject.classes import RegisteredSubjectDashboard
-from edc.subject.registration.models import RegisteredSubject
 
 
 class ClinicDashboard(RegisteredSubjectDashboard):
@@ -14,17 +14,15 @@ class ClinicDashboard(RegisteredSubjectDashboard):
     def __init__(self, *args, **kwargs):
         self.subject_dashboard_url = 'subject_dashboard_url'
         self.dashboard_type_list = ['clinic']
-        kwargs.update({'dashboard_models': {'clinic_eligibility': ClinicEligibility}, 'membership_form_category': 'consenting'})
+        kwargs.update({'dashboard_models': {'clinic_eligibility': ClinicEligibility},
+                       'membership_form_category': 'consenting',
+                       })
+        self._registered_subject = None
         self._requisition_model = ClinicRequisition
         self.visit_model = ClinicVisit
         self._locator_model = ClinicSubjectLocator
-        self._registered_subject = None
         self.extra_url_context = ""
         super(ClinicDashboard, self).__init__(*args, **kwargs)
-#         self._locator_model = None
-#         self._registered_subject = None
-#         self.extra_url_context = ""
-#         super(ClinicDashboard, self).__init__(*args, **kwargs)
 
     def add_to_context(self):
         super(ClinicDashboard, self).add_to_context()
@@ -36,6 +34,13 @@ class ClinicDashboard(RegisteredSubjectDashboard):
             clinic_consent=self.consent,
             )
 
+    def set_registered_subject(self, pk=None):
+        # try:
+        self._registered_subject = ClinicEligibility.objects.get(pk=self.dashboard_id).household_member.registered_subject
+        # except ClinicEligibility.DoesNotExist:
+        #    pass
+        #    # self._registered_subject = RegisteredSubject.objects.get(pk=pk)
+
     @property
     def consent(self):
         """Returns to the subject consent, if it has been completed."""
@@ -43,11 +48,6 @@ class ClinicDashboard(RegisteredSubjectDashboard):
         if ClinicConsent.objects.filter(subject_identifier=self.subject_identifier):
             self._consent = ClinicConsent.objects.get(subject_identifier=self.subject_identifier)
         return self._consent
-
-    def set_registered_subject(self, pk=None):
-        self._registered_subject = self.registered_subject
-        if RegisteredSubject.objects.filter(subject_identifier=self.subject_identifier):
-            self._registered_subject = RegisteredSubject.objects.get(subject_identifier=self.subject_identifier)
 
     def set_membership_form_category(self):
         self._membership_form_category = self.membership_form_category
@@ -70,37 +70,3 @@ class ClinicDashboard(RegisteredSubjectDashboard):
 
     def render_labs(self, update=False):
         return ''
-
-    @property
-    def registered_subject(self):
-        return self._registered_subject
-
-    @registered_subject.setter
-    def registered_subject(self, pk=None):
-        self._registered_subject = None
-        print self.dashboard_id
-        self.set_registered_subject(pk)
-        re_pk = re.compile('[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}')
-        if not self._registered_subject:
-            if not self._registered_subject and self.dashboard_model == RegisteredSubject or self.dashboard_model == ClinicEligibility:
-                try:
-                    self._registered_subject = RegisteredSubject.objects.get(registration_identifier=self.dashboard_id)
-                except RegisteredSubject.DoesNotExist:
-                    try:
-                        self._registered_subject = RegisteredSubject.objects.get(pk=self.dashboard_id)
-                    except RegisteredSubject.DoesNotExist:
-                        pass
-            elif not self._registered_subject and 'get_registered_subject' in dir(self.dashboard_model):
-                self._registered_subject = self.dashboard_model_instance.registered_subject
-            elif not self._registered_subject and re_pk.match(str(pk)):
-                self._registered_subject = RegisteredSubject.objects.get(pk=pk)
-            elif not self._registered_subject and self.appointment:
-                # can i get it from an appointment? TODO: is this even possible?
-                self._registered_subject = self.appointment.registered_subject
-            elif self._registered_subject:
-                if not isinstance(self._registered_subject, RegisteredSubject):
-                    raise TypeError('Expected instance of RegisteredSubject. See {0}'.format(self))
-            else:
-                pass
-        if not self._registered_subject:
-            raise TypeError('Attribute \'_registered_subject\' may not be None. Perhaps add method registered_subject to the model {0}. See {1}'.format(self.dashboard_model, self))
