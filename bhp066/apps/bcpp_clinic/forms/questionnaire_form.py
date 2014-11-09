192f056ceadc107a333f3cf5c9378cd7e7c1ab69
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:00:59 +0200

    minor edits
    Former-commit-id: 98d1c1ba1db7e35258055f10e3c6266c9589d469

--- bhp066/apps/bcpp_clinic/forms/questionnaire_form.py
+++ bhp066/apps/bcpp_clinic/forms/questionnaire_form.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from ..models import Questionnaire
+
 from .base_clinic_model_form import BaseClinicModelForm
 
 
@@ -11,16 +12,13 @@ class QuestionnaireForm (BaseClinicModelForm):
         cleaned_data = super(QuestionnaireForm, self).clean()
 
         if cleaned_data.get('knows_last_cd4', None) == 'Yes' and not cleaned_data.get('cd4_count', None):
-            raise forms.ValidationError('Participant answered that they know their last CD4 value, please provide this value.')
+            raise forms.ValidationError('Participant answered that they know their last '
+                                        'CD4 value, please provide this value.')
 
         if cleaned_data.get('knows_last_cd4', None) != 'Yes' and cleaned_data.get('cd4_count', None):
-            raise forms.ValidationError('Participant answered \'{0}\' to knowledge of their CD4 result. Do not provide this value.'.format(cleaned_data.get('knows_last_cd4', None)))
-
-#         if cleaned_data.get('other_identifiers', None) != 'None' and not cleaned_data.get('htc_and_or_pims', None):
-#             raise forms.ValidationError('Participant reports to have either HTC, PIMS identifier or both. Please enter that identifier.')
-#
-#         if cleaned_data.get('other_identifiers', None) == 'None' and cleaned_data.get('htc_and_or_pims', None):
-#             raise forms.ValidationError('Participant reports to NOT have either HTC, PIMS identifier or both. Please DO NOT enter any identifier.')
+            raise forms.ValidationError('Participant answered \'{0}\' to knowledge of '
+                                        'their CD4 result. Do not provide this value.'.format(
+                                            cleaned_data.get('knows_last_cd4', None)))
 
         return cleaned_data
 
