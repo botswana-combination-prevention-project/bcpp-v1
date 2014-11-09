3ed8c2ab50e251510af83814184f30d38f4e95ca
erik van widenfelt <ew2789@gmail.com>
2014-11-09 21:57:33 +0200

    link models to household_member, signal creates plot for clinic.

--- bhp066/apps/bcpp_clinic/models/questionnaire.py
+++ bhp066/apps/bcpp_clinic/models/questionnaire.py
@@ -1,35 +1,18 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import ugettext as _
-from south.migration import base
-
-from .base_clinic_visit_model import BaseClinicVisitModel
-
-from .clinic_visit import ClinicVisit
-
-from apps.clinic.choices import YES_NO_DWTA
 
 from edc.audit.audit_trail import AuditTrail
 from edc.entry_meta_data.managers import EntryMetaDataManager
 
+from apps.clinic.choices import YES_NO_DWTA
+
+from .base_clinic_visit_model import BaseClinicVisitModel
+from .clinic_visit import ClinicVisit
+
 
 class Questionnaire (BaseClinicVisitModel):
-
-#     other_identifiers = models.CharField(
-#         verbose_name=_("Does the participant have one of these identification numbers?"),
-#         max_length=25,
-#         choices=OTHER_IDENTIFIERS,
-#         help_text="",
-#         )
-#
-#     htc_and_or_pims = models.CharField(
-#         verbose_name=_("Enter the HTC and or PIMS identifiers(comma separated)?"),
-#         max_length=50,
-#         null=True,
-#         blank=True,
-#         help_text="htc_identifier, pims_identifier",
-#         )
-
+    """A model completed by the user that captures ARV and CD4 data."""
     on_arv = models.CharField(
         verbose_name=_("Are you currently taking antiretroviral therapy (ARVs)?"),
         max_length=25,
