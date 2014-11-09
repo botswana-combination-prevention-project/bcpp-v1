a6a75f14dc3936801d10f08548e7f2a88be7a5ff
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:03:08 +0200

    add survey dates and clinic days as dict with key survey, add additional methods for clinic query
    Former-commit-id: 56945f7050afe2e38429a146fa68b59bd0a26326

--- bhp066/apps/bcpp_household/mappers/letlhakeng_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/letlhakeng_plot_mapper.py
@@ -1,13 +1,12 @@
 from datetime import date
 from dateutil.relativedelta import MO, TU, WE, TH, FR
 
-from django.conf import settings
-
 from edc.map.classes import site_mappers
 
 from .base_plot_mapper import BasePlotMapper
 from .choices import SECTIONS, SUB_SECTIONS, LETLHAKENG_LANDMARKS
-from ..utils import ClinicDaysTuple
+
+from ..utils import ClinicDaysTuple, SurveyDatesTuple
 
 
 class LetlhakengPlotMapper(BasePlotMapper):
@@ -25,7 +24,8 @@ class LetlhakengPlotMapper(BasePlotMapper):
     location_boundary = ()
 
     intervention = False
-    survey_dates = {'bcpp-year-1': dict(
+
+    survey_dates = {'bcpp-year-1': SurveyDatesTuple(
         bhs_start_date=date(2014, 9, 5),
         bhs_full_enrollment_date=date(2014, 10, 15),
         bhs_end_date=date(2014, 10, 21),
@@ -35,7 +35,7 @@ class LetlhakengPlotMapper(BasePlotMapper):
         'IDCC': ClinicDaysTuple((TU, TH), None),
         'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
         'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
-        'SMC': ClinicDaysTuple((WE, ), survey_dates['bcpp-year-1'].get('smc_start_date'))
+        'SMC': ClinicDaysTuple((WE, ), survey_dates['bcpp-year-1'].smc_start_date)
         }
     }
 
