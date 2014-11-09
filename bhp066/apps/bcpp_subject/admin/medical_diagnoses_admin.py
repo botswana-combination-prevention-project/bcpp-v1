129bdfac408b0bc331831e2f2b9db147346252da
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:16:15 +0200

    pep8
    Former-commit-id: 59743f131ab3ecb4ff9c902c60c2fbebeb7e0d13

--- bhp066/apps/bcpp_subject/admin/medical_diagnoses_admin.py
+++ bhp066/apps/bcpp_subject/admin/medical_diagnoses_admin.py
@@ -11,27 +11,22 @@ class MedicalDiagnosesAdmin(SubjectVisitModelAdmin):
 
     form = MedicalDiagnosesForm
     fields = (
-        "subject_visit",
-       'diagnoses',
-       'heart_attack_record',
-#        'heart_attack_comment',
-       'cancer_record',
-#        'cancer_record_comment',
-#        'sti_record',
-       'tb_record',
-#        'tb_record_comment',
-       )
+        'subject_visit',
+        'diagnoses',
+        'heart_attack_record',
+        'cancer_record',
+        'tb_record',
+        )
     radio_fields = {
         "heart_attack_record": admin.VERTICAL,
         "cancer_record": admin.VERTICAL,
-#         "sti_record": admin.VERTICAL,
         "tb_record": admin.VERTICAL, }
     filter_horizontal = ('diagnoses',)
     instructions = [_("Read to Participant: I am now going to ask you"
-                             " some questions about major illnesses that you may"
-                             " have had in the past 12 months. Sometimes people"
-                             " call different sicknesses by different names."
-                             " If you do not understand what I mean, please ask."
-                             " Also, please remember that your answers will be"
-                             " kept confidential.")]
+                      " some questions about major illnesses that you may"
+                      " have had in the past 12 months. Sometimes people"
+                      " call different sicknesses by different names."
+                      " If you do not understand what I mean, please ask."
+                      " Also, please remember that your answers will be"
+                      " kept confidential.")]
 admin.site.register(MedicalDiagnoses, MedicalDiagnosesAdmin)
