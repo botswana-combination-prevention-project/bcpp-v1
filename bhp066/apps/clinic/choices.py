fef4ec0107c369b7ce91c64d53a5bf0c71620031
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:18:48 +0200

    remove reference to survey_clinic_app_configuration
    Former-commit-id: 71d9f6ea3a45c64769baba060f018e7376a7a23f

--- bhp066/apps/clinic/choices.py
+++ bhp066/apps/clinic/choices.py
@@ -2,53 +2,38 @@ from django.utils.translation import ugettext as _
 
 
 COMMUNITIES = (
-        ('Bokaa', _('Bokaa')),
-        ('Digawana', _('Digawana')),
-        ('Gumare', _('Gumare')),
-        ('Gweta', _('Gweta')),
-        ('Lentsweletau', _('Lentsweletau')),
-        ('Lerala', _('Lerala')),
-        ('Letlhakeng', _('Letlhakeng')),
-        ('Mandunyane', _('Mandunyane')),
-        ('Mankgodi', _('Mankgodi')),
-        ('Mmadinare', _('Mmadinare')),
-        ('Mmathethe', _('Mmathethe')),
-        ('Masunga', _('Masunga')),
-        ('Maunatlala', _('Maunatlala')),
-        ('Mathangwane', _('Mathangwane')),
-        ('Metsimotlhabe', _('Metsimotlhabe')),
-        ('Molapowabojang', _('Molapowabojang')),
-        ('Nata', _('Nata')),
-        ('Nkange', _('Nkange')),
-        ('Oodi', _('Oodi')),
-        ('Otse', _('Otse')),
-        ('Raikops', _('Raikops')),
-        ('Ramokgonami', _('Ramokgonami')),
-        ('Ranaka', _('Ranaka')),
-        ('Sebina', _('Sebina')),
-        ('Sefare', _('Sefare')),
-        ('Sefophe', _('Sefophe')),
-        ('Shakawe', _('Shakawe')),
-        ('Shoshong', _('Shoshong')),
-        ('Tati Siding', _('Tati Siding')),
-        ('Tsetsebjwe', _('Tsetsebjwe')),
-        ('OTHER', _('Other non study community')),
-    )
-
-
-VERBALHIVRESULT_CHOICE = (
-        ('POS', _('HIV Positive')),
-        ('NEG', _('HIV Negative')),
-        ('UNK', _('I am not sure')),
-        ('not_answering', _('Don\'t want to answer')),
-    )
-
-INABILITY_TO_PARTICIPATE_REASON = (
-           ('Mental Incapacity', _('Mental Incapacity')),
-           ('Deaf/Mute', _('Deaf/Mute')),
-           ('Too sick to talk', _('Too sick to talk')),
-           ('None', _('None')),
-        )
+    ('Bokaa', _('Bokaa')),
+    ('Digawana', _('Digawana')),
+    ('Gumare', _('Gumare')),
+    ('Gweta', _('Gweta')),
+    ('Lentsweletau', _('Lentsweletau')),
+    ('Lerala', _('Lerala')),
+    ('Letlhakeng', _('Letlhakeng')),
+    ('Mandunyane', _('Mandunyane')),
+    ('Mankgodi', _('Mankgodi')),
+    ('Mmadinare', _('Mmadinare')),
+    ('Mmathethe', _('Mmathethe')),
+    ('Masunga', _('Masunga')),
+    ('Maunatlala', _('Maunatlala')),
+    ('Mathangwane', _('Mathangwane')),
+    ('Metsimotlhabe', _('Metsimotlhabe')),
+    ('Molapowabojang', _('Molapowabojang')),
+    ('Nata', _('Nata')),
+    ('Nkange', _('Nkange')),
+    ('Oodi', _('Oodi')),
+    ('Otse', _('Otse')),
+    ('Raikops', _('Raikops')),
+    ('Ramokgonami', _('Ramokgonami')),
+    ('Ranaka', _('Ranaka')),
+    ('Sebina', _('Sebina')),
+    ('Sefare', _('Sefare')),
+    ('Sefophe', _('Sefophe')),
+    ('Shakawe', _('Shakawe')),
+    ('Shoshong', _('Shoshong')),
+    ('Tati Siding', _('Tati Siding')),
+    ('Tsetsebjwe', _('Tsetsebjwe')),
+    ('OTHER', _('Other non-study community')),
+)
 
 YES_NO_DWTA = (
     ('Yes', _('Yes')),
@@ -62,7 +47,7 @@ OTHER_IDENTIFIERS = (
     ('Pims identifier', 'Pims identifier'),
     ('Htc_Pims', 'Htc and Pims identifier'),
     ('Barcode_Pims', 'Barcode and Pims identifier'),
-    ('None', 'None'),
+    ('N/A', 'Not Applicable'),
     )
 
 VISIT_UNSCHEDULED_REASON = (
@@ -76,7 +61,7 @@ VISIT_REASON = (
     ('Initiation Visit', _('Initiation Visit')),
     ('MASA Scheduled VL Visit', _('MASA Scheduled Viral Load Visit')),
     ('CCC visit', _('CCC Enrolment Visit')),
-    ('Other NON-VL Visit', _('Other NON-Viral Load Visit'))
+    ('Other non-VL Visit', _('Other non-Viral Load Visit'))
     )
 
 GENDER_UNDETERMINED = (
