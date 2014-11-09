251dee1fa3deb71f6bf1b12b64a255e2a5226e93
erik van widenfelt <ew2789@gmail.com>
2014-11-09 22:03:08 +0200

    add survey dates and clinic days as dict with key survey, add additional methods for clinic query

--- bhp066/apps/bcpp_household/mappers/lentsweletau_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/lentsweletau_plot_mapper.py
@@ -1,14 +1,12 @@
 from datetime import date
 from dateutil.relativedelta import MO, TU, WE, TH, FR
 
-from django.conf import settings
-
 from edc.map.classes import site_mappers
 
 from .base_plot_mapper import BasePlotMapper
 from .choices import SECTIONS, SUB_SECTIONS, LENTSWELETAU_LANDMARKS
 
-from ..utils import ClinicDaysTuple
+from ..utils import ClinicDaysTuple, SurveyDatesTuple
 
 
 class LentsweletauPlotMapper(BasePlotMapper):
@@ -26,7 +24,7 @@ class LentsweletauPlotMapper(BasePlotMapper):
     location_boundary = ()
 
     intervention = True
-    survey_dates = {'bcpp-year-1': dict(
+    survey_dates = {'bcpp-year-1': SurveyDatesTuple(
         bhs_start_date=date(2014, 9, 5),
         bhs_full_enrollment_date=date(2014, 10, 15),
         bhs_end_date=date(2014, 10, 21),
@@ -36,7 +34,7 @@ class LentsweletauPlotMapper(BasePlotMapper):
         'IDCC': ClinicDaysTuple((MO, WE), None),
         'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
         'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
-        'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates['bcpp-year-1'].get('smc_start_date'))
+        'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates['bcpp-year-1'].smc_start_date)
         }
     }
 
