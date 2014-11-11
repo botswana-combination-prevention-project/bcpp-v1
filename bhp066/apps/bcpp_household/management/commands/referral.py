6068ed00dd7143c80e57953530223b94d8387366
erik van widenfelt <ew2789@gmail.com>
2014-11-11 17:33:24 +0200

    add referral resave command
    Former-commit-id: 6b693a91740e6d52360d71ee85ad2ce305ab6e7a

---
+++ bhp066/apps/bcpp_household/management/commands/referral.py
@@ -0,0 +1,18 @@
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ...models import SubjectReferral
+
+class Command(BaseCommand):
+
+    args = ''
+    help = ''
+
+    def handle(self, *args, **options):
+        for subject_referral in SubjectReferral.objects.all():
+            referral_appt_date = subject_referral.referral_appt_date
+            print subject_referral
+            subject_referral.save()
+            if referral_appt_date != subject_referral.referral_appt_date:
+                print '  {} {}'.format(subject_referral.referral_appt_date, referral_appt_date)
