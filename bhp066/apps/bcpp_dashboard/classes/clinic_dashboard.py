b37bb623ffadc123be637e7692077a7bf9d464b9
erik van widenfelt <ew2789@gmail.com>
2014-11-16 20:22:02 +0200

    fix dashboard after merge from bcpp_clinic_dashboard
    Former-commit-id: c0b249c1a3da3f1cf3d38ce615037127f09b3b06

--- bhp066/apps/bcpp_dashboard/classes/clinic_dashboard.py
+++ bhp066/apps/bcpp_dashboard/classes/clinic_dashboard.py
@@ -1,23 +1,25 @@
-# import re
-
-from edc.dashboard.subject.classes import RegisteredSubjectDashboard
-# from edc.subject.registration.models import RegisteredSubject
+from edc.subject.appointment.models import Appointment
 
 from apps.bcpp_clinic.models import ClinicConsent, ClinicVisit, ClinicSubjectLocator, ClinicEligibility
 from apps.bcpp_lab.models import ClinicRequisition, PackingList
 
+from .base_subject_dashboard import BaseSubjectDashboard
 
-class ClinicDashboard(RegisteredSubjectDashboard):
+
+class ClinicDashboard(BaseSubjectDashboard):
 
     view = 'clinic_dashboard'
+    dashboard_url_name = 'clinic_dashboard_url'
+    # dashboard_name = 'Clinic Participant Dashboard'
 
     def __init__(self, *args, **kwargs):
-        self.subject_dashboard_url = 'subject_dashboard_url'
+        self.subject_dashboard_url = 'clinic_dashboard_url'
         self.dashboard_type_list = ['clinic']
         kwargs.update({'dashboard_models': {'clinic_eligibility': ClinicEligibility},
-                       'membership_form_category': 'consenting',
+                       'membership_form_category': 'bcpp_clinic',
                        })
         self._registered_subject = None
+        self.form_category = 'bcpp_clinic'
         self._requisition_model = ClinicRequisition
         self.visit_model = ClinicVisit
         self._locator_model = ClinicSubjectLocator
@@ -25,21 +27,15 @@ class ClinicDashboard(RegisteredSubjectDashboard):
         super(ClinicDashboard, self).__init__(*args, **kwargs)
 
     def add_to_context(self):
-        super(ClinicDashboard, self).add_to_context()
+        super(BaseSubjectDashboard, self).add_to_context()
         self.context.add(
             home='clinic',
             search_name='clinic',
             subject_dashboard_url=self.subject_dashboard_url,
             title='Clinic Subject Dashboard',
-            clinic_consent=self.consent,
+            subject_consent=self.consent,
+            household_member=self.household_member,
             )
-
-    def set_registered_subject(self, pk=None):
-        # try:
-        self._registered_subject = ClinicEligibility.objects.get(pk=self.dashboard_id).household_member.registered_subject
-        # except ClinicEligibility.DoesNotExist:
-        #    pass
-        #    # self._registered_subject = RegisteredSubject.objects.get(pk=pk)
 
     @property
     def consent(self):
@@ -51,7 +47,7 @@ class ClinicDashboard(RegisteredSubjectDashboard):
 
     def set_membership_form_category(self):
         self._membership_form_category = self.membership_form_category
-        self._membership_form_category = 'consenting'
+        self._membership_form_category = 'bcpp_clinic'
         return self._membership_form_category
 
     def subject_hiv_status(self):
@@ -70,3 +66,22 @@ class ClinicDashboard(RegisteredSubjectDashboard):
 
     def render_labs(self, update=False):
         return ''
+
+    @property
+    def household_member(self):
+        return self._household_member
+
+    @household_member.setter
+    def household_member(self, (dashboard_model_name, dashboard_id)):
+        try:
+            self._household_member = ClinicEligibility.objects.get(pk=dashboard_id).household_member
+        except ClinicEligibility.DoesNotExist:
+            try:
+                self._household_member = ClinicVisit.objects.get(pk=dashboard_id).household_member
+            except ClinicVisit.DoesNotExist:
+                try:
+                    appointment = Appointment.objects.get(pk=dashboard_id)
+                    self._household_member = ClinicVisit.objects.get(appointment=appointment).household_member
+                except Appointment.DoesNotExist:
+                    raise TypeError('Attribute _household_member may not be None. Using dashboard_model={0}, '
+                                    'dashboard_id={1}'.format(dashboard_model_name, dashboard_id))
