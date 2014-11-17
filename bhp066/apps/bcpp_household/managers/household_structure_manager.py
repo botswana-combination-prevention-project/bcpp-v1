42d33411905399572c71f0f27f79499bd4757d8b
erik van widenfelt <ew2789@gmail.com>
2014-11-17 08:49:18 +0200

    add manage method to copy household members
    Former-commit-id: 3c5f3fbd94c68792c87ba2b23952add86d5222ae

--- bhp066/apps/bcpp_household/managers/household_structure_manager.py
+++ bhp066/apps/bcpp_household/managers/household_structure_manager.py
@@ -11,14 +11,25 @@ class HouseholdStructureManager(models.Manager):
         household = Household.objects.get_by_natural_key(household_identifier)
         return self.get(household=household, survey=survey)
 
-    def fetch_household_members(self, household_structure):
-        """Gets (or creates) members for the given household structure.
-
-        .. note:: this calls for the members from the household dashboard. (which is stupid)"""
-        from apps.bcpp_dashboard.classes import HouseholdDashboard
-        dashboard_type = 'household'
-        dashboard_model = 'household_structure'
-        dashboard_id = household_structure.pk
-        household_dashboard = HouseholdDashboard(
-            dashboard_type, dashboard_id, dashboard_model, survey=household_structure.survey.pk)
-        return household_dashboard.household_members
+    def fetch_household_members(self, household, source_survey, target_survey):
+        """Gets (or creates) members for the given household structure."""
+        HouseholdMember = models.get_model('bcpp_household_member', 'HouseholdMember')
+        source_household_structure = self.get(household=household, survey=source_survey)
+        try:
+            target_household_structure = self.get(household=household, survey=target_survey, enumerated=False)
+            for household_member in HouseholdMember.object.filter(household_structure=source_household_structure):
+                options = dict(
+                    household_structure=target_household_structure,
+                    first_name=household_member.first_name,
+                    initials=household_member.initials,
+                    age_in_years=household_member.age_in_years,
+                    gender=household_member.gender,
+                    relation=household_member.relation,
+                    inability_to_participate=household_member.inability_to_participate,
+                    internal_identifier=household_member.internal_identifier,
+                    )
+                HouseholdMember.object.create(**options)
+            target_household_structure.enumerated = False
+            target_household_structure.save(update_fields=['enumerated'])
+        except self.model.DoesNotExist:
+            pass
