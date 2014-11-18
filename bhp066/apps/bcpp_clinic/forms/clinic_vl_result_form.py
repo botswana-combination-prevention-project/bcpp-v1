fb6c4c85f53d54ab0490f95f5a609fea06967b7c
melissa kewagamang <mrbkew@gmail.com>
2014-11-18 12:15:03 +0200

    setting appt_type default
    Former-commit-id: 89ad1805ca6f552e9564cc1105154cfb9fd54ce4

--- bhp066/apps/bcpp_clinic/forms/clinic_vl_result_form.py
+++ bhp066/apps/bcpp_clinic/forms/clinic_vl_result_form.py
@@ -1,9 +1,6 @@
 from django import forms
 
 from edc.base.form.forms import BaseModelForm
-from edc.constants import NEW
-from edc.entry_meta_data.models import RequisitionMetaData
-from edc.subject.entry.models import LabEntry
 
 from ..models import ClinicVlResult
 
@@ -12,13 +9,6 @@ class ClinicVlResultForm (BaseModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        vl_requisition_metadata = RequisitionMetaData.objects.get(
-            appointment=cleaned_data.get('clinic_visit').appointment,
-            lab_entry=LabEntry.objects.get(requisition_panel__name='Clinic Viral Load')
-            )
-        if vl_requisition_metadata.entry_status == NEW:
-            raise forms.ValidationError('If a Viral Load Clinic Requisition is required, '
-                                        'then you have to fill it before this form.')
 
         if cleaned_data.get('assay_date', None) <= cleaned_data.get('collection_datetime', None).date():
             raise forms.ValidationError('Assay date CANNOT be less than or equal to '
