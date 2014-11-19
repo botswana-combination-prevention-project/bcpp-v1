b6343df597d3011a398764d95f3b54f592c771b4
erik van widenfelt <ew2789@gmail.com>
2014-11-19 05:20:34 +0200

    add members
    Former-commit-id: 35390e843aefe52f0a2f375a14bc5305e91a63c3

--- bhp066/apps/bcpp_household/managers/household_structure_manager.py
+++ bhp066/apps/bcpp_household/managers/household_structure_manager.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from apps.bcpp_survey.models import Survey
 
+from ..exceptions import AlreadyEnumerated, EligibleRepresentativeError
+
 
 class HouseholdStructureManager(models.Manager):
 
@@ -11,25 +13,43 @@ class HouseholdStructureManager(models.Manager):
         household = Household.objects.get_by_natural_key(household_identifier)
         return self.get(household=household, survey=survey)
 
-    def fetch_household_members(self, household, source_survey, target_survey):
-        """Gets (or creates) members for the given household structure."""
-        HouseholdMember = models.get_model('bcpp_household_member', 'HouseholdMember')
-        source_household_structure = self.get(household=household, survey=source_survey)
-        try:
-            target_household_structure = self.get(household=household, survey=target_survey, enumerated=False)
-            for household_member in HouseholdMember.object.filter(household_structure=source_household_structure):
-                options = dict(
-                    household_structure=target_household_structure,
-                    first_name=household_member.first_name,
-                    initials=household_member.initials,
-                    age_in_years=household_member.age_in_years,
-                    gender=household_member.gender,
-                    relation=household_member.relation,
-                    inability_to_participate=household_member.inability_to_participate,
-                    internal_identifier=household_member.internal_identifier,
-                    )
-                HouseholdMember.object.create(**options)
-            target_household_structure.enumerated = False
-            target_household_structure.save(update_fields=['enumerated'])
-        except self.model.DoesNotExist:
-            pass
+    def add_household_members_from_survey(self, household, source_survey, target_survey):
+        """Adds household members from a previous survey to an
+        unenumerated household structure of a new survey.
+
+        May raise an error if RepresentativeEligibility is not found."""
+        if source_survey:
+            HouseholdMember = models.get_model('bcpp_household_member', 'HouseholdMember')
+            if source_survey.survey_slug == target_survey.survey_slug:
+                raise ValueError('Source survey and target survey may not be the same.')
+            source_household_structure = self.get(household=household, survey=source_survey)
+            try:
+                target_household_structure = self.get(household=household, survey=target_survey, enumerated=False)
+                if target_household_structure.has_household_log_entry:
+                    target_household_structure.check_eligible_representative_filled(
+                        exception_cls=EligibleRepresentativeError)
+                    for household_member in HouseholdMember.objects.filter(household_structure=source_household_structure):
+                        try:
+                            household_member = HouseholdMember.objects.get(
+                                internal_identifier=household_member.internal_identifier,
+                                household_structure=target_household_structure)
+                        except HouseholdMember.DoesNotExist:
+                            options = dict(
+                                household_structure=target_household_structure,
+                                first_name=household_member.first_name,
+                                initials=household_member.initials,
+                                age_in_years=household_member.age_in_years,
+                                gender=household_member.gender,
+                                relation=household_member.relation,
+                                inability_to_participate=household_member.inability_to_participate,
+                                internal_identifier=household_member.internal_identifier,
+                            )
+                            household_member = HouseholdMember.objects.create(**options)
+                            print 'Added {}.'.format(household_member)
+                    target_household_structure.enumerated = False
+                    target_household_structure.save(update_fields=['enumerated'])  # skips current survey check in save()
+            except self.model.DoesNotExist:
+                raise AlreadyEnumerated(
+                    'household structure {} is already enumerated.'.format(target_household_structure))
+            except EligibleRepresentativeError:
+                pass
