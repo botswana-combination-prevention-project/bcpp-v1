9c9f3769481e5c70f80292328d210ade2e2a5f2d
erik van widenfelt <ew2789@gmail.com>
2014-11-20 05:45:59 +0200

    section admin, update app config, visit schedule
    Former-commit-id: 20c9290ab0736b0d3483464d3b6292762b09a41a

--- bhp066/apps/bcpp_survey/managers/survey_manager.py
+++ bhp066/apps/bcpp_survey/managers/survey_manager.py
@@ -9,14 +9,7 @@ class SurveyManager(models.Manager):
     def get_by_natural_key(self, survey_name):
         return self.get(survey_name=survey_name)
 
-#     def current_survey(self):
-#         """Returns the instance of the current survey based on today's date or None."""
-#         current_survey = None
-#         if super(SurveyManager, self).filter(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today()).count() == 1:
-#             current_survey = super(SurveyManager, self).get(datetime_start__lte=datetime.today(), datetime_end__gte=datetime.today())
-#         return current_survey
-
-    def current_survey(self, report_datetime=None, survey_slug=None, datetime_label=None):
+    def current_survey(self, report_datetime=None, survey_slug=None, datetime_label=None, community=None):
         """Returns a survey instance or None.
 
         The return value may be:
@@ -25,30 +18,34 @@ class SurveyManager(models.Manager):
         * a survey relative to the given report_datetime and survey_slug;
         * None if both survey_slug and settings.CURRENT are None."""
         survey = None
-        report_datetime = report_datetime or datetime.today()
-        survey_slug = survey_slug or settings.CURRENT_SURVEY
-        datetime_label = datetime_label or 'report_datetime'
-        try:
-            survey = self.get(
-                datetime_start__lte=report_datetime,
-                datetime_end__gte=report_datetime)
-            if survey_slug:
+        if settings.CURRENT_SURVEY:
+            try:
+                report_date = report_datetime.date() or datetime.today()
+            except AttributeError:
+                report_date = report_datetime or datetime.today()
+            survey_slug = survey_slug or settings.CURRENT_SURVEY
+            datetime_label = datetime_label or 'report_datetime'
+            try:
                 survey = self.get(
-                    datetime_start__lte=report_datetime,
-                    datetime_end__gte=report_datetime,
-                    survey_slug=survey_slug)
-        except MultipleObjectsReturned:
-            raise ImproperlyConfigured('Date {} falls within more than one Survey. Start and end dates'
-                                       'may not overlap between Surveys. Check the survey configuration.'.format(
-                                           report_datetime))
-        except self.model.DoesNotExist:
-            raise ImproperlyConfigured(
-                'Expected survey \'{0}\'. {2} {1} does not fall within '
-                'the start/end dates of Survey \'{0}\'.'.format(
-                    survey_slug,
-                    report_datetime.strftime('%Y-%m-%d'),
-                    '{}{}'.format(datetime_label[0].upper(), datetime_label[1:])
+                    datetime_start__lte=report_date,
+                    datetime_end__gte=report_date)
+                if survey_slug:
+                    survey = self.get(
+                        datetime_start__lte=report_date,
+                        datetime_end__gte=report_date,
+                        survey_slug=survey_slug)
+            except MultipleObjectsReturned:
+                raise ImproperlyConfigured('Date {} falls within more than one Survey. Start and end dates'
+                                           'may not overlap between Surveys. ({}). See app configuration.'.format(
+                                               report_date, community))
+            except self.model.DoesNotExist:
+                raise ImproperlyConfigured(
+                    'Expected survey \'{0}\'. {2} {1} does not fall within '
+                    'the start/end dates of Survey \'{0}\' ({3}). See app_configuration or reload urls.'.format(
+                        survey_slug,
+                        report_datetime.strftime('%Y-%m-%d'),
+                        '{}{}'.format(datetime_label[0].upper(), datetime_label[1:]),
+                        community,
+                        )
                     )
-                )
-
         return survey
