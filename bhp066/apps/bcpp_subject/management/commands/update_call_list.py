3573a6f35519e908f05639354f6f561c78899a93
erik van widenfelt <ew2789@gmail.com>
2014-11-21 10:33:26 +0200

    add to call list, add label field
    Former-commit-id: df2afe78364144f8c0c7830842ffb3ef46255e5e

--- bhp066/apps/bcpp_subject/management/commands/update_call_list.py
+++ bhp066/apps/bcpp_subject/management/commands/update_call_list.py
@@ -7,8 +7,8 @@ from apps.bcpp_survey.models import Survey
 
 class Command(BaseCommand):
 
-    args = 'survey_slug'
-    help = 'Add to the call list subject consents from the specified survey.'
+    args = 'survey_slug label'
+    help = 'Add to the call list info from all subject consents from the specified survey.'
 
     def handle(self, *args, **options):
         CallList = get_model('bcpp_subject', 'CallList')
@@ -18,6 +18,10 @@ class Command(BaseCommand):
             Survey.objects.get(survey_slug=survey_slug)
         except (IndexError, Survey.DoesNotExist):
             raise CommandError('Specify a valid survey_slug')
-        update_call_list(survey_slug)
+        try:
+            label = args[1]
+        except IndexError:
+            raise CommandError('Specify a label')
+        update_call_list(survey_slug, label)
         new_count = CallList.objects.all().count()
-        print 'Added {} records to the Call List.'.format(new_count - count)
+        print 'Added {} records to the Call List for {}: {}.'.format(new_count - count, survey_slug, label)
