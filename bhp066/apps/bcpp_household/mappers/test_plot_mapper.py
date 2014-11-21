942758a93eddf39d9f56ec47569b864e6b0cc025
erik van widenfelt <ew2789@gmail.com>
2014-11-21 14:26:32 +0200

    minor fixes from UAT
    Former-commit-id: 53b49c949aed38667916d63846913c2448ecc6a1

--- bhp066/apps/bcpp_household/mappers/test_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/test_plot_mapper.py
@@ -34,10 +34,10 @@ class TestPlotMapper(BasePlotMapper):
             smc_start_date=date(2013, 11, 7)),
         'bcpp-year-2': SurveyDatesTuple(
             name='t1',
-            start_date=date(2014, 10, 18),
-            full_enrollment_date=date(2014, 11, 7),
-            end_date=date(2014, 11, 22),
-            smc_start_date=date(2014, 11, 7)),
+            start_date=date(2014, 11, 21),
+            full_enrollment_date=date(2014, 12, 17),
+            end_date=date(2014, 12, 22),
+            smc_start_date=date(2014, 12, 22)),
     }
 
     clinic_days = {
