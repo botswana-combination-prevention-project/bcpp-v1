a4e9fdc40784ddaa3e1cbfc53fdad4e4ee472157
erik van widenfelt <ew2789@gmail.com>
2014-11-23 23:36:29 +0200

    change to work with revamped edc section and search
    Former-commit-id: 0fc0d7ab7001e6219822626b7aa8731b4460e090

--- bhp066/apps/bcpp_household_member/search/member_search_by_word.py
+++ bhp066/apps/bcpp_household_member/search/member_search_by_word.py
@@ -12,43 +12,31 @@ class MemberSearchByWord(BaseSearchByWord):
 
     name = 'word'
     search_model = HouseholdMember
-    order_by = '-modified'
+    order_by = ['-modified']
     template = 'section_member_include.html'
 
-    def get_qset_by_search_term_pattern(self):
-        qset_filter = Q()
-        qset_exclude = Q()
-        if re.match('^[0-9]{6}-[0-9]{2}$', self.get_search_term()):
-            qset_filter = Q(household_structure__household__plot__plot_identifier=self.get_search_term())
-        elif re.match('^[0-9]{7}-[0-9]{2}$', self.get_search_term()):
-            qset_filter = Q(household_structure__household__household_identifier=self.get_search_term())
+    @property
+    def qset(self):
+        qset = self.qset_for_registered_subject
+        qset.add(Q(household_structure__household__household_identifier__icontains=self.search_value), Q.OR)
+        return qset
+
+    @property
+    def qset_by_search_term_pattern(self):
+        qset_filter = None
+        if re.match('^[0-9]{6}-[0-9]{2}$', self.search_value):
+            qset_filter = Q(household_structure__household__plot__plot_identifier=self.search_value)
+        elif re.match('^[0-9]{7}-[0-9]{2}$', self.search_value):
+            qset_filter = Q(household_structure__household__household_identifier=self.search_value)
         if qset_filter:
-            return qset_filter, qset_exclude
+            return (qset_filter, None)
         return None
 
-    def get_filter_keyword_list(self):
+    @property
+    def keyword_list(self):
+        """Returns a list of search values to be used to query."""
+        return [item[0] for item in HOUSEHOLD_MEMBER_PARTICIPATION]
+
+    @property
+    def display_keyword_list(self):
         return [item[1] for item in HOUSEHOLD_MEMBER_PARTICIPATION]
-
-    def get_filter_keyword_url_list(self):
-        pass
-
-    def get_qset_by_filter_keyword(self):
-        """Returns a qset based on matching keyword.
-
-        If you predefine keywords, the search term will be intercepted and
-        used to select a query instead."""
-        qset_filter = Q()
-        qset_exclude = Q()
-        if (self.get_search_term().lower() in [item[1].lower() for item in HOUSEHOLD_MEMBER_PARTICIPATION] +
-                ['-{0}'.format(item[1].lower()) for item in HOUSEHOLD_MEMBER_PARTICIPATION]):
-            if self.get_search_term()[0] == '-':
-                search_term = [item[0] for item in HOUSEHOLD_MEMBER_PARTICIPATION
-                               if item == self.get_search_term()[1:]][0]
-                qset_exclude = Q(member_status=search_term)
-            else:
-                search_term = [item[0] for item in HOUSEHOLD_MEMBER_PARTICIPATION
-                               if item[1].lower() == self.get_search_term().lower()][0]
-                qset_filter = Q(member_status=search_term)
-        if qset_filter or qset_exclude:
-            return (qset_filter, qset_exclude)
-        return None
