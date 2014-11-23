fc0a24f3ddc00919093af101a45f99a0e77b6a88
erik van widenfelt <ew2789@gmail.com>
2014-11-23 23:36:29 +0200

    change to work with revamped edc section and search
    Former-commit-id: 226e6bb0a4249d5efe8defb33a786be1685a22d6

--- bhp066/apps/bcpp_subject/search/subject_search_by_word.py
+++ bhp066/apps/bcpp_subject/search/subject_search_by_word.py
@@ -6,13 +6,12 @@ from ..models import SubjectConsent
 class SubjectSearchByWord(BaseSearchByWord):
 
     name = 'word'
+    order_by = ['-created']
     search_model = SubjectConsent
-    order_by = '-created'
     template = 'subjectconsent_include.html'
 
     def contribute_to_context(self, context):
         context = super(BaseSearchByWord, self).contribute_to_context(context)
-        # FIXME: this should not be hard coded, get it from the section class somehow.
-        context.update({
-            'subject_dashboard_url': 'subject_dashboard_url'})
+#         context.update({
+#             'subject_dashboard_url': 'subject_dashboard_url'})
         return context
