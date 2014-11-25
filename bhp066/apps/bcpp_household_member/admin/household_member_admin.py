efcbd77b66bfe38c280b2dc45c304a02ea8ae9c3
erik van widenfelt <ew2789@gmail.com>
2014-11-25 13:36:59 +0200

    member appts, call list, work list
    Former-commit-id: 1a7b69c6240afeffcb93bd7f8d3b071b82bca90f

--- bhp066/apps/bcpp_household_member/admin/household_member_admin.py
+++ bhp066/apps/bcpp_household_member/admin/household_member_admin.py
@@ -25,6 +25,12 @@ class HouseholdMemberAdmin(BaseModelAdmin):
             'eligible_member', 'eligible_subject', 'member_status'],
         extra_fields={'plot_identifier': 'household_structure__household__plot__plot_identifier'})]
 
+    def save_model(self, request, obj, form, change):
+        """Sets updated_after_auto_filled to True as user edits the form."""
+        if self.auto_filled:
+            obj.updated_after_auto_filled = True
+        super(HouseholdMemberAdmin, self).save_model(request, obj, form, change)
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "household_structure":
             kwargs["queryset"] = HouseholdStructure.objects.filter(id__exact=request.GET.get('household_structure', 0))
