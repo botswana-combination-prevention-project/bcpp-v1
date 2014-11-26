5ace88aa46364e147eaf63a9cda53fb900c952c8
erik van widenfelt <ew2789@gmail.com>
2014-11-26 22:49:38 +0200

    change instructions, wording, rules

--- bhp066/apps/bcpp_subject/admin/medical_diagnoses_admin.py
+++ bhp066/apps/bcpp_subject/admin/medical_diagnoses_admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext as _
 
-from apps.bcpp_survey.models import Survey
-
 from ..models import MedicalDiagnoses
 from ..forms import MedicalDiagnosesForm
 
@@ -24,7 +22,7 @@ class MedicalDiagnosesAdmin(SubjectVisitModelAdmin):
         "cancer_record": admin.VERTICAL,
         "tb_record": admin.VERTICAL, }
     filter_horizontal = ('diagnoses',)
-    instructions = [_("Read to Participant: I am now going to ask you"
+    instructions = [_("<h5>Read to Participant</h5> I am now going to ask you"
                       " some questions about major illnesses that you may"
                       " have had in the past 12 months. Sometimes people"
                       " call different sicknesses by different names."
@@ -32,7 +30,7 @@ class MedicalDiagnosesAdmin(SubjectVisitModelAdmin):
                       " Also, please remember that your answers will be"
                       " kept confidential.")]
 
-if Survey.objects.current_survey().survey_slug != Survey.objects.first_survey.survey_slug:
+if MedicalDiagnosesAdmin.current_survey != MedicalDiagnosesAdmin.first_survey:
     MedicalDiagnosesAdmin.instructions = [
         _("<h5>Read to Participant</h5> I am now going to ask you"
           " some questions about major illnesses that you may"
@@ -41,6 +39,5 @@ if Survey.objects.current_survey().survey_slug != Survey.objects.first_survey.su
           " If you do not understand what I mean, please ask."
           " Also, please remember that your answers will be"
           " kept confidential."),
-        ('<h5>Instructions for RA</h5> Please review participant medical card for new diagnoses')
     ]
 admin.site.register(MedicalDiagnoses, MedicalDiagnosesAdmin)
