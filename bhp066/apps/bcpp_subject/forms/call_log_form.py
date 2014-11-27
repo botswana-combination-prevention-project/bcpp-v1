eb0c717834353c8699f8d182a01d79630ec252cf
erik van widenfelt <ew2789@gmail.com>
2014-11-27 15:09:25 +0200

    appt
    Former-commit-id: 49ef71581487481a8c9b482e9ec054a023e3ebde

--- bhp066/apps/bcpp_subject/forms/call_log_form.py
+++ bhp066/apps/bcpp_subject/forms/call_log_form.py
@@ -74,7 +74,7 @@ class CallLogEntryForm (BaseModelForm):
                 if value and item not in [
                         'id', 'call_log', 'call_datetime', 'contact_type', 'survival_status',
                         'invalid_numbers', 'call_again', 'new_community', 'update_locator',
-                        'moved_community', 'appt_date', 'appt_grading', 'time_of_week', 'time_of_day',
+                        'moved_community', 'appt', 'appt_date', 'appt_grading', 'time_of_week', 'time_of_day',
                         'appt_location', 'appt_location_other']:
                     raise forms.ValidationError(
                         'Question {} should be left blank. Got \'{}\' when moved_community=\'Yes\''.format(item, value))
