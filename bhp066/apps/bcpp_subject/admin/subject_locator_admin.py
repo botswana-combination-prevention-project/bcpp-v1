080c2b45d3609e1556dd17b4ccaf67e0f4388512
erik van widenfelt <ew2789@gmail.com>
2014-11-28 08:45:56 +0200

    admin

--- bhp066/apps/bcpp_subject/admin/subject_locator_admin.py
+++ bhp066/apps/bcpp_subject/admin/subject_locator_admin.py
@@ -2,12 +2,10 @@ from collections import OrderedDict
 
 from django.contrib import admin
 
-# from edc.export.actions import export_as_csv_action
-
+from ..actions import export_locator_for_cdc_action
 from ..filters import SubjectLocatorIsReferredListFilter, SubjectCommunityListFilter
 from ..forms import SubjectLocatorForm
 from ..models import SubjectLocator
-from ..actions import export_locator_for_cdc_action
 
 from .subject_visit_model_admin import SubjectVisitModelAdmin
 
@@ -50,30 +48,43 @@ class SubjectLocatorAdmin(SubjectVisitModelAdmin):
         "has_alt_contact": admin.VERTICAL,
         "may_call_work": admin.VERTICAL,
         "may_contact_someone": admin.VERTICAL, }
-    list_filter = (SubjectLocatorIsReferredListFilter, SubjectCommunityListFilter, 'may_follow_up', 'may_contact_someone', 'may_call_work', "home_visit_permission")
-    list_display = ('subject_visit', 'date_signed', "home_visit_permission", "may_follow_up", "may_sms_follow_up", "has_alt_contact", "may_call_work", "may_contact_someone")
+    list_filter = (
+        SubjectLocatorIsReferredListFilter,
+        SubjectCommunityListFilter,
+        'may_follow_up',
+        'may_contact_someone',
+        'may_call_work',
+        "home_visit_permission")
+    list_display = (
+        'subject_visit', 'date_signed', "home_visit_permission",
+        "may_follow_up", "may_sms_follow_up", "has_alt_contact",
+        "may_call_work", "may_contact_someone")
 
     def get_actions(self, request):
         actions = super(SubjectLocatorAdmin, self).get_actions(request)
-        actions['export_locator_for_cdc_action'] = (  # This is a django SortedDict (function, name, short_description)
+        actions['export_locator_for_cdc_action'] = (
             export_locator_for_cdc_action(
                 delimiter='|',
                 encrypt=False,
                 strip=True,
-                exclude=['exported', 'registered_subject', self.visit_model_foreign_key, 'revision', 'hostname_created', 'hostname_modified', 'created', 'modified', 'user_created', 'user_modified', ],
+                exclude=['exported', 'registered_subject',
+                         self.visit_model_foreign_key, 'revision', 'hostname_created',
+                         'hostname_modified', 'created', 'modified', 'user_created', 'user_modified', ],
                 extra_fields=OrderedDict(
-                    {'subject_identifier': self.visit_model_foreign_key + '__appointment__registered_subject__subject_identifier',
+                    {'subject_identifier': self.visit_model_foreign_key + \
+                     '__appointment__registered_subject__subject_identifier',
                      'first_name': self.visit_model_foreign_key + '__appointment__registered_subject__first_name',
                      'last_name': self.visit_model_foreign_key + '__appointment__registered_subject__last_name',
                      'initials': self.visit_model_foreign_key + '__appointment__registered_subject__initials',
                      'dob': self.visit_model_foreign_key + '__appointment__registered_subject__dob',
                      'identity': self.visit_model_foreign_key + '__appointment__registered_subject__identity',
                      'identity_type': self.visit_model_foreign_key + '__appointment__registered_subject__identity_type',
-                     'plot_identifier': self.visit_model_foreign_key + '__household_member__household_structure__household__plot__plot_identifier',
+                     'plot_identifier': self.visit_model_foreign_key + \
+                     '__household_member__household_structure__household__plot__plot_identifier',
                      })
                 ),
-                'export_locator_for_cdc_action',
-                "Export Locator in CDC format (Manual)")
+            'export_locator_for_cdc_action',
+            "Export Locator in CDC format (Manual)")
         return actions
 
 admin.site.register(SubjectLocator, SubjectLocatorAdmin)
