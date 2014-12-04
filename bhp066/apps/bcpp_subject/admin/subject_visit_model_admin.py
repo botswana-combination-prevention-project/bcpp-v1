527eeed2f800dea3a506fee66d612ab0c1375b37
erik van widenfelt <ew2789@gmail.com>
2014-12-04 15:19:11 +0200

    subject visit admins, select baseline of annual fields on visit code

--- bhp066/apps/bcpp_subject/admin/subject_visit_model_admin.py
+++ bhp066/apps/bcpp_subject/admin/subject_visit_model_admin.py
@@ -5,6 +5,7 @@ from edc.subject.visit_tracking.admin import BaseVisitTrackingModelAdmin
 from apps.bcpp_survey.models import Survey
 
 from ..models import SubjectVisit, SubjectConsent
+from ..constants import BASELINE
 
 
 class SubjectVisitModelAdmin (BaseVisitTrackingModelAdmin):
@@ -30,21 +31,21 @@ class SubjectVisitModelAdmin (BaseVisitTrackingModelAdmin):
     def get_fieldsets(self, request, obj=None):
         """Returns fieldsets based on the survey, baseline or annual."""
         fieldsets = super(SubjectVisitModelAdmin, self).get_fieldsets(request, obj)
-        subject_consent = None
-        fieldsets[0][1]['fields'] = self.baseline_fields or self.fields
-        try:
-            survey_slug = obj.subject_visit.household_member.household_structure.survey.survey_slug
-        except (AttributeError, ):
-            try:
-                subject_consent = SubjectConsent.objects.get(registered_subject=request.GET.get('registered_subject'))
-                survey_slug = subject_consent.household_member.survey.survey_slug
-            except (AttributeError, SubjectConsent.DoesNotExist):
-                survey_slug = None
-        if survey_slug != self.current_survey:
-            try:
-                fieldsets[0][1]['fields'] = self.annual_fields or self.fields
-            except AttributeError:
-                pass
+#         subject_consent = None
+#         fieldsets[0][1]['fields'] = self.baseline_fields or self.fields
+#         try:
+#             survey_slug = obj.subject_visit.household_member.household_structure.survey.survey_slug
+#         except (AttributeError, ):
+#             try:
+#                 subject_consent = SubjectConsent.objects.get(registered_subject=request.GET.get('registered_subject'))
+#                 survey_slug = subject_consent.household_member.survey.survey_slug
+#             except (AttributeError, SubjectConsent.DoesNotExist):
+#                 survey_slug = None
+#         if survey_slug != self.current_survey:
+#             try:
+#                 fieldsets[0][1]['fields'] = self.annual_fields or self.fields
+#             except AttributeError:
+#                 pass
         return fieldsets
 
     def formfield_for_choice_field(self, db_field, request=None, **kwargs):
@@ -70,16 +71,22 @@ class SubjectVisitModelAdmin (BaseVisitTrackingModelAdmin):
 
     def add_view(self, request, form_url='', extra_context=None):
         """Set the instructions based on the survey, baseline or annual."""
-        if self.fields == self.annual_fields:
-            self.instructions = self.annual_instructions or self.instructions
-        else:
+        subject_visit = SubjectVisit.objects.get(pk=request.GET.get('subject_visit'))
+        if subject_visit.appointment.visit_definition.code == BASELINE:
+            self.fields = self.baseline_fields
             self.instructions = self.baseline_instructions or self.instructions
+        else:
+            self.fields = self.annual_fields
+            self.instructions = self.annual_instructions or self.instructions
         return super(SubjectVisitModelAdmin, self).add_view(request, form_url=form_url, extra_context=extra_context)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         """Set the instructions based on the survey, baseline or annual."""
-        if self.fields == self.annual_fields:
-            self.instructions = self.annual_instructions or self.instructions
-        else:
+        subject_visit = SubjectVisit.objects.get(pk=request.GET.get('subject_visit'))
+        if subject_visit.appointment.visit_definition.code == BASELINE:
+            self.fields = self.baseline_fields
             self.instructions = self.baseline_instructions or self.instructions
+        else:
+            self.fields = self.annual_fields
+            self.instructions = self.annual_instructions or self.instructions
         return super(SubjectVisitModelAdmin, self).change_view(request, object_id, form_url=form_url, extra_context=extra_context)
--- bhp066/apps/bcpp_subject/constants.py
+++ bhp066/apps/bcpp_subject/constants.py
@@ -1,3 +1,4 @@
 FULL = 'FULL'
 DECLINED = 'Declined'
 NOT_PERFORMED = 'Not performed'
+BASELINE = 'T0'
