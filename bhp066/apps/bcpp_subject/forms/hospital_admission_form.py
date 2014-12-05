bdbd47736fcfa41d303e8f4dc5eb3a8cab0a56f4
melissa kewagamang <mrbkew@gmail.com>
2014-12-05 10:36:25 +0200

    cleaned_data
    Former-commit-id: 2dc0da63e0eb0c2ac4dcbd750a086503dd5574c9

--- bhp066/apps/bcpp_subject/forms/hospital_admission_form.py
+++ bhp066/apps/bcpp_subject/forms/hospital_admission_form.py
@@ -16,22 +16,22 @@ class HospitalAdmissionForm (BaseSubjectModelForm):
         self.validate_cleaned_data('hospitalization_costs', cleaned_data)
 
         # if greater than zero
-        if cleaned_data.get('admission_nights', None) > 0 and not cleaned_data.get('reason_hospitalized', None):
+        if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('reason_hospitalized'):
             raise forms.ValidationError(
                 'If admission nights is greater than zero, what was the reason for hospitalization?')
-        if cleaned_data.get('admission_nights', None) > 0 and not cleaned_data.get('facility_hospitalized', None):
+        if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('facility_hospitalized'):
             raise forms.ValidationError(
                 'If admission nights is greater than zero, where was the participant hospitalized?')
         if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('nights_hospitalized'):
             raise forms.ValidationError(
                 'For how many nights in total was the participant hospitalized?')
-        if cleaned_data.get('admission_nights', None) > 0 and not cleaned_data.get('healthcare_expense', None):
+        if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('healthcare_expense'):
             raise forms.ValidationError(
                 'How much was paid for the entire stay including medicines?')
-        if cleaned_data.get('admission_nights', None) > 0 and not cleaned_data.get('travel_hours', None):
+        if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('travel_hours'):
             raise forms.ValidationError(
                 'How many hours did it take you to get to the hospital?')
-        if cleaned_data.get('admission_nights', None) > 0 and not cleaned_data.get('hospitalization_costs', None):
+        if cleaned_data.get('admission_nights') > 0 and not cleaned_data.get('hospitalization_costs'):
             raise forms.ValidationError(
                 'Did anyone else besides you cover the hospitalization costs?')
 
