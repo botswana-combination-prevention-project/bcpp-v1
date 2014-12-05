2aefe1262de4f9e6f96ef2fefb6b14fb5d5ceeb2
melissa kewagamang <mrbkew@gmail.com>
2014-12-05 11:55:39 +0200

    remove death form from bcpp_subject

--- bhp066/apps/bcpp_subject/forms/main.py
+++ bhp066/apps/bcpp_subject/forms/main.py
@@ -1,14 +1,6 @@
-from ..models import (SubjectDeath, QualityOfLife,
-                                 OutpatientCare,
-                                 HivHealthCareCosts, Grant,
-                                 SubstanceUse)
+from ..models import QualityOfLife, OutpatientCare, HivHealthCareCosts, Grant, SubstanceUse
+
 from .base_subject_model_form import BaseSubjectModelForm
-
-
-class SubjectDeathForm (BaseSubjectModelForm):
-
-    class Meta:
-        model = SubjectDeath
 
 
 class QualityOfLifeForm (BaseSubjectModelForm):
