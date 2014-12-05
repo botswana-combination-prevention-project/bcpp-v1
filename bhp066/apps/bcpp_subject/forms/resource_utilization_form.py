95acf6544dc6abf543947be5cc43db5f2801eae7
melissa kewagamang <mrbkew@gmail.com>
2014-12-05 10:36:25 +0200

    cleaned_data

--- bhp066/apps/bcpp_subject/forms/resource_utilization_form.py
+++ bhp066/apps/bcpp_subject/forms/resource_utilization_form.py
@@ -10,14 +10,14 @@ class ResourceUtilizationForm (BaseSubjectModelForm):
     def clean(self):
         cleaned_data = super(ResourceUtilizationForm, self).clean()
 
-        if cleaned_data.get('out_patient', None) == 'Yes' and not cleaned_data.get('hospitalized', None):
+        if cleaned_data.get('out_patient') == 'Yes' and not cleaned_data.get('hospitalized'):
             raise forms.ValidationError(
                 'If participant sought medical care in the past 3months, how many times was he/she admitted?')
 
-        if cleaned_data.get('out_patient', None) == 'Yes' and not cleaned_data.get('money_spent', None):
+        if cleaned_data.get('out_patient') == 'Yes' and not cleaned_data.get('money_spent'):
             raise forms.ValidationError('How much money was spent?')
 
-        if cleaned_data.get('out_patient', None) == 'Yes' and not cleaned_data.get('medical_cover', None):
+        if cleaned_data.get('out_patient') == 'Yes' and not cleaned_data.get('medical_cover'):
             raise forms.ValidationError('Did anyone else cover the costs?')
 
         return cleaned_data
