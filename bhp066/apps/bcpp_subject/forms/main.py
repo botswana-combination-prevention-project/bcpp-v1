e7eb9d4e7b0996f7219812d09be092f7a06e3a78
melissa kewagamang <mrbkew@gmail.com>
2014-12-05 17:26:27 +0200

    health_care_costs validations

--- bhp066/apps/bcpp_subject/forms/main.py
+++ bhp066/apps/bcpp_subject/forms/main.py
@@ -1,3 +1,4 @@
+from django import forms
 from ..models import QualityOfLife, OutpatientCare, HivHealthCareCosts, Grant, SubstanceUse
 
 from .base_subject_model_form import BaseSubjectModelForm
@@ -16,6 +17,26 @@ class OutpatientCareForm (BaseSubjectModelForm):
 
 
 class HivHealthCareCostsForm (BaseSubjectModelForm):
+
+    def clean(self):
+        cleaned_data = super(HivHealthCareCostsForm, self).clean()
+
+        if cleaned_data.get('hiv_medical_care') == 'Yes' and cleaned_data.get('reason_no_care') != None:
+            raise forms.ValidationError('If participant has received HIV medical care, reason '
+                                        'for not receiving care should be None ')
+
+        if cleaned_data.get('hiv_medical_care') == 'Yes' and not cleaned_data.get('place_care_received'):
+            raise forms.ValidationError('If participant has received HIV medical care, '
+                                        'where was it received? ')
+
+        if cleaned_data.get('hiv_medical_care') == 'Yes' and not cleaned_data.get('care_regularity'):
+            raise forms.ValidationError('If participant has received HIV medical care, how '
+                                        'often was the care received? ')
+
+        if cleaned_data.get('hiv_medical_care') == 'Yes' and not cleaned_data.get('doctor_visits'):
+            raise forms.ValidationError('If participant has received HIV medical care, how '
+                                        'often where you taken to see a doctor? ')
+        return cleaned_data
 
     class Meta:
         model = HivHealthCareCosts
