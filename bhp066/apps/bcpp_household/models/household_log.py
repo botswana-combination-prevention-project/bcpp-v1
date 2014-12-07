a582376e2f529c76654ac3e3a21c3b1c5acfcf48
ckgathi <ckgathi@bhp.org.bw>
2014-12-07 09:47:35 +0200

    dispatch bug  fixes
    Former-commit-id: 19b37fcaae97128c7c115c1d2970362a77032300

--- bhp066/apps/bcpp_household/models/household_log.py
+++ bhp066/apps/bcpp_household/models/household_log.py
@@ -17,7 +17,7 @@ from .plot import Plot
 
 class HouseholdLog(BaseDispatchSyncUuidModel):
     """A system model that links the household log to the household."""
-    household_structure = models.OneToOneField(HouseholdStructure)
+    household_structure = models.ForeignKey(HouseholdStructure)
 
     history = AuditTrail()
 
