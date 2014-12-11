2cfc67a458bbe1542500dd4ac5ab1b0baa404d37
erik van widenfelt <ew2789@gmail.com>
2014-12-11 12:00:26 +0200

    change managers to filter on community, fix grant, viral load result, update admin with list_display, search

--- bhp066/apps/bcpp_household_member/models/subject_death.py
+++ bhp066/apps/bcpp_household_member/models/subject_death.py
@@ -1,30 +1,14 @@
-from django.db import models
-from datetime import datetime, time
-
 from edc.audit.audit_trail import AuditTrail
-from edc.base.model.validators import date_not_before_study_start, date_not_future
-#from edc.subject.adverse_event.models import BaseBaseDeath
-from edc.subject.registration.models import BaseRegisteredSubjectModel
+
+from .base_member_status_model import BaseMemberStatusModel
 
 
-class SubjectDeath(BaseRegisteredSubjectModel):
-
-    date_death_reported = models.DateField(
-        verbose_name="Date Death Reported:",
-        validators=[
-            date_not_before_study_start,
-            date_not_future,
-            ],
-        help_text="",
-        )
+class SubjectDeath(BaseMemberStatusModel):
 
     history = AuditTrail()
 
     def __unicode__(self):
         return unicode(self.registered_subject)
-
-    def get_report_datetime(self):
-        return datetime.combine(self.death_date, time(0, 0))
 
     class Meta:
         app_label = "bcpp_household_member"
--- bhp066/apps/bcpp_subject/admin/labour_market_wages_admin.py
+++ bhp066/apps/bcpp_subject/admin/labour_market_wages_admin.py
@@ -1,37 +1,10 @@
 from django.contrib import admin
 
-from edc.base.modeladmin.admin import BaseTabularInline, BaseModelAdmin
+from ..forms import LabourMarketWagesForm
+from ..models import LabourMarketWages
 
-from ..forms import LabourMarketWagesForm, GrantForm
-from ..models import LabourMarketWages, Grant, SubjectVisit
-
+from .grant_admin import GrantInlineAdmin
 from .subject_visit_model_admin import SubjectVisitModelAdmin
-
-
-class GrantAdmin(BaseModelAdmin):
-    form = GrantForm
-    fields = ('labour_market_wages', 'grant_number', 'grant_type', 'other_grant',)
-    list_display = ('labour_market_wages', 'grant_number', 'grant_type', 'other_grant', )
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "labour_market_wages":
-            kwargs["queryset"] = LabourMarketWages.objects.filter(id__exact=request.GET.get('labour_market_wages', 0))
-        return super(GrantAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-admin.site.register(Grant, GrantAdmin)
-
-
-class GrantInlineAdmin(BaseTabularInline):
-    model = Grant
-    form = GrantForm
-    extra = 1
-    fields = ('subject_visit', 'grant_number', 'grant_type', 'other_grant',)
-    list_display = ('subject_visit', 'grant_number', 'grant_type', 'other_grant', )
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "subject_visit":
-            kwargs["queryset"] = SubjectVisit.objects.filter(id__exact=request.GET.get('subject_visit', 0))
-        return super(GrantInlineAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class LabourMarketWagesAdmin(SubjectVisitModelAdmin):
