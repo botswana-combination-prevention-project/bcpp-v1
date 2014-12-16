7bb6c8cb807077257c64f5eb6716314250c68124
erik van widenfelt <ew2789@gmail.com>
2014-12-16 17:06:56 +0200

    lab_packing
    Former-commit-id: 424af68e5e91a31b7aa6fc05a384f17514b50a80

--- bhp066/apps/bcpp_lab/managers/clinic_requisition_manager.py
+++ bhp066/apps/bcpp_lab/managers/clinic_requisition_manager.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+
+from edc.map.classes import site_mappers
+
+
 from edc.entry_meta_data.managers import RequisitionMetaDataManager
 
 
@@ -5,3 +10,9 @@ class ClinicRequisitionManager(RequisitionMetaDataManager):
 
     def get_by_natural_key(self, requisition_identifier):
         return self.get(requisition_identifier=requisition_identifier)
+
+    def get_queryset(self):
+        if settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY:
+            community = site_mappers.current_mapper.map_area
+            return super(ClinicRequisitionManager, self).get_queryset().filter(community=community)
+        return super(ClinicRequisitionManager, self).get_queryset()
