836a996a7e14aedb55a350e1e020f3a29a6e3085
ckgathi <you@example.com>
2015-02-06 13:44:22 +0200

    correct consent bug fix n tests
    Former-commit-id: 6197c8f4ac632d3b2afbd18f9f598a035cff56bf

--- bhp066/apps/bcpp_subject/tests/__init__.py
+++ bhp066/apps/bcpp_subject/tests/__init__.py
@@ -11,3 +11,5 @@ from .test_referral import TestReferral
 # from .test_subject_status_helper import TestSubjectStatusHelper
 # from .test_subject_referral_appt_helper import TestSubjectReferralApptHelper
 # from .test_next_clinic_date import TestNextClinicDate
+from .test_correct_consent import TestCorrectConsent
+
