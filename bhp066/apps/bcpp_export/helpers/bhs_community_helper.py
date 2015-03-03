2d2411957e5eb01f4b2e3362603c0060fe4c474b
Leslie <you@example.com>
2015-03-03 13:29:00 +0200

    Fixed Doc String comments.
    Former-commit-id: 8835f88727852638630343519171d570d7d736e3

--- bhp066/apps/bcpp_export/helpers/bhs_community_helper.py
+++ bhp066/apps/bcpp_export/helpers/bhs_community_helper.py
@@ -1,4 +1,5 @@
-from . import BHSBaseExport
+from .bhs_base_export import BHSBaseExport
+
 
 class BHSCommunityHelper(BHSBaseExport):
 
@@ -20,70 +21,70 @@ class BHSCommunityHelper(BHSBaseExport):
 
     @property
     def community_id(self):
-        """Returns community id"""
+        """Returns community id."""
         return self._community_id
 
     @property
     def community_name(self):
-        """Return community name"""
+        """Return community name."""
         return self._community_name
 
     @property
     def community_code(self):
-        """Return community code"""
+        """Return community code."""
         return self._community_code
 
     @property
     def pair_name(self):
-        """Return village pair name"""
+        """Return village pair name."""
         return self._pair
 
     @property
     def cpc(self):
-        """Returns True or False if community is cpc"""
+        """Returns True or False if community is cpc."""
         return self._is_cpc
 
     @property
     def bhs_start_date(self):
-        """Returns main bhs start date"""
+        """Returns main bhs start date."""
         return self._bhs_start_date
 
     @property
     def bhs_end_date(self):
-        """Returns main bhs end date"""
+        """Returns main bhs end date."""
         return self._main_bhs_end_date
 
     @property
     def bhs_mop_up_end_date(self):
-        """Returns bhs mop up start date"""
+        """Returns bhs mop up start date."""
         return self._bhs_mop_up_end_date
 
     @property
     def fuv_1_start_date(self):
-        """Returns follow up visit 1 start date"""
+        """Returns follow up visit 1 start date."""
         return self._fuv_1_end_date
 
     @property
     def fuv_1_end_date(self):
-        """Returns follow up visit 1 end date"""
+        """Returns follow up visit 1 end date."""
         return self._fuv_1_end_date
 
     @property
     def fuv_2_start_date(self):
-        """Returns follow up visit 2 start date"""
+        """Returns follow up visit 2 start date."""
         return self._fuv_2_start_date
 
     @property
     def fuv_2_end_date(self):
-        """Returns follow up visit 2 end date"""
+        """Returns follow up visit 2 end date."""
         return self._fuv_2_end_date
 
     @property
     def ess_start_date(self):
-        """Returns End of Study Survey start date"""
+        """Returns End of Study Survey start date."""
         return self._ess_start_date
 
     @property
     def ess_end_date(self):
-        """Returns End of Study Survey end date"""
+        """Returns End of Study Survey end date."""
         return self._ess_end_date
