6c0e87bb14153eb5043faedc9b2bac0a275cf2ee
erik van widenfelt <ew2789@gmail.com>
2015-03-07 09:38:51 +0200

    add class to handle plot related variables
    Former-commit-id: a778722156456f286331a8234b82eff302495686

--- bhp066/apps/bcpp_export/helpers/plot.py
+++ bhp066/apps/bcpp_export/helpers/plot.py
@@ -1,42 +1,60 @@
-from datetime import datetime
-
 from apps.bcpp_household.models import Household, HouseholdStructure, PlotLog, PlotLogEntry
 from apps.bcpp_household_member.models import HouseholdMember
-from apps.bcpp_household.constants import TWENTY_PERCENT, FIVE_PERCENT
+from apps.bcpp_household.constants import TWENTY_PERCENT, FIVE_PERCENT, CONFIRMED
+from apps.bcpp_survey.models import Survey
+
+from .base_helper import BaseHelper
 
 
-class Plot(object):
+class Plot(BaseHelper):
 
     def __init__(self, plot=None, household=None, household_structure=None, household_member=None):
-        self.instance_datetime = datetime.today()
-        self.plot_log = None
-        self.household_identifier = None
+        super(Plot, self).__init__()
+        self.household = household
         self.household_member = household_member
         self.household_structure = household_structure
         try:
             self.plot = household_member.household_structure.household.plot
-            self.household = household_member.household_structure.household
         except AttributeError:
             try:
                 self.plot = household_structure.household.plot
-                self.household = household_structure.household
             except AttributeError:
                 try:
                     self.plot = household.plot
-                    self.household = household
                 except AttributeError:
                     self.plot = plot
-                    self.household = Household.objects.filter(plot=self.plot)
-        self.plot_identifier = self.plot.plot_identifier
+        self.update_plots()
+        self.update_households()
+        self.update_members()
+        self.update_plot_log()
+
+    def __repr__(self):
+        return ('{0}(plot={1.plot!r})').format(self.__class__.__name__, self)
+
+    def __str__(self):
+        return '{0.plot!s}'.format(self)
+
+    def unique_key(self):
+        return self.plot_identifier
+
+    def customize_for_csv(self):
+        self.data['household_members'] = [hm.internal_identifier for hm in self.household_members]
+        super(Plot, self).customize_for_csv()
+        del self.data['plot']
+        del self.data['plot_logs']
+        del self.data['household_member']
+        del self.data['household_structure']
+        del self.data['household_structures']
+        del self.data['households']
+        del self.data['household']
+        del self.data['customized']
+
+    def update_plots(self):
+        self.plot_identifier = self.plot.plot_identifier if not self.plot.plot_identifier.endswith('-00') else 'clinic'
         self.gps_lat_target = self.plot.gps_target_lat
         self.gps_lon_target = self.plot.gps_target_lon
         self.gps_lat_actual = self.plot.gps_lat
         self.gps_lon_actual = self.plot.gps_lon
-        self.household_structure = HouseholdStructure.objects.filter(household__plot=self.plot)
-        self.household_member = HouseholdMember.objects.filter(household_structure__household__plot=self.plot)
-        self.household_member_count = len(list(set([hm.internal_identifier for hm in self.household_member])))
-        self.household_identifier = [h.household_identifier for h in self.household]
-        self.household_count = len(self.household)
         self.confirmed = self.plot.action
         self.status = self.plot.status
         self.gps_lat = self.plot.gps_lat
@@ -55,25 +73,43 @@ class Plot(object):
         self.htc_plot = True if self.plot.htc else False
         self.bhs_plot = True if self.plot.bhs else False
         self.community = self.plot.community
+
+    def update_plot_log(self):
         self.confirmed_date = None
+        if self.confirmed == CONFIRMED:
+            try:
+                self.confirmed_date = PlotLog.objects.filter(plot=self.plot).order_by('created')[0].created
+            except IndexError:
+                print 'Warning: Missing plot log for confirmed plot {}.'.format(self.plot)
         try:
-            self.plot_log = PlotLogEntry.objects.filter(plot_log=PlotLog.objects.get(plot=self.plot))
+            self.plot_logs = PlotLogEntry.objects.filter(plot_log=PlotLog.objects.get(plot=self.plot))
         except (PlotLog.DoesNotExist, PlotLogEntry.DoesNotExist):
-            self.plot_log = []
-        self.plot_first_visit_date = min(list(set([
-            l.report_datetime.date() for l in self.plot_log
+            self.plot_logs = []
+        self.plot_log_first_date = min(list(set([
+            l.report_datetime.date() for l in self.plot_logs
             ] or [self.plot.modified.date()])))
-        self.plot_visit_date = list(set([l.report_datetime.date() for l in self.plot_log] or [self.plot.modified.date()]))
-        self.plot_visit_date.sort()
-        self.plot_log_status = [p.log_status for p in self.plot_log]
+        self.plot_log_dates = list(set([l.report_datetime.date() for l in self.plot_logs] or [self.plot.modified.date()]))
+        self.plot_log_dates.sort()
+        self.plot_log_status = [p.log_status for p in self.plot_logs]
 
-    def __repr__(self):
-        return ('Plot(plot={0.plot!r}, household={0.household!r}, '
-                'household_structure={0.household_structure!r}, '
-                'household_member={0.household_member!r})').format(self)
+    def update_households(self):
+        self.households = Household.objects.filter(plot=self.plot)
+        self.household_structures = HouseholdStructure.objects.filter(household__plot=self.plot).order_by('survey__survey_slug')
+        self.household_identifiers = [h.household_identifier for h in self.households]
+        self.household_count = self.households.count()
 
-    def __str__(self):
-        return '{0.plot!s}'.format(self)
-
-    def unique_key(self):
-        return self.plot_identifier
+    def update_members(self):
+        self.household_members = HouseholdMember.objects.filter(household_structure__household__plot=self.plot)
+        for survey in Survey.objects.all():
+            setattr(
+                self, '{}_{}'.format('member_count', survey.survey_abbrev.lower()),
+                self.household_members.filter(household_structure__survey__survey_slug=survey.survey_slug).count()
+                )
+            setattr(
+                self, '{}_{}'.format('members_eligible', survey.survey_abbrev.lower()),
+                self.household_members.filter(household_structure__survey__survey_slug=survey.survey_slug, eligible_member=True).count()
+                )
+            setattr(
+                self, '{}_{}'.format('members_eligible_for_survey', survey.survey_abbrev.lower()),
+                self.household_members.filter(household_structure__survey__survey_slug=survey.survey_slug, eligible_subject=True).count()
+                )
