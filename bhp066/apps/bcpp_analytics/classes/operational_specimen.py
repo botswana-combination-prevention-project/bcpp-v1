62ae3d477d0171d862178f9ed46d4cd0411471db
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-03-12 13:54:27 +0200

    fixing operational specimen
    Former-commit-id: 8c54e88aea38d9913605a0d9dc92c9e0d50f7d21

--- bhp066/apps/bcpp_analytics/classes/operational_specimen.py
+++ bhp066/apps/bcpp_analytics/classes/operational_specimen.py
@@ -1,20 +1,17 @@
 import collections
 import datetime
-from django.db.models import Count
 from django.contrib.auth.models import User
 from edc.core.bhp_birt_reports.classes import OperationalReportUtilities
-from edc.device.dispatch.models import DispatchContainerRegister
+from edc.constants import NO
 
-from apps.bcpp_household.constants import (CONFIRMED, UNCONFIRMED, INACCESSIBLE, NON_RESIDENTIAL,
-                                           RESIDENTIAL_NOT_HABITABLE, RESIDENTIAL_HABITABLE)
+from apps.bcpp_lab.models import SubjectRequisition
 from apps.bcpp.choices import COMMUNITIES
-from apps.bcpp_household.models import Plot, PlotLogEntry, HouseholdStructure
 
 
 class OperationalSpecimen():
 
     def __init__(self, request):
-        self.plot_info = {}
+        self.specimen_info = {}
         self.utilities = OperationalReportUtilities()
         self.date_from = self.utilities.date_format_utility(request.GET.get('date_from', ''), '1960-01-01')
         self.date_to = self.utilities.date_format_utility(request.GET.get('date_to', ''), '2099-12-31')
@@ -34,87 +31,34 @@ class OperationalSpecimen():
     def return_ra_usernames(self):
         return self.ra_usernames
 
-    def return_plot_data(self):
+    def return_specimen_data(self):
 
         if self.community.find('----') != -1:
             self.community = ''
         if self.ra_username.find('----') != -1:
             self.ra_username = ''
         self.date_to += datetime.timedelta(days=1)
-        plt = Plot.objects.all()
-        dispatched_identifiers = [container.plot_identifier for container in DispatchContainerRegister.objects.filter(is_dispatched=True)]
-        total_dispatched = Plot.objects.filter(community__icontains=self.community,
-                                               modified__gte=self.date_from, modified__lte=self.date_to,
-                                               user_modified__icontains=self.ra_username,
-                                               plot_identifier__in=dispatched_identifiers)
-        self.plot_info['1. Total No. of plots'] = plt.filter(community__icontains=self.community,
-                                               modified__gte=self.date_from, modified__lte=self.date_to,
-                                               user_modified__icontains=self.ra_username).count()
-        self.plot_info['2. Total plots dispatched'] = len(total_dispatched)
-        #plot_info['1. Plots reached'] = reached
-        inaccessible_log_entry = PlotLogEntry.objects.filter(plot_log__plot__community__icontains=self.community,
-                                      plot_log__plot__status=INACCESSIBLE,
-                                      modified__gte=self.date_from, modified__lte=self.date_to,
-                                      user_modified__icontains=self.ra_username,
-                                      log_status=INACCESSIBLE)
-        annotated_log_enry = inaccessible_log_entry.values('plot_log__plot__plot_identifier').annotate(dcount=Count('plot_log__plot__plot_identifier'))
-        not_accessible_plots1 = 0
-        not_accessible_plots2 = 0
-        not_accessible_plots3 = 0
-        for group in annotated_log_enry:
-            if group.get('dcount') == 1:
-                not_accessible_plots1 += 1
-            if group.get('dcount') == 2:
-                not_accessible_plots2 += 1
-            if group.get('dcount') == 3:
-                not_accessible_plots3 += 1
-        accessible_plots = plt.filter(community__icontains=self.community,
-                                      modified__gte=self.date_from, modified__lte=self.date_to,
-                                      user_modified__icontains=self.ra_username).exclude(status=INACCESSIBLE).count()
-        self.plot_info['3. Plots accessible'] = accessible_plots
-        in_accessible_plots = plt.filter(community__icontains=self.community,
-                                      modified__gte=self.date_from, modified__lte=self.date_to,
-                                      user_modified__icontains=self.ra_username,
-                                      status=INACCESSIBLE).count()
-        self.plot_info['4. Plots in-accessible'] = in_accessible_plots
-        self.plot_info['5. Plots in-accessible once'] = not_accessible_plots1
-        self.plot_info['6. Plots in-accessible twice'] = not_accessible_plots2
-        self.plot_info['7. Plots in-accessible thrice'] = not_accessible_plots3
-        confirmed = plt.filter(action=CONFIRMED, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['8. Plots confirmed'] = confirmed
-        un_confirmed = plt.filter(action=UNCONFIRMED, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['9. Plots not confirmed'] = un_confirmed
-        plots_enmerated_structures = HouseholdStructure.objects.filter(household__plot__community__icontains=self.community,
-                                                             modified__gte=self.date_from, modified__lte=self.date_to,
-                                                             user_modified__icontains=self.ra_username,
-                                                             enumerated=True)
-        plots_enumerated = plots_enmerated_structures.values('household__plot__plot_identifier').annotate(dcount=Count('household__plot__plot_identifier'))
-        self.plot_info['91. Enumerated plots'] = len(plots_enumerated)
-        res_habitable = plt.filter(status=RESIDENTIAL_HABITABLE, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['92. Residential habitable plots'] = res_habitable
-        res_not_habitable = plt.filter(status=RESIDENTIAL_NOT_HABITABLE, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['93. Residential not habitable plots'] = res_not_habitable
-        non_residential = plt.filter(status=NON_RESIDENTIAL, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['94. Not residential plots'] = non_residential
-        enrolled_plots = plt.filter(bhs=True, community__icontains=self.community,
-                                        modified__gte=self.date_from, modified__lte=self.date_to,
-                                        user_modified__icontains=self.ra_username).count()
-        self.plot_info['95. Enrolled plots'] = enrolled_plots
-#         not_reached = (plt.filter(action=UNCONFIRMED, community__icontains=self.community,
-#                                                 modified__gte=self.date_from, modified__lte=self.date_to,
-#                                                 user_modified__icontains=self.ra_username).count())
-#         self.plot_info['12. Plots not reached'] = not_reached
-        values = collections.OrderedDict(sorted(self.plot_info.items()))
+        requisitions = SubjectRequisition.objects.filter(community__icontains=self.community,
+                                                      modified__gte=self.date_from, modified__lte=self.date_to,
+                                                      user_modified__icontains=self.ra_username)
+        number_requisitions = requisitions.count()
+        self.specimen_info['1. Total No. of requisitions'] = number_requisitions
+        requisitions_received = requisitions.filter(is_receive=True).count()
+        self.specimen_info['2. Total No. of requisitions received'] = requisitions_received
+        microtube_requisitions = requisitions.filter(panel__name='Microtube').count()
+        self.specimen_info['3. Total No. of microtube requisitions'] = microtube_requisitions
+        failed_microtube = requisitions.filter(panel__name='Microtube', is_drawn=NO).count()
+        self.specimen_info['4. Total No. of FAILED microtube requisitions'] = failed_microtube
+        viral_load_requisitions = requisitions.filter(panel__name='Viral Load').count()
+        self.specimen_info['5. Total No. of viral load requisitions'] = viral_load_requisitions
+        rbd_requisitions = requisitions.filter(panel__name='Research Blood Draw').count()
+        self.specimen_info['6. Total No. of research blood draw requisitions'] = rbd_requisitions
+        venous_requisitions = requisitions.filter(panel__name='Venous (HIV)').count()
+        self.specimen_info['7. Total No. of venous requisitions requisitions'] = venous_requisitions
+        elisa_requisitions = requisitions.filter(panel__name='ELISA').count()
+        self.specimen_info['8. Total No. of elisa requisitions requisitions'] = elisa_requisitions
+
+        values = collections.OrderedDict(sorted(self.specimen_info.items()))
         communities = []
         if (self.previous_community.find('----') == -1) and (not self.previous_community == ''):  # Passing filtered results
             # communities = [community[0].lower() for community in  COMMUNITIES]
