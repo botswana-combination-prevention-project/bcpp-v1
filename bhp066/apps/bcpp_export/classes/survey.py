dcf507d457ccbf7bc5ccd498383c976fce237e12
erik van widenfelt <ew2789@gmail.com>
2015-03-14 23:31:34 +0200

    add additional classes
    Former-commit-id: 923a0e11072e973686db5d2349fb86488fdb19a7

---
+++ bhp066/apps/bcpp_export/classes/survey.py
@@ -0,0 +1,46 @@
+from edc.map.classes import site_mappers
+
+from apps.bcpp_household.models import Plot
+from apps.bcpp_survey.models import Survey as SurveyModel
+
+from .base import Base
+
+
+class Survey(Base):
+
+    def __init__(self, community):
+        """Set attributes related to the surveys the member has been enumerated in."""
+        self.community = community
+        self.plot_count_all = Plot.objects.filter(community=self.community).count()
+        # self.plot_count_htc = Plot.objects.filter(community=self.community, htc=True).count()
+        # self.plot_count_bhs = Plot.objects.filter(community=self.community, bhs=True).count()
+        mapper = site_mappers.get_mapper(self.community)
+        self.survey_slugs = []
+        self.survey_abbrevs = []
+        fieldattrs = [
+            ('name', 'survey_name'),
+            ('start_date', 'survey_start_date'),
+            ('full_enrollment_date', 'survey_full_enrollment_date'),
+            ('smc_start_date', 'survey_smc_start_date')]
+        try:
+            self.pair = mapper.pair
+        except AttributeError:
+            self.pair = None
+        for survey in SurveyModel.objects.all().order_by('chronological_order'):
+            self.survey_slugs.append(survey.survey_slug)
+            self.survey_abbrevs.append(survey.survey_abbrev.lower())
+        for index, survey_slug in enumerate(self.survey_slugs):
+            try:
+                dates = mapper.survey_dates.get(survey_slug)
+            except AttributeError:
+                dates = None
+            attrs = {
+                'name': dates.name.upper() if dates else None,
+                'start_date': dates.start_date if dates else None,
+                'full_enrollment_date': dates.full_enrollment_date if dates else None,
+                'smc_start_date': dates.smc_start_date if dates else None,
+                }
+            self.denormalize(
+                self.survey_abbrevs[index], fieldattrs,
+                instance=type('Instance', (object, ), attrs)
+                )
---
+++ bhp066/apps/bcpp_export/collectors/subject_identifiers.py
@@ -0,0 +1,40 @@
+import csv
+from datetime import datetime
+import os
+
+from apps.bcpp_subject.models import SubjectConsent, HicEnrollment
+from apps.bcpp_clinic.models import ClinicConsent
+from .base_collector import BaseCollector
+
+
+class SubjectIdentifiers(BaseCollector):
+
+    def __init__(self):
+        """Generates a file of subject identifiers for each community.
+
+        For example, list can be used by auditors to select subjects to audit.
+        """
+        super(SubjectIdentifiers, self).__init__()
+        self.subject_identifiers = {}
+        for community in self.community_list:
+            with open(os.path.join(os.path.expanduser('~/export_to_cdc'), 'Enrolled{}-{}.csv'.format(community.title(), datetime.today().strftime('%Y%m%d%H%M%s'))), 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(['community', 'subject_identifier', 'type', 'survey'])
+                for subject_consent in SubjectConsent.objects.filter(
+                        household_member__household_structure__household__plot__community=community).order_by('subject_identifier'):
+                    writer.writerow([
+                        community, subject_consent.subject_identifier, 'SURVEY',
+                        subject_consent.household_member.household_structure.survey.survey_slug])
+                for hic_enrollment in HicEnrollment.objects.filter(
+                        subject_visit__household_member__household_structure__household__plot__community=community
+                        ).order_by('subject_visit__household_member__registered_subject__subject_identifier'):
+                    writer.writerow([
+                        community, hic_enrollment.subject_visit.household_member.registered_subject.subject_identifier, 'HIC',
+                        hic_enrollment.subject_visit.household_member.household_structure.survey.survey_slug])
+                for clinic_consent in ClinicConsent.objects.filter(
+                        household_member__household_structure__household__plot__community=community
+                        ).order_by('household_member__registered_subject__subject_identifier'):
+                    writer.writerow([
+                        community, clinic_consent.subject_identifier, 'RBD',
+                        clinic_consent.household_member.member_status,
+                        clinic_consent.household_member.household_structure.survey.survey_slug])
