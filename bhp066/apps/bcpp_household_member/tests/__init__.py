0b6ec003d3631bc8c489ea8cc08d30d9633cbd3a
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-03-18 16:23:43 +0200

    sexual_behaviour, validation for new household_member

--- bhp066/apps/bcpp_household_member/tests/__init__.py
+++ bhp066/apps/bcpp_household_member/tests/__init__.py
@@ -4,7 +4,7 @@ from .natural_key_tests import NaturalKeyTests
 # from .eligibility_model_tests import EligibilityModelTests
 # from .eligibility_form_tests import EligibilityFormTests
 # from .enrollment_checklist_tests import EnrollmentChecklistTests
-# from .household_member_tests import HouseholdMemberTests
+from .household_member_tests import HouseholdMemberTests
 
 from .test_member_status import TestMemberStatus
 from .test_member_status_choices import TestMemberStatusChoices
