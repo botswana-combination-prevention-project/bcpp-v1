8c4007cab036a21170b5874a6474aad6dfba1ce3
erik van widenfelt <ew2789@gmail.com>
2015-03-23 10:08:05 +0200

    FIX app config not updating survey chronological order.

--- bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
+++ bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
@@ -100,19 +100,22 @@ class BcppAppConfiguration(BaseAppConfiguration):
              'survey_slug': BASELINE_SURVEY_SLUG,
              'survey_abbrev': 'Y1',
              'datetime_start': study_start_datetime,
-             'datetime_end': datetime(2014, 12, 9, 23, 59, 0)},
+             'datetime_end': datetime(2014, 12, 9, 23, 59, 0),
+             'chronological_order': 1},
         'bcpp-year-2':
             {'survey_name': 'BCPP Year 2',
              'survey_slug': 'bcpp-year-2',
              'survey_abbrev': 'Y2',
              'datetime_start': datetime(2014, 12, 10, 0, 0, 0),
-             'datetime_end': datetime(2016, 11, 19, 23, 59, 0)},
+             'datetime_end': datetime(2016, 11, 19, 23, 59, 0),
+             'chronological_order': 2},
         'bcpp-year-3':
             {'survey_name': 'BCPP Year 3',
              'survey_slug': 'bcpp-year-3',
              'survey_abbrev': 'Y3',
              'datetime_start': datetime(2016, 11, 20, 0, 0, 0),
-             'datetime_end': datetime(2017, 10, 29, 23, 59, 0)}
+             'datetime_end': datetime(2017, 10, 29, 23, 59, 0),
+             'chronological_order': 3},
     }
 
     lab_clinic_api_setup = {
@@ -376,20 +379,27 @@ class BcppAppConfiguration(BaseAppConfiguration):
                 survey.survey_abbrev = survey_values.get('survey_abbrev')
                 survey.datetime_start = survey_values.get('datetime_start')
                 survey.datetime_end = survey_values.get('datetime_end')
+                survey.chronological_order = survey_values.get('chronological_order')
                 survey.save()
             except Survey.DoesNotExist:
                 Survey.objects.create(**survey_values)
 
     def search_limit_setup(self):
-        if not str(device) == '99':
-            if not (settings.LIMIT_EDIT_TO_CURRENT_SURVEY and settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY and settings.FILTERED_DEFAULT_SEARCH):
-                raise ImproperlyConfigured('LIMIT_EDIT_TO_CURRENT_SURVEY,  LIMIT_EDIT_TO_CURRENT_COMMUNITY and FILTERED_DEFAULT_SEARCH'
-                ' should be set to true in a notebook. Update in bcpp_settings.py.'
-                )
-        elif str(device) == '99':
-            if (settings.LIMIT_EDIT_TO_CURRENT_SURVEY and settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY and settings.FILTERED_DEFAULT_SEARCH):
-                raise ImproperlyConfigured('LIMIT_EDIT_TO_CURRENT_SURVEY,  LIMIT_EDIT_TO_CURRENT_COMMUNITY and FILTERED_DEFAULT_SEARCH'
-                ' should be set to false in a central server. Update in bcpp_settings.py.'
-                )
+        if str(device) == '99':
+            if (settings.LIMIT_EDIT_TO_CURRENT_SURVEY and
+                    settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY and
+                    settings.FILTERED_DEFAULT_SEARCH):
+                raise ImproperlyConfigured(
+                    'LIMIT_EDIT_TO_CURRENT_SURVEY,  LIMIT_EDIT_TO_CURRENT_COMMUNITY '
+                    'and FILTERED_DEFAULT_SEARCH should be set to false in a central '
+                    'server. Update in bcpp_settings.py.')
+        else:
+            if not (settings.LIMIT_EDIT_TO_CURRENT_SURVEY and
+                    settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY and
+                    settings.FILTERED_DEFAULT_SEARCH):
+                raise ImproperlyConfigured(
+                    'LIMIT_EDIT_TO_CURRENT_SURVEY,  LIMIT_EDIT_TO_CURRENT_COMMUNITY '
+                    'and FILTERED_DEFAULT_SEARCH should be set to true in a notebook. '
+                    'Update in bcpp_settings.py.')
 
 bcpp_app_configuration = BcppAppConfiguration()
