95023ff2c1fc667d1740cf1ad26bb4ae487126ed
ckgathi <ckgathi@bhp.org.bw>
2015-03-31 12:32:18 +0200

    rule_group tests
    Former-commit-id: 20ec0e325faada980576b8ca2f33d6ddcb863481

--- bhp066/apps/bcpp_subject/tests/__init__.py
+++ bhp066/apps/bcpp_subject/tests/__init__.py
@@ -7,7 +7,7 @@
 from .test_referral import TestReferral
 # from .test_referral_annual import TestReferralAnnual
 # from .test_hic_enrollment import TestHicEnrollment
-# from .test_rule_group import TestRuleGroup
+from .test_rule_group import TestRuleGroup
 # from .test_subject_status_helper import TestSubjectStatusHelper
 from .test_subject_referral_appt_helper import TestSubjectReferralApptHelper
 # from .test_next_clinic_date import TestNextClinicDate
