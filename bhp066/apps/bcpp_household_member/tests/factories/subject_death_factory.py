6f253a023daae15fe927dffd8498c80b001b1107
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-04-13 17:00:37 +0200

    subject_death_factory
    Former-commit-id: c3ef655ffbf390b3c06b31f8551e01d68faa1187

---
+++ bhp066/apps/bcpp_household_member/tests/factories/subject_death_factory.py
@@ -0,0 +1,25 @@
+import factory
+
+from datetime import datetime
+
+from edc.base.model.tests.factories import BaseUuidModelFactory
+from edc.subject.adverse_event.tests.factories import DeathCauseCategoryFactory, DeathCauseInfoFactory
+from apps.bcpp_survey.tests.factories import SurveyFactory
+
+from ...models import SubjectDeath
+
+from .household_member_factory import HouseholdMemberFactory
+
+
+class SubjectDeathFactory(BaseUuidModelFactory):
+
+    class Meta:
+        model = SubjectDeath
+
+    household_member = factory.SubFactory(HouseholdMemberFactory)
+    report_datetime = datetime.today()
+    survey = factory.SubFactory(SurveyFactory)
+    death_date = datetime(2015, 4, 11)
+    participant_hospitalized = 'No'
+    death_cause_category = factory.SubFactory(DeathCauseCategoryFactory)
+    death_cause_info = factory.SubFactory(DeathCauseInfoFactory)
