9a5d3443501623e82a5e5e7ce874ff00b8e86972
ckgathi <ckgathi@bhp.org.bw>
2015-05-18 15:00:23 +0200

    five percent and twenty percent as int

--- bhp066/apps/bcpp_household/constants.py
+++ bhp066/apps/bcpp_household/constants.py
@@ -4,7 +4,7 @@ CONFIRMED = 'confirmed'
 DECEASED = 'deceased'
 ELIGIBLE_REPRESENTATIVE_ABSENT = 'eligible_representative_absent'
 ELIGIBLE_REPRESENTATIVE_PRESENT = 'eligible_representative_present'
-FIVE_PERCENT = '2'  # plot.selected=2
+FIVE_PERCENT = 2  # plot.selected=2
 INACCESSIBLE = 'inaccessible'
 NON_RESIDENTIAL = 'non_residential'
 NO_HOUSEHOLD_INFORMANT = 'no_household_informant'
@@ -14,7 +14,7 @@ RESIDENTIAL_NOT_HABITABLE = 'residential_not_habitable'
 RARELY_NEVER_OCCUPIED = 'rarely_or_never_occupied'  # NOT replaced
 SEASONALLY_NEARLY_ALWAYS_OCCUPIED = 'seasonally_nearly_present'  # replace
 SEVENTY_FIVE_PERCENT = None
-TWENTY_PERCENT = '1'
+TWENTY_PERCENT = 1
 UNCONFIRMED = 'unconfirmed'
 UNKNOWN_OCCUPIED = 'unknown'  # replaced
 VISIT_ATTEMPTS = 3
