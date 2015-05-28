2482ebfaa65d3923a62449cf01db666db12a5b4f
ckgathi <ckgathi@bhp.org.bw>
2015-05-28 11:11:50 +0200

    landmarks and new mappers
    Former-commit-id: 6363a49d36ab32526dac8c70e345ced6a91e0c18

---
+++ bhp066/apps/bcpp_household/mappers/mathangwane_plot_mapper.py
@@ -0,0 +1,63 @@
+from dateutil.relativedelta import MO, TU, WE, TH, FR
+from datetime import date
+
+from edc.map.classes import site_mappers
+
+from ..constants import BASELINE_SURVEY_SLUG
+from ..utils import ClinicDaysTuple, SurveyDatesTuple
+
+from .base_plot_mapper import BasePlotMapper
+from .choices import SECTIONS, SUB_SECTIONS, MATHANGWANE_LANDMARKS
+
+
+class MathangwanePlotMapper(BasePlotMapper):
+
+    map_area = 'mathangwane'
+    map_code = '31'
+    regions = SECTIONS
+    sections = SUB_SECTIONS
+
+    landmarks = MATHANGWANE_LANDMARKS
+
+    intervention = True
+
+    gps_center_lat = -20.993214
+    gps_center_lon = 27.333963
+    radius = 6.5
+    location_boundary = ()
+
+    survey_dates = {
+        BASELINE_SURVEY_SLUG: SurveyDatesTuple(
+            name='bhs',
+            start_date=date(2015, 5, 1),
+            full_enrollment_date=date(2015, 5, 31),
+            end_date=date(2015, 6, 21),
+            smc_start_date=date(2015, 8, 10)),
+        'bcpp-year-2': SurveyDatesTuple(
+            name='t1',
+            start_date=date(2015, 11, 21),
+            full_enrollment_date=date(2015, 12, 17),
+            end_date=date(2015, 12, 22),
+            smc_start_date=date(2015, 12, 22)),
+        'bcpp-year-3': SurveyDatesTuple(
+            name='t2',
+            start_date=None,
+            full_enrollment_date=None,
+            end_date=None,
+            smc_start_date=None),
+    }
+
+    clinic_days = {
+        BASELINE_SURVEY_SLUG: {
+            'IDCC': ClinicDaysTuple((MO, WE, ), None),
+            'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates[BASELINE_SURVEY_SLUG].smc_start_date)},
+        'bcpp-year-2': {
+            'IDCC': ClinicDaysTuple((MO, ), None),
+            'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates['bcpp-year-2'].smc_start_date)},
+    }
+
+site_mappers.register(MathangwanePlotMapper)
