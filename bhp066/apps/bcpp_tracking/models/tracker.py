bd6d83de6d3dc10fc74c0554b07e04e44ac6a80c
ckgathi <ckgathi@bhp.org.bw>
2015-06-05 16:36:00 +0200

    models for tracking
    Former-commit-id: d8b7ae7fda810eb19fe58ab6f3e517a68f6f469f

--- bhp066/apps/bcpp_tracking/models/tracker.py
+++ bhp066/apps/bcpp_tracking/models/tracker.py
@@ -2,8 +2,15 @@ from django.db import models
 
 from edc.base.model.models import BaseModel
 
+from .site_tracker import SiteTracker
+
 
 class Tracker(BaseModel):
+
+    update_date = models.DateTimeField(
+         verbose_name='Start Date/Time',
+         null=True
+        )
 
     app_name = models.CharField(
         max_length=100,
@@ -13,15 +20,16 @@ class Tracker(BaseModel):
         max_length=150,
         )
 
-    name = models.CharField(
-        max_length=200,
-        )
-
-    limit = models.IntegerField(
+    tracked_value = models.IntegerField(
         default=0,
         editable=True,
         help_text=('Updated by a signal on <<<>>>. '
                    'Number of <<><>>>.')
+    )
+
+    value_limit = models.IntegerField(
+        default=400,
+        editable=True,
     )
 
     start_date = models.DateTimeField(
@@ -34,5 +42,53 @@ class Tracker(BaseModel):
         null=True
         )
 
+    def save(self, *args, **kwargs):
+        using = kwargs.get('using,')
+        super(Tracker, self).save(*args, **kwargs)
+
+
     class Meta:
-        abstract = True
+        app_label = 'bcpp_tracking'
+
+class SiteTracker(BaseDispatchSyncUuidModel):
+
+    tracker = models.ForeignKey(Tracker)
+
+    update_date = models.DateTimeField(
+         verbose_name='Start Date/Time',
+         null=True
+        )
+
+    app_name = models.CharField(
+        max_length=100,
+        )
+
+    model = models.CharField(
+        max_length=150,
+        )
+
+    tracked_value = models.IntegerField(
+        default=0,
+        editable=True,
+        help_text=('Updated by a signal on <<<>>>. '
+                   'Number of <<><>>>.')
+    )
+
+    site_name = models.CharField(
+        max_length=200,
+        )
+
+    start_date = models.DateTimeField(
+         verbose_name='Start Date/Time',
+         null=True
+        )
+
+    end_date = models.DateTimeField(
+        verbose_name='End Date/Time',
+        null=True
+        )
+
+
+    def save(self, *args, **kwargs):
+        using = kwargs.get('using,')
+        super(SiteTracker, self).save(*args, **kwargs)
