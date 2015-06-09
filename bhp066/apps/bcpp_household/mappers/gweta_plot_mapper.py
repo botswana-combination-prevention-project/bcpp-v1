1f88a465952978ceff60a6d2773b2e246aa2c258
ckgathi <ckgathi@bhp.org.bw>
2015-06-09 13:30:22 +0200

    mappers

---
+++ bhp066/apps/bcpp_household/mappers/gweta_plot_mapper.py
@@ -0,0 +1,64 @@
+from dateutil.relativedelta import MO, TU, WE, TH, FR
+from datetime import date
+
+from edc.map.classes import site_mappers
+
+from ..constants import BASELINE_SURVEY_SLUG
+from ..utils import ClinicDaysTuple, SurveyDatesTuple
+
+from .base_plot_mapper import BasePlotMapper
+from .choices import SECTIONS, SUB_SECTIONS, GWETA_LANDMARKS
+
+
+class GwetaPlotMapper(BasePlotMapper):
+
+    map_area = 'GWETA'
+    map_code = '34'
+    pair = 7
+    regions = SECTIONS
+    sections = SUB_SECTIONS
+
+    landmarks = GWETA_LANDMARKS
+
+    intervention = False
+
+    gps_center_lat = -20.205621
+    gps_center_lon = 25.251474
+    radius = 5.5
+    location_boundary = ()
+
+    survey_dates = {
+        BASELINE_SURVEY_SLUG: SurveyDatesTuple(
+            name='bhs',
+            start_date=date(2015, 6, 1),
+            full_enrollment_date=date(2015, 6, 20),
+            end_date=date(2015, 6, 21),
+            smc_start_date=date(2015, 2, 13)),
+        'bcpp-year-2': SurveyDatesTuple(
+            name='t1',
+            start_date=date(2015, 11, 21),
+            full_enrollment_date=date(2015, 12, 17),
+            end_date=date(2015, 12, 22),
+            smc_start_date=date(2015, 12, 22)),
+        'bcpp-year-3': SurveyDatesTuple(
+            name='t2',
+            start_date=None,
+            full_enrollment_date=None,
+            end_date=None,
+            smc_start_date=None),
+    }
+
+    clinic_days = {
+        BASELINE_SURVEY_SLUG: {
+            'IDCC': ClinicDaysTuple((FR, ), None),
+            'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates[BASELINE_SURVEY_SLUG].smc_start_date)},
+        'bcpp-year-2': {
+            'IDCC': ClinicDaysTuple((MO, ), None),
+            'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
+            'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates['bcpp-year-2'].smc_start_date)},
+    }
+
+site_mappers.register(GwetaPlotMapper)
