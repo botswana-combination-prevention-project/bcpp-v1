0e8a2dc688f5ce03d9f4a7184d3bcae740e6a7f9
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-06-10 15:02:26 +0200

    correcting mathangwane idcc day

--- bhp066/apps/bcpp_household/mappers/mathangwane_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/mathangwane_plot_mapper.py
@@ -49,7 +49,7 @@ class MathangwanePlotMapper(BasePlotMapper):
 
     clinic_days = {
         BASELINE_SURVEY_SLUG: {
-            'IDCC': ClinicDaysTuple((TU, ), None),
+            'IDCC': ClinicDaysTuple((TH, ), None),
             'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates[BASELINE_SURVEY_SLUG].smc_start_date)},
