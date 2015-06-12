9ceb2adcb0f2eab6191d15763ac28ac750ef0c71
ckgathi <ckgathi@bhp.org.bw>
2015-06-12 16:14:22 +0200

    tracker methods
    Former-commit-id: b4abc4ca2c0201b29455bb8e9d027d1a5f1edc19

--- bhp066/apps/bcpp_dispatch/classes/bcpp_consumer.py
+++ bhp066/apps/bcpp_dispatch/classes/bcpp_consumer.py
@@ -1,5 +1,11 @@
 from edc.device.sync.classes import Consumer
 
+from apps.bcpp_tracking.classes import TrackerHelper
+
 
 class BcppConsumer(Consumer):
-    pass
+
+    def post_sync(self, using=None, lock_name=None, **kwargs):
+        """Overides post_sync to update tracker values."""
+
+        TrackerHelper().update_trackers()
