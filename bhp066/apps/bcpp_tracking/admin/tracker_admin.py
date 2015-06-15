f843893f54fa8ded80bd5d0edc0bfc8e59650cf7
ckgathi <ckgathi@bhp.org.bw>
2015-06-15 10:43:29 +0200

    admin forms
    Former-commit-id: 7c02c8cabec52c381f18f3c5892e7078f27c81de

--- bhp066/apps/bcpp_tracking/admin/tracker_admin.py
+++ bhp066/apps/bcpp_tracking/admin/tracker_admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from edc.base.modeladmin.admin import BaseModelAdmin
-from apps.bcpp_household.models import SiteTracker, Tracker
-from apps.bcpp_household.forms import TrackerForm, SiteTrackerForm
+from apps.bcpp_tracking.models import SiteTracker, Tracker
+from apps.bcpp_tracking.forms import TrackerForm, SiteTrackerForm
 
 
 class SiteTrackerAdmin(BaseModelAdmin):
@@ -10,14 +10,14 @@ class SiteTrackerAdmin(BaseModelAdmin):
     list_per_page = 15
     list_display = ('tracked_value', 'site_name', 'update_date', 'tracker')
     list_filter = ('end_date', 'start_date', 'tracked_value', 'site_name', 'update_date')
-    search_fields = ('site_name')
+    search_fields = ['site_name']
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "tracker":
             kwargs["queryset"] = Tracker.objects.filter(id__exact=request.GET.get('tracker', 0))
         return super(SiteTrackerAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register(Tracker, SiteTrackerAdmin)
+admin.site.register(SiteTracker, SiteTrackerAdmin)
 
 
 class SiteTrackerInline(admin.TabularInline):
@@ -32,6 +32,7 @@ class TrackerAdmin(BaseModelAdmin):
     inlines = [SiteTrackerInline, ]
     date_hierarchy = 'modified'
     list_per_page = 15
-    list_display = ('end_date', 'start_date', 'tracked_value', 'model', 'app_name', 'update_date', 'tracker', 'value_limit')
+    list_display = ('end_date', 'start_date', 'tracked_value', 'name', 'model', 'app_name', 'update_date', 'value_limit')
     list_filter = ('end_date', 'start_date', 'update_date')
+    search_fields = ['name']
 admin.site.register(Tracker, TrackerAdmin)
