798fab20d0a7d734ead54f547aee8129fe778985
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-06-25 08:18:59 +0200

    sexual behaviour forms, skip logic
    Former-commit-id: d579ec32663f37350a8121f2e05abafcc30b5f54

--- bhp066/apps/bcpp_subject/models/months_second_partner.py
+++ bhp066/apps/bcpp_subject/models/months_second_partner.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+from edc.constants import NOT_APPLICABLE
 from edc.audit.audit_trail import AuditTrail
 
 from .base_sexual_partner import BaseSexualPartner
@@ -6,7 +9,18 @@ from .base_sexual_partner import BaseSexualPartner
 class MonthsSecondPartner (BaseSexualPartner):
     """A model completed by the user on the participant's recent sexual behaviour."""
 
+    second_partner_arm = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+                )
     history = AuditTrail()
+
+    def save(self, *args, **kwargs):
+        self.second_partner_arm = self.is_ecc_or_cpc() if self.is_ecc_or_cpc() else NOT_APPLICABLE\
+                                                            if self.sex_partner_community == NOT_APPLICABLE else\
+                                                                    'OTHER' if self.sex_partner_community == 'OTHER' else ''
+        super(MonthsSecondPartner, self).save(*args, **kwargs)
 
     class Meta:
         app_label = 'bcpp_subject'
