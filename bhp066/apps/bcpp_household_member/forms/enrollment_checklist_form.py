df3a5fd03f735a6b08d38c7c52b3f34bea72ca3f
ckgathi <ckgathi@bhp.org.bw>
2015-07-03 15:44:01 +0200

    enrollment form bug and IDDC date for gweta

--- bhp066/apps/bcpp_household_member/forms/enrollment_checklist_form.py
+++ bhp066/apps/bcpp_household_member/forms/enrollment_checklist_form.py
@@ -29,14 +29,6 @@ class EnrollmentChecklistForm(BaseModelForm):
             if not cleaned_data.get('marriage_certificate') == NOT_APPLICABLE:
                 raise forms.ValidationError('Marriage Certificate is not applicable, Participant is a citizen.')
 
-        if cleaned_data.get('study_participation') == 'Yes':
-            if not cleaned_data.get('confirm_participation'):
-                raise forms.ValidationError('Confirmation on study participation required.')
-
-        if cleaned_data.get('study_participation') == 'No':
-            if cleaned_data.get('confirm_participation'):
-                raise forms.ValidationError('Confirmation on study participation is not required.')
-
         if cleaned_data.get('citizen') == 'No':
             if cleaned_data.get('legal_marriage') == NOT_APPLICABLE:
                 raise forms.ValidationError('Participant is not a citizen, indicate if he/she is legally '
