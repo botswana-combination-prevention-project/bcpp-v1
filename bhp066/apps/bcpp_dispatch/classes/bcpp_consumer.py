f308fdb524d2bb14ae14aed04fd394350ffe1673
ckgathi <ckgathi@bhp.org.bw>
2015-07-15 17:52:03 +0200

    delete tracker and where its called
    Former-commit-id: 1aa7cfec33aa9c0122fc631c473eb7dc4241f7f1

--- bhp066/apps/bcpp_dispatch/classes/bcpp_consumer.py
+++ bhp066/apps/bcpp_dispatch/classes/bcpp_consumer.py
@@ -1,11 +1,8 @@
 from edc.device.sync.classes import Consumer
 
-from apps.bcpp_tracking.classes import TrackerHelper
-
 
 class BcppConsumer(Consumer):
 
     def post_sync(self, using=None, lock_name=None, **kwargs):
         """Overides post_sync to update tracker values."""
 
-        TrackerHelper().update_trackers()
