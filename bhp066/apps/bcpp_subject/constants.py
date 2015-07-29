f8017a1e4a53b32cb8d14d4d25d1344a7bfd8349
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-07-29 16:28:28 +0200

    creating group and user in unit test
    Former-commit-id: 397130c4624407ac5c3be745432b9f68f9a81f09

--- bhp066/apps/bcpp_subject/constants.py
+++ bhp066/apps/bcpp_subject/constants.py
@@ -6,3 +6,5 @@ ANNUAL_CODES = ['T1', 'T2', 'T3']
 ANNUAL = 'annual'
 PIMA_VL_TYPE = 'Mobile settings'
 CENTRAL_SERVER = 'central'
+FIELD_SUPERVISOR_GROUP = 'field_supervisor'
+FIELD_RESEARCH_ASSISTANT_GROUP = 'field_research_assistant'
