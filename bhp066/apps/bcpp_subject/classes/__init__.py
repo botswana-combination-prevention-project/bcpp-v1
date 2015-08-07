2488dba1ddda2d82639292a46b976936744f2618
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-07 16:16:39 +0200

    re factored rule group functions
    Former-commit-id: e0047f889130e364386fc81248f4dceed214f456

--- bhp066/apps/bcpp_subject/classes/__init__.py
+++ bhp066/apps/bcpp_subject/classes/__init__.py
@@ -2,3 +2,4 @@ from .subject_status_helper import SubjectStatusHelper
 from .subject_referral_helper import SubjectReferralHelper
 from .subject_referral_appt_helper import SubjectReferralApptHelper
 from .call_helper import CallHelper
+from .rule_group_utilities import *
--- bhp066/apps/bcpp_subject/tests/__init__.py
+++ bhp066/apps/bcpp_subject/tests/__init__.py
@@ -1,16 +1,18 @@
 # from .base_scheduled_model_test_case import BaseScheduledModelTestCase
+from .base_rule_group_test_setup import BaseRuleGroupTestSetup
 # from .test_forms import TestForms
 # from .test_natural_key import TestNaturalKey
 # from .test_choice import TestChoice
 # from .test_consent_history import TestConsentHistory
 # from .test_consent import TestConsent
-from .test_referral import TestReferral
+#from .test_referral import TestReferral
 # from .test_referral_annual import TestReferralAnnual
 # from .test_hic_enrollment import TestHicEnrollment
 from .test_rule_group import TestRuleGroup
+from .test_rule_group_utilities import TestRuleGroupUtilities
 # from .test_subject_status_helper import TestSubjectStatusHelper
-from .test_subject_referral_appt_helper import TestSubjectReferralApptHelper
+#from .test_subject_referral_appt_helper import TestSubjectReferralApptHelper
 # from .test_next_clinic_date import TestNextClinicDate
-from .test_correct_consent import TestCorrectConsent
+#from .test_correct_consent import TestCorrectConsent
 
-from .test_sexual_behaviour_form import TestSexualBehaviourForm
+#from .test_sexual_behaviour_form import TestSexualBehaviourForm
