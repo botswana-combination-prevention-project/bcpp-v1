5e2366e95805bcdeb7dd0e53cc85b1d2aaedc1d3
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-08-10 20:19:15 +0200

    plots notebook plot list distributes
    Former-commit-id: 0855916bdf7c5999fa5189f4c3ba675f5d6a4934

---
+++ bhp066/apps/bcpp_household/tests/test_notebook_plot_list.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+
+from apps.bcpp_household.tests.factories.plot_factory import PlotFactory
+from apps.bcpp_household.classes.notebook_plot_allocation import NotebookPlotAllocation
+from apps.bcpp_household.models.plot import Plot
+from apps.bcpp_household.models.notebook_plot_list import NotebookPlotList
+
+from edc.map.classes import site_mappers
+from edc.subject.lab_tracker.classes import site_lab_tracker
+from edc.subject.rule_groups.classes import site_rule_groups
+from edc.lab.lab_profile.exceptions import AlreadyRegistered as AlreadyRegisteredLabProfile
+from edc.lab.lab_profile.classes import site_lab_profiles
+
+from apps.bcpp_lab.lab_profiles import BcppSubjectProfile
+from apps.bcpp.app_configuration.classes.app_configuration import BcppAppConfiguration
+from apps.bcpp_subject.visit_schedule.bcpp_subject import BcppSubjectVisitSchedule
+
+
+class TestNotebookPlotAllocation(TestCase):
+
+    community = 'otse'
+
+    def setUp(self):
+        try:
+            site_lab_profiles.register(BcppSubjectProfile())
+        except AlreadyRegisteredLabProfile:
+            pass
+        BcppAppConfiguration().prepare()
+        site_lab_tracker.autodiscover()
+        BcppSubjectVisitSchedule().build()
+        site_rule_groups.autodiscover()
+
+    def test_allocation_even(self):
+        Plot.objects.all().delete()
+        for _ in range(0, 50):
+            PlotFactory(community=self.community)
+        plots = Plot.objects.all()
+        self.assertEqual(50, plots.count())
+        allocation = NotebookPlotAllocation(plots, 5)
+        allocation.distributed_notebook_plot_list
+        for i in range(0, 5):
+            self.assertEqual(len(allocation.machine_notebook_plot_list(i)), 10)
+
+    def test_distribute_remainder(self):
+        Plot.objects.all().delete()
+        for _ in range(0, 13):
+            PlotFactory(community=self.community)
+        plots = Plot.objects.all()
+        self.assertEqual(13, plots.count())
+        allocation = NotebookPlotAllocation(plots, 2)
+        allocation.distributed_notebook_plot_list
+        for i in range(0, 1):
+            self.assertEqual(len(allocation.machine_notebook_plot_list(i)), 7)
+
+    def test_create_notebook_plot(self):
+        Plot.objects.all().delete()
+        for _ in range(0, 13):
+            PlotFactory(community=self.community)
+        plots = Plot.objects.all()
+        self.assertEqual(13, plots.count())
+        allocation = NotebookPlotAllocation(plots, 2)
+        allocation.distributed_notebook_plot_list
+
+        allocation.create_notebook_plot_list(0)
+        self.assertEqual(NotebookPlotList.objects.all().count(), 7)
