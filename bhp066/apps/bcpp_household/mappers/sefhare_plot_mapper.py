60c6100b493ea9597eff7525e1df0bb5c2c28ce0
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-28 14:05:40 +0200

    sefhare plot mapper
    Former-commit-id: f1029be971e55837f2710871b4482a16e739b4c1

--- bhp066/apps/bcpp_household/mappers/sefhare_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/sefhare_plot_mapper.py
@@ -50,7 +50,7 @@ class SefharePlotMapper(BasePlotMapper):
 
     clinic_days = {
         BASELINE_SURVEY_SLUG: {
-            'IDCC': ClinicDaysTuple((FR, ), None),
+            'IDCC': ClinicDaysTuple((MO, TU, TH, ), None),
             'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates[BASELINE_SURVEY_SLUG].smc_start_date)},
