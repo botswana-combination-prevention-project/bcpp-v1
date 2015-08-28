e3864e076cf93e74070cde6d797f62c99a75b999
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-28 14:17:06 +0200

    tsetsebjwe idcc date
    Former-commit-id: b698b348d73991b948d345b3516923447eea1401

--- bhp066/apps/bcpp_household/mappers/tsetsebjwe_plot_mapper.py
+++ bhp066/apps/bcpp_household/mappers/tsetsebjwe_plot_mapper.py
@@ -50,7 +50,7 @@ class TsetsebjwePlotMapper(BasePlotMapper):
 
     clinic_days = {
         BASELINE_SURVEY_SLUG: {
-            'IDCC': ClinicDaysTuple((FR, ), None),
+            'IDCC': ClinicDaysTuple((WE, ), None),
             'ANC': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'VCT': ClinicDaysTuple((MO, TU, WE, TH, FR), None),
             'SMC': ClinicDaysTuple((MO, TU, WE, TH, FR), survey_dates[BASELINE_SURVEY_SLUG].smc_start_date)},
