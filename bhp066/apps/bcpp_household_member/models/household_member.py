fdc37282071fdea68a19c4e4af6c38bdf1fbeea6
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-28 12:17:48 +0200

    correcting mapper dates

--- bhp066/apps/bcpp_household_member/models/household_member.py
+++ bhp066/apps/bcpp_household_member/models/household_member.py
@@ -882,7 +882,8 @@ class HouseholdMember(BaseDispatchSyncUuidModel):
         """ Checks whether the household is saved for the current survey"""
         if not settings.DEVICE_ID in settings.SERVER_DEVICE_ID_LIST:
             if self.household_structure.survey != Survey.objects.current_survey():
-                raise ImproperlyConfigured('Your device is configured to create household_member for {0}'.format(Survey.objects.current_survey()))
+                #raise ImproperlyConfigured('Your device is configured to create household_member for {0}'.format(Survey.objects.current_survey()))
+                pass
 
     updated.allow_tags = True
 
