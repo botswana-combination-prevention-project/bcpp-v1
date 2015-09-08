34fae711f4d8113619893bbb7123f076889eda9e
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-08 12:31:59 +0200

    making sure an aliquot identifier cannot be reused
    Former-commit-id: d068f233240a16c8107dace1f62dff1f6a694ff7

--- bhp066/apps/bcpp_lab/models/signals.py
+++ bhp066/apps/bcpp_lab/models/signals.py
@@ -2,12 +2,13 @@ from datetime import datetime
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import transaction
+from django.db.utils import IntegrityError
 
 from apps.bcpp_subject.classes import func_poc_vl
 from apps.bcpp_subject.constants import CLOSED, VIRAL_LOAD, ARBIT_VIRAL_LOAD, POC_VIRAL_LOAD
 
 from ..models import ClinicRequisition, SubjectRequisition, PreOrder, Panel, Order
-from django.db import transaction
 
 
 @receiver(post_save, weak=False, dispatch_uid="clinic_requisition_on_post_save")
@@ -32,10 +33,22 @@ def create_requisition_preorder_on_post_save(sender, instance, raw, created, usi
                 if instance.panel.name == VIRAL_LOAD and func_poc_vl(instance.subject_visit):
                     poc_viral_load = Panel.objects.get(name=POC_VIRAL_LOAD)
                     arbit_viral_load = Panel.objects.get(name=ARBIT_VIRAL_LOAD)
-                    PreOrder.objects.create(panel=poc_viral_load, preorder_datetime=datetime.now(), subject_identifier=instance.subject_identifier)
-                    PreOrder.objects.create(panel=arbit_viral_load, preorder_datetime=datetime.now(), subject_identifier=instance.subject_identifier)
+                    try:
+                        PreOrder.objects.create(panel=poc_viral_load, preorder_datetime=datetime.now(), subject_visit=instance.subject_visit)
+                        PreOrder.objects.create(panel=arbit_viral_load, preorder_datetime=datetime.now(), subject_visit=instance.subject_visit)
+                    except IntegrityError as integrity_error:
+                        if 'unique' in str(integrity_error):
+                            pass
+                        else:
+                            raise integrity_error
                 else:
-                    PreOrder.objects.create(panel=instance.panel, preorder_datetime=datetime.now(), subject_identifier=instance.subject_identifier)
+                    try:
+                        PreOrder.objects.create(panel=instance.panel, preorder_datetime=datetime.now(), subject_visit=instance.subject_visit)
+                    except IntegrityError as integrity_error:
+                        if 'unique' in str(integrity_error):
+                            pass
+                        else:
+                            raise integrity_error
 
 
 @receiver(post_save, weak=False, dispatch_uid="create_order_on_pre_save")
