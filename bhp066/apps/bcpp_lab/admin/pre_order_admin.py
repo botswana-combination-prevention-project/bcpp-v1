e82093e4e57e7ff4cfd7b071486bb3cb76f73a53
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-08 12:31:59 +0200

    making sure an aliquot identifier cannot be reused

--- bhp066/apps/bcpp_lab/admin/pre_order_admin.py
+++ bhp066/apps/bcpp_lab/admin/pre_order_admin.py
@@ -7,9 +7,9 @@ from ..models import PreOrder
 
 class PreOrderAdmin(BaseModelAdmin):
 
-    list_display = ('subject_identifier', 'panel', 'preorder_datetime', 'status', 'aliquot_identifier')
+    list_display = ('subject_visit', 'panel', 'preorder_datetime', 'status', 'aliquot_identifier')
     list_filter = ('panel', 'preorder_datetime', 'status',)
-    search_fields = ('status', 'subject_identifier')
+    search_fields = ('status', 'ssubject_visit__subject_identifier')
     list_editable = ('aliquot_identifier', )
 
 admin.site.register(PreOrder, PreOrderAdmin)
