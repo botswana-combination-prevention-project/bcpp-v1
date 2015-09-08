4831333c0acf078e8f610cf3b14bbb14ce35c3cc
Florence Chilisa <fchilisa@fchilisa.local>
2015-09-08 19:23:43 +0200

    pimavl quota
    Former-commit-id: 9e1e86fa06525950655d9a02ba6c20151fc05bef

--- bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
+++ bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
@@ -13,7 +13,7 @@ from edc.device.device.classes import device
 from edc.lab.lab_packing.models import DestinationTuple
 from edc.lab.lab_profile.classes import ProfileItemTuple, ProfileTuple
 from edc.map.classes import site_mappers
-from edc_quota.client.models import Quota, QuotaModelWithOverride
+from edc_quota.client.models import Quota
 from edc_quota.controller.models import ControllerQuota
 from lis.labeling.classes import LabelPrinterTuple, ZplTemplateTuple, ClientTuple
 from lis.specimen.lab_aliquot_list.classes import AliquotTypeTuple
@@ -37,7 +37,7 @@ class BcppAppConfiguration(BaseAppConfiguration):
         super(BcppAppConfiguration, self).prepare()
         self.update_or_create_survey()
         self.search_limit_setup()
-        self.create_quota()
+#         self.create_quota()
 
     global_configuration = {
         'dashboard':
@@ -414,37 +414,37 @@ class BcppAppConfiguration(BaseAppConfiguration):
                     'and FILTERED_DEFAULT_SEARCH should be set to true in a notebook. '
                     'Update in bcpp_settings.py.')
 
-    def create_quota(self):
-        for ct in ContentType.objects.filter(app_label='bcpp_subject'):
-            if ct is None:
-                continue
-            if issubclass(ct.model_class(), Quota) or issubclass(ct.model_class(), QuotaModelWithOverride):
-                if device.is_community_server:
-                    try:
-                        ControllerQuota.objects.get(
-                            app_label=ct.model_class()._meta.app_label,
-                            model_name=ct.model_class()._meta.model_name,
-                        )
-                    except ControllerQuota.DoesNotExist:
-                        ControllerQuota.objects.create(
-                            app_label=ct.model_class()._meta.app_label,
-                            model_name=ct.model_class()._meta.model_name,
-                            target=0,
-                            expiration_date=timezone.now()
-                        )
-                else:
-                    if not device.is_central_server:
-                        try:
-                            Quota.objects.get(
-                                app_label=ct.model_class()._meta.app_label,
-                                model_name=ct.model_class()._meta.model_name,
-                            )
-                        except Quota.DoesNotExist:
-                            Quota.objects.create(
-                                app_label=ct.model_class()._meta.app_label,
-                                model_name=ct.model_class()._meta.model_name,
-                                target=0,
-                                expiration_date=timezone.now()
-                            )
+#     def create_quota(self):
+#         for ct in ContentType.objects.filter(app_label='bcpp_subject'):
+#             if ct is None:
+#                 continue
+#             if issubclass(ct.model_class(), Quota) or issubclass(ct.model_class(), QuotaModelWithOverride):
+#                 if device.is_community_server:
+#                     try:
+#                         ControllerQuota.objects.get(
+#                             app_label=ct.model_class()._meta.app_label,
+#                             model_name=ct.model_class()._meta.model_name,
+#                         )
+#                     except ControllerQuota.DoesNotExist:
+#                         ControllerQuota.objects.create(
+#                             app_label=ct.model_class()._meta.app_label,
+#                             model_name=ct.model_class()._meta.model_name,
+#                             target=0,
+#                             expiration_date=timezone.now()
+#                         )
+#                 else:
+#                     if not device.is_central_server:
+#                         try:
+#                             Quota.objects.get(
+#                                 app_label=ct.model_class()._meta.app_label,
+#                                 model_name=ct.model_class()._meta.model_name,
+#                             )
+#                         except Quota.DoesNotExist:
+#                             Quota.objects.create(
+#                                 app_label=ct.model_class()._meta.app_label,
+#                                 model_name=ct.model_class()._meta.model_name,
+#                                 target=0,
+#                                 expiration_date=timezone.now()
+#                             )
 
 bcpp_app_configuration = BcppAppConfiguration()
