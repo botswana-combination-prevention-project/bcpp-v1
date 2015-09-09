06791655aaa8c4716ede6a294339b470eb12aeb8
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-09 09:49:14 +0200

    subclassing quota manager to create pimavl manager

---
+++ bhp066/apps/bcpp_subject/managers/pima_vl_manager.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+
+from django.conf import settings
+from django.db import models
+
+from edc.map.classes import site_mappers
+from edc_quota.client.models import QuotaManager
+
+from apps.bcpp_household.classes import PlotIdentifier
+
+
+class PimaVlManager(QuotaManager):
+
+    def get_by_natural_key(self, report_datetime, visit_instance, code, subject_identifier_as_pk):
+        SubjectVisit = models.get_model('bcpp_subject', 'SubjectVisit')
+        subject_visit = SubjectVisit.objects.get_by_natural_key(report_datetime, visit_instance, code, subject_identifier_as_pk)
+        return self.get(subject_visit=subject_visit)
+
+    def get_queryset(self):
+        if settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY:
+            community = site_mappers.current_mapper.map_area
+            if PlotIdentifier.get_notebook_plot_lists():
+                return super(PimaVlManager, self).get_queryset().filter(
+                    subject_visit__household_member__household_structure__household__plot__community=community,
+                    subject_visit__household_member__household_structure__household__plot__plot_identifier__in=PlotIdentifier.get_notebook_plot_lists()
+                    )
+            else:
+                return super(PimaVlManager, self).get_queryset().filter(
+                    subject_visit__household_member__household_structure__household__plot__community=community)
+        return super(PimaVlManager, self).get_queryset()
--- bhp066/apps/bcpp_subject/models/pima_vl.py
+++ bhp066/apps/bcpp_subject/models/pima_vl.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 
 from django.utils.translation import ugettext_lazy as _
@@ -10,16 +11,21 @@ from edc.base.model.fields import OtherCharField
 from edc.base.model.validators import datetime_not_future
 from edc.choices.common import YES_NO, PIMA, PIMA_SETTING_VL
 
-from edc_quota.client.models import QuotaMixin, QuotaManager
+from edc_quota.client.models import QuotaMixin
 
 
-from .base_scheduled_visit_model import BaseScheduledVisitModel
+from edc.subject.consent.models import BaseConsentedUuidModel
 
 from apps.bcpp.choices import EASY_OF_USE
-from datetime import date
+from apps.bcpp_household.models import Plot
+
+from ..managers import PimaVlManager
+from .subject_visit import SubjectVisit
 
 
-class PimaVl (QuotaMixin, BaseScheduledVisitModel):
+class PimaVl (QuotaMixin, BaseConsentedUuidModel):
+
+    subject_visit = models.ForeignKey(SubjectVisit, null=True)
 
     poc_vl_type = models.CharField(
         verbose_name=_("Type mobile or household setting"),
@@ -100,13 +106,25 @@ class PimaVl (QuotaMixin, BaseScheduledVisitModel):
 
     history = AuditTrail()
 
-    objects = QuotaManager()
+    objects = PimaVlManager()
 
     def bypass_for_edit_dispatched_as_item(self, using=None, update_fields=None):
         return True
 
-    def bypass_meta_data(self):
-        return True
+    def natural_key(self):
+        return self.subject_visit.natural_key()
+
+    def get_visit(self):
+        return self.subject_visit
+
+    def get_subject_identifier(self):
+        return self.get_visit().get_subject_identifier()
+
+    def get_report_datetime(self):
+        return self.created
+
+    def dispatch_container_lookup(self, using=None):
+        return (Plot, 'subject_visit__household_member__household_structure__household__plot__plot_identifier')
 
     class Meta:
         app_label = 'bcpp_subject'
