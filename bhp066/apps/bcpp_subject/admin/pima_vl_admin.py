83671ab2a0a79fd81d0b0336569db243d8883189
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-14 16:10:54 +0200

    corrected import error in pima_vl_admin

--- bhp066/apps/bcpp_subject/admin/pima_vl_admin.py
+++ bhp066/apps/bcpp_subject/admin/pima_vl_admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.utils.encoding import force_unicode
-from django.db.models import get_model
-from datetime import datetime
-
-from edc_quota import Override
-
 
 from .subject_visit_model_admin import SubjectVisitModelAdmin
 from ..models import PimaVl
 from ..forms import PimaVlForm
-#from ..filters import Cd4ThreshHoldFilter
 
 
 class PimaVlAdmin(SubjectVisitModelAdmin):
@@ -36,28 +29,5 @@ class PimaVlAdmin(SubjectVisitModelAdmin):
         'poc_vl_today': admin.VERTICAL,
         'poc_vl_today_other': admin.VERTICAL,
         'easy_of_use': admin.VERTICAL}
-
-#     @property
-#     def client_key_instructions(self):
-#         return (
-#             'You have reached quota limit. Send client code: <span style="color:red;">{0}</span>'
-#             'to CBS for confirmation key to increase quota limit.'
-#             'Otherwise ignore the key if you are still waiting for confirmation code.'.format(self.quota_code())
-#         ) if self.quota_code() else ''
-#
-#     @property
-#     def required_instructions(self):
-#         required_instructions = (
-#             'Required questions are in bold. '
-#             'When all required data has been entered click SAVE to return to the dashboard '
-#             'or SAVE NEXT to go to the next form (if available). Additional questions may be required'
-#             'or may need to be corrected when you attempt to save.<br><strong>{}</strong>'.format(self.client_key_instructions))
-#         return required_instructions
-# 
-#     def quota_code(self):
-#         try:
-#             return Override().code if PimaVl().quota_reached else False
-#         except:
-#             return False
 
 admin.site.register(PimaVl, PimaVlAdmin)
