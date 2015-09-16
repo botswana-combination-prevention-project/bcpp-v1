10574fb8853aa770e90e80ebfee0323afe7d3bec
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-09-16 12:45:42 +0200

    link edc-quota create override view with bcpp
    Former-commit-id: f40ce39fef0ae7e4dff5f5cf577cae1ff041d70c

--- bhp066/config/urls.py
+++ bhp066/config/urls.py
@@ -115,6 +115,7 @@ urlpatterns += patterns(
     '',
     url(r'^dispatch/{app_name}/'.format(app_name=APP_NAME), include('apps.bcpp_dispatch.urls')),
     url(r'^bcpp_household/{app_name}/'.format(app_name=APP_NAME), include('apps.bcpp_household.urls')),
+    url(r'^bcpp_subject/{app_name}/'.format(app_name=APP_NAME), include('apps.bcpp_subject.urls')),
 )
 
 urlpatterns += patterns(
