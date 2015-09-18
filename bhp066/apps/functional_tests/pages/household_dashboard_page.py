05c01601f3ec84ada3cc545db9ab31b96385b235
Florence Chilisa <fchilisa@fchilisa.local>
2015-09-18 16:44:49 +0200

    more pages
    Former-commit-id: efe9091deb0c1e8b1ece7e978d53252a67ceb8c2

--- bhp066/apps/functional_tests/pages/household_dashboard_page.py
+++ bhp066/apps/functional_tests/pages/household_dashboard_page.py
@@ -4,6 +4,10 @@ from .base_page import BasePage
 
 class HouseholdDashboardPage(BasePage):
     household_log_entry = (By.LINK_TEXT, "Add a household log entry")
+    household_member = (By.LINK_TEXT, 'Add another household member')
 
-    def click_addhousehouldentry(self, household_log_entry):
+    def click_add_househouldentry(self, household_log_entry):
         self.browser.find_element(*HouseholdDashboardPage.household_log_entry).click()
+
+    def click_add_householdmember(self):
+        self.browser.find_element(*HouseholdDashboardPage.household_member).click()
