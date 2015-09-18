97b1d86f867ae49fa3fe8aa7047649202fb71b19
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-09-18 13:05:31 +0200

    add plot to base selenium
    Former-commit-id: d2491bac7e1e0ac6b3b6ac49cfdc2b2a5cf272f4

--- bhp066/apps/functional_tests/base_selinium_test.py
+++ bhp066/apps/functional_tests/base_selinium_test.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth.models import User
 
 from .pages.login_page import LoginPage
+from apps.bcpp_household.tests.factories import PlotFactory
 
 
 class BaseSeleniumTest(StaticLiveServerTestCase):
@@ -11,14 +12,18 @@ class BaseSeleniumTest(StaticLiveServerTestCase):
     username = 'testuser'
     password = '12345'
     email = 'testuser@123.org'
+    app_label = 'bcpp_subject'
+    community = 'otse'
 
     def setUp(self):
         User.objects.create_superuser(self.username, self.email, self.password)
         super().setUp()
         self.browser = webdriver.Firefox()
         self.browser.get(self.live_server_url)
+        self.plot = PlotFactory(community=self.community, household_count=1, status='residential_habitable', plot_identifier='400007-03')
 
     def tearDown(self):
+        self.plot.delete()
         self.browser.quit()
 
     def login(self):
