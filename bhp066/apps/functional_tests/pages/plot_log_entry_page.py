d36792dd6ea6b9dfc8b7c3a980b52c72b683b801
Florence Chilisa <fchilisa@fchilisa.local>
2015-09-18 14:41:00 +0200

    fix fill log entry
    Former-commit-id: ddb779926cad41a327f5898866a6362519a07c42

--- bhp066/apps/functional_tests/pages/plot_log_entry_page.py
+++ bhp066/apps/functional_tests/pages/plot_log_entry_page.py
@@ -25,11 +25,20 @@ class PlotLogEntryPage(BasePage):
     def click_today(self):
         self.browser.find_element(*PlotLogEntryPage.today).click()
 
+    def click_now(self):
+        self.browser.find_element(*PlotLogEntryPage.now).click()
+
     def click_save_button(self):
         self.browser.find_element(*PlotLogEntryPage.save_button).click()
 
-    def fill_plot_log_entry(self, date, time, plot_status):
-        self.set_report_date(date)
-        self.set_report_time(time)
+    def fill_plot_log_entry(self, date=None, time=None, plot_status):
+        if not date:
+            self.click_today()
+        else:
+            self.set_report_date(date)
+        if not time:
+            self.click_now()
+        else:
+            self.set_report_time(time)
         self.set_plot_status(plot_status)
         self.click_save_button()
