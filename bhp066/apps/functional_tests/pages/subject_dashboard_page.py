32e2d91d34dd381a574ceac868816f2e9fc740d5
Florence Chilisa <fchilisa@fchilisa.local>
2015-09-18 20:23:06 +0200

    page minor fix

--- bhp066/apps/functional_tests/pages/subject_dashboard_page.py
+++ bhp066/apps/functional_tests/pages/subject_dashboard_page.py
@@ -5,5 +5,5 @@ from .base_page import BaseModelAdminPage
 class SubjectDasbhoardPage(BaseModelAdminPage):
     new_subject_consent = (By.LINK_TEXT, 'subject consent (new)')
 
-    def click_subject_consent(self):
+    def click_new_subject_consent(self):
         self.browser.find_element(*SubjectDasbhoardPage.new_subject_consent).click()
