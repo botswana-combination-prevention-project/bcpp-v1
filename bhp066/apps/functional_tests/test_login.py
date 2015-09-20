4f6734b7695234fdaa5c1fdef4491db3ac23ac67
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-09-20 12:40:43 +0200

    selenium tests
    Former-commit-id: 974a28c66625c50bed9dbabbc8ea65d4790791ef

--- bhp066/apps/functional_tests/test_login.py
+++ bhp066/apps/functional_tests/test_login.py
@@ -8,4 +8,4 @@ class LoginSeleniumTest(BaseSeleniumTest):
     def test_success_login(self):
         self.login()
         time.sleep(1)
-        self.assertIn('/home/', self.browser.current_url)
+        self.assertIn('http://localhost:8000/bcpp/section/', self.browser.current_url)
