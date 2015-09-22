2914f9531f4d03cf12d6930a071e4767af77ba3f
Florence Chilisa <fchilisa@fchilisa.local>
2015-09-22 16:53:12 +0200

    selenium model tests
    Former-commit-id: 7fd4566dca8a059da16fbc7da35d05c8bee777a5

--- bhp066/apps/functional_tests/pages/subject_dashboard_page.py
+++ bhp066/apps/functional_tests/pages/subject_dashboard_page.py
@@ -7,8 +7,9 @@ class SubjectDasbhoardPage(BaseModelAdminPage):
     new_subject_consent = (By.XPATH, ".//*[@id='left']/tbody/tr[1]/td/a")
     new_visit_link = (By.XPATH, ".//*[@id='left']/tbody/tr[2]/td/table/tbody/tr/td[6]/a")
     show_forms_link = (By.XPATH, ".//*[@id='left']/tbody/tr[2]/td/table/tbody/tr/td[8]/a")
+    residency_and_mobility_link = (By.XPATH, ".//*[@id='left']/tbody/tr[5]/td/table/tbody/tr[2]/td[3]/a")
 
-    subject_dashboard_url ="http://localhost:8000/bcpp/dashboard/subject/visit/1ac500c6-bb5c-4ff1-b0ea-18f657a1dcb0/forms/"
+    subject_dashboard_url ="http://localhost:8000/bcpp/dashboard/subject/household_member/1dfb8ee2-ab1e-4381-8ccb-44d6764e7a1f/appointments/"
 
     def click_new_subject_consent(self):
         self.browser.find_element(*SubjectDasbhoardPage.new_subject_consent).click()
@@ -18,6 +19,9 @@ class SubjectDasbhoardPage(BaseModelAdminPage):
 
     def click_show_forms_link(self):
         self.browser.find_element(*SubjectDasbhoardPage.show_forms_link).click()
+
+    def click_residency_mobility_link(self):
+        self.browser.find_element(*SubjectDasbhoardPage.residency_and_mobility_link).click()
 
     def is_show_forms_link_visible(self):
         try:
