7fb1571e953b0c1d14bfd92cae7ebce453d1caa3
Leslie <leslie.kep@gmail.com>
2015-09-22 18:04:47 +0200

    subject locator fill form
    Former-commit-id: 81a6272a4870b064fe6fe32022c9d63e821a4dc2

---
+++ bhp066/apps/functional_tests/test_subject_visits.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from .base_selinium_test import BaseSeleniumTest
+
+from .pages import SubjectDasbhoardPage, SubjectLocatorPage
+
+
+class TestSubjectVisitModels(BaseSeleniumTest):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def open_subject_dashboard(self):
+        self.login()
+        sb_dash = SubjectDasbhoardPage(self.browser)
+        self.browser.get(sb_dash.subject_dashboard_url)
+        sb_dash.click_show_forms_link()
+
+    def test_subject_locator_link(self):
+        self.open_subject_dashboard()
+        self.subject_dash = SubjectDasbhoardPage(self.browser)
+        if self.subject_dash.click_subject_locator_link():
+            self.assertIn('/admin/bcpp_subject/subjectlocator/add', self.browser.current_url)
+
+    def test_fill_subject_locator_page(self):
+        self.test_subject_locator_link()
+        locator = SubjectLocatorPage(self.browser)
+        locator.fill_subject_locator(
+            locator.home_visit_permission_yes, locator.may_follow_up_yes,
+            locator.may_sms_follow_up_yes, locator.may_call_work_yes, locator.may_contact_someone_yes,
+            locator.has_alt_contact_yes
+        )
