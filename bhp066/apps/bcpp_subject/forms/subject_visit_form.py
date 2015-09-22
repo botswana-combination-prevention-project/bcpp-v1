22e21a71dbcec9389da3355906d8b79e5e0d73ab
erikvw <ew2789@gmail.com>
2015-09-22 09:20:07 +0200

    add subject visit form
    Former-commit-id: ba242c3f1720bb3dd0a4167c53d282ec20d229bc

--- bhp066/apps/bcpp_subject/forms/subject_visit_form.py
+++ bhp066/apps/bcpp_subject/forms/subject_visit_form.py
@@ -3,6 +3,7 @@ from django import forms
 from bhp066.apps.bcpp.base_model_form import BaseModelForm
 
 from ..models import SubjectVisit
+from ..models import SubjectConsent
 
 
 class SubjectVisitForm (BaseModelForm):
@@ -10,6 +11,7 @@ class SubjectVisitForm (BaseModelForm):
     def clean(self):
         cleaned_data = super(SubjectVisitForm, self).clean()
         subject_identifier = cleaned_data.get('household_member').get_subject_identifier()
+        self.instance.CONSENT_MODEL = SubjectConsent
         self.instance.consented_for_period_or_raise(
             subject_identifier=subject_identifier,
             exception_cls=forms.ValidationError)
