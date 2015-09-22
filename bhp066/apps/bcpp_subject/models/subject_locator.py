562459c8af78a46c56247742f92e5434328a3f9c
erikvw <ew2789@gmail.com>
2015-09-22 10:42:55 +0200

    grant / locator require consent
    Former-commit-id: fd75770a0b6a029b7f3241f1146d9795705c06ef

--- bhp066/apps/bcpp_subject/models/subject_locator.py
+++ bhp066/apps/bcpp_subject/models/subject_locator.py
@@ -125,12 +125,10 @@ class SubjectLocator(ExportTrackingFieldsMixin, SubjectOffStudyMixin, BaseLocato
         return self.subject_visit
 
     def get_subject_identifier(self):
-        if self.get_visit():
+        try:
             return self.get_visit().get_subject_identifier()
-        return None
-
-    def get_report_datetime(self):
-        return self.created
+        except AttributeError:
+            return self.registered_subject.subject_identifier
 
     @property
     def ready_to_export_transaction(self):
