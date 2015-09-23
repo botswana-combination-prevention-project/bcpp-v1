6e46ef10de7f5934f9db801840953537376465fe
ckgathi <ckgathi@bhp.org.bw>
2015-09-23 10:56:07 +0200

    add get_by_natural_key for sync
    Former-commit-id: 447b70a93ec4cd878f87bc8ac86bbb25dfa9c495

--- bhp066/apps/bcpp_subject/managers/subject_consent_manager.py
+++ bhp066/apps/bcpp_subject/managers/subject_consent_manager.py
@@ -8,6 +8,9 @@ from bhp066.apps.bcpp_household.classes import PlotIdentifier
 
 class SubjectConsentManager(models.Manager):
 
+    def get_by_natural_key(self, subject_identifier_as_pk):
+        return self.get(subject_identifier_as_pk=subject_identifier_as_pk)
+
     def get_queryset(self):
         if settings.LIMIT_EDIT_TO_CURRENT_COMMUNITY:
             community = site_mappers.get_current_mapper().map_area
