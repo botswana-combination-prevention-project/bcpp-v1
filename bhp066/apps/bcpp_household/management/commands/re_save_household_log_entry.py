9d6a6ccebf83a8d3cd5fca94b700b8f1aee9760a
ckgathi <ckgathi@bhp.org.bw>
2015-09-28 09:20:52 +0200

    re-save household log entries

---
+++ bhp066/apps/bcpp_household/management/commands/re_save_household_log_entry.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from bhp066.apps.bcpp_household.models import HouseholdLog
+
+
+class Command(BaseCommand):
+
+    APP_NAME = 0
+    MODEL_NAME = 1
+    args = '<community name e.g otse>'
+    help = 'Re save household log entries.'
+
+    def handle(self, *args, **options):
+        if not args or len(args) < 2:
+            raise CommandError('Missing \'using\' parameters.')
+        community_name = args[0]
+        household_log_entries = HouseholdLog.objects.filter(
+            household_log__household_structure__household__plot__community=community_name)
+        total_entries = len(household_log_entries)
+        count = 0
+        for h_log_entries in household_log_entries:
+            h_log_entries.save()
+            count += 1
+            print count, " of ", total_entries
