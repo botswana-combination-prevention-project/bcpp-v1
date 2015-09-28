1846014c02da564fd2e7c64c2ef1688945cb8062
ckgathi <ckgathi@bhp.org.bw>
2015-09-28 14:52:43 +0200

    fix re-save log entries
    Former-commit-id: c419de4c6a6fdb83569375103cd4af239e168f2b

--- bhp066/apps/bcpp_household/management/commands/re_save_household_log_entry.py
+++ bhp066/apps/bcpp_household/management/commands/re_save_household_log_entry.py
@@ -19,7 +19,8 @@ class Command(BaseCommand):
             household_log__household_structure__household__replaced_by__isnull=True)
         total_entries = len(household_log_entries)
         count = 0
-        for h_log_entries in household_log_entries:
-            h_log_entries.save()
-            count += 1
-            print count, " of ", total_entries
+        for h_log_entry in household_log_entries:
+            if not h_log_entry.household_log.household_structure.household.replaced_by:
+                h_log_entry.save()
+                count += 1
+                print count, " of ", total_entries
