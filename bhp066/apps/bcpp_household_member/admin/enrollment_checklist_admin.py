84103e1c64e8791cd33a6d9c498325e6b578350d
erikvw <ew2789@gmail.com>
2015-09-29 08:40:54 +0200

    fix formfield_for_foreignkey
    Former-commit-id: fd54fbf42be1a9ad914a291bea628f4dda7a2c57

--- bhp066/apps/bcpp_household_member/admin/enrollment_checklist_admin.py
+++ bhp066/apps/bcpp_household_member/admin/enrollment_checklist_admin.py
@@ -57,10 +57,16 @@ class EnrollmentChecklistAdmin(BaseModelAdmin):
     search_fields = ('household_member__first_name', 'household_member__pk')
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "household_structure":
-            kwargs["queryset"] = HouseholdStructure.objects.filter(id__exact=request.GET.get('household_structure', 0))
         if db_field.name == "household_member":
-            kwargs["queryset"] = HouseholdMember.objects.filter(id__exact=request.GET.get('household_member', 0))
+            if request.GET.get('household_member'):
+                kwargs["queryset"] = HouseholdMember.objects.filter(
+                    id__exact=request.GET.get('household_member', 0))
+            else:
+                self.readonly_fields = list(self.readonly_fields)
+                try:
+                    self.readonly_fields.index('household_member')
+                except ValueError:
+                    self.readonly_fields.append('household_member')
         return super(EnrollmentChecklistAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(EnrollmentChecklist, EnrollmentChecklistAdmin)
