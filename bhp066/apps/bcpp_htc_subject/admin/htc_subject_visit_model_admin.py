0cc39a0c0297ab0fb81e0f44ae43739922e7ca7c
erikvw <ew2789@gmail.com>
2015-09-29 08:08:00 +0200

    consildate base class

--- bhp066/apps/bcpp_htc_subject/admin/htc_subject_visit_model_admin.py
+++ bhp066/apps/bcpp_htc_subject/admin/htc_subject_visit_model_admin.py
@@ -7,6 +7,6 @@ class HtcSubjectVisitModelAdmin (BaseVisitTrackingModelAdmin):
     """Model Admin for models with a foreignkey to the htc subject visit model."""
 
     visit_model = HtcSubjectVisit
-    visit_model_foreign_key = 'htc_subject_visit'
+    visit_attr = 'htc_subject_visit'
     dashboard_type = 'htc_subject'
     date_heirarchy = 'htc_subject_visit__report_datetime'
