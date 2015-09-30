8e536afbdbb1da6d34bb7f1ab610a8e576778299
erikvw <ew2789@gmail.com>
2015-09-30 08:26:04 +0200

    removed unused files
    Former-commit-id: f861ed52cf6b25574805cb1bdbc1f7a4bf357880

--- misc/setup/make_gittrees.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-import settings
-f = open('gitrees.sample','w')
-for app in settings.INSTALLED_APPS:
-    if not app.startswith('django') and app not in ['south', 'dajax', 'dajaxice', 'autocomplete', 'djcelery', 'databrowse']:
-        print('[subtree "{0}"]\n    url = git@gitserver:{0}\n    path =  {0}\n    branch = master\n\n'.format(app), file=f)
