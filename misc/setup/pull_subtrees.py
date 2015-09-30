2ba33218dd229987a7862e0baf70b8747cb42040
erikvw <ew2789@gmail.com>
2015-09-30 08:26:04 +0200

    removed unused files
    Former-commit-id: f861ed52cf6b25574805cb1bdbc1f7a4bf357880

--- misc/setup/pull_subtrees.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-import settings
-f = open('pull_subtrees.sh','w')
-for app in settings.INSTALLED_APPS:
-    if not app.startswith('django') and app not in ['south', 'dajax', 'dajaxice', 'autocomplete', 'tastypie']:
-        print('git subtree pull --prefix {0} --squash git@192.168.1.50:{0} master'.format(app), file=f)
-        print('git commit -a -m "pull {0} subtree"'.format(app), file=f)
