69521bda53c54ee5567eb23adcad4736e4473125
erikvw <ew2789@gmail.com>
2015-10-05 08:05:25 +0200

    change admin baseline, annual to use edc_admin_exclude

--- bhp066/apps/bcpp_subject/admin/hiv_result_documentation_admin.py
+++ bhp066/apps/bcpp_subject/admin/hiv_result_documentation_admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
+from ..constants import ANNUAL, BASELINE
 from ..forms import HivResultDocumentationForm
 from ..models import HivResultDocumentation
 
+from .subject_admin_exclude_mixin import SubjectAdminExcludeMixin
 from .subject_visit_model_admin import SubjectVisitModelAdmin
 
 
-class HivResultDocumentationAdmin (SubjectVisitModelAdmin):
+class HivResultDocumentationAdmin (SubjectAdminExcludeMixin, SubjectVisitModelAdmin):
 
     form = HivResultDocumentationForm
     fields = (
@@ -16,23 +18,24 @@ class HivResultDocumentationAdmin (SubjectVisitModelAdmin):
     radio_fields = {
         'result_doc_type': admin.VERTICAL, }
 
-    baseline_instructions = [
-        ("This section collects information on whether or not the"
-         " participant has either:"
-         " <ol><li>documentation of an HIV test result other than the"
-         " most recent HIV test (positive, negative, or "
-         " indeterminate) ; OR </li>"
-         " <li> documentation that supports a previous diagnosis of"
-         " HIV, if record of positive HIV test is not available.</li></ol> ")
-    ]
-
-    annual_instructions = [
-        ("This section collects information on whether or not the"
-         " participant has either:"
-         " <ol><li>documentation of an HIV test result other than the"
-         " most recent HIV test; OR</li>"
-         " <li>documentation that supports a previous diagnosis of"
-         " HIV, if record of positive HIV test is not available.</li></ol>")
-    ]
+    instructions = {
+        BASELINE: [
+            ("This section collects information on whether or not the"
+             " participant has either:"
+             " <ol><li>documentation of an HIV test result other than the"
+             " most recent HIV test (positive, negative, or "
+             " indeterminate) ; OR </li>"
+             " <li> documentation that supports a previous diagnosis of"
+             " HIV, if record of positive HIV test is not available.</li></ol> (baseline) ")
+        ],
+        ANNUAL: [
+            ("This section collects information on whether or not the"
+             " participant has either:"
+             " <ol><li>documentation of an HIV test result other than the"
+             " most recent HIV test; OR</li>"
+             " <li>documentation that supports a previous diagnosis of"
+             " HIV, if record of positive HIV test is not available.</li></ol> (annual)")
+        ]
+    }
 
 admin.site.register(HivResultDocumentation, HivResultDocumentationAdmin)
