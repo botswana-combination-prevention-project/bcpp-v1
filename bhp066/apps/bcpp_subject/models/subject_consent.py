e94c18a57bb3236badea275e84ad4279a6f77d15
erikvw <ew2789@gmail.com>
2015-10-05 15:49:20 +0200

    remove class Constant on consent
    Former-commit-id: 8267eaf587c1ec281b9c3a3c1d2520cf9c48af45

--- bhp066/apps/bcpp_subject/models/subject_consent.py
+++ bhp066/apps/bcpp_subject/models/subject_consent.py
@@ -317,12 +317,11 @@ class SubjectConsentExtended(SubjectConsent):
 
     """ A system model that serves as a proxy model for SubjectConsent."""
 
-    class Constants:
-        SUBJECT_TYPES = ['subject']
-        GENDER_OF_CONSENT = ['M', 'F']
-        AGE_IS_ADULT = 18
-        MIN_AGE_OF_CONSENT = 16
-        MAX_AGE_OF_CONSENT = 120
+    SUBJECT_TYPES = ['subject']
+    GENDER_OF_CONSENT = ['M', 'F']
+    AGE_IS_ADULT = 18
+    MIN_AGE_OF_CONSENT = 16
+    MAX_AGE_OF_CONSENT = 120
 
     class Meta:
         proxy = True
