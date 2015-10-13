59bad1d087b11bcc980f92b08de0b2feb8919475
erikvw <ew2789@gmail.com>
2015-10-13 12:42:08 +0200

    update preorder forms  validation
    Former-commit-id: 39503ecab0a0d24a1eac7e5523a2fb89ee263f38

--- bhp066/apps/bcpp_lab/forms/pre_order_form.py
+++ bhp066/apps/bcpp_lab/forms/pre_order_form.py
@@ -1,9 +1,22 @@
+from django import forms
+
 from bhp066.apps.bcpp.base_model_form import BaseModelForm
 
 from ..models import PreOrder
 
 
 class PreOrderForm (BaseModelForm):
+
+    def clean(self):
+        cleaned_data = super(PreOrderForm, self).clean()
+        self._meta.model().aliquot_exists_or_raise(
+            cleaned_data.get('aliquot_identifier'),
+            cleaned_data.get('subject_visit'),
+            exception_cls=forms.ValidationError)
+        self._meta.model().aliquot_unused_or_raise(
+            cleaned_data.get('aliquot_identifier'),
+            exception_cls=forms.ValidationError)
+        return cleaned_data
 
     class Meta:
         model = PreOrder
