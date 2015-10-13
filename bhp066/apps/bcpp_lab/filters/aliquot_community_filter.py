c6f29d9c4f7760a2b1b2aa0f6b3eea06d9763d02
erikvw <ew2789@gmail.com>
2015-10-13 13:54:21 +0200

    move filter to file, use constants
    Former-commit-id: 69e82bdb220a8aec2345d4143e5431fd7614d511

--- bhp066/apps/bcpp_lab/filters/aliquot_community_filter.py
+++ bhp066/apps/bcpp_lab/filters/aliquot_community_filter.py
@@ -1,13 +1,9 @@
 from django.contrib.admin import SimpleListFilter
 from django.utils.translation import ugettext_lazy as _
 
-from edc_constants.choices import YES_NO
-from edc_constants.constants import YES, NO
-
 from bhp066.apps.bcpp.choices import COMMUNITIES
-from bhp066.apps.bcpp_subject.constants import POC_VIRAL_LOAD
 
-from ..models import SubjectRequisition, Receive, PreOrder
+from ..models import SubjectRequisition, Receive
 
 
 class AliquotCommunityFilter(SimpleListFilter):
@@ -24,22 +20,4 @@ class AliquotCommunityFilter(SimpleListFilter):
             req_identifiers = [req.requisition_identifier for req in requisition]
             receives = Receive.objects.filter(requisition_identifier__in=req_identifiers)
             return queryset.filter(receive__in=receives)
-        return queryset
-
-
-class PocViralLoadRequsitions(SimpleListFilter):
-
-    title = _('poc_vl')
-    parameter_name = 'poc_vl'
-
-    def lookups(self, request, model_admin):
-        return YES_NO
-
-    def queryset(self, request, queryset):
-        if self.value():
-            subject_visits = [obj.subject_visit for obj in PreOrder.objects.filter(panel__name=POC_VIRAL_LOAD)]
-            if self.value() == YES:
-                return queryset.filter(subject_visit__in=subject_visits)
-            elif self.value() == NO:
-                return queryset.exclude(subject_visit__in=subject_visits)
         return queryset
