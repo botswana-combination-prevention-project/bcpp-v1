60b7cd8ec6e8347ab8211d56b5946cd41729ac88
Leslie <leslie.kep@gmail.com>
2015-11-12 16:24:03 +0200

    update clinic base visit subclass
    Former-commit-id: 2f9a081dc6bb40bb66f6e5b5b40b5584be95e759

--- bhp066/apps/bcpp_clinic/forms/clinic_visit_form.py
+++ bhp066/apps/bcpp_clinic/forms/clinic_visit_form.py
@@ -1,9 +1,9 @@
-from edc_consent.forms import BaseConsentedModelForm
+from edc_consent.forms import BaseSubjectConsentForm
 
 from ..models import ClinicVisit
 
 
-class ClinicVisitForm (BaseConsentedModelForm):
+class ClinicVisitForm (BaseSubjectConsentForm):
 
     class Meta:
         model = ClinicVisit
