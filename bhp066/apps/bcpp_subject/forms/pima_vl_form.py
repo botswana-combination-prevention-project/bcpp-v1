ceef6bd93420146731f2476c0ebd66a05385d2c2
Coulson <ckgathi@bhp.org.bw>
2016-01-15 16:07:34 +0200

    method complexity

--- bhp066/apps/bcpp_subject/forms/pima_vl_form.py
+++ bhp066/apps/bcpp_subject/forms/pima_vl_form.py
@@ -13,6 +13,29 @@ class PimaVlForm (BaseSubjectModelForm):
 
     def clean(self):
         cleaned_data = super(PimaVlForm, self).clean()
+        self.check_preorder_exists(cleaned_data)
+
+        if cleaned_data.get('poc_vl_today') == NO:
+            if not cleaned_data.get('poc_vl_today_other'):
+                raise forms.ValidationError('If POC VL NOT done today, please explain why not?')
+            if cleaned_data.get('pima_id'):
+                raise forms.ValidationError('Do not provide the PIMA machine id if the POC VL was not performed')
+            if cleaned_data.get('poc_vl_value'):
+                raise forms.ValidationError('POC VL was not performed, do not provide the POC viral load count')
+
+        if cleaned_data.get('poc_vl_today') == YES:
+            if not cleaned_data.get('pima_id'):
+                raise forms.ValidationError('If POC VL done today, please provide machine id?')
+            if not cleaned_data.get('poc_vl_value'):
+                raise forms.ValidationError('If POC VL done today, what is the POC viral load count?')
+            if not cleaned_data.get('time_of_test') or not cleaned_data.get('time_of_result'):
+                raise forms.ValidationError('Time of test and time of result should be provided.')
+
+        return cleaned_data
+
+    def check_preorder_exists(self, cleaned_data):
+        """Check if preorder exists."""
+
         Panel = get_model('bcpp_lab', 'Panel')
         PreOrder = get_model('bcpp_lab', 'PreOrder')
         try:
@@ -23,22 +46,6 @@ class PimaVlForm (BaseSubjectModelForm):
                 aliquot_identifier__isnull=False)
         except PreOrder.DoesNotExist:
             raise forms.ValidationError('Please complete the pre-order before entering the result')
-        if cleaned_data.get('poc_vl_today') == NO:
-            if not cleaned_data.get('poc_vl_today_other'):
-                raise forms.ValidationError('If POC VL NOT done today, please explain why not?')
-            if cleaned_data.get('pima_id'):
-                raise forms.ValidationError('Do not provide the PIMA machine id if the POC VL was not performed')
-            if cleaned_data.get('poc_vl_value'):
-                raise forms.ValidationError('POC VL was not performed, do not provide the POC viral load count')
-        if cleaned_data.get('poc_vl_today') == YES:
-            if not cleaned_data.get('pima_id'):
-                raise forms.ValidationError('If POC VL done today, please provide machine id?')
-            if not cleaned_data.get('poc_vl_value'):
-                raise forms.ValidationError('If POC VL done today, what is the POC viral load count?')
-            if not cleaned_data.get('time_of_test') or not cleaned_data.get('time_of_result'):
-                raise forms.ValidationError('Time of test and time of result should be provided.')
-
-        return cleaned_data
 
     class Meta:
         model = PimaVl
