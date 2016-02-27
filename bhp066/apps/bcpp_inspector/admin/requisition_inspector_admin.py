134c2ee6b2b02cb4bfee52db5092add9e61d3bfb
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-02-27 17:12:09 +0200

    bcpp inspector flake8 issues

--- bhp066/apps/bcpp_inspector/admin/requisition_inspector_admin.py
+++ bhp066/apps/bcpp_inspector/admin/requisition_inspector_admin.py
@@ -8,9 +8,13 @@ from ..models import SubjectRequisitionInspector
 
 class RequisitionInspectorAdmin(BaseInspectorAdmin):
 
-    list_display = ['subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier', 'specimen_identifier', 'device_id']
-    list_filter = ['subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier', 'specimen_identifier', 'device_id']
-    search_fields = ['subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier', 'specimen_identifier', 'device_id']
+    list_display = [
+        'subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier',
+        'specimen_identifier', 'device_id']
+    list_filter = ['subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier',
+                   'specimen_identifier', 'device_id']
+    search_fields = ['subject_identifier', 'requisition_datetime', 'is_confirmed', 'requisition_identifier',
+                     'specimen_identifier', 'device_id']
 
     actions = [set_inspectors_as_confirmed]
 
