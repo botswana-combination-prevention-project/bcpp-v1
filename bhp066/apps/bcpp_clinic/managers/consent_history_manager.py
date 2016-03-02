7828ab9728530bb536e33bf814d160ba6c0365fc
Coulson <ckgathi@bhp.org.bw>
2016-03-02 15:17:52 +0200

    fix clinic consent manager

--- bhp066/apps/bcpp_clinic/managers/consent_history_manager.py
+++ bhp066/apps/bcpp_clinic/managers/consent_history_manager.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
 
 from django.db.models import get_model
+from django.db import models
+from django.core.exceptions import MultipleObjectsReturned
 
-from edc.subject.consent.managers import BaseConsentHistoryManager
 
-
-class ConsentHistoryManager(BaseConsentHistoryManager):
+class ConsentHistoryManager(models.Manager):
 
     def get_by_natural_key(self, consent_datetime, survey_name, subject_identifier_as_pk):
         margin = timedelta(minutes=5)
@@ -17,16 +17,33 @@ class ConsentHistoryManager(BaseConsentHistoryManager):
                         survey=survey, registered_subject=registered_subject)
 
     def update_consent_history(self, consent_inst, created, using):
-        updated_values = {'consent_datetime': consent_inst.consent_datetime,
-                          'survey': consent_inst.survey,
-                          'household_member': consent_inst.household_member}
-        inst, created = super(ConsentHistoryManager, self).using(using).get_or_create(
-            registered_subject=consent_inst.registered_subject,
-            consent_app_label=consent_inst._meta.app_label,
-            consent_model_name=consent_inst._meta.object_name,
-            consent_pk=consent_inst.pk,
-            defaults=updated_values
-        )
-        if not created:
+        try:
+            inst = self.get(
+                registered_subject=consent_inst.registered_subject,
+                consent_app_label=consent_inst._meta.app_label,
+                consent_model_name=consent_inst._meta.object_name,
+                consent_pk=consent_inst.pk)
             inst.consent_datetime = consent_inst.consent_datetime
             inst.save(using=using)
+        except MultipleObjectsReturned:
+            # not sure why this is happening????
+            inst = self.filter(
+                registered_subject=consent_inst.registered_subject,
+                consent_app_label=consent_inst._meta.app_label,
+                consent_model_name=consent_inst._meta.object_name,
+                consent_pk=consent_inst.pk).order_by('-created')[0]
+            inst.consent_datetime = consent_inst.consent_datetime
+            inst.save(using=using)
+        except self.model.DoesNotExist:
+            self.create(
+                registered_subject=consent_inst.registered_subject,
+                consent_app_label=consent_inst._meta.app_label,
+                consent_model_name=consent_inst._meta.object_name,
+                consent_pk=consent_inst.pk,
+                survey=consent_inst.survey,
+                household_member=consent_inst.household_member,
+                consent_datetime=consent_inst.consent_datetime)
+
+    def delete_consent_history(self, app_label, model_name, pk, using):
+        super(ConsentHistoryManager, self).filter(
+            consent_app_label=app_label, consent_model_name=model_name, consent_pk=pk).delete()
