c0e0714002be11d2c1e0ec51336bc27180ccfbd5
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-07-25 11:33:00 +0200

    survey dates pair 12

--- bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
+++ bhp066/apps/bcpp/app_configuration/classes/app_configuration.py
@@ -98,8 +98,8 @@ class BcppAppConfiguration(BaseAppConfiguration):
             {'survey_name': 'BCPP Year 2',
              'survey_slug': 'bcpp-year-2',
              'survey_abbrev': 'Y2',
-             'datetime_start': datetime(2016, 6, 28, 0, 0, 0),
-             'datetime_end': datetime(2016, 7, 28, 23, 59, 0),
+             'datetime_start': datetime(2016, 7, 26, 0, 0, 0),
+             'datetime_end': datetime(2016, 8, 28, 23, 59, 0),
              'chronological_order': 2},
         'bcpp-year-3':
             {'survey_name': 'BCPP Year 3',
