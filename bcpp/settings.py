55fc6e9f5be44fa2230c2d2ec93aa048b7c82d16
erikvw <ew2789@gmail.com>
2016-11-03 18:41:47 -0500

    move modules to root

--- bcpp/settings.py
+++ bcpp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bcpp_household.apps.AppConfig',
     'bcpp.apps.AppConfig',
 ]
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GIT_DIR = BASE_DIR
