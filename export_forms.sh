53da1d905df19746497b524f32f41a64894cd5db
erikvw <ew2789@gmail.com>
2016-11-03 18:14:05 -0500

    update export forms and rdb code

--- export_forms.sh
+++ export_forms.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "CPCT0" ]
 then 
-echo "Exporting CPC T0 from ~/source/bhp066"
-cd ~/source/bhp066
+echo "Exporting CPC T0 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
 echo "bcpp_subject"
 python manage.py export_forms --visit=T0  > ~/export_forms/export_forms_cpc_t0-survey.txt
 echo "bcpp_household"
@@ -15,8 +15,8 @@ python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_c
 fi
 if [ "$1" = "CPCT1" ]
 then
-echo "Exporting CPC T1 from ~/source/bhp066"
-cd ~/source/bhp066
+echo "Exporting CPC T1 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
 echo "bcpp_subject"
 python manage.py export_forms --visit=T1  > ~/export_forms/export_forms_cpc_t1-survey.txt
 echo "bcpp_household"
@@ -28,10 +28,25 @@ python manage.py export_forms --app bcpp_lab  > ~/export_forms/export_forms_cpc_
 echo "bcpp_clinic"
 python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_cpc_t1-clinic.txt
 fi
+if [ "$1" = "CPCT2" ]
+then
+echo "Exporting CPC T2 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
+echo "bcpp_subject"
+python manage.py export_forms --visit=T2  > ~/export_forms/export_forms_cpc_t2-survey.txt
+echo "bcpp_household"
+python manage.py export_forms --app bcpp_household  > ~/export_forms/export_forms_cpc_t2-household.txt
+echo "bcpp_household_member"
+python manage.py export_forms --app bcpp_household_member  > ~/export_forms/export_forms_cpc_t2-householdmember.txt
+echo "bcpp_lab"
+python manage.py export_forms --app bcpp_lab  > ~/export_forms/export_forms_cpc_t2-lab.txt
+echo "bcpp_clinic"
+python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_cpc_t2-clinic.txt
+fi
 if [ "$1" = "ECCT0" ]
 then
-echo "Exporting ECC T0 from ~/source/bhp066"
-cd ~/source/bhp066
+echo "Exporting ECC T0 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
 echo "bcpp_subject"
 python manage.py export_forms --visit=T0  > ~/export_forms/export_forms_ecc_t0-survey.txt
 echo "bcpp_household"
@@ -45,8 +60,8 @@ python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_e
 fi
 if [ "$1" = "ECCT1" ]
 then
-echo "Exporting ECC T1 from ~/source/bhp066"
-cd ~/source/bhp066
+echo "Exporting ECC T1 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
 echo "bcpp_subject"
 python manage.py export_forms --visit=T1  > ~/export_forms/export_forms_ecc_t1-survey.txt
 echo "bcpp_household"
@@ -57,5 +72,20 @@ echo "bcpp_lab"
 python manage.py export_forms --app bcpp_lab  > ~/export_forms/export_forms_ecc_t1-lab.txt
 echo "bcpp_clinic"
 python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_ecc_t1-clinic.txt
+fi
+if [ "$1" = "ECCT2" ]
+then
+echo "Exporting ECC T2 from ~/source/bhp066/bhp066_master"
+cd ~/source/bhp066/bhp066_master/
+echo "bcpp_subject"
+python manage.py export_forms --visit=T2  > ~/export_forms/export_forms_ecc_t2-survey.txt
+echo "bcpp_household"
+python manage.py export_forms --app bcpp_household  > ~/export_forms/export_forms_ecc_t2-household.txt
+echo "bcpp_household_member"
+python manage.py export_forms --app bcpp_household_member  > ~/export_forms/export_forms_ecc_t2-householdmember.txt
+echo "bcpp_lab"
+python manage.py export_forms --app bcpp_lab  > ~/export_forms/export_forms_ecc_t2-lab.txt
+echo "bcpp_clinic"
+python manage.py export_forms --app bcpp_clinic  > ~/export_forms/export_forms_ecc_t2-clinic.txt
 fi
 echo "Done"
